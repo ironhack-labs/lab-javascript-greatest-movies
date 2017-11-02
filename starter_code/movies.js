0616b8d9c5bd56a5873b8ddfb8b1aaf5a5ddd69a
Victor Arza <papu.arza@gmail.com>
2017-11-02 15:52:51 +0100

    exercise added

---
+++ starter_code/movies.js
@@ -0,0 +1,23 @@
+// Turn duration of the movies from hours to minutes  ---MAP---
+
+
+// Get the average of all rates with 2 decimals  ---REDUCE---
+
+
+// Get the average of Drama Movies ---FILTER AND REDUCE--
+
+
+// Order by time duration, in growing order
+
+
+// How many movies did STEVEN SPIELBERG ---FILTER AND REDUCE---
+
+
+// Order by title and print the first 20 titles
+
+
+// Best yearly rate average ---forEach and REDUCE---
+
+
+
+
