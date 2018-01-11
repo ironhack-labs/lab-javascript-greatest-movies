cbb38c03258ed836d3320f29fa4fb8ebbf3964e6
Pere Amorós <amorospere@gmail.com>
2018-01-11 15:33:00 +0100

    wip

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -5,9 +5,33 @@
 // Get the average of all rates with 2 decimals 
 
 
+function ratesAverage(elements){
+    var sum = elements.reduce(function(accumulator,item){
+        return accumulator + parseFloat(item.rate);
+    }, 0);
+    
+   return Math.round(sum / elements.length*100)/100;
+};
 // Get the average of Drama Movies
 
-
+function dramaMoviesRate(elements){
+    var count = 0;
+    var sum = elements.reduce(function(accumulator,item){
+        if(Number(item.rate) === 0){
+            count++;
+        }
+        if(item.genre.includes('Drama') <= 0){
+            count++;
+            return accumulator + parseFloat(item.rate);
+        }
+        else{
+            return accumulator;
+        }
+        
+    }, 0);
+    
+   return Math.round(sum / count*100)/100;
+};
 // Order by time duration, in growing order
 
 
