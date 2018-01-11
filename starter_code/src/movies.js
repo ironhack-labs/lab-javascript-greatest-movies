fea891c5e66e225c8a686d5822abda7e47986b5f
Ana Recuero <anarecsa@gmail.com>
2018-01-11 16:47:25 +0100

    par of iteration 1

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,6 +1,28 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
 
+function turnHoursToMinutes(a){
+  
+  var moviesMinutes = a.map(function(item){
+   return item;
+  });
+
+  moviesMinutes.map(function(mov){
+    
+    var hours = mov.duration.slice(0,1) * 60;
+    var minutes = mov.duration.slice(3,-3);
+  
+    var total = parseInt(hours) + parseInt(minutes)
+    
+    mov.duration = total
+    return mov.duration;
+  });
+  
+  return moviesMinutes
+}
+
+turnHoursToMinutes(movies)
+
 
 // Get the average of all rates with 2 decimals 
 
