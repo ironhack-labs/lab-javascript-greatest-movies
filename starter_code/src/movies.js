982522281c8141fa0ba5bc6f8fdd01a389035e62
kissy89 <stephanie.senoner@gmail.com>
2018-01-11 16:11:47 +0100

    done

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -4,8 +4,32 @@
 
 // Get the average of all rates with 2 decimals 
 
+function reduceFunction (sum, item){
+    return sum + item.rate;
+}
 
-// Get the average of Drama Movies
+function ratesAverage(arr) {
+    var averageRate = arr.reduce(reduceFunction , 0) / arr.length;
+    return averageRate;
+}
+
+//  Get the average of Drama Movies
+
+ function dramaMoviesRate(item){
+     return item.genre.includes("Drama");
+ }
+
+ function reduceDrama (sum, item, index){
+   return sum + parseFloat(item.rate);
+ }
+
+ function dramaMoviesAverage(arr) {
+   var drama = arr.filter(dramaMoviesRate);
+   
+   var averageDrama = drama.reduce(reduceDrama , 0) / drama.length;
+   return averageDrama;
+ };
+
 
 
 // Order by time duration, in growing order
