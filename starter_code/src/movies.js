14e24991439751d985547a40cc4542d5539a81f4
100fue <malvacien@gmail.com>
2018-01-11 21:33:51 +0100

    iteration 2 done

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,20 +1,39 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
-movies.map(function turnHoursToMinutes(e){
-    var dur = e.duration;
-    var dur2 = dur.split("h");
-    var hor = dur2[0] * 60;
-    var min = dur2[1];
-        min = min.split("min")[0];
-        
-    var oper = parseInt(hor) + parseInt(min);
-    
-    return oper; 
-})
+function turnHoursToMinutes(array) {
+    var long;
+    return array.map(function(e) {
+      if(e.duration.length < 3){
+        long = parseInt(e.duration.slice(0,1) * 60);
+        return Object.assign({}, e, { duration: long });
+      } else if (e.duration.length < 6){
+        long = parseInt(e.duration.slice(0,2));
+        return Object.assign({}, e, { duration: long });
+      } else {
+        long = parseInt(e.duration.slice(0,1) * 60) + parseInt(e.duration.slice(3,-3));
+        return Object.assign({}, e, { duration: long });
+      }
+    })
+  }
+   
+  turnHoursToMinutes(movies);
 
 
 // Get the average of all rates with 2 decimals 
 
+function ratesAverage(array){
+  
+  var val = array.map(function(e){
+  return e.rate; 
+  });
+
+  var media = array.reduce(function(acc,e){
+    return e.rate / array.length + acc;
+  },0);
+ 
+  return parseFloat(media.toFixed(2));
+}
+ratesAverage(movies);
 
 // Get the average of Drama Movies
 
