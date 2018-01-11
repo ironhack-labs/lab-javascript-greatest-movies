c638dc8da8a6b9474aa981c9d0b78b6e8c58ccbe
Pedro Neto <pedro.diogo.neto@outlook.pt>
2018-01-11 15:31:21 +0100

    iterations 2&3

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -5,8 +5,35 @@
 // Get the average of all rates with 2 decimals 
 
 
+function ratesAverage (array) {
+
+    var reduceFunction = function(rateSum, movie) {
+        return rateSum + Number(movie.rate);
+    };
+
+    var ratesTotal = array.reduce(reduceFunction, 0);
+
+    var average = ratesTotal / array.length;
+
+    return Math.round(100 * average) / 100;
+};
+
 // Get the average of Drama Movies
 
+function dramaMoviesRate(movies) {
+
+    function isDrama (movie) {
+        return movie.genre.indexOf("Drama") >= 0;
+    }
+
+    var dramaMovies = movies.filter(isDrama);
+
+    if (dramaMovies.length === 0) {
+        return;
+    }
+
+    return ratesAverage(dramaMovies);
+}
 
 // Order by time duration, in growing order
 
@@ -18,3 +45,4 @@
 
 
 // Best yearly rate average
+
