82c11587e4dcd7ef725dda6937e94b6d27ead277
Ana Recuero <anarecsa@gmail.com>
2018-01-11 18:13:02 +0100

    iteration 1 and 2

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,31 +1,62 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
 
-function turnHoursToMinutes(a){
-  
-  var moviesMinutes = a.map(function(item){
-   return item;
-  });
-
-  moviesMinutes.map(function(mov){
-    
-    var hours = mov.duration.slice(0,1) * 60;
-    var minutes = mov.duration.slice(3,-3);
-  
-    var total = parseInt(hours) + parseInt(minutes)
-    
-    mov.duration = total
-    return mov.duration;
-  });
-  
-  return moviesMinutes
+function turnHoursToMinutes(array) {
+  var long;
+  return array.map(function(e) {
+    if(e.duration.length < 3){
+      long = parseInt(e.duration.slice(0,1) * 60);
+      return Object.assign({}, e, { duration: long });
+    } else if (e.duration.length < 6){
+      long = parseInt(e.duration.slice(0,2));
+      return Object.assign({}, e, { duration: long });
+    } else {
+      long = parseInt(e.duration.slice(0,1) * 60) + parseInt(e.duration.slice(3,-3));
+      return Object.assign({}, e, { duration: long });
+    }
+  })
 }
 
-turnHoursToMinutes(movies)
-
+console.log(turnHoursToMinutes(movies))
 
 // Get the average of all rates with 2 decimals 
 
+function ratesAverage(a){
+
+  
+  var allRates = a.map(function(movie){
+    return movie.rate;
+  });
+  
+  allRates = allRates.reduce(function(acc,e,i){
+    return e/a.length + acc;
+  },0)
+  
+  
+  if(allRates.toString().indexOf(".") > -1){
+    allRates = allRates.toFixed(2);
+  }
+  return parseFloat(allRates);
+}
+
+
+// function ratesAverage(a){
+  
+//   var allRates = a.map(function(movie){
+//     return movie.rate;
+//   });
+  
+//   allRates = allRates.reduce(function(acc,e,i){
+//     return e/a.length + acc;
+//   },0)
+  
+  
+//   if(allRates.toString().indexOf(".") > -1){
+//     allRates = allRates.toFixed(2);
+//   }
+
+//   return parseInt(allRates);
+// }
 
 // Get the average of Drama Movies
 
