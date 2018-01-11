4317f2222deaa7682afba62ef0929f5bed05dee5
Oscar Jorge <oscar.jorge.jorge@hotmail.com>
2018-01-11 15:34:44 +0100

    en proceso

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -5,7 +5,55 @@
 // Get the average of all rates with 2 decimals 
 
 
+
+
+
+
+
+function ratesAverage (movies) {
+    var sumMo = movies.reduce(function(sum,item){
+        return sum + parseFloat(item.rate);
+    }, 0);
+    var average = Math.round(sumMo/movies.length*100)/100;
+    return average;
+}
+
+
 // Get the average of Drama Movies
+
+
+// var dramaFi = movies.filter(function (item) {
+//     if (item.genre === 'Drama') {
+//         return true;
+//     } else {
+//         return false;
+//     }   
+// } );
+
+// function dramaMoviesRate (){
+
+// }
+
+
+
+
+
+
+function dramaMoviesRate (movies){
+
+    var dramaFi = movies.filter(function(movies) { 
+        return movies.genre.includes('Drama');      
+  });
+    var sumMo = dramaFi.reduce(function(sum,item){
+        item.rate = item.rate || 0
+        return sum + parseFloat(item.rate);
+  }, 0);
+    var average = Math.round(sumMo/dramaFi.length*100)/100;
+        return average;
+
+}
+
+
 
 
 // Order by time duration, in growing order
