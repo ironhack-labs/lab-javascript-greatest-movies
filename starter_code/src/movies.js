332c7f646eab5f410e07bfe03f9d992b79432d99
Jose Sanchez <joseansanher@gmail.com>
2018-01-11 20:46:44 +0100

    forth de done

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,6 +1,23 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
+  
+  turnHoursToMinutes(movies, movies.duration);
 
+  function turnHoursToMinutes(array){
+    var arrayTiempo = [];
+    var arrayMin = [];
+    var time =0;
+    for(var i=0; i<array.length;i++){
+  
+      arrayTiempo.push(array[i].duration.split(' ')[0].substring(0, 1) * 60);
+      arrayMin.push(parseInt(array[i].duration.split(' ')[1].substring(0,2),10));
+      arrayMin[i]=arrayTiempo[i]+arrayMin[i];
+    
+    }
+    return arrayMin;
+    }
+    
+    turnHoursToMinutes(movies);
 
 // Get the average of all rates with 2 decimals 
 
