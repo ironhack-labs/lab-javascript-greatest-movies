60053b0974f5956209130e6e293257e1fa8b1207
Alfio <alfioanr@gmail.com>
2018-01-12 01:37:55 +0100

    canas verdes y las iteraciones

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,9 +1,39 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
+    var newfilms = [];
 
+    function turnHoursToMinutes(array) {
+      var peliculas = [];
+       console.log(array.length);
+      array.forEach(function(e) {
+         var pelicula = Object.assign({},e);
+         console.log (pelicula);
+        var hourMin = parseInt(pelicula.duration.split("h")[0] * 60);
+        var min = parseInt(pelicula.duration
+            .split("h")[1]
+            .split("min")[0]);
+        if (min) {
+          hourMin += min;
+        }
+       
+        pelicula.duration = hourMin;
+
+        peliculas.push(pelicula);
+      });
+      console.log(peliculas)
+      console.log(array.length);
+      
+      return peliculas;
+    }
+
+    //turnHoursToMinutes(movies);
 
 // Get the average of all rates with 2 decimals 
+var average = {};
 
+function ratesAverage(Movies.) {
+  
+}
 
 // Get the average of Drama Movies
 
