8ab89963d3bf52c5dee0e450a0214218d22798a0
Jesús Silva <hola@jesussilva.es>
2018-01-12 12:47:00 +0100

    Iteración 1

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,6 +1,22 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
-
+function turnHoursToMinutes(array) {
+    var long;
+    return array.map(function(e) {
+      if(e.duration.length < 3){
+        long = parseInt(e.duration.slice(0,1) * 60);
+        return Object.assign({}, e, { duration: long });
+      } else if (e.duration.length < 6){
+        long = parseInt(e.duration.slice(0,2));
+        return Object.assign({}, e, { duration: long });
+      } else {
+        long = parseInt(e.duration.slice(0,1) * 60) + parseInt(e.duration.slice(3,-3));
+        return Object.assign({}, e, { duration: long });
+      }
+    })
+  }
+  
+  //turnHoursToMinutes(movies)
 
 // Get the average of all rates with 2 decimals 
 
