b6445a0aef4b632516a269a9f166482843a84c4a
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2018-01-25 13:45:22 -0600

    almost done

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,8 +1,28 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
 
+function  turnHoursToMinutes(array){
+  array.forEach(function(element) {
+    var hour = parseInt(element.duration.substring(0,1));
+    var minutes = parseInt(element.duration.split("min")[0].substring(2));
+    element.duration = hour *60 + minutes;
+  });
+  return array;
+}
+turnHoursToMinutes(movies);
 
 // Get the average of all rates with 2 decimals 
+function totalReviews(array){
+  var rateAvg = 0;
+  array.forEach(function(elem){
+  var rateNum = parseFloat(elem.rate);
+  rateAvg+=rateNum
+  });
+console.log(rateAvg);
+return (rateAvg / array.length).toFixed(2);
+
+}
+
 
 
 // Get the average of Drama Movies
