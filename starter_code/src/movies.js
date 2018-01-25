9ed9aae3c81efa0cd0479e5f4c551245d5c0cd59
Alexinder Virdee <alex.virdee08@gmail.com>
2018-01-25 17:50:24 -0500

    ratesAvg

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,9 +1,36 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
 
+function turnHoursToMinutes(arr) {
+    var hours = 0;
+    var minutes = 0;
+    return arr.map(function(time) {
+        var movieTime = time.duration.split(" ");
+        // console.log(movieTime)
+
+        hours = parseInt(movieTime[0].slice(0, 1) * 60);
+        // console.log(hours)
+
+        minutes = parseInt(movieTime[1].slice(0, 2));
+// console.log(minutes)
+        // return hours + minutes;
+       return Object.assign({}, time, {duration: hours+minutes})
+    });
+}
+
+turnHoursToMinutes(movies);
 
 // Get the average of all rates with 2 decimals 
+function ratesAverage(arr) {
 
+    var avg;
+
+    return arr.reduce(function(rating) {
+        
+
+        avg = arr.rate / arr.rate.length;
+    });
+}
 
 // Get the average of Drama Movies
 
