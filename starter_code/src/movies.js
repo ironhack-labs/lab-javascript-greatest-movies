5e7875b0c3f42614f218c6563a05be780e90a00e
Juan Sanchez Mesa <sanchezmesaj@gmail.com>
2018-03-22 18:43:42 +0100

    ratesAverage function created

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,11 +1,31 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
-
+function turnHoursToMinutes(moviesArray) {
+  return moviesArray.map(function (elem) {
+    var hours = 0;
+    var minutes = 0;
+    if (elem.duration.indexOf('h') !== -1) {
+      hours = parseInt(elem.duration[0], 10) * 60;
+    }
+    if (elem.duration.indexOf('min') !== -1) {
+      minutes = parseInt(elem.duration.substring(elem.duration.length - 5, elem.duration.length - 3), 10);
+    }
+    return Object.assign({}, elem, { duration: hours + minutes });
+  });
+}
+turnHoursToMinutes(movies);
 
 // Get the average of all rates with 2 decimals 
+function ratesAverage(moviesArray) {
+  var total = moviesArray.reduce(function(sum, movie){
+    return sum + movie.rate;
+  }, 0);
 
+  return total/moviesArray.length;
+}
 
 // Get the average of Drama Movies
+
 
 
 // Order by time duration, in growing order
