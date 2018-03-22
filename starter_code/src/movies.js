1c20a6cc221c9b992e169f4a0bb94d4b58731e43
muflonex <muflonex@gmail.com>
2018-03-22 19:31:42 +0100

    El momento de darse cuenta que son todos strings casi me hizo tirar una lágrima de felicidad en océano de ingenuidad humana

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,9 +1,31 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
-
+function turnHoursToMinutes(moviesArray) {
+  return moviesArray.map(function (elem) {
+    var hours = 0;
+    var minutes = 0;
+    if (elem.duration.indexOf('h') !== -1) {
+      hours = parseInt(elem.duration[0], 10) * 60;
+    }
+    if (elem.duration.indexOf('min') !== -1) {
+      minutes = parseInt(elem.duration.substring(elem.duration.length - 5, elem.duration.length - 3), 10);
+    }
+    return Object.assign({}, elem, { duration: hours + minutes });
+  });
+}
+turnHoursToMinutes(movies);
 
 // Get the average of all rates with 2 decimals 
+function ratesAverage(movies){
+  
+ var sum = movies.reduce(function(accum, currentMov){
+    return accum + parseInt(currentMov.rate);
+  },0);
+  console.log(sum)
+  var ratesAverage = sum/movies.length
 
+return ratesAverage;
+}
 
 // Get the average of Drama Movies
 
