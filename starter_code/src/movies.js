28860d14de313589d3e2eb5173ae5bf73d09dce0
Raul.dll <rdl.llave@gmail.com>
2018-03-22 18:52:23 +0100

    primera funcion

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,6 +1,20 @@
 /* eslint no-restricted-globals: 'off' */
-// Turn duration of the movies from hours to minutes 
+// Turn duration of the movies from hours to minutes o 
 
+function turnHoursToMinutes(moviesArray) {
+  return moviesArray.map(function (elem) {
+   var hours = 0;
+   var minutes = 0;
+   if (elem.duration.indexOf('h') !== -1) {
+    hours = parseInt(elem.duration[0], 10) * 60;
+   }
+   if (elem.duration.indexOf('min') !== -1) {
+    minutes = parseInt(elem.duration.substring(elem.duration.length - 5, elem.duration.length - 3), 10);
+   }
+   return Object.assign({}, elem, { duration: hours + minutes });
+  });
+ }
+ turnHoursToMinutes(movies);
 
 // Get the average of all rates with 2 decimals 
 
