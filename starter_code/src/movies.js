260cf7f73bb112dd6b63ea89e87f51ba661aa2ac
Germán Rodríguez <germanrodriguez@MacBook-Pro-de-German.local>
2018-03-22 15:59:27 +0100

    jasmine is shit

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -3,9 +3,44 @@
 
 
 // Get the average of all rates with 2 decimals 
+var getTotalRates = movies.reduce(function (acc, index) {
+  var intNumber = parseFloat(index.rate);
+  var result = acc + intNumber;
+  console.log(result)
+  return result
+},0);
 
+var ratesAverage = function (result) {
+  return result/movies.length
+}
 
 // Get the average of Drama Movies
+
+var dramaMoviesRates =  function(movies) {
+  
+}
+
+
+
+
+
+
+/*
+function dramaMoviesRates (movies) {
+ for (var i = 0; i < movies.length; i++){
+   var dramaMovies = movies[i].genre.find(function(element) {
+    console.log(element); 
+    return element;
+   });
+   if(element === "Dramas"){
+     console.log(ratesAverage(movies));
+     ratesAverage(movies);
+   }
+
+ }
+}
+*/
+
 
 
 // Order by time duration, in growing order
