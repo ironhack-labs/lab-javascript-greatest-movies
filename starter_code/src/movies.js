a659e35325626c7c1c7b5192b84593c5cb032e52
A-Tr <alvarotrancon@gmail.com>
2018-03-22 19:32:56 +0100

    avanzado el segundo ejercicio

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,9 +1,27 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
-
+function turnHoursToMinutes(moviesArray) {
+  return moviesArray.map(function (elem) {
+    var hours = 0;
+    var minutes = 0;
+    if (elem.duration.indexOf('h') !== -1) {
+      hours = parseInt(elem.duration[0], 10) * 60;
+    }
+    if (elem.duration.indexOf('min') !== -1) {
+      minutes = parseInt(elem.duration.substring(elem.duration.length - 5, elem.duration.length - 3), 10);
+    }
+    return Object.assign({}, elem, { duration: hours + minutes });
+  });
+}
 
 // Get the average of all rates with 2 decimals 
 
+function ratesAverage(moviesArray) {
+  var sum = movies.reduce(function(sum, person){   
+    return (sum + parseFloat(person.rate)); }, 0);
+  var average = (sum / 250).toFixed(2)
+  return parseFloat(average)
+}
 
 // Get the average of Drama Movies
 
