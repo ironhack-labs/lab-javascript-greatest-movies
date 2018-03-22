6e4c7f13edaf7f5ea1ccc5df5a27ad9eb5ab8bfe
Miriam Aparicio <miriam.aparicio@gmail.com>
2018-03-22 16:02:34 +0100

    BCN-Sam-Miriam

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,13 +1,29 @@
 /* eslint no-restricted-globals: 'off' */
-// Turn duration of the movies from hours to minutes 
+// Turn duration of the movies from hours to minutes
+
 
 
 // Get the average of all rates with 2 decimals 
+function ratesAverage(array) {
+  var totalRates = array.reduce(function(accumulator, item) {
+    return accumulator + item.rate;
+  }, 0);
+
+  return parseFloat(totalRates / array.length);
+}
 
 
 // Get the average of Drama Movies
 
+function dramaMoviesRate(array) {
+  var dramaMovies = array.filter(function(movie) {
+    return movie.genre.includes("Drama");
+  }, 0);
 
+  var averageDrama = ratesAverage(dramaMovies);
+
+  return averageDrama;
+}
 // Order by time duration, in growing order
 
 
