439f86161f6181c536994bd48f5cc704440210f0
Alejandro <alexibape@hotmail.com>
2018-03-23 00:05:50 +0100

    Iteration 3

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,12 +1,47 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
 
+function turnHoursToMinutes(moviesArray) {
+  return moviesArray.map(function (elem) {
+    var hours = 0;
+    var minutes = 0;
+    if (elem.duration.indexOf('h') !== -1) {
+      hours = parseInt(elem.duration[0], 10) * 60;
+    }
+    if (elem.duration.indexOf('min') !== -1) {
+      minutes = parseInt(elem.duration.substring(elem.duration.length - 5, elem.duration.length - 3), 10);
+    }
+    return Object.assign({}, elem, { duration: hours + minutes });
+  });
+}
+turnHoursToMinutes(moviesArray);
 
 // Get the average of all rates with 2 decimals 
+
+function ratesAverage (moviesArray) {
+  
+  var sum = moviesArray.reduce (function (accumulator, current){
+   return accumulator + parseInt(current.rate);
+ },0);
+ 
+   return Math.round((sum /moviesArray.length) * 100) / 100  
+ }
 
 
 // Get the average of Drama Movies
 
+function dramaMoviesRate (moviesArray) {
+  
+  
+  var dramaList = moviesArray.filter (function(element){
+  
+    
+          return element.genre.indexOf('Drama') !== -1
+    });
+  
+    return ratesAverage (dramaList);
+
+}
 
 // Order by time duration, in growing order
 
