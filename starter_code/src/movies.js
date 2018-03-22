9f52c7f024eee5d9fe8d72a9137cb14ca704690b
Rapha Montenegro <raphaelmmontenegro@gmail.com>
2018-03-22 16:04:25 +0100

    Almost nothing done

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -5,8 +5,36 @@
 // Get the average of all rates with 2 decimals 
 
 
+
+function ratesAverage(array) {
+  var totalRate = array.reduce(function (acc, movie) { 
+    console.log(movie);
+    return acc + Number(movie.rate)
+  }, 0);
+  return totalRate / array.length;
+};
+
+
 // Get the average of Drama Movies
 
+function dramaMoviesRate(array) {
+  var dramaMovies = movies.filter(function(elem) {
+    if (elem.genre.includes("Drama")) {
+      return true;
+    } 
+    });
+  return parseInt(ratesAverage(array));
+};
+
+dramaMoviesRate(dramaMovies)
+/*
+function dramaMoviesRate(array) {
+  var totalRate = array.reduce(function (acc, movie) { 
+    return acc + movie.rate
+  }, 0);
+  return totalRate / array.length;
+};
+*/
 
 // Order by time duration, in growing order
 
