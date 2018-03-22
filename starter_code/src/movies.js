b11f9a6b9d681a8a86717e4c450f570f0f29e466
Luna Fidalgo <lunafidalgo@gmail.com>
2018-03-22 22:43:35 +0100

    Todo hecho excepto el bonus

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,20 +1,128 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
-
+function turnHoursToMinutes(moviesArray) {
+  return moviesArray.map(function (elem) {
+    var hours = 0;
+    var minutes = 0;
+    if (elem.duration.indexOf('h') !== -1) {
+      hours = parseInt(elem.duration[0], 10) * 60;
+    }
+    if (elem.duration.indexOf('min') !== -1) {
+      minutes = parseInt(elem.duration.substring(elem.duration.length - 5, elem.duration.length - 3), 10);
+    }
+    return Object.assign({}, elem, { duration: hours + minutes });
+  });
+}
+turnHoursToMinutes(movies);
 
 // Get the average of all rates with 2 decimals 
+//conseguir un array con todos los average => y luego lo sumamos con reduce 
 
 
-// Get the average of Drama Movies
+function ratesAverage(array){
+  var arrayRates =  array.map(function(element){
+    
+    if(isNaN(parseFloat(element.rate))){
+      return 0.0;
+    }
+   
+     return parseFloat(element.rate);
+   });
+ 
+   var total =  arrayRates.reduce(function(accumulator, current){
+     
+     return accumulator + current;
+   }, 0);
+ 
+
+   return  Math.round(((total/(array.length))) * 100) / 100;
+ }
+ 
+
+//buscamos las peliculas que tengan genero drama => devolvemos
+//en un array con los objetos
+
+function dramaMoviesRate (array){
+  
+  var dramaMoviesAvg = array.filter(function(movie){
+    console.log(movie.genre);
+  
+    if(movie.genre.indexOf('Drama') != -1){
+      console.log("entra");
+      return true;
+    }else{
+      return false;
+    }
+  
+  });
+  
+  //dramaMoviesRate => es un array con las peliculas que tienen el 
+  //genero drama
+  console.log(dramaMoviesAvg);
+  if(dramaMoviesAvg.length===0){
+    return undefined;
+  }
+  return ratesAverage(dramaMoviesAvg);
+}
+
 
 
 // Order by time duration, in growing order
 
+function orderByDuration(array){
+  
+
+ var ordenado = array.sort(function(a,b) {
+   if(a.duration - b.duration === 0){
+     //misma duracion => hay que ordenarlo alfabeticamente segun titulo
+     return a.title > b.title;
+   }else{return a.duration - b.duration;}
+  
+});
+
+ return ordenado; 
+}
 
 // How many movies did STEVEN SPIELBERG
 
+function howManyMovies(array){
+  var stevenMovies = array.filter(function(movie){
+    if(movie.director ==="Steven Spielberg"  && movie.genre.indexOf('Drama')!= -1){
+
+
+      return true;
+    }else{
+      return false;
+    }
+    return array.map
+  
+  });
+
+if(array.length===0){
+  return undefined;
+  
+}else{ 
+  return "Steven Spielberg directed " + stevenMovies.length + " drama movies!";}
+
+}
 
 // Order by title and print the first 20 titles
 
+function orderAlphabetically (array){
+
+  var titulosPeliculas =  array.map(function(element){
+   
+     return element.title;
+   });
+
+   return titulosPeliculas.sort().slice(0,20);
+}
 
 // Best yearly rate average
+
+function bestYearAvg (array){
+  //puedo calcular el avg con el método de arriba, para ello, tengo que conseguir tener un array con todas las 
+  //peliculas de un año
+  
+
+}
