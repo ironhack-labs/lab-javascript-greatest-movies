1c5befd0e6996bd680f95e401bb005d4108d1a2b
luisnueda <nueda.luis@gmail.com>
2018-03-23 01:12:38 +0100

    completado, falta bonus

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -27,24 +27,25 @@ function ratesAverage(moviesArray){
 
 // Get the average of Drama Movies
 function dramaMoviesRate(moviesArray){
+
   var generoBusqueda = moviesArray.filter(function(e){
-    if(e.genre.indexOf("Drama") == -1){
-      return undefined
+    if(e.genre.indexOf("Drama") !== -1){
+      return true
     }else{    
-       return e.genre.indexOf("Drama") !== -1
+       return false
     }
   });
+  if(generoBusqueda.length == 0){
+    return undefined;
+  }
   return ratesAverage(generoBusqueda);
 };
 
 
-
-//dramaMoviesRate Should return undefined if there is no Drama movie!
-
 // Order by time duration, in growing order
 
 function orderByDuration(moviesArray) {
-  moviesArray.sort(function (a, b) {
+  moviesArray.sort(function(a, b) {
     if (a.duration === b.duration) {
       if (a.title > b.title) {
         return 1;
@@ -57,30 +58,46 @@ function orderByDuration(moviesArray) {
 
 // How many movies did STEVEN SPIELBERG
 
-function howManyMovies (moviesArray){
+function howManyMovies(moviesArray) {
   if (moviesArray.length == 0) {
     return undefined;
   }
-  var busqueda = moviesArray.filter(function(e){
+  var busqueda = moviesArray.filter(function(e) {
     var newArr = [];
     var a = e.genre.indexOf("Drama");
     var b = e.director.indexOf("Steven Spielberg");
-    
-    if(a !== -1 && b !== -1){
-     return newArr.push(e)
-    }
 
+    if (a !== -1 && b !== -1) {
+      return newArr.push(e);
+    }
   });
-  return 'Steven Spielberg directed ' + busqueda.length + ' drama movies!';
+  return "Steven Spielberg directed " + busqueda.length + " drama movies!";
 }
 
-
+/*------------Codigo Victor-------- */
+/* 
+function howManyMovies(moviesArr) {
+  if (moviesArr.length === 0) {
+      return undefined;
+  }
+  var directorMovies = moviesArr.filter(function (e) {
+      return e.director === 'Steven Spielberg' && e.genre.indexOf('Drama') !== -1;
+  });
+  return 'Steven Spielberg directed ' + directorMovies.length + ' drama movies!';
+} 
+*/
 
 // Order by title and print the first 20 titles
 
 function orderAlphabetically (moviesArray){
 
-}
+  var titulo = moviesArray.map(function(e){
+      return e.title;
+  });
+
+  return titulo.sort().slice(0, 20);
+  }
+
 
 // Best yearly rate average
 
