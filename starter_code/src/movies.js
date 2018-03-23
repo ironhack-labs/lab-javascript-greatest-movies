128960d9139df8b04445cf46dec4ba9619e476f6
luisnueda <nueda.luis@gmail.com>
2018-03-23 01:33:51 +0100

    falta bonus

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,46 +1,50 @@
 /* eslint no-restricted-globals: 'off' */
-// Turn duration of the movies from hours to minutes 
+// Turn duration of the movies from hours to minutes
 
 function turnHoursToMinutes(moviesArray) {
-  return moviesArray.map(function (elem) {
+  return moviesArray.map(function(elem) {
     var hours = 0;
     var minutes = 0;
-    if (elem.duration.indexOf('h') !== -1) {
+    if (elem.duration.indexOf("h") !== -1) {
       hours = parseInt(elem.duration[0], 10) * 60;
     }
-    if (elem.duration.indexOf('min') != -1) {
-      minutes = parseInt(elem.duration.substring(elem.duration.length - 5, elem.duration.length - 3), 10);
+    if (elem.duration.indexOf("min") != -1) {
+      minutes = parseInt(
+        elem.duration.substring(
+          elem.duration.length - 5,
+          elem.duration.length - 3
+        ),
+        10
+      );
     }
     return Object.assign({}, elem, { duration: hours + minutes });
   });
 }
 turnHoursToMinutes(movies);
 
-// Get the average of all rates with 2 decimals 
+// Get the average of all rates with 2 decimals
 
-function ratesAverage(moviesArray){
-  var suma = moviesArray.reduce(function(acumulador, current){
+function ratesAverage(moviesArray) {
+  var suma = moviesArray.reduce(function(acumulador, current) {
     return acumulador + parseInt(current.rate);
   }, 0);
-  return Math.round((suma/moviesArray.length) * 100) / 100;
+  return Math.round(suma / moviesArray.length * 100) / 100;
 }
 
 // Get the average of Drama Movies
-function dramaMoviesRate(moviesArray){
-
-  var generoBusqueda = moviesArray.filter(function(e){
-    if(e.genre.indexOf("Drama") !== -1){
-      return true
-    }else{    
-       return false
+function dramaMoviesRate(moviesArray) {
+  var generoBusqueda = moviesArray.filter(function(e) {
+    if (e.genre.indexOf("Drama") !== -1) {
+      return true;
+    } else {
+      return false;
     }
   });
-  if(generoBusqueda.length == 0){
+  if (generoBusqueda.length == 0) {
     return undefined;
   }
   return ratesAverage(generoBusqueda);
-};
-
+}
 
 // Order by time duration, in growing order
 
@@ -89,20 +93,18 @@ function howManyMovies(moviesArr) {
 
 // Order by title and print the first 20 titles
 
-function orderAlphabetically (moviesArray){
-
-  var titulo = moviesArray.map(function(e){
-      return e.title;
+function orderAlphabetically(moviesArray) {
+  var titulo = moviesArray.map(function(e) {
+    return e.title;
   });
 
   return titulo.sort().slice(0, 20);
-  }
-
+}
 
 // Best yearly rate average
 
-function bestYearAvg (moviesArray){
-  if(moviesArray.length == 0){
+function bestYearAvg(moviesArray) {
+  if (moviesArray.length == 0) {
     return undefined;
   }
   return moviesArray;
