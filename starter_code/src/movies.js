6313a3b326260604855969af79955314cf46c52e
Gabriel Cebrián <kunry3@hotmail.com>
2018-03-26 00:54:41 +0200

    Orden de las peliculas, solo falta el bonus

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -17,7 +17,7 @@ var movies = [
     }]
 
 /* eslint no-restricted-globals: 'off' */
-// Turn duration of the movies from hours to minutes 
+// Turn duration of the movies from hours to minutes
 function turnHoursToMinutes(moviesArray) {
     return moviesArray.map(function (elem) {
       var hours = 0;
@@ -36,18 +36,18 @@ function turnHoursToMinutes(moviesArray) {
 
 
 
-// Get the average of all rates with 2 decimals 
+// Get the average of all rates with 2 decimals
   function ratesAverage(array) {
       return Math.round((array.reduce(function(acumulador,e){
 
         if (e.rate.length === 0)
           return 0;
-        var int =  parseFloat(e.rate);
+        var float =  parseFloat(e.rate);
         var rest = 0
 
-        
-        return int + rest + acumulador;
-      },0) / array.length)*100 ) /100;      
+
+        return float + rest + acumulador;
+      },0) / array.length)*100 ) /100;
   }
  console.log(ratesAverage(movies));
 
@@ -60,24 +60,24 @@ function dramaMoviesRate(array) {
     for (let i = 0; i < e.genre.length; i++) {
       if (e.genre[i] === "Drama"){
         cont ++;
-          if (e.rate.length === 0){            
+          if (e.rate.length === 0){
             return  acumulador;}
-        return acumulador + parseFloat(e.rate); }     
+        return acumulador + parseFloat(e.rate); }
     }
     return acumulador;
   },0) )
   if (drama === 0)
     return undefined;
   else{
-    
+
     return Math.round((drama/cont) * 100)/100;}
-  
+
 }
 console.log(dramaMoviesRate(movies));
 // Order by time duration, in growing order
 function orderByDuration(array) {
 
-     
+
     array = array.sort(function(a,b){
       return a.duration - b.duration;
     });
@@ -85,16 +85,16 @@ function orderByDuration(array) {
     return array.sort(function(a,b){
       if (a.duration === b.duration)
       return a.title > b.title;
-    })    
-  
+    })
 
-  
-  
-  
+
+
+
+
 }
 console.log(orderByDuration([{ title: 'abc', duration: 180 }, { title: 'bac', duration: 90 }, { title: 'aab', duration: 90 }]));
 // How many movies did STEVEN SPIELBERG
-function howManyMovies(array) {   //No se porque me da error.
+function howManyMovies(array) {   //No se porque me encuentra en el último test solo una.
   var cont = 0;
   if (array.length === 0){
     return undefined;
@@ -105,7 +105,7 @@ function howManyMovies(array) {   //No se porque me da error.
         if (array[i].genre[j].toLowerCase().replace(/[^a-z]/g, '') === 'drama') {
           cont++;
         };
-        
+
       }
           }
   }
@@ -115,17 +115,28 @@ function howManyMovies(array) {   //No se porque me da error.
 console.log(howManyMovies(movies));
 // Order by title and print the first 20 titles
 function orderAlphabetically(array) {
-  var x = [];
+var x = [];
   array = array.sort(function(a, b){
-    return a.title > b.title;
+    return (a.title.localeCompare(b.title));
   })
-  console.log(x);
+  if (array.length >= 19){
+    for (let i = 0; i < 20; i++)
+    {
+      x.push(array[i].title);
+    }
+    return x;
+  }
   for (let i = 0; i < array.length; i++) {
-     x.push(array[i].title);   
-   }
-  
-  
+    x.push(array[i].title);
+    
+  }
+
+
   return x;
 }
-console.log(orderAlphabetically([{ title: 'aab' }, { title: 'bab' }, { title: 'acb' }, { title: 'aab' }, { title: 'bab' }, { title: 'acb' }, { title: 'aab' }, { title: 'bab' }, { title: 'acb' }, { title: 'aab' }, { title: 'bab' }, { title: 'acb' }, { title: 'aab' }, { title: 'bab' }, { title: 'acb' }, { title: 'aab' }, { title: 'bab' }, { title: 'acb' }, { title: 'aab' }, { title: 'bab' }, { title: 'acb' }, { title: 'aab' }, { title: 'bab' }, { title: 'acb' }, { title: 'aab' }, { title: 'bab' }, { title: 'acb' }, { title: 'aab' }, { title: 'bab' }, { title: 'acb' }, { title: 'aab' }, { title: 'bab' }, { title: 'acb' }, { title: 'aab' }, { title: 'bab' }, { title: 'acb' }]));
+console.log(orderAlphabetically([{ title: 'aaa' }, { title: 'bab' }, { title: 'acb' }, { title: 'aab' }, { title: 'bbb' }, { title: 'anc' }, { title: 'kns' }, { title: 'zds' }, { title: 'pow' }, { title: 'gda' }, { title: 'res' }, { title: 'ter' }, { title: 'bca' }, { title: 'ccc' }, { title: 'bbt' }, { title: 'qas' }, { title: 'kmn' }, { title: 'frt' }, { title: 'afb' }, { title: 'agb' }, { title: 'apo' }, { title: 'poa' }, { title: 'cdf' }, { title: 'sea' }, { title: 'lom' }, { title: 'acs' }, { title: 'qas' }, { title: 'mns' }, { title: 'bvc' }, { title: 'gha' }, { title: 'lkj' }, { title: 'era' }, { title: 'ert' }, { title: 'tex' }, { title: 'zas' }, { title: 'pol' }]));
 // Best yearly rate average
+
+
+
+
