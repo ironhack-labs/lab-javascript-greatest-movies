80c74f05f4031737e74fb8067a0e74989e372f64
JMXXII <jeysonmmurillo@gmail.com>
2018-04-06 09:11:52 -0400

    1st iteration only

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,5 +1,36 @@
+// var movies = require("../src/data.js");
+console.log(movies)
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
+
+// Turn duration of the movies from hours to minutes 
+
+
+function getMovieDuration(movie) {
+  var duration = movie.duration.split("h "); //[3, 22min]
+  var hour = duration[0];
+  var minutes = duration[1].split("min")[0]; // ["22"]
+  var durationInMinutes = (Number(hour) * 60) + Number(minutes);
+  var newMovie = {};
+  newMovie.title = movie.title;
+  newMovie.year = movie.year;
+  newMovie.director = movie.director;
+  newMovie.duration = durationInMinutes;
+  newMovie.genre = movie.genre
+  newMovie.rate = movie.title
+  return newMovie;
+}
+
+function turnHoursToMinutes(movies){
+var totalMinutes = movies.map(getMovieDuration);
+return totalMinutes;
+// console.log(movies);
+}
+turnHoursToMinutes(movies);
+
+// var minutes = hour[1].split("min");
+// Number(minutes[0])
+
 
 
 // Get the average of all rates with 2 decimals 
