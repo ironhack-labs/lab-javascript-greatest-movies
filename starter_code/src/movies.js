eab1286dcab2201d1a0163d1e2afe8752b64bcd5
Ryan Martin <rmart106@fiu.edu>
2018-04-06 09:01:35 -0400

    Had trouble with this one. Will keep working on it over the weekend.

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,3 +1,29 @@
+// var movies = require("./data.js")
+
+var movies = [
+  {
+    title: 'The Shawshank Redemption',
+    year: '1994',
+    director: 'Frank Darabont',
+    duration: '2h 22min',
+    genre: ['Crime', 'Drama'],
+    rate: '9.3'
+  }
+];
+
+function turnHoursToMinutes(anArray){
+  movies.map(function(){
+    var hours = movies[0].duration.slice(0,1) * 60;
+    console.log(hours);
+    return hours;
+  });
+};
+
+console.log(turnHoursToMinutes(movies);
+
+// var result = movies[0].duration.slice(3,-3) + hours;
+//     console.log(result);
+
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
 
