4bb7982bd7aed390a2b624b95290d913cbf94dbe
Diego Vazquez <diurivj@icloud.com>
2018-04-06 09:41:28 -0500

    Almost

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,12 +1,41 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
-
+var durationInLetters = movies.map(function(movies){
+  return movies.duration.split('');
+});
+var horasSinLetras= durationInLetters.map(function(elem, i){
+  return (durationInLetters[i][0] * 60);
+});
+var minutosSinLetras= durationInLetters.map(function(elem,i){
+  if (elem.indexOf("m")!=-1){
+      return elem.splice(elem.indexOf(' ')+1,elem.indexOf('m')-elem.indexOf(' ')-1).join("");
+  } else {
+   return 0;
+  }
+});
+var tiempoEnMinutos = horasSinLetras.map(function(elem, i){
+  return horasSinLetras[i] + parseInt(minutosSinLetras[i]);
+});
+movies.map(function(elem,i){
+  return elem.duration === tiempoEnMinutos[i];
+});
 
 // Get the average of all rates with 2 decimals 
-
+var ratesAverage = movies.reduce(function(acumulador, current){
+  return acumulador + current.rate / movies.length;
+}, 0);
+ratesAverage = Math.round(ratesAverage * 100) / 100;
+console.log(ratesAverage);
 
 // Get the average of Drama Movies
-
+var dramaMovies = movies.filter(function(elem){
+  return elem.genre.includes('Drama');
+  });
+  var dramaMoviesRate = dramaMovies.reduce(function(acc,cur){
+  return acc + cur.rate/dramaMovies.length;
+  },0);
+  dramaMoviesRate = Math.round(dramaMoviesRate * 100) / 100;
+  console.log(dramaMoviesRate);
 
 // Order by time duration, in growing order
 
@@ -18,3 +47,9 @@
 
 
 // Best yearly rate average
+
+
+
+
+
+
