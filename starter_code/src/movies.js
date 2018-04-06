9f729c8b565ca4e2396c8e718f80fdbdce75fe68
neilxcode <developros@icloud.com>
2018-04-06 09:08:23 -0400

    update1

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,6 +1,59 @@
-/* eslint no-restricted-globals: 'off' */
-// Turn duration of the movies from hours to minutes 
+var movies = [
+  {
+    title: 'The Shawshank Redemption',
+    year: '1994',
+    director: 'Frank Darabont',
+    duration: '2h 22min',
+    genre: ['Crime', 'Drama'],
+    rate: '9.3'
+  },
+  {
+    title: 'The Shawshank Redemption',
+    year: '1994',
+    director: 'Frank Darabont',
+    duration: '1h 22min',
+    genre: ['Crime', 'Drama'],
+    rate: '9.3'
+  },
+  {
+    title: 'The Shawshank Redemption',
+    year: '1994',
+    director: 'Frank Darabont',
+    duration: '3h 33min',
+    genre: ['Crime', 'Drama'],
+    rate: '9.3'
+  }
+  ]
+  //split on letter "h" and return hours * 60
+  
+  function calculateHours(hours){
+    var minutesFromHours = hours.split("h")
+    return minutesFromHours[0]*60
+  }
+  // console.log(calculateHours("3h"))
+  
+  
+  //splitting on minutes
+  function calculateMinutes(minutes){
+    var minutesFromMinutes = minutes.split("min")
+    return minutesFromMinutes[0]
+  }
+    // console.log(calculateMinutes("42min"))
 
+    //Create a turnHoursToMinutes method that receive an array as parameter, 
+  // duration: "2h 44min"
+  function turnHoursToMinutes(duration){
+    
+    var timeChunks = duration.split(" ");
+    var a = calculateHours(timeChunks[0]);
+    var b = calculateMinutes(timeChunks[1])
+    // console.log(b)
+}
+
+//replace the duration info of each of the movies for it equivalent in minutes. For example:
+
+
+//loop through every movie
 
 // Get the average of all rates with 2 decimals 
 
