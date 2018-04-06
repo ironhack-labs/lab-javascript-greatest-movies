2c0eb15c135b0bf850059fa884a7cf429805c9dd
Claudia Hernandez <hernandez.clauds@gmail.com>
2018-04-06 12:12:47 -0400

    done

--- starter_code/src/data.js
+++ starter_code/src/data.js
@@ -1,4 +1,4 @@
-var movies = [
+var movies [
   {
     title: 'The Shawshank Redemption',
     year: '1994',
@@ -2000,3 +2000,6 @@ var movies = [
     rate: '8.0'
   }
 ];
+
+
+module.export = movies;
