b8c004a08fbc1358dea412cb478fd5bbd0914271
Richard Batista <stalfos@icloud.com>
2018-04-05 22:40:16 -0400

    Submitting

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,5 +1,34 @@
+var theActualMovies = require("./data.js");
+
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
+
+function turnHoursToMinutes(movies) {
+	
+	movies.forEach(function(movie){
+		var theDuration = [];
+
+		theDuration[0] = movie.duration.split("h");
+		theDuration[1] = movie.duration[1].split("min")
+
+		// plainHours = parseInt(plainHours);
+		// plainMinutes = parseInt(plainMinutes);
+
+		// movie.duration = ((plainHours * 60) + plainMinutes);
+		
+		// console.log("" + movie.duration + "min");
+		// movie.duration[0] = plainHours;
+		// movie.duration[1] = plainMinutes;
+
+		//return movies.duration;
+		//console.log(theDuration);
+		return theDuration;
+	});
+	return movies;
+}
+
+
+turnHoursToMinutes(theActualMovies);
 
 
 // Get the average of all rates with 2 decimals 
