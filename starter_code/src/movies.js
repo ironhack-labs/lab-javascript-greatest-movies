1c424c2fe43e3f19727eb64d2eba958efc390e16
Brian Jaimes <jaimes.brian001@gmail.com>
2018-04-06 13:12:54 -0400

    done

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,5 +1,22 @@
+var bleh =require("./data.js");
+
+console.log(movies);
+
+
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
+
+var turnHoursToMinutes = movies.map(function(oneMovie){
+  var minFromHours = oneMovie.duration.split("h");
+  return minFromHours[0]*60
+});
+var toMinutes = movies.map(function(oneMovie){
+  var minFromMin = oneMovie.duration.split("min");
+  return minFromMin[0]
+})
+
+console.log(toMinutes);
+
 
 
 // Get the average of all rates with 2 decimals 
