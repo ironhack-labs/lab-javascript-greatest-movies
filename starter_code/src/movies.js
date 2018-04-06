e5e524a68bbc55132deba1a70681f298d95f12af
Robert Gelbman <rgelbman@aol.com>
2018-04-06 09:13:26 -0400

    MIA-FTWD-Robert Gelbman, Jeyson Murillo-lab-javascript-all-times-movies

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,7 +1,34 @@
 /* eslint no-restricted-globals: 'off' */
-// Turn duration of the movies from hours to minutes 
+var movies = require("./data.js");
+// Turn duration of the movies from hours to minutes
+function convertDurationToMinutes(){
+  var totalMinutes = movies.map(function(movie){
+    // console.log(movie);
+    var hour = movie.duration.split("h ");
+      if (hour.length > 1){
+      var minutes = hour[1].split("min");
+      return Number(hour[0] *60) + Number(minutes[0]);
+      } else {
+      return Number(hour[0] * 60);
+      }
+  })
+  return totalMinutes;
+}
 
+function turnHoursToMinutes(movies){
+  var minutesArray = convertDurationToMinutes();
+  console.log(minutesArray);
+  var moviesMinutes = movies;
+  // console.log(moviesMinutes);
+  moviesMinutes.forEach(function(movie, index){
+    movie.duration = minutesArray[index];
 
+  })
+  console.log(moviesMinutes);
+  return moviesMinutes;
+}
+
+turnHoursToMinutes(movies);
 // Get the average of all rates with 2 decimals 
 
 
