1615f3fc83eed9d1ba41e3c3647adef24939eaf0
Estibaliz Fernandez <estibalizfpalacios@gmail.com>
2018-05-05 18:19:32 +0200

    first iteration Hours to Minutes

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,6 +1,40 @@
-/* eslint no-restricted-globals: 'off' */
-// Turn duration of the movies from hours to minutes 
 
+  
+  //Ejercicio 1
+  
+  function turnHoursToMinutes(movies) {
+  
+    var allMinutes = movies.map(function(movie) {
+      movie = Object.assign({}, movie);
+      var durationSplit = movie.duration.split(' ');
+      var hoursToMin = 0;
+      var newMin = 0;
+  
+      if (durationSplit[0].indexOf("h") !=-1){
+        hoursToMin = Number(durationSplit[0].replace("h",""))*60;
+      } else {
+        hoursToMin = Number(durationSplit[0].replace("min",""));
+      }
+  
+      if (durationSplit[1]){
+        newMin = Number(durationSplit[1].replace("min",""));
+      }
+  
+      var totalMin = hoursToMin + newMin;
+      
+      movie.duration = totalMin;
+      return movie;
+  
+      // return creator(durationSplit)
+    });
+  
+    console.log(allMinutes)
+  
+  }
+  
+  turnHoursToMinutes(movies)
+ 
+  
 
 // Get the average of all rates with 2 decimals 
 
