9c0ef7bde3a8876d9ac1387e20ac38ca40aff497
Belén Garcia <belengarcia@MacBook-Pro-de-pc1177.local>
2018-05-05 16:49:53 +0200

    turnsHoursToMinutes

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -2,6 +2,29 @@
 // Turn duration of the movies from hours to minutes 
 
 
+function turnHoursToMinutes(array) {
+  return array.map(function(movie){
+    var newMovie = Object.assign({},movie);
+    var split = newMovie.duration.split(' ');
+    var hours;
+    var minutes;
+    if (split.length > 1){
+      hours = Number(split[0].replace('h', ''));
+      minutes = Number(split[1].replace('min', ''));
+    } else {
+      if (split[0].indexOf('h') > -1) {
+        hours = Number(split[0].replace('h', ''));
+        minutes = 0;
+      } else {
+        hours = 0;
+        minutes = Number(split[0].replace('min', ''));
+      }
+    }
+  newMovie.duration = hours*60 + minutes;
+  return newMovie;
+  });
+}
+
 // Get the average of all rates with 2 decimals 
 
 
