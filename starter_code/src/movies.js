a1444deedab09dc2a093f0e6bf92fb27d31af08c
Miguel <finkel218@gmail.com>
2018-05-08 16:17:07 -0500

    codigo actualizado

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,17 +1,41 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
 
+function turnHoursToMinutes(array){
+
 
 // Get the average of all rates with 2 decimals 
 
+function ratesAverage(array){
+  let average = array.reduce((sum, movie)=> {
+    return sum += parseFloat(movie.rate);
+  }, 0);
+  return average/array.length;
+} 
+
 
 // Get the average of Drama Movies
+
+function dramaMoviesRate(array){
+  if(dramaMoviesRate != 0){
+   var drama = array.filter(function(movie){
+    for(var i = 0; i < movie.genre.length; i++){
+    return movie.genre[i] === "Drama";
+      
+      }
+    });
+  }else{
+    return undefined;
+  }
+  return ratesAverage(drama.parseFloat);
+}
 
 
 // Order by time duration, in growing order
 
 
 // How many movies did STEVEN SPIELBERG
+
 
 
 // Order by title and print the first 20 titles
