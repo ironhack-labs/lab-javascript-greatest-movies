6ec9c4ff41f959bca76c1e40faa0824647ffe3ab
Alex González <alexgm@MacBook-Pro-de-Alex.local>
2018-05-08 09:50:51 +0200

    changes in functions

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -31,17 +31,40 @@ function turnHoursToMinutes(movieArray) {
 
 // Get the average of all rates with 2 decimals 
 
+function ratesAverage(movies) {
+    var ratesAverage = movies.reduce(function (preview, next) {
+        return preview + parseFloat(next.rate)
+    }, 0) / movies.length;
+
+    return Math.round(ratesAverage * 100) / 100;
+}
 
 // Get the average of Drama Movies
+function dramaMoviesRate(movies) {
+    var filterMoviesDrama = movies.filter(function (item) {
+        return item.genre.some(
+            function (item) {
+                return item === 'Drama';
+            })
+    });
 
+    return filterMoviesDrama.length ? ratesAverage(filterMoviesDrama) : undefined;
+}
 
 // Order by time duration, in growing order
+function orderByDuration(movies) {
+    var moviesParseToMinutes = turnHoursToMinutes(movies);
 
+    return moviesParseToMinutes.sort(function (item1, item2) {
+        return item2.duration - item1.duration;
+    });
+
+}
 
 // How many movies did STEVEN SPIELBERG
 
 function howManyMovies(movieArray) {
-    if(!movieArray.length){
+    if (!movieArray.length) {
         return undefined;
     }
 
@@ -52,17 +75,32 @@ function howManyMovies(movieArray) {
                     return item === 'Drama';
                 });
     });
+
     return 'Steven Spielberg directed ' + steveMovies.length + ' drama movies!';
 }
 // Order by title and print the first 20 titles
-function orderAlphabetically(moviesArray){
-    var moviesTitle=moviesArray.map(function(item){
+function orderAlphabetically(moviesArray) {
+    var moviesTitle = moviesArray.map(function (item) {
         return item.title;
     }).sort(function (title1, title2) {
         return title1.localeCompare(title2)
-      });
-    
-    return moviesTitle.length > 20 ? moviesTitle.filter((item,i)=> i < 20) : moviesTitle ;
+    });
+
+    return moviesTitle.length > 20 ? moviesTitle.filter((item, i) => i < 20) : moviesTitle;
 }
 
 // Best yearly rate average
+function bestYearAvg(movies) {
+    if (movies.length === 0) {
+        return undefined;
+    }
+
+    var year = movies.map(item => item.year).unique();
+
+
+
+
+    var arrayYears = movies.reduce(function (preview, next) {
+
+    }, {})
+}
