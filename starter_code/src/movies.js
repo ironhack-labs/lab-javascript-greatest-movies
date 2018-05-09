7a8198dff666828538054db2065f3db48c647525
Paula Quitián <pquitian@idealista.com>
2018-05-09 09:05:40 +0200

    [webptmad0418]3 interactions

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -5,12 +5,12 @@ function turnHoursToMinutes(movies) {
     return movies.map(function(movie) {
         movie = Object.assign({}, movie);
         var duration = movie.duration.split(' ').reduce(function(acc, time) {
-                if (time.indexOf('h') !== -1) {
-                    return Number(time.replace('h', '')) * 60 + acc;
-                } else {
-                    return Number(time.replace('min', '')) + acc;
-                }
-            }, 0);
+          if (time.indexOf('h') !== -1) {
+              return Number(time.replace('h', '')) * 60 + acc;
+          } else {
+              return Number(time.replace('min', '')) + acc;
+          }
+        }, 0);
         movie.duration = duration;
         return movie;
     });
@@ -20,17 +20,14 @@ function turnHoursToMinutes(movies) {
 
 function ratesAverage(arr) {
     var rates = arr.map(function(movies){
-      return movies.rate;
+      return Number(movies.rate);
     });
     var numbers = rates.map(function(rate){
-      
       return parseFloat(rate);
-
     });
     var sum = numbers.reduce(function(acc, curr){
       return acc + curr;
-    });
-    
+    }, 0);
     return sum / numbers.length;
 }
 
@@ -38,49 +35,16 @@ function ratesAverage(arr) {
 // Get the average of Drama Movies
 
 function dramaMoviesRate(films){
-    /*var genre = films.map(function(each){
-      if(each.genre.indexOf('Drama') !== -1) {
-        var dramaRate = each.rate;
-        return ratesAverage(films);
-        //return dramaRate;
-      }
-      else {
-        return;
-      }
-    });*/
-
-    /*var genre = films.reduce(function(acc, each){
-        if(each.genre.indexOf('Drama') !== -1) {
-          var dramaRate = each.rate;
-          return ratesAverage(films);
-          //return dramaRate;
-        }
-        else {
-          return;
-        }
-      });*/
-
-      var genre;
-    
-      if(films.length !== 1){
-        genre = films.reduce(function(acc, each){
-          if(each.genre.indexOf('Drama') !== -1) {
-            var dramaRate = each.rate;
-            return ratesAverage(films);
-            //return dramaRate;
-          }
-          else {
-            return;
-          }
-        });
-      }
-      else {
-        genre = films[0].rate;
-      }
-    
-    return genre;
-
-
+  var justDramas = films.filter(function(movie){
+    if(movie.genre.indexOf('Drama') !== -1) {
+      var dramaRate = movie.rate;
+      return dramaRate;
+    }
+    else {
+      return undefined;
+    }
+  });
+  return parseFloat(ratesAverage(justDramas).toFixed(2));
 }
 
 
