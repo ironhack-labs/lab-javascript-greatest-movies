3f189753bd8c4238269a32a045868af47cadda17
kainmaligno <s.e.jugr@gmail.com>
2018-05-09 00:54:16 -0500

    begin

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,6 +1,20 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
+function turnHoursToMinutes(movies){
+  let newMovies = movies.map((movie)=>{
+   var numbersOnlyPattern= /\d+/g;
+   var element = movie;
+   element.duration = element.duration.match(numbersOnlyPattern);
+   element.duration = parseInt(element.duration[0]*60) + parseInt(element.duration[1]);
+   return element;
+   });
+   return newMovies;
+}
+//let nuevoarreglo = 
 
+console.log(turnHoursToMinutes(movies));
+//
+console.log(movies)
 
 // Get the average of all rates with 2 decimals 
 
