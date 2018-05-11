8091831f337def8cdc36b448c4bd868018f3ff87
Ravi Kalia <ravkalia@gmail.com>
2018-05-11 14:17:31 -0500

    almost all tests on turnHoursToMinutes

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,14 +1,27 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes
+
+const itemHoursToMinutes = ({ duration: dur }) => {
+  const splitsArray = dur.toString().split(' ');
+  const hoursInMinutes = splitsArray[0].includes('h')
+    ? 60 * (parseInt(splitsArray[0], 10) || 0)
+    : 0;
+  const minutes = splitsArray[0].includes('h')
+    ? parseInt(splitsArray[1], 10) || 0
+    : parseInt(splitsArray[0], 10) || 0;
+  const result = hoursInMinutes + minutes;
+  return result;
+};
 const turnHoursToMinutes = (arr) => {
-  const itemHoursToMinutes = dur => ((parseInt(dur.split(' ')[1], 10) * 60) + parseInt(dur.split(' ')[2], 10));
   const result = arr.map((item) => {
-    item.duration = itemHoursToMinutes(item.duration);
+    item.duration = itemHoursToMinutes(item);
     return item;
   });
   return result;
 };
 
+const w = turnHoursToMinutes([{ duration: '2h 12m' }, { duration: '2h' }]);
+w;
 // Get the average of all rates with 2 decimals
 const ratesAverage = (arr) => {
 
