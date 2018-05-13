4d2df22d178d6cf2163aeedb031fb86a31632f5b
Ravi Kalia <ravkalia@gmail.com>
2018-05-12 20:21:32 -0500

    almost done on bonus exercise

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -25,7 +25,7 @@ const turnHoursToMinutes = (arr) => {
 const ratesAverage = (arr) => {
   const ratesArray = arr.map((item) => {
     const copy = Object.assign({}, item);
-    return parseInt(copy.rate, 10);
+    return (parseInt(copy.rate, 10) || 0);
   });
   const avg = (1 / ratesArray.length) *
     ratesArray.reduce((prev, curr) => prev + curr, 0);
@@ -36,25 +36,40 @@ const ratesAverage = (arr) => {
 const dramaMoviesRate = (arr) => {
   const dramasArray = arr.filter(item => item.genre.includes('Drama'));
   if (dramasArray.length === 0) return undefined;
-  ratesAverage(dramasArray);
+  return ratesAverage(dramasArray);
 };
 
 // Order by time duration, in growing order
 const orderByDuration = (arr) => {
-
+  const modifiedArray = turnHoursToMinutes(arr)
+    .sort((a, b) => a.duration - b.duration || a.title.localeCompare(b.title));
+  return modifiedArray;
 };
 
 // How many movies did STEVEN SPIELBERG
 const howManyMovies = (arr) => {
-
+  if (arr.length === 0) return undefined;
+  const count = arr
+    .filter(item => item.director === 'Steven Spielberg')
+    .filter(item => item.genre.includes('Drama'))
+    .length;
+  return `Steven Spielberg directed ${count} drama movies!`;
 };
-
 // Order by title and print the first 20 titles
 const orderAlphabetically = (arr) => {
-
+  const result = arr.sort((a, b) => a.title.localeCompare(b.title));
+  const count = (result.length > 20 ? 20 : result.length);
+  return result.slice(0, count).map(x => x.title);
 };
 
 // Best yearly rate average
-const bestYearRatings = (arr) => {
-
+const bestYearAvg = (arr) => {
+  if (arr.length === 0) return undefined;
+  const arrByYears = arr
+    .reduce((prev, curr) => (prev[curr.year]).push(curr), {});
+  const best = Object.values(arrByYears)
+    .reduce((prev, curr) => (prev.avgRate < ratesAverage(curr)
+      ? ({ year: curr.year, avgRate: ratesAverage(curr) })
+      : prev), { avgRate: -1, year: 0 });
+  return `The best year was ${best.year} with an average rate of ${best.avgRate}.`;
 };
