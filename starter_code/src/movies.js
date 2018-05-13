0ffb05f976cd0b29894c70a6ba37ce8608e72a14
Paula Quitián <pquitian@idealista.com>
2018-05-14 01:39:51 +0200

    [webptmad0418]Octavio-Paula_done!

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -20,6 +20,7 @@ function turnHoursToMinutes(movies) {
 
 function ratesAverage(arr) {
     var rates = arr.map(function(movies){
+      // Mira aquí!
       return Number(movies.rate);
     });
     var numbers = rates.map(function(rate){
@@ -38,23 +39,94 @@ function dramaMoviesRate(films){
   var justDramas = films.filter(function(movie){
     if(movie.genre.indexOf('Drama') !== -1) {
       var dramaRate = movie.rate;
-      return dramaRate;
+      if(movie.rate === ''){
+        movie.rate = 0;
+        return movie;
+      }
+      else {
+        return dramaRate;
+      }
     }
     else {
-      return undefined;
+      return 0;
     }
   });
-  return parseFloat(ratesAverage(justDramas).toFixed(2));
+  console.log(justDramas);
+  if(justDramas.length === 0){
+    return undefined;
+  }else{
+    return parseFloat(ratesAverage(justDramas).toFixed(2));
+  }
 }
 
 
 // Order by time duration, in growing order
 
+function orderByDuration(films) { 
+   
+  var sortedFilms = films.map(function(films) {
+    return films;
+   }).sort(function(first, second){
+     if(first.duration === second.duration) {
+      if(first.title > second.title){ //Repasar esto: se puede mejorar
+        return 1;
+      }
+      else if(first.title < second.title){
+        return -1;
+      }
+      else {
+        return 0;
+      }
+     }
+     else {
+       return first.duration - second.duration;
+     }
+  });
+  return sortedFilms;
+}
+
 
 // How many movies did STEVEN SPIELBERG
+
+function howManyMovies(films) {
+  console.log(films)
+  var spielbergMovies = films.filter(function(film){
+    return film.director === 'Steven Spielberg';
+  });
+
+  var spielbergDramas = spielbergMovies.filter(function(film){
+    return film.genre.indexOf('Drama') !== -1;
+  });
+
+  return films.length === 0 ?  undefined : 'Steven Spielberg directed ' +  spielbergDramas.length + ' drama movies!';
+}
 
 
 // Order by title and print the first 20 titles
 
+function orderAlphabetically(films) {
+  var filmsTitles = films.map(function(film){
+    var title = film.title;
+    return title
+  })
+
+  var sortedTitles = filmsTitles.sort(function(film1, film2){
+    return film1.localeCompare(film2);
+  });
+
+  if (films.length <= 20){
+    return sortedTitles;
+  }
+  else {
+    return sortedTitles.slice(0, 20);
+  }
+}
+
 
 // Best yearly rate average
+
+function bestYearAvg() {
+  // sort by year 
+  // separate the same year films
+
+}
