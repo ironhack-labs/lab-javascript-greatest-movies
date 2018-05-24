9a80bbacc4b79f8920a5fe01ea0770a37873b110
tjHsu <tim800821@hotmail.com>
2018-05-24 19:46:51 +0200

    passed iteration 3

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,12 +1,86 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
+function turnHoursToMinutes(movieInHour) {
+    // var movieInMinute;
+    
+    var movieInMinute = movieInHour.map(function(movie){
+
+      if(movie.duration.length>=8) {
+        var movieAfter={
+          title: movie.title,
+          year: movie.year,
+          director: movie.director,
+          duration: parseInt(movie.duration.charAt(0))*60+parseInt(movie.duration.charAt(3))*10+parseInt(movie.duration.charAt(4)),
+          genre: movie.genre,
+          rate: movie.rate
+        };
+        return  movieAfter;
+      } else if (movie.duration.length>=5){
+        var movieAfter={
+            title: movie.title,
+            year: movie.year,
+            director: movie.director,
+            duration: parseInt(movie.duration.charAt(0))*10+parseInt(movie.duration.charAt(1)),
+            genre: movie.genre,
+            rate: movie.rate
+          };
+          return  movieAfter;
+
+      } else if (movie.duration.length>=4){
+        var movieAfter={
+            title: movie.title,
+            year: movie.year,
+            director: movie.director,
+            duration: parseInt(movie.duration.charAt(0)),
+            genre: movie.genre,
+            rate: movie.rate
+          };
+          return  movieAfter;
+      } else {
+        var movieAfter={
+            title: movie.title,
+            year: movie.year,
+            director: movie.director,
+            duration: parseInt(movie.duration.charAt(0))*60,
+            genre: movie.genre,
+            rate: movie.rate
+          };
+          return  movieAfter;
+      }
+        
+    });
+    return movieInMinute;
+};
 
 
 // Get the average of all rates with 2 decimals 
 
+function ratesAverage(arr){
+    var sumRating =  arr.reduce(function(accumulator, current){
+        // if(current.rate==undefined){
+        //     continue;
+        // }
+        return current.rate ? accumulator + parseInt(current.rate):accumulator;
+    },0);
+
+    var averageRating = sumRating/arr.length;
+    return Math.round(averageRating*100) / 100;
+};
+
 
 // Get the average of Drama Movies
 
+function dramaMoviesRate(arr){
+    var arrDrama = arr.filter(function(movie){
+        return movie.genre.indexOf("Drama")>-1;
+    });
+
+    if (arrDrama.length==0) {
+        return undefined;
+    }
+
+    return ratesAverage(arrDrama);
+}
 
 // Order by time duration, in growing order
 
