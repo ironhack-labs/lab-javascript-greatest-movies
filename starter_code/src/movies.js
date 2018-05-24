44279799c713a731ba7616811c8fefc85f1b61f0
Chris Manthey <calined@gmx.de>
2018-05-24 12:59:48 +0200

    turnHoursToMinutes done

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,8 +1,35 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
-function turnHoursToMinutes(arrayOfThatMovieObject) {
 
-    return arrrayOfThatMovieObject;
+function turnHoursToMinutes(arrayOfMovies) {
+
+    var newArrayOfMovies = arrayOfMovies.map(function (movie) {
+
+        var durationHours = 0;
+
+        var hPos = movie.duration.indexOf("h");
+
+        if (hPos >= 0) {
+
+            durationHours = movie.duration.substr(0, hPos) * 1;
+
+        }
+
+        var durationMinutes = 0;
+
+        var mPos = movie.duration.indexOf("m");
+
+        if (mPos >= 0) {
+
+            durationMinutes = movie.duration.substring(hPos >= 0 ? hPos + 2 : 0, mPos) * 1;
+
+        }
+
+        return { duration: durationHours * 60 + durationMinutes };
+    })
+
+    return newArrayOfMovies;
+
 }
 
 // Get the average of all rates with 2 decimals 
