6d2b86d3ca74a90a77f8bcdf406e7088300a7aa2
Marie-Léa Vincent <marielea.vincent@gmail.com>
2018-06-07 18:36:48 +0200

    done

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,6 +1,20 @@
 /* eslint no-restricted-globals: 'off' */
+console.log("hello")
+console.log(movies.map(function(elmt){
+  return elmt.title
+}))
 // Turn duration of the movies from hours to minutes 
-
+function turnHoursToMinutes(array) {
+  return movies.map(function(elmt){
+    var minutes = 0;
+    console.log(elmt.duration)
+    var parsedString = (elmt.duration.indexOf("h") !== -1 ? elmt.duration.split("h")
+    parsedString[0] = parseInt(parsedString[0]) * 60;
+    parsedString[1] = parseInt(parsedString[1]);
+    elmt.duration = parsedString[0] + parsedString[1];
+    return elmt
+  })
+}
 
 // Get the average of all rates with 2 decimals 
 
