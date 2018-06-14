b7de1a14863b2d91bc515f4ab9f7d133cae73115
Junior Mendez-Mateo <junior_abigail@hotmail.com>
2018-06-14 15:17:18 -0400

    tests 1 to 3 complete

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,14 +1,59 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
 
+function turnHoursToMinutes(moviesArray) {
+  var moviesWithCorrectTimeFormat = [];
+  var eachMovieHours, eachMovieMinutes, totalMinutes, singleMovieWithCorrectTimeFormat;
+  moviesArray.map(function(eachMovie) {
+    eachMovieHours = 0;
+    eachMovieMinutes = 0;
+    totalMinutes = 0;
+    if (eachMovie.duration.indexOf("h") !== -1) {
+      eachMovieHours = eachMovie.duration.slice(0, eachMovie.duration.indexOf("h"));
+    } else {
+      eachMovieHours = 0;
+    }    
+    eachMovieMinutes = eachMovie.duration.slice(eachMovie.duration.indexOf("h") + 1, eachMovie.duration.indexOf("m"));
+    totalMinutes = (60 * eachMovieHours) + (1* eachMovieMinutes);
+    singleMovieWithCorrectTimeFormat = Object.assign({},eachMovie);
+    singleMovieWithCorrectTimeFormat.duration = totalMinutes;
+    moviesWithCorrectTimeFormat.push(singleMovieWithCorrectTimeFormat);
+  });
+  return moviesWithCorrectTimeFormat;
+}
 
 // Get the average of all rates with 2 decimals 
 
+function ratesAverage(moviesArray) {
+  var result = moviesArray.reduce(function(totalOfAllRatings, currentMovieRating){
+    return totalOfAllRatings + currentMovieRating.rate;
+  }, 0);
+
+  if(result.length === 0 ) {
+    return 0;
+  }
+  return result / moviesArray.length;
+}
 
 // Get the average of Drama Movies
 
+function dramaMoviesRate(moviesArray) {
+  var result =0;
+  var dramaMoviesArray = moviesArray.filter(function(eachMovie){
+    return eachMovie.genre.includes("Drama");
+  });
+  if (dramaMoviesArray.length === 0) {
+    return undefined;
+  }
+  result = Math.round(ratesAverage(dramaMoviesArray) * 100) / 100;
+  return result;
+}
 
 // Order by time duration, in growing order
+
+function orderByDuration(moviesArray) {
+  
+}
 
 
 // How many movies did STEVEN SPIELBERG
@@ -18,3 +63,4 @@
 
 
 // Best yearly rate average
+ 
