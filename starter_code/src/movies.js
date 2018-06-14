31e53b732a725d46e812723474d9b57a1699b93b
Niko <nkouuu@hotmail.com>
2018-06-14 12:39:16 +0200

    iteracion1

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,7 +1,26 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
 
+function turnHoursToMinutes(arr){
+    var duration;   
 
+    var arr2 = arr.map(function(e){
+        var element=e.duration.replace('h',"").replace("min","");
+        var a=element.split(' ');
+        if(a.length==2){
+            duration=parseInt(a[0])*60 + parseInt(a[1]);
+        }else{
+            if(e.duration.indexOf('h')!=-1)
+                 duration=parseInt(a[0])*60 ;
+            else
+            duration=parseInt(a[0]) ;
+        }
+        
+
+        return {duration:duration};
+    })
+    return arr2;
+}
 // Get the average of all rates with 2 decimals 
 
 
