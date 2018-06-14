05f1e1377f9218abd13c0ee67adeac36adeddf96
Andrea Moya <andrea@MacBook-Pro-de-Andrea.local>
2018-06-14 17:43:17 +0200

    we tried our best

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -4,15 +4,46 @@
 
 // Get the average of all rates with 2 decimals 
 
+function ratesAverage (movies) {
+  var total = movies.reduce(function (acc, item) {
+    return acc + Number(item.rate);
+  }, 0);
+  var average = total / movies.length ;
+  return Number(average.toFixed(2));
+};
 
 // Get the average of Drama Movies
+
+
+function dramaMoviesRate(movies){
+  
+  var dramas = movies.filter(function(item) {
+    if (item.genre.indexOf('Drama') !== -1){
+      return true;
+    };
+  });
+  if (dramas.length === 0) {
+    return;
+  }
+  return ratesAverage(dramas);
+}
 
 
 // Order by time duration, in growing order
 
 
 // How many movies did STEVEN SPIELBERG
-
+function howManyMovies(movies){
+  var amount = movies.filter(function(item) {
+    if (item.director.indexOf('Steven Spielberg') !== -1){
+      return true;
+    };
+  });
+  if (amount.length === 0) {
+    return;
+  }
+  return (''+amount.length);
+  }
 
 // Order by title and print the first 20 titles
 
