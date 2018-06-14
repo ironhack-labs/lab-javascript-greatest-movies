41e9fed2029de9d65fbc103f4d858ecc10ceff66
Ruben <rubenvilchesperez1985@hotmail.com>
2018-06-14 13:25:59 +0200

    Iteracion 1

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -2,6 +2,25 @@
 // Turn duration of the movies from hours to minutes 
 
 
+function turnHoursToMinutes (arr) {
+    var newArr=arr.map(function(element) {
+      if (element.duration.includes("h") && element.duration.includes("min")) {
+        var hoursToMinutes = element.duration.split(" ");
+        var hours = parseInt(hoursToMinutes[0].slice(0,1));
+        var minutes = parseInt(hoursToMinutes[1].slice(0,-3));
+        element = ((hours * 60) + minutes);
+      } else if (element.duration.includes("h")) {
+        var hours = parseInt(element.duration.slice(0,1));
+        element = (hours * 60);
+      } else {
+        var minutes = parseInt(element.duration.slice(0,-3));
+        element =  minutes;
+      }
+      return {duration: element};
+    }); 
+    return newArr; 
+}   
+
 // Get the average of all rates with 2 decimals 
 
 
