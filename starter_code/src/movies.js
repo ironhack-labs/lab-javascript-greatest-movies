903746ee990733a6501342bf400907f0bdc34839
Alberto <albertomm0687@gmail.com>
2018-06-14 17:55:04 -0500

    cine

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,8 +1,80 @@
 /* eslint no-restricted-globals: 'off' */
-// Turn duration of the movies from hours to minutes 
+// Turn duration of the movies from hours to minutes
+var nuevo=[];
+  var rata=[];
+function turnHoursToMinutes(movies){
+  //console.log(movies);
+  var dato_nuevo=0;
+  nuevo=movies.map(function(pelis){
+    var sin;
+    var sinm;
+    var hora;
+    var min1;
+    var min2;
+    var mintot;
+
+    sin= pelis.duration.replace("h","");
+    sinm= sin.replace("min","");
+    //console.log(sinm)
+    min1=sinm.substr(2);
+    min2=sinm.substr(3);
+    hora=sinm.substr(0);
+    min1=parseInt(min1)
+    min2=parseInt(min2)
+    hora=parseInt(sinm)
+    hora=hora*60;
+    min1=min1*10;
+    min2=min2*1;
+    mintot=hora+min1+min2;
+    console.log("son " + mintot + " minutos");
+    //pelis.duration=parseInt(pelis.duration);
+    //pelis.duration=pelis.duration*60;*/
+
+    /*for(var i=0;i<pelis.duration.length;i++)
+    {
+      if(pelis.duration[i]===0||1||2||3||4||5||6||7||8||9)
+      {
+        dato_nuevo.push(pelis.duration[i]);
+      }
+    }*/
 
 
-// Get the average of all rates with 2 decimals 
+    return hora;
+  });
+  //console.log(nuevo);
+  //console.log(hora);
+  //console.log(dato_nuevo);
+}
+function ratesAverage(peli)
+{
+
+  for(var i=0;i<peli.length;i++)
+  {
+    var nav=parseInt(peli.rate);
+    console.log(nav);
+    rata.push(nav);
+    console.log(nav);
+  }
+  var total=rata.reduce(function(acumulador,numero)
+  {
+    return acumulador+numero;
+  });
+console.log(total);
+}
+
+
+turnHoursToMinutes(movies);
+ratesAverage(movies);
+
+  //console.log(movies);
+  var dato_nuevo=[];
+  nuevo=movies.map(function(pelis){
+    pelis.duration=1
+    return pelis;
+  });
+  console.log(nuevo);
+}
+// Get the average of all rates with 2 decimals
 
 
 // Get the average of Drama Movies
