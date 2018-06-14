fef60d13d7b7d5c51f966afc06618b96b7c3d235
Paula <paulagarizcun@gmail.com>
2018-06-14 17:00:20 +0200

    Iteración 1 e intento de 2

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,9 +1,30 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
-
+function turnHoursToMinutes(arr){ 
+  return arr.map(function(e,i,a){
+    if (e.duration.split(" ").length > 1) {
+      return {duration: +e.duration.split(" ")[0].slice(0,1)*60 + +e.duration.split(" ")[1].replace(/min/g,"")}
+    } 
+    else if (e.duration.split(" ").length == 1 && e.duration.includes("h")) {
+      return {duration: +e.duration.split(" ")[0].slice(0,1)*60}
+    }
+    else {
+      return {duration: +e.duration.replace(/min/g,"")}
+    }
+  });
+};
 
 // Get the average of all rates with 2 decimals 
-
+function ratesAverage(arr){
+  for (var i=0;i<arr.length;i++ ){
+  var ratesArray = [];
+  ratesArray.push(arr[i].rate);
+  console.log(ratesArray);
+  var average = ratesArray.reduce(function(accumulator, current){
+    return +accumulator + current / ratesArray.length;
+  }, 0);
+  }
+};
 
 // Get the average of Drama Movies
 
