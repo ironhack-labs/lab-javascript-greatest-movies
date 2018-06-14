2e76196a939e1e1a0b6ce36ff97b0ebcedc4f822
Jackson Mades <jacksonmades@gmail.com>
2018-06-14 12:40:19 -0400

    The turnHoursToMinutes mostly works now

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,6 +1,59 @@
 /* eslint no-restricted-globals: 'off' */
-// Turn duration of the movies from hours to minutes 
-
+// Turn duration of the movies from hours to minutes
+function turnHoursToMinutes(movies){
+    return movies.map(function(curMovie){
+        if(curMovie.duration.length===8){//hours and minutes
+            return{
+                title: curMovie.title,
+                year: curMovie.year,
+                director: curMovie.director,
+                duration:(Number(curMovie.duration.charAt(0))*60+ Number(curMovie.duration.substring(3,5))),
+                genre: curMovie.genre,
+                rate: curMovie.rate
+        }
+        }
+        else if(curMovie.duration.length===7){//hour and minutes
+            return{
+                title: curMovie.title,
+                year: curMovie.year,
+                director: curMovie.director,
+                duration:(Number(curMovie.duration.charAt(0))*60+Number(curMovie.duration.substring(3,4))),
+                genre: curMovie.genre,
+                rate: curMovie.rate
+        }
+        }
+        else if(curMovie.duration.length===3){//hour
+            return{
+                title: curMovie.title,
+                year: curMovie.year,
+                director: curMovie.director,
+                duration:Number(curMovie.duration.charAt(0))*60,
+                genre: curMovie.genre,
+                rate: curMovie.rate
+            }
+        }
+        else if(curMovie.duration.length===5){//minutes
+            return{
+                title: curMovie.title,
+                year: curMovie.year,
+                director: curMovie.director,
+                duration:Number(curMovie.duration.substring(0,2)),
+                genre: curMovie.genre,
+                rate: curMovie.rate
+            }
+        }
+        else{//minute
+            return{
+                title: curMovie.title,
+                year: curMovie.year,
+                director: curMovie.director,
+                duration:Number(curMovie.duration.substring(0,1)),
+                genre: curMovie.genre,
+                rate: curMovie.rate
+            }
+        }
+    })
+}
 
 // Get the average of all rates with 2 decimals 
 
