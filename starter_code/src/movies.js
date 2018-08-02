5799fb3dbd261b53f3f42699329bef4442965d63
trong nhan nguyen <ntnhan30@gmail.com>
2018-08-02 15:22:30 +0200

    we try

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,6 +1,18 @@
 /* eslint no-restricted-globals: 'off' */
-// Turn duration of the movies from hours to minutes 
+//Turn duration of the movies from hours to minutes 
 
+function turnHoursToMinutes(movies) {
+    var newMovies= movies.map(function(film){  
+       var x=film.duration.split(" ");
+        if (film.duration.includes("h")) {
+         var h = parseInt(x[0].replace("h",""), 10)*60;
+        }
+         var min= parseInt(x[1].replace("min",""), 10) ;
+         film.duration= h+min;
+         return film;
+       });
+        return newMovies;
+ } 
 
 // Get the average of all rates with 2 decimals 
 
