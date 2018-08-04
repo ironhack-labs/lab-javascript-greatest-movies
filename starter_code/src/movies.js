3fd2addbfa2e81ca17a6f460b32fc82d8299698a
vazz <elisa.merinovazquez@gmail.com>
2018-08-04 23:11:26 +0200

    done 3 out of 7

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -20,12 +20,60 @@ var ratesAverage = movies.map(function(movie) {
 	return rates;
 });
 console.log(ratesAverage);
-// Get the average of Drama Movies
 
+// Get the average of Drama Movies
+function dramaMoviesRate(movielist) {}
 // Order by time duration, in growing order
 
 // How many movies did STEVEN SPIELBERG
+function howManyMovies(movies) {
+	var counter = 0;
 
+	var dramaMovies = movies.map(function(movies) {
+		let result = movies.genre.find((genre) => genre === 'Drama');
+		return result;
+	});
+	//console.log(dramaMovies);
+
+	let searchSpielberg = movies.map(function(movies) {
+		if (movies.director === 'Steven Spielberg' && dramaMovies != undefined) {
+			counter++;
+			return true;
+		} else if (movies.director !== 'Steven Spielberg') {
+			return 'Steven Spielberg directed 0 drama movies!';
+		} else {
+			counter;
+		}
+	});
+
+	if (counter > 0) {
+		return 'Steven Spielberg directed ' + counter + ' drama movies!';
+	} else {
+		return undefined;
+	}
+}
+
+console.log(howManyMovies(movies));
 // Order by title and print the first 20 titles
+function orderAlphabetically(movies) {
+	var topTwenty = [];
 
+	var orderedTitles = movies.map(function(movies) {
+		return movies.title;
+	});
+	orderedTitles.sort();
+
+	if (orderedTitles.length <= 20) {
+		topTwenty = orderedTitles;
+	} else {
+		for (var i = 0; i < 20; i++) {
+			topTwenty.push(orderedTitles[i]);
+		}
+		return topTwenty;
+	}
+
+	return orderedTitles;
+}
+
+console.log(orderAlphabetically(movies));
 // Best yearly rate average
