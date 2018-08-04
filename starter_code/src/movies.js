dcc99ff1ba8c45946a546e9906c734b3d79448b5
vazz <elisa.merinovazquez@gmail.com>
2018-08-04 17:24:23 +0200

    first done

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,20 +1,31 @@
 /* eslint no-restricted-globals: 'off' */
-// Turn duration of the movies from hours to minutes 
+// Turn duration of the movies from hours to minutes
 
+var turnHoursToMinutes = movies.map(function(movies) {
+	movies.duration = movies.duration.match(/\d+/g);
+	var hours = movies.duration[0] * 60;
+	var minutes = movies.duration[1] * 1;
 
-// Get the average of all rates with 2 decimals 
+	movies.duration = hours + minutes;
+});
+console.log(movies);
+// Get the average of all rates with 2 decimals
+var ratesAverage = movies.map(function(movie) {
+	var moviescores = parseFloat(movie.rate);
 
-
+	var movierates = [ 9.3, 9.2, 9 ];
+	var rates = movierates.reduce(function(sum, score) {
+		return sum + score;
+	}, 0);
+	return rates;
+});
+console.log(ratesAverage);
 // Get the average of Drama Movies
-
 
 // Order by time duration, in growing order
 
-
 // How many movies did STEVEN SPIELBERG
 
-
 // Order by title and print the first 20 titles
-
 
 // Best yearly rate average
