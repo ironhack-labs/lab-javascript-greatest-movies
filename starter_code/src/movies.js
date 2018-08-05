16b6a2b5bae9148ee8f2f2ca750e8cbd5af45c74
Adolfo Vela <vela@tekplanner.com>
2018-08-05 08:07:22 -0500

    Faltan 2 errores, no pudo hacer que no se altere el objeto movies

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,6 +1,53 @@
 /* eslint no-restricted-globals: 'off' */
-// Turn duration of the movies from hours to minutes 
+// Turn duration of the movies from hours to minutes
 
+
+ console.log(movies)
+
+// var moviesDuration = movies;
+var moviesDuration = Object.assign({}, movies);
+  console.log(movies); // { a: 1 }
+
+function turnHoursToMinutes(moviesDuration) {
+
+  Object.keys(moviesDuration).forEach(function (item) {
+
+    // console.log(moviesDuration[item].duration);
+    // Declaraciòn de variables
+    var horasArray = moviesDuration[item].duration.split(" ");
+    // console.log(horasArray);
+    var horasNumber = 0;
+    var minNumber = 0;
+
+    // Si no encuentra la "h" entonces hora = 0
+    if (horasArray[0].indexOf("h") == -1) {
+      horasNumber = 0;
+      minNumber = parseInt(horasArray);
+    } else if (!horasArray[1]) { //Si no encuentra los minutos entonces minutos = 0
+      horasNumber = parseInt(horasArray);
+      minNumber = 0;
+    } else {
+      horasNumber = parseInt(horasArray); //Hay horas y minutos
+      minNumber = parseInt(horasArray[1]);
+    }
+
+    // Calculo convierte horas-minutos a minutos
+    var minutos = horasNumber * 60 + minNumber;
+
+
+    //Se adiciona el valor a la matriz
+    moviesDuration[item].duration = minutos;
+    // console.log(item);
+  });
+
+  // console.log(moviesDuration);
+  return moviesDuration;
+
+}
+
+//  console.log(turnHoursToMinutes(movies))
+console.log(movies);
+turnHoursToMinutes(movies);
 
 // Get the average of all rates with 2 decimals 
 
