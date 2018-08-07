e5f2f17d22981884d5acfb18b8c832f733a4c17f
Gonzalo Quiroz <kiyegoo@gmail.com>
2018-08-07 14:56:18 -0500

    Falta corregir errores y terminar el Bonus

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -44,7 +44,7 @@ function ratesAverage(object) {
 
 function dramaMoviesRate(object) {
   var arrayDrama = [];
-  var newArray = object.map(function(movie) {
+  object.map(function(movie) {
     if (movie.genre.indexOf("Drama") >= 0) {
       arrayDrama.push(movie.rate);
     }
@@ -60,14 +60,101 @@ function dramaMoviesRate(object) {
 
   rate = rate / arrayDrama.length;
 
-  console.log(rate);
   return rate;
 }
 
 // Order by time duration, in growing order
 
+function orderByDuration(object) {
+  var newArray = object.map(function(movie) {
+    return movie;
+  });
+
+  newArray.sort(function(a, b) {
+    if (a.duration === b.duration) {
+      var nameA = a.title.toLowerCase();
+      var nameB = b.title.toLowerCase();
+      if (nameA < nameB) {
+        return a.duration;
+      }
+
+      if (nameA > nameB) {
+        return b.duration;
+      }
+    }
+    return a.duration - b.duration;
+  });
+
+  return newArray;
+}
+
 // How many movies did STEVEN SPIELBERG
+
+function howManyMovies(object) {
+  if (object <= 0) {
+    return undefined;
+  }
+
+  var spielbergArray = [];
+  object.map(function(movie) {
+    if (
+      movie.director === "Steven Spielberg" &&
+      movie.genre.indexOf("Drama") >= 0
+    ) {
+      spielbergArray.push(movie);
+    }
+  });
+
+  return (
+    "Steven Spielberg directed " + spielbergArray.length + " drama movies!"
+  );
+}
 
 // Order by title and print the first 20 titles
 
+function orderAlphabetically(object) {
+  var orderAlphabeticallyArray = [];
+  var objectOrdered = object.sort(function(a, b) {
+    var nameA = a.title.toLowerCase();
+    var nameB = b.title.toLowerCase();
+
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+    return 0;
+  });
+
+  objectOrdered.map(function(movie) {
+    if (orderAlphabeticallyArray.length < 20) {
+      orderAlphabeticallyArray.push(movie.title);
+    }
+  });
+
+  return orderAlphabeticallyArray;
+}
+
 // Best yearly rate average
+
+function bestYearAvg(object) {
+  var yearsAvg = [];
+  object.forEach(function(movie) {
+    if (yearsAvg.indexOf(movie.year) === -1) {
+      yearsAvg.push(movie.year);
+    }
+  });
+
+  var movieReduce = [];
+  yearsAvg.forEach(function(year) {
+    object.map(function(movie) {
+      if (year === movie.year) {
+        movieReduce.push(movie);
+      }
+    });
+  });
+
+  console.log(movieReduce);
+  //console.log(yearsAvg);
+}
