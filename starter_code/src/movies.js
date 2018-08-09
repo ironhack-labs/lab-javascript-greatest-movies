84d9c286c247bdef3642bd24b3c1ff76b12cd402
Pauline Brothier <paulinebrothier@gmail.com>
2018-08-09 17:06:45 +0200

    done

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -4,8 +4,39 @@
 
 // Get the average of all rates with 2 decimals 
 
+function ratesAverage (movies) {
+    var myAverage = 
+        movies.reduce(function (acc, oneRate) {
+        return acc + parseFloat(oneRate.rate);
+}, 0);
+return(myAverage / movies.length);
+}
+
+ratesAverage (movies);
+
 
 // Get the average of Drama Movies
+
+// new array with only dramas
+function  dramaMoviesThing (myArray) {
+    var dramaMovies = 
+      myArray.filter(function (oneMovie) {
+      return oneMovie.genre.includes('Drama');
+    });
+    return dramaMovies;
+  }
+
+  // get the average
+  function dramaMoviesRate (myArray) {
+  var myDramaArray =  dramaMoviesThing(myArray);
+  var myDramaAverage = 
+    myDramaArray.reduce(function(acc, oneRate){
+      return acc + parseFloat(oneRate.rate);
+    }, 0);
+    return (myDramaAverage / myDramaArray.length);
+  }
+  
+  dramaMoviesRate (movies);
 
 
 // Order by time duration, in growing order
