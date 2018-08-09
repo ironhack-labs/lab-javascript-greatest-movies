39d315750a4eff59be5c8ea14b8f0fb83cc5f011
Nerimene Akari <akari.nerimene@gmail.com>
2018-08-09 17:59:37 +0200

    done

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -4,9 +4,36 @@
 
 // Get the average of all rates with 2 decimals 
 
+function ratesAverage(movies) {
+    var sum =
+    movies.reduce(function (acc, oneRate) {
+      var toInt =  parseFloat(oneRate.rate)
+      return acc + toInt;
+      
+    }, 0);
+    var average = sum / movies.length;
+    return average;
+  };
+
 
 // Get the average of Drama Movies
-
+function dramaMoviesRate(movies) {
+    var drama = 
+    movies.filter(function (oneMovie) {
+        if (oneMovie.rate || oneMovie.rate !== "") {
+        return oneMovie.genre.includes("Drama");
+        }
+    });
+   var sum = 
+    drama.reduce(function (acc, oneRate) {
+    if (oneRate.rate || oneMovie.rate !== "") {
+       var toInt = Number(oneRate.rate);
+       return acc + toInt;
+    }
+   }, 0);
+   var average = sum / drama.length;
+   return Number(average.toFixed(2));
+};
 
 // Order by time duration, in growing order
 
