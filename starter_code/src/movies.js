e34e1f8accf2735e3b566651bdca5e5daa3e94ec
robotic-amoeba <raul.martin.majadas@gmail.com>
2018-08-23 18:57:06 +0200

    Implement Iteration1

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,6 +1,30 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
 
+function turnHoursToMinutes(arrayMovies) {
+  var arrayMoviesParsed=arrayMovies.map(function(movie){
+    var minutes=0;
+    var haveHours=movie.duration.indexOf("h");
+    var haveMinutes=movie.duration.indexOf("min");
+    if(haveHours > 0 && haveMinutes > 0){
+      minutes+=parseInt(movie.duration.split(" ")[1].slice(0,-3))
+      minutes+=parseInt(movie.duration.split(" ")[0].slice(0,-1))*60
+    }else if(haveMinutes > 0){
+      minutes=parseInt(movie.duration.slice(0,-3))
+    }else{
+      minutes=parseInt(movie.duration.slice(0,-1))*60
+    }
+    return Object.assign({},movie,{duration:minutes})
+  })
+
+  return arrayMoviesParsed;
+}
+
+/* function turnHoursToMinutes (arrayMovies) {
+  var newMovies = arrayMovies.map(function(film, i){
+    
+  })
+} */
 
 // Get the average of all rates with 2 decimals 
 
