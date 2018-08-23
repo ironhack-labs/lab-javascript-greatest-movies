e5a8e97a00ffa4a635fc95ebaec133be4a035306
Celia Arnelas <37745928+Carnelas@users.noreply.github.com>
2018-08-23 18:51:00 +0200

    Hasta 3

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,12 +1,41 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
 
+function turnHoursToMinutes(array) {
+  var timing = array.map(function(e){
+    text = e.duration.replace("h","").replace("min","").split(" ")
+    hours = text[0]*60;
+    min = text[1];
+    totalDuration = parseInt(hours) + parseInt(min);
+    console.log(totalDuration)
+    return Object.assign({},e,{duration:totalDuration});
+  })
+  return timing;
+ }
 
 // Get the average of all rates with 2 decimals 
 
+function ratesAverage(a) {
+  return a.reduce(function(acc, e, i) {
+  var media = e.rate / a.length + acc;
+  return Number(media.toFixed(2));
+  }, 0);
+  }
 
 // Get the average of Drama Movies
 
+
+function dramaMoviesRate(a) {
+  var dramaFilter = a.filter(function(e) {
+    return e.genre.includes("Drama");
+  });
+  var averageDrama = ratesAverage(dramaFilter);
+  if (averageDrama == 0) {
+    return undefined;
+  } else {
+    return averageDrama;
+  }
+  };
 
 // Order by time duration, in growing order
 
@@ -18,3 +47,22 @@
 
 
 // Best yearly rate average
+
+
+/* var movies = [
+  {
+    title: 'The Shawshank Redemption',
+    year: '1994',
+    director: 'Frank Darabont',
+    duration: '2h 22min',
+    genre: ['Crime', 'Drama'],
+    rate: '9.3'
+  },
+]
+*/
+
+
+
+
+
+ 
