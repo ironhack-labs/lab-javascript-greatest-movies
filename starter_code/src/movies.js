e286ba442f20fa31828b4f0f56f7c62074a50272
andrew <andrew00624@gmail.com>
2018-08-23 16:41:16 -0400

    done

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,8 +1,30 @@
 /* eslint no-restricted-globals: 'off' */
+
 // Turn duration of the movies from hours to minutes 
+
+function turnHoursToMinutes (movie){
+    for (var i=0; i<movie.length;i++){
+      var horas=parseInt(movie[i].duration.split(" ")[0].toString().replace('h',''));
+      var minutos=parseInt(movie[i].duration.split(" ")[1].toString().replace('min',''));
+      var tiempo=(horas*60)+minutos
+      movie[i].duration=tiempo + ' mins';
+    }
+    return movie
+   }
+   
+   turnHoursToMinutes(movies)
 
 
 // Get the average of all rates with 2 decimals 
+
+function ratesAverage (movie){
+    var rate=0.0;
+    for (var i=0; i<movie.length;i++){
+    rate+=parseFloat(movie[i].rate.toString());
+   }
+   return ((rate/movie.length).toFixed(2));
+   }
+   ratesAverage(movies)
 
 
 // Get the average of Drama Movies
