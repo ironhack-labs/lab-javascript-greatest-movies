7c3519f40fc3a832e6b52f8087b9d26dee951359
jsplmns <jonathan@spielmannsbilder.de>
2018-08-24 16:18:40 +0200

    Exercise BCN Jonathan & Yenderly

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -4,8 +4,31 @@
 
 // Get the average of all rates with 2 decimals 
 
+function ratesAverage(movies) {
+  var sumRatings = movies.reduce(function(acc, item){
+    return acc + item.rate;
+  }, 0);
+  
+  return sumRatings / movies.length;
+};
 
 // Get the average of Drama Movies
+
+function dramaMoviesRate(movies) {
+  var dramaMovies = movies.filter(function(item){
+    // if (item.genre === ['Drama']) {
+    //   return item;
+    //   }; 
+    return item.genre.indexOf('Drama') !== -1;
+  });
+
+  var sumDramaRatings = dramaMovies.reduce(function(acc, item){
+    return acc + item.rate;
+  }, 0);
+  
+  return sumDramaRatings / dramaMovies.length;
+
+};
 
 
 // Order by time duration, in growing order
