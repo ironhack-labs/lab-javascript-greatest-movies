fdf34628c32ec99273edbe0928971b130a7ee732
MJHR <majohr89@gmail.com>
2018-08-24 16:19:11 +0200

    movies

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -4,9 +4,28 @@
 
 // Get the average of all rates with 2 decimals 
 
+function ratesAverage (moviesArray){
+  var sum = moviesArray.reduce (function (acu, item){
+    return acu + item.rate;
+  }, 0);
+  return sum/moviesArray.length;
+};
 
 // Get the average of Drama Movies
 
+function dramaMoviesRate (moviesArray) {
+  var dramaMovies = moviesArray.filter(function (item) {
+    if (item.genre.indexOf('Drama') !== -1) {
+      return item
+    } else{
+      return undefined;
+    } 
+
+    // return item.genre === ['Drama'];
+  });
+  var ratesDrama = ratesAverage(dramaMovies);
+  return ratesDrama;
+}
 
 // Order by time duration, in growing order
 
