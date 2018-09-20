b67608c2ec58c3e0e23779eb1f0080f62e773394
Evapm <eva.p.mejias@gmail.com>
2018-09-20 20:54:15 +0200

    probando

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -3,9 +3,31 @@
 
 
 // Get the average of all rates with 2 decimals 
-
+function ratesAverage(array){
+    var sum = array.reduce(function(acc, currentValue){
+        return acc + currentValue.rate
+    }, 0);
+    return sum/array.length
+}
 
 // Get the average of Drama Movies
+function dramaMoviesRate(array){
+        var noDramaMamma;
+        var dramaMovies = array.filter(function(movie){
+            return movie.genre.find(function(sinGen){
+                return sinGen === "Drama"
+        });
+    });
+    if(!dramaMovies){
+        return undefined;
+    }
+    if else(!noDramaMamma){
+        return undefined;
+    }
+    else{
+        return Math.round(ratesAverage(dramaMovies) * 100)/100;
+    }
+};
 
 
 // Order by time duration, in growing order
