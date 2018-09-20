ff68b088112ffbe9665754968eda5777373dcb3c
stellarwaves <matias.salasdev@gmail.com>
2018-09-20 23:39:06 +0200

    commit

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -3,16 +3,35 @@
 
 
 // Get the average of all rates with 2 decimals 
-
+function ratesAverage(array){
+    var sum = array.reduce(function(acc, currentValue){
+        return acc + currentValue.rate;
+    },0);
+    return sum/array.length;
+}
 
 // Get the average of Drama Movies
-
-
+function dramaMoviesRate(array){
+    var dramaMovies = array.filter(function(movie){
+      return movie.genre.find(function(singleGenre){
+        return singleGenre === 'Drama';
+      });
+    });
+    if(!dramaMovies.length){
+      return undefined;
+    }
+    return ratesAverage(dramaMovies);
+}
 // Order by time duration, in growing order
 
 
 // How many movies did STEVEN SPIELBERG
-
+function howManyMovies(array){
+    var stevenMovies = array.filter(function(movie){
+        return movie.director === 'Steven Spielberg';
+    });
+    console.log(stevenMovies);
+}
 
 // Order by title and print the first 20 titles
 
