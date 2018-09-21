6ddaf2eddf4d34d446937898c9a1a47e78a84947
Evapm <eva.p.mejias@gmail.com>
2018-09-21 20:11:57 +0200

    All except bonus

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,9 +1,26 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
-function turnHoursToMinutes(array){
-    
-
-}
+function turnHoursToMinutes (array) {
+    var findMinutes = array.map(function (movie) {
+      var copyMovie = Object.assign({}, movie);
+      var horasEnMinutos = 0;
+      var minutos = 0;
+      if (copyMovie.duration.indexOf('h') > -1) {
+        horasEnMinutos += copyMovie.duration[0]*60;
+      }
+      if (copyMovie.duration.indexOf('min') > -1) {
+        if (copyMovie.duration.indexOf('h') > -1) {
+          minutos += copyMovie.duration.substring(3, (copyMovie.duration.length-3));
+        } else {
+          minutos += copyMovie.duration.substring(0, (copyMovie.duration.length-3));
+        }
+      }
+        var totalMinutes = Number(horasEnMinutos) + Number(minutos);
+        copyMovie.duration = totalMinutes;
+        return copyMovie;
+    });
+    return findMinutes;
+}   
 
 // Get the average of all rates with 2 decimals 
 function ratesAverage(array){
@@ -20,7 +37,7 @@ function dramaMoviesRate(array){
                 return sinGen === "Drama"
         });
     });
-    if(!dramaMovies){
+    if(!dramaMovies.length){
         return undefined;
     }
     else{
@@ -31,25 +48,34 @@ function dramaMoviesRate(array){
 
 // Order by time duration, in growing order
 function orderByDuration(array){
-
+    array.sort(function(a,b){
+        if(a.duration === b.duration){
+            array.sort(function(a,b){
+                if(a.title < b.title){
+                    return 1;
+                }
+            }) 
+        }
+        return a.duration - b.duration
+    });
+    return array;
 }
 
 // How many movies did STEVEN SPIELBERG
 function howManyMovies(array){
-    var spielberg = array.filter(function(movsofspi){
-        return movsofspi.director === "Steven Spielberg";
-    });
-    var dramasOfSpielberg = spielberg.filter(function(dramas){
+    if (!array.length){
+        return undefined;
+    }
+    var dramamov = array.filter(function(dramas){
         return dramas.genre.find(function(drama){
             return drama === "Drama";
         });
     });
+    var dramasOfSpielberg = dramamov.filter(function(movsofspi){
+        return movsofspi.director === "Steven Spielberg";
+    });
     var total = "Steven Spielberg directed " + dramasOfSpielberg.length + " drama movies!"
-    if (!dramasOfSpielberg){
-        "Steven Spielberg directed 0 drama movies!";
-    }else{
         return total;
-    }
     //Me iluminé (ni siquiera recordaba por qué había hecho el map) :D
 };
 
@@ -58,10 +84,17 @@ function orderAlphabetically(array){
     var movieTitles = array.map(function(titles){
         return titles.title;
     }).sort();
-    for(var i = 0; i < 20;i++){
-        console.log(movieTitles[i]);
+    var AlphOrder =[]
+    if(movieTitles.length < 20){
+        for(var i = 0; i < movieTitles.length;i++){
+            AlphOrder.push(movieTitles[i]);
+        }
+    }else{
+        for(var i = 0; i < 20;i++){
+        AlphOrder.push(movieTitles[i]);
+        }
     }
-    //He limpiado un poco el código resumiéndolo en una única variable
+    return AlphOrder
 }
 
 // Best yearly rate average
