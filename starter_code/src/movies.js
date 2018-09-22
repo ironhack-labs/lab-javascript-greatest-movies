df78c7809730a01c5dfe3dbbc27661ab6096826a
Bjornhona <asaeri3@hotmail.com>
2018-09-22 09:12:24 +0200

    some more exercises added

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,6 +1,26 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
-
+function turnHoursToMinutes (array) {
+  var findMinutes = array.map(function (movie) {
+    var copyMovie = Object.assign({}, movie);
+    var horasEnMinutos = 0;
+    var minutos = 0;
+    if (copyMovie.duration.indexOf('h') > -1) {
+      horasEnMinutos += copyMovie.duration[0]*60;
+    }
+    if (copyMovie.duration.indexOf('min') > -1) {
+      if (copyMovie.duration.indexOf('h') > -1) {
+        minutos += copyMovie.duration.substring(3, (copyMovie.duration.length-3));
+      } else {
+        minutos += copyMovie.duration.substring(0, (copyMovie.duration.length-3));
+      }
+    }
+      var totalMinutes = Number(horasEnMinutos) + Number(minutos);
+      copyMovie.duration = totalMinutes;
+      return copyMovie;
+  });
+  return findMinutes;
+}
 
 // Get the average of all rates with 2 decimals 
 function ratesAverage(array) {
@@ -26,12 +46,17 @@ function dramaMoviesRate(array) {
 console.log('The average rate of "Drama" movies is: ' + dramaMoviesRate(movies));
 
 // Order by time duration, in growing order
-/* function orderByDuration(array) {
-  var durationMovies = array.filter(function(movie) {
-    return movie.duration;
+function orderByDuration(array) {
+  var orderedArray = array.sort(function (a, b) {
+    if (a.duration === b.duration) {
+      if (a.title > b.title) {
+        return 1;
+      }
+    }
+    return a.duration - b.duration;
   });
-  return durationMovies.sort();
-} */
+  return orderedArray;
+}
 // How many movies did STEVEN SPIELBERG
 
 // function howManyMovies (array) {
@@ -50,16 +75,19 @@ console.log('The average rate of "Drama" movies is: ' + dramaMoviesRate(movies))
 
 // Order by title and print the first 20 titles
 function orderAlphabetically(array) {
-  array.sort(function(a, b) {
+  /* array.sort(function(a, b) {
     return a.title - b.title;
-  });
+  }); */
+  var movieTitles = array.map(function (titles) {
+    return titles.title;
+  }).sort();
   var first20 = [];
   var limit = 20;
-  if (array.length < 20) {
-    limit = array.length;
+  if (movieTitles.length < 20) {
+    limit = movieTitles.length;
   }
   for (var i = 0; i < limit; i++) {
-    first20.push(array[i].title);
+    first20.push(movieTitles[i]);
   }
   return first20;
 }
