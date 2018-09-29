989e39a4653bbf21dc32563b1f8edfd7649d5581
Guillaume Lepercq <glepercq.pro@gmail.com>
2018-09-29 14:47:31 +0200

    Ite1 ok

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,5 +1,29 @@
 /* eslint no-restricted-globals: 'off' */
+
+//[2,h, ,2,2,m,i,n]
 // Turn duration of the movies from hours to minutes 
+
+function turnsHoursToMinutes(movies){
+  var fixedMovies = movies.map(
+  function(oneMovie) {
+  var splitDuration = oneMovie.duration.split("")
+  var getHours = splitDuration[0]
+  
+  var getMinutes = 0
+  if (splitDuration[4]==="m") {
+      getMinutes = splitDuration[3]
+  }
+  else {
+    var arrayMinutes = [splitDuration [3], splitDuration [4]]  
+   getMinutes = arrayMinutes.join("")  
+   };
+var minutes = Number(getHours*60) + Number(getMinutes)
+oneMovie.duration = String(minutes)
+return oneMovie}
+)
+return fixedMovies
+}
+console.log(turnsHoursToMinutes(movies))
 
 
 // Get the average of all rates with 2 decimals 
