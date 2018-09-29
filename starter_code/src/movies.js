e5d79bf0019bff8e6365f2aace3a8172d650e28e
Rita Mernissi <rita.mernissi@hotmail.com>
2018-09-29 14:52:31 +0200

    first iteration works but doesn't pass the tests

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,6 +1,19 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
 
+function turnHoursToMinutes(duration) {
+    var splitDuration = duration.split("h ");
+    var hours = 60 * parseInt(splitDuration[0]);
+    var splitMinutes = parseInt(splitDuration[1].slice(0,2));
+    var totalInMinutes = hours + splitMinutes;
+   return String(totalInMinutes);
+}
+  
+var newDuration = movies.map(function(object){
+    object.duration = turnHoursToMinutes(object.duration);
+    return object;
+  })
+
 
 // Get the average of all rates with 2 decimals 
 
