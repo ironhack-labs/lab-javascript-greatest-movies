992a389a0c4f6185b4e3f974bc40c2d8beeaca2f
wildhamster26 <38760849+wildhamster26@users.noreply.github.com>
2018-10-13 22:32:24 +0200

    BER-Ziggy

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -22,54 +22,187 @@
 function turnHoursToMinutes (array){
     return array.map(function(i){
         var newArrayItem = Object.assign({}, i); 
-        console.log("this is the newArrayItem in the beginning of the function:" , newArrayItem);
         if(i.duration.includes("m") && i.duration.includes("h")){
-          console.log("1st condition");
           newArrayItem.duration = parseInt(i.duration,10)*60+parseInt(i.duration.slice(3,6),10);
         } else if (i.duration.includes("h")) {
-          console.log("2nd condition",typeof newArrayItem.duration)
           newArrayItem.duration = parseInt(i.duration,10)*60
         } else {
-          console.log("3rd condition");
            newArrayItem.duration = parseInt(i.duration,10)
         }
-        console.log(typeof newArrayItem.duration)
-        console.log("this is the newArrayItem after the logic:" , newArrayItem);
         return newArrayItem;
       });
 }
 
     
 // Get the average of all rates with 2 decimals 
-// function round(value, decimals) {
-//     return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
-//   }
-  function createAvg(array) {
-    var result = array.map(function(a) {
-      return +a.rate;});
-      return result
-  }
-  console.log(createAvg(movies))
-  
-  function ratesAverage(array) {
-    return parseFloat(array.reduce(function(acc, n){
-      return (acc + n)
-    },0)/array.length)
-  }
-  console.log(ratesAverage(createAvg(movies)))
-  
-//   round(ratesAverage(createAvg(movies)),2)
+function createAvg(array) {
+    if(!!array === false){
+      return undefined;
+    }
+    return array.map(function(a) {
+        return Number(a.rate);});
+      
+  };
+
+function ratesAverage(array) {
+    if(array[0] === undefined){
+        return undefined;
+    }
+    return parseFloat((array.reduce(function(acc, n){
+        return (acc + Number(n));
+    }, 0)/array.length).toFixed(2));
+};
+
+// function ratesAverage(array) {
+//     if(array[0] === undefined){
+//       return undefined;
+//     }
+//     var num = (array.reduce(function(acc, n){
+//         return (acc + n);
+//     },0)/array.length).toFixed(2);
+//     return parseFloat(num);
+// };
 
 // Get the average of Drama Movies
-
+function dramaMoviesRate(array) {
+    return ratesAverage(createAvg(array.filter(function(item) {
+      if (!(item.genre.includes('Drama'))){
+          return undefined;
+        }
+        return item.genre.includes('Drama');
+      })));
+  };
+  
+  dramaMoviesRate(movies);
 
 // Order by time duration, in growing order
-
+function orderByDuration(array){
+    return array.sort(function(a, b){
+        if (a.duration === b.duration){
+            var titleA = a.title.toLowerCase(), titleB = b.title.toLowerCase();
+            if (titleA < titleB)
+                return -1 
+            if (titleA > titleB)
+                return 1
+        }
+        return a.duration-b.duration;
+    });
+  };
 
 // How many movies did STEVEN SPIELBERG
-
+function spielbergFilter(array){
+    return array.filter(function(item) {
+      return (item.genre.includes('Drama') && (item.director === 'Steven Spielberg')
+      )})
+  }
+  
+  function howManyMovies(array) {
+      if(array.length === 0){
+          return undefined;
+      }
+    var spielberg = spielbergFilter(array);
+    return "Steven Spielberg directed " + spielberg.length + " drama movies!";
+  };
+  
 
 // Order by title and print the first 20 titles
+function onlyTitles(array){
+    return array.map(function(item){
+        return item.title;
+    });
+};
+
+function orderAlphabetically(array){
+    var titleArray = onlyTitles(array);
+    titleArray.sort(function(a, b){
+        var titleA = a.toLowerCase(), titleB = b.toLowerCase();
+        if (titleA < titleB)
+            return -1
+        if (titleA > titleB)
+            return 1
+    });
+    return titleArray.slice(0,20);
+}
 
 
 // Best yearly rate average
+
+//Get an array containing only the years of each item - including duplicates
+function arrayOfAllYears(array){
+    return array.map(function(item){
+        return item.year;
+    });
+};
+
+//Sort the array of years from oldest to newest and remove duplicates
+function moviesByYear(array) {
+    var array = arrayOfAllYears(array);
+    return array.sort().filter(function(item, pos, arr) {
+        return !pos || item != arr[pos - 1];
+    })
+};
+
+// Get an array of all items from a single year by filtering each movie according to it's year
+function allItemsInASingleYear(moviesArray){
+    var yearArray = moviesByYear(moviesArray);
+    return yearArray.map(function(i){
+        return moviesArray.filter(function(item){
+            return item.year === i;
+        });
+    });
+}
+
+// Create a new array(of years) of arrays(with rates and year) that contains only the year and rate of each movie, sorted by year
+function getOnlyRatesByYear(array){
+    var array = allItemsInASingleYear(array);
+    return array.map(function(item){
+        return {
+                year: item[0].year,
+                rate: item.map(function(insideItem){
+                return insideItem.rate;
+            })
+        }
+    })
+}
+
+
+//Create an array where each item has the year itself and each year's average rate 
+function yearlyRatesAverage(array){
+    var array = getOnlyRatesByYear(array);
+    return array.map(function(item){
+        if (item.rate.length === 1) {
+            return {
+                year: item.year,
+                rate: Number(item.rate)
+            }
+        } else if(item.rate.length > 1) {
+            return {
+                year: item.year,
+                rate: parseFloat((item.rate.reduce(function(acc, n){
+                    return (acc + Number(n));
+                }, 0)/item.rate.length).toFixed(2))
+            }
+            
+        } else
+        console.log("something is wrong in the yearlyRatesAverage function");
+    })
+}
+// console.log(yearlyRatesAverage(movies));
+
+// return the final result - a string that sayd which year was best and what was it's rate
+function bestYearAvg(array){
+    var array = yearlyRatesAverage(array);
+    if(array[0] === undefined){
+        return undefined;
+    }
+    else {
+        var sortedByRate = array.sort(function(a,b){
+            return b.rate - a.rate;
+        })
+        var bestYear = sortedByRate[0];
+        return "The best year was " + bestYear.year + " with an average rate of "+ bestYear.rate;
+    }
+     
+};
+console.log(bestYearAvg(movies));
+
