136222e576e06174d6936d23d594f66a22ed0c47
Susanne Tünker <susanne.tuenker@hotmail.de>
2018-10-20 16:11:53 +0200

    first try

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -2,10 +2,71 @@
 // Turn duration of the movies from hours to minutes 
 
 
+function turnHoursToMinutes(moviesArr) {
+
+  return moviesArr.map(function(element) {
+
+    function stringToMinutes(string) {
+    var arr = string.split('h')
+    var hours = Number(arr[0])
+    var minutes = Number(arr[1].split("min")[0])
+    
+      return hours*60+minutes
+    } 
+  })
+}
+/* function stringToMinutes(string) {
+    var arr = string.split('h')    
+    if (arr.length < 2) {
+        arr.unshift("0")        
+    }    
+    var hours = Number(arr[0])
+    var minutes = Number(arr[1].split("min")[0])
+
+    return hours*60+minutes
+}
+
+*/ 
+
+
 // Get the average of all rates with 2 decimals 
 
+function ratesAverage (array) {
+
+var sum = array.reduce(function(accu, current) {
+
+return accu + parseFloat(current.rate)
+
+}, 0) 
+
+var count = array.length
+
+return sum / count;
+
+}
 
 // Get the average of Drama Movies
+
+
+function dramaMoviesRate (array) {
+
+var filter = array.filter(function(first){
+ return first.genre.includes("Drama")
+})
+
+var rating = filter.reduce(function(accu, current) {
+
+  return accu + parseFloat(current.rate)
+  
+  }, 0) 
+  
+var count = array.length
+  
+  return rating / count;
+  
+
+}
+
 
 
 // Order by time duration, in growing order
