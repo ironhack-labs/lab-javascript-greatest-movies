79f0c74427df5f9904ac4fb2c8f9a3cf0ca23e0f
annearbor <betting.anne@gmail.com>
2018-10-20 16:12:33 +0200

    done

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,20 +1,42 @@
 /* eslint no-restricted-globals: 'off' */
-// Turn duration of the movies from hours to minutes 
+// Turn duration of the movies from hours to minutes
 
+// function stringToMinutes(string) {
+//     var minutes = 0;
 
-// Get the average of all rates with 2 decimals 
+//     if (string.length == 2) {
+//       minutes += (Number((string[0])))*60;
+//     }
+//     else if (string.length == 5) {
+//       minutes += Number(string[0] + string[1]);
+//     }
+//     else {
+//       minutes += (Number((string[0])))*60;
+//       minutes += Number(string[3] + string[4]);
+//       }
 
+//       return minutes;
+
+// Get the average of all rates with 2 decimals
+function ratesAverage(arr) {
+  var sum = arr.reduce(function(acc, movie) {
+    return acc + Number(movie.rate);
+  }, 0);
+  var averageRating = sum / arr.length;
+  return Math.round(averageRating * 100) / 100;
+}
+console.log(ratesAverage(movies));
 
 // Get the average of Drama Movies
 
+//function dramaMovieRate(arr) {
+
+//}
 
 // Order by time duration, in growing order
 
-
 // How many movies did STEVEN SPIELBERG
 
-
 // Order by title and print the first 20 titles
-
 
 // Best yearly rate average
