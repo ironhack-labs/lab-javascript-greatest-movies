205a38919b297fc04f793ee05f716925691c8c55
nirtzur <nirtzur89@gmail.com>
2018-10-20 16:15:06 +0200

    I dont like it

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,9 +1,29 @@
 /* eslint no-restricted-globals: 'off' */
+console.log (movies[0]);
 // Turn duration of the movies from hours to minutes 
+function stringToMinutes(string) {
+    var arr = string.split('h')
+    var hours = Number(arr[0])
+    var minutes = Number(arr[1].split("min")[0])
+
+    return hours*60+minutes
+}
+ 
+function turnHoursToMinutes(moviesarr){
+    var newTime = moviesarr.map(function(movie){
+        return movie.duration= stringToMinutes;
+    });
+}
+
 
 
 // Get the average of all rates with 2 decimals 
-
+function ratesAverage (moviesarr){
+    var resultSum = moviesarr.reduce(function(sum, movie){
+        return sum += parseFloat(movie.rate);
+    },0)
+    return resultSum / movies.length;
+}
 
 // Get the average of Drama Movies
 
