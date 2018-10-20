4c6d796a608d17656928e8ce723c4c7d09057583
Juan Ignacio Benito <juanignaciobenito@Juans-MacBook-Air.local>
2018-10-20 16:14:43 +0200

    not finished

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,12 +1,45 @@
 /* eslint no-restricted-globals: 'off' */
-// Turn duration of the movies from hours to minutes 
+// Turn duration of the movies from hours to minutes
+function stringToMinutes(string) {
+    var arr = string.split('h')
+    var hours = Number(arr[0])
+    var minutes = Number(arr[1].split("min")[0])
+
+    return hours*60+minutes
+}
+function stringToMinutes(string) {
+    var arr = string.split('h')    
+    if (arr.length < 2) {
+        arr.unshift("0")        
+    }    
+    var hours = Number(arr[0])
+    var minutes = Number(arr[1].split("min")[0])
+
+    return hours*60+minutes
+}
 
 
-// Get the average of all rates with 2 decimals 
 
+function ratesAverage(array){    
+        
+    var sumRates = array.reduce(function(acc, movie) {
+        return acc + parseFloat(movie.rate)
+    }, 0);
+
+    var count = array.length
+
+    return sumRates / count
+}
 
 // Get the average of Drama Movies
 
+function dramaMoviesRate(moviesArr){
+    var drama = moviesArr.filter(function(movie){
+        return movie.genre === "drama";
+        
+
+    })
+}
 
 // Order by time duration, in growing order
 
@@ -18,3 +51,6 @@
 
 
 // Best yearly rate average
+
+console.log(ratesAverage(movies))
+console.log(dramaMoviesRate(movies))
