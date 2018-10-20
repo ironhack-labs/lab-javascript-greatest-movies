2403e8929fd7922d6ca6f124da4bb07cee829d38
Nike Michel-Soth <nikemichelsoth@gmail.com>
2018-10-20 16:12:07 +0200

    not done yet

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,11 +1,38 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
+console.log(movies[0])
 
+    function stringToMinutes(string) {
+        var minutes = 0;
+          
+        if (string.length == 2) {
+          minutes += (Number((string[0])))*60;
+        }
+        else if (string.length == 5) {
+          minutes += Number(string[0] + string[1]);
+        }
+        else {
+          minutes += (Number((string[0])))*60;
+          minutes += Number(string[3] + string[4]);
+          }
+    
+          return minutes;
+    }
 
 // Get the average of all rates with 2 decimals 
 
 
+function ratesAverage(arr) {
+ 
+   var sum = arr.reduce(function(acc, movie) {
+        return acc + Number(movie.rate);
+    }, 0)
+    var averageRating = sum / arr.length;
+        return Math.round(averageRating * 100) / 100;
+}
+
 // Get the average of Drama Movies
+
 
 
 // Order by time duration, in growing order
