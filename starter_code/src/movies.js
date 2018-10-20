ff37cc6a537eb6b7e376fa9d2c0d6a465b906214
Rebeca Cabañas Arauz <beckyarauz@users.noreply.github.com>
2018-10-20 16:09:31 +0200

    done

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,6 +1,29 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
-
+function turnHoursToMinutes(arr){
+    var num,numbers,hours,mins,hourToMinutes,totalMins,newArr;
+  
+    num = /\d+/g;
+  
+    newArr = [];
+  
+    for(let i = 0; i < arr.length; i++){
+      newArr.push(Object.assign({},arr[i]));
+    }
+  
+    newArr.map(movie => {
+      numbers = movie.duration.match(num).map(Number);
+   // numbers = [2,22]
+      hours = numbers[0];
+      mins = numbers[1];
+      hourToMinutes = hours * 60;
+  
+      totalMins = hourToMinutes + mins;
+  
+      movie.duration = totalMins;
+    })
+    return newArr;
+  }
 
 // Get the average of all rates with 2 decimals 
 
