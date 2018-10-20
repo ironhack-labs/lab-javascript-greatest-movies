568c731397fba2b958873fe0cf58d3d26335e5bb
Yelizaveta Crofts <yscrofts@hotmail.com>
2018-10-20 16:13:02 +0200

    not done

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,11 +1,39 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
+/*function turnHoursToMinutes(movies){
+  return movies.duration.slice("");
+});*/
+
+function stringToMinutes(string) {
+  var arr = string.split('h')
+  var hours = Number(arr[0])
+  var minutes = Number(arr[1].split("min")[0])
+
+  return hours*60+minutes
+}
+
+function turnHoursToMinutes(array) {
+  var result = array.map(function(element) {
+    return element.stringToMinutes();
+  });
+  return result;
+}
 
 
 // Get the average of all rates with 2 decimals 
+var total = movies.reduce(function(sum, elem){
+  return sum + parseFloat(elem.rate);
+}, 0);
+
+var average = (total / movies.length).toFixed(2);
+console.log(average);
 
 
 // Get the average of Drama Movies
+var dramaMovies = movies.filter(function(elem){
+  return elem.genre["Drama"];
+});
+return dramaMovies;
 
 
 // Order by time duration, in growing order
