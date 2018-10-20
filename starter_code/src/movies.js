4ee16befc26cd3ce5bd7ab04e80044c0e9b7d2f7
beckyxde <beckyxde@users.noreply.github.com>
2018-10-20 16:12:17 +0200

    done

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,8 +1,29 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
+console.log(movies[0])
 
+  
+  function stringToMinutes(string){
+  var arr = string.split('h')
+  var hours = Number(arr[0])
+  var minutes = Number(arr[1].split("min")[0])
 
+  return hours*60+minutes
+}
+function turnHoursToMinutes(moviesArr) {
+  var answer = moviesArr.map(function(movie) {
+    movie[duration] = stringToMinutes(movie.duration)
+  })
+  return answer
+}
 // Get the average of all rates with 2 decimals 
+
+function ratesAverage (moviesarr){
+var resultSum = moviesarr.reduce(function(sum, movie){
+  return sum + parseFloat(movie.rate);
+} ,0)
+}
+return parseFloat(resultSum/movie.length)
 
 
 // Get the average of Drama Movies
