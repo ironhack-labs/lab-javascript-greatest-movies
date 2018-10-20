92e8ce0c2972bc3daa30699ba3747ef30cfa21bb
Rebeca Cabañas Arauz <beckyarauz@users.noreply.github.com>
2018-10-20 21:41:02 +0200

    almost done

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -17,7 +17,6 @@ function turnHoursToMinutes(arr) {
             if (timeArr[0].includes('h') && timeArr[1].includes('m')) {
                 hours = Number(timeArr[0].split("").filter(Number).join(''));
                 mins = Number(timeArr[1].split("").filter(Number).join(''));
-                console.log(hours, mins);
                 hourToMinutes = hours * 60;
                 totalMins = hourToMinutes + mins;
             }
@@ -28,8 +27,6 @@ function turnHoursToMinutes(arr) {
             hours = Number(timeArr[0].split("").filter(Number).join(''));
             totalMins = hours * 60;
         }
-
-        console.log(totalMins);
 
         movie.duration = totalMins;
     })
@@ -47,25 +44,68 @@ function ratesAverage(arr) {
 }
 
 // Get the average of Drama Movies
-function dramaMoviesRate(arr){
+function dramaMoviesRate(arr) {
     var dramaMovies = arr.filter(movie => movie.genre.includes('Drama'));
-    if(dramaMovies.length > 0){
+    if (dramaMovies.length > 0) {
         var dramaMovieRates = dramaMovies.map(movie => Number(movie.rate));
-   
-    var sumRates = dramaMovieRates.reduce((sum,rate) => sum + rate,0);
-  
-    var avgRates = sumRates / dramaMovies.length;
-    return Number(avgRates.toFixed(2));
+
+        var sumRates = dramaMovieRates.reduce((sum, rate) => sum + rate, 0);
+
+        var avgRates = sumRates / dramaMovies.length;
+        return Number(avgRates.toFixed(2));
     }
-  }
+}
 
 // Order by time duration, in growing order
+function orderByDuration(arr){
+    var newMovies = [];
+  
+    for(i in arr){
+      newMovies.push(Object.assign({}, arr[i]));
+    }
 
+    var sortedMoviesArr = newMovies.sort(function(a, b) {
+        if(a.duration === b.duration){ 
+            if(a.title < b.title) {return -1};
+            if(a.title > b.title) {return 1};
+            return 0;
+        } 
+        return a.duration - b.duration;
+    });
+  
+    return sortedMoviesArr;
+  }
 
 // How many movies did STEVEN SPIELBERG
-
+function howManyMovies(arr){
+    if(arr.length > 0){
+        var spielbergDramaMovies = arr
+            .filter(movies => movies.director === 'Steven Spielberg' && movies.genre.includes('Drama'));
+        if(spielbergDramaMovies.length > 0){
+            return "Steven Spielberg directed " + spielbergDramaMovies.length + " drama movies!";
+        }
+        return 'Steven Spielberg directed 0 drama movies!';
+    }
+  };
 
 // Order by title and print the first 20 titles
+function orderAlphabetically(arr){
+    var newMovies = [];
+  
+    for(i in arr){
+      newMovies.push(Object.assign({}, arr[i]));
+    }
 
+    var sortedMoviesArr = newMovies
+
+    .sort(function(a, b) {
+        if(a.title < b.title) {return -1};
+        if(a.title > b.title) {return 1};
+        return 0;
+    })
+    .map(movie => movie.title);
+    
+    return sortedMoviesArr.slice(0,20);
+  }
 
 // Best yearly rate average
