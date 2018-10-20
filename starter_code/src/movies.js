7dd1f15d97b31a2b74897fe73d3ff0fed9af4b30
Rebeca Cabañas Arauz <beckyarauz@users.noreply.github.com>
2018-10-20 22:54:37 +0200

    done

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -57,55 +57,103 @@ function dramaMoviesRate(arr) {
 }
 
 // Order by time duration, in growing order
-function orderByDuration(arr){
+function orderByDuration(arr) {
     var newMovies = [];
-  
-    for(i in arr){
-      newMovies.push(Object.assign({}, arr[i]));
+
+    for (i in arr) {
+        newMovies.push(Object.assign({}, arr[i]));
     }
 
-    var sortedMoviesArr = newMovies.sort(function(a, b) {
-        if(a.duration === b.duration){ 
-            if(a.title < b.title) {return -1};
-            if(a.title > b.title) {return 1};
+    var sortedMoviesArr = newMovies.sort(function (a, b) {
+        if (a.duration === b.duration) {
+            if (a.title < b.title) { return -1 };
+            if (a.title > b.title) { return 1 };
             return 0;
-        } 
+        }
         return a.duration - b.duration;
     });
-  
+
     return sortedMoviesArr;
-  }
+}
 
 // How many movies did STEVEN SPIELBERG
-function howManyMovies(arr){
-    if(arr.length > 0){
+function howManyMovies(arr) {
+    if (arr.length > 0) {
         var spielbergDramaMovies = arr
             .filter(movies => movies.director === 'Steven Spielberg' && movies.genre.includes('Drama'));
-        if(spielbergDramaMovies.length > 0){
+        if (spielbergDramaMovies.length > 0) {
             return "Steven Spielberg directed " + spielbergDramaMovies.length + " drama movies!";
         }
         return 'Steven Spielberg directed 0 drama movies!';
     }
-  };
+};
 
 // Order by title and print the first 20 titles
-function orderAlphabetically(arr){
+function orderAlphabetically(arr) {
     var newMovies = [];
-  
-    for(i in arr){
-      newMovies.push(Object.assign({}, arr[i]));
+
+    for (i in arr) {
+        newMovies.push(Object.assign({}, arr[i]));
     }
 
     var sortedMoviesArr = newMovies
 
-    .sort(function(a, b) {
-        if(a.title < b.title) {return -1};
-        if(a.title > b.title) {return 1};
-        return 0;
-    })
-    .map(movie => movie.title);
-    
-    return sortedMoviesArr.slice(0,20);
-  }
+        .sort(function (a, b) {
+            if (a.title < b.title) { return -1 };
+            if (a.title > b.title) { return 1 };
+            return 0;
+        })
+        .map(movie => movie.title);
+
+    return sortedMoviesArr.slice(0, 20);
+}
 
 // Best yearly rate average
+
+function bestYearAvg(arr) {
+    if (arr.length > 0) {
+        var yearArr = arr.map(movies => movies.year);
+        //console.log(yearArr);
+        var uniqueYears = [...new Set(yearArr)];
+        //console.log(uniqueYears);
+        var sortedYears = uniqueYears.sort((a, b) => a - b);
+        //console.log(sortedYears);
+        var years = {};
+
+        for (let i = 0; i < sortedYears.length; i++) {
+            years[sortedYears[i]] = {};
+        }
+
+        for (key in years) {
+            for (let i = 0; i < arr.length; i++) {
+                years[key] = arr
+                    .filter(movie => movie.year == key)
+            }
+        }
+        //console.log(years);
+
+        var yearsRateSums = {};
+        var yearRateAvg = [];
+
+        for (key in years) {
+            yearsRateSums[key] = arr
+                .filter(movie => movie.year == key)
+                .map(movie => movie.rate)
+                .reduce((sum, rate) => sum + Number(rate), 0);
+        }
+
+        for (key in years) {
+            yearRateAvg.push(yearsRateSums[key] / years[key].length);
+        }
+
+        //console.log(yearRateAvg);
+
+        var maxRateIndex = yearRateAvg.indexOf(Math.max(...yearRateAvg));
+        //console.log(maxRateIndex);
+
+        var yearProps = Object.keys(years);
+        //console.log(yearProps);
+
+        return 'The best year was ' + yearProps[maxRateIndex] + ' with an average rate of ' + yearRateAvg[maxRateIndex];
+    }
+}
