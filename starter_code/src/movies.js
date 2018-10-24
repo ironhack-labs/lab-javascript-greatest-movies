7461b950b92d08bcf978d15e48421fc9532df45b
filippo fresilli <filippofresilli@MacBook-Pro-di-filippo-3.local>
2018-10-24 13:10:47 +0200

    not finished

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -4,10 +4,44 @@
 
 // Get the average of all rates with 2 decimals 
 
+var ratesSum = 
+movies.reduce(function (accumulator, currentValue ){
+    return parseFloat(currentValue.rate) + accumulator;
+},0);
+
+function ratesAverage(moviesArray) {
+
+    var SumRates = moviesArray.reduce(function (accumulator, currentValue ){
+    return parseFloat(currentValue.rate) + accumulator;
+},0);
+    var indexOfArray = moviesArray.length();
+
+    return SumRates / indexOfArray;
+}; 
+
 
 // Get the average of Drama Movies
 
-
+function dramaMoviesRate (dramaMoviesArray) {
+  
+    var dramaValue = dramaMoviesArray.filter(function (oneItem) {
+      if (oneItem.genre[1] === 'Drama'){
+      return oneItem; }
+    });
+  
+  
+    var SumRates = dramaValue.reduce(function (accumulator, currentValue){
+      return parseFloat(dramaValue.rate) + accumulator;
+    },0);
+      
+    
+  
+    var indexOfArray = dramaValue.length;
+      
+    return SumRates / indexOfArray;
+  }
+  
+  console.log(dramaMoviesRate(movies));
 // Order by time duration, in growing order
 
 
