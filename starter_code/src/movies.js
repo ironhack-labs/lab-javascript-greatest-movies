3c2ee4d8f449433aa3f06ce3cf6297fcb1223055
Jaime Jacobo <jaimejacobordv@gmail.com>
2018-10-25 19:27:47 -0400

    Im trying to find the way to use 'reduce'

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,6 +1,24 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
 
+function turnHoursToMinutes(movieArray){
+
+    return movieArray.map((movieObject)=>{
+  
+    let durationArray = movieObject.duration.split(' ');
+    let hours = durationArray[0][0]
+    let howManyDigitsMinutes =  durationArray[1].length - 3
+    let minutes = durationArray[1].substr(0, howManyDigitsMinutes)
+    let totalDuration =  Number(hours)*60 + Number(minutes)
+    let newMovie = movieObject;
+    newMovie.duration =  totalDuration.toString();
+  
+    return newMovie;
+  
+    })
+  }
+  
+  turnHoursToMinutes(movies)
 
 // Get the average of all rates with 2 decimals 
 
