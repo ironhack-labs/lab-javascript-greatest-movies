aecb81aa12e2e0a674596a16d64c27054e76927e
Xiaomi Notebook Pro <xiaomi@gmailcom>
2018-10-25 20:17:49 +0200

    primera iteracion terminada

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,6 +1,28 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
 
+function turnHoursToMinutes(arrayMovies) {
+
+    return  arrayMovies.map(movie => {
+
+        var hoursMinutes = (parseInt(movie.duration.split(' ')[0])) * 60;
+        var minutes = parseInt(movie.duration.split(' ')[1]);
+
+
+        if (hoursMinutes && minutes) {
+            return {...movie, duration: hoursMinutes + minutes};
+        } else if (!hoursMinutes && minutes) {
+            return {...movie, duration: minutes};
+        } else if (hoursMinutes && !minutes) {
+            return {...movie, duration: hoursMinutes};
+        }
+
+        //  var newDuration = (parseInt(movie.duration.split(' ')[0]) * 60) + parseInt(movie.duration.split(' ')[1]);
+        //  console.log(newDuration);
+        //  return {...movie, duration: newDuration};
+    })
+
+}
 
 // Get the average of all rates with 2 decimals 
 
