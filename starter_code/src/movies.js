c42accf0d23c06c20b7a241a4b79f928a2bb38ab
Xiaomi Notebook Pro <xiaomi@gmailcom>
2018-10-25 23:54:37 +0200

    cuatro iteraciones

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -5,21 +5,24 @@ function turnHoursToMinutes(arrayMovies) {
 
     return  arrayMovies.map(movie => {
 
-        var hoursMinutes = (parseInt(movie.duration.split(' ')[0])) * 60;
-        var minutes = parseInt(movie.duration.split(' ')[1]);
+        var hoursMinutes = 0;
+        var minutes = 0;
 
-
-        if (hoursMinutes && minutes) {
-            return {...movie, duration: hoursMinutes + minutes};
-        } else if (!hoursMinutes && minutes) {
-            return {...movie, duration: minutes};
-        } else if (hoursMinutes && !minutes) {
-            return {...movie, duration: hoursMinutes};
+        if (movie.duration.indexOf('h') === -1) {
+            minutes = parseInt(movie.duration.split(' ')[0]);
+        } else {
+            hoursMinutes = (parseInt(movie.duration.split(' ')[0])) * 60;
+            minutes = parseInt(movie.duration.split(' ')[1]);
         }
 
-        //  var newDuration = (parseInt(movie.duration.split(' ')[0]) * 60) + parseInt(movie.duration.split(' ')[1]);
-        //  console.log(newDuration);
-        //  return {...movie, duration: newDuration};
+        if (!hoursMinutes) {
+            return {...movie, duration: minutes};
+        } else if (!minutes) {
+            return {...movie, duration: hoursMinutes};
+        } else {
+            return {...movie, duration: hoursMinutes + minutes};
+        }
+
     })
 
 }
@@ -27,10 +30,72 @@ function turnHoursToMinutes(arrayMovies) {
 // Get the average of all rates with 2 decimals 
 
 
+function ratesAverage(arrayMovies) {
+
+     var count = 0;
+     var newArray = arrayMovies.reduce(function(accumulator, movie){
+        count ++;
+        return (accumulator + +movie.rate);
+      }, 0);
+    
+    return (newArray/count);
+
+
+ 
+
+
+}
+
+
 // Get the average of Drama Movies
+
+function dramaMoviesRate(arrayMovies) {
+
+    var dramaMovies = []
+
+    var newArray = arrayMovies.filter(movie => {
+      movie.genre.filter(genero => {
+           if (genero === 'Drama') {
+               dramaMovies.push(movie)
+           }
+         })
+    })
+
+    var result = ratesAverage(dramaMovies);
+
+    if (!result) {
+        return undefined;
+    } else {
+        return parseFloat(result.toFixed(2));
+    }
+
+    
+    
+}
 
 
 // Order by time duration, in growing order
+
+
+function orderByDuration(arrayMovies) {
+
+    var newArray = turnHoursToMinutes(arrayMovies).sort(function(a,b) {
+
+        if (a.duration > b.duration){
+            return -1;
+        }
+    
+        if (a.duration > b.duration){
+            return 1;
+        }
+    
+        return 0;
+    });
+
+    return newArray;
+
+
+}
 
 
 // How many movies did STEVEN SPIELBERG
