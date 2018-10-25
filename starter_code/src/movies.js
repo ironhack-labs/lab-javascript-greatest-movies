b8abd53fc801b4c00856c73caa939e8680d3df47
RuddyLotar <ruddylotar@gmail.com>
2018-10-26 01:07:27 +0200

    Ejercicio realizado

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,8 +1,45 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
 
+function turnHoursToMinutes(films) {
+
+  return films.map(function (movie) {
+
+    var hours = 0;
+    var minutes = 0;
+
+    if (movie.duration.indexOf("h") !== - 1) {
+
+      hours = parseInt(movie.duration[0], 10) * 60;
+
+    }
+    if (movie.duration.indexOf("min") !== -1) {
+
+      minutes = parseInt(movie.duration.substr(movie.duration.length - 5, movie.duration.length - 3), 10);
+
+    }
+    return Object.assign({}, movie, { duration: hours + minutes });
+
+  });
+}
 
 // Get the average of all rates with 2 decimals 
+
+function ratesAverage(films) {
+
+  var ratingArray = films.map(function (movies) {
+    return movies.rate;
+  });
+
+  var avg = ratingArray.reduce(function (ac, cu) {
+    return ac + cu;
+  });
+
+  avg = (avg / ratingArray.length).toFixed(2);  
+  avg = parseFloat(avg);
+  return avg;
+}
+
 
 
 // Get the average of Drama Movies
