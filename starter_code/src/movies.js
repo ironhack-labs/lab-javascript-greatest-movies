4a45fda49167e13638fc653c6f24b51c043176e3
jrecio <recio.juan@gmail.com>
2018-10-25 21:09:58 +0200

    First function finished

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -2,6 +2,31 @@
 // Turn duration of the movies from hours to minutes 
 
 
+function hoursToMinutes(hoursMinutes){
+  var totalMins=0;
+  hIndex=hoursMinutes.indexOf("h");
+  if (hIndex!=-1){
+    totalMins+= 60*parseInt(hoursMinutes.substring(0,hIndex));
+    console.log (hoursMinutes.substring(hIndex));
+    hoursMinutes=hoursMinutes.substring(hIndex).replace("h","").replace(" ","")
+    console.log (hoursMinutes);
+  }
+  minIndex=hoursMinutes.indexOf("min");
+  if (minIndex!=-1){
+    totalMins+= parseInt(hoursMinutes.substring(0,minIndex));
+  }
+  return totalMins;
+  }
+
+
+function turnHoursToMinutes(movies){
+  return movies.map(function(movie){
+    var newMovie={...movie,duration:hoursToMinutes(movie.duration)};
+    return newMovie;
+  });
+
+}
+
 // Get the average of all rates with 2 decimals 
 
 
