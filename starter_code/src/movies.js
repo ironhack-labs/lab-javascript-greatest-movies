a015abf577c682b148e9f5cfafe67077317f52f4
Sonia Ruiz <yune.vk@gmail.com>
2018-10-25 21:35:12 +0200

    Iteration 2

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -2,7 +2,7 @@
 // Turn duration of the movies from hours to minutes 
 function turnHoursToMinutes(movies) {
   var arr = movies.map(function(movie) {
-    var minutes = movie.duration.split(' ').map(function(arrTime) {
+    var minutes = movie.duration.split(' ').map(function(arrTime) { 
 
       var time = parseInt(arrTime);
 
@@ -11,16 +11,24 @@ function turnHoursToMinutes(movies) {
       } else {
         return time;
       }
-    }).reduce(function(accumulator, current) {
-      return accumulator + current;
-    });
+    }).reduce(function(acumulator, current) {
+      return acumulator + current;
+    }, 0);
     return Object.assign({}, movie, {duration: minutes});
   });
   return arr;
 }
 
 // Get the average of all rates with 2 decimals 
-
+function ratesAverage(movies) {
+  var moviesCopy = Object.assign([], movies);
+  
+  var average = moviesCopy.reduce(function(acumulator, current) {
+    return acumulator + parseFloat(current.rate);
+  }, 0) / moviesCopy.length;
+  
+  return Math.round(average * 100) / 100;
+}
 
 // Get the average of Drama Movies
 
