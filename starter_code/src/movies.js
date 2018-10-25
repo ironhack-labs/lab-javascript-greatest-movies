35530f074f9a02cb367bf0a770e9d8be7302e2d8
Simon G Negrete <simongnegrete@MacBook-Pro-de-Simon.local>
2018-10-26 00:37:08 +0200

    [MAD DE] Simon gonzalez

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,20 +1,66 @@
 /* eslint no-restricted-globals: 'off' */
-// Turn duration of the movies from hours to minutes 
+// Turn duration of the movies from hours to minutes
 
+function turnHoursToMinutes(moviesArray) {
+	return moviesArray.map(function(elem) {
+		var hours = 0;
+		var minutes = 0;
+		if (elem.duration.indexOf('h') !== -1) {
+			hours = parseInt(elem.duration[0], 10) * 60;
+		}
+		if (elem.duration.indexOf('min') !== -1) {
+			minutes = parseInt(elem.duration.substring(elem.duration.length - 5, elem.duration.length - 3), 10);
+		}
+		return Object.assign({}, elem, { duration: hours + minutes });
+	});
+}
+turnHoursToMinutes(movies);
 
-// Get the average of all rates with 2 decimals 
+// Get the average of all rates with 2 decimals
+function ratesAverage(moviesArray) {
+	var rateReducer = function(acc, elem) {
+		var toDigit = parseFloat(elem.rate);
 
+		return (acc += toDigit);
+	};
+
+	var ratingVariable = moviesArray.reduce(rateReducer, 0);
+
+	ratingVariable /= moviesArray.length;
+	ratingVariable = ratingVariable.toFixed(2);
+	ratingVariable = parseFloat(ratingVariable);
+
+	return ratingVariable;
+}
+ratesAverage(movies);
 
 // Get the average of Drama Movies
+function dramaMoviesRate(moviesArray) {
+	var filterDrama = moviesArray.filter(function(movie) {
+		return movie.genre.includes('Drama') == true;
+	});
+	var rateReducer = function(acc, elem) {
+		var toDigit = parseFloat(elem.rate);
 
+		return (acc += toDigit);
+	};
 
-// Order by time duration, in growing order
+	var ratingVariable = filterDrama.reduce(rateReducer, 0);
+
+	ratingVariable /= filterDrama.length;
+	ratingVariable = ratingVariable.toFixed(2);
+	ratingVariable = parseFloat(ratingVariable);
+
+	return ratingVariable;
+}
+
+dramaMoviesRate(movies);
+
+// Order by time duration, in growing order 
 
 
 // How many movies did STEVEN SPIELBERG
 
-
 // Order by title and print the first 20 titles
-
 
 // Best yearly rate average
