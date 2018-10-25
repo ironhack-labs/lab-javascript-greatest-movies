e23578f88dd9561bb6c6106249624fe94bbb5914
unknown <alejo.daudi@gmail.com>
2018-10-25 15:45:41 +0200

    Exwercices 2,5,6

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -3,7 +3,14 @@
 
 
 // Get the average of all rates with 2 decimals 
+function ratesAverage(array){
+    array.reduce(function(accum,currentValue){
+        var rate = 0;
+        accum.rate += currentValue;
 
+    },0);
+}
+    
 
 // Get the average of Drama Movies
 
@@ -12,9 +19,23 @@
 
 
 // How many movies did STEVEN SPIELBERG
-
+function howManyMovies(array) {
+    if (!array.length) {
+        return undefined;
+    } else {
+        var result = array.filter(function(movie) {
+            return movie.director === 'Steven Spielberg' && movie.genre.indexOf('Drama') > -1 ;
+        });
+        if (!result.length) {
+            return 'Steven Spielberg directed 0 drama movies!';
+        };
+        return 'Steven Spielberg directed ' + result.length + ' drama movies!';
+    };
+};
 
 // Order by title and print the first 20 titles
-
+function orderAlphabetically(array) {
+    title.sort()
+}
 
 // Best yearly rate average
