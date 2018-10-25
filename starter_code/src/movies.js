c55e4ec9f477839e8b20521002b2b3e2588aa03f
Jorge Devesa <jrgdevesa@gmail.com>
2018-10-26 00:23:09 +0200

    ninguno completado con éxito

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,12 +1,38 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
-
-
+var newArray=0;
+function turnHoursToMinutes(array){
+    array.map(function(movie){
+        return movie.duration})
+    // imovie.duration.splice(1)
+}
+    
 // Get the average of all rates with 2 decimals 
-
+//var p = 0; //
+function ratesAverage(array){  
+return parseInt(array.reduce(function(acc, elem){ //Intento igualar el parse int a p, en lugar de dar return
+return acc + elem.rate
+},0))/array.length
+//parseFloat(p.toFixed)
+}
 
 // Get the average of Drama Movies
-
+var dramaMovies = 0;
+var otherMovies = 0;
+var dramaGenre="";
+drama
+function dramaMoviesRate(array){
+   return array.map(function(movie){
+       if (array.genre==="Drama"){
+           array.push(array.genre)
+           dramaMovies++;
+       }
+       else{
+           dramaMovie++;
+           return dramaMovies
+       }
+         }) //     resultado/dramaMovies
+    
 
 // Order by time duration, in growing order
 
