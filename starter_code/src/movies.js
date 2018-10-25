439b31faa229bd56260c63e6a8d408c0e62f0df5
Sonia Ruiz <yune.vk@gmail.com>
2018-10-25 20:17:12 +0200

    Iteration 1

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,6 +1,23 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
+function turnHoursToMinutes(movies) {
+  var arr = movies.map(function(movie) {
+    var minutes = movie.duration.split(' ').map(function(arrTime) {
 
+      var time = parseInt(arrTime);
+
+      if (arrTime.endsWith('h')) {
+        return time * 60;
+      } else {
+        return time;
+      }
+    }).reduce(function(accumulator, current) {
+      return accumulator + current;
+    });
+    return Object.assign({}, movie, {duration: minutes});
+  });
+  return arr;
+}
 
 // Get the average of all rates with 2 decimals 
 
