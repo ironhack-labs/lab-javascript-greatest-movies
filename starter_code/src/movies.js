3035aa5932f8af8eabac19f8ae1762c9acc43bdd
Javier Samper <jsamperarias@hotmail.com>
2018-10-26 09:34:23 +0200

    [MAD DE] Javier Samper

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -3,7 +3,15 @@
 
 
 // Get the average of all rates with 2 decimals 
+function ratesAverage(movies) {
 
+    var promedio = movies.reduce(function(valor, movie) {
+        return valor + parseFloat(movie.rate);
+    }, 0);
+
+    let sumaPromedio = (promedio / movies.length);
+    return sumaPromedio
+}
 
 // Get the average of Drama Movies
 
