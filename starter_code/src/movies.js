a061d2782a7a5256a34d9858c41ef123fa0bb860
adrigt00 <adriangomez@MacBook-Pro-de-Adrian.local>
2018-10-26 09:23:04 +0200

    Hecho

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,11 +1,29 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
-
-
+function turnHoursToMinutes(arr) {
+  return minuteArr = arr.map(function(elem) {
+    var newDuration = ""
+    if (elem.duration.indexOf("h") == -1) {
+      newDuration = parseInt(elem.duration);
+    } else if (elem.duration.indexOf("m") == -1) {
+      newDuration = parseInt(elem.duration) * 60;
+    } else {
+      newDuration = parseInt(elem.duration.split(" ")[0]) * 60 + parseInt(elem.duration.split(" ")[1]);
+    }
+  return Object.assign({}, elem, {duration: newDuration})
+})
+}
 // Get the average of all rates with 2 decimals 
-
+function ratesAverage(arr) {
+  var rateSum = arr.reduce(function(acumulator, current){
+    return acumulator + parseInt(current.rate);
+}, 0);
+var average = (rateSum / arr.length);
+return average;
+}
 
 // Get the average of Drama Movies
+
 
 
 // Order by time duration, in growing order
