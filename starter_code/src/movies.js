6f6e7ffbdebec99850c1a20689fdc0d80604c6a5
Rodrigo.Munoz <rodrigomunozga@gmail.com>
2018-10-26 06:55:52 +0200

    first commit

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,6 +1,41 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
 
+const turnHoursToMinutes = (arr) => {
+
+     arr.map(function(movie){
+
+      var newDuration = 0;
+
+      if (movie.duration.include(h) && movie.duration.includes("m")){
+
+        newDuration += parseInt.movie(movie.duration * 60);
+
+
+      }else if (movie.duration.include(h) && movie.duration.indexof(m) == -1 ){
+
+        newDuration += parseInt(movie.duration * 60);
+
+      }else (movie.duration.indexof(h) == -1 && movie.duration.include(m)){
+        newDuration += parseInt(movie.duration);
+      }
+
+    });
+
+    console.log('newDuration');
+
+    // Object.assign({},e,{duration: new timeee})
+
+
+
+
+}
+
+
+
+
+
+
 
 // Get the average of all rates with 2 decimals 
 
