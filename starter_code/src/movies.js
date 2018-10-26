79b9e0e534d82c76e53d4580768ada7008e7ca5e
Ross Gravitte <Rbgravitte@gmail.com>
2018-10-25 23:36:05 -0400

    completed function that pushes movie.duration array. not sure how to check for array containing min or h

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,6 +1,21 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
+//reduce method?
+function turnHoursToMinutes(obj){
 
+  obj.map((obj)=>{
+    var durationArray = obj.duration.split(' ');
+    var hours= durationArray[0][0];
+    hoursToMinutes = obj.duration.substr(0, 1)*60;
+    let howManyDigitsMinutes =  durationArray[1].length - 3
+    let minutes = durationArray[1].substr(0,   howManyDigitsMinutes)
+    let answer = Number(hoursToMinutes) + Number(minutes);
+    let newMovie = obj;
+    newMovie.duration = answer.toString();
+    console.log(newMovie);
+  })
+  
+}
 
 // Get the average of all rates with 2 decimals 
 
