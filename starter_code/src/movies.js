fd50f73da17aecac8770aa2ea20385f5e8fd7972
jrecio <recio.juan@gmail.com>
2018-10-26 08:18:31 +0200

    Done without the bonus

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -4,12 +4,11 @@
 
 function hoursToMinutes(hoursMinutes){
   var totalMins=0;
-  hIndex=hoursMinutes.indexOf("h");
+  var hIndex=hoursMinutes.indexOf("h");
   if (hIndex!=-1){
     totalMins+= 60*parseInt(hoursMinutes.substring(0,hIndex));
     console.log (hoursMinutes.substring(hIndex));
     hoursMinutes=hoursMinutes.substring(hIndex).replace("h","").replace(" ","")
-    console.log (hoursMinutes);
   }
   minIndex=hoursMinutes.indexOf("min");
   if (minIndex!=-1){
@@ -28,18 +27,51 @@ function turnHoursToMinutes(movies){
 }
 
 // Get the average of all rates with 2 decimals 
-
-
+function ratesAverage(movies){
+  if (movies.length==0){
+    return undefined;
+  }
+  return Math.round((movies.reduce(function(acc,movie){
+     return acc + parseInt(movie.rate==''?0:movie.rate);
+     },0)/movies.length)*100)/100;
+   
+ }
 // Get the average of Drama Movies
 
+function dramaMoviesRate(movies){
 
+  return ratesAverage(movies.filter(function(movie){
+    return movie.genre.indexOf("Drama")!=-1;
+  }));
+}
 // Order by time duration, in growing order
-
-
+function orderByDuration(movies){
+  // var newMovies= {...movies};
+  movies.sort(function (a,b){
+    var order=a.duration>b.duration ;
+    return (order==0?(a.title>b.title):order);
+  })
+  return movies;
+}
 // How many movies did STEVEN SPIELBERG
-
+function howManyMovies(movies){
+  if (movies.length==0) return undefined;
+  var moviesCount=movies.filter(function(movie){
+    return movie.genre.indexOf("Drama")!=-1 && movie.director=="Steven Spielberg";
+  }).length;
+  return `Steven Spielberg directed ${moviesCount} drama movies!`;
+}
 
 // Order by title and print the first 20 titles
-
+function orderAlphabetically(movies){
+  var sortedTitles=[];
+  movies.sort(function (a,b){
+    return ((a.title>b.title)?1:-1);
+  });
+  for (i=0;i<20 && i<movies.length;i++){
+      sortedTitles.push(movies[i].title);
+  }
+  return sortedTitles;
+}
 
 // Best yearly rate average
