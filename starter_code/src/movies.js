503b6bd91d0e0cf39690bcff9b072f04ac6dda1f
cgandiaf <c.gandia.f@gmail.com>
2018-10-26 08:44:02 +0200

    moviesRanking

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,15 +1,40 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
+function turnHoursToMinutes(Film) {
+    
+    var newArray = Object.assign({},Film);
+    return Films({...newArrayFilms, duration: strintToInt(Film.duration)});
+}
+
+function stringToInt(stringData) {
+    intRaw = stringData.slice(0,-3).split("h "); 
+
+    return parseInt(intRaw[0])*60 + parseInt(intRaw[1]);
+}
 
 
 // Get the average of all rates with 2 decimals 
 
 
-// Get the average of Drama Movies
 
+// Get the average of Drama Movies
+function dramaMoviesRate(arrayFilms) {
+    var dramaFilm = 0;
+    arrayFilms.forEach(function(film){
+      film.genre.forEach(function(genresKinds){
+        if(genresKinds=="Drama") dramaFilm++;
+      });
+        
+    });
+    return dramaFilm/arrayFilms.length;
+}
 
 // Order by time duration, in growing order
-
+function orderByDuration(arrayFilms) {
+  arrayFilms.sort(function(a,b){
+      return b.duration-a.duration;
+  });  
+}
 
 // How many movies did STEVEN SPIELBERG
 
