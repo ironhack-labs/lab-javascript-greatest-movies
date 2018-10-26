d6274218d6f05e7a32c61a301de3a6810b318a25
Eduardo Gandía Fernández <eduardogandiafernandez@MacBook-Pro-de-Eduardo.local>
2018-10-26 08:44:26 +0200

    1st iteration

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,9 +1,26 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
+function strigToMinutes (durationHour) {
+   var arrHourMin = durationHour.slice(0,-3).split("h ");
+   var arrHourMinNum = [parseInt(arrHourMin[0]),parseInt(arrHourMin[1])];
+   return arrHourMinNum[0]*60 + arrHourMinNum[1];
+}
 
-
+function turnHoursToMinutes (films) {
+  var newFilms = Object.assign({}, films)
+  newFilms.duration = strigToMinutes(newFilms.duration);
+  console.log(film);
+  return newFilms;
+}
 // Get the average of all rates with 2 decimals 
-
+function ratesAverage(filmsArr){
+  
+  var resultado = parseInt(filmsArr.reduce(function(acumulator, current)
+  {
+  console.log(acumulator, current);
+  return accumulator.rate + current.rate;
+  },0));
+}
 
 // Get the average of Drama Movies
 
