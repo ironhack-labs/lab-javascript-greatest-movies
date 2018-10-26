45b3f22ea4cd4b065989e10a28463909370048b3
Jaime Jacobo <jaimejacobordv@gmail.com>
2018-10-25 22:08:43 -0400

    Im trying to at least making it work, but without functions

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -22,8 +22,29 @@ function turnHoursToMinutes(movieArray){
 
 // Get the average of all rates with 2 decimals 
 
+let ratesAverage = movies.reduce((constAcc, eachObject)=>{
+
+    let sum = constAcc + Number(eachObject.rate)
+    return sum
+    
+      },0)
+    
+      console.log((ratesAverage / movies.length).toFixed(2));
+
 
 // Get the average of Drama Movies
+
+let dramaMoviesRate = movies.reduce((constAcc, eachObject)=>{
+
+    if ((eachObject.genre).includes('Drama')) {
+         sum = constAcc + eachObject.rate;
+         return sum;
+        } 
+        
+        
+    },0)
+    
+    console.log(dramaMoviesRate)
 
 
 // Order by time duration, in growing order
