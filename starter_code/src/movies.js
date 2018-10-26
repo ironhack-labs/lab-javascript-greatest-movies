baa3e162ebc3182caef6197c1ecc18b80262c21f
N R <mnpuzzle@gmail.com>
2018-10-26 02:50:39 +0200

    Sólo un ejercicio conseguido

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -2,6 +2,27 @@
 // Turn duration of the movies from hours to minutes 
 
 
+
+
+function turnHoursToMinutes(movies) {
+  return  movies.map(function (movie){
+  var hr = 0;
+  var min = 0;
+  if (movie.duration.indexOf("h") !== -1){
+    hr = parseInt(movie.duration[0]) * 60;
+  } else (movie.duration.indexOf("min") !== -1);{
+    min = parseInt(movie.duration.slice(-3));
+  }
+  return Object.assign({}, movie, { duration: hr + min });
+})
+
+  }
+
+
+
+  
+  
+
 // Get the average of all rates with 2 decimals 
 
 
