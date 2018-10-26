c3e55e2aaec8b524993f0e3582d49fb7f9c06624
Oscar Pachano <opachano@gmail.com>
2018-10-25 23:38:07 -0400

    done

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,15 +1,81 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
 
+function turnHoursToMinutes(arr) {
+  
+  return arr.map(movie => {
+
+    if (movie.duration.charAt(1) === "h") {
+
+      let tempDuration = movie.duration;
+      let hours = parseInt(tempDuration, 10) * 60;
+      movie.duration = hours; 
+      
+      if (parseInt(tempDuration.substr(2), 10)) {
+
+      let minutes = parseInt(tempDuration.substr(2), 10);
+      movie.duration += minutes;
+    }
+      movie.duration = movie.duration.toString();
+      return movie
+
+    } else {
+
+      let tempDuration = movie.duration;
+      let minutes = parseInt(tempDuration, 10);
+      movie.duration = minutes;
+      movie.duration = movie.duration.toString();
+      return movie
+
+    }
+  });
+
+}
 
 // Get the average of all rates with 2 decimals 
 
+function ratesAverage(arr) {
+
+  let average = arr.reduce((total, movie) => {
+    return total + Number(movie.rate);
+    }, 0)/arr.length
+
+    return Number(average.toFixed(2));
+};
 
 // Get the average of Drama Movies
 
+function dramaMoviesRate(arr) {
+
+  let filteredArray = arr.filter(movie => {
+    return movie.genre.indexOf("Drama") !== -1;
+  });
+  if (!filteredArray.length) {
+    return undefined
+  }
+
+  return Number(ratesAverage(filteredArray).toFixed(2));
+
+};
 
 // Order by time duration, in growing order
 
+function orderByDuration(arr) {
+  if (arr.length === 1) {
+    return arr;
+  }
+  let resultArray = turnHoursToMinutes(arr);
+  resultArray.sort((a, b) => {
+    if (Number(a.duration) > Number(b.duration)) {
+        return 1;
+    }
+    if (Number(a.duration) < Number(b.duration)) {
+      return -1
+    }
+    return 0;
+  })
+  return resultArray;
+}
 
 // How many movies did STEVEN SPIELBERG
 
