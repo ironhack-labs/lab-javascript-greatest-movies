b8f6fecdbd9bd826a381d3670d0128ca790eac9e
Jaime <herrerotajaime@gmail.com>
2018-10-26 08:00:55 +0200

    dos

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,9 +1,31 @@
-/* eslint no-restricted-globals: 'off' */
-// Turn duration of the movies from hours to minutes 
+function ratesAverage(array){
+  var sum = array.reduce(function(accum, current){
+    accum = accum + current.rate
+    return accum
+  },0)
+  
+  return sum / array.length 
+}
+
+
+
+function dramaMoviesRate(movies){
+  var dramaMovies =  movies.filter(function(item){
+     return item.genre.includes("Drama")
+ }) 
+ 
+ if(dramaMovies.length > 0){
+ return ratesAverage(dramaMovies)
+}
+} 
+
+
 
 
 // Get the average of all rates with 2 decimals 
 
+/* eslint no-restricted-globals: 'off' */
+// Turn duration of the movies from hours to minutes 
 
 // Get the average of Drama Movies
 
