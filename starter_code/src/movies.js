9eca016d47a621d2652296e7ccec95f6a44e17e1
Rachel Hofer <rhofer3@gmail.com>
2018-10-27 21:17:14 -0400

    Movie v1, Incomplete

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,9 +1,35 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
 
+function hoursToMinutes(movieObject){
+    let durationArray = movieObject.duration.split(' ')
+    let hours = durationArray[0][0]
+    let howManyDigitsMinutes = durationArray[1].length - 3
+    let minutes = durationArray[1].substr(0, howManyDigitsMinutes)
+    let totalDuration = Number(hours) * 60 + Number(minutes)
+    let newMovie = movieObject;
+  
+    newMovie.duration = totalDuration.toString();
+    return newMovie;
+  };
+  
+  function turnHoursToMinutes(movieArray){
+    return movieArray.map(hoursToMinutes);
+  }
+  
+  turnHoursToMinutes(movies);
 
 // Get the average of all rates with 2 decimals 
 
+const averageMovieRating = movies.filter((mov)=>{
+    if(mov.rate > 0){
+      return true
+    }
+  }).reduce((total, mov)=>{
+    return total + parseInt(mov.rate)
+  },0)/movies.length 
+  
+  console.log(averageMovieRating);
 
 // Get the average of Drama Movies
 
