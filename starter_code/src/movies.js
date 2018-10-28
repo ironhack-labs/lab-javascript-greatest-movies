adbdb14a0658b8b63f27f0ad829450a51836da28
Rodrigo.Munoz <rodrigomunozga@gmail.com>
2018-10-28 21:24:38 +0100

    Iteration 2

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -33,48 +33,136 @@ function turnHoursToMinutes(movies) {
 // Get the average of all rates with 2 decimals
 function ratesAverage(movies) {
 
-  
-  var stringToNum = movies.map(function(film) {
-    var num = film.rate.split(".");
+   
 
-    return (num[0] + num[1]) / 10;
-  });
-
-  var sumRate = stringToNum.reduce(function(sum, total) {
-    return sum + total;
-  });
-
- 
-  return parseFloat((sumRate / stringToNum.length).toFixed(2));
+    var toNumber = movies.map(function(film){
+      return film.rate = parseFloat(film.rate);
+   })
+   
 
 
-  
+
+   var averageRate = toNumber.reduce(function(sum, film){
+
+    return sum + film.rate;
+
+   })
+
+   return parseFloat(averageRate / averageRate.length)
+
+
 };
-
-var getRate8 = movies.filter(function(film){
-
-  return film.rate === '8.0';
-
-})
-
-  return((ratesAverage(getRate8).toFixed(2)))
-
-
-
-
-
-
-
-
-
 
 
 
 // Get the average of Drama Movies
+function dramaMoviesRate(movies){
+
+  var toNumber = movies.map(function(film){
+    return film.rate = parseFloat(film.rate);
+ })
+ 
+
+  var averageDrama = toNumber.reduce(function(sum, film){
+
+
+    if (film.genre.indexOf("Drama")){
+      return sum + film.rate;
+   }
+
+  })
+ 
+    console.log(parseFloat(averageDrama / averageDrama.length));
+}
+
+
+
+
+
+
+
 
 // Order by time duration, in growing order
 
+function orderByDuration(movies){
+
+  var films = turnHoursToMinutes(movies);
+  
+  var sortByTitle = films.title.sort()
+  console.table(sortByTitle)
+
+  var sortNumDuration = films.sort(function(a, b){
+
+
+
+    if (a.duration > b.duration){
+      return -1;
+    }
+
+    if (a.duration < b.duration){
+        return 1;
+    }
+    else{
+      return 0;
+
+    }
+    
+    
+    
+  
+
+
+  // console.table(sortNumDuration.title.sort())
+  })
+}
+
+
+
+
 // How many movies did STEVEN SPIELBERG
+
+function howManyMovies(movies){
+
+  var SpielbergFilmsDrama = [];
+
+  var SpielbergFilms =  movies.filter(function(film){
+
+    SpielbergFilmsDrama.push(SpielbergFilms.genre.indexOf("Drama"))
+    console.log(SpielbergFilmsDrama)
+
+    // return film.director.indexOf('Spielberg');
+
+  })
+
+
+
+
+  if (SpielbergFilms === []){
+    return undefined;
+
+  }else if (SpielbergFilmsDrama === []){
+    return "Steven Spielberg directed 0 drama movies!"
+  }
+
+
+
+
+
+
+
+
+    // Should return an string
+    // Should return undefined if the array is empty
+    // Return "Steven Spielberg directed 0 drama movies!" if he does not directed none of the movies in the array
+    // Only drama movies! Should return "Steven Spielberg directed 1 drama movies!"
+    // Only Steven Spielberg movies! Should return "Steven Spielberg directed 2 drama movies!"
+    // Should return "Steven Spielberg directed 4 drama movies!"
+
+
+
+
+
+}
 
 // Order by title and print the first 20 titles
 
