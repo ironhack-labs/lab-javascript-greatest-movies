6d9de43adfd061e30169df68048fb3a6039573be
MontseOrtiz <montse_2908@hotmail.com>
2018-11-01 18:12:16 -0600

    MEX-PP MONTSE ORTIZ SANTIAGO HINOJOSA

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,9 +1,50 @@
 /* eslint no-restricted-globals: 'off' */
+var movies = require("./data.js");
+
 // Turn duration of the movies from hours to minutes 
 
+function turnHoursToMinutes(movies){
+    var minutos =movies.map((elem)=>{
+      duracion = elem.duration 
+      if (duracion.length === 7) {
+    var a = duracion.charAt(0)
+    var b = duracion.charAt(3)
+    
+    var Hora = parseInt(a)
+    var dmin = parseInt(b)
+    
+    var duracionCorregida = Hora*60 + dmin
+    
+    elem.duration = duracionCorregida
+    return elem
+    }
+    
+    else if (duracion.length === 8) {
+    
+    var a = duracion.charAt(0)
+    var b = duracion.charAt(3)
+    var c = duracion.charAt(4)
+    
+    var Hora = parseInt(a)
+    var dmin = parseInt(b)
+    var seg = parseInt(c)
+    
+    var duracionCorregida = Hora*60 + dmin*10 +seg
+    
+    elem.duration = duracionCorregida
+    return elem
+    }
+        })
+        return minutos
+    }
+  
+  turnHoursToMinutes(movies)
 
 // Get the average of all rates with 2 decimals 
 
+function ratesAverage (movies){
+    
+}
 
 // Get the average of Drama Movies
 
