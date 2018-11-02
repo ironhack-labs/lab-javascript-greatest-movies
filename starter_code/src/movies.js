d94d8ed7d50470d574a1ac66b29bbcf41eba9586
MontseOrtiz <montse_2908@hotmail.com>
2018-11-01 22:18:29 -0600

    parte 2

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -42,18 +42,85 @@ function turnHoursToMinutes(movies){
 
 // Get the average of all rates with 2 decimals 
 
-function ratesAverage (movies){
-    
+function ratesAverage (movies2){
+  var promedio = 0
+  for(var i = 0; i < movies2.length; i++){
+   promedio = promedio + parseFloat(movies2[i].rate)
+  }
+  return (parseFloat((promedio/movies2.length).toFixed(2)))
 }
 
 // Get the average of Drama Movies
 
-
+function dramaMoviesRate (movies2){
+  var drama = 0
+  var cont= 0
+  for(var i = 0; i < movies2.length; i++){
+    for(var j=0;j<movies2[i].genre.length; j++ ){
+        if(movies2[i].genre[j] === "Drama"){
+          drama = drama + parseFloat(movies2[i].rate) 
+          cont++
+        }
+    }
+    
+  }
+  return (parseFloat((drama/cont).toFixed(2)))
+}
 // Order by time duration, in growing order
+
+function orderByDuration (arr){
+
+  var x= turnHoursToMinutes(arr)
+  console.log(x)
+  
+  
+  
+   var d = x.map(function(i){
+     return i.duration
+   })
+  
+  
+  
+   var order = d.sort(function(a,b){
+     return a-b
+   })
+  
+  
+  var ordenado = []
+  
+  for(var i=0 ; i < x.length ; i++){
+  
+   if(order[i] ===  x[i].duration){
+       ordenado.push( x[i])
+   }
+  
+  }
+   return ordenado
+  
+  }
 
 
 // How many movies did STEVEN SPIELBERG
 
+
+function dramaMoviesRate (arr){
+
+  var arre=[];
+    for(var i = 0; i < arr.length; i++){
+      if (arr[i].director === 'Steven Spielberg') {
+          var cont=0;
+            for(var j=0;j<arr[i].genre.length; j++ ){
+  
+          
+              if (arr[i].genre[j]==="Drama"){
+              arre.push(arr[i]) }
+  
+              }
+  
+      }
+    }
+    return arre;
+  }
 
 // Order by title and print the first 20 titles
 
