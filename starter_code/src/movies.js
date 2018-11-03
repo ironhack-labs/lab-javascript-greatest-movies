3f81044a290d17abb3991e42cf388110c5d55834
Patricia Defez <ptddds@gmail.com>
2018-11-03 17:01:49 +0100

    in progres

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,5 +1,16 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
+ 
+function turnHoursToMinutes(array) {
+ var newArray = []; 
+
+  newArray = array.map(function (elem) {
+    var newElem = Object.assign({}, el);
+    var newDuration = newElem.duration.replace('h','').replace('min','').split(' ');
+    var hours = Number(newDuration[0])
+  })
+  
+}
 
 
 // Get the average of all rates with 2 decimals 
