016759c949bae18512b334a9849064bdca35a1cb
Christopher Vazquez <c_vazquez08@yahoo.com>
2018-11-03 17:14:49 -0400

    finish first iteration

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,6 +1,37 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
 
+function turnHoursToMinutes(array){
+  let movie = array.map(elem => {
+    let durationToMinutes = 0;
+    if(elem.duration.includes('h') && elem.duration.includes('min')){
+      //console.log('hr/min', elem.duration)
+      let hoursAndMinutes = elem.duration.replace('h','').replace('min','').split(' ')
+      durationToMinutes = 60 * parseInt(hoursAndMinutes[0]) + parseInt(hoursAndMinutes[1])
+      //console.log('hours/minutes/convertion: ',hoursAndMinutes, durationToMinutes)
+      //console.log(hoursAndMinutes)
+    }else if(elem.duration.includes('h')){
+      //console.log('hours', elem.duration)
+      let justHours = elem.duration.replace('h', '');
+      durationToMinutes = justHours * 60;
+      //console.log('just hours convert', durationToMinutes)
+    }else{
+      let justMinutes = elem.duration.replace('min', '');
+      console.log(justMinutes);
+      //console.log('minutes', elem.duration)
+    }
+    return {title: elem.title, year: elem.year, director: elem.director, duration: durationToMinutes, genre: elem.genre, rate: elem.rate};
+  }) 
+  console.log(movie);
+  return movie;
+
+}
+
+
+
+
+
+
 
 // Get the average of all rates with 2 decimals 
 
