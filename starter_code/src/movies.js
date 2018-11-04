f582cd8ce0d5ca4911bac90a988bb9087f0d8ae3
Javier Requejo Melchor <javierrequejomelchor@gmail.com>
2018-11-04 20:22:08 +0100

    turnHoursToMinutes completed!!

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,6 +1,23 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
-
+function turnHoursToMinutes(arr) {
+  var newArray = arr.map(function(element) {
+    var newEl = Object.assign({}, element);
+    var minutes = newEl.duration.replace('h','').replace(' ', ':').replace('min','');
+    var totalHour = minutes.split(':');
+    console.log(totalHour);
+    if (totalHour.length === 2) {
+    var definitiveHour = (60*Number(totalHour[0]) + Number(totalHour[1]));
+    } else if (totalHour[0]<10) {
+      var definitiveHour = 60 * Number(totalHour[0]);
+    } else {
+      var definitiveHour = Number(totalHour[0]);
+    }
+    newEl.duration = definitiveHour;
+    return newEl;
+  })
+  return newArray;
+}
 
 // Get the average of all rates with 2 decimals 
 
