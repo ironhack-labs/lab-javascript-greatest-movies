52772a4f7d3b8fb9ca9d4d6bf9d75fd6789c053e
Chris Rodriguez <crodr521@gmail.com>
2018-11-05 18:02:39 -0500

    Added ratesAverage

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -31,12 +31,46 @@ function turnHoursToMinutes(duration) {
     return movie;
 }
 
+var movies = [
+    {
+        title: 'The Shawshank Redemption',
+        year: '1994',
+        director: 'Frank Darabont',
+        duration: '2h 22min',
+        genre: ['Crime', 'Drama'],
+        rate: '9.3'
+      },
+      {
+        title: 'The Godfather',
+        year: '1972',
+        director: 'Francis Ford Coppola',
+        duration: '2h 55min',
+        genre: ['Crime', 'Drama'],
+        rate: '9.2'
+      },
+      {
+        title: 'The Godfather: Part II',
+        year: '1974',
+        director: 'Francis Ford Coppola',
+        duration: '3h 22min',
+        genre: ['Crime', 'Drama'],
+        rate: '9.0'
+      }
+]
 
 // Get the average of all rates with 2 decimals 
+function ratesAverage(ratings) {
+    let rate = 0;
 
+    ratings.map(element => {rate = rate + element.rate})
+
+    return parseFloat((rate / ratings.length).toFixed(2));
+}
 
 // Get the average of Drama Movies
-
+function dramaMoviesRate(ratings) {
+    
+}
 
 // Order by time duration, in growing order
 
