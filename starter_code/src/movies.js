a730cae2ae3023ec51e60086bb8216aac833a7b0
Chris Rodriguez <crodr521@gmail.com>
2018-11-05 17:08:38 -0500

    Added turnHourstoMinutes function

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,5 +1,35 @@
-/* eslint no-restricted-globals: 'off' */
-// Turn duration of the movies from hours to minutes 
+// Turn duration of the movies from hours to minutes
+function turnHoursToMinutes(duration) {
+    let movie = duration.map(element => {
+        let durationToMinutes = 0;
+        
+        if(element.duration.includes('h') && element.duration.includes('min')) {
+          let hoursAndMinutes = element.duration.replace('h','').replace('min','').split(' ')
+          durationToMinutes = 60 * parseInt(hoursAndMinutes[0]) + parseInt(hoursAndMinutes[1])
+        }
+
+        else if(element.duration.includes('h')){
+          let justHours = element.duration.replace('h', '');
+          durationToMinutes = justHours * 60;
+        }
+
+        else {
+          let justMinutes = element.duration.replace('min', '');
+          durationToMinutes = justMinutes * 1;
+        }
+        
+        return {
+            title: element.title, 
+            year: element.year, 
+            director: element.director, 
+            duration: durationToMinutes, 
+            genre: element.genre, 
+            rate: element.rate
+        };
+      })
+
+    return movie;
+}
 
 
 // Get the average of all rates with 2 decimals 
