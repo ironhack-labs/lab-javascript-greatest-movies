2b46ac75f3700c9c5c330f61762db987e1725584
Alex McDaniel <amcdaniel186@gmail.com>
2018-11-04 20:14:15 -0500

    update2

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,44 +1,39 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes
 
-function turnHoursToMinutes(movieArray){
-  let newMovie = [];
-  return movieArray.map((movieObject)=>{
-
-  let durationArray = movieObject.duration.split(' ');
-  let hours = durationArray[0][0]
-  let howManyDigitsMinutes =  durationArray[1].length - 3
-  let minutes = durationArray[1].substr(0, howManyDigitsMinutes)
-  let totalDuration =  Number(hours)*60 + Number(minutes)
-  let newMovie = movieObject;
-  newMovie.duration =  totalDuration.toString();
-
-    
-  return newMovie;
-
-  })
-}
-
-
-// function turnHoursToMinutes(movieArray){
-//   return movieArray.map(hoursToMinutes);
-// }
-
-turnHoursToMinutes(movies)
+function
+// return movies.map((movieObject)=>{
 
 // Get the average of all rates with 2 decimals 
+let text = movies.rate.map(function(num){
+  return movies.rate.parseFloat(text);
+})
+let ratesAverage = movies.rate.reduce(function(sum, num){
+  return sum + num;
+}, 0);
+console.log(totalRate);
+
+// avg 1. convert rate strings into numbers. parseFloat
+// 2. run .reduce() through movies Array, to sum up all of the rate values in the Array.
+// 3. With the totalRate, then divide the total rate by the Array.length to get the average.
+
+
+
 
 
 // Get the average of Drama Movies
-
+let dramaMoviesRate = movies.genre['drama'].reduce.
+Then with the total drama rate, let avgDramaRate = divide by a totalDrama = movies.genre['drama'].length to get the avg.
 
 // Order by time duration, in growing order
-
+let orderByDuration = movies.duration.sort()
 
 // How many movies did STEVEN SPIELBERG
-
+let howManyMovies = movies.filter(function(number){
+  return number.genre['drama'].includes('Steven Spielberg')
+});
 
 // Order by title and print the first 20 titles
-
+let orderAlphabetically = movies.map()
 
 // Best yearly rate average
