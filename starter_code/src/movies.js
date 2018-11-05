8304cb46906893ffef85cff7413065bfbba853a0
Vero Celemin <veronica.celemin@gmail.com>
2018-11-05 17:37:20 +0100

    Iteration 1 done

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,9 +1,41 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
 
+function turnHoursToMinutes(arr){
+  var newArray = arr.map(function(element){
+    var newElement = Object.assign({},element);
+    
+    var timeArr = element.duration.split(" ");
+
+    var timeArrNum = timeArr.map(function(cad){
+      if (cad.endsWith('h')) {
+        cad = cad.substring(0,(cad.length-1));
+        return parseInt(cad) * 60;
+      }
+      if (cad.endsWith('min')) {
+        cad = cad.substring(0,(cad.length-3));
+        return parseInt(cad);
+      } 
+    });
+
+    var timeMinutes = timeArrNum.reduce(function(accumulator,number){
+      return accumulator + number;
+    },0);
+
+    newElement.duration = timeMinutes;
+    
+    return newElement;
+  });
+  
+ return newArray;
+}
+
 
 // Get the average of all rates with 2 decimals 
 
+function ratesAverage(array){
+  return prueba;
+} 
 
 // Get the average of Drama Movies
 
