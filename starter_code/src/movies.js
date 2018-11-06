07ec18a4f4ffcee13dfa6576c6630109ec25dd88
Marco Monter <monter.marco@gmail.com>
2018-11-06 17:04:51 -0600

    JS all time movies started

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,5 +1,23 @@
 /* eslint no-restricted-globals: 'off' */
+
+
+
+
 // Turn duration of the movies from hours to minutes 
+
+function hoursToMinutes(){
+
+let hr = movies.map(duration => movies.duration.substring(0,1));
+let min = movies.map(duration => movies.duration.substring(2).replace("min", " "));
+
+return Number(hr) + min;
+
+}
+
+let newDuration = movies.map(hoursToMinutes);
+console.log(newDuration);
+
+
 
 
 // Get the average of all rates with 2 decimals 
