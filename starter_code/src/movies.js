9024f022206e73c8764aa3949d810d7160b12af1
Camilo Campo <mcp.ccampo@gmail.com>
2018-11-06 19:25:16 -0500

    finished iteration 6

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -2,21 +2,19 @@
 // Turn duration of the movies from hours to minutes
 function turnHoursToMinutes(movies) {
   let arr = movies.map(element => {
-    let durationConvertion = 0; // use later to built duration-convertion to minutes
-    let { title, year, director, duration, genre, rate } = element; // decustruct object... ease of use
+    // decustruct object, ease of use mainly
+    let { title, year, director, duration, genre, rate } = element;
 
-    // parse hour and minutes and solve accordingly...
-    let MovieHours = 0;
-    let MovieMinutes = 0;
+    let durationConvertion = 0; // use later to built duration-convertion to minutes
     let durationSplit = [];
     let hours = 0;
     let minutes = 0;
-
-    //split duration property,
+    //string from time object property then split duration into array for later use
     durationSplit = duration
       .replace(' ', '')
       .replace('min', '')
       .split('h');
+
     // decustruct array and set hour, and minute values
     if (duration.includes('h') && duration.includes('min')) {
       [hours, minutes] = durationSplit;
@@ -27,7 +25,8 @@ function turnHoursToMinutes(movies) {
       [minutes] = durationSplit;
       hours = 0;
     }
-    // convert duration to hour or minute or both accordintly 
+
+    // convert hour-minute and calucation duration to minutes
     if (hours) {
       if (hours => 2) {
         durationConvertion += parseInt(hours, 10) * 60;
@@ -38,27 +37,81 @@ function turnHoursToMinutes(movies) {
       }
     }
 
+    // add minutes to duration convertion
     if (minutes) {
       durationConvertion += parseInt(minutes, 10);
     }
 
     // overide duration value with converted value
     duration = durationConvertion;
+
     // return object to map function..
     return { title, year, director, duration, genre, rate };
   });
-  // return formated duration to minutes
+
+  // returns new array object from function..
   return arr;
 }
 
 // Get the average of all rates with 2 decimals
+function ratesAverage(movies) {
+  let avg = 0;
+  let sum = movies.reduce((base, movie) => {
+    if (movie.rate > 0) {
+      return base + parseInt(movie.rate, 10);
+    } else {
+      return base + 0;
+    }
+  }, 0);
+  avg = +(sum / movies.length).toFixed(2);
+  return avg;
+}
 
 // Get the average of Drama Movies
+function dramaMoviesRate(movies) {
+  let filteredDramaMovieList = movies.filter(movie => movie.genre.includes('Drama') || movie.genre.includes('drama'));
+  return ratesAverage(filteredDramaMovieList) || undefined;
+}
 
 // Order by time duration, in growing order
+function orderByDuration(movies) {
+  let movieSortAsc = movies.sort((b, a) => {
+    if (a.duration > b.duration) {
+      return -1;
+    } else if (a.duration < b.duration) {
+      return 1;
+    } else if (a.duration === b.duration) {
+      return a.title < b.title ? 1 : -1;
+    }
+  });
+  return movieSortAsc;
+}
 
 // How many movies did STEVEN SPIELBERG
+function howManyMovies(movies){
+  let movieList = movies
+    .filter(movie => movie.director === 'Steven Spielberg')
+    .filter(movie => movie.genre.includes('Drama'))
 
+  if(movies.length > 0){
+    return `Steven Spielberg directed ${movieList.length} drama movies!` 
+  } else {
+    return undefined;
+  }
+}
 // Order by title and print the first 20 titles
+function orderAlphabetically(movies){
+  
+  let movieSortAsc = movies.sort((b, a) => {
+    if(a.title < b.title){
+      return 1
+    } else if(a.title > b.title){
+      return -1
+    } else {
+      return 0
+    }
+  }).map(element => element.title)
+  return movieSortAsc.slice(0,20)
+}
 
 // Best yearly rate average
