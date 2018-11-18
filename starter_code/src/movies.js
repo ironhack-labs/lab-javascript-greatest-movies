a38a594edc3de085f675247adf00c96bc6ab7dcc
Diego <diegoramos.media@gmail.com>
2018-11-18 12:32:13 -0200

    All Times Movies - wip

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,138 +1,59 @@
 /* eslint no-restricted-globals: 'off' */
-// Turn duration of the movies from hours to minutes 
 
-var movies = [
-    {
-        title: 'Ahe Shawshank Redemption',
-        year: '1994',
-        director: 'Frank Darabont',
-        duration: '2h 22min',
-        genre: ['Crime', 'Drama'],
-        rate: '9.3'
-    },
-    {
-        title: 'Bchindler\'s List',
-        year: '1993',
-        director: 'Steven Spielberg',
-        duration: '3h 15min',
-        genre: ['Biography', 'Drama', 'History'],
-        rate: '8.9'
-    },
-    {
-        title: 'Che Godfather',
-        year: '1972',
-        director: 'Francis Ford Coppola',
-        duration: '2h 55min',
-        genre: ['Crime', 'Drama'],
-        rate: '9.2'
-    },
-    {
-        title: 'Dhe Godfather: Part II',
-        year: '1974',
-        director: 'Francis Ford Coppola',
-        duration: '3h 22min',
-        genre: ['Crime', 'Drama'],
-        rate: '9.0'
-    },
-    {
-        title: 'Echindler\'s List',
-        year: '1993',
-        director: 'Steven Spielberg',
-        duration: '3h 15min',
-        genre: ['Biography', 'Drama', 'History'],
-        rate: '8.9'
-    }
-]
+// Turn duration of the movies from hours to minutes
 
-// var arraysTime = movies.map(function (movie) {
-//     var hourInMinutes = movie.duration[0] * 60;
-//     var totalMinutes = movie.duration.split(' ');
-//     var minutes;
+function turnHoursToMinutes(arrayOfMovies) {
+    var hour;
+    var minutes;
+    var duration = [];
 
-//     if (totalMinutes.length === 1) {
-//         minutes = 0;
-//     }
-//     if (totalMinutes.length > 1) {
-//         if (totalMinutes[1].length === 5) {
-//             minutes = totalMinutes[1].slice(0, 2);
-//         }
-//     }
-//     if (totalMinutes.length > 1) {
-//         if (totalMinutes[1].length === 4) {
-//             minutes = totalMinutes[1].slice(0, 1);
-//         }
-//     }
-//     return (hourInMinutes + parseInt(minutes));
-// });
+    var moviesInMinutes = arrayOfMovies.map(function (movie) {
+        if (movie.duration.indexOf(`h`) === -1) {
+            hour = 0;
+            duration = movie.duration.split(`min`)
+            minutes = Number(duration[0]);
+            movie.newDuration = minutes;
+        }
+        else if (movie.duration.indexOf(`m`) === -1) {
+            minutes = 0;
+            duration = movie.duration.split(`h`)
+            hour = Number(duration[0]);
+            movie.newDuration = hour * 60;
+        }
+        else {
+            duration = movie.duration.split(`h `)
+            duration[1] = duration[1].slice(0, (0, duration[1].length - 3));
+            hour = Number(duration[0]) * 60;
+            minutes = Number(duration[1]);
+            movie.newDuration = (hour + minutes);
+        }
+        var updatedMovie = {
+            title: movie.title,
+            year: movie.year,
+            director: movie.director,
+            duration: movie.newDuration,
+            genre: movie.genre,
+            rate: movie.rate
+        }
+        return updatedMovie;
+    });
+    return moviesInMinutes;
+};
 
 // // Get the average of all rates with 2 decimals 
-// console.log(arraysTime);
-// console.log('------------------')
-// console.log('')
-// function turnHoursToMinutes(movies, arraysTime) {
-//     for (let i = 0; i < movies.length; i++) {
-//         movies[i].duration = arraysTime[i];
-//     }
-//     console.log(movies)
-//     return movies;
-// }
-// turnHoursToMinutes(movies, arraysTime);
+function ratesAverage(arrayOfMovies) {
+    var totalRating = arrayOfMovies.reduce(function (total, movie) {
+        return total + Number(movie.rate);
+    }, 0);
+    return totalRating / arrayOfMovies.length;
+}
 
 // // Get the average of Drama Movies
-// function ratesAverage(moviesArray) {
-//     var totalRate = moviesArray.reduce(function (total, elem, index) {
-//         return total + parseFloat(elem.rate);
-//     }, 0)
-//     return (totalRate / moviesArray.length).toFixed(2);
-// }
-// console.log('------------------')
-// console.log('')
-// // console.log(ratesAverage(movies));
-// function dramaMoviesRate(moviesArray) {
-//     var dramaAverage = movies.filter(function (item) {
-//         return item.genre.includes('Drama')
-//     });
-//     return ratesAverage(dramaAverage)
-// }
-// console.log(dramaMoviesRate(movies));
-// console.log('------------------')
-// console.log('')
 
 // // Order by time duration, in growing order
-// function orderByDuration(moviesArray) {
-//     moviesArray.sort(function (a, b) {
-//         return a.duration - b.duration;
-//     })
-//     return moviesArray;
-// }
-
-// console.log(orderByDuration(movies));
-// console.log('------------------')
-// console.log('')
 
 // // How many movies did STEVEN SPIELBERG
-// function howManyMovies(moviesArray) {
-//     var moviesSteven = moviesArray.filter(function (item) {
-//         return (item.genre.includes('Drama') && item.director.includes('Steven Spielberg'))
-//     });
-//     return moviesSteven
-// }
-// console.log(howManyMovies(movies));
-// console.log('------------------')
-// console.log('')
 
 // Order by title and print the first 20 titles
-
-movies.sort(function (a, b) {
-    if (a.title < b.title) {
-        return -1
-    }
-    if (a.title > b.title) {
-        return 1
-    }
-    return 0;
-})
-
-console.log(movies);
 
 // Best yearly rate average
