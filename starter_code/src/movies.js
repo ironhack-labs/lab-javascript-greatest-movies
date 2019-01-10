df25b5e57a75a07ec0094cd7acad96c05d32a89b
Estelle Olawinski <estelle.olawinski@me.com>
2019-01-10 19:26:52 +0100

    /

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,20 +1,33 @@
 /* eslint no-restricted-globals: 'off' */
-// Turn duration of the movies from hours to minutes 
+// Turn duration of the movies from hours to minutes
 
+// Get the average of all rates with 2 decimal
 
-// Get the average of all rates with 2 decimals 
-
+function ratesAverage(movies) {
+  var total = movies.reduce(function(sum, oneMovie) {
+    return sum + Number(oneMovie.rate);
+  }, 0);
+  var average = Number(total / movies.length);
+  return average;
+}
 
 // Get the average of Drama Movies
+function dramaMoviesRate(movies) {
+  if (movies === NaN) {
+    return undefined;
+  }
+  var drama = movies.filter(function(oneDrama) {
+    return oneDrama.genre.indexOf("Drama") !== -1;
+  });
+  return parseFloat(ratesAverage(drama), 2);
+}
 
+console.log;
 
 // Order by time duration, in growing order
 
-
 // How many movies did STEVEN SPIELBERG
 
-
 // Order by title and print the first 20 titles
-
 
 // Best yearly rate average
