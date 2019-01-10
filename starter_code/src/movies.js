611cd4851ed9c28f9f178fe8df8d07b62c0d616f
frietpoune <celine.desnoe@skema.edu>
2019-01-10 18:11:21 +0100

    Paris - Kevin M & Céline

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,20 +1,38 @@
 /* eslint no-restricted-globals: 'off' */
-// Turn duration of the movies from hours to minutes 
+// Turn duration of the movies from hours to minutes
 
+// Get the average of all rates with 2 decimals
 
-// Get the average of all rates with 2 decimals 
+function ratesAverage(movies) {
+  var average = movies.reduce(function(sum, oneMovie) {
+    return sum + Number(oneMovie.rate);
+  }, 0);
+  return Number((average / movies.length).toFixed(2));
+}
 
+console.log(ratesAverage(movies));
 
 // Get the average of Drama Movies
 
+function dramaMoviesRate(movies) {
+  var dramaMovies = movies.filter(function(oneMovie) {
+    return oneMovie.genre.indexOf("Drama") > -1;
+  });
+  console.log(dramaMovies);
+
+  console.log(ratesAverage(dramaMovies));
+  if (dramaMovies.length === 0) {
+    return undefined;
+  }
+  return ratesAverage(dramaMovies);
+}
+
+console.log(dramaMoviesRate(movies));
 
 // Order by time duration, in growing order
 
-
 // How many movies did STEVEN SPIELBERG
 
-
 // Order by title and print the first 20 titles
-
 
 // Best yearly rate average
