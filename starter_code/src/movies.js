602f217a2563a07f1253c6595b9a13fb3969513d
Jorge <jlegazpegutierrez@gmail.com>
2019-01-10 22:28:31 +0100

    Empezado pero bloqueado en el primer ejercicio

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,6 +1,44 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
 
+var minutes = [];
+var hoursMinuts = []
+
+function turnHoursToMinutes (array){
+  
+  arrayToReturn = array.map(function(movie){
+    var newDuration= 0;
+    var partition = [];
+    
+
+    if (typeof(movie.duration) == 'number'){
+      newDuration = movie.duration;
+    } 
+    
+    if (typeof(movie.duration) == 'string'){
+      if (movie.duration.includes("h") && movie.duration.includes("min")){
+        partition = movie.duration.split(" ");
+        var hours = parseInt(partition[0])
+        var minutes = parseInt(partition[1]);
+        return newDuration = hours * 60 + minutes;
+     
+      } 
+     if (!movie.duration.includes("h")){
+        return newDuration = parseInt(partition[1]); 
+        } 
+     if (movie.duration.includes("h") && !movie.duration.includes("min")){
+      return newDuration = parseInt(partition[0]) * 60;
+     } 
+    } 
+    return Object.assign({},movie,movie.duration=newDuration)
+  })
+  return arrayToReturn
+}
+
+
+// hoursMinuts = array.duration.splice(" ")
+
+
 
 // Get the average of all rates with 2 decimals 
 
