da6fc43a4315feb116f764c0fb506900c6ac1251
Deie <chantalsauvann@gmail.com>
2019-01-11 10:09:50 +0100

    Paris - Alexandre & Delphine

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -4,9 +4,27 @@
 
 // Get the average of all rates with 2 decimals 
 
+function ratesAverage(moviesArray) {
+
+    moviesRate = moviesArray.map(function (oneMovie) {
+        return parseFloat(oneMovie.rate);
+    });
+
+    rateTotal = moviesRate.reduce(function (sum, oneRate) {
+        return sum + oneRate;
+    }, 0);
+
+    return Number(rateTotal / moviesArray.length).toFixed(2);
+
+};
 
 // Get the average of Drama Movies
-
+function dramaMoviesRate(movies) {
+   
+    if (drama.length === 0) {
+        return undefined;
+    };
+ };
 
 // Order by time duration, in growing order
 
