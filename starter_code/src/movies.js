48e97e6a262ad5fd42525129bdf69dea7b9bb8f5
Mr.X <seniorito.x@icloud.com>
2019-01-11 09:07:55 +0100

    First commit

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,6 +1,24 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
 
+function turnHoursToMinutes(movies) {
+  var moviesCopy = movies.map(function(movie) {
+    var time = movie.duration.split(' ');
+    var minutes = 0;
+
+    time.forEach(function(date) {
+      if (date.indexOf('h') !== -1) {
+        minutes += parseInt(date) * 60
+      } else {
+        minutes += parseInt(date)
+      }
+    })
+
+    return Object.assign({}, movie, {duration: minutes})
+  })
+
+  return moviesCopy;
+}
 
 // Get the average of all rates with 2 decimals 
 
