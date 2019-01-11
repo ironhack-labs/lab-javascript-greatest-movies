ce33e7373d44d4573358c3b956318ef57c6b9fcd
Rojiblanco182 <salinas.182@hotmail.com>
2019-01-11 09:29:13 +0100

    first version

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -2,11 +2,53 @@
 // Turn duration of the movies from hours to minutes 
 
 
+function turnHoursToMinutes(movies){
+  
+    var time = 0;
+    
+    var minutesArr = movies.map(function(film){
+      var splitTime = film.duration.split(" ");
+      var minutes = 0;
+    
+      if (splitTime.length > 1){
+        var hours = parseInt(splitTime[0])*60;
+        minutes = parseInt(splitTime[1]);
+        time = hours+minutes;
+      }
+      else {
+        minutes = parseInt(splitTime[0]);
+        time = minutes;
+      }
+      return Object.assign({}, film, {duration: time})
+    
+    })
+  return minutesArr;
+ }
+
+
+
 // Get the average of all rates with 2 decimals 
+
+function ratesAverage (movies) {
+  var avgSum = movies.reduce(function (total, movie) {
+    return total + movie.rate;
+  }, 0);
+
+  return parseFloat(avgSum / movies.length);
+}
 
 
 // Get the average of Drama Movies
 
+function dramaMoviesRate (movies){
+  var dramaArr = movies.filter(function (movie) {
+    if (movie.genre.includes ('Drama')){
+      return movie;
+    }
+  })
+  var dramaRate = ratesAverage(dramaArr);
+  console.log(dramaRate);
+}
 
 // Order by time duration, in growing order
 
