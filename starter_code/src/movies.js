49d4d72a6a38806051cd5e90b0536b9514c926b9
Poeli Bojorquez <pbojorquez@poeli.com>
2019-01-11 14:06:11 +0100

    Paris - Poeli

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -34,20 +34,23 @@ function ratesAverage(movies) {
 //// Get the average of Drama Movies
 // Create a dramaMoviesRate that receives an array as a parameter to get the average rate
 // of all drama movies.
+
 // 1. create new array of dramas
-
-var dramas = movies.filter(function(oneMovie) {
-  return oneMovie.genre === "Drama";
-});
-
-// 2. create function of rates average
+// 2. create rates average and return undefined
 
 function dramaMoviesRate(dramas) {
-  var total = dramas.reduce(function(sum, oneDrama) {
+  var dramaMovies = dramas.filter(function(oneMovie) {
+    return oneMovie.genre.indexOf("Drama") !== -1;
+  });
+  if (dramaMovies.length === 0) {
+    return undefined;
+  }
+
+  var total = dramaMovies.reduce(function(sum, oneDrama) {
     return sum + Number(oneDrama.rate);
   }, 0);
 
-  return Number((total / dramas.length).toFixed(2));
+  return Number((total / dramaMovies.length).toFixed(2));
 }
 
 //// Order by time duration, in growing order
@@ -55,18 +58,24 @@ function dramaMoviesRate(dramas) {
 //// How many movies did STEVEN SPIELBERG
 // Create a howManyMovies method that receives an array as a parameter and filter the array
 // so we can have only the drama movies where Steven Spielberg is the director.
-// 1. create new array of Spielberg movies
-var spielberg = movies.filter(function(oneMovie) {
-  return (oneMovie.director = "Steven Spielberg");
-});
 
+// 1. create new array of Spielberg movies and return undefined
 // 2. create function to filter dramas
-function howManyMovies(spielberg) {
-  var spielbergDramas = spielberg.filter(function(oneMovie) {
-    return Number((oneMovie.genre = "Drama"));
-  });
 
-  return "Steven Spielberg directed " + spielbergDramas + " dramas";
+function howManyMovies(movies) {
+  var spielberg = movies.filter(function(oneMovie) {
+    return oneMovie.director.indexOf("Steven Spielberg") !== -1;
+  });
+  if (spielberg.length === 0) {
+    return undefined;
+  }
+
+  var spielbergDramas = spielberg.filter(function(oneMovie) {
+    return oneMovie.genre.indexOf("Drama") !== -1;
+  });
+  return (
+    "Steven Spielberg directed " + spielbergDramas.length + " drama movies"
+  );
 }
 
 //// Order by title and print the first 20 titles
