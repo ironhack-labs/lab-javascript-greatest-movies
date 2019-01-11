aa0028472b06772651b31bb4bf7af35bfaf6615e
Zenchy <alvaro.corral.soilan@gmail.com>
2019-01-11 01:05:15 +0100

    Empezarndo el 3

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,20 +1,45 @@
 /* eslint no-restricted-globals: 'off' */
-// Turn duration of the movies from hours to minutes 
+// Turn duration of the movies from hours to minutes
+function turnHoursToMinutes(movie) {
+  var finalMovies = movie.map(function(elementDuration) {
+    var soloHoras = elementDuration.duration.split("h");
 
+    if (soloHoras == elementDuration.duration) {
+      soloHoras = [0, elementDuration.duration];
+    }
+    var soloMinutos = soloHoras[1].split("min");
+    if (soloMinutos[0] == "") {
+      soloMinutos[0] = 0;
+    }
 
-// Get the average of all rates with 2 decimals 
+    var newMovies = Object.assign({}, movies, {
+      duration: parseInt(soloHoras[0]) * 60 + parseInt(soloMinutos[0])
+    });
+    console.log(newMovies);
 
+    return newMovies;
+  });
+  console.log("string");
+  return finalMovies;
+}
+// Get the average of all rates with 2 decimals
+function ratesAverage(arrayMovies) {
+
+  var aver = arrayMovies.reduce(function(average, movie) 
+  {
+    console.log(average + "  " + movie.rate);
+    return average+=movie.rate
+    
+  }, 0);
+  return parseFloat(aver/arrayMovies.length);
+}
 
 // Get the average of Drama Movies
 
-
 // Order by time duration, in growing order
-
 
 // How many movies did STEVEN SPIELBERG
 
-
 // Order by title and print the first 20 titles
-
 
 // Best yearly rate average
