35357e8d71d2374245098ac03ca699b2bd138962
Dani Garcia <danielgarciamartinez1985@gmail.com>
2019-01-11 09:27:33 +0100

    there is still a long way to go

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,6 +1,35 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
 
+function turnHoursToMinutes(movies) {
+
+  var updatedMovies = movies.map(function(movie) {
+    var splitTime = movie.duration.split(" ");
+      var minutes = 0;
+      var hours = 0;
+    for (var i = 0; i < splitTime.length; i++){
+      if (splitTime[i].indexOf("h") === -1){
+        minutes = parseInt(splitTime[i]);
+      } else {
+        hours = parseInt(splitTime[i])* 60;
+      }
+    }
+    /*if (splitTime < 2) {
+      splitTime = ("has only minutes")
+    } else {
+      splitTime = ("has hours and minutes")
+    }*/
+    console.log(hours + " " + minutes)
+
+    return hours + minutes;
+    //console.log(turnHoursToMinutes)
+   
+  })
+
+  return updatedMovies;
+}
+
+turnHoursToMinutes(movies)
 
 // Get the average of all rates with 2 decimals 
 
