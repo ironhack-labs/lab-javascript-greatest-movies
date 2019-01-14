64050e7345e7e35e15a8ee1271476ae29304caf8
Jorge Silva <jorgeaferreirasilva@gmail.com>
2019-01-14 20:43:25 -0200

    done

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,6 +1,25 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
-
+// Turn duration of the movies from hours to minutes 
+function turnHoursToMinutes(arr) {
+    let newArr = [];
+    arr.forEach(function(item){
+      item.duration = parseInt(item.duration.substring(0, 1)) * 60 + parseInt(item.duration.substring(3, 5));
+      newArr.push(item);
+    });
+    return newArr;
+  }
+  console.log(turnHoursToMinutes(movies));
+  // Get the average of all rates with 2 decimals 
+  function ratesAverage(arr) {
+    let newArr = [];
+    let count = 0;
+    arr.forEach(function(item){
+      count += parseInt(item.rate);
+      newArr.push(item);
+    });
+    return count / newArr.length;
+  }
 
 // Get the average of all rates with 2 decimals 
 
