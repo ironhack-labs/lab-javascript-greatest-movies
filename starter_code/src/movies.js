cc2fd68e3524351fbea56ac7d7c461c472e3ab74
Matheus Araujo <abarbierijunior@gmail.com>
2019-01-19 23:07:47 -0200

    all-times-movies

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,20 +1,76 @@
-/* eslint no-restricted-globals: 'off' */
-// Turn duration of the movies from hours to minutes 
+var movies = [
+  {
+    title: 'The Shawshank Redemption',
+    year: '1994',
+    director: 'Frank Darabont',
+    duration: '2h 22min',
+    genre: ['Crime', 'Drama'],
+    rate: '9.3'
+  },
+  {
+    title: 'The Godfather',
+    year: '1972',
+    director: 'Francis Ford Coppola',
+    duration: '2h 55min',
+    genre: ['Crime', 'Drama'],
+    rate: '9.2'
+  }];
 
+function turnHoursToMinutes(arr) {
+  let arrMovies = movies[0].rate;
+}
 
-// Get the average of all rates with 2 decimals 
+function ratesAverage(array) {
+  let sumRate = array.reduce(function (sum, movie) {
 
+    return sum += parseFloat(movie.rate);
+  }, 0);
 
-// Get the average of Drama Movies
+  let averageRate = sumRate / array.length;
+  return averageRate;
+}
 
+function dramaMoviesRate(array) {
+  let movieDrama = array.filter(function (movie) {
 
-// Order by time duration, in growing order
+    return movie.genre.includes('Drama');
+  });
 
+  let sumDram = movieDrama.reduce(function (sum, movie) {
+    return sum += movie.rate;
+  });
 
-// How many movies did STEVEN SPIELBERG
+  let averageDrama = sumDram / movieDrama.length;
+  return averageDrama;
+}
 
+function howManyMovies(array) {
 
-// Order by title and print the first 20 titles
+  let movies = array.filter(function (movies) {
+    return (movie.director.includes('Steven Spielberg') && movie.genre.includes('Drama'));
+  });
+}
 
+function orderAlphabetically(array) {
 
-// Best yearly rate average
+  let organizaMovies = array.sort(function (a, b) {
+    if (a > b) {
+      return 1;
+    }
+    if (a < b) {
+      return -1;
+    }
+    else {
+      return 0;
+    }
+  });
+
+  let titles = [];
+  let bestFilms = organizaMovies.slice(0, 20);
+
+  titles.forEach(function (movie) {
+    bestFilms.push(movie.title);
+  });
+
+  return titles;
+}
