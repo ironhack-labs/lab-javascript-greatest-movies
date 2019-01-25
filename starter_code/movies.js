ac86569a8aa631161946825fc182944c6b1fa156
cameron-chromiak <cam.chromiak@hotmail.com>
2019-01-25 12:24:03 -0500

    -stopped working on 1 -worked on two got to converting all to int but need to calc average

--- starter_code/movies.js
+++ starter_code/movies.js
@@ -6,7 +6,7 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes
 
-//movieLength so original objects are not altered
+// movieLength so original objects are not altered
 movieLength = []
 
 function stripText(){
@@ -19,24 +19,76 @@ function stripText(){
     movieLength[i] = movieLength[i].replace(alphaCharReg,'')
   }
 
-
-  console.log(movieLength)
+  console.log(movieLength.sort())
   for (let item = 0; item<movieLength.length;item+=1){
-    // movieLength[item].split(" ")
-    movieLength[item][1] = Number(movieLength[item].split(' ')[1])
-    movieLength[item] = Number(movieLength[item].split(' ')[1])
+    movieLength[item] = movieLength[item].replace(' ','')
 
+    let  hours = movieLength[item][0]
 
-  }
+      if (movieLength[item].length == 1){
+        movieLength[item] = Number(movieLength[item])
+        // change to minutes
+        movieLength[item] *= 60
 
-  console.log(movieLength)
+      }
+      else if (movieLength[item].length == 2){
+        movieLength[item] = Number(movieLength[item])
+        // if movieLength[item].length
+      }
+      else if (movieLength[item].length == 3){
+        movieLength[item] = Number(movieLength[item])
+        movieLength[item][1] *= 60
+      }
+      else if (movieLength[item].length == 3){
+        movieLength[item] = Number(movieLength[item])
+      }
+    }
 }
-
+  console.log(movieLength.sort())
 
 stripText()
 
 // Get the average of all rates with 2 decimals
+// avgRating = []
+//
+// function pushRatings(){
+//   for (let i =0; i<movies.length;i+=1){
+//     avgRating.push((movies[i].rate))
+//     }
+//     splitRating(avgRating)
+//   }
+//
+//   function splitRating(array){
+//     for (let i =0; i<array.length;i+=1){
+//       array[i] = array[i].replace('.',' ')
+//     }
+//     convertToInt(array)
+//   }
+//
+//
+// function convertToInt(array){
+//   firstNum = []
+//   secNum = []
+//   for (let i =0; i<array.length;i+=1){
+//       array[i] = array[i].split(' ')
+//       array[i][0] = Number(array[i][0])
+//       firstNum.push(array[i][0])
+//       array[i][1] = Number(array[i][1])
+//       secNum.push(array[i][1])
+//   }
+//   console.log(firstNum.reduce((a, b) => a + b, 0)/250);
+//   console.log(secNum.reduce((a, b) => a + b, 0)/250);
+//
+//
+//
+// }
+//
+//
+// pushRatings()
 
+
+
+// console.log(avgRating)
 
 // Get the average of Drama Movies
 
