b97e26376b90e1d3a79bc9b7e04de8dac2361226
Guillermo X. Aiken <guiller@Guillers-MacBook-Pro.local>
2019-01-25 15:40:33 -0500

    Second commit

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -29,6 +29,7 @@ const ratesAverage = (arr) => {
   let totalAverage = arr.reduce((acc, movie) => acc + parseFloat(movie.rate),0);
   totalAverage = Math.round((totalAverage/arr.length) * 100) / 100;
   console.log(totalAverage);
+  return totalAverage;
 };
 // Get the average of Drama Movies
 const dramaMoviesRate = (arr) => {
@@ -57,7 +58,49 @@ const orderAlphabetically = (arr) => {
 };
 
 // Best yearly rate average
+/*
 const bestYearlyRate = (arr) => {
   let bestArray = arr.sort((a,b) => (a.rate > b.rate) ? 1 : ((b.rate > a.rate) ? -1 : 0));
   console.log(bestArray.reverse());
 };
+*/
+/*
+const bestYearlyRate = (arr) => {
+  let uniqueYears = [...new Set(arr.map(movie => movie.year).sort())];
+  var yearRepo = [];
+  for (var i = 0; i < uniqueYears.length; i++)
+    yearRepo.push(arr.filter(movie => movie.year === uniqueYears[i]));
+  console.log(yearRepo);
+
+};
+*/
+
+//Pull all unique years
+let uniqueYears = [...new Set(movies.map(movie => movie.year))];
+//Sort all years
+uniqueYears.sort((a,b) => {return a-b});
+//Create year map
+let yearMap = {};
+//Create keys for yearMap for each unique year
+uniqueYears.forEach(element => {
+  yearMap[element] = [];
+});
+//Make a copy of the keys of yearMap to be used for mapping average ratings to years
+let yearRatings = Object.assign({}, yearMap)
+//Map each movie to the year key in which it was released
+movies.forEach(element => {
+  yearMap[element.year].push(element);
+});
+
+//Iterate through the yearMap and generate an average rating for each year. Send this average to the correct year key in yearRatings
+for (var y in yearMap){
+    yearRatings[y] = ratesAverage(yearMap[y]);
+}
+//Print yearly ratings
+//console.log(yearRatings);
+
+var result = Object.keys(yearRatings).map(function(key) {
+  return [yearRatings[key], Number(key),];
+});
+let bestYear = result.sort().splice((result.length - 1), 1);
+console.log(`The best year was ${bestYear[0][1]} with an average of ${bestYear[0][0]}`);
