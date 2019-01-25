016bb06cad064eba6dfd5f2499a8b5f66b038d67
cameron-chromiak <cam.chromiak@hotmail.com>
2019-01-25 10:18:36 -0500

    -added to time convert section

--- starter_code/movies.js
+++ starter_code/movies.js
@@ -1,15 +1,35 @@
+// All time Movies Lab
+// Cameron Chromiak
+// Oscar Leon
+
+
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes
-movieLength = []
-for (let i =0; i<movies.length;i+=1){
-  movieLength.push((movies[i].duration))
 
+//movieLength so original objects are not altered
+movieLength = []
+
+function convertStringToInt(array){
+  array.forEach(function(item){
+    item.parseInt(item,10)
+  })
 }
-let alphaCharReg = /[a-z]/ig;
-for (let i =0; i<movieLength.length;i+=1){
-  movieLength[i] = movieLength[i].replace(alphaCharReg,'')
+
+function stripText(){
+  for (let i =0; i<movies.length;i+=1){
+    movieLength.push((movies[i].duration))
+
+  }
+  let alphaCharReg = /[a-z]/ig;
+  for (let i =0; i<movieLength.length;i+=1){
+    movieLength[i] = movieLength[i].replace(alphaCharReg,'')
+  }
+  convertedNumber = convertStringToInt(movieLength)
+  console.log(movieLength.sort())
 }
-console.log(movieLength)
+
+
+stripText()
 
 // Get the average of all rates with 2 decimals
 
