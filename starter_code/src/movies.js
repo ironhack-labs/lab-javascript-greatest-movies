79a1aab2d4f535ed82c81ac67343ecae07ff55e8
Antonio Méndez <antonio.mendez.martin89@gmail.com>
2019-02-02 15:02:20 +0100

    45specs 28failures

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -2,6 +2,21 @@
 // Turn duration of the movies from hours to minutes 
 
 
+function turnHoursToMinutes(movies){
+  return movies.map(function(movie, i){  
+    var x = movie.duration.split("h");
+  if(movie.duration.includes("h")){
+    return {duration:parseInt(x)*60+parseInt(x[1]) }
+  }else{
+   return  {duration:parseInt(x)}
+  }
+  
+})
+}
+turnHoursToMinutes()
+
+
+
 // Get the average of all rates with 2 decimals 
 
 
