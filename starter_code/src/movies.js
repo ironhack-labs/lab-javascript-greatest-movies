37f5e68e269e2aa91b68e0b749fb1d7a2b21c0f9
joseocamar <joseocamar@gmail.com>
2019-02-08 09:41:32 +0100

    par de testeos

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,12 +1,67 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
 
+function turnHoursToMinutes(obj, key) {
+
+  return obj.map(function (elm){
+
+  } 
+  )}
+
+
+/*
+----------------------------------------------------------------------------------------
+
+Estuve haciendo testeos para hacer un cambio de strings a num para su posterior inclusión en el array.
+
+----------------------------------------------------------------------------------------
+
+ var string = "2 hours 10 mins"
+
+
+var stringtoarray = string.split('hours')
+
+
+function hourstominutes() {
+  var hours
+  var mins
+  
+  if (string.indexOf('hours') !== -1){
+    stringtoarray = string.split('hours')
+    hours = parseFloat(stringtoarray[0]) * 60
+      if (string.indexOf('m') !== -1) {
+        mins = parseFloat(stringtoarray[1])
+        
+    } else {
+      mins = 0
+    }
+  } else if (string.indexOf('m') !== -1) {
+    mins = parseFloat(stringtoarray[1])
+  } else {
+    mins = 0
+  
+  }
+
+  return hours + mins
+
+}
+
+hourstominutes()
+
+*/
+  
+
+  
 
 // Get the average of all rates with 2 decimals 
 
+function ratesAverage() {}
 
 // Get the average of Drama Movies
+function dramaMoviesRate() {
 
+  return 
+}
 
 // Order by time duration, in growing order
 
@@ -18,3 +73,9 @@
 
 
 // Best yearly rate average
+
+
+
+// return Object.assign({}, movies.duration, {duration: movies.map(function (elm){    })} 
+
+//  .parseInt(obj.key)//
