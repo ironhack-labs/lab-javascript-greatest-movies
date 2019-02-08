c54f1c465ba7c86fd65df67f6c17ee3139f4a9d1
Cristina Blanco <cristinablanco@MacBook-Pro-de-Cristina.local>
2019-02-08 09:35:28 +0100

    done

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,8 +1,32 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
 
+function turnHoursToMinutes(movies){
+  console.log('Entro a la función y recibo')
+  console.log(movies);
+
+  var resultado = movies.map(function(movie) {
+    console.log('estoy iterando en');
+    console.log(movie.duration)
+  })
+  // var resultados = movies.map(function(elm){
+
+  //   if ()
+  //   return object.assign;
+  // })
+  // return []
+}
+
 
 // Get the average of all rates with 2 decimals 
+
+// function ratesAverage(myArray){
+//   var sum = 0 
+//   movies.map(duration.length
+
+//   return sum
+  
+// })
 
 
 // Get the average of Drama Movies
