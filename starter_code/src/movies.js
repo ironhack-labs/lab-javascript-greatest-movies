f9e54eafea52a242591ad64e6ea9e768c1e6bb28
Carlos Martínez Crespo <carlos.martinez.crespo@gmail.com>
2019-02-08 09:26:31 +0100

    almost done

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,12 +1,52 @@
 /* eslint no-restricted-globals: 'off' */
-// Turn duration of the movies from hours to minutes 
+// Turn duration of the movies from hours to minutes
 
+function separateDuration(durationString) {
+    return durationString.split(" ")
+}
 
-// Get the average of all rates with 2 decimals 
+function oneArgument(duration) {
+    return duration[0].indexOf("h") === -1 ?
+        +duration[0].replace("min", " ") : +duration[0].replace("h", "") * 60
+}
 
+function twoArguments(duration) {
+    return +duration[0].replace("h", "") * 60 + +duration[1].replace("min", " ")
+}
+
+function durationInMinutes(durationArray) {
+    return durationArray.length === 1 ? oneArgument(durationArray) : twoArguments(durationArray)
+}
+
+function newObject(movie, minutes) {
+    return Object.assign({}, movie, { duration: minutes })
+
+}
+
+function turnHoursToMinutes(movies) {
+    var newMovies = []
+    movies.map(function(movie) {
+        newMovies.push(newObject(movie, durationInMinutes(separateDuration(movie.duration))))
+    })
+    return newMovies
+}
+
+// Get the average of all rates with 2 decimals
+function ratesAverage(arr) {
+    var sum = arr.reduce((acc, val) => {
+        return acc + Number(val.rate)
+    }, 0)
+    return sum / arr.length
+}
 
 // Get the average of Drama Movies
+function dramaMoviesRate(arr) {
+    var sum = arr.filter(movie => movie.genre = 'Drama').reduce((acc, val) => {
+        return acc + Number(val.rate)
+    }, 0)
+    return sum / arr.length, 2
 
+}
 
 // Order by time duration, in growing order
 
