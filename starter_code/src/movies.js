622a8755c2fd5a34c7bb2210f263487d453c14ff
Tomas Yaya <tomasyaya@Tomass-MacBook-Pro.local>
2019-02-12 21:19:51 +0100

    new commit

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,11 +1,42 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
-
+  function turnHoursToMinutes(arr){
+    arr.map(function(val){
+      let split = val.duration.split('');
+      let filterArr = split.filter(function(val){
+        let parse = parseInt(val);
+        return typeof parse === "number"
+      });
+      let min = filterArr[0] * 60;
+      let finalMin = min.join('')
+      
+      return val.duration = finalMin;
+    })
+  }
 
 // Get the average of all rates with 2 decimals 
 
+function ratesAverage(arr){
+  var sum = arr.map(function(val){
+    return val.rate;
+  }).reduce(function(acc, val){
+    return acc + val;
+  });
 
+  return sum / arr.length;
+}
 // Get the average of Drama Movies
+function dramaMoviesRate(arr){
+  var sum = arr.filter(function(val, i){
+    return val.genre.includes("Drama")
+  }).map(function(val){
+    return val.rate;
+  }).reduce(function(acc, val){
+    return acc + val;
+  });
+
+  return sum / arr.length;
+}
 
 
 // Order by time duration, in growing order
