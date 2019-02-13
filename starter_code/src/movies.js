e63021af200fab98550b260a44bf42ab067a3664
Miguel Sanchez <migsandia@gmail.com>
2019-02-13 08:42:24 +0100

    primer ejercicio realizado

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,6 +1,27 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
 
+function turnHoursToMinutes(arrMovies){
+  
+  let newArrMovies = arrMovies;
+  
+  let finalArrMovies = newArrMovies.map(oneMovie => {
+    let movie = Object.assign({},oneMovie);
+    var hourMinutes = movie.duration.match(/\d+/g).map(Number);
+    if(hourMinutes.length>1){
+      movie.duration=(hourMinutes[0]*60)+hourMinutes[1];
+    }else if(hourMinutes.length==1){
+      if(hourMinutes>9){
+        movie.duration=hourMinutes[0];
+      }else{
+        movie.duration=hourMinutes[0]*60;
+      }
+    }
+    return movie;
+  });
+  return finalArrMovies;
+  
+}
 
 // Get the average of all rates with 2 decimals 
 
@@ -17,4 +38,4 @@
 // Order by title and print the first 20 titles
 
 
-// Best yearly rate average
+// Best yearly rate... 
