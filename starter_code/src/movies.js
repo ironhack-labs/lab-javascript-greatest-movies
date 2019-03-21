a0dd523d9d7e0bc488bfffc75adceeafb0171ae1
alex fox <to.alexfox@gmail.com>
2019-03-21 17:50:17 +0100

    completed exercises 2 & 3 w/ Mathis

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -3,11 +3,48 @@
 
 
 // Get the average of all rates with 2 decimals 
+function ratesAverage(array) {
+	var sumRate = null;
 
+	array.forEach(movie => {
+		var ratenumber = Number(movie.rate);
+		sumRate += ratenumber;
+	});
+
+	if (isNaN(sumRate) === false){ 
+  	return;
+	}
+  return Math.round(sumRate/array.length*100)/100;
+}
 
 // Get the average of Drama Movies
 
+function isDrama(string) {
+		if (string === "Drama") {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
+function containsDrama(array){
+	var yesContainsDrama = null;
+	array.forEach(element => {
+		if (isDrama(element) === true) { 
+			yesContainsDrama = true;
+		} 
+	})
+	return yesContainsDrama;
+};
+
+function dramaMoviesRate(array) {
+	var dramaMovies = array.filter(movie => {
+		return containsDrama(movie.genre) === true;
+		});
+	return ratesAverage(dramaMovies);
+
+}
+	
 // Order by time duration, in growing order
 
 
