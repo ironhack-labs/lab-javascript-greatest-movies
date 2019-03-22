f7b36aed19a0108869e9f7e78e355bd13af8a28f
Fernando Lopez <fnandolopezsanchez@gmail.com>
2019-03-22 09:06:35 +0100

    hechos iteraciones 2 y 3

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -4,8 +4,31 @@
 
 // Get the average of all rates with 2 decimals 
 
+function ratesAverage (arr) {
+  
+  const sumRates = arr.reduce((acc, current) => {
+    if (current.rate == "") {
+      current.rate = 0;
+    }
+    
+  return acc + parseFloat(current.rate); }, 0);
+    
+  return Math.round((sumRates/arr.length)*100)/100;
+  }
 
 // Get the average of Drama Movies
+
+function dramaMoviesRate (arr) {
+
+  var dramaList = arr.filter(function (movie){
+    for (var i=0; i < movie.genre.length; i++ ){
+      return movie.genre[i] == "Drama";}
+  } );
+  if (dramaList.length == 0) {
+    return;
+  }  
+  return ratesAverage(dramaList);
+}
 
 
 // Order by time duration, in growing order
