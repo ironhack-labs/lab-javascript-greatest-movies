7e5c2fb0877884a941e74e07c2ca98260de46327
Ruben Polo <rpoloredondo@outlook.es>
2019-03-22 09:21:00 +0100

    done

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,9 +1,14 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
+function turnHoursToMinutes() {
+  
 
+} 
 
 // Get the average of all rates with 2 decimals 
-
+const ratesAverage = (arr) => movies.reduce((acc, cValue) => {
+   return (acc + parseFloat(cValue.rate)) / arr.length;  
+}, 0)
 
 // Get the average of Drama Movies
 
