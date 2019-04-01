d56d7fd6ceebc3db1779227b6062f06245078f92
gustavvopenna <gustavo.pema@gmail.com>
2019-04-01 13:27:49 -0600

    Done without bonus

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,27 +1,40 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
 
-function turnHoursToMinutes(arr){
-  let newArr = arr.map(function(original){
-    let val = {...original}
-   if(val.duration.length > 3) {
+// function turnHoursToMinutes(arr){
+//   let newArr = arr.map(function(original){
+//     let val = {...original}
+//    if(val.duration.length > 3) {
 
-     let hrArr = val.duration.split('');
-     let hr = parseInt(hrArr[0]) * 60;
-     let minArr = hrArr.join('').split(' ');
-     let min = parseInt(minArr[1]);
-     let sum = hr + min;
-     val.duration = sum;
-     console.log(val)
-     return val;
+//      let hrArr = val.duration.split('');
+//      let hr = parseInt(hrArr[0]) * 60;
+//      let minArr = hrArr.join('').split(' ');
+//      let min = parseInt(minArr[1]);
+//      let sum = hr + min;
+//      val.duration = sum;
+//      console.log(val)
+//      return val;
 
-   } else {
-       return val
-   }
-  });
+//    } else {
+//        return val
+//    }
+//   });
+//   return newArr
+//  }
 
-  return newArr
- }
+ function turnHoursToMinutes(movies){
+  return movies.map( original => {
+    let movie = {...original}
+    if (!movie.duration) movie.duration = ""
+    let min = 0
+    movie.duration.split(" ").forEach( el => {
+      if( el.includes('h')) min += parseInt(el) * 60
+      if ( el.includes('min')) min += parseInt(el)
+    })
+    movie.duration = min
+    return movie
+  })
+}
 
 // Get the average of all rates with 2 decimals 
 
@@ -39,26 +52,35 @@ const ratesAverage = (arr) => {
 
 const dramaMoviesRate = (arr) => {
   let dramaMovies = arr.filter(movie => (movie.genre.includes('Drama')))
+  if ( dramaMovies.length === 0 ) return undefined
   let averageDramaMovies = dramaMovies.map(movie => movie.rate)
   let reduceDramaMovies = averageDramaMovies.reduce((acc, rate) => {
     return acc + Number(rate)
   }, 0)
-  return reduceDramaMovies / dramaMovies.length
+  let rate = reduceDramaMovies / dramaMovies.length 
+  return Number(rate.toFixed(2))
 }
 
 // Order by time duration, in growing order
 
 const orderByDuration = (arr) => {
-  let newArr = turnHoursToMinutes(arr)
-  let arrSort = newArr.sort((a, b) => (a.duration > b.duration) ? 1 : ((b.duration > a.duration) ? -1 : 0))
-  return arrSort
+  arr.sort((a, b) => (a.duration !== b.duration) ? (a.duration - b.duration) : a.title.localeCompare(b.title))
+  return arr
 }
 
 // How many movies did STEVEN SPIELBERG
 
+const howManyMovies = (arr) => {
+  if (arr.length === 0) return undefined
+  let filterArr = arr.filter(movie => (movie.genre.includes('Drama')) && (movie.director === 'Steven Spielberg'))
+  return `Steven Spielberg directed ${filterArr.length} drama movies!`
+}
 
 // Order by title and print the first 20 titles
 
+const orderAlphabetically = arr => {
+  return arr.map((el) => el.title).sort().slice(0, 20)
+}
 
 // Best yearly rate average
 
