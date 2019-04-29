9a9352a91d19f6325aa01e4f8ad2ec0fb4e1a6d0
LadyCrispy <cvg.94@hotmail.com>
2019-04-29 21:47:53 +0200

    Drama movies + Steven

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -14,13 +14,25 @@ console.log(ratesAverage(movies))
 
 // Get the average of Drama Movies
 
+function dramaMoviesRate(movies){
+    const dramaRate = movies.filter(movie => movie.genre.indexOf("Drama") !=-1) //si es TRUE se añade a dramaRate
+   
+    return ratesAverage(dramaRate)
+	   
+}
+console.log(dramaMoviesRate(movies))
+
 
 // Order by time duration, in growing order
 
 
 // How many movies did STEVEN SPIELBERG
-
-
+function howManyMovies(movies){
+    const stevenMovies = movies.filter(movie => movie.director.indexOf('Steven Spielberg')!=-1)
+    return stevenMovies
+	   
+}
+console.log(howManyMovies(movies))
 // Order by title and print the first 20 titles
 
 
