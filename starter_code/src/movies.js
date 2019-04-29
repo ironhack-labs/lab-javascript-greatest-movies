e7ccb7020c53b50b8fa8b4a3b02bde6ab79b09fb
Lucia Astray <lu@MacBook-Pro-de-Lucia.local>
2019-04-29 20:00:12 +0200

    Iteration 1: Time Format & Iteration 2: All rates average

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,20 +1,82 @@
 /* eslint no-restricted-globals: 'off' */
-// Turn duration of the movies from hours to minutes 
+// Turn duration of the movies from hours to minutes
 
+const turnHoursToMinutes = movies => {
+  return movies.map(movie => {
+    let movieCopy = { ...movie };
+    let duration = 0;
+    //console.log(movieCopy.duration);
+    //let hoursPorMinutos = Number(movieCopy.duration.charAt(0) * 60);
+    //let minutos = "";
+    /* by sachin
+if(movieCopy.duration.indexof("min") == -1)
+const horas = number(movieCopy.duration)
+elseif(movieCopy.duration.indexof("h")== -1)
+const min =number(movieCopy.duration) */
+    //console.log(movieCopy.duration);
+    if (movieCopy.duration.indexOf("min") == -1) {
+      const hours = parseInt(movieCopy.duration.charAt(0)) * 60;
+      //console.log(hours);
+      duration = hours;
+    } else if (movieCopy.duration.indexOf("h") == -1) {
+      const onlyMinutes = parseInt(movieCopy.duration);
+      //console.log(onlyMinutes);
+      duration = onlyMinutes;
+    } else {
+      const hours = parseInt(movieCopy.duration.charAt(0)) * 60;
+      const minutes = parseInt(
+        movieCopy.duration.substring(2, movieCopy.duration.length)
+      );
+      duration = hours + minutes;
+      //console.log(minutes);
+      //console.log(hours);
+    }
+    movieCopy.duration = duration;
+    return movieCopy;
+  });
+  //} else if (movieCopy.duration[indexMinutos - 2] === " ") {
+  //  minutos = movieCopy.duration[indexMinutos - 1];
+  // console.log("Estoy en el primero");
+  /* } else if (movieCopy.duration[indexMinutos - 2] === "h") {
+    minutos = movieCopy.duration[indexMinutos - 1];
+    console.log("Estoy en el segundo");
+  } else {
+    minutos =
+      movieCopy.duration[indexMinutos - 2] +
+      movieCopy.duration[indexMinutos - 1];
+    console.log("Estoy en el tercero"); */
+  //}else if (movieCopy.duration[indexMinutes])
 
-// Get the average of all rates with 2 decimals 
+  //console.log(minutos);
 
+  //console.log(movieCopy);
+  //  Number(movieCopy.duration.substring(3, 5));
+  /* const movieTurnHoursToMinutes = { ...moviecopy };
+  console.log(movieTurnHoursToMinutes);
+  return (movieTurnHoursToMinutes.duration = hoursPorMinutos + Number(minutos)); */
+};
+
+//console.log(turnHoursToMinutes);
+
+// Get the average of all rates with 2 decimals
+const ratesAverage = movies => {
+  let totalRate = movies.reduce((sum, elem) => {
+    return sum + parseFloat(elem.rate);
+  }, 0);
+  console.log(totalRate);
+  const averageRate = totalRate / movies.length;
+
+  return parseFloat(averageRate.toFixed(2));
+};
+
+// console.log(`the average is ${averageRate}`);
 
 // Get the average of Drama Movies
 
-
 // Order by time duration, in growing order
-
 
 // How many movies did STEVEN SPIELBERG
 
-
 // Order by title and print the first 20 titles
-
 
 // Best yearly rate average
