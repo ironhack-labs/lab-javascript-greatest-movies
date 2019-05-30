26e10ed4e8fbe9a096c209e9ad6a4acf64e9b813
elinlarsen <elin_larsen1@hotmail.fr>
2019-05-30 14:21:43 +0200

    convert into minutes done

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,6 +1,18 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
 
+function turnHoursToMinutes(movies) {
+    copie=Object.assign(movies);
+    copie.map (movie => {
+    num=movie.duration.match(/\d+/g).map(Number)
+    if (num.length==1 && num[0]<=3){movie.duration=num[0]*60}
+    if (num.length==1 && num[0]>3){movie.duration=num[0]}
+    else if (num.length>1){movie.duration=num[0]*60+num[1]}
+    }
+    ) ;
+    return copie
+}
+
 
 // Get the average of all rates with 2 decimals 
 
