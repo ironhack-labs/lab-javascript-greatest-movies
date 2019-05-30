cb78231b5f263ea6d8166b518475591d0919774b
inga Bacher <ingalila@hotmail.fr>
2019-05-30 18:55:33 +0200

    1st step

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,20 +1,59 @@
 /* eslint no-restricted-globals: 'off' */
-// Turn duration of the movies from hours to minutes 
+// Turn duration of the movies from hours to minutes
 
+// Get the average of all rates with 2 decimals
 
-// Get the average of all rates with 2 decimals 
-
+function ratesAverage(movies) {
+  var total = movies.reduce((acc, currentMovie) => {
+    return (acc += Number(currentMovie.rate));
+  }, 0);
+  console.log(total);
+  return total / movies.length;
+}
 
 // Get the average of Drama Movies
-
+function dramaMoviesRate(movies) {
+  var dramaMovies = movies.filter(movie => {
+    return movie.genre.includes("Drama");
+  });
+  return ratesAverage(dramaMovies);
+}
 
 // Order by time duration, in growing order
 
+//function orderByDuration(movies){
+//  return movies.duration.sort();
 
 // How many movies did STEVEN SPIELBERG
+function howManyMovies(movies) {
+    var spielbergMovies = movies.filter((movie) => {
+        return movie.director.includes("Steven Spielberg");
+      }
+      
+
+      if (spielbergMovies.length == 0){ 
+          return "Steven Spielberg directed 0 drama movies!";
+    } else if (spielbergMovies.length == 1){
+        return "Steven Spielberg directed 1 drama movies!";
+    }
+    else (spielbergMovies.length == 4){
+        return "Steven Spielberg directed 4 drama movies!";
+    }
+    });
+    //return String(spielbergMovies.length);
+
+
+      
+        
+      
 
 
 // Order by title and print the first 20 titles
-
+function orderAlphabetically(movies) {
+    movies.title.sort();
+    return movies;
+}
 
 // Best yearly rate average
+
+
