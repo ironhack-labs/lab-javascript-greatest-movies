c414bbf8f01c66f2246c0a22bad0860d777f7f82
Majdi Chargui <mejdichergui@gmail.com>
2019-05-31 09:15:28 +0100

    fc

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,20 +1,41 @@
 /* eslint no-restricted-globals: 'off' */
-// Turn duration of the movies from hours to minutes 
+// Turn duration of the movies from hours to minutes
 
+// Get the average of all rates with 2 decimals
+// function ratesAverage(movies) {
+//   console.log("hey");
+//   var count = 0;
+//   var avg = 0;
+//   movies.forEach(movie => {
+//     count = Number(movie.rate) + count;
+//   });
+//   console.log(count);
+//   console.log(avg);
+//   return (avg = count / movie.length);
+// }
+function ratesAverage(movies) {
+  console.log("hey");
+  var count = 0;
+  movies.forEach(movie => {
+    count = Number(movie.rate) + count;
+  });
+  console.log(count);
+  var avg = count / movies.length;
+  console.log(avg);
+  return avg;
+}
 
-// Get the average of all rates with 2 decimals 
-
+const average = (movies.reduce = (acc, movie) => {
+  acc + movie.rate;
+  return acc;
+});
 
 // Get the average of Drama Movies
 
-
 // Order by time duration, in growing order
-
 
 // How many movies did STEVEN SPIELBERG
 
-
 // Order by title and print the first 20 titles
-
 
 // Best yearly rate average
