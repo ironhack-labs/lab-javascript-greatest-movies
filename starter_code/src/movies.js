6223eb43fa8fc8c7bc50c86c4a23e5fd8428cbab
leaandrea <leaandreapaumier@gmail.com>
2019-06-02 21:45:28 +0200

    didn't do the last one yet.

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,20 +1,84 @@
 /* eslint no-restricted-globals: 'off' */
-// Turn duration of the movies from hours to minutes 
+// Turn duration of the movies from hours to minutes
 
+// Get the average of all rates with 2 decimals
+function ratesAverage(movies) {
+  const totalRates = movies.reduce((acc, movie, i) => {
+    acc += parseFloat(movie.rate);
+    return acc;
+  }, 0);
+  return totalRates / movies.length;
+}
 
-// Get the average of all rates with 2 decimals 
-
-
-// Get the average of Drama Movies
-
+// drama movies rate
+function dramaMoviesRate(movies) {
+  var onlyDrama = movies.filter(function (movie) {
+    return movie.genre.includes("Drama");
+  });
+  var sum = onlyDrama.reduce(function (acc, movie) {
+    return Number(acc + movie.rate);
+  }, 0);
+  if (onlyDrama.length == 0) {
+    return undefined;
+  }
+  sum = sum / onlyDrama.length;
+  return Number(sum.toFixed(2));
+}
 
 // Order by time duration, in growing order
-
+function orderByDuration(movies) {
+  var longestMovies = movies.sort(function (a, b) {
+    if (a.duration === b.duration) {
+      if (a.title < b.title) {
+        return -1;
+      }
+      if (a.title > b.title) {
+        return 1;
+      }
+      return 0;
+    }
+    return a.duration - b.duration;
+  });
+  return longestMovies;
+}
 
 // How many movies did STEVEN SPIELBERG
+function howManyMovies(sumMovies) {
+  if (sumMovies.length == 0) {
+    return undefined;
+  }
 
+  var onlySteven = sumMovies.filter(function (movie) {
+    return movie.director.includes("Steven Spielberg");
+  });
+  const onlyDramaSteven = onlySteven.filter(function (movie) {
+    return movie.genre.includes("Drama");
+  });
+
+  return (
+    "Steven Spielberg directed " + onlyDramaSteven.length + " drama movies!"
+  );
+}
 
 // Order by title and print the first 20 titles
+function orderAlphabetically(movies) {
+  var onlyTitles = movies.map(a => {
+    return a.title;
+  });
+
+  var orderedOnlyTitles = onlyTitles.sort();
+  return orderedOnlyTitles.slice(0, 20);
+}
+
 
 
 // Best yearly rate average
+
+function bestYearAvg(movies) {
+  var orderedYears = movies.sort(function (a, b) {
+    return a.year - b.year;
+  });
+  return orderedYears;
+
+}
+// didn't finish this one :(, will try again tomorrow ! 
--- starter_code/tests/MoviesSpec.js
+++ starter_code/tests/MoviesSpec.js
@@ -1,6 +1,6 @@
 /* eslint no-undef: "off" */
 /* eslint jasmine/no-spec-dupes: "off" */
-
+/*
 describe('Turn duration of the movies - turnHoursToMinutes', function () {
   it('Defines turnHoursToMinutes', function () {
     expect(typeof turnHoursToMinutes).toBe('function');
@@ -42,7 +42,7 @@ describe('Turn duration of the movies - turnHoursToMinutes', function () {
     expect(turnHoursToMinutes(movieTry)[0].duration).toBe(54);
   });
 });
-
+*/
 describe('Get the average rate - ratesAverage', function () {
   it('Defines ratesAverage', function () {
     expect(typeof ratesAverage).toBe('function');
@@ -53,15 +53,33 @@ describe('Get the average rate - ratesAverage', function () {
   });
 
   it('You should return the average rate of 2 movies with rate 8 each!', function () {
-    expect(ratesAverage([{ rate: 8 }, { rate: 8 }])).toBe(8);
+    expect(ratesAverage([{
+      rate: 8
+    }, {
+      rate: 8
+    }])).toBe(8);
   });
 
   it('It should be rounded to 2 decimals digits', function () {
-    expect(ratesAverage([{ rate: 8 }, { rate: 9 }, { rate: 9 }, { rate: 7 }])).toBeCloseTo(8.25, 2);
+    expect(ratesAverage([{
+      rate: 8
+    }, {
+      rate: 9
+    }, {
+      rate: 9
+    }, {
+      rate: 7
+    }])).toBeCloseTo(8.25, 2);
   });
 
   it('It should be rounded to nearest hundredths ', function () {
-    expect(ratesAverage([{ rate: 8 }, { rate: 9 }, { rate: 9 }])).toBeCloseTo(8.67, 2);
+    expect(ratesAverage([{
+      rate: 8
+    }, {
+      rate: 9
+    }, {
+      rate: 9
+    }])).toBeCloseTo(8.67, 2);
   });
 });
 
@@ -75,27 +93,72 @@ describe('Average rate of Drama Movies - dramaMoviesRate', function () {
   });
 
   it('Return the rate of a single element array!', function () {
-    expect(dramaMoviesRate([{ genre: ['Drama'], rate: 8 }])).toBe(8);
+    expect(dramaMoviesRate([{
+      genre: ['Drama'],
+      rate: 8
+    }])).toBe(8);
   });
 
   it('Return Average even if one of the movies does not have rate!', function () {
-    expect(dramaMoviesRate([{ genre: ['Drama'], rate: 8 }, { genre: ['Drama'], rate: '' }])).toBe(4);
+    expect(dramaMoviesRate([{
+      genre: ['Drama'],
+      rate: 8
+    }, {
+      genre: ['Drama'],
+      rate: ''
+    }])).toBe(4);
   });
 
   it('It should return the average of the array!', function () {
-    expect(dramaMoviesRate([{ genre: ['Drama'], rate: 8 }, { genre: ['Drama'], rate: 9 }, { genre: ['Drama'], rate: 7 }])).toBe(8);
+    expect(dramaMoviesRate([{
+      genre: ['Drama'],
+      rate: 8
+    }, {
+      genre: ['Drama'],
+      rate: 9
+    }, {
+      genre: ['Drama'],
+      rate: 7
+    }])).toBe(8);
   });
 
   it('It should return the average of the array, float!', function () {
-    expect(dramaMoviesRate([{ genre: ['Drama'], rate: 9 }, { genre: ['Drama'], rate: 9 }, { genre: ['Drama'], rate: 7 }])).toBe(8.33);
+    expect(dramaMoviesRate([{
+      genre: ['Drama'],
+      rate: 9
+    }, {
+      genre: ['Drama'],
+      rate: 9
+    }, {
+      genre: ['Drama'],
+      rate: 7
+    }])).toBe(8.33);
   });
 
   it('Only Drama Movies! You should return the average of Drama movies only!', function () {
-    expect(dramaMoviesRate([{ genre: ['Drama'], rate: 8 }, { genre: ['Romance'], rate: 9 }, { genre: ['Drama'], rate: 7 }])).toBe(7.5);
+    expect(dramaMoviesRate([{
+      genre: ['Drama'],
+      rate: 8
+    }, {
+      genre: ['Romance'],
+      rate: 9
+    }, {
+      genre: ['Drama'],
+      rate: 7
+    }])).toBe(7.5);
   });
 
   it('Should return undefined if there is no Drama movie!', function () {
-    expect(dramaMoviesRate([{ genre: ['Action'], rate: 8 }, { genre: ['Romance'], rate: 9 }, { genre: ['Sci-Fi'], rate: 7 }])).toBe(undefined);
+    expect(dramaMoviesRate([{
+      genre: ['Action'],
+      rate: 8
+    }, {
+      genre: ['Romance'],
+      rate: 9
+    }, {
+      genre: ['Sci-Fi'],
+      rate: 7
+    }])).toBe(undefined);
   });
 });
 
@@ -109,15 +172,49 @@ describe('Order the movies by duration - orderByDuration', function () {
   });
 
   it('Should return the element in a single element array', function () {
-    expect(orderByDuration([{ duration: 100 }])).toEqual([{ duration: 100 }]);
+    expect(orderByDuration([{
+      duration: 100
+    }])).toEqual([{
+      duration: 100
+    }]);
   });
 
   it('Return the new array in growing order', function () {
-    expect(orderByDuration([{ duration: 100 }, { duration: 180 }, { duration: 90 }])).toEqual([{ duration: 90 }, { duration: 100 }, { duration: 180 }]);
+    expect(orderByDuration([{
+      duration: 100
+    }, {
+      duration: 180
+    }, {
+      duration: 90
+    }])).toEqual([{
+      duration: 90
+    }, {
+      duration: 100
+    }, {
+      duration: 180
+    }]);
   });
 
   it('If two movies have the same length, order them alphabetically by their title', function () {
-    expect(orderByDuration([{ title: 'abc', duration: 180 }, { title: 'bac', duration: 90 }, { title: 'aab', duration: 90 }])).toEqual([{ title: 'aab', duration: 90 }, { title: 'bac', duration: 90 }, { title: 'abc', duration: 180 }]);
+    expect(orderByDuration([{
+      title: 'abc',
+      duration: 180
+    }, {
+      title: 'bac',
+      duration: 90
+    }, {
+      title: 'aab',
+      duration: 90
+    }])).toEqual([{
+      title: 'aab',
+      duration: 90
+    }, {
+      title: 'bac',
+      duration: 90
+    }, {
+      title: 'abc',
+      duration: 180
+    }]);
   });
 });
 
@@ -135,17 +232,14 @@ describe('Get how many movies - howManyMovies', function () {
   });
 
   it('Return "Steven Spielberg directed 0 drama movies!" if he does not directed none of the movies in the array', function () {
-    expect(howManyMovies([
-      {
-        director: 'James McTeigue',
-        genre: ['Action', 'Drama', 'Thriller']
-      }
-    ])).toBe('Steven Spielberg directed 0 drama movies!');
+    expect(howManyMovies([{
+      director: 'James McTeigue',
+      genre: ['Action', 'Drama', 'Thriller']
+    }])).toBe('Steven Spielberg directed 0 drama movies!');
   });
 
   it('Only drama movies! Should return "Steven Spielberg directed 1 drama movies!"', function () {
-    expect(howManyMovies([
-      {
+    expect(howManyMovies([{
         director: 'Steven Spielberg',
         genre: ['Action', 'Drama', 'Thriller']
       },
@@ -157,8 +251,7 @@ describe('Get how many movies - howManyMovies', function () {
   });
 
   it('Only Steven Spielberg movies! Should return "Steven Spielberg directed 2 drama movies!"', function () {
-    expect(howManyMovies([
-      {
+    expect(howManyMovies([{
         director: 'Steven Spielberg',
         genre: ['Action', 'Drama', 'Thriller']
       },
@@ -192,29 +285,191 @@ describe('Order alphabetically - orderAlphabetically', function () {
   });
 
   it('Only return the title of the movies! Each element should be a string', function () {
-    expect(typeof orderAlphabetically([{ title: 'aab' }])[0]).toBe('string');
+    expect(typeof orderAlphabetically([{
+      title: 'aab'
+    }])[0]).toBe('string');
   });
 
   it('If there are less than 20 elements, return all of them.', function () {
-    var moviesArr = [{ title: 'aab' }, { title: 'bab' }, { title: 'acb' }];
+    var moviesArr = [{
+      title: 'aab'
+    }, {
+      title: 'bab'
+    }, {
+      title: 'acb'
+    }];
 
     expect(orderAlphabetically(moviesArr).length).toEqual(3);
   });
 
   it('If there are more than 20 elements, return only 20 of them.', function () {
-    var moviesArr = [{ title: 'aab' }, { title: 'bab' }, { title: 'acb' }, { title: 'aab' }, { title: 'bab' }, { title: 'acb' }, { title: 'aab' }, { title: 'bab' }, { title: 'acb' }, { title: 'aab' }, { title: 'bab' }, { title: 'acb' }, { title: 'aab' }, { title: 'bab' }, { title: 'acb' }, { title: 'aab' }, { title: 'bab' }, { title: 'acb' }, { title: 'aab' }, { title: 'bab' }, { title: 'acb' }, { title: 'aab' }, { title: 'bab' }, { title: 'acb' }, { title: 'aab' }, { title: 'bab' }, { title: 'acb' }, { title: 'aab' }, { title: 'bab' }, { title: 'acb' }, { title: 'aab' }, { title: 'bab' }, { title: 'acb' }, { title: 'aab' }, { title: 'bab' }, { title: 'acb' }];
+    var moviesArr = [{
+      title: 'aab'
+    }, {
+      title: 'bab'
+    }, {
+      title: 'acb'
+    }, {
+      title: 'aab'
+    }, {
+      title: 'bab'
+    }, {
+      title: 'acb'
+    }, {
+      title: 'aab'
+    }, {
+      title: 'bab'
+    }, {
+      title: 'acb'
+    }, {
+      title: 'aab'
+    }, {
+      title: 'bab'
+    }, {
+      title: 'acb'
+    }, {
+      title: 'aab'
+    }, {
+      title: 'bab'
+    }, {
+      title: 'acb'
+    }, {
+      title: 'aab'
+    }, {
+      title: 'bab'
+    }, {
+      title: 'acb'
+    }, {
+      title: 'aab'
+    }, {
+      title: 'bab'
+    }, {
+      title: 'acb'
+    }, {
+      title: 'aab'
+    }, {
+      title: 'bab'
+    }, {
+      title: 'acb'
+    }, {
+      title: 'aab'
+    }, {
+      title: 'bab'
+    }, {
+      title: 'acb'
+    }, {
+      title: 'aab'
+    }, {
+      title: 'bab'
+    }, {
+      title: 'acb'
+    }, {
+      title: 'aab'
+    }, {
+      title: 'bab'
+    }, {
+      title: 'acb'
+    }, {
+      title: 'aab'
+    }, {
+      title: 'bab'
+    }, {
+      title: 'acb'
+    }];
 
     expect(orderAlphabetically(moviesArr).length).toEqual(20);
   });
 
   it('You should order them alphabetically.', function () {
-    var moviesArr = [{ title: 'aab' }, { title: 'aaa' }, { title: 'abc' }, { title: 'acb' }, { title: 'abb' }];
+    var moviesArr = [{
+      title: 'aab'
+    }, {
+      title: 'aaa'
+    }, {
+      title: 'abc'
+    }, {
+      title: 'acb'
+    }, {
+      title: 'abb'
+    }];
 
     expect(orderAlphabetically(moviesArr)).toEqual(['aaa', 'aab', 'abb', 'abc', 'acb']);
   });
 
   it('You should return the top20 after order them alphabetically.', function () {
-    var moviesArr = [{ title: 'aab' }, { title: 'bab' }, { title: 'acb' }, { title: 'aaa' }, { title: 'bbb' }, { title: 'anc' }, { title: 'kns' }, { title: 'zds' }, { title: 'pow' }, { title: 'gda' }, { title: 'res' }, { title: 'ter' }, { title: 'bca' }, { title: 'ccc' }, { title: 'bbt' }, { title: 'qas' }, { title: 'kmn' }, { title: 'frt' }, { title: 'afb' }, { title: 'agb' }, { title: 'apo' }, { title: 'poa' }, { title: 'cdf' }, { title: 'sea' }, { title: 'lom' }, { title: 'acs' }, { title: 'qas' }, { title: 'mns' }, { title: 'bvc' }, { title: 'gha' }, { title: 'lkj' }, { title: 'era' }, { title: 'ert' }, { title: 'tex' }, { title: 'zas' }, { title: 'pol' }];
+    var moviesArr = [{
+      title: 'aab'
+    }, {
+      title: 'bab'
+    }, {
+      title: 'acb'
+    }, {
+      title: 'aaa'
+    }, {
+      title: 'bbb'
+    }, {
+      title: 'anc'
+    }, {
+      title: 'kns'
+    }, {
+      title: 'zds'
+    }, {
+      title: 'pow'
+    }, {
+      title: 'gda'
+    }, {
+      title: 'res'
+    }, {
+      title: 'ter'
+    }, {
+      title: 'bca'
+    }, {
+      title: 'ccc'
+    }, {
+      title: 'bbt'
+    }, {
+      title: 'qas'
+    }, {
+      title: 'kmn'
+    }, {
+      title: 'frt'
+    }, {
+      title: 'afb'
+    }, {
+      title: 'agb'
+    }, {
+      title: 'apo'
+    }, {
+      title: 'poa'
+    }, {
+      title: 'cdf'
+    }, {
+      title: 'sea'
+    }, {
+      title: 'lom'
+    }, {
+      title: 'acs'
+    }, {
+      title: 'qas'
+    }, {
+      title: 'mns'
+    }, {
+      title: 'bvc'
+    }, {
+      title: 'gha'
+    }, {
+      title: 'lkj'
+    }, {
+      title: 'era'
+    }, {
+      title: 'ert'
+    }, {
+      title: 'tex'
+    }, {
+      title: 'zas'
+    }, {
+      title: 'pol'
+    }];
 
     expect(orderAlphabetically(moviesArr)).toEqual(['aaa', 'aab', 'acb', 'acs', 'afb', 'agb', 'anc', 'apo', 'bab', 'bbb', 'bbt', 'bca', 'bvc', 'ccc', 'cdf', 'era', 'ert', 'frt', 'gda', 'gha']);
   });
@@ -230,7 +485,10 @@ describe('Best year average - bestYearAvg', function () {
   });
 
   it('Should return the correct answer to a single element array', function () {
-    expect(bestYearAvg([{ year: '2007', rate: 8 }])).toEqual('The best year was 2007 with an average rate of 8');
+    expect(bestYearAvg([{
+      year: '2007',
+      rate: 8
+    }])).toEqual('The best year was 2007 with an average rate of 8');
   });
 
   it('Should return the correct answer to a multiple elements array', function () {
@@ -238,7 +496,19 @@ describe('Best year average - bestYearAvg', function () {
   });
 
   it('Should return the oldest year when there is a tie', function () {
-    var newMoviesArr = [{ year: '2000', rate: 9 }, { year: '2000', rate: 8 }, { year: '1978', rate: 10 }, { year: '1978', rate: 7 }];
+    var newMoviesArr = [{
+      year: '2000',
+      rate: 9
+    }, {
+      year: '2000',
+      rate: 8
+    }, {
+      year: '1978',
+      rate: 10
+    }, {
+      year: '1978',
+      rate: 7
+    }];
 
     expect(bestYearAvg(newMoviesArr)).toEqual('The best year was 1978 with an average rate of 8.5');
   });
