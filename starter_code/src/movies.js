15e38e8fec69b329cbe0b14890ca628bfb3e181e
bichous <bichous07@gmail.com>
2019-06-06 15:29:46 -0500

    Hay la llevamos

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,9 +1,38 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
+function stringNum (str) {
+  let numMin = 0
+  let numHora = 0
+ if (str.indexOf('h') != -1){ 
+  let hora = str.substring(0, str.indexOf('h') ) .trim(" ") 
+//  console.log(hora)
+  numHora = parseInt(hora) 
+ } 
+ if (str.indexOf("min") != -1){
+  let min = str.substring(str.indexOf('h') + 1, str.indexOf('min') ) .trim(" ") 
+//  console.log(min)
+  numMin = parseInt(min) 
+}
+ let minHoras = numHora * 60
+ let totalMin = minHoras + numMin
+ 
+ return totalMin
+}
 
-
+const turnHoursToMinutes = (movies) => {
+ return movies.map(e => {
+   let newElement = e
+   newElement.duration=stringNum(e.duration)
+   return newElement
+ });
+}
 // Get the average of all rates with 2 decimals 
-
+const ratesAverage = (movies)=> {
+  return parseFloat( (movies.reduce((a, b) => {
+    let num = parseFloat(b.rate)
+ return a + num
+  }, 0) /movies.length).toFixed(2) )
+}
 
 // Get the average of Drama Movies
 
