c8a02979462d5aac943a3b51f720f3b298fdfbaf
Daniel Orio <danielislas3@live.com>
2019-06-06 17:25:26 -0500

    funcion 1 terminada

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,4 +1,28 @@
 /* eslint no-restricted-globals: 'off' */
+function turnHoursToMinutes(str){
+  let horas = 0
+  let minutos = 0
+  let total = 0
+  let h = str.split("h")
+  horas = parseInt(h)
+  //minutos
+  if(h[1] == null){
+    console.log('true')
+    total = horas * 60
+  } 
+  else {
+    let j = h[1].split("min")
+    minutos = parseInt(j[0])
+    //console.log("minutos: "+minutos)
+    //console.log("horas: "+ horas)
+    total = (horas*60)+minutos
+    return total
+  } //console.log("total:"+ total)
+}
+
+movies.forEach((e)=>{
+  turnHoursToMinutes(e.duration)
+})
 // Turn duration of the movies from hours to minutes 
 
 
