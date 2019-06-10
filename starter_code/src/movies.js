a9d8381d0f9ba46b4032eccef100e0b5fc5497ce
mariana-gamino <mariianagamino@gmail.com>
2019-06-10 00:56:01 -0500

    PP all-time-movies terminado (done)

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -30,22 +30,44 @@ ratesAverage = (arr) => {
 // Get the average of Drama Movies
 function dramaMoviesRate (arr) {
   let dramaMoviesArr = arr.filter(e => e.genre.includes('Drama'))
+  if(dramaMoviesArr.length === 0) return undefined
   let dramaRates = dramaMoviesArr.map(e => {
-    return parseFloat(e.rate)
+    if(e.rate === ''){
+      return 0
+    } else {
+    return parseFloat(e.rate)}
   })
   const averageRate = (dramaRates.reduce((accum, current) => accum + current)/dramaMoviesArr.length).toFixed(2)
   console.log(averageRate)
+  return Number(averageRate)
 }
 
 dramaMoviesRate(movies)
 
 // Order by time duration, in growing order
-
+function orderByDuration (arr) {
+  arr.sort((a, b) => {
+    if (a.duration !== b.duration){
+      return (a.duration - b.duration)} else {
+        return a.title.localeCompare(b.title)}
+      })
+  return arr
+}
 
 // How many movies did STEVEN SPIELBERG
-
+const howManyMovies = (arr) => {
+  let dramaMoviesArr = arr.filter(e => e.genre.includes('Drama'))
+  let steveDramaMovies = dramaMoviesArr.filter(e => e.director.toUpperCase() === 'STEVEN SPIELBERG')
+  let howMany = steveDramaMovies.length
+  if(dramaMoviesArr.length === 0) return undefined
+  return `Steven Spielberg directed ${howMany} drama movies!`
+}
 
 // Order by title and print the first 20 titles
+const orderAlphabetically = (arr) => {
+  return arr.map(e => e.title).sort().slice(0, 20)
+}
+
+orderAlphabetically(movies)
 
 
-// Best yearly rate average
