888020cc1f28ddeec85177771f96ba0c59fe1ff6
Francho Garcia <franchog79@gmail.com>
2019-06-10 17:10:28 +0200

    Iteration 1 finished

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,6 +1,27 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
+function turnHoursToMinutes(movies) {
+    totalTime = 0
+    let durationFilm = []
+    let newListOfFilms = movies.map(film => {
+        console.log(film.duration)
+        durationFilm = film.duration.split(" ")
+        if (durationFilm.length === 2) {
+            totalTime = parseInt(durationFilm[0]) * 60 + parseInt(durationFilm[1])
+        } else {
+            if (durationFilm[0].includes("h")) {
+                totalTime = parseInt(durationFilm[0]) * 60
+            } else {
+                totalTime = parseInt(durationFilm[0])
+            }
+        }
 
+        console.log(totalTime)
+        return {...film, duration: totalTime }
+    })
+    return newListOfFilms
+}
+turnHoursToMinutes(movies)
 
 // Get the average of all rates with 2 decimals 
 
