73514ac60659783b52819d5cf9008ea5d517fb4e
Miriloper <miriam.mad.ml@gmail.com>
2019-06-10 22:42:03 +0200

    Changes, almost finish

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -29,12 +29,49 @@ function turnHoursToMinutes(movies) {
 
 // Get the average of all rates with 2 decimals 
 
+function ratesAverage (movies) {
+  return movies.reduce((acumRate, movie)=>{
+    return acumRate + movie.rate;
+
+  }, 0)/movies.length;
+}
+
 
 // Get the average of Drama Movies
 
+// function dramaMoviesRate (movies) {
+//   return movies.reduce((acumRate, movie)=>{
+//     if ('drama' in movie) {
+//       return acumRate + movie.rate;
+//     }
+//   }, 0)/movies.length;
+// }
+
+function dramaMoviesRate (movies) {
+    let dramaArray = movies.filter(dramaMovie => dramaMovie.genre.includes('Drama'));
+    if (dramaArray.length == 0) {
+      return undefined;
+    }
+    return (Math.round(ratesAverage(dramaArray)*100)/100);
+}
 
 // Order by time duration, in growing order
 
+function orderByDuration (movies) {
+    movies.sort((a,b)=> a.duration - b.duration);
+    // if (a.duration == b.duration) {
+    //   movies.sort((a,b)=> a.title - b.title);
+    // }
+  return movies;
+}
+
+// movies.sort((a,b)=> {
+//   if a.duration > b.duration 
+//   if (a.duration == b.duration) {
+//     movies.sort((a,b)=> a.title - b.title);
+//   }
+// }
+//   );
 
 // How many movies did STEVEN SPIELBERG
 
