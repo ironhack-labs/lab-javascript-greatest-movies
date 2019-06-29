73e54b352a109b19b97b9dd219ea4fa4b4b92371
Fernando Yto <fernando.toshioyto@gmail.com>
2019-06-29 01:21:24 -0300

    still missing last 2 functions

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,20 +1,76 @@
 /* eslint no-restricted-globals: 'off' */
-// Turn duration of the movies from hours to minutes 
+// const movies = require('./data.js');
 
+// Turn duration of the movies from hours to minutes 
+function turnHoursToMinutes (moviesArray) {
+  const array = JSON.parse(JSON.stringify(moviesArray));
+  const newMoviesArray = array.map(movie =>{
+    let durationInMinutes;
+    //First let's check if the movie has a duration of hours AND mins
+    if (movie.duration.includes("h") && movie.duration.includes("min")) {
+      durationInMinutes = Number(movie.duration[0]) * 60 + Number(movie.duration.split(" ")[1].slice(0,-3));
+    } else {
+      //I already treated cases where there are mins and hours
+      //Now I'm checking if the movie has a duration of exact hour(s) ONLY. No 'mins'
+      if (movie.duration.includes("h")){
+        //it has a duration of exact hour(s)
+        durationInMinutes = Number(movie.duration[0]) * 60;
+      } else {
+        //it has a duration of mins only. no full hours
+        durationInMinutes = Number(movie.duration.slice(0,-3));
+      }
+    }
+    movie.duration = durationInMinutes;
+    return movie;
+  });
+  return newMoviesArray;
+}
 
 // Get the average of all rates with 2 decimals 
-
+function ratesAverage (moviesArray) {
+  let count = 0;
+  const finalSum = moviesArray.reduce((sum, movie) => {
+    count += 1;
+    return typeof movie.rate !== "number" ? sum : sum + parseFloat(movie.rate);
+  },0)
+  return parseFloat((finalSum / count).toFixed(2));
+}
 
 // Get the average of Drama Movies
-
+function dramaMoviesRate (moviesArray) {
+  const filteredMovies = moviesArray.filter(movie => {
+    return movie.genre.includes("Drama");
+  })
+  return filteredMovies.length === 0 ? undefined : ratesAverage(filteredMovies);
+}
 
 // Order by time duration, in growing order
-
+function orderByDuration (moviesArray) {
+  moviesArray.sort((a, b) => {
+    if (a.duration === b.duration) return a.title.localeCompare(b.title);
+    if (a.duration > b.duration) return 1;
+    if (a.duration < b.duration) return -1;
+  })
+  return moviesArray;
+}
 
 // How many movies did STEVEN SPIELBERG
-
+function howManyMovies (moviesArray) {
+  if (moviesArray.length === 0) return undefined;
+  const dramaMovies = moviesArray.filter(movie => {
+    return movie.genre.includes("Drama");
+  })
+  const dramaSpielberg = dramaMovies.filter(movie => {
+    return movie.director === 'Steven Spielberg';
+  })
+  return `Steven Spielberg directed ${dramaSpielberg.length} drama movies!`
+}
 
 // Order by title and print the first 20 titles
-
+function orderAlphabetically (moviesArray) {
+  const sortedArray = moviesArray.sort((a, b) => {
+    return a.title.localeCompare(b.title);
+  })
+}
 
 // Best yearly rate average
