249a5b6dd12abaa786945391388158d6d7783023
Mark Trevail <trevail.m@gmail.com>
2019-07-01 15:38:07 +0200

    More

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -32,7 +32,7 @@ class imdbManager{
     return avgRating;
   };
 
-  ratingAvgByGenre(arr, genre) {
+  ratingAvgForGenre(arr, genre) {
     let arrFiltered = arr.filter( element => element.genre.includes(genre) );
     let totalRating = arrFiltered.reduce( (sum, element) => sum + parseFloat(element.rate), 0);
     let avgRating = (totalRating / arrFiltered.length).toFixed(2);
@@ -43,6 +43,30 @@ class imdbManager{
     let arrFiltered = arr.filter(element => element.director === director); 
     let count = arrFiltered.length;
     return count;
+  }
+
+  topNAlphabetically(arr, n) {
+    let arrTitles = arr.map(element => element.title);
+    let arrTitlesSorted = arrTitles.sort((a,b) => {
+      if (a < b) {return -1};
+      if (a > b) {return 1};
+      if (a === b) {return 0};
+    });
+    let arrTopNTitlesSorted = arrTitlesSorted.slice(0, n);
+    return arrTopNTitlesSorted;
+  }
+
+  ratingByKeyValues(arr, key) {
+
+    // Create array of key:value pairs, where the "keys" are the values for the key input above...
+    // ... and the "value" is an array of movie objects 
+    
+    
+    
+
+    };
+
+    
   }
 
 };
@@ -59,29 +83,46 @@ var avgRating = imdbManager.ratingAvg(moviesCleaned);
 console.log(`Average rating is: ${avgRating}`);
 
 var genre = "Drama";
-var ratingAvgByGenre = imdbManager.ratingAvgByGenre(moviesCleaned, genre);
-console.log(`Average rating for ${genre} movies is: ${ratingAvgByGenre}`);
+var ratingAvgForGenre = imdbManager.ratingAvgForGenre(moviesCleaned, genre);
+console.log(`Average rating for ${genre} movies is: ${ratingAvgForGenre}`);
 
 var director = "Steven Spielberg";
 var countByDirector = imdbManager.countByDirector(moviesCleaned, director);
 console.log(`Number of movies by ${director} is: ${countByDirector}`);
 
+var n = 10;
+topNAlphabetically = imdbManager.topNAlphabetically(moviesCleaned, n);
+console.log(`Top ${n} movies alphabetically are:`);
+for(let i=0; i<topNAlphabetically.length; i++) {
+  console.log(topNAlphabetically[i]);
+};
+
+var key = "year";
+var ratingByKeyValues = imdbManager.ratingByKeyValues(moviesCleaned, key);
+console.log(ratingByKeyValues);
 
 
 
-// Get the average of all rates with 2 decimals 
 
 
-// Get the average of Drama Movies
 
+// ratingByKeyValues(arr, key) {
 
-// Order by time duration, in growing order
+//   // Get array of all values for the key
+//   let arrOfValues = arr.map(element => element[key]);
 
+//   // Get just unique values of the key
+//   function onlyUnique(value, index, self) {   // Function returns true only on first instance of element in array
+//     return self.indexOf(value) === index;
+//   }
+//   let arrOfValuesUnique = arrOfValues.filter(onlyUnique).sort((a,b) => a - b);
 
-// How many movies did STEVEN SPIELBERG
+//   // For each value of the key, get the average rating, and store both
+//   let avgRatingsByValue = [];
 
+//   arrOfValuesUnique.forEach(element => {
+    
+//   });
 
-// Order by title and print the first 20 titles
-
-
-// Best yearly rate average
+  
+// }
