efb9452e29c0f0c8cd602935dbb63be4572ca76b
Lena Lau <frau.lena.lau@gmail.com>
2019-07-02 21:34:35 +0200

    not even close to 'done'

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,20 +1,47 @@
 /* eslint no-restricted-globals: 'off' */
-// Turn duration of the movies from hours to minutes 
 
+// Turn duration of the movies from hours to minutes
 
-// Get the average of all rates with 2 decimals 
+function stringToMinutes() {
+  let string = movies.map(e => e.duration);
+  var arr = string.split("h");
+  if (arr.length < 2) {
+    arr.unshift("0");
+  }
+  var hours = Number(arr[0]);
+  var minutes = Number(arr[1].split("min")[0]);
+
+  return hours * 60 + minutes;
+}
+
+// Get the average of all rates with 2 decimals
+let ratesNumbers = function(array) {
+    let numbers = array.map(movie => {
+       return parseFloat(movie.rate)
+     })
+     return numbers
+    }
+   let ratesInNUmbers = ratesNumbers(movies)
+
+const ratesAverage = ratesInNumbers.reduce((sum, movie) => {
+    return sum + movie /movies.length;
+  });
+  ratesAverage
+}
+// your code:
+
 
 
 // Get the average of Drama Movies
+function avgDrama(movies) {
+  let result = movies.filter(element => element.genre === "Drama");
+}
 
-
+console.log(result);
 // Order by time duration, in growing order
-
 
 // How many movies did STEVEN SPIELBERG
 
-
 // Order by title and print the first 20 titles
-
 
 // Best yearly rate average
