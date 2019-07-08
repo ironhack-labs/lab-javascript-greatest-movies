afc7805071def4e3d09882ebc5d5bc81dd663964
Alex Cabo Martinez <alex@MacBook-Air-de-Alex.local>
2019-07-08 20:36:22 +0200

    movies

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,11 +1,50 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
 
+function turnHoursToMinutes(movies) {
+
+    const time = movies.map(elm => {
+        const timeClean = elm.duration.split("")
+
+        if (timeClean.length == 2) {
+            (parseInt(timeClean[0]) * 60) + parseInt(timeClean[1])
+        }
+        else {
+            parseInt(timeClean[0] * 60)
+        }
+    })
+
+
+}
 
 // Get the average of all rates with 2 decimals 
 
+function ratesAverage(movies) {
+
+    const rating = movies.reduce((acc, elm) => {
+        return acc + parseInt(elm.rate)
+    }, 0)
+
+    return rating / movies.length
+}
 
 // Get the average of Drama Movies
+
+function dramaMoviesRate(movies) {
+
+    const dramaMovies = movies.filter(elm => elm.genre.includes("Drama"))
+
+    const rating = dramaMovies.reduce((acc, ele) => {
+
+
+        return acc + parseInt(ele.rate)
+
+
+
+    }, 0)
+
+    return rating / dramaMovies.length
+}
 
 
 // Order by time duration, in growing order
@@ -14,7 +53,37 @@
 // How many movies did STEVEN SPIELBERG
 
 
+
+function howManyMovies(movies) {
+
+
+    const stevenMovies = movies.filter(elm => elm.director == 'Steven Spielberg')
+
+    const dramaMovies = movies.filter(ele => ele.genre.includes("Drama"))
+
+    if (!stevenMovies) {
+        return undefined
+    }
+    else if (stevenMovies && dramaMovies) {
+        return "Steven Spielberg directed 1 drama movies!"
+    }
+    else {
+        return "Steven Spielberg directed 0 drama movies!"
+    }
+
+
+
+}
+
+
+
 // Order by title and print the first 20 titles
 
+function orderAlphabetically(movies) {
+    const titles = movies.map(elm => elm.title)
+    const ordTitles = titles.sort()
 
+    return ordTitles.splice(0, 20)
+}
 // Best yearly rate average
+
