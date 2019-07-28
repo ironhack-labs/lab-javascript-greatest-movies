34414f358302be2dc0b6792643005fc6df42041e
elwiiman <emmanuel.sanro@gmail.com>
2019-07-28 00:52:46 -0500

    avanzado hasta ejercicio 3

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,5 +1,5 @@
 /* eslint no-restricted-globals: 'off' */
-
+console.log(movies);
 // Iteration 1: All rates average - Get the average of all rates with 2 decimals
 
 function ratesAverage(movies) {
@@ -11,7 +11,7 @@ function ratesAverage(movies) {
   }, 0);
   return Math.round((reduced / movies.length) * 100) / 100;
 }
-console.log(ratesAverage(movies));
+// console.log(ratesAverage(movies));
 
 // Iteration 2: Drama movies - Get the average of Drama Movies
 
@@ -25,9 +25,59 @@ function dramaMoviesRate(movies) {
     return ratesAverage(dramaMovies);
   }
 }
-console.log(dramaMoviesRate(movies));
+// console.log(dramaMoviesRate(movies));
 
 // Iteration 3: Ordering by duration - Order by time duration, ascending (in growing order)
+function orderByDuration(movies) {
+  let durationArr = [];
+  let minutes = [];
+  let hours = [];
+  movies.forEach((movie, index) => {
+    durationArr[index] = movie.duration;
+
+    minutes[index] = parseInt(
+      durationArr[index].substring(
+        durationArr[index].indexOf("m") - 2,
+        durationArr[index].indexOf("m")
+      )
+    );
+    hours[index] = parseInt(
+      durationArr[index].substring(
+        durationArr[index].indexOf("h") - 1,
+        durationArr[index].indexOf("h")
+      )
+    );
+  });
+
+  // horas a minutos
+  let totalMinutes = hours.map(function(element, index) {
+    return element * 60 + minutes[index];
+  });
+
+  //asocia el indice actual al arreglo de minutos
+  for (let i = 0; i < totalMinutes.length; i++) {
+    totalMinutes[i] = [totalMinutes[i], i];
+  }
+
+  // realiza ordenamiento de totalMinutes
+  totalMinutes.sort(function(a, b) {
+    return a[0] - b[0];
+  });
+
+  //obtencion del arreglo de los indices derivados de aplicar ordenamiento
+  let indexSorted = [];
+  for (let i = 0; i < totalMinutes.length; i++) {
+    indexSorted[i] = totalMinutes[i][1];
+  }
+
+  // aplica los indices de ordenamiento a todo movies
+  let moviesSorted = indexSorted.map(function(element) {
+    return movies[element];
+  });
+  return moviesSorted;
+}
+
+console.log(orderByDuration(movies));
 
 // Iteration 4: Steven Spielberg. The best? - How many movies did STEVEN SPIELBERG direct
 
