00cdb92b559d9c65bf1b8be9f877e0657ef32f2a
maerd19 <revolutions_333@hotmail.com>
2019-07-29 16:19:10 -0500

    second commit

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,33 +1,28 @@
 /* eslint no-restricted-globals: 'off' */
 
 
-// Iteration 1: All rates average - Get the average of all rates with 2 decimals 
+// Iteration 1: All rates average - Get the average of all rates with 2 decimals
 const ratesAverage = movies => {
-    movies.reduce( (acc, movie) => {
-        subtotal = acc += parseFloat(movie.rate);
-        return subtotal;
-    }, 0);
-    return subtotal / movies.length;
+  total = movies.reduce((sum, movie) => sum += movie.rate, 0);
+  return total / movies.length;
 }
 
- 
 // Iteration 2: Drama movies - Get the average of Drama Movies
-const dramaMoviesRate = movies => {  
-    let dramas = movies.filter(movie => {
-        movie.genre.includes('Drama');
-    }); 
-    return ratesAverage(dramas);
+const dramaMoviesRate = movies => {
+    let dramas = movies.filter(movie => movie.genre.includes('Drama'));
+    return (dramas.length > 0) ? parseFloat(ratesAverage(dramas).toFixed(2)) : 0;
 };
-// console.log(dramaMoviesRate(movies));
 
 
-
+// We need to sort the movies in ascending order by their duration. This should be easy using one of the methods we've just learned.
+// Create a function orderByDuration() that receives an array as parameter and returns a sorted array.
+// If two movies have the same duration, order them in alphabetical order by their title!
 
 // Iteration 3: Ordering by duration - Order by time duration, ascending (in growing order)
 const orderByDuration = movies => {
     let arr = [];
     movies.forEach(movie => {
-        // console.log(movie.duration);      
+        // console.log(movie.duration);
         let hrsInMinutes = (movie.duration.slice(0, 1) * 60);
         let minutes = parseInt(movie.duration.slice(3, 5));
         // console.log(`minutes: ${hrsInMinutes + minutes}`)
@@ -35,14 +30,17 @@ const orderByDuration = movies => {
         arr.push(movie);
     })
     arr.sort((a, b) => a - b);
-    
+
     return arr;
 };
 // console.log(orderByDuration(movies));
 
 
 // Iteration 4: Steven Spielberg. The best? - How many movies did STEVEN SPIELBERG direct
-const howManyMovies = movies => {};
+const howManyMovies = movies => {
+  const spielbergMovies = movies.filter( movie => (movie.director.includes('Steven Spielberg')) && (movie.genre.includes('Drama')) );
+  return (spielbergMovies.length > 0) ? spielbergMovies.length : 0;
+};
 
 
 // Iteration 5: Alphabetic Order - Order by title and print the first 20 titles
