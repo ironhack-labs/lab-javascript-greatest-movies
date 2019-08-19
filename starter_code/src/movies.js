ba9fb7f30e629ea8b256167403f82192e3326aff
Ro <rodrigo_betan@hotmail.com>
2019-08-19 14:08:31 -0500

    ejercicios resueltos

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,4 +1,4 @@
-/* eslint no-restricted-globals: 'off' */
+
 
 /* Iteration 1: All rates average - 
 Get the average of all rates with 2 decimals */
@@ -18,57 +18,105 @@ ratesAverage(movies)
 // Iteration 2: Drama movies -
 /*Get the average of Drama Movies*/
 
-const dramaMoviesRate = anyArr => {
-  const drama = anyArr.filter(any => {
-    return any.genre.includes('Drama')
-  })
 
-  if (drama.length > 0) {
-    const dramaAvg = ratesAverage(drama)
-    {
-      return Number(dramaAvg.toFixed(2))
-    }
-  } else {
-    return 0
-  }
+function dramaMoviesRate(movieArray) {
+
+  const dramaMovies = movieArray.filter(function(movieElement) {
+     return movieElement.genre.indexOf('Drama') !== -1}  )
+
+      if (isNaN  (ratesAverage (dramaMovies) ) ) {return 0}
+      return ratesAverage(dramaMovies)
 }
+
 
 // Iteration 3: Ordering by duration -
 /* Order by time duration, ascending 
 (in growing order) */
 
-const orderByDuration = arr => {
-  const sortedByDuration = arr.sort(function(a,b) {
-    return a.duration - b.duration
-  })
+function orderByDuration(moviesArray) {
+  moviesArray.sort(function(a, b) {
+    if (a.duration === b.duration) {
+        if (a.title > b.title) {return 1} 
+        else if (a.title < b.title) {return -1} 
+        else {return 0}     }
+    return a.duration - b.duration })
+  return moviesArray
 }
-
-const sortedByTitle = sortedByDuration.sort(function(a,b){
-if (a.duration === b.duration) {
-  if(a.title < b.title) {return -1}
-  if (a.title > b.title) {return 1}
-
-  return 0
-}
-})
 
 // Iteration 4: Steven Spielberg. The best? 
 /*- How many movies did STEVEN SPIELBERG direct
 */
 
-const howManyMovies = (array) => {
-  const spielbergMovies = array.filter(   (currentMovie) => {
-    return currentMovie.director === "Steven Spielberg" && 
-            currentMovie.genre.includes('Drama')
+function howManyMovies(moviesArr) {
+  if (moviesArr.length === 0) {return 0}
+  
+  const directorMovies = moviesArr.filter(function(e) {
+    return e.director === 'Steven Spielberg' && e.genre.indexOf('Drama') !== -1
   })
-
-   return spielbergMovies.length
+  return directorMovies.length
 }
-
 
 
 // Iteration 5: Alphabetic Order - Order by title and print the first 20 titles
 
+
+function orderAlphabetically(moviesArray) {
+  moviesArray.sort(function(a, b) {
+    return a.title < b.title ? -1 : 1
+  })
+    const top20Movies = []
+    let limit = 20
+      if (moviesArray.length < 20) {limit = moviesArray.length}
+          for (i = 0; i < limit; i++) {top20Movies.push(moviesArray[i].title)}
+    return top20Movies
+}
+
+
+
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
+function turnHoursToMinutes(moviesArray) {
+  return moviesArray.map(function(elem) {
+    let hours = 0
+    let minutes = 0
+    if (elem.duration.indexOf('h') !== -1) {
+      hours = parseInt(elem.duration[0], 10) * 60
+    }
+    if (elem.duration.indexOf('min') !== -1) {
+      minutes = parseInt(elem.duration.substring(elem.duration.length - 5, elem.duration.length - 3), 10)
+    }
+    return { ...elem, duration: hours + minutes }
+    //return Object.assign({}, elem, { duration: hours + minutes })
+  })
+}
 
-
+function bestYearAvg(moviesArr) {
+  const ratesYear = {}
+  const moviesYear = {}
+  const averageYear = {}
+  if (moviesArr.length === 0) {
+    return null
+  }
+  moviesArr.forEach(function(e) {
+    if (ratesYear[e.year]) {
+      moviesYear[e.year] += 1
+      ratesYear[e.year] += parseFloat(e.rate)
+      averageYear[e.year] = parseFloat((ratesYear[e.year] / moviesYear[e.year]).toFixed(2))
+    } else {
+      ratesYear[e.year] = parseFloat(e.rate)
+      moviesYear[e.year] = 1
+      averageYear[e.year] = parseFloat(e.rate)
+    }
+  })
+  const year = Object.keys(averageYear).reduce(function(a, b) {
+    if (averageYear[a] === averageYear[b]) {
+      if (b < a) {
+        return b
+      }
+      return a
+    } else if (averageYear[a] > averageYear[b]) {
+      return a
+    }
+    return b
+  })
+  return 'The best year was ' + year + ' with an average rate of ' + averageYear[year]
+}
