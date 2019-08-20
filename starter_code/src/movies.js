3b8d66568141b366008037d735c48c399515e257
Larissa <dejamepienso@gmail.com>
2019-08-19 22:00:50 -0500

    dramaMoviesRate & orderByDuration

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -14,75 +14,37 @@ const ratesAverage = (arr) => {
 
  ratesAverage(movies)
  // Iteration 2: Drama movies - Get the average of Drama Movies
- const dramaMoviesRate = (arr) => {
-   const arrr = arr.filter(element => element.genre.includes('Drama'))
-   const arrRed = arrr.reduce((sum,elem) => {
-    let intg = parseFloat(elem.rate)
-		return sum += intg
-	 },0)
-   let averageRate = arrRed/arr.length
-   return arrRed
- }
+ function dramaMoviesRate(arr) {
+	const dramaMovies = arr.filter(function (drama) {
+			return drama.genre.includes('Drama') && drama.genre.length === 1
+	})
+	if (dramaMovies.length === 0) {
+			return 0
+	} else {
+			return Math.round((dramaMovies.reduce(function (acc, current) {
+					return acc + Number(current.rate)
+			}, 0) / dramaMovies.length) * 100) / 100
+
+	}
+}
 dramaMoviesRate(movies)
 
 // Iteration 3: Ordering by duration - Order by time duration, ascending (in growing order)
-const orderByDuration = (arr) => {
-	const nodo = arr.map(element =>element.duration)
-	return nodo.sort()
-	}
-	
+function orderByDuration(arr) {
+  let ordArr = arr.map(function(durations) {
+    return durations;
+  });
+	ordArr.sort(function(a, b) {
+    if (a.duration === b.duration) {
+      if (a.title < b.title) return -1;
+      if (a.title > b.title) return 1;
+    }
+    return a.duration - b.duration;
+  });
+
+  return ordArr;
+}
  orderByDuration(movies)
 
-// Iteration 4: Steven Spielberg. The best? - How many movies did STEVEN SPIELBERG direct
-const howManyMovies =(arr) => {
-	const steven = arr.filter(element => element.director.includes('Steven Spielberg'))
-  const titles = steven.map(e => e.title)
-  const onlyDrama = steven.filter(gen => gen.genre.includes('Drama'))
-
-	return onlyDrama
-}
-
-howManyMovies(movies)
-
-// Iteration 5: Alphabetic Order - Order by title and print the first 20 titles
-const orderAlphabetically = (arr) => {
-	const titles = arr.map(e => e.title)
-	const alph = titles.sort()
- 
-	if(alph.length < 20) {
-	  return alph
-	} else {
-	  return alph.slice(20)
-	}
- 
- }
- 
- orderAlphabetically(movies)
-
-// Iteration 6: Time Format - Turn duration of the movies from hours to minutes
-
-const turnHoursToMinutes = (arr) => {
-	const hrs = arr.map(element =>element.duration)
- 
-	const sinMin = hrs.map(n => n.split('min').join('')) 
- 
-	const sinH = sinMin.map(h => h.split('h').join(''))
- 
-	const hrsInt = sinH.map(num => num.slice(0,2))
- 
-	const hrsInt2 = hrsInt.map(x => parseInt(x))
- 
-	const numHrs = sinH.map(num => num.slice(2))
- 
-	const minInt = numHrs.map(x => parseInt(x))
- 
-	const tot = hrsInt2.map((e, i) => e * 60 + minInt[i])
- 
-	const longest = tot.sort(function(a,b){return a-b})
-	
-	return longest
- }
- 
- turnHoursToMinutes(movies)
 
 // BONUS Iteration: Best yearly rate average - Best yearly rate average
