edca31e192c1ba7189a4d322d59da683cae3c51c
Antonio Llamas <antoniollamasln@gmail.com>
2019-08-26 16:46:05 +0200

    Iteration 2

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -5,6 +5,7 @@
 // Iteration 1: All rates average - Get the average of all rates with 2 decimals 
 
 const ratesAverage = movies => {
+
   let average = (movies.reduce(
                         (ac, movie) => 
                         ac + +movie.rate, 0))
@@ -16,7 +17,15 @@ console.log(ratesAverage(movies));
  
 // Iteration 2: Drama movies - Get the average of Drama Movies
 
+const dramaMoviesRate = movies => {
 
+  let dramaMovies = movies.filter(movie => movie.genre.indexOf('Drama') >= 0);
+
+  return (dramaMovies.length == 0)? 0 : ratesAverage(dramaMovies);
+  
+}
+
+console.log(dramaMoviesRate(movies));
 // Iteration 3: Ordering by duration - Order by time duration, ascending (in growing order)
 
 
