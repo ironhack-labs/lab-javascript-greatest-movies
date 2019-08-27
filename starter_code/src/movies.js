f56fe769dc64c8455614ccb1fe525348107fe04e
pauromeropau <pauromeropau@gmail.com>
2019-08-27 08:56:01 +0200

    third commit

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -23,15 +23,12 @@ function  orderByDuration(movieArray) {
   return a.title.localeCompare(b.title);
   })
 }
-  
 // Iteration 4: Steven Spielberg. The best? - How many movies did STEVEN SPIELBERG direct
 function howManyMovies(movieArray){
 const SpielbergMovies =[...movieArray.filter(movie => movie.genre.includes('Drama') && movie.director ==='Steven Spielberg')];
 return +SpielbergMovies.length;
 }
 // Iteration 5: Alphabetic Order - Order by title and print the first 20 titles
-//Create a orderAlphabetically() function, that receives an array and returns an array of first 20 titles, alphabetically ordered. Return only the title of each movie, and if the array you receive has less than 20 movies, return all of them.
-
 function orderAlphabetically(movieArray){
   const sortedMovies = movieArray.sort((a, b) => {
     if (a.title.length!==b.title.length) return a.title-b.title;
@@ -39,12 +36,34 @@ function orderAlphabetically(movieArray){
   });
   return sortedMovies.map(movieTitle => movieTitle.title).slice(0,20);
 }
-
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
-
-  
+function toMinutes(duration) {
+  let hours = 0
+  let min = 0
+  let totalHours = 0
+  let durationSplit = duration.split(" ")
+  if (durationSplit.length >= 2) {
+      hours = parseInt(durationSplit[0])
+      min = parseInt(durationSplit[1])
+      } else {
+        if (durationSplit[0].indexOf("m") !== -1) {
+            min = parseInt(durationSplit[0])
+      } else if (durationSplit[0].indexOf("h") !== -1) {
+          hours = parseInt(durationSplit[0]);
+      }
+  }
+  totalHours = (hours * 60) + min
+  return totalHours
+}
+function turnHoursToMinutes(movies) {
+  let moviesInMinutes = movies.map(movie => {
+    let newMovie = { ...movie }
+    newMovie.duration = toMinutes(movie.duration)
+    return newMovie
+});
+return moviesInMinutes
+}
 
 // BONUS Iteration: Best yearly rate average - Best yearly rate average
 function bestYearAvg (movieArray){
-
 }
