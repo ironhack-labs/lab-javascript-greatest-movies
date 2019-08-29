3d822ad7adad73b633b5daa471818fa5c9d3ca30
Elba Chimilio <elba@elliot-mac.local>
2019-08-29 08:01:55 -0400

    avgRates() and dramaRates() functions working

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,10 +1,31 @@
 /* eslint no-restricted-globals: 'off' */
 
-// Iteration 1: All rates average - Get the average of all rates with 2 decimals 
+// Iteration 1: All rates average - Get the average of all rates with 2 decimals
+function ratesAverage(arr) {
 
- 
-// Iteration 2: Drama movies - Get the average of Drama Movies
-
+    let totalRate = arr.reduce((sum, elem) => {
+      return sum + elem.rate;
+    }, 0);
+  
+    let averageRate = totalRate / arr.length;
+    return Number(averageRate.toFixed(2));
+  
+  }
+  
+  // Iteration 2: Drama movies - Get the average of Drama Movies
+  function dramaMoviesRate(arr) {
+    
+    let dramaMoviesRate = arr.filter(arr => arr.genre == 'Drama');
+  
+    if (dramaMoviesRate < 1) {
+      return dramaMoviesRate = 0;
+    }
+    if (ratesAverage(dramaMoviesRate) == 0) {
+      return 0;
+    } 
+    return ratesAverage(dramaMoviesRate)
+  
+  }
 
 // Iteration 3: Ordering by duration - Order by time duration, ascending (in growing order)
 
