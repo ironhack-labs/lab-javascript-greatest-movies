b71554b318f748578a9acf12f01457ee14fe209e
Jose Galindo <jgalindo.ironhack@gamil.com>
2019-08-29 08:36:32 -0400

    working on 3rd iteration

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -2,12 +2,70 @@
 
 // Iteration 1: All rates average - Get the average of all rates with 2 decimals 
 
- 
+
+function ratesAverage(movies) {
+    const averages = movies.reduce((sum, movies) => {
+        return sum + movies.rate;
+    }, 0);
+
+    return (averages / movies.length)
+}
+
 // Iteration 2: Drama movies - Get the average of Drama Movies
 
+function dramaMoviesRate(movies) {
+    const dramaFilter = movies.filter(themovies => themovies.genre.includes("Drama"))
+        // console.log(dramaFilter);
+    const dramaaverages = dramaFilter.reduce((sum, dramaFilter) => {
+        return sum + dramaFilter.rate;
+    }, 0);
+
+    if (dramaFilter.length == 0) {
+        return 0;
+    }
+    return Number((dramaaverages / dramaFilter.length).toFixed(2))
+}
+
+
+
+
+
+// }
 
 // Iteration 3: Ordering by duration - Order by time duration, ascending (in growing order)
+function orderByDuration(movies) {
+    // const durationHour = []
+    // const durationMinute = []
 
+
+    // for (i = 0; i < movies.length; i++) {
+    //     if (movies[i].duration.includes('h') && !movies[i].duration.includes('min')) {
+    //         // console.log(Number(movies[i].duration.split('')[0]))
+    //         let hours = Number(movies[i].duration.split('')[0]);
+    //         durationHour.push(hours);
+    //         // console.log(durationHour);
+    //         // console.log(hours)
+    //     } else if (movies[i].duration.includes('h') && movies[i].duration.includes('min')) {
+    //         // console.log(Number(movies[i].duration.split('')[0]))
+    //         let hourAndMinute = Number(movies[i].duration.split('')[0])
+    //         durationMinute.push(hourAndMinute);
+    //         // console.log(durationMinute)
+    //         console.log(hourAndMinute)
+    //     }
+    //     const array = []
+    //     array.push(durationHour);
+    //     array.push(durationMinute);
+
+    // }
+
+
+
+    // conts fullDuration =
+    //     array.sort(function(a, b) {
+    //         return a.duration - b.duration
+    //     })
+
+}
 
 // Iteration 4: Steven Spielberg. The best? - How many movies did STEVEN SPIELBERG direct
 
