0cf2b26f5c54b87492b0ed264e0f1911b9f8483a
Stefan Grulke <grulke@mediamarktsaturn.com>
2019-08-29 15:41:09 +0200

    iteration 3 and 6 not complete

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -12,8 +12,7 @@ function ratesAverage(array) {
 // Iteration 2: Drama movies - Get the average of Drama Movies
 
 function dramaMoviesRate(array) {
-    const dramaMovies = array.filter(element => element.genre == "Drama");
-    console.log(dramaMovies);
+    const dramaMovies = array.filter(element => element.genre.includes("Drama"));
     if (dramaMovies.length === 0) {
         return 0;
     }
@@ -23,16 +22,22 @@ function dramaMoviesRate(array) {
             mapDramaMovies[index] = "0";
         }
     })
-    console.log(mapDramaMovies);
     const sum = (sumarizer, currentElement) => parseFloat(sumarizer) + parseFloat(currentElement);
-    console.log(sum);
     let avg = parseFloat(((mapDramaMovies.reduce(sum)) / mapDramaMovies.length).toFixed(2));
-    console.log(avg);
     return avg;
 }
 
 // Iteration 3: Ordering by duration - Order by time duration, ascending (in growing order)
-
+function orderByDuration(array){
+    let newArray = Array.from(array);
+    newArray.sort(function (a,b){
+        if(a.duration === b.duration){
+            return a.title > b.title;
+        }
+        return a.duration - b.duration;
+    });
+    return
+}
 
 // Iteration 4: Steven Spielberg. The best? - How many movies did STEVEN SPIELBERG direct
 
@@ -40,5 +45,19 @@ function dramaMoviesRate(array) {
 // Iteration 5: Alphabetic Order - Order by title and print the first 20 titles
 
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
-
+function turnHoursToMinutes(array) {
+    let movieArr = Array.from(array);
+    let newDuration = 0;
+    let splitDuration = [];
+    for (i = 0; i <= movieArr.length - 1; i++) {
+        splitDuration = movieArr[i].duration.split(" ");
+        if (splitDuration.length > 1) {
+            newDuration = (parseInt(splitDuration[0]) * 60) + parseInt(splitDuration[1]);
+        } else {
+            newDuration = parseInt(splitDuration[0]) * 60;
+        }
+        movieArr[i].duration = newDuration;
+    };
+    return movieArr;
+}
 // BONUS Iteration: Best yearly rate average - Best yearly rate average
