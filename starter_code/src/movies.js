cb43eb72774355b414ca5df7bb97a4cb4085d926
Hoshimotao <jthomas302@gmail.com>
2019-08-30 12:29:47 -0400

    add Greatest Movies to github

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,19 +1,135 @@
-/* eslint no-restricted-globals: 'off' */
+// Rates Average
 
-// Iteration 1: All rates average - Get the average of all rates with 2 decimals 
+function ratesAverage(movies) {
+  if (movies.length === 0) {
+    return 0;
+  }
+  let ratings = 0;
+  for (let i = 0; i < movies.length; i++) {
+    ratings += parseFloat(movies[i].rate);
+  }
 
- 
-// Iteration 2: Drama movies - Get the average of Drama Movies
+  ratings = ratings / movies.length;
+
+  console.log(Math.round(ratings * 100) / 100);
+  return Math.round(ratings * 100) / 100;
+}
+
+// Drama Movies Rate
+
+function dramaMoviesRate(movies) {
+  let drama = movies.filter(cog => cog.genre.includes("Drama"));
+
+  for (let i = 0; i < drama.length; i++) {
+    if (!drama[i].rate) {
+      drama[i].rate = 0;
+    }
+    if (drama.length < 1) {
+      return 0;
+    }
+  }
+  return ratesAverage(drama);
+}
+
+// Order By Duration
+
+function orderByDuration(bubba) {
+  return bubba.sort((a, b) => {
+    if (a.duration > b.duration) return 1;
+    if (b.duration > a.duration) return -1;
+    if (b.duration === a.duration) {
+      if (a.title > b.title) return 1;
+      else return -1;
+    }
+  });
+}
+
+// How Many Movies
+
+function howManyMovies(movies) {
+  let steven = movies.filter(eachMovie => {
+    return (
+      eachMovie.director.includes("Steven Spielberg") &&
+      eachMovie.genre.includes("Drama")
+    );
+  });
+
+  return steven.length;
+}
+
+function orderAlphabetically(alphabet) {
+  let newAlpha = alphabet.map(function(eachTitle) {
+    return String(eachTitle.title);
+  });
+  console.log(newAlpha);
+
+  return newAlpha.sort().slice(0, 20);
+}
 
 
-// Iteration 3: Ordering by duration - Order by time duration, ascending (in growing order)
 
 
-// Iteration 4: Steven Spielberg. The best? - How many movies did STEVEN SPIELBERG direct
 
 
-// Iteration 5: Alphabetic Order - Order by title and print the first 20 titles
 
-// Iteration 6: Time Format - Turn duration of the movies from hours to minutes
 
-// BONUS Iteration: Best yearly rate average - Best yearly rate average
+  
+
+  function turnHoursToMinutes(theArray) {
+
+// 1. split the string at the space to create [hours, minutes]
+// 2. use parseFloat on each of the elements
+// 3. [3, 22]: multiply the first number by 60, and add it to the 2nd number; this will become the new movie.duration 202
+// 4. sort movies in ascending order
+
+    let newArr = theArray.map(oneElement => {
+      let tempObj = {}
+       tempObj = {title, year, director, duration, genre, rate} = oneElement
+        tempObj.imNew = true;
+      return tempObj
+    })
+    return newArr
+  }
+  
+  convertMin = minArr => {
+    return min = Number(minArr.split('min')[0])
+  }
+  
+  convertHour = hourArr => {
+    return Number(hourArr.split('h')[0] * 60)
+  }
+  
+  convertDuration = arr => {
+    let tempArr = arr.map(function (oneElement) {
+      let hours = 0;
+      let mins = 0;
+      if(oneElement.duration.includes('h') && oneElement.duration.includes('min')) {
+        hours += convertHour(oneElement.duration.split(' ')[0])
+        mins += convertMin(oneElement.duration.split(' ')[1])
+      } else if(oneElement.duration.includes('h')) {
+        hours += convertHour(oneElement.duration)
+      } else if (oneElement.duration.includes('min')) {
+        mins += convertMin(oneElement.duration)
+      }
+      oneElement.duration = hours + mins
+      return oneElement 
+    })
+    return tempArr;
+  }
+  
+  convertDuration(turnHoursToMinutes(movies))
+
+
+
+  let bestYear = movies.map(function(movie){
+      
+
+
+
+  })
+
+}
+
+
+
+
