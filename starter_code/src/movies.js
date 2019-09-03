4f4b75d0633cfeaea74209eb63bbf6e333981eac
marciano <mcnvrodrigues@gmail.com>
2019-09-03 00:46:16 -0300

    done

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -129,29 +129,67 @@ function turnHoursToMinutes(movies){
     return newFormatDurationMovies;
 }
 
-
-
 // BONUS Iteration: Best yearly rate average - Best yearly rate average
 function bestYearAvg(movies){
-	
-	let yearRates = [];
 
-    movies.forEach(function(movie){
-		
-		if(yearRates.length === 0){
-			yearRates.push([movie.year, movie.rate]);
-		}else{
-		
-		for(let i = 0; i < yearRates; i++){
-			if(yearRates[i].indexOf(movie.year) !== -1){
-				yearRates[i].push(movie.rate);
-			}else{
-				yearRates.push([movie.year, movie.rate]);
-			}
-		}
-		}
-	});
+    if (movies.length === 0){
+        return null;
+    }
 	
+    let yearRates = [];
+    let index = -1;
 
-    return yearRates;
+    /*
+    At the end of "movies.forEach(function(movie){}" execution we will have the following example:
+
+    let yearRates = [ 
+        [ '1994', '9.3', '8.9' ],
+        [ '1972', '9.2' ],
+        [ '1974', '9.0' ],
+        [ '2008', '9.0' ],
+        [ '1957', '8.9' ],
+        [ '1993', '8.9' ],
+        [ '2003', '8.9' ],
+        [ '1966', '8.9' ],
+        [ '1999', '8.8' ] 
+    ];
+    */
+    movies.forEach(function(movie){ 
+		
+        for(let i = 0; i < yearRates.length; i++){
+            if(yearRates[i].indexOf(movie.year) !== -1){
+                index = i;                
+            }
+        }
+
+        if(index !== -1){
+            yearRates[index].push(movie.rate);
+        }else{
+            yearRates.push([movie.year, movie.rate]);
+        }        
+        
+       index = -1;
+    });
+    
+    let bestYear = yearRates[0][0];
+    let averageYearCurrent = 0.0;
+    let averageYearPrevious = 0.0;
+
+    console.log(yearRates);
+
+    for(let i = 0; i < yearRates.length; i++){
+        for(let j = 1; j < yearRates[i].length; j++){
+            averageYearCurrent += parseFloat(yearRates[i][j]);            
+        }
+
+        if(averageYearCurrent > averageYearPrevious){
+            bestYear = yearRates[i][0];
+            averageYearPrevious = averageYearCurrent/((yearRates[i].length) - 1);
+            console.log((yearRates[i].length) - 1 );
+        }
+        
+        averageYearCurrent = 0;
+    }	
+
+    return `The best year was ${bestYear} with an average rate of ${averageYearPrevious}`;
 }
