523707fe75b53f63746978c448c420cddc34d82a
jzsiggy <jzsiggy@gmail.com>
2019-09-03 22:22:29 -0300

    Done

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -88,19 +88,107 @@ function howManyMovies(movies) {
 // Iteration 5: Alphabetic Order - Order by title and print the first 20 titles
 
 function orderAlphabetically(movies) {
-    res = [];
-    sortedMovies = movies.sort(sortByTitle);
-    for (let i = 0; i < 20 || i > movies.length; i++) {
-        res.push(sortedMovies[i]["title"]);
-    }
-    console.log(typeof(res));
-    console.log(typeof(res[0]));
-    console.log(res);
+
+    titles = [];
+    movies.forEach(movie => {
+        titles.push(movie.title);
+    });
+    titles.sort();
+
+    res = titles.filter((str, i) => i < 20)
+
     return res;
 }
 
-orderAlphabetically(data.movies)
+// console.log(orderAlphabetically(data.movies))
 
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
 
+function hToM(str) {
+    let min;
+    let h;
+
+    if (str.indexOf("min") == -1) {
+        min = 0;
+    }
+    else {
+        let min_index = str.indexOf("min");
+        let space_index = str.indexOf(" ");
+        min = str.slice(space_index+1, min_index)
+    }
+
+    if (str.indexOf("h") == -1) {
+        h = 0;
+    }
+    else {
+        h = str.slice(0, str.indexOf("h"));
+    }
+
+    min = parseInt(min);
+    h = parseInt(h);
+    return (h * 60) + min
+}
+
+function turnHoursToMinutes(movies) {
+    duration = movies.map(movie => {
+        let obj = {
+            title: movie.title,
+            year: movie.year,
+            director: movie.director,
+            duration: hToM(movie.duration),
+            genre: movie.genre,
+            rate: movie.rate,
+        };
+        return obj;
+    });
+
+    return duration;
+}
+
+// console.log(turnHoursToMinutes(data.movies))
+console.log(hToM("1h 56min"))
+
 // BONUS Iteration: Best yearly rate average - Best yearly rate average
+
+
+function getObjectByYear(movies) {
+
+    let years = {};
+
+    for (let i in movies) {
+        let year = movies[i]["year"]
+        let movie = movies[i]
+        if (year in years) {
+            years[year].push(movie);
+        }
+        else {
+            years[year] = [];
+            years[year].push(movie);
+        }
+    }
+
+    return years;
+}
+
+function bestYearAvg(movies) {
+
+    if (!movies.length)
+        return null;
+
+    let obj = getObjectByYear(movies);
+    
+    let highestAvg = 0
+    let year;
+
+    for (key in obj) {
+        let yearAvg = ratesAverage(obj[key]);
+        if (yearAvg > highestAvg) {
+            highestAvg = yearAvg;
+            year = key;
+        }
+    }
+
+    return `The best year was ${year} with an average rate of ${highestAvg}`;
+}
+
+// console.log(bestYearAvg(data.movies))
