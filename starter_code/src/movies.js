24130c111cceafca797fb6a3017f702f1a1380af
EnriqueBarba <enrique.barbadillo26@gmail.com>
2019-09-07 16:27:28 +0200

    Primeras dos iteraciones sin errores

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -2,12 +2,50 @@
 
 // Iteration 1: All rates average - Get the average of all rates with 2 decimals 
 
+const ratesAverage = arrMovies => {
+    const arrRates = arrMovies.map(movie => {
+        if(movie.rate === ""){
+            return 0;
+        } else {
+            return parseFloat(movie.rate)
+        }
+    })
+    const result = arrRates.reduce( (totalRate, rate) => (totalRate + rate), 0)/arrRates.length
+    return Number(result.toFixed(2));
+}
  
 // Iteration 2: Drama movies - Get the average of Drama Movies
 
+const dramaMoviesRate = arrMovies => {
+
+    const arrGender = arrMovies.filter( movie => {
+        if ( movie.genre.includes('Drama')) {
+            return true;
+        } else {
+            return false;
+        }
+        
+    })
+
+    if (arrGender.length > 0){
+        return ratesAverage(arrGender)
+    } else {
+        return 0;
+    }
+
+}
 
 // Iteration 3: Ordering by duration - Order by time duration, ascending (in growing order)
 
+function orderByDuration(movies) {
+    return movies.sort((a,b) =>{
+        if(a.duration === b.duration){
+            return a.title > b.tittle ? 1 : -1
+        }
+
+        return a.duration - b.duration
+    })
+}
 
 // Iteration 4: Steven Spielberg. The best? - How many movies did STEVEN SPIELBERG direct
 
