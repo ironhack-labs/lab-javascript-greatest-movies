18f22bb3b80bea014173318d146ab0e35059e7cc
Camilosorio <tech@camilo.fr>
2019-10-18 18:40:32 +0200

    Iteration 1 & 2 done

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,10 +1,34 @@
 /* eslint no-restricted-globals: 'off' */
 
+
 // Iteration 1: All rates average - Get the average of all rates with 2 decimals 
 
- 
+function ratesAverage(arr) {
+
+    let sum = arr.reduce((accumulator, currentValue) => accumulator + parseFloat(currentValue.rate), 0);
+    let average = sum / arr.length;
+    let averageReduce = average.toFixed(2);
+    return parseFloat(averageReduce);
+}
+
+console.log(ratesAverage(movies))
+
 // Iteration 2: Drama movies - Get the average of Drama Movies
 
+function dramaMoviesRate(arr) {
+    const dramaMovies = arr.filter(film => film.genre.includes("Drama"))
+    console.log(dramaMovies)
+
+    if (!dramaMovies.length) return 0;
+
+    const sumRateDrama = dramaMovies.reduce((accumulator, currentValue) => { return accumulator += Number(currentValue.rate) }, 0);
+
+    let averageDrama = sumRateDrama / dramaMovies.length;
+    let averageReduceDrama = averageDrama.toFixed(2);
+    console.log(parseFloat(averageReduceDrama))
+    return parseFloat(averageReduceDrama);
+}
+dramaMoviesRate(movies)
 
 // Iteration 3: Ordering by duration - Order by time duration, ascending (in growing order)
 
