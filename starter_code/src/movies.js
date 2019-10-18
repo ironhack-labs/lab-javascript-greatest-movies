b226c92fdf78e8d829a45ebb0b85da9292360c5a
Louise Marissal <louisemarissal@MacBook-Pro-de-Louise.local>
2019-10-18 22:26:36 +0200

    iteration 4 almost done

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -18,13 +18,27 @@ function dramaMoviesRate(arr2) {
 
 // Iteration 3: Ordering by duration - Order by time duration, ascending (in growing order)
 function orderByYear(movies) {
-  //var movieByYear = [];
   return movies.sort((a, b) => (a.year > b.year ? 1 : -1));
 }
 // Iteration 4: Steven Spielberg. The best? - How many movies did STEVEN SPIELBERG direct
-
+function howManyMovies(movies) {
+  var stevenMovies = movies.filter(element =>
+    element.director.includes("Steven Spielberg")
+  );
+  if (stevenMovies.length === 0) return 0;
+  if (stevenMovies.genre !== "drama") return 1;
+  if (stevenMovies.director === "Steven Spielberg") return 2;
+  for (let i = 0; i < stevenMovies.length; i++) {
+    console.log(stevenMovie[i]);
+  }
+  return stevenMovies;
+}
+console.log(howManyMovies(movies));
 // Iteration 5: Alphabetic Order - Order by title and print the first 20 titles
 
+function orderAlphabetically(movies) {
+  return movies.sort((a, b) => (a.title > b.title ? 1 : -1));
+}
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
 
 // BONUS Iteration: Best yearly rate average - Best yearly rate average
