47406900baacc0b0f0ad6e747703e96d25718cf8
mendosaurus <masaki.endo@nex.nikkei.com>
2019-10-19 18:23:00 -0400

    this is a commit

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -9,25 +9,15 @@ function ratesAverage(movies) {
 // Iteration 2: Drama movies - Get the average of Drama Movies
 function dramaMoviesRate(movies) {
     const drama = movies.filter(movie => movie.genre.includes('Drama') && ('rate' in movie))
-    if (drama.length > 0) {
-        return ratesAverage(drama)
-    } else {
-        return 0
-    }
+    return ratesAverage(drama) || 0
 }
 
 // Iteration 3: Ordering by year, ascending (in growing order)
 function orderByYear(movies) {
     movies.sort((a, b) => {
-        if (a.year - b.year) {
-            return a.year -b.year
-        }
-        if (a.title > b.title) {
-            return 1;
-        }
-        else if (a.title < b.title) {
-            return -1;
-        }
+        if (a.year - b.year) {return a.year - b.year}
+        if (a.title > b.title) {return 1;}
+        if (a.title < b.title) {return -1;}
     })
     return movies
 }
@@ -35,25 +25,21 @@ function orderByYear(movies) {
 // Iteration 4: Steven Spielberg. The best? - How many movies did STEVEN SPIELBERG direct
 function howManyMovies(movies){
     const theDirector = "Steven Spielberg"
-    const dramaByTheDirector = movies.filter(movie =>
-        movie.director === theDirector && movie.genre.includes('Drama'))
+    const dramaByTheDirector = movies.filter(eachMovie =>
+        eachMovie.director === theDirector && eachMovie.genre.includes('Drama'))
     return dramaByTheDirector.length
 }
 
 // Iteration 5: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically(movies) {
     movies.sort((a, b) => {
-        if (a.title > b.title) {
-            return 1
-        } else if (a.title < b.title) {
-            return -1
-        } else {
-            return 0
-        }
+        if (a.title > b.title) {return 1}
+        else if (a.title < b.title) {return -1}
+        else {return 0}
     })
 
     //Return only the title of each movie
-    const titles = movies.map(movie => movie.title)
+    const titles = movies.map(eachMovie => eachMovie.title)
     return titles.slice(0, 20);
 }
 
@@ -65,7 +51,7 @@ function turnHoursToMinutes(movies) {
         const newObject = Object.assign({}, movie)
 
         // turn 'Xhr' to min
-        if (movie.duration.length < 3) {
+        if (movie.duration.length < 5) {
             newObject.duration = parseInt(movie.duration[0] * 60);
         // turn 'X min' to min
         } else if (movie.duration.length < 7) {
@@ -78,7 +64,6 @@ function turnHoursToMinutes(movies) {
     });
     return newArr;
 }
-turnHoursToMinutes(movies)
 
 // BONUS Iteration: Best yearly rate average - Best yearly rate average
 // Go ahead and find which year have the best average rate
@@ -89,7 +74,7 @@ turnHoursToMinutes(movies)
 function bestYearAvg(movies) {
     if(movies.length > 0){
         
-        // order movies by year
+        // group by year
         let moviesYear = {};
         movies.forEach(eachMovie => {
             if (moviesYear[eachMovie.year]) {
