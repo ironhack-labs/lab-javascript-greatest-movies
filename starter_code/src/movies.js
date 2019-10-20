4d3bc58a7d367d349e6a67e6d4b8e53e95fd7c60
Jepassaispar <51320916+Jepassaispar@users.noreply.github.com>
2019-10-20 13:47:47 +0200

    all exercises done, 1 failure, no Bonus

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,4 +1,4 @@
-var film = [{
+let film = [{
         title: 'Dog Day Afternoon',
         year: '1975',
         director: 'Sidney Lumet',
@@ -13,10 +13,258 @@ var film = [{
         duration: '2h 8min',
         genre: ['Drama'],
         rate: '8.0'
+    },
+    {
+        title: 'Annie Hall',
+        year: '1977',
+        director: 'Woody Allen',
+        duration: '1h 33min',
+        genre: ['Comedy', 'Romance'],
+        rate: '8.1'
+    },
+    {
+        title: 'The Terminator',
+        year: '1984',
+        director: 'James Cameron',
+        duration: '1h 47min',
+        genre: ['Action', 'Sci-Fi'],
+        rate: '8.0'
+    },
+    {
+        title: 'Barry Lyndon',
+        year: '1975',
+        director: 'Stanley Kubrick',
+        duration: '3h 4min',
+        genre: ['Adventure', 'Drama', 'History', 'War'],
+        rate: '8.1'
+    },
+    {
+        title: 'The Bourne Ultimatum',
+        year: '2007',
+        director: 'Paul Greengrass',
+        duration: '1h 55min',
+        genre: ['Action', 'Mystery', 'Thriller'],
+        rate: '8.1'
+    },
+    {
+        title: 'The Wizard of Oz',
+        year: '1939',
+        director: 'Victor Fleming',
+        duration: '1h 42min',
+        genre: ['Adventure', 'Family', 'Fantasy', 'Musical'],
+        rate: '8.1'
+    },
+    {
+        title: 'Groundhog Day',
+        year: '1993',
+        director: 'Harold Ramis',
+        duration: '1h 41min',
+        genre: ['Comedy', 'Fantasy', 'Romance'],
+        rate: '8.0'
+    },
+    {
+        title: 'La haine',
+        year: '1995',
+        director: 'Mathieu Kassovitz',
+        duration: '1h 38min',
+        genre: ['Crime', 'Drama'],
+        rate: '8.1'
+    },
+    {
+        title: '8½',
+        year: '1963',
+        director: 'Federico Fellini',
+        duration: '2h 18min',
+        genre: ['Drama'],
+        rate: '8.1'
+    },
+    {
+        title: 'Jaws',
+        year: '1975',
+        director: 'Steven Spielberg',
+        duration: '2h 4min',
+        genre: ['Adventure', 'Drama', 'Thriller'],
+        rate: '8.0'
+    },
+    {
+        title: 'Twelve Monkeys',
+        year: '1995',
+        director: 'Terry Gilliam',
+        duration: '2h 9min',
+        genre: ['Mystery', 'Sci-Fi', 'Thriller'],
+        rate: '8.0'
+    },
+    {
+        title: 'The Best Years of Our Lives',
+        year: '1946',
+        director: 'William Wyler',
+        duration: '2h 50min',
+        genre: ['Drama', 'Romance', 'War'],
+        rate: '8.1'
+    },
+    {
+        title: 'Mou gaan dou',
+        year: '2002',
+        director: 'Wai-Keung Lau',
+        duration: '1h 41min',
+        genre: ['Crime', 'Drama', 'Mystery', 'Thriller'],
+        rate: '8.1'
+    },
+    {
+        title: 'Paris, Texas',
+        year: '1984',
+        director: 'Wim Wenders',
+        duration: '2h 25min',
+        genre: ['Drama'],
+        rate: '8.1'
+    },
+    {
+        title: 'The Help',
+        year: '2011',
+        director: 'Tate Taylor',
+        duration: '2h 26min',
+        genre: ['Drama'],
+        rate: '8.1'
+    },
+    {
+        title: 'Faa yeung nin wa',
+        year: '2000',
+        director: 'Kar-Wai Wong',
+        duration: '1h 38min',
+        genre: ['Drama', 'Romance'],
+        rate: '8.1'
+    },
+    {
+        title: 'Modern Times',
+        year: '1936',
+        director: 'Charles Chaplin',
+        duration: '1h 27min',
+        genre: ['Comedy', 'Drama', 'Family', 'Romance'],
+        rate: '8.5'
+    },
+    {
+        title: 'Raiders of the Lost Ark',
+        year: '1981',
+        director: 'Steven Spielberg',
+        duration: '1h 55min',
+        genre: ['Action', 'Adventure'],
+        rate: '8.5'
+    },
+    {
+        title: 'The Pianist',
+        year: '2002',
+        director: 'Roman Polanski',
+        duration: '2h 30min',
+        genre: ['Biography', 'Drama', 'Music', 'War'],
+        rate: '8.5'
+    },
+    {
+        title: 'The Departed',
+        year: '2006',
+        director: 'Martin Scorsese',
+        duration: '2h 31min',
+        genre: ['Crime', 'Drama', 'Thriller'],
+        rate: '8.5'
+    },
+    {
+        title: 'Rear Window',
+        year: '1954',
+        director: 'Alfred Hitchcock',
+        duration: '1h 52min',
+        genre: ['Mystery', 'Thriller'],
+        rate: '8.5'
+    },
+    {
+        title: 'Terminator 2: Judgment Day',
+        year: '1991',
+        director: 'James Cameron',
+        duration: '2h 17min',
+        genre: ['Action', 'Sci-Fi', 'Thriller'],
+        rate: '8.5'
+    },
+    {
+        title: 'Back to the Future',
+        year: '1985',
+        director: 'Robert Zemeckis',
+        duration: '1h 56min',
+        genre: ['Adventure', 'Comedy', 'Sci-Fi'],
+        rate: '8.5'
+    },
+    {
+        title: 'Whiplash',
+        year: '2014',
+        director: 'Damien Chazelle',
+        duration: '1h 47min',
+        genre: ['Drama', 'Music'],
+        rate: '8.5'
+    },
+    {
+        title: 'Gladiator',
+        year: '2000',
+        director: 'Ridley Scott',
+        duration: '2h 35min',
+        genre: ['Action', 'Adventure', 'Drama'],
+        rate: '8.5'
+    },
+    {
+        title: 'The Prestige',
+        year: '1994',
+        director: 'Christopher Nolan',
+        duration: '2h 10min',
+        genre: ['Drama', 'Mystery', 'Sci-Fi', 'Thriller'],
+        rate: '8.5'
+    },
+    {
+        title: 'The Lion King',
+        year: '1994',
+        director: 'Roger Allers',
+        duration: '1h 28min',
+        genre: ['Animation', 'Adventure', 'Drama', 'Family', 'Musical'],
+        rate: '8.5'
+    },
+    {
+        title: 'Memento',
+        year: '2000',
+        director: 'Christopher Nolan',
+        duration: '1h 53min',
+        genre: ['Mystery', 'Thriller'],
+        rate: '8.5'
+    },
+    {
+        title: 'Apocalypse Now',
+        year: '1979',
+        director: 'Francis Ford Coppola',
+        duration: '2h 27min',
+        genre: ['Drama', 'War'],
+        rate: '8.5'
+    },
+    {
+        title: 'Alien',
+        year: '1979',
+        director: 'Ridley Scott',
+        duration: '1h 57min',
+        genre: ['Horror', 'Sci-Fi'],
+        rate: '8.5'
+    },
+    {
+        title: 'The Great Dictator',
+        year: '1940',
+        director: 'Charles Chaplin',
+        duration: '2h 5min',
+        genre: ['Comedy', 'Drama', 'War'],
+        rate: '8.5'
+    },
+    {
+        title: 'The Great Dictator',
+        year: '1940',
+        director: 'Charles Chaplin',
+        duration: '22min',
+        genre: ['Comedy', 'Drama', 'War'],
+        rate: '8.5'
     }
 ];
 
-// Iteration 1: All rates average - Get the average of all rates with 2 decimals 
+// Iteration 1: All rates average - Get the average of all rates with 2 decimals // FINALISED
 
 function ratesAverage(movies) {
     var totalRate = movies.map(movie => Number(movie.rate))
@@ -30,7 +278,9 @@ function ratesAverage(movies) {
     return Number((sum / movies.length).toFixed(2));
 }
 
-// Iteration 2: Drama movies - Get the average of Drama Movies
+
+
+// Iteration 2: Drama movies - Get the average of Drama Movies // FINALISED
 
 function dramaMoviesRate(movies) {
     var drama = movies.filter((movie) => movie.genre.includes("Drama"));
@@ -38,16 +288,135 @@ function dramaMoviesRate(movies) {
 
 }
 
-console.log(dramaMoviesRate(film))
+// console.log(dramaMoviesRate(film))
 
 
 
-// Iteration 3: Ordering by duration - Order by time duration, ascending (in growing order)
+// // Iteration 3: Ordering by duration - Order by time duration, ascending (in growing order) // FINALISED
+
+function orderByYear(movies) {
+    var sortedArray = movies.sort(function (movie1, movie2) {
+        var movie1Year = movie1.year;
+        var movie2Year = movie2.year;
+        if (movie1Year < movie2Year) {
+            // console.log("first movie is older " + movie1Year + " " + movie2Year);
+            return -1;
+        } else if (movie1Year > movie2Year) {
+            // console.log("first movie is younger " + movie1Year + " " + movie2Year);
+            return +1;
+        } else {
+            // console.log("movies came out the same year " + movie1Year + " " + movie2Year);
+            var movie1Name = movie1.title;
+            var movie2Name = movie2.title;
+            if (movie1Name > movie2Name) {
+                // console.log("the first movie should go after " + movie1Name + " " + movie2Name)
+                return +1;
+            } else if (movie1Name < movie2Name) {
+                // console.log("the first movie should go first " + movie1Name + " " + movie2Name);
+                return -1;
+            }
+        }
+    })
+    // console.log(sortedArray);
+    return sortedArray;
+}
+
+var lol = orderByYear(film);
 
 
-// Iteration 4: Steven Spielberg. The best? - How many movies did STEVEN SPIELBERG direct
+// Iteration 4: Steven Spielberg. The best? - How many movies did STEVEN SPIELBERG direct // FINALISED
+
+function howManyMovies(movies) {
+    var numberOfMovies = 0;
+    movies.filter(function (movie) {
+        if (movie.director === "Steven Spielberg" && movie.genre.includes("Drama")) {
+            // console.log(movie);
+            return numberOfMovies += 1;
+        } else {
+            // console.log("movie is not from Steven Spielberg & is not Drama");
+            return Number(0);
+        }
+    })
+    // console.log(numberOfMovies);
+    return numberOfMovies;
+}
+
+// howManyMovies(film)
+
+// Iteration 5: Alphabetic Order - Order by title and print the first 20 titles // FINALISED
+
+function orderAlphabetically(movies) {
+    var onlyMoviesTitle = []
+    var alphabeticallOrder = movies.sort(function (movie1, movie2) {
+        var title1 = movie1.title;
+        var title2 = movie2.title;
+        if (title1 > title2) {
+            // console.log(title1 + " should be under " + title2);
+            return +1;
+        } else if (title1 < title2) {
+            // console.log(title1 + " should be above " + title2);
+            return -1;
+        }
+    })
+
+    alphabeticallOrder.reduce(function (allmovietitle, movie) {
+        onlyMoviesTitle.push(movie.title);
+    }, []);
+    // console.log(onlyMoviesTitle);
+    if (onlyMoviesTitle.length <= 20) {
+        return onlyMoviesTitle;
+    } else {
+        // console.log(onlyMoviesTitle.slice(0, 20))
+        return onlyMoviesTitle.slice(0, 20)
+    }
+}
+
+// orderAlphabetically (film)
+
+// Iteration 6: Time Format - Turn duration of the movies from hours to minutes // 1 FAILURE
+
+function turnHoursToMinutes(movies) {
+    var changeMovieTime = movies.map(function (movie) {
+        var movieDuration = movie.duration;
+        var hours = Number(movieDuration[0] * 60);
+        var minutes1digit = Number(movieDuration[3]);
+        var minutes2digit = Number(movieDuration[3] + movieDuration[4]);
+        var minutesNoHour = Number(movieDuration[0] + movieDuration[1]);
+        var hoursNoMinutes = Number(movieDuration[0] * 60)
+        var totalTime1 = hours + minutes1digit;
+        var totalTime2 = hours + minutes2digit;
+        var newMovie = movie;
 
 
-// Iteration 5: Alphabetic Order - Order by title and print the first 20 titles
+        if (movieDuration.length === 7) {
+            newMovie.duration = totalTime1;
+            return newMovie;
+        } else if (movieDuration.length === 8) {
+            newMovie.duration = totalTime2;
+            return newMovie;
+        } else if (movieDuration.length === 5) {
+            newMovie.duration = minutesNoHour;
+            return newMovie;
+        } else {
+            newMovie.duration = hoursNoMinutes;
+            return newMovie;
+        }
+    })
+    console.log(changeMovieTime)
+    return changeMovieTime;
+}
 
-// Iteration 6: Time Format - Turn duration of the movies from hours to minutes
+// turnHoursToMinutes(film);
+
+
+// BONUS Iteration: Best yearly rate average
+console.log("------------------------------------------")
+
+function bestYearAvg(movies) {
+    var orderedMovies = orderByYear(movies);
+
+}
+
+// bestYearAvg(film);
+
+// console.log("------------------------------------------")
