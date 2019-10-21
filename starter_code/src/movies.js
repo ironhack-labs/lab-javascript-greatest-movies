8f8061636b5d0bfd60a5f78e4c896cc00b438495
mjarraya <mjarraya@student.42.fr>
2019-10-21 19:36:21 +0200

    order of iterations changed

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,14 +1,14 @@
 /* eslint no-restricted-globals: 'off' */
 
-// Iteration 1: All rates average - Get the average of all rates with 2 decimals
+// Iteration 1: Ordering by year - Order by year, ascending (in growing order)
 
-// Iteration 2: Drama movies - Get the average of Drama Movies
+// Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
 
-// Iteration 3: Ordering by year - Order by year, ascending (in growing order)
+// Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
 
-// Iteration 4: Steven Spielberg. The best? - How many movies did STEVEN SPIELBERG direct
+// Iteration 4: All rates average - Get the average of all rates with 2 decimals
 
-// Iteration 5: Alphabetic Order - Order by title and print the first 20 titles
+// Iteration 5: Drama movies - Get the average of Drama Movies
 
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
 
