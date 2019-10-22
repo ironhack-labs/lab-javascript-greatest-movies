2d6a95d5f1d02150a160cee98905db9f062529c6
Camilosorio <tech@camilo.fr>
2019-10-22 11:36:51 +0200

    All iteration done. The iteration 6 has been corrected with Frank.

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -38,7 +38,7 @@ function orderByYear(arr) {
         if (a.year > b.year) return 1;
         if (a.year < b.year) return -1;
         if (a.year === b.year) {
-            if (b.title > a.title) return -1
+            if (b.title > a.title) return -1;
             else return 1
         };
     })
@@ -49,9 +49,52 @@ console.log(orderByYear(movies))
 
 // Iteration 4: Steven Spielberg. The best? - How many movies did STEVEN SPIELBERG direct
 
+function howManyMovies(arr) {
+    if (!arr.length) return 0;
+    const dramaMovies = arr.filter(film => film.genre.includes("Drama") && film.director.includes("Steven Spielberg"));
+    return dramaMovies.length;
+}
+
+console.log(howManyMovies(movies));
 
 // Iteration 5: Alphabetic Order - Order by title and print the first 20 titles
 
+function orderAlphabetically(arr) {
+    arr.sort(function (a, b) {
+        if (a.title >= b.title) return 1;
+        if (a.title < b.title) return -1;
+    })
+    top20 = arr.slice(0, 20)
+    return top20.reduce((accumulator, currentValue) => [...accumulator, currentValue.title], []);
+}
+
+console.log(orderAlphabetically(movies))
+
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
+
+function turnHoursToMinutes(arr) {
+    const copy = JSON.parse(JSON.stringify(arr));
+    copy.forEach(movie => {
+        let duration = 0;
+        const splittedTime = movie.duration.split(" ");
+        if (splittedTime.length === 1) {
+            let hoursOrMinutes = splittedTime[0];
+            if (hoursOrMinutes.includes("h")) {
+                duration = parseFloat(hoursOrMinutes) * 60;
+                movie.duration = duration;
+                return;
+            }
+            if (hoursOrMinutes.includes("min")) {
+                movie.duration = parseFloat(hoursOrMinutes)
+                return;
+            }
+        }
+        let hours = parseFloat(splittedTime[0]) * 60;
+        let minutes = parseFloat(splittedTime[1]);
+        movie.duration = hours + minutes;
+    })
+    return copy;
+}
+
 
 // BONUS Iteration: Best yearly rate average - Best yearly rate average
