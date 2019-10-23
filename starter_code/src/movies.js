1661f1a23fb36662e2799a19bb77351cfceba7a2
Fernando Servín <tarzanboycraft@hotmail.com>
2019-10-23 15:25:19 -0500

    alpha order

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,19 +1,48 @@
 /* eslint no-restricted-globals: 'off' */
 
 // Iteration 1: All rates average - Get the average of all rates with 2 decimals 
-
+function ratesAverage(arreglo){
+    if(arreglo.length == 0)
+        return 0
+    return + (arreglo.reduce((accumulator, currentValue) => {
+        console.log(accumulator, currentValue)
+        return accumulator + currentValue.rate
+    }, 0)/arreglo.length).toFixed(2)
+}
  
 // Iteration 2: Drama movies - Get the average of Drama Movies
+function dramaMoviesRate(arreglo){
+    return ratesAverage(arreglo.filter((movie) => {
+        return movie.genre.indexOf('Drama') != -1
+    }))
+}
 
 
-// Iteration 3: Ordering by duration - Order by time duration, ascending (in growing order)
+// Iteration 3: Ordering by year - Order by year, ascending (in growing order)
+function orderByYear(arreglo){
+    return arreglo.sort((a, b) => {
+        if(a.year != b.year)
+            return a.year - b.year
+        return a.title < b.title ? -1 : a.title > b.title ? 1 : 0
+    })
+}
 
 
 // Iteration 4: Steven Spielberg. The best? - How many movies did STEVEN SPIELBERG direct
-
+function howManyMovies(arreglo){
+    return arreglo.filter((movie) => {
+        return movie.director == 'Steven Spielberg' && movie.genre.indexOf('Drama') != -1
+    }).length
+}
 
 // Iteration 5: Alphabetic Order - Order by title and print the first 20 titles
-
+function orderAlphabetically(arreglo){
+    return arreglo.map((movie) => {
+        return movie.title
+    }).sort((a, b) => {
+        return a < b ? -1 : a > b ? 1 : 0
+    }).slice(0, 20)
+}
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
 
 // BONUS Iteration: Best yearly rate average - Best yearly rate average
