ee6b25cd535999d3b333bcee2acf34cc482b624b
robbiesanchez <rfsanchez004@gmail.com>
2019-10-24 15:59:41 -0400

    iteration1

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -2,9 +2,54 @@
 
 // Iteration 1: All rates average - Get the average of all rates with 2 decimals 
 
- 
+let newRate = movies.map(num => {
+    return {
+      title: num.title,
+      year: num.year,
+      director: num.director,
+      duration: num.duration,
+      genre: num.genre,
+      rate: Number(num.rate)
+    }
+    })
+    
+    
+    console.log(newRate)
+
+
+ratesAverage = function(rates) {
+    let sumOfRates = rates.reduce(function(a,b){
+      let sum = a + b.rate;
+      return sum
+    },0);
+    average = (sumOfRates / rates.length).toFixed(2);
+    return Number(average);
+  }
+  
+
+  ratesAverage(newRate)
+
+
 // Iteration 2: Drama movies - Get the average of Drama Movies
 
+let dramaMovies = newRate.filter(function(number){
+    return number.genre.includes('Drama')
+  })
+  
+  dramaMoviesRate = function(rates) {
+    if(rates.genre.includes('Drama')) {
+        let sumOfRates = rates.reduce(function(a,b){
+            let sum = a + b.rate;
+            return sum
+          },0);
+          average = (sumOfRates / rates.length).toFixed(2);
+          return Number(average)
+        }
+  }
+  
+  
+  console.log(dramaMoviesRate(dramaMovies))
+  
 
 // Iteration 3: Ordering by duration - Order by time duration, ascending (in growing order)
 
