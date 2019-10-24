155c9ff065892ff2e57bef8492e2f85d47766fa3
Juan Grajales <juanfe.gl11@gmail.com>
2019-10-24 14:44:12 -0400

    add average drama movie rating function

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -4,7 +4,7 @@
 function ratesAverage(arr) {
   let sumOfRating = arr.reduce((sum, curr) => {
     let ratingAsFloat = parseFloat(curr.rate);
-    return sum += ratingAsFloat;
+    return sum + ratingAsFloat;
   }, 0)
 
   let averageRating = sumOfRating/arr.length;
@@ -15,12 +15,71 @@ function ratesAverage(arr) {
 
 // iteration 1: fancy function
 function ratesAverageFancy(arr) {
-  return parseFloat((arr.reduce((sum, curr) => sum += parseFloat(curr.rate), 0)/arr.length).toFixed(2));;
+  return parseFloat((arr.reduce((sum, curr) => sum + parseFloat(curr.rate), 0)/arr.length).toFixed(2));;
 }
 
- 
+let movies2 = [
+  {
+    title: 'The Shawshank Redemption',
+    year: '1994',
+    director: 'Frank Darabont',
+    duration: '2h 22min',
+    genre: ['Crime', 'Drama'],
+    rate: '5'
+  },
+  {
+    title: 'The Godfather',
+    year: '1972',
+    director: 'Francis Ford Coppola',
+    duration: '2h 55min',
+    genre: ['Crime', 'Drama'],
+    rate: '9.2'
+  },
+  {
+    title: 'The Godfather: Part II',
+    year: '1974',
+    director: 'Francis Ford Coppola',
+    duration: '3h 22min',
+    genre: ['Crime', 'Drama'],
+    rate: '9.0'
+  },
+  {
+    title: 'The Lord of the Rings: The Return of the King',
+    year: '2003',
+    director: 'Peter Jackson',
+    duration: '3h 21min',
+    genre: ['Adventure', 'Drama', 'Fantasy'],
+    rate: ''
+  },
+  {
+    title: 'Il buono, il brutto, il cattivo',
+    year: '1966',
+    director: 'Sergio Leone',
+    duration: '3h 2min',
+    genre: ['Western'],
+    rate: '8.9'
+  },
+]
+
 // Iteration 2: Drama movies - Get the average of Drama Movies
 
+function dramaMoviesRate(arr) {
+  // if Drama is found in the genre array then return elements into array
+  let dramaOnly = arr.filter(curr => {
+    return curr.genre.indexOf("Drama") >= 0;
+  })
+
+  let dramaAvgRating = ratesAverage(dramaOnly)
+
+  if(dramaAvgRating >= 0)
+  return dramaAvgRating;
+  else
+  return 0/arr.length;
+}
+
+
+
+console.log(dramaMoviesRate(movies2));
 
 // Iteration 3: Ordering by duration - Order by time duration, ascending (in growing order)
 
