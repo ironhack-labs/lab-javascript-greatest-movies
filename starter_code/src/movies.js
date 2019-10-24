7836f1589d7c1762b4cfc012db19e74c7f07d6bc
Julian Phillips <julian.p@me.com>
2019-10-24 12:59:53 -0400

    Completed 'ratesAverage' function

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,16 +1,51 @@
 /* eslint no-restricted-globals: 'off' */
 
-// Iteration 1: All rates average - Get the average of all rates with 2 decimals 
+// Iteration 1: All rates average - Get the average of all rates with 2 decimals
+function ratesAverage(array) {
+  return (
+    Math.round(
+      (array.reduce((count, eachMovie) => {
+        return count + Number(eachMovie.rate);
+      }, 0) /
+        array.length) *
+        100,
+    ) / 100
+  );
+}
+let testMovies = [
+  {
+    title: 'The Shawshank Redemption',
+    year: '1994',
+    director: 'Frank Darabont',
+    duration: '2h 22min',
+    genre: ['Crime', 'Drama'],
+    rate: '9.3',
+  },
+  {
+    title: 'The Godfather',
+    year: '1972',
+    director: 'Francis Ford Coppola',
+    duration: '2h 55min',
+    genre: ['Crime', 'Drama'],
+    rate: '9.2',
+  },
+  {
+    title: 'The Godfather: Part II',
+    year: '1974',
+    director: 'Francis Ford Coppola',
+    duration: '3h 22min',
+    genre: ['Crime', 'Drama'],
+    rate: '9.0',
+  },
+];
+let testResult = ratesAverage(testMovies);
+console.log(testResult);
 
- 
 // Iteration 2: Drama movies - Get the average of Drama Movies
-
 
 // Iteration 3: Ordering by duration - Order by time duration, ascending (in growing order)
 
-
 // Iteration 4: Steven Spielberg. The best? - How many movies did STEVEN SPIELBERG direct
-
 
 // Iteration 5: Alphabetic Order - Order by title and print the first 20 titles
 
