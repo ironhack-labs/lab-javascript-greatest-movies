8c2b0dc52a83defda78e0c86b7dcd63aecb662e2
Daphne <daphne.feresin@gmail.com>
2019-10-24 22:12:33 -0400

    done from 1-5

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -17,3 +17,74 @@
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
 
 // BONUS Iteration: Best yearly rate average - Best yearly rate average
+
+
+function ratesAverage(arr){
+
+    const ratesTotal = arr.reduce((accumulator, currentRate) => {
+
+    
+    return accumulator + Number(currentRate.rate);
+    },0);
+    
+    let ratesAvg = ratesTotal / arr.length;
+    return Number(ratesAvg.toFixed(2)) || 0;
+    
+    }
+
+    function dramaMoviesRate(arr){
+
+        let dramaMovies = arr.filter((movie) =>{
+          return movie.genre.includes("Drama");
+        });
+        return ratesAverage(dramaMovies);
+        
+        }
+
+
+        function orderByYear(arr){
+
+            let newOrderedArray = arr.sort((a, b) => {
+                if (parseFloat(a.year) < parseFloat(b.year)){
+                    return -1;
+                }
+            
+                if (parseFloat(a.year) > parseFloat(b.year)){
+                    return 1;
+                }
+            
+              if (a.title < b.title) {
+                    return -1;
+                } else if (a.title > b.title) {
+                    return 1;
+                }
+                    return 0;
+            }); 
+            return newOrderedArray;
+            }
+
+            function howManyMovies(arr){
+                var stevenDramaMovies = arr.filter(function(movie){
+                
+                return movie.genre.includes("Drama") && movie.director === ("Steven Spielberg")
+                });
+                return stevenDramaMovies.length;
+              }
+
+
+
+              function orderAlphabetically(arr){
+
+                const orderedByTitleArray = arr.sort((a, b) => {
+                if (a.title < b.title){
+                return -1;
+                    }
+                if (a.title > b.title){
+                return 1;
+                }
+                return 0;
+                });
+                
+                return orderedByTitleArray.slice(0,20).map(eachMovieOrderedByTitle => eachMovieOrderedByTitle.title);
+                
+                }
