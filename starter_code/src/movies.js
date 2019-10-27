3d043d5b14ecde0accab49d8e857fb3b9099a6fd
LIZ <liz@MacBook-Air-de-LIZ.local>
2019-10-27 14:55:25 -0600

    update iteracion 5, 6 bonus

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -57,7 +57,58 @@ function ratesAverage(array){
     return averageRate;
 }
 // Iteration 5: Drama movies - Get the average of Drama Movies
+const dramaMoviesRate = (movies) => {
+    let dramaGenre = movies.filter((movie) => {
+        return movie.genre="Drama"
+    });
 
+    let totalSum = movies.reduce((sum, movie) => //reduce hace la suma
+      sum + Number(movie.rate), 0);
+    let avg = totalSum / movies.length;
+    return Number(avg.toFixed(2));
+}
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
-
+const turnHoursToMinutes = (movies)=>  {
+    return movies.map(movie => {
+      let strDuration = movie.duration
+      let duration
+  
+      if (strDuration.length === "Xh XXmin".length)
+        duration = strDuration[0]*60 + strDuration[3]*10 + strDuration[4]*1
+      
+      if (strDuration.length === "Xh Xmin".length)
+        duration = strDuration[0]*60 + strDuration[3]*1
+      
+      if (strDuration.length === "XXmin".length)
+        duration = strDuration[0]*10 + strDuration[1]*1
+      
+      if (strDuration.length === "Xh".length)
+        duration = strDuration[0]*60
+  
+      return {
+        title: movie.title,
+        year: movie.year,
+        director: movie.director,
+        duration: duration,
+        genre: movie.genre,
+        rate: movie.rate
+      }
+    })
+  }
 // BONUS Iteration: Best yearly rate average - Best yearly rate average
+function bestYearAvg (arr) {
+    if (arr.length === 0) return null
+    if (arr.length === 1) return `The best year was ${arr[0].year} with an average rate of ${arr[0].rate}`
+    let moviesYearAvg = []
+    for (i = 0, l = arr.length; i < l; i++) {
+      let moviesYear = arr.filter(movie => movie.year.includes(arr[i].year))
+      moviesYearAvg.push([ratesAverage(moviesYear), arr[i].year])
+    }
+    moviesYearAvg.sort((a, b) => {
+      if (a[0] === b[0]) {
+        return a[1] > b[1] ? 1 : -1
+      }
+      return b[0] - a[0]
+    })
+    return `The best year was ${moviesYearAvg[0][1]} with an average rate of ${moviesYearAvg[0][0]}`
+  }
