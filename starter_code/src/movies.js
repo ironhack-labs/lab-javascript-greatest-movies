3cfe98b3cbbb7a8a8f533c8dcb0e66321a4bb1b3
Daniel Gonzalez <daniel.gonvie@gmail.com>
2019-10-28 20:49:40 +0100

    Im beginning to think that i'm retarded

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -15,15 +15,31 @@ function howManyMovies(movies) {
 
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically(movies){
-    let secArray =[...movies];
-    let titles = secArray.map(movie => movie.title);
+    let titles = movies.map(movie => movie.title);
     return titles.sort().splice(0,20);
- 
 }
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
+function ratesAverage(movies) {
+    if (movies.length === 0) {
+        return 0;
+    }
+    let sum = movies.reduce((ac, movie) => {
+        if (typeof(movie.rate) != "number") {
+            movie.rate = 0
+        } return movie.rate + ac
+    }, 0)
+      return parseFloat((sum/movies.length).toFixed(2));
+}
 
 // Iteration 5: Drama movies - Get the average of Drama Movies
-
+function dramaMoviesRate(movies){
+    let titles = movies.filter((movie) => movie.genre.includes("Drama"));
+    titles = titles.map(movie => movie.rate);
+    return titles.reduce((ac, cu) => {
+        return ac + cu
+    });
+    console.log(titles);
+}
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
 
 // BONUS Iteration: Best yearly rate average - Best yearly rate average
