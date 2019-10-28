e115149d2a407f54fe107bceef31c6a431acd136
gabriel <gab.zafra@gmail.com>
2019-10-28 15:52:20 +0100

    implemented Steven Rules

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -6,7 +6,9 @@ function orderByYear(arr){
     return sArr.sort((movie1, movie2) => movie1.year > movie2.year ? 1 : -1);
 }
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
-
+function howManyMovies(arr){
+    return arr.filter(movie=> movie.director === `Steven Spielberg` && movie.genre.indexOf(`Drama`) !== -1).length;
+}
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
 
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
