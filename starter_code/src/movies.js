bca40b2299125ff067b2928a07232b69817451a1
Gustavo Jordao <gustavojordao@Gustavos-MacBook-Pro.local>
2019-11-07 21:31:22 -0500

    Work on iterations #1-3

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,10 +1,46 @@
 /* eslint no-restricted-globals: 'off' */
 
 // Iteration 1: Ordering by year - Order by year, ascending (in growing order)
+function orderByYear(arr) {
+    let newOrderedArray = arr.slice(0);
+    newOrderedArray.sort(function (a,b) {
+      if (a.year > b.year) {
+            return 1;
+      } else if (a.year < b.year) {
+            return -1;
+      } else if (a.year === b.year) {
+          if (a.title < b.title) {
+              return -1;
+          } else if (a.title > b.title) {
+              return 1;
+          } else {
+              return 0
+            }
+          }
+      });
+    return newOrderedArray;
+};
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
+function howManyMovies(arr) {
+    let newMovieList = arr.filter(arr1 => arr1.director === "Steven Spielberg" && arr1.genre.includes("Drama") === true);
+    return newMovieList.length;
+}
 
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
+function orderAlphabetically(arr) {
+    let newArrayToOrder = arr.slice(0);
+    newArrayToOrder.sort(function (a,b) {
+        if (a.title > b.title) {
+            return 1;
+        } else if (a.title < b.title) {
+            return -1;
+        } else {
+            return 0;
+        }
+    });
+    return newArrayToOrder;
+  };
 
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
 
