5d2bfa1845f54c3af5420c7f65d9ae13d5f01943
Gustavo Jordao <gustavojordao@Gustavos-MacBook-Pro.local>
2019-11-09 11:46:56 -0500

    Work on iterations 3-5

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -29,7 +29,7 @@ function howManyMovies(arr) {
 
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically(arr) {
-    let newArrayToOrder = arr.slice(0);
+    let newArrayToOrder = [...arr];
     newArrayToOrder.sort(function (a,b) {
         if (a.title > b.title) {
             return 1;
@@ -39,13 +39,31 @@ function orderAlphabetically(arr) {
             return 0;
         }
     });
-    return newArrayToOrder;
-  };
+    return newArrayToOrder.map((eachMovie) => {
+      return eachMovie.title;
+    }).slice(0, 20);
+};  
 
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
+function ratesAverage(arr) {
+    if (!arr.length) {
+      return 0;
+    };
+    return Number((arr.reduce((a,b) => {
+        if (b.rate) {
+            return a + b.rate;
+        } else {
+            return a
+        };
+    }, 0) / arr.length).toFixed(2));
+  };
 
 // Iteration 5: Drama movies - Get the average of Drama Movies
-
+function dramaMoviesRate(arr) {
+    let newMovieList2 = arr.filter(arr1 => arr1.genre.includes("Drama") === true);
+      return ratesAverage(newMovieList2);
+  };
+  
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
 
 // BONUS Iteration: Best yearly rate average - Best yearly rate average
