bf227a01fbb21a03f4179ade72ee2faf7c78baad
Evgeny Galitskiy <gha665@gmail.com>
2019-11-09 10:01:01 -0500

    Complete Iterations 1, 2, 3

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -2,9 +2,69 @@
 
 // Iteration 1: Ordering by year - Order by year, ascending (in growing order)
 
+function orderByYear(greatestMovies) {
+  const ascMovies = greatestMovies.slice().sort(function(a, b) {
+    if (a.year === b.year) {
+      if (a.title < b.title) {
+        return -1;
+      }
+      if (a.title > b.title) {
+        return 1;
+      }
+      return 0;
+    }
+    return a.year - b.year;
+  });
+  return ascMovies;
+}
+
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
 
+function howManyMovies(greatestMovies) {
+  const spielbergMovies = greatestMovies.slice().filter(function(spiel) {
+    if (
+      spiel.director === "Steven Spielberg" &&
+      spiel.genre.indexOf("Drama") != -1
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+  return spielbergMovies.length;
+}
+
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
+function orderAlphabetically(greatestMovies) {
+  // this part sorts the segment
+  let sortedMovies = greatestMovies.slice().sort(function sort20(a, b) {
+    if (a.title < b.title) {
+      return -1;
+    }
+    if (a.title > b.title) {
+      return 1;
+    }
+    return 0;
+  });
+
+  // this part gets a segment
+  let first20 = [];
+  for (i = 0; i < 20; i++) {
+    if (i >= sortedMovies.length) {
+      break;
+    }
+    first20.push(sortedMovies[i]);
+  }
+
+  // this returns result titles
+  let titles = first20.map(function(el) {
+    return el.title;
+  });
+
+  //console.log(first20);
+  return titles;
+}
+//return greatestMovies.title;
 
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
 
