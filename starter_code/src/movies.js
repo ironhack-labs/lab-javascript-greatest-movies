94aab7a1ed380205fa70973d2199d59a3b6626cb
mario010203 <mariojose1490@hotmail.com>
2019-11-19 19:13:15 +0100

    first iteration done

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -2,6 +2,19 @@
 
 // Iteration 1: Ordering by year - Order by year, ascending (in growing order)
 
+function orderByYear(movies) {
+  let mapper = movies.map(element => {
+    return element;
+  });
+  mapper.sort((a, b) => {
+    if (a.year > b.year) return 1;
+    if (a.year < b.year) return -1;
+    if (a.year == b.year && a.title > b.title) return 1;
+    if (a.year == b.year && a.title < b.title) return -1;
+  });
+  return mapper;
+}
+
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
 
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
