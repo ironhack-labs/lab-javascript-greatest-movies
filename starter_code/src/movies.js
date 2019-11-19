a4d0b4474c88f70f2b6a1b52c9879ca43dd35bd6
Ruben Vaquero <rvaquero@elpublicamaleon.com>
2019-11-19 21:46:08 +0100

    iteration 2 finish

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -29,6 +29,22 @@ function orderByYear(arrayFilm){
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
 
+function howManyMovies(object){
+
+  let filmSpielberg = object.filter(function(film){
+
+    if(film.director == "Steven Spielberg" && film.genre.includes("Drama")){
+
+      return film;
+
+    }
+
+  })
+
+  return filmSpielberg.length;
+
+}
+
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
 
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
