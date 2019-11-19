eec82dcf2e2215373841a15a458b22c98e32ff2a
hbuniotto <hbuniotto@gmail.com>
2019-11-19 15:44:00 -0500

    Done with Erick Sicard

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,68 +1,121 @@
 /* eslint no-restricted-globals: 'off' */
 
-// Iteration 1: Ordering by year - Order by year, ascending (in growing order)
+/************** TOOLS ****************/
+// Utility function to sorted an array
+function ordered ( val1, val2 ) {
+    return typeof val1 == 'number' && typeof val2 == 'number' ? val1 - val2 : val1.localeCompare(val2);
+}
 
-function orderByYear(arrayOfMovies) { // create function to order, receiving the unordered array as an argument
-    const copyOfArray = [...arrayOfMovies]; // declare a variable to store the copy of the original array
-    copyOfArray.sort(function(a, b) { // used .sort method to order the array by year, taking in a function with 1-2, 2-3, 3-4
-        if (a.year - b.year == 0) { // if year is the same (==0) sort by name
-            if (a.title < b.title) { // when the title of A starts with an earlier letter 
-                return -1;
-            }
-            if (a.title < b.title) {
-                return 1;
-            }
-            else {
-                return 0;
-            }
-        }
-        return a.year - b.year; // returns 0, 1, or -1, which determines the order in the new array
-    })
-    return copyOfArray; // returns the newly-created array
-};
+// Utility function to format time
+function timeFormat( acc, cur ) {
+    if ( cur.includes('min')) cur = parseInt( cur )
+    else if ( cur.includes('h')) { cur = parseInt( cur ) * 60 }
+    return acc + cur;
+}
+/************************************/
+
+
+// Iteration 1: Ordering by year - Order by year, ascending (in growing order)
+function orderByYear( moviesArr) {
+    let orderedArray = [...moviesArr];
+    
+    orderedArray.sort( (elm1, elm2) => { 
+        if ( elm1.year - elm2.year == 0 ) return ordered (elm1.title, elm2.title)
+        return ordered( elm1.year, elm2.year);
+    });
+
+    return orderedArray;
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
+function howManyMovies( arr ) {
+    return arr.filter( elm => elm.genre.includes('Drama') && elm.director.toUpperCase() === "STEVEN SPIELBERG").length;
+}
 
-function howManyMovies(arrayOfMovies) {
-    const filteredArrayofMovies = arrayOfMovies.filter(movie => {
-        let movieCategory = movie.genre.includes('Drama');
-         if(movie.director === "Steven Spielberg" && movieCategory) {
-             return movie;
-         }
-     });
-     return filteredArrayofMovies.length;
- };
+// Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
+function orderAlphabetically( moviesArr ) {
+    let orderedArray = [...moviesArr];
 
-// // howManyMovies(movies);
+    let onlyFirst20Titles = orderedArray
+            .sort( ( elm1, elm2 ) => ordered( elm1.title, elm2.title))
+            .map( elm => elm.title)
+            .filter( (elm, idx) => idx < 20 );
+            
+    return onlyFirst20Titles;
+}
 
-// // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
 
-const orderAlphabetically = (arrayOfMovies) => {
-    const movieTitles = [];
-    const alphabeticallyOrderedArray = arrayOfMovies.sort(function (a, b) {
-        if (a.title[0] > b.title[0]) {
-            return -1;
-        }
-        if (a.title[0] < b.title[0]) {
-            return 1;
-        }
-        if (a.title[0] === b.title[0]) {
-            return 0;
-        }
-    })
-    return alphabeticallyOrderedArray;
-}; // couldn't figure out how to return the array element (title) and only 20;
-
-// // Iteration 4: All rates average - Get the average of all rates with 2 decimals
-
-function ratesAverage(arrayOfMovies) {
-    arrayOfMovies.rate.reduce((acc, val) => {
-        return acc + val / arrayOfMovies.length
-      }, 0)
-}; // still getting erros but I believe this should work
+// Iteration 4: All rates average - Get the average of all rates with 2 decimals
+function ratesAverage( moviesArr ) {
+    if ( moviesArr.length == 0 ) return 0;
+    
+    // Sum of all movies' rate
+    let ratesSum = moviesArr.reduce( function(acc, cur) {
+        if ( !cur.rate ) cur.rate = 0;
+        return acc + cur.rate;
+    }, 0);
+    
+    // Average
+    let avg = Math.round( (ratesSum / moviesArr.length) * 100) / 100  ;
+    return avg;    
+}
 
 // Iteration 5: Drama movies - Get the average of Drama Movies
+function dramaMoviesRate( moviesArr ) {
+    let dramaMovies = moviesArr.filter( el => el.genre.includes("Drama"));
+    return ratesAverage(dramaMovies);
+}
 
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
+function turnHoursToMinutes( moviesArr ) {
+    // creating a shadow copy of an array of objects
+    let localArr = moviesArr.map( obj => {
+        const newObj = {...obj};
+        return newObj;
+    })
+    // changing the movies' duration format
+    let moviesArrFormatted = localArr
+                                .map( elm => {
+                                    elm.duration = elm.duration
+                                                    .split(' ')
+                                                    .reduce( timeFormat, 0)
+                                    return elm;
+                                });
+
+    return moviesArrFormatted;
+}
 
 // BONUS Iteration: Best yearly rate average - Best yearly rate average
+function bestYearAvg( moviesArr ) {
+    if ( moviesArr.length == 0 ) return null;
+
+    let arrYearAvgRate = [];
+    
+    // finding unique values of years
+    let uniqueYears = moviesArr
+                        .map( el => el.year )
+                        .filter( (el, idx, arr) => arr.indexOf(el) == idx);
+
+    
+    // calculating average rate per year
+    for ( let i = 0; i < uniqueYears.length; i++ ) {
+        let moviesPerYear = [];
+        let avg = 0;
+
+        for ( let j = 0; j < moviesArr.length; j++) {
+            if ( uniqueYears[i] === moviesArr[j].year  ) {
+                moviesPerYear.push( moviesArr[j] )
+            }
+        }
+        
+        avg = ratesAverage( moviesPerYear );
+        arrYearAvgRate.push( {
+            year: uniqueYears[i],
+            avg
+        })
+    }
+    // sorting the array by rate average value
+    arrYearAvgRate.sort( ( elm1, elm2 ) => ordered( elm1.avg, elm2.avg ))
+    return `The best year was ${arrYearAvgRate[arrYearAvgRate.length - 1].year} with an average rate of ${arrYearAvgRate[arrYearAvgRate.length - 1].avg}`;
+    
+}
