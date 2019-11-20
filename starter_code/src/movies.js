5e0fbe32ff91e80813457c954a29c228f7b07329
Gonzalo Hernández <gonzalohernandez@icloud.com>
2019-11-20 09:23:21 +0100

    refactoring

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -19,7 +19,7 @@ function orderByYear([...movies]){
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
 
 function howManyMovies(movies){
-  return movies.filter( e => e.director === 'Steven Spielberg' && e.genre.some( g => g === 'Drama' ) ).length;
+  return movies.filter(movie => movie.director === 'Steven Spielberg' && movie.genre.some(genre => genre === 'Drama')).length;
 }
 
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
@@ -32,13 +32,8 @@ function orderAlphabetically([...movies]){
       return -1;
     }
   })
-
-  let titles20 = [];
-  for (i = 0; i < 20; i++){
-    if (typeof orderArr[i] != 'undefined') titles20.push(orderArr[i].title);
-  }
-
-  return titles20;
+  
+  return orderArr.slice(0, 20).map(movie => movie.title);
 }
 
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
@@ -62,21 +57,8 @@ function ratesAverage(movies){
 // Iteration 5: Drama movies - Get the average of Drama Movies
 
 function dramaMoviesRate(movies){
-  let dramaMovies = movies.filter( e => e.genre.some( g => g === 'Drama' ) );
-  
-  let rates = dramaMovies.reduce( (acc, current) => {
-    if (typeof current.rate == 'number'){
-      return acc + current.rate;
-    } else {
-      return acc;
-    }
-  }, 0 );
-
-  if (rates > 0){
-    return parseFloat( (rates / dramaMovies.length).toFixed(2) );
-  } else {
-    return 0;
-  }
+  let dramaMovies = movies.filter(movie => movie.genre.some(genre => genre === 'Drama'));
+  return ratesAverage(dramaMovies);
 }
 
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
@@ -84,10 +66,10 @@ function dramaMoviesRate(movies){
 function turnHoursToMinutes(movies){
   let moviesArr = JSON.parse(JSON.stringify(movies));
 
-  return moviesArr.map( movie => {
+  return moviesArr.map(movie => {
     let time = movie.duration.split(' ');
 
-    let minutes = time.reduce( (acc, current) => {
+    let minutes = time.reduce((acc, current) => {
       if (current.indexOf('h') != -1) {
         return parseInt(current.replace('h', '')) * 60;
       } else {
@@ -114,15 +96,14 @@ function bestYearAvg(movies){
     }
   })
 
-  let yearsAverageKeys = Object.keys(yearsAverage);
-  yearsAverageKeys.forEach(year => {
-    let average = yearsAverage[year].reduce( (acc, current) => {
+  for (year in yearsAverage){
+    let average = yearsAverage[year].reduce((acc, current) => {
       return acc + current / yearsAverage[year].length;
     }, 0);
     yearsAverageArr.push({ 'year' : year, 'average' : parseFloat(average.toFixed(2)) });
-  })
+  }
 
-  yearsAverageArr.sort( (a, b) => {
+  yearsAverageArr.sort((a, b) => {
     return b.average - a.average;
   })
 
