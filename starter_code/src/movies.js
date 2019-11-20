578e4601b191e5d4bf4e450a00fb1e6551438396
mario010203 <mariojose1490@hotmail.com>
2019-11-20 13:29:03 +0100

    3rd ite almost done

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -17,20 +17,34 @@ function orderByYear(movies) {
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
 function howManyMovies(movies) {
-  let fil = movies.slice().filter(
-    drama =>
-      drama.genre.includes("Drama") && drama.director === "Steven Spielberg"
-  );
+  let fil = movies
+    .slice()
+    .filter(
+      drama =>
+        drama.genre.includes("Drama") && drama.director === "Steven Spielberg"
+    );
 
   return fil.length;
 }
 
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
 
+function orderAlphabetically(movies) {
+  let order = movies.map(element => {
+    return element.title;
+  });
+
+  movies.sort((a, b) => {
+    if (a > b) return 1;
+    if (a < b) return -1;
+    if (a === b) return 0;
+  });
+  return order.splice(0, 20);
+}
+
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
 
 // Iteration 5: Drama movies - Get the average of Drama Movies
-
 
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
 
