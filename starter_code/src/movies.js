a22dea6705c71e8bfb9948acf72186536c6ec968
J. A. Hernández <joseanher81@gmail.com>
2019-11-21 18:49:29 +0100

    Iteration 4 and 5

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -8,13 +8,9 @@ function orderByYear(films) {
 
   // Sort the copied array by year and by title
   filmsClone.sort(function(a, b) {
-      if (a.year === b.year && a.title.toUpperCase() > b.title.toUpperCase()) {
-          return 1;
-      }
+      if (a.year === b.year && a.title.toUpperCase() > b.title.toUpperCase()) return 1;
 
-      if (a.year === b.year && a.title.toUpperCase() < b.title.toUpperCase()) {
-          return -1;
-      }
+      if (a.year === b.year && a.title.toUpperCase() < b.title.toUpperCase()) return -1;
 
       return a.year - b.year;
   });
@@ -51,8 +47,47 @@ function orderAlphabetically(films) {
 }
 
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
+function roundTwo(num) {
+  return Math.round(num * 100) / 100;
+}
+
+function ratesAverage(films) {
+
+  if (films.length < 1) return 0;
+
+  // Clone to not mutate the original array
+  filmsClone = JSON.parse(JSON.stringify(films));
+
+  // Sum up all rates
+  let totalRating = filmsClone.reduce(function(acc, e, i) {
+
+      if (i === 1) {
+          return (parseFloat(acc.rate || 0) + parseFloat(e.rate || 0)).toFixed(2);
+      }
+
+      return (parseFloat(acc || 0) + parseFloat(e.rate || 0)).toFixed(2);
+
+  });
+
+  // Return average rating
+  return parseFloat((totalRating / filmsClone.length).toFixed(2));
+
+}
 
 // Iteration 5: Drama movies - Get the average of Drama Movies
+function dramaMoviesRate(films) {
+
+  // Filter the array
+  var dramaFilms = films.filter(function(film) {
+      return (film.genre.includes('Drama'));
+  });
+
+  // If there is only one drama film
+  if (dramaFilms.length === 1) return dramaFilms[0].rate;
+
+  // Calculate average
+  return ratesAverage(dramaFilms);
+}
 
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
 
