83ad3081504489b11bb4010c50bc234951525f55
Carlos Mateo <cmc2k5@gmail.com>
2019-11-23 22:17:26 +0100

    Iteration 5 - Completed

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -21,8 +21,33 @@ function howManyMovies(movies) {
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
 
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
+function ratesAverage(movies) {
+
+    if (!movies.length) {
+        return 0;
+    }
+
+    return parseFloat((movies
+        .reduce((acc, m) => {
+            return acc + (m.rate || 0)
+        }, 0) / movies.length)
+        .toFixed(2));
+}
 
 // Iteration 5: Drama movies - Get the average of Drama Movies
+function dramaMoviesRate(movies) {
+
+    if (!movies.length) {
+        return 0;
+    }
+
+    let dramaMoviesAvg = movies
+        .filter((movie) => movie.genre.includes('Drama'))
+        .reduce((acc, m) => { return acc + (m.rate || 0) }, 0) / movies.length;
+
+    return parseFloat((dramaMoviesAvg).toFixed(2));
+}
+
 
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
 
