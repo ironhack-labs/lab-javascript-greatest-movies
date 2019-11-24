57b61de67b2d9abf67af887a400a52fcf2b11654
Grecia Patricia <grecia.busquets@gmail.com>
2019-11-24 22:13:57 +0100

    No he podido seguir con la práctica

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -2,8 +2,40 @@
 
 // Iteration 1: Ordering by year - Order by year, ascending (in growing order)
 
+function orderByYear (yearArray) {
+    let arr = []
+
+    arr.sort(function(a,b){
+    if (a.year == b.year) {
+        
+        if (a > b) {
+          return 1;
+        } 
+        else if (a < b) {
+         return -1;
+        }
+        return 0;
+    }
+      return a.year-b.year;
+    })
+      return arr;
+    }
+
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
 
+function howManyMovies (movies) {
+    
+    const SpielbergMovies = movies.filter(function(theMovies) {
+
+        if (theMovies.director == "Steven Spielberg" && film.genre.includes("Drama")){
+            return theMovies;
+        }
+    })
+
+    return SpielbergMovies.length;
+  
+  }
+  
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
 
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
