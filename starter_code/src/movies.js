2c3744096abc316975578700307e1150f6c42344
Luismi Feijoo <luism.feijoo26@gmail.com>
2019-11-24 22:29:57 +0100

    Format code

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -2,70 +2,69 @@
 
 // Iteration 1: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear(arr) {
-  let movies = arr;
+  let movies = arr.map(a => a);
 
   if (movies.length != 0) {
-  
-    arr.sort( (a,b) => {
+    movies.sort((a, b) => {
       if (a.year != b.year) {
         return a.year - b.year;
       } else {
         return a.title.localeCompare(b.title);
       }
-    })
+    });
 
-  return movies
+    return movies;
   }
-  
 }
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
 function howManyMovies(arr) {
-  let movies = arr.map(a => a)
+  let movies = arr.map(a => a);
 
-  return movies.filter( movie => {
-    return (movie.director == 'Steven Spielberg') && (movie.genre.indexOf("Drama") >= 0)
-  }).length
-
+  return movies.filter(movie => {
+    return (
+      movie.director == "Steven Spielberg" && movie.genre.indexOf("Drama") >= 0
+    );
+  }).length;
 }
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically(arr) {
   let movies = arr.map(a => a);
 
-  movies.sort((a,b) => {
-    return (a.title).localeCompare(b.title)
-  })
-  
-  return movies.map( e => e.title).filter( (e,i) => i < 20)
+  movies.sort((a, b) => {
+    return a.title.localeCompare(b.title);
+  });
+
+  return movies.map(e => e.title).filter((e, i) => i < 20);
 }
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
-function ratesAverage(arr){
-  let movieRates = arr.map( a => a.rate)
-
+function ratesAverage(arr) {
+  let movieRates = arr.map(a => a.rate);
 
   if (movieRates.length === 0) {
-    return 0
+    return 0;
   }
 
   //I used toFix() for the 2 decimal points and then parseFloat() to make it a number again
-  return parseFloat((movieRates.reduce( (acc, e) => {
-    if (typeof(e) === 'number') {
-      return acc + e;
-    } else {
-      return acc;
-    }
-    
-  })/(movieRates.length)).toFixed(2))
-  
+  return parseFloat(
+    (
+      movieRates.reduce((acc, e) => {
+        if (typeof e === "number") {
+          return acc + e;
+        } else {
+          return acc;
+        }
+      }) / movieRates.length
+    ).toFixed(2)
+  );
 }
 // Iteration 5: Drama movies - Get the average of Drama Movies
 function dramaMoviesRate(arr) {
   // create an array with only the drama movies
-  let dramaMovies = arr.filter( e => {
-    return (e.genre.indexOf('Drama')) >= 0
-  })
+  let dramaMovies = arr.filter(e => {
+    return e.genre.indexOf("Drama") >= 0;
+  });
 
-  return ratesAverage(dramaMovies)
-
+  return ratesAverage(dramaMovies);
 }
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
 
