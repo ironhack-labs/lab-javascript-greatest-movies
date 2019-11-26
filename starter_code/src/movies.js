7047b008bbea595866f7870fd051fc24b44716ad
Pilar <p.garciacampo@gmail.com>
2019-11-26 21:01:37 +0100

    Add first iteration function

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,6 +1,23 @@
 /* eslint no-restricted-globals: 'off' */
 
 // Iteration 1: Ordering by year - Order by year, ascending (in growing order)
+function orderByYear(arr) {
+  var newArray = arr.map(function(e) {
+    return e;
+  });
+  newArray.sort(function(a, b) {
+    if (a.year === b.year) {
+      if (a.title > b.title) {
+        return 1;
+      } else if (a.title < b.title) {
+        return -1;
+      }
+      return 0;
+    }
+    return a.year - b.year;
+  });
+  return newArray;
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
 
