bd92a678ac9d79cc3acb977b129295437263474e
Fernando Lima Borrelli <flborrelli@gmail.com>
2019-11-29 21:01:57 -0300

    1st commit

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -3,29 +3,37 @@
 // Iteration 1: Ordering by year - Order by year, ascending (in growing order)
 
 const orderByYear = (array) => {
-    let newArray = array.map(number => number)
+    let newArray = array.map(element => element)
     newArray.sort((a,b) => {
         if (a.year === b.year) {
             return a.title.localeCompare(b.title)
         }else {
-           return a.year - b.year
+        return a.year - b.year
         }
     });
-    return newArray
+    return newArray;
 }
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
 
 
 const howManyMovies = (array) => {
-
-  const newArray = array.filter(element => element.genre.includes('Drama') === true && element.director === 'Steven Spielberg')
-  return newArray.length
+    const newArray = array.filter(element => element.genre.includes('Drama') === true && element.director === 'Steven Spielberg')
+    return newArray.length
 } 
 
-
-
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
+const orderAlphabetically = arr => {
+  const newArray = arr.map(element => element.title);
+  newArray.sort((a, b) => {
+    return a.localeCompare(b);
+  });
+  const newArray2 = newArray.filter((element, i) => {
+    return i < 20;
+  });
+  return newArray2;
+};
+
 
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
 
