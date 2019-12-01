7611258f1b1a13f119e870eb25cc52ddf3970123
Fernando Lima Borrelli <flborrelli@gmail.com>
2019-12-01 01:16:47 -0300

    "Up to interation 4 done"

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -34,8 +34,16 @@ const orderAlphabetically = arr => {
   return newArray2;
 };
 
-
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
+const ratesAverage = (arr) => {
+  if(arr.length === 0){
+    return 0;
+  }
+  const arrayOfMoviesWithRates = arr.filter(element => typeof element.rate !== 'undefined');
+  const sumOfRates = arrayOfMoviesWithRates.reduce((sum, element) => sum += element.rate, 0);
+  let averageRound = Math.round((sumOfRates / arr.length) * 100) / 100; //Get the average and round it to 2 decimals
+  return averageRound;
+};
 
 // Iteration 5: Drama movies - Get the average of Drama Movies
 
