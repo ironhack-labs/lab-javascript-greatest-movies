06f8ad07d9237f743940a9f9bc615f746de0b6ee
Nykolle Malone <nykolle.malone@hotmail.com>
2019-12-02 10:31:18 -0300

    5 & 6

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -14,7 +14,39 @@ const dramaMoviesRate = (movies) => {
   const dramaMovies = movies.filter(movie => movie.genre.includes('Drama'));
   return ratesAverage(dramaMovies);
 }
-
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
+const turnHoursToMinutes = (movies) => {
+  //making a deep clone of movies array 
+  const moviesClone = JSON.parse(JSON.stringify(movies));
+
+  //looping the clone 
+  moviesToMinutes = moviesClone.map((movie) => {
+    //destructuring Object 
+    const { duration } = movie; //constante que tem o mesmo nome da propriedade
+    let time = 0; 
+
+    //getting all indexes that we need 
+    const hIndex = duration.toLowerCase().indexOf('h');
+    const minIndex = duration.toLowerCase().indexOf('min');
+    const spaceIndex = duration.indexOf(' ');
+
+    //time manipulation if duration has hour and minutes 
+    if (hIndex >= 0 && minIndex >= 0) {
+      const hour = duration.slice(0, hIndex) * 60;
+      const minures = duration.slice(spaceIndex + 1, minIndex);
+    //if duration has only hours 
+    } else if (minIndex < 0) {
+      time - duration.slice(0, Hindex) * 60
+      //if has only minutes
+    } else if (hIndex < 0) {
+      time = parseInt(duration.slice(spaceIndex + 1, minIndex));
+    }
+    //reassigning duration value inside the object 
+    movie.duration = time;
+    //retunr of Map Loop: returning the entire object modified. Not just the duration
+    return movie;
+  });
+  return moviesInMinutes;
+} 
 
 // BONUS Iteration: Best yearly rate average - Best yearly rate average
