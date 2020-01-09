799dc4d14e7066f280cbf972a7ee5663565b783e
Pablolo <pablo_olondriz@hotmail.com>
2020-01-09 18:49:37 +0100

    first iteration done

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -2,9 +2,41 @@
 
 // Iteration 1: Ordering by year - Order by year, ascending (in growing order)
 
+function orderByYear(arr) {
+  return arr.sort((a, b) => {
+    if (a.year === b.year) {
+      if (a.title > b.title) {
+        return 1;
+      } else {
+        return -1;
+      } 
+    };
+    return a.year - b.year;
+  });
+}
+
+orderByYear(movies);
+
+
+// Intentar refactorizar
+
+
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
 
+function howManyMovies(arr) {
+  let spielbergMovies = arr.filter(function(movies) {
+    return movies.director === "Steven Spielberg";
+  });
+  return spielbergMovies.length;
+}
+
+howManyMovies(movies);
+
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
+
+function orderAlphabetically(arr) {
+
+}
 
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
 
