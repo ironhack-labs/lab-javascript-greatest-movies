1d3ab66893cb810278aa09991d73c13034b728fe
CarlaOnate <carlaonategardella@gmail.com>
2020-01-09 22:21:17 -0600

    Asi quedo este rollo

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -2,8 +2,17 @@
 
 // Iteration 1: Ordering by year - Order by year, ascending (in growing order)
 const orderByYear = (array) => {
-    const sorted = obj["year"].sort( (a,b)=> a-b).join('')
-    const newArray = array.map(obj =>  newArray)
+    const newArray = [...array] //guarda array nueva
+    const sorted = newArray.sort((a,b)=> {
+        if (a.year === b.year){
+            // newArray.sort((a,b =>
+        } else {
+
+        }
+        a.year-b.year
+    })
+
+    return sorted
     // (a,b) => a-b)
 }
 
@@ -17,8 +26,7 @@ const howManyMovies = array => { const newArray = array.filter(obj => {
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
 
 const orderAlphabetically = array => {
-    const newArray = array.sort().slice(0,20).map(obj => obj.title.toLowerCase())
-    return newArray
+    return array.map(obj => obj.title).sort().slice(0,20)
 }
 
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
@@ -26,12 +34,14 @@ const orderAlphabetically = array => {
 const ratesAverage = array => {
     let counter = 0;
     array.forEach(value => {
-    counter += value.rate;
+    if(value.rate != null){
+        counter += value.rate;
+    }
 })
     if (array.length === 0){
         return 0
     } else {
-        return (counter/array.length.toFixed(2))
+        return Number(parseFloat(counter/array.length).toFixed(2))
     }
 }
 // Iteration 5: Drama movies - Get the average of Drama Movies
@@ -49,5 +59,26 @@ const dramaMoviesRate = array => {
 
 
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
+
+const turnHoursToMinutes = array => {
+    let hours = 0;
+    let minutes = 0;
+    const newArr = array.map(obj => { //Aqui mapeamos para generar un nuevo arreglo con los valores
+        for(i=0; i<obj.duration.length; i++){ //Hacemos un for que saca los numeros del texto duration
+            if(obj.duration[i].parseInt() === Number){
+              hours += obj.duration //agrega el string de numeros a variable horas
+            }
+        }
+        if(obj.duration.includes('h')){ //si es h multiplica el primer numero del string por 60 y le suma lo demas
+            minutes = hours[0].parseInt()*60 + hours.slice(1).parseInt() //guarda minutes en variable minutes
+        } else {
+            minutes = obj.duration.parseInt()
+            console.log(minutes)
+        }
+        obj["duration"] = minutes //cambia el valor de obl.duration
+        return obj //regresa el objeto con el cambio, y se va guardando en un nuevo array que es el que se regresa al final
+    })
+    return newArr //aqui regresamos arreglo con objetos
+}
 
 // BONUS Iteration: Best yearly rate average - Best yearly rate average
