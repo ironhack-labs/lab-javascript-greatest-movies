ba53ac893e41d42cb9ef2a28198962bf17028210
alex <alex@PC-AZUL.localdomain>
2020-01-10 15:13:24 +0100

    firts interactions

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -2,6 +2,26 @@
 
 // Iteration 1: Ordering by year - Order by year, ascending (in growing order)
 
+//para ordenar alfabeticamente hacer uso del metodo array.sort()
+
+function orderByYear(array){
+    const newArray = []
+
+    array.sort((num1, num2) => num1 - num2);
+    console.log(array)
+    /*
+    for (let i=0; i<array.length; i++){
+        if(array[i].movies<newArray[i]){
+            newArray.push(array[i])
+        } else{
+
+        }
+    }
+    */
+    return newArray
+}
+
+
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
 
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
