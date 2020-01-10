c10a67a59f7f1cb20fe00d5412d3ab8dcff671c3
Jaime <jaimemoravallejo@gmail.com>
2020-01-10 14:43:13 +0100

    iteration 2 and 3 done

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -31,8 +31,8 @@ function howManyMovies(movies){
   if(!movies){
     return 0;
   }
-  let dramaMovies = movies.filter(element => element.genre == 'drama');
-  let dramaMoviesSpielberg = dramaMovies.filter(element => element.director == 'Spielberg');
+  let dramaMovies = movies.filter(element => element.genre.includes("Drama"));
+  let dramaMoviesSpielberg = dramaMovies.filter(element => element.director == 'Steven Spielberg');
   return dramaMoviesSpielberg.length;
 
 }
@@ -40,20 +40,40 @@ function howManyMovies(movies){
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
 
 function orderAlphabetically(movies) {
-   const moviesByAlphabet = [];
-   movies.sort(function comparar(a, b) {
-     if (a.title > b.title) {
+    let moviesOrder = movies.map(x => x);
+   
+   moviesOrder.sort(function comparar(a, b) {
+     if (a.title < b.title) {
        return -1;
      }
-     if (a.title < b.title) {
+     if (a.title > b.title) {
        return 1;
      }
      return 0;
    });
-   return movies;
+
+   let moviesByAlphabet = moviesOrder.map(x => x.title);
+   if( moviesByAlphabet.length > 20 ){
+     moviesByAlphabet = moviesByAlphabet.splice(0,20)
+    }
+
+   return moviesByAlphabet;
 }
 
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
+
+
+function ratesAverage(movies) {
+  
+  let totalAverage = 0;
+  let totalMovies = movies.length;
+  let averageNum = movies.reduce(average(accumulator, currentValue.rate))
+
+  totalAverage = averageNum / totalMovies;
+  totalAverage = average.toFixed(2);
+  console.log(average);
+  return average;
+}
 
 // Iteration 5: Drama movies - Get the average of Drama Movies
 
