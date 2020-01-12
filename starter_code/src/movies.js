6eb98618881f12e62f30ab06cf5f0e151c303df3
Marta Mullor <Marta@MacBook-Air-de-marta.local>
2020-01-12 12:17:30 +0100

    Iterations 1 done with errors  and 2 done

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -2,18 +2,32 @@
 
 // Iteration 1: Ordering by year - Order by year, ascending (in growing order)
 
+
 let year = 0;
 let yearArray = [];
+let movieTitle = "";
+let movieTitleArray = [];
 
 function orderByYear(array){
   for (let i = 0; i < array.length; i++){
     year = array[i].year;
+    movieTitle = array[i].title;
     yearArray.push(year);
+    movieTitleArray.push(movieTitle);
+    yearArray.sort(function ascendingOrder(movie1, movie2){
+      return movie2 - movie1;
+    });
   }
-  return yearArray.sort();
+  return yearArray;
 }
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
+
+function howManyMovies(movies) {
+  const spielbergDramaMovies = movies.filter(movie => (movie.director === 'Steven Spielberg') && (movie.genre.includes('Drama')));
+
+  return spielbergDramaMovies.length;
+}
 
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
 
