ce5591238c159ea1717149d1d8c8f9f04d4e2318
Javier Varela <jvarela@strands.com>
2020-01-13 16:37:27 +0100

    Iteration 2: Steven Spielberg how many dramas

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,11 +1,10 @@
 /* eslint no-restricted-globals: 'off' */
 
 // Iteration 1: Ordering by year - Order by year, ascending (in growing order)
-const orderByYear = (movies) => {
-    return movies.slice(0).sort((a, b) => a.year !== b.year ? a.year - b.year : (a.title > b.title) ? 1 : -1);
-}
+const orderByYear = (movies) => movies.slice(0).sort((a, b) => a.year !== b.year ? a.year - b.year : (a.title > b.title) ? 1 : -1);
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
+const howManyMovies = (movies) => movies.filter(movie => movie.director === 'Steven Spielberg' && movie.genre.includes('Drama')).length;
 
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
 
