a63108df409030c2714bdda4a3c47b75a98e1867
Leinadm <danielmonteru@gmail.com>
2020-01-14 23:25:27 +0100

    Iteration 3

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -22,14 +22,31 @@ function orderByYear(movies) {
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
 
 function howManyMovies(movies) {
-    if (movies.length === 0) {
-        return 0;
-    }
-    
-    return 1;
-} 
+
+    let filteredMovies = movies.filter((movie,i) => {
+        return movie.director === "Steven Spielberg" && movie.genre.includes("Drama");
+    });
+
+    return filteredMovies.length;
+}
 
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
+
+function orderAlphabetically(movies) {
+
+    // Shallow copy - create an Array of movie titles
+    let moviesList = movies.map( (movie, i) => {
+      return movie.title;
+    });
+  
+    // Order the list alphabetically
+    moviesList.sort( (a,b) => {
+      return a === b ? 0 : a > b ? 1 : -1;
+    });
+  
+    // Return first 20 elements
+    return moviesList.slice(0,20);
+  }
 
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
 
