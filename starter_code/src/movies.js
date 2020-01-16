24692e41f34d45e122f0599971729b4b83bb23e4
LolaEnBeta <lolitaenbeta@gmail.com>
2020-01-16 20:54:05 +0100

    Need to finish last iteration

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -78,8 +78,12 @@ function ratesAverage(arr) {
   }
 
   const rate = arr.reduce( (sum, movie) => {
-    if (movie.rate<=0) {
-      movie.rate = 2.00;
+    if (movie.rate>0) {
+      return sum + movie.rate;
+    } else if (!movie.rate) {
+      return sum;
+    } else if (movie.rate = 0) {
+      return sum + 2;
     }
     return sum + movie.rate;
   },0);
@@ -88,9 +92,63 @@ function ratesAverage(arr) {
   return Number((rate/arr.length).toFixed(2));
 }
 
-
 // Iteration 5: Drama movies - Get the average of Drama Movies
 
+function dramaMoviesRate(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+
+  let newArr = arr.filter(arr => arr.genre.includes("Drama"));
+  if (newArr.length === 0) {
+    return 0;
+  }
+  const rate = newArr.reduce( (sum, movie) => {
+    if (movie.rate>0) {
+      return sum + movie.rate;
+    } else if (!movie.rate) {
+      return sum;
+    } else if (movie.rate = 0) {
+      return sum + 2;
+    }
+    return sum + movie.rate;
+  },0);
+
+  return Number((rate/newArr.length).toFixed(2));
+
+}
+
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
+
+function turnHoursToMinutes(arr) {
+  if (arr.length === 0) {
+    return [];
+  }
+  let newArr = arr.map(function(movie){
+    let filmTime=movie;
+    
+    if (filmTime.time.includes(" ")) {
+      filmTime.time = parseInt(filmTime.time)*60;
+    } else if (filmTime.time.includes(" ")){
+      let time = [];
+      time.push((filmTime.time).split(" "));
+
+      time[0]=parseInt(time[0])*60;
+      time[1]=parseInt(time[1]);
+      filmTime.time=time[0]+time[1];
+    }
+    return filmTime;
+  })
+  newArr.sort(function(a, b) {
+    if (a.time < b.time) {
+      return 1;
+    } else if (a.time > b.time) {
+      return -1;
+    } else {
+      return 0;
+    }
+  });
+  return newArr;
+}
 
 // BONUS Iteration: Best yearly rate average - Best yearly rate average
