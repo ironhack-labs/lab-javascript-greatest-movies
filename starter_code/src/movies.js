45ed1336a2ba773c711eb8f25b65e9847aa0c983
LolaEnBeta <lolitaenbeta@gmail.com>
2020-01-17 09:04:48 +0100

    Almost finished

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -124,31 +124,89 @@ function turnHoursToMinutes(arr) {
   if (arr.length === 0) {
     return [];
   }
-  let newArr = arr.map(function(movie){
+  if (arr.duration===undefined){
+    return 0;
+  }
+// method 0 :D
+  /*arr=arr.forEach(function (element){
+    if(!element.duration.includes(“min”)){
+      element.duration=parseInt(element.duration)*60;
+    }
+    else if(element.duration.includes(“min”)){
+      if(element.duration.includes(” “)){
+        let time=[];
+        time.push(element.duration.split(” “));
+         time[0]=parseInt(time[0])*60;
+         time[1]=parseInt(time[1]);
+         element.duration=time[0]+time[1];
+      }
+      else {
+        element.duration=parseInt(element.duration);
+      }
+    }
+    return arr;
+  })
+  */
+  //method 1 XDDD
+ /* arr=arr.forEach(function (element){
+    if(!element.duration.includes(” “)){
+      if(element.duration<15){
+        element.duration=parseInt(element.duration)*60;
+        return arr;
+     }else{
+      element.duration=parseInt(element.duration);
+      return arr;
+      }
+      }
+       else if(element.duration.includes(” “)){
+         let time=[];
+        time.push(element.duration.split(” “));
+         time[0]=parseInt(time[0])*60;
+         time[1]=parseInt(time[1]);
+         element.duration=time[0]+time[1];
+         return arr;
+        }
+  })
+*/
+//method 2 XD
+  //let newArr=[];
+  //for(let i=0;i<=arr.length;i++){
+  //  if(!arr[i].duration.includes(” “)){
+  //    if(arr[i].duration<15){
+  //     arr[i].duration=parseInt(arr[i].duration)*60;
+  //    }
+  //    else {
+  //      arr[i].duration=parseInt(arr[i].duration);
+  //    }
+  //  }
+  //  else if(arr[i].duration.includes(” “)){
+  //    let time=[];
+  //    time.push(arr[i].split(” “));
+  //   time[0]=parseInt(time[0])*60;
+  //    time[1]=parseInt(time[1]);
+  //    arr[i].duration=time[1]+time[0];
+  //  }
+  //  newArr.push(arr[i]);
+  //}
+  
+//method 3 XD
+  arr.map(function(movie){
     let filmTime=movie;
-    
-    if (filmTime.time.includes(" ")) {
-      filmTime.time = parseInt(filmTime.time)*60;
-    } else if (filmTime.time.includes(" ")){
-      let time = [];
-      time.push((filmTime.time).split(" "));
-
+    if (!(filmTime.duration).includes(” “)) {
+    if(parseInt(filmTime.duration)<=25){
+        filmTime.duration=parseInt(filmTime.duration);
+      }
+      else if (parseInt(filmTime.duration)>25){
+      filmTime.duration = parseInt(filmTime.duration)*60;
+      }
+    } else if ((filmTime.duration).includes(” “)){
+     let time = [];
+     time.push((filmTime.duration).split(” “));
       time[0]=parseInt(time[0])*60;
       time[1]=parseInt(time[1]);
-      filmTime.time=time[0]+time[1];
+      filmTime.duration=time[0]+time[1];
     }
     return filmTime;
   })
-  newArr.sort(function(a, b) {
-    if (a.time < b.time) {
-      return 1;
-    } else if (a.time > b.time) {
-      return -1;
-    } else {
-      return 0;
-    }
-  });
-  return newArr;
 }
-
 // BONUS Iteration: Best yearly rate average - Best yearly rate average
