fe2f3bb98670f73565779385127e4bb7990ad72b
arnoldosicilia <arnoldo.sicilia.labarquilla@gmail.com>
2020-01-20 22:56:42 +0100

    Iteracion 1 y 2" dkask "

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -2,7 +2,18 @@
 
 // Iteration 1: Ordering by year - Order by year, ascending (in growing order)
 
+
+function orderByYear(arr) {
+    const orderedMovies = arr.sort((a, b) => a - b)
+    console.log(orderedMovies)
+    return [orderedMovies]
+}
+
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
+
+howManyMovies(arr => {
+    return arr.filter(arr.genre.find(elm => elm == "Drama"))
+})
 
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
 
