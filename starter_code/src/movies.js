a7ac831ffbb7d493a2393962872d65d0e9fded1f
Nelson <nelsoncc1812@gmail.com>
2020-01-20 14:55:40 +0000

    Fallo corregido en la primera funcion

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,4 +1,7 @@
 /* eslint no-restricted-globals: 'off' */
+
+// Iteration 1: Ordering by year - Order by year, ascending (in growing order)
+
 function orderByYear(arr_) {
     let arr = [...arr_]
     let result = []
@@ -6,22 +9,29 @@ function orderByYear(arr_) {
     arr = arr.sort((a, b) => {
 
         if (a.year > b.year) return 1
-        else if (b.year < a.year) return -1
+        else if (b.year > a.year) return -1
         else {
 
-            if (a.name > b.name) return 1
-            else return -1
+            if (a.title > b.title) return 1
+            else if(a.title < b.title)      return -1
         }
     })
 
     arr.forEach(elm => result.push(elm))
 
+    console.log(result)
+
     return result
 }
 
-// Iteration 1: Ordering by year - Order by year, ascending (in growing order)
-
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
+
+function howManyMovies(arr){
+    if(!arr.filter(elm=>elm.name).length)   return 0
+    if(arr.filter(elm=>elm.genre=="Drama").length==arr.length) return 1
+    if(arr.filter())
+    return arr.length
+}
 
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
 
