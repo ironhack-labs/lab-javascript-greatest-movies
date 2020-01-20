19ecf0bd90f004eaa8b14836e0467722078ff2ea
luciaoSainz <danielsainz28@gmail.com>
2020-01-20 18:52:18 +0100

    first commit

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,15 +1,58 @@
 /* eslint no-restricted-globals: 'off' */
 
-// Iteration 1: Ordering by year - Order by year, ascending (in growing order)
+ //Iteration 1: Ordering by year - Order by year, ascending (in growing order)
+ function orderByYear(arr){
+    arr.sort(function (movie1, movie2) {
+        if (movie1.year > movie2.year) return 1
+        if (movie1.year < movie2.year) return -1
+        if (movie1.year === movie2.year) {
+            // console.log(`mismo año`);
+            a = movie1.title.localeCompare(movie2.title);
+            // console.log(`a = ` + a);
+            return a;
+        }
+    })
+    let arr2 = [...arr];
+    return arr2;
+}
+console.log(movies);
+ 
 
-// Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
+//Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
 
+ function howManyMovies(movies){
+    let moviesGenre = movies.filter((movie) => {
+     if(movie.genre.includes("Drama") && movie.director === "Steven Spielberg") return true
+      
+ }) 
+    return moviesGenre.length
+ }
+ 
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
 
-// Iteration 4: All rates average - Get the average of all rates with 2 decimals
+function orderAlphabetically(movies) {
+    let allMovies = [...movies]
+    return allMovies.sort(function(movie1, movie2) {
+      if(movie1.title < movie2.title) return -1;
+      if(movie1.title > movie2.title) return 1;
+      return 0;
+    }).map(function(movie) {
+      return movie.title;
+    }).splice(0,20);
+  }
+  // Iteration 4: All rates average - Get the average of all rates with 2decimals
 
-// Iteration 5: Drama movies - Get the average of Drama Movies
+  ratesAverage(movies){
+      if(movies.length === 0) 
+      return;
 
-// Iteration 6: Time Format - Turn duration of the movies from hours to minutes
+   let moviesCopy = Object.assign([], movies);
+      
+  }
+
+
+// Iteration 5: Drama allMovies - Get the average of Drama allMovies
+
+// Iteration 6: Time Format - Turn duration of the allMovies from hours to minutes
 
 // BONUS Iteration: Best yearly rate average - Best yearly rate average
