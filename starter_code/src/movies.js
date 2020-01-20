a3b7fcdbd48fc77382aa6252748bcbead87257cc
Pablo Carceller <pablocarceller@me.com>
2020-01-20 17:51:44 +0100

    Iteration 1,2,3

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -2,9 +2,33 @@
 
 // Iteration 1: Ordering by year - Order by year, ascending (in growing order)
 
+function orderByYear(movies) {
+    let mvYear;
+    mvYear = [...movies].sort((a, b) => {
+        if (a.year === b.year) {
+            return a.title.localeCompare(b.title)
+        } return a.year - b.year;
+    });
+    return mvYear;
+}
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
+function howManyMovies(moviesArr) {
+    let mvSpielberg = moviesArr.filter((movie) => {
+      if(movie.director === "Steven Spielberg" && movie.genre.includes("Drama")) return true
+    })
+    return mvSpielberg.length;
+}
 
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
+function orderAlphabetically(movies){
+    let mvAlpha 
+    mvAlpha = [...movies].sort((a, b) => {
+        return a.title.localeCompare(b.title)
+    });   
+   let list = mvAlpha.map(firstMovie => firstMovie = firstMovie.title)
+   let shortList = list.splice(0,20);
+   return shortList;
+}
 
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
 
