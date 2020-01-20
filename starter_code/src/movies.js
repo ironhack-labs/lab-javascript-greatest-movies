2c293620c42c7542e8ed242bbc2f2d35382a578d
FrankyMolina <fjmolinatorrejon@gmail.com>
2020-01-21 00:00:10 +0100

    remains to be finished

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -33,26 +33,49 @@ function howManyMovies(movies) {
 
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
 
-function orderAlphabetically(movie) {
+function orderAlphabetically(movies) {
     let moviesArr = [... movies];
 
-    let alphabetArr = moviesArr.sort((a, b) => {a.title - b.title});
+    moviesArr.sort((a, b) => a.title.localeCompare(b.title));
 
-    let twentyFirsts = alphabetArr.splice(0, 20).map(element => element.title);
+    let twentyFirsts = moviesArr.splice(0, 20).map(element => element.title);
 
     return twentyFirsts;
 }
 
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
 
-function ratesAverage(movie) {
+function ratesAverage(movies) {
     let moviesArr = [... movies];
 
+    let filteredMovies = moviesArr.filter(movie => {
+        if (movie.length === 0) {
+            return 0
+        } else {
+            return movie.rate;
+        }
+    })
 
+    let avg = filteredMovies.reduce((acc, mRate) => acc + mRate.rate, 0)/filteredMovies.length;
 
+    return Number (avg.toFixed(2));
+
+    
 }
 
 // Iteration 5: Drama movies - Get the average of Drama Movies
+
+function dramaMoviesRate(movies) {
+    let moviesArr = [... movies];
+
+   let dramaRate =  moviesArr.filter(movie => movie.genre.includes('Drama'));
+   return movie.rate;
+        
+            
+        
+   
+   console.log(dramaRate);
+}
 
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
 
