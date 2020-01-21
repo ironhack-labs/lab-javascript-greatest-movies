5c41e27f1271bfa586244ccef8826aef816285eb
CrisSugar <aguacero18@hotmail.com>
2020-01-21 01:08:34 +0100

    done

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -43,44 +43,44 @@ function orderAlphabetically(movies){
 
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
 
-// function ratesAverage(movies){
-//     let totalRates=movies.reduce((sum,movies)=>{
-//         return {
-//     sum.rate += movies.rate;},0);
+function ratesAverage(movies){
+    let totalRates=movies.reduce((sum,movies)=>{
+        return {
+    sum.rate += movies.rate;},0);
 
-//     let averageRates=totalRates/movies.rate.length;
+    let averageRates=totalRates/movies.rate.length;
 
-//     console.log(averageRates)
-// }
+    console.log(averageRates)
+}
 
-// parseFloat(Math.round(averageRates * 100) / 100).toFixed(2);
+parseFloat(Math.round(averageRates * 100) / 100).toFixed(2);
 
 
 // // Iteration 5: Drama movies - Get the average of Drama Movies
 
-// function dramaMoviesRate(movies){
-//     let dramaFilms = movies.filter(function(movie){
-//         return movie.genre==="Drama";
-//     });
-//     console.log(dramaFilms.rate);
-//     let sumRates=dramaFilm.reduce((sum,dramaFilms)=>{
-//         return sum += dramaFilms.rate;},0);
+function dramaMoviesRate(movies){
+    let dramaFilms = movies.filter(function(movie){
+        return movie.genre==="Drama";
+    });
+    console.log(dramaFilms.rate);
+    let sumRates=dramaFilm.reduce((sum,dramaFilms)=>{
+        return sum += dramaFilms.rate;},0);
 
-//     let averageRatesDrama=sumRates/dramaFilms.length;
-//         return averageRatesDrama;
-// }
+    let averageRatesDrama=sumRates/dramaFilms.length;
+        return averageRatesDrama;
+}
 
-// if(averageRatesDrama>averageRates){
-//         console.log(`People prefers drama film.`);
-// else if(averageRatesDrama<averageRates){
-//         console.log(`Drama film is not very popular.`);
-// else{
-//         console.log(`Drama film is a popular genre.`)
-//         }
-// }
+if(averageRatesDrama>averageRates){
+        console.log(`People prefers drama film.`);
+else if(averageRatesDrama<averageRates){
+        console.log(`Drama film is not very popular.`);
+else{
+        console.log(`Drama film is a popular genre.`)
+        }
+}
     
 
-// parseFloat(Math.round(averageRatesDrama * 100) / 100).toFixed(2);
+parseFloat(Math.round(averageRatesDrama * 100) / 100).toFixed(2);
 
 
 
