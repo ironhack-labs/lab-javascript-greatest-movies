bf248ccb1a42da816e19fba9e6e6b719c0b12a4e
arnoldosicilia <arnoldo.sicilia.labarquilla@gmail.com>
2020-01-21 09:37:21 +0100

    iteracion2

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -4,20 +4,33 @@
 
 
 function orderByYear(arr) {
-    const orderedMovies = arr.sort((a, b) => a - b)
-    console.log(orderedMovies)
-    return [orderedMovies]
+
+    let orderedMovies = [...arr]
+
+    return orderedMovies.sort((a, b) => a.years - b.years)
+
+
 }
+
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
 
-howManyMovies(arr => {
-    return arr.filter(arr.genre.find(elm => elm == "Drama"))
-})
+function howManyMovies(arr) {
+
+    let dramaMovies = arr.filter(elm => elm.director == 'Steven Spielberg' && elm.genre.includes("Drama"))
+
+    return dramaMovies.length
+}
 
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
 
+function orderAlphabetically(arr) {
+
+}
+
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
+
+
 
 // Iteration 5: Drama movies - Get the average of Drama Movies
 
