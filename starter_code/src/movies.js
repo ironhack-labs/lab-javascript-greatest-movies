f906e180cc1a091767322a855aae1e4787d20830
Luca Campora <luca.campora@gmail.com>
2020-01-23 18:47:32 +0000

    first iteration more and less :/

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -2,14 +2,36 @@
 
 // Iteration 1: Ordering by year - Order by year, ascending (in growing order)
 
+
+let arrayCopy = [...movies]
+
+function orderByYear (array) { 
+ 
+ array.sort(function(a, b){
+    return a.year - b.year;
+  })
+  return array
+}
+
+
+
+
+
+
+
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
+
 
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
 
+
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
+
 
 // Iteration 5: Drama movies - Get the average of Drama Movies
 
+
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
+
 
 // BONUS Iteration: Best yearly rate average - Best yearly rate average
