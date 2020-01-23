6b7af5f2c5cd23242c498f3995fcf2b4c60fa972
David Felix <davidfelix1997@hotmail.com>
2020-01-23 21:09:59 +0000

    Finishing 4

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -52,10 +52,39 @@ const howMAnyMovies = array => {
 };*/
 
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
+/*const orderAlphabetically = array => {
+  let sortedTitle = array.sort(function(a, b) {
+    if (a.Title > b.Title) {
+      return 1;
+    } else if (a.Title < b.Title) {
+      return -1;
+    } else {
+      return 0;
+    }
+  });
+  return sortedTitle;
+
+};*/
+
 const orderAlphabetically = array => {
-  return array;
+  return (title = array
+    .map(title => title.title)
+    .sort()
+    .slice(0, 20));
 };
+
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
+const ratesAverage = array => {
+  if (array.length === 0) {
+    return 0;
+  }
+
+  /* return average !== 'undefined';*/
+  let average = array.reduce(function(accumulator, value) {
+    return accumulator + value.array;
+  });
+  return Math.round(ratesAverage / array.length);
+};
 
 // Iteration 5: Drama movies - Get the average of Drama Movies
 
