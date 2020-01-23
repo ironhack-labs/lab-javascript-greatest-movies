43d696e32bffe7c4f2be0cb1264750be8db42a5e
Manuel Rocha <manueltemidorocha@gmail.com>
2020-01-23 18:36:44 +0000

    first done, two missing drama

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,10 +1,39 @@
 /* eslint no-restricted-globals: 'off' */
 
 // Iteration 1: Ordering by year - Order by year, ascending (in growing order)
+function orderByYear(arrData) {
+  const final = [];
+  arrData.map(x => final.push(x));
+  return final.sort((a, b) => {
+    if (a.year > b.year) {
+      return 1;
+    } else if (a.year < b.year) {
+      return -1;
+    } else {
+      if (a.title > b.title) {
+        return 1;
+      } else if (a.title < b.title) {
+        return -1;
+      } else {
+        return 0;
+      }
+    }
+  });
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
+function howManyMovies(stevenArr) {
+  return stevenArr.filter(x => x.director === 'Steven Spielberg').length;
+}
 
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
+function orderAlphabetically(arr) {
+  const final = arr
+    .reduce(x => x.title)
+    .sort()
+    .slice(0, 21);
+  return final;
+}
 
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
 
