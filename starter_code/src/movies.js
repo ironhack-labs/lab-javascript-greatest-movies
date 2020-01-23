74e54b40a4abfe4c98c94332978d5eaefe115a19
André Ferreira <mrawesomeferreira@gmail.com>
2020-01-23 18:28:27 +0000

    Finished first 2 iterations

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -2,9 +2,46 @@
 
 // Iteration 1: Ordering by year - Order by year, ascending (in growing order)
 
+function orderByYear(array) {
+  let orderedArray = [];
+  if (array.length !== 0) {
+    orderedArray = array.sort(function(a, b) {
+      if (a.year < b.year) {
+        return -1;
+      } else if (a.year > b.year) {
+        return 1;
+      } else if (a.year === b.year) {
+        return a.title < b.title ? -1 : 1;
+      } else {
+        return 0;
+      }
+    });
+  }
+  return orderedArray;
+}
+
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
 
+function howManyMovies(array) {
+  let dramaSteven = [];
+  if (array.length !== 0) {
+    dramaSteven = array.filter(
+      movie => movie.director === 'Steven Spielberg' && movie.genre.includes('Drama')
+    );
+  }
+
+  return dramaSteven.length;
+}
+
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
+
+function orderAlphabetically(array) {
+  let orderedAlphabet = [];
+
+  return orderedAlphabet;
+}
+
+return orderedAlphabet;
 
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
 
