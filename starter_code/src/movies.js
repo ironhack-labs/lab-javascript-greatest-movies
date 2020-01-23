dbfc2ab0701964888b940fdb339fb0355b0f336a
André Ferreira <mrawesomeferreira@gmail.com>
2020-01-23 22:45:40 +0000

    Finished all but the last iteration and bonus

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -36,17 +36,54 @@ function howManyMovies(array) {
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
 
 function orderAlphabetically(array) {
-  let orderedAlphabet = [];
-
-  return orderedAlphabet;
+  let top20 = [];
+  top20 = array
+    .map(movies => movies.title)
+    .sort()
+    .splice(0, 20);
+  return top20;
 }
-
-return orderedAlphabet;
 
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
 
+function ratesAverage(array) {
+  let averageTotal = [];
+  averageTotal =
+    array.reduce(function(acc, movie) {
+      if (typeof movie.rate !== 'undefined') {
+        return acc + movie.rate;
+      } else {
+        return acc;
+      }
+    }, 0) / array.length;
+  return isNaN(averageTotal) ? 0 : Math.round(averageTotal * 100) / 100;
+}
+
 // Iteration 5: Drama movies - Get the average of Drama Movies
 
+function dramaMoviesRate(array) {
+  let dramaRate = [];
+  dramaRate = array.filter(movie => movie.genre.includes('Drama'));
+  return ratesAverage(dramaRate);
+}
+
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
+
+//Not working
+
+function turnHoursToMinutes(array) {
+  let minutesArray = [];
+  minutesArray = array.map(movie => {
+    if (movie.duration.includes('h')) {
+      let timeSplit = movie.duration.split(' ');
+      let minutesSum = parseInt(timeSplit[0]) / 60 + parseInt(timeSplit[1]);
+      return minutesSum;
+    } else {
+      let minutesSum = Number.parseInt(movie.duration);
+      return minutesSum;
+    }
+  });
+  return minutesArray;
+}
 
 // BONUS Iteration: Best yearly rate average - Best yearly rate average
