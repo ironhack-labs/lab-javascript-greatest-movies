af683f859544ecb3aab4d505a0f32d4491095bb2
Manuel Rocha <manueltemidorocha@gmail.com>
2020-01-24 00:17:13 +0000

    missing duration movies and bonus

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -23,22 +23,63 @@ function orderByYear(arrData) {
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
 function howManyMovies(stevenArr) {
-  return stevenArr.filter(x => x.director === 'Steven Spielberg').length;
+  return stevenArr.filter((x, i) => x.director === 'Steven Spielberg' && x.genre.includes('Drama'))
+    .length;
 }
 
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically(arr) {
   const final = arr
-    .reduce(x => x.title)
+    .map(x => x.title)
     .sort()
-    .slice(0, 21);
+    .slice(0, 20);
   return final;
 }
 
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
+function ratesAverage(arr) {
+  let sum = arr
+    .filter(x => {
+      if (x.rate) {
+        return true;
+      }
+    })
+    .map(y => {
+      return y.rate;
+    })
+    .reduce((acc, z) => {
+      return acc + z;
+    }, 0);
+  if (!arr[0]) return 0;
+  else return Number((sum / arr.length).toFixed(2));
+}
 
 // Iteration 5: Drama movies - Get the average of Drama Movies
+function dramaMoviesRate(arr) {
+  //here I get all movies with Drama
+  var s = arr.filter((x, i) => arr[i].genre.filter(z => z === 'Drama')[0] === 'Drama');
 
+  //here I go for the average rate of drama movies
+  var ac = 0;
+  var r =
+    Math.round(
+      (s.reduce((acc, val, i) => {
+        if (val.rate !== undefined) {
+          ac++;
+          return acc + val.rate;
+        }
+      }, 0) /
+        ac) *
+        100
+    ) / 100;
+  if (arr.length === 1) return arr[0].rate;
+  else if (s[0] === undefined) return 0;
+  else return r;
+}
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
-
+function turnHoursToMinutes(arr) {
+  arr.forEach(element => {
+    element.time;
+  });
+}
 // BONUS Iteration: Best yearly rate average - Best yearly rate average
