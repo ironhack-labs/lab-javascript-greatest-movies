d1391dd9bed214d9d6196896a201a787f0f4bd98
David Felix <davidfelix1997@hotmail.com>
2020-01-24 10:41:34 +0000

    #

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -5,15 +5,16 @@ const orderByYear = array => {
   if (array.length === 0) {
     return [];
   }
+  /*const newArray = [...array]*/
   let sortedYear = array.sort(function(a, b) {
     if (a.year > b.year) {
       return 1;
     } else if (a.year < b.year) {
       return -1;
     } else if (a.year === b.year) {
-      if (a.title > b.title) {
+      if (a.title.toLowerCase() > b.title.toLowerCase()) {
         return 1;
-      } else if (a.title < b.title) {
+      } else if (a.title.toLowerCase() < b.title.toLowerCase()) {
         return -1;
       } else {
         return 0;
@@ -67,27 +68,80 @@ const howMAnyMovies = array => {
 };*/
 
 const orderAlphabetically = array => {
-  return (title = array
-    .map(title => title.title)
+  return (movies = array
+    .map(movies => movies.title)
     .sort()
     .slice(0, 20));
 };
 
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
+/*
 const ratesAverage = array => {
   if (array.length === 0) {
     return 0;
   }
-
-  /* return average !== 'undefined';*/
+  //return average !== 'undefined';
   let average = array.reduce(function(accumulator, value) {
     return accumulator + value.array;
   });
-  return Math.round(ratesAverage / array.length);
+  return Math.round(((ratesAverage / array.length) * 100) / 100); //.toFixed(2);
+};
+*/
+
+const roundNumber = (number, digits) => Math.round(number * 10 ** digits) / 10 ** digits;
+const ratesAverage = array => {
+  if (array.length === 0) {
+    return 0;
+  }
+  const rates = array
+    .map(movie => movie.rate) /*.filter(rate => typeof rate === 'number');*/
+    .map(rate => {
+      if (typeof rate === 'number') {
+        return rate;
+      } else {
+        return 0;
+      }
+    });
+  /*const averageRate = rates.reduce((accumulator, rate) => accumulator + rate / rates.length, 0);*/
+  const sumOfAllRates = rates.reduce((accumulator, rate) => {
+    if (typeof rate /*!== 'undefined'*/ === 'number') {
+      return accumulator + rate;
+    } else {
+      return accumulator;
+    }
+  }, 0);
+  const averageRate = sumOfAllRates / rates.length;
+  return averageRate;
+  return roundNumber(averageRate, 2);
 };
 
 // Iteration 5: Drama movies - Get the average of Drama Movies
+const dramaMoviesRate = array => {
+  const dramaMovies = array.filter(movie => movie.genre.includes('Drama'));
+  const averageDramaMovieRate = ratesAverage(dramaMovies);
+  return averageDramaMovieRate;
+};
 
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
+const turnHoursToMinutes = array => {
+  return array.map(movie => {
+    const durationAsString = movie.duration;
 
+    let duration = 0;
+
+    for (let value of durationAsString.split(' ')) {
+      const number = parseInt(value);
+      if (value.includes('h')) {
+        duration += number * 60;
+      } else if (value.includes('min')) {
+        duration += number;
+      }
+    }
+
+    return {
+      ...movie,
+      duration
+    };
+  });
+};
 // BONUS Iteration: Best yearly rate average - Best yearly rate average
