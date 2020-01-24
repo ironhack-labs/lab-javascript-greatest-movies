fc9a8d30cdc5ad9a927802c459ab1b89e11d1c05
Myrian Katto <myriankatto@gmail.com>
2020-01-24 08:17:07 +0000

    -iteration 4 done

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -27,10 +27,8 @@ function orderByYear(arr) {
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
 
 function howManyMovies(arr) {
-  let spielbergMovies = arr
-  .filter(movie => movie.director === 'Steven Spielberg');
-  let spilbergDramaMovies = spielbergMovies
-  .filter(movie => movie.genre.includes('Drama'));
+  let spielbergMovies = arr.filter(movie => movie.director === 'Steven Spielberg');
+  let spilbergDramaMovies = spielbergMovies.filter(movie => movie.genre.includes('Drama'));
 
   return spilbergDramaMovies.length;
 }
@@ -58,20 +56,53 @@ function orderAlphabetically(arr) {
 
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
 
-
 function ratesAverage(arr) {
-  let rates = arr .map(function(movie) {
-    return movie.rate;
-  });
-  let avgRates = rates.reduce(function(start, end, index, arr){
-    let total = start + end;
-    return total/arr.length;
-  });
+  //new array
+  const newArray = [...arr];
+  // if there is no movie
+  if (newArray.length != 0) {
+    //array with all the rates
+    const moviesRate = newArray
+      .map(function(movie) {
+        if (typeof movie.rate === 'number') {
+          return movie.rate;
+        } else {
+          return 0;
+        }
+      })
 
-  return avgRates;
-};
+      .filter(function(movie) {
+        return movie >= 0;
+      });
+
+    //sum all the rates
+    const sumOfAllRates = moviesRate.reduce(function(acc, movie) {
+      return acc + movie;
+    }, 0);
+    //calculate the average
+    let avgRates = sumOfAllRates / moviesRate.length;
+    //to round to 2 decimals
+    return Math.round(avgRates * 100) / 100;
+  } else {
+    return 0;
+  }
+}
 
 // Iteration 5: Drama movies - Get the average of Drama Movies
+
+// function dramaMoviesRate(arr) {
+//   let dramaAvgRate = 0;
+//   let allDramasMovies = arr
+
+//   .filter(movie => movie.genre
+
+//   .includes('Drama'))
+//   .reduce((accumulator, value) => accumulator + value.rate, 0)),
+//     (lengthDrama = allDramasMovies.length);
+//   if (lengthDrama === 0) return 0;
+//   dramaAvgRate = (avgRateDrama / lengthDrama).toFixed(2);
+//   return dramaAvgRate;
+// }
 
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
 
