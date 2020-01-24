f15b0714b8cf9b77ace671dfa023d070d0b6daaa
Luca Campora <luca.campora@gmail.com>
2020-01-24 09:01:46 +0000

    pull what i add yester, two more steps, but the first one doesnt work properly

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -3,26 +3,58 @@
 // Iteration 1: Ordering by year - Order by year, ascending (in growing order)
 
 
-let arrayCopy = [...movies]
 
-function orderByYear (array) { 
- 
- array.sort(function(a, b){
-    return a.year - b.year;
-  })
-  return array
+
+function orderByYear(array){
+
+  let arrayCopy = [...movies]
+  
+  arrayCopy.sort((a, b) => {
+      if(a.year > b.year){
+          return 1;
+      }
+      else if(a.year < b.year){
+          return -1;
+      }
+      else if(a.year === b.year){
+         if(a.title > b.title){
+             return 1;
+         }
+         if(a.title < b.title){
+             return -1;
+         }
+      }
+      else{
+          return 0;
+      }
+      
+  });
+  
+  return arrayCopy;
 }
-
-
-
-
-
 
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
 
 
+function howManyMovies(array){
+ 
+  let newList = array.filter(movies => movies.director === "Steven Spielberg");
+ 
+  return newList.filter(movies => movies.genre.includes("Drama")).length;
+}
+
+
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
+
+
+function orderAlphabetically(array){
+ 
+  let newArr = array.map((movie) => movie.title);
+  let newList = newArr.sort();
+ 
+  return newList.slice(0,20); 
+}
 
 
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
