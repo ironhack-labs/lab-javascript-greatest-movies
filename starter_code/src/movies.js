892d2ab44d776465527c7bd0286d35bd6cd86a80
Eduardo <eduardo_muller0301@Hotmail.com>
2020-02-21 18:11:36 -0300

    parte1

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -2,14 +2,41 @@
 
 // Iteration 1: Ordering by year - Order by year, ascending (in growing order)
 
+
+function orderByYear(movies) {
+
+const orderYear = [...movies];
+orderYear.sort((a,b ) => a-b)
+
+return orderYear;
+}
+
+
+
+//numbers.sort((a, b) => a - b);
+
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
-
+function howManyMovies() {
+    
+}
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
-
+function orderAlphabetically() {
+    
+}
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
-
+function ratesAverage() {
+    
+}
 // Iteration 5: Drama movies - Get the average of Drama Movies
-
+function dramaMoviesRate() {
+    
+}
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
-
+function turnHoursToMinutes() {
+    
+}
 // BONUS Iteration: Best yearly rate average - Best yearly rate average
+function bestYearAvg() {
+  if(bestYearAvg.length ==0) return null;  
+  // if(bestYearAvg.length == [1]) return true 
+}
