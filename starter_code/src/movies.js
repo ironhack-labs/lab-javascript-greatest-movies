0c7738e67606ef943128fd280f9bade831556f36
Wilson Cruz <galatoide@gmail.com>
2020-02-22 14:11:40 +0000

    iteration 2

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -22,7 +22,19 @@ function orderByYear(array){
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
 
+function howManyMovies(array){
+    if(!array.length)
+    return 0;
+
+    let moviesSpielberg = array.filter(function(movies){
+    return movies.director === 'Steven Spielberg' && movies.genre.includes('Drama')
+    });
+    return moviesSpielberg.length
+}
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
+
+
+
 
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
 
