1e84eeea1ac0b613c26ae194040eb0845d4300b6
Wilson Cruz <galatoide@gmail.com>
2020-02-23 21:54:37 +0000

    Done with bonus

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -59,26 +59,102 @@ function orderAlphabetically(array){
 function ratesAverage(array){
     if(!array.length){
         return 0
-        }
-    
 
-        let rateArray = array.map((movie) => {
-            return movie.rate
-        }); 
+        // let rateArray = array.map((movie) => {
+        //     return movie.rate
+        // }); 
         
-        let reducedrateArray = rateArray.reduce((accumulator,number) => {
-            return accumulator + number}, 0);
-        let rateAverage = reducedrateArray / rateArray.length;
-        let avg=Math.round(rateAverage * 10 ** 2) / 10 ** 2
-        return avg
-
-        
-        
+        // let reducedrateArray = rateArray.reduce((accumulator,number) => {
+        //     return accumulator + number}, 0);
+        // let rateAverage = reducedrateArray / rateArray.length;
+        // let avg=Math.round(rateAverage * 10 ** 2) / 10 ** 2
+        // return avg
+    } else{
+        let total = array.reduce((accumulator, current) => {
+            if (current.rate) {
+                return accumulator + current.rate;
+            } else {
+                return accumulator;
+            }
+        }, 0);
+        let average = total / array.length;
+            // return Math.round(average * 10 ** 2) / 10 ** 2;
+            // return Number(average.toFixed(2));
+            return parseFloat(average.toFixed(2));
+    }
 }
-
 
 // Iteration 5: Drama movies - Get the average of Drama Movies
 
+function dramaMoviesRate(array) {
+  if (!array.length) {
+    return 0;
+  } else {
+    const dramaMovies = array.filter(movie => movie.genre.includes("Drama"));
+
+    const sumArray = dramaMovies.reduce((accumulator, value) => {
+      return accumulator + value.rate;
+    }, 0);
+
+    const average = sumArray / dramaMovies.length;
+    if (dramaMovies.length) {
+      return Math.round(average * 100) / 100;
+    } else {
+      return 0;
+    }
+  }
+}
+
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
 
+function turnHoursToMinutes(array) {
+
+  let newArray = array.slice().map(element =>{
+    let newElement = {...element};
+    if (newElement.duration.indexOf('min') >= 0 && newElement.duration.indexOf('h') >= 0) {
+      newElement.duration = Number((newElement.duration.split('h')[0] * 60)) + Number((newElement.duration.split(' ')[1].split('min')[0]));
+    } else if (newElement.duration.indexOf('h') >= 0 && newElement.duration.indexOf('min') < 0){
+      newElement.duration = Number((newElement.duration.split('h')[0] * 60));
+    } else {
+      newElement.duration = Number((newElement.duration.split('min')[0]));
+    }
+    return newElement
+  });
+
+  return newArray;
+}
+
+// console.log(movies);
+
 // BONUS Iteration: Best yearly rate average - Best yearly rate average
+
+function bestYearAvg(array) {
+  if (!array.length){
+     return null;
+  }
+  let bestAverage={}
+  let averageRate=0 
+  array.forEach(movie => {
+
+      let counter=0
+      averageRateForYear=array.reduce((avgRate,movieYear)=>{
+      if(movie.year == movieYear.year){
+        avgRate+= movieYear.rate;
+        counter++;
+      } 
+      return avgRate
+    },0);
+
+      averageRateForYear=averageRateForYear/counter;
+
+      if (averageRate == averageRateForYear){
+        if (bestAverage.year>movie.year){
+        bestAverage = movie;
+        averageRate=averageRateForYear;
+        }} else if (averageRate < averageRateForYear){
+        bestAverage = movie;
+        averageRate=averageRateForYear;
+        }
+      })
+   return `The best year was ${bestAverage.year} with an average rate of ${averageRate}`
+ }
