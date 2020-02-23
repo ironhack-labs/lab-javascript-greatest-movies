a3b489486487251b312e25b00eb28603b95cb4be
Rafael Freitas <rafaelborges1985@hotmail.com>
2020-02-23 18:38:13 -0300

    Finish bonus exercise

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -36,3 +36,66 @@ const ratesAverage = dt =>
 const dramaMoviesRate = (obj) => 
 ratesAverage(obj.filter(a => a.genre.includes("Drama")));
 
+// Iteration 6: Time Format - Turn duration of the movies from hours to minutes
+
+const turnHoursToMinutes = dt => dt.slice().reduce((a, b) => {
+    a.push({
+      ...b,
+      duration: b.duration.replace(/[a-zA-z]/g, "")
+      .split(' ')
+      .map((a, i) => a.length == 2 ? parseInt(a) : i == 0 ? parseInt(a) * 60 : parseInt(a))
+      .reduce((a,b) => a + b)
+    })
+    return a;
+  }, [])
+
+  // BONUS Iteration: Best yearly rate average - Best yearly rate average
+
+const bestYearAvg = arr => {
+  
+  let copiedData = [...arr].sort((a,b)=> a.year - b.year);
+ 
+  let moviesContainer = [];
+  
+  if(arr.length == 0) return null;
+  
+  for(let i = 0; i < copiedData.length - 1; i++){
+    
+    if(copiedData[i].year == copiedData[i + 1].year){
+      
+      copiedData[i+1]={
+        year: copiedData[i].year,
+        rate: (copiedData[i].rate + copiedData[i + 1].rate)/2
+      }
+
+    } else{
+      
+      moviesContainer.push({
+        year:copiedData[i].year,
+        rate:copiedData[i].rate
+      });
+      
+    }
+  }//for
+
+
+
+  moviesContainer = moviesContainer.sort((a,b) => a.rate - b.rate).reduce((a,b)=>{
+    a[0].rate < b.rate ?
+      a = {
+      year:b.year,
+      rate:b.rate
+    } :
+    a = {
+      year: a[0].year,
+      rate: a[0].rate
+    }
+    return [a];
+  }, [{
+    year:0,
+    rate:0
+  }]);
+
+  return `The best year was ${moviesContainer[0].year} with an average rate of ${moviesContainer[0].rate}`
+ 
+}
