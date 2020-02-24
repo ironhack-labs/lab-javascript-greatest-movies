774e7cd7e2aab16e7f20579984be6eea024fa85f
Adrian Vide <adrian.vide.paris@gmail.com>
2020-02-24 12:51:18 +0100

    allClear

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,112 +1,187 @@
 /* eslint no-restricted-globals: 'off' */
-
+// ************************************************************************************
 // Iteration 1: Ordering by year - Order by year, ascending (in growing order)
-
-function orderByYear(array) {
-    const newArr = [...array]
-    newArr.sort((a, b) => {
-        if (a.year === b.year) {
-            if (a.title < b.title) {
-                return -1
-            } else if (a.title > b.title) {
-                return 1
-            } else {
-                return 0
-            }
-        } else {
-            return a.year - b.year
+function orderByYear(lotsOfMovies) {
+    let newArray = [...lotsOfMovies];
+    // spread operator is to make sure not to mutate the original array because .sort() does change/mutate the original array so always make sure you create a safe copy before sorting
+    newArray.sort((a, b) => {
+      if (a.year > b.year) {
+        return 1;
+      } else if (b.year > a.year) {
+        return -1;
+      } else {
+        if (a.title > b.title) {
+          return 1;
+        } else if (b.title > a.title) {
+          return -1;
         }
-    })
-    return newArr
-}
-
-// Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
-
-var directorSt =  movies.filter(function(dir) {
-    if (dir.director == "Steven Spielberg" && dir.genre.includes("Drama")) {
-    return true;
-    } else {
-    return false;
+        return 0;
+      }
+    });
+    return newArray;
+  }
+  ​
+  // ************************************************************************************
+  // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
+  function howManyMovies(someMovies) {
+    return someMovies.filter(
+      eachMovie =>
+        eachMovie.director === 'Steven Spielberg' &&
+        eachMovie.genre.includes('Drama')
+    ).length;
+  }
+  ​
+  // ************************************************************************************
+  // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
+  function orderAlphabetically(lotsOfMovies) {
+    return [...lotsOfMovies]
+      .sort((a, b) => {
+        if (a.title > b.title) {
+          return 1;
+        } else if (a.title < b.title) {
+          return -1;
+        } else {
+          return 0;
+        }
+      })
+      .map(eachMovie => eachMovie.title)
+      .slice(0, 20);
+  }
+  ​
+  // ************************************************************************************
+  // Iteration 4: All rates average - Get the average of all rates with 2 decimals
+  function ratesAverage(lotsOfMovies) {
+    //  return !lotsOfMovies.length? 0 : Number((lotsOfMovies.reduce((a,b) => b.rate? a + b.rate : a, 0)/lotsOfMovies.length).toFixed(2));
+    // this is the one line version
+  ​
+    if (!lotsOfMovies.length) {
+      return 0;
     }
-});
-
-
-
-function howManyMovies() {
-    return directorSt.length;
-}
-
-
-// Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
-
-var sortedMovies = movies.sort(function(a, b) {
-    var textA = a.title.toUpperCase();
-    var textB = b.title.toUpperCase();
-    return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
-});
-
-var result = sortedMovies.map(a => a.title);
-
-
-
-function orderAlphabetically(){
-    if (result.length > 19) {
-        return result.slice(0, 20);   
-    } else if (result.length < 20) {
-        return sortedMovies;
+  ​
+    let total = lotsOfMovies.reduce((a, b) => {
+      if (b.rate) {
+        return a + b.rate;
+      } else {
+        return a;
+      }
+    }, 0);
+  ​
+    // you can use Number(), parseInt() or simply plus +
+    return Number((total / lotsOfMovies.length).toFixed(2));
+  }
+  ​
+  // ************************************************************************************
+  // Iteration 5: Drama movies - Get the average of Drama Movies
+  function dramaMoviesRate(someMovies) {
+    let drMovies = someMovies.filter(eachMovie =>
+      eachMovie.genre.includes('Drama')
+    );
+    return ratesAverage(drMovies);
+  }
+  ​
+  // ************************************************************************************
+  // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
+  ​
+  // "2h"
+  function convertHours(hourString) {
+    // ["2", ""]
+    let calculateHour = hourString.split('h');
+    return calculateHour[0] * 60;
+    // "2" * 60
+    // 120
+  }
+  ​
+  // "33min"
+  function convertMinutes(minuteString) {
+    // ["33", ""]
+    let calculateMinutes = minuteString.split('min');
+    return Number(calculateMinutes[0]);
+    // return +(calculateMinutes[0]); // this is alternative fancier way
+    // 33
+  }
+  ​
+  function convertDuration(duration) {
+    let timePieces = duration.split(' ');
+    // ["2h", "33min"]
+    // ["2h"]
+    // ["33min"]
+  ​
+    let minutes = timePieces.reduce((sum, onePiece) => {
+      if (onePiece.includes('h')) {
+        return sum + convertHours(onePiece);
+      }
+      return sum + convertMinutes(onePiece);
+    }, 0);
+  ​
+    return minutes;
+  }
+  ​
+  function turnHoursToMinutes(movies) {
+    let newCentArray = movies.map(oneMovie => {
+      let newMovie = {};
+      newMovie.title = oneMovie.title;
+      newMovie.year = oneMovie.year;
+      newMovie.director = oneMovie.director;
+      newMovie.duration = convertDuration(oneMovie.duration);
+      newMovie.genre = oneMovie.genre;
+      newMovie.rate = oneMovie.rate;
+  ​
+      return newMovie;
+    });
+  ​
+    return newCentArray;
+  }
+  ​
+  // Another possible approach could be:
+  // function turnHoursToMinutes(lotsOfMovies) {
+  //   return lotsOfMovies.map(reformat);
+  // }
+  ​
+  // function reformat(oneSingleMovieObject) {
+  //   let newThing = { ...oneSingleMovieObject };
+  //   let timeString = oneSingleMovieObject.duration;
+  //   let hasHours = timeString.includes('h');
+  //   let hasMinutes = timeString.includes('min');
+  //   let timeArray, hours, minutes, newTime;
+  ​
+  //   if (hasMinutes && hasHours) {
+  //     timeArray = timeString.split('h');
+  //     hours = Number(timeArray[0]);
+  //     minutes = Number(timeArray[1].substr(1, 2));
+  //     newTime = hours * 60 + minutes;
+  //   } else if (hasMinutes) {
+  //     minutes = parseInt(timeString.substr(0, 2));
+  //     newTime = minutes;
+  //   } else {
+  //     hours = parseInt(timeString);
+  //     newTime = hours * 60;
+  //   }
+  //   newThing.duration = newTime;
+  //   return newThing;
+  // }
+  ​
+  // ************************************************************************************
+  // BONUS Iteration: Best yearly rate average - Best yearly rate average
+  function bestYearAvg(lotsOfMovies) {
+    if (!lotsOfMovies.length) return null;
+  ​
+    let masterObject = {};
+  ​
+    lotsOfMovies.forEach(eachMovie => {
+      if (!masterObject[eachMovie.year]) {
+        masterObject[eachMovie.year] = [eachMovie];
+      } else {
+        masterObject[eachMovie.year].push(eachMovie);
+      }
+    });
+  ​
+    let highest = 0;
+    let theActualYear;
+    for (let theYear in masterObject) {
+      if (ratesAverage(masterObject[theYear]) > highest) {
+        highest = ratesAverage(masterObject[theYear]);
+        theActualYear = theYear;
+      }
     }
-    return;
-}
-
-// Iteration 4: All rates average - Get the average of all rates with 2 decimals
-
-var allRates = movies.map(a => a.rate);
-
-var sum = 0;
-for( var i = 0; i < allRates.length ; i++ ){
-    sum += allRates[i]; 
-}
-
-var avg = parseInt(sum, 10)/allRates.length;
-
-var avgNum = parseInt(avg, 10);
-
-
-function ratesAverage(){
-    return avgNum.toFixed(2);
-}
-
-// Iteration 5: Drama movies - Get the average of Drama Movies
-
-var dramaMovies = movies.filter(function(drama) {
-    if (drama.genre == "Drama") {
-    return true;
-    } else {
-    return false;
-    }
-});
-
-var dramaMoviesRateVar = dramaMovies.map(a => a.rate);
-
-var sumDrama = 0;
-for( var i = 0; i < dramaMoviesRateVar.length ; i++ ){
-    sumDrama += dramaMoviesRateVar[i]; 
-}
-
-var avgDrama = parseInt(sumDrama, 10)/dramaMoviesRateVar.length;
-
-var avgNum = parseInt(avgDrama, 10);
-
-function dramaMoviesRate() {
-    return avgNum;
-}
-
-
-// Iteration 6: Time Format - Turn duration of the movies from hours to minutes
-
-var onlyTimeArr = movies.map(a => a.duration);
-
-var date = new Date();
-console.log(date.getMinutes());
-
-// BONUS Iteration: Best yearly rate average - Best yearly rate average
+    return `The best year was ${theActualYear} with an average rate of ${highest}`;
+  }
