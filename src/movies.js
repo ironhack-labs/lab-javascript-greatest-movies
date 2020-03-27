66389780376ae091ae65026d5d6525c87c5cd12b
Victoria Kiseleva <111victoria.toria@gmail.com>
2020-03-28 00:57:13 +0100

    half way there

--- src/movies.js
+++ src/movies.js
@@ -1,15 +1,96 @@
 /* eslint no-restricted-globals: 'off' */
 
+const moviesDemo = [
+    {
+        "title": "The Shawshank Redemption",
+        "year": 1994,
+        "director": "Frank Darabont",
+        "duration": "2h 22min",
+        "genre": [
+            "Drama",
+            "Crime"
+        ],
+        "rate": 9.3
+        },
+    {
+        "title": "The Godfather",
+        "year": 1972,
+        "director": "Francis Ford Coppola",
+        "duration": "2h 55min",
+        "genre": [
+            "Crime",
+            "Drama"
+        ],
+        "rate": 9.2
+    }
+];
+
 // Iteration 1: Ordering by year - Order by year, ascending (in growing order)
-
+function orderByYear (movies){
+    let ordered = movies.slice().sort(function(a,b){
+        if (a.year===b.year){
+            return a.title.localeCompare(b.title);
+        }
+        return a.year-b.year;
+    })
+    return ordered;
+}
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
-
+function howManyMovies(movies){
+    let count=0;
+    let howMany = movies.slice().filter(function(drama){
+     if (drama.director === "Steven Spielberg"&& drama.genre.indexOf("Drama")>-1){
+         count ++;
+         return true;
+        };
+    })
+    return count;
+}
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
+function orderAlphabetically(movies){
+    let titles = movies.map(function(movie){
+        return movie.title;
+    });
+    
+    // let titles = [];
+    // for (let i = 0; i < movies.length; i++) {
+    //     let movie = movies[i];
+    //     titles.push(movie.title);
+    // }
+
+    titles.sort(function(a,b){
+        return a.localeCompare(b);
+    });
+    return titles.slice(0,20);
+}
 
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
+function ratesAverage(movies){
+    if (movies.length > 0) {
+        let sum= movies.reduce(function(sum,movie){
+           if (movie.rate) {
+                return sum+movie.rate;
+           }
+           else {return sum;}
+        },0);
+        const average = (sum/movies.length);
+        const averageRounded = Math.round(average*100)/100;
+        return averageRounded;
+    } else {
+        return 0;
+    }
+}
 
 // Iteration 5: Drama movies - Get the average of Drama Movies
-
+function dramaMoviesRate(movies){
+    const dramaMovies= movies.filter(function(movie){
+        // return movie.genre.indexOf("Drama")>-1; 
+        return movie.genre.includes("Drama"); 
+    });
+    return ratesAverage(dramaMovies);
+}
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
 
 // BONUS Iteration: Best yearly rate average - Best yearly rate average
+
+
