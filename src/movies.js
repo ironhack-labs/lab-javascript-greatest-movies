45bab7ea86b0cc8f8a90ee49a8205c92c919acf0
HeiseMo <tim.james.tanurhan@gmail.com>
2020-03-27 19:35:03 +0100

    Work in progress

--- src/movies.js
+++ src/movies.js
@@ -1,13 +1,51 @@
 /* eslint no-restricted-globals: 'off' */
 
 // Iteration 1: Ordering by year - Order by year, ascending (in growing order)
+function orderByYear(arr){
+    let arrYear=[...arr];
+    let yearList = arrYear.sort(function(a, b){
+        if (a.year < b.year) {
+            return -1;
+          }
+          if (a.year > b.year) {
+            return 1;
+          }
+          if(a.title > b.title){
+              return 1
+          }
+          return -1;
+    })
+    return yearList;
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
-
+function howManyMovies(arr){
+    let movies = arr.filter(function(dir){
+        if(dir.director == "Steven Spielberg" && dir.genre.includes("Drama")){ 
+        return dir.director.length;
+    } return 0
+    })
+   return movies.length
+}
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
-
+function orderAlphabetically(arr){
+    let newArr=[...arr]
+    return newArr = arr.map(function(movie){
+        title = movie.title;
+        return title
+    }).sort().slice(0,20)
+}
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
-
+function ratesAverage(arr){
+    if(arr.length == 0){return 0 } 
+  let avg = arr.map(function(avgMovieRate){  
+    return avgMovieRate.rate 
+    }).reduce(function (acc, cur){    
+  return  (acc + cur)
+ })
+ let val = parseFloat((avg/arr.length).toFixed(2));
+ return val
+}
 // Iteration 5: Drama movies - Get the average of Drama Movies
 
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
