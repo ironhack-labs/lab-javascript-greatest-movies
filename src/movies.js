9bc602b845a36553baeaee71a3d031c9c15f5e9e
NellyBP <nelly.borjon@hotmail.fr>
2020-03-29 12:54:30 +0200

    3 is done

--- src/movies.js
+++ src/movies.js
@@ -1,12 +1,23 @@
+const movies = require("./data")
+
 /* eslint no-restricted-globals: 'off' */
 
 // Iteration 1: Ordering by year - Order by year, ascending (in growing order)
+
+function orderByYear (array){
+    
+}
+
+
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
 
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
 
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
+function rateAverage(array) {
+    
+}
 
 // Iteration 5: Drama movies - Get the average of Drama Movies
 
