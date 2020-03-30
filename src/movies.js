69808334f0991e3406a32f9aef5b77c33dadb758
Elvira Ramírez <elvirarp92@gmail.com>
2020-03-30 16:16:01 +0200

    define howManyMovies

--- src/movies.js
+++ src/movies.js
@@ -4,6 +4,15 @@
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
 
+function howManyMovies(movieArray) {
+  const movieList = movieArray.filter(
+    movie =>
+      movie.genre.includes('Drama') && movie.director == 'Steven Spielberg'
+  );
+  return movieList.length;
+}
+
+
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
 
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
