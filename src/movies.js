bd9e0c9c5d3049304e131c010a019e957dad0c0c
Elvira Ramírez <elvirarp92@gmail.com>
2020-03-30 16:36:08 +0200

    define ratesAverage, passes spec test

--- src/movies.js
+++ src/movies.js
@@ -12,10 +12,21 @@ function howManyMovies(movieArray) {
   return movieList.length;
 }
 
-
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
 
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
+
+function ratesAverage(array) {
+  if (array.length == 0) {
+    return 0;
+  }
+  const hasRate = array.filter(elm => elm.rate);
+  const ratesSummation = hasRate.reduce((acc, movie) => {
+    return acc + movie.rate;
+  }, 0);
+  const average = ratesSummation / array.length;
+  return parseFloat(average.toFixed(2));
+}
 
 // Iteration 5: Drama movies - Get the average of Drama Movies
 
