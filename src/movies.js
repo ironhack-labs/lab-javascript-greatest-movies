fa2d8512a5ce47ef3573aa5e1d1a85d87652a468
Siiri Sikkilä <sisikkil@student.oulu.fi>
2020-04-02 17:43:27 +0300

    initial commit

--- src/movies.js
+++ src/movies.js
@@ -1,8 +1,27 @@
-/* eslint no-restricted-globals: 'off' */
+// THIS IS WHERE YOU PUT THE CODE !!!!! RIGHT?!
 
+/* eslint no-restricted-globals: 'off' */
 // Iteration 1: Ordering by year - Order by year, ascending (in growing order)
 
+//Create a function orderByYear() that receives an array as parameter and returns a new sorted array.
+//If two movies have the same year, order them in alphabetical order by their title! 
+
+list = ["lion king", "jumanji", "husbandStopMakingNoise"];
+
+function orderByYear(filmArr) {const whateverUwant = filmArr.sort((a,b) => a-b)
+    return whateverUwant}
+
+print = orderByYear(list)
+console.log(print);
+
+
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
+//One of the most famous directors in cinema is Steven Spielberg, and he has some really awesome drama movies 
+//that are on our list, but we want to know how many of them are there.
+//Go ahead and create a howManyMovies() function that receives an array as a parameter and filter the array 
+//so we can have only the drama movies where Steven Spielberg is the director.
+
+
 
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
 
