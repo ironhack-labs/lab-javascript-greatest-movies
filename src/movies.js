d8cda9215b0121fd5dd8e68fd2b9680bbb85acff
Marc De Mena <mdemena@gmail.com>
2020-04-02 21:36:10 +0200

    Iteration 1 finished

--- src/movies.js
+++ src/movies.js
@@ -1,9 +1,27 @@
 /* eslint no-restricted-globals: 'off' */
 
 // Iteration 1: Ordering by year - Order by year, ascending (in growing order)
+function orderByYear(films){
+    let sortedMovies = Array.from(films);
+
+    let sorting = (film1, film2) => {
+        if (film1.year===film2.year){
+            if (film1.title.toLowerCase()>film2.title.toLowerCase()){
+                return 1;
+            } else if (film1.title.toLowerCase()<film2.title.toLowerCase()) {
+                return -1;
+            } else { return 0; }
+        } 
+        else return film1.year-film2.year;
+    }
+
+    return sortedMovies.sort(sorting);
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
-
+function howManyMovies(films){
+    return films.length;
+}
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
 
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
