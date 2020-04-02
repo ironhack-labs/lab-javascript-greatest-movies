a8af38f1b71353fabb7235aa0333bdbd57bd4f6d
Herzen Cortes <herzencc@icloud.com>
2020-04-02 18:06:17 -0400

    iterations 1, 2

--- src/movies.js
+++ src/movies.js
@@ -1,8 +1,26 @@
 /* eslint no-restricted-globals: 'off' */
 
+
 // Iteration 1: Ordering by year - Order by year, ascending (in growing order)
+function orderByYear(arr){
+    let newMovies = [...arr]
+        return newMovies.sort((a,b) =>{
+    if(a.year === b.year) {
+        return a.title.localeCompare(b.title)
+    }
+    else {
+        return a.year - b.year
+    }
+    })
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
+
+const howManyMovies = (arr) => {
+    let sbMovies = arr.filter(obj => obj.director === "Steven Spielberg" && obj.genre.includes("Drama"));
+    return sbMovies.length
+}
+
 
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
 
