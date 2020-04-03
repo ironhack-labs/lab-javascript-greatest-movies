43a66a76e85ddbcb9766107e369b901d23bbb40f
JesusSantana <jesus@esmarketingdigital.com>
2020-04-03 17:52:11 +0200

    v2 Steben Spielberg

--- src/movies.js
+++ src/movies.js
@@ -1,19 +1,31 @@
 /* eslint no-restricted-globals: 'off' */
 
 // Iteration 1: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(film){
-    // crear una copia
-   let orderMovies = Array.from(film);
 
-   let order = (film1, film2) => 
-       film1.year === film2.year ?
-        film1.title.toUpperCase() > film2.title.toUpperCase() ? 1 : film1.title.toUpperCase() < film2.title.toUpperCase() ? -1 : 0 
-        : film1.year-film2.year;
-         
-   return orderMovies.sort(order);
+function orderByYear(films) {
+    // crear una copia
+    let orderMovies = Array.from(films);
+
+    let order = (film1, film2) =>
+        film1.year === film2.year ?
+        film1.title.toUpperCase() > film2.title.toUpperCase() ? 1 : film1.title.toUpperCase() < film2.title.toUpperCase() ? -1 : 0 :
+        film1.year - film2.year;
+
+    return orderMovies.sort(order);
 }
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
+
+function howManyMovies(films) {
+
+    return films.filter(film => film.director === "Steven Spielberg"  && filmGenre(film, 'Drama')).length;
+
+}
+
+function filmGenre(movie, genreToFilter) {
+    
+    return movie.genre.filter(filmGenre => filmGenre === genreToFilter).length > 0;
+}
 
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
 
