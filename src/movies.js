33631a7df2bc6271c2efb066295a7ff01685cd90
Toni <sancheztoni.garcia@gmail.com>
2020-04-03 22:51:45 +0200

    Iteration 3, 4 & 5 done

--- src/movies.js
+++ src/movies.js
@@ -1,4 +1,6 @@
 /* eslint no-restricted-globals: 'off' */
+'use strict';
+
 
 // Iteration 1: Ordering by year - Order by year, ascending (in growing order)
 const orderByYear = arrToOrder => {
@@ -14,28 +16,40 @@ const orderByYear = arrToOrder => {
     return moviesCopy
 }
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
-const howManyMovies = moviesArr => {
-    return moviesArr.filter(movie => movie.director === 'Steven Spielberg' && movie.genre.includes('Drama')).length;
-}
-/*const howManyMovies = moviesArr => {
-    if (!moviesArr.length) {
-        return 0;
-    }
-    if (!moviesArr.filter(movie => movie.genre.includes('Drama'))) {
-        return 0
-    } else {
-        return 1
-    }
+//Devolvemos las movies en una sola linea con arrow function y .filter()
+const howManyMovies = moviesArr => moviesArr.filter(movie => movie.director === 'Steven Spielberg' && movie.genre.includes('Drama')).length;
 
-    return 0
-    
-}*/
 
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
+const orderAlphabetically = moviesArr => {
+    //Copiamos array original y ordenamos alfabeticamente
+    let newArr = Array.from(moviesArr).sort((a, b) => {
+        if (a.title > b.title) return 1;
+        if (a.title < b.title) return -1;
+    })
+    //Hacemos un return de los titles con .map() y de los 20 primeros con .slice()
+    return newArr.map(a => a.title).slice(0, 20);
+}
 
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
 
+// La iteracion comienza analizando si el array introducido contiene datos.
+// Si es false, devuelve 0
+// Si es true, devuelve el rating de todas las peliculas (map) filtrado para que no haya datos invalidos (filter - ratio > 0) y reducido con reduce.
+
+const ratesAverage = moviesArr => !moviesArr.length ? 0 : Math.round(moviesArr.map(a => a.rate).filter(ratio => ratio > 0).reduce((acc, elem) => acc + elem, 0) / moviesArr.length * 100) / 100;
+
+
 // Iteration 5: Drama movies - Get the average of Drama Movies
+
+const dramaMoviesRate = moviesArr => {
+    //Almacenamos en dramaMovies las peliculas filtradas para que solo tengan genero 'Drama' y devolvemos unicamente el rate
+    let dramaMovies = moviesArr.filter(movie => movie.genre.includes('Drama') && movie.genre.length === 1).map(a => a.rate);
+    //Almacenamos la media de dramaMovies y reducimos a 2 decimales
+    let newArr = Math.round(dramaMovies.reduce((acc, elem) => acc + elem,0) /dramaMovies.length * 100) / 100;
+    //Devolvemos newArr si contiene datos para eliminar la posibilidad de que entre un array vacio
+    return !newArr ? 0 : newArr;
+}
 
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
 
