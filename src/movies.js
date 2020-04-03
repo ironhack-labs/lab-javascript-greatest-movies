284a234814108ded1ace86a7852fa59379f18890
Antoine Rolland <antoine-rolland@hotmail.fr>
2020-04-03 12:13:41 +0200

    Iteration 1 to 3

--- src/movies.js
+++ src/movies.js
@@ -2,9 +2,64 @@
 
 // Iteration 1: Ordering by year - Order by year, ascending (in growing order)
 
+function orderByYear(yearOfMovie) {
+
+    let copiaMovies = Array.from(yearOfMovie);
+    function ordenar(a,b) {
+        if (a.year !== b.year){
+            return a.year - b.year;
+        } else {
+            if (a.title.toUpperCase() > b.title.toUpperCase()) {
+                return 1;
+            } else {
+                return -1;
+            }
+        }
+    }
+
+    return copiaMovies.sort(ordenar);
+}
+
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
 
+function howManyMovies(genreOfMovie) {
+    let copiaMovies = Array.from(genreOfMovie);
+
+    function filterMovies(dramaMovies){
+        return dramaMovies.director === "Steven Spielberg" && dramaMovies.genre.includes("Drama");
+    }
+    copiaMovies = genreOfMovie.filter(filterMovies);
+
+    function sumaDrama(acc,elem) {
+      return acc + 1;
+    };
+    copiaMovies = copiaMovies.reduce(sumaDrama,0)
+
+    return copiaMovies;
+  }
+
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
+
+function orderAlphabetically(alphabetOrderedArr) {
+    let copiaMovies = Array.from(alphabetOrderedArr);
+
+    function ordenarAZ(a,b) {
+        if (a.title.toUpperCase() > b.title.toUpperCase()) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+
+    copiaMovies.sort(ordenarAZ);
+
+    copiaMovies = copiaMovies.map(movies => {
+
+        return movies.title
+    }).slice(0,20);
+
+    return copiaMovies;
+}
 
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
 
