d24149dbf59cf3754245005c9a4b10bcd1b16bcb
Albert Gozzi <albertgozzi@gmail.com>
2020-04-03 17:33:41 -0400

    Updated bonus question with in-class option

--- src/movies.js
+++ src/movies.js
@@ -1,11 +1,24 @@
 /* eslint no-restricted-globals: 'off' */
 
 // Iteration 1: Ordering by year - Order by year, ascending (in growing order)
+// const orderByYear = (arr) => {
+//     let newArr = [...arr.sort((a, b) => (a.year < b.year) ? -1 : 1)];
+//     newArr.sort((a, b) => (a.year === b.year && a.title.toLowerCase() < b.title.toLowerCase()) ? -1 : 1);
+//     return newArr;
+// } 
 const orderByYear = (arr) => {
-    let newArr = [...arr.sort((a, b) => (a.year < b.year) ? -1 : 1)];
-    newArr.sort((a, b) => (a.year === b.year && a.title.toLowerCase() < b.title.toLowerCase()) ? -1 : 1);
-    return newArr;
-} 
+    // concat() method combines two arrays
+    let sortedArr = [...arr]; // ... spread operator - we make a copy of the arr https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
+  
+    return sortedArr.sort((a, b) => {
+      if (a.year === b.year) {
+        if(!a.title){return}
+        return a.title.localeCompare(b.title); // localeCompare - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/localeCompare
+      } else {
+        return a.year - b.year;
+      }
+    });
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
 const howManyMovies = (arr) => {
@@ -56,32 +69,87 @@ const turnHoursToMinutes = (arr) => {
 }
 
 // BONUS Iteration: Best yearly rate average - Best yearly rate average
-const bestYearAvg = (arr) => {
-    if (arr.length === 0) { return null};
- 
-    let avgRateObject = {};
-
-    // Create an array of objects with key = year, value = array of ratings
-    arr.forEach((element) => {
-        if (!Object.keys(avgRateObject).includes(element.year.toString())) { avgRateObject[element.year] = [] }
-        avgRateObject[element.year].push(element.rate);
-    }); 
-
-    // Push average rating into array of objects
-    let avgRateArray = [];
-    for (const year in avgRateObject) {
-        let numRates = avgRateObject[year].length;
-        avgRateArray.push({
-            'year': year,
-            'avgRate': avgRateObject[year].reduce((a, b) => a + b) / numRates
-        })
+const bestYearAvg = (movies) => {
+    if(movies.length === 0){
+      return null
     }
+    let orderedMovies = orderByYear(movies)
+    console.log(orderedMovies)
+  
+    let bestYear = 0
+    let bestRate = 0
+    let year = 0;
+    let total = 0
+    let howMany = 0; 
+  
+  
+    console.log(orderedMovies, ' ordered in here')
+    if (orderedMovies.length === 1) {
+        return `The best year was ${orderedMovies[0].year} with an average rate of ${orderedMovies[0].rate}`
+    }
+    
+    for(let i=0; i<orderedMovies.length; i++){
+      if(year != orderedMovies[i].year){ //We have a change in year, so we want to calculate the average 
+        // Count prior when it changes
+        if( (total/howMany) > bestRate ){
+            bestRate = total/howMany
+            bestYear = year
+        }
 
-    // Sort by rating
-    avgRateArray.sort((a, b) => (a.avgRate > b.avgRate) ? -1 : 1);
+        // Reset
+        howMany = 0; 
+        total = 0; 
+        
+        // Count first
+        year = orderedMovies[i].year;
+        howMany++
+        total += orderedMovies[i].rate
 
-    // Sort by year with same rating
-    avgRateArray.sort((a, b) => (a.avgRate === b.avgRate && a.year < b.year) ? -1 : 1);
+      } else { //We are not changing years so we dont' want to calucalate average yet.  
+        // Count next ones
+        howMany++
+        total += orderedMovies[i].rate
+      }
+  
+    }
+    console.log(`The best year was ${bestYear} with an average rate of ${bestRate}`)
+  
+    return `The best year was ${bestYear} with an average rate of ${bestRate}`
+  }
 
-    return `The best year was ${avgRateArray[0].year} with an average rate of ${avgRateArray[0].avgRate}`;
-}
+// Anothre option
+// const bestYearAvg = (arr) => {
+//     if (arr.length === 0) { return null};
+    
+//     let avgRateObject = {};
+
+//     // Create an array of objects with key = year, value = array of ratings
+//     arr.forEach((element) => {
+//         if (!Object.keys(avgRateObject).includes(element.year.toString())) { avgRateObject[element.year] = [] }
+//         avgRateObject[element.year].push(element.rate);
+//     }); 
+
+//     console.log(`Avg rate object year : ratings`)
+//     console.log(avgRateObject);
+
+//     // Push average rating into array of objects
+//     let avgRateArray = [];
+//     for (const year in avgRateObject) {
+//         let numRates = avgRateObject[year].length;
+//         avgRateArray.push({
+//             'year': year,
+//             'avgRate': avgRateObject[year].reduce((a, b) => a + b) / numRates
+//         })
+//     }
+
+//     console.log(`Avg rate array year: year, avgRate: avgRate`)
+//     console.log(avgRateArray);
+
+//     // Sort by rating
+//     avgRateArray.sort((a, b) => (a.avgRate > b.avgRate) ? -1 : 1);
+
+//     // Sort by year with same rating
+//     avgRateArray.sort((a, b) => (a.avgRate === b.avgRate && a.year < b.year) ? -1 : 1);
+
+//     return `The best year was ${avgRateArray[0].year} with an average rate of ${avgRateArray[0].avgRate}`;
+// }
