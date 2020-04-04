8b6c5cee8b253ea0ed08c49c5c00b17a9276fa14
JesusSantana <jesus@esmarketingdigital.com>
2020-04-04 08:09:29 +0200

    v6.2 Time format

--- src/movies.js
+++ src/movies.js
@@ -1,4 +1,5 @@
 /* eslint no-restricted-globals: 'off' */
+"use strict"
 
 // Iteration 1: Ordering by year - Order by year, ascending (in growing order)
 
@@ -7,13 +8,13 @@ function orderByYear(films) {
   let orderMovies = Array.from(films);
 
   let order = (film1, film2) =>
-    film1.year === film2.year
-      ? film1.title.toUpperCase() > film2.title.toUpperCase()
-        ? 1
-        : film1.title.toUpperCase() < film2.title.toUpperCase()
-        ? -1
-        : 0
-      : film1.year - film2.year;
+    film1.year === film2.year ?
+    film1.title.toUpperCase() > film2.title.toUpperCase() ?
+    1 :
+    film1.title.toUpperCase() < film2.title.toUpperCase() ?
+    -1 :
+    0 :
+    film1.year - film2.year;
 
   return orderMovies.sort(order);
 }
@@ -45,15 +46,15 @@ function orderAlphabetically(films) {
 
 function ratesAverage(films) {
   let rates =
-    films.length === 0
-      ? 0
-      : parseFloat(
-          (
-            films
-              .filter(film => "rate" in film)
-              .reduce((sum, film) => sum + film.rate, 0) / films.length
-          ).toFixed(2)
-        );
+    films.length === 0 ?
+    0 :
+    parseFloat(
+      (
+        films
+        .filter(film => "rate" in film)
+        .reduce((sum, film) => sum + film.rate, 0) / films.length
+      ).toFixed(2)
+    );
 
   return rates;
 }
@@ -61,47 +62,29 @@ function ratesAverage(films) {
 // Iteration 5: Drama movies - Get the average of Drama Movies
 
 function dramaMoviesRate(films) {
+
   return ratesAverage(films.filter(film => filmGenre(film, "Drama")));
 }
 
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
 
 function turnHoursToMinutes(films) {
-  let filmMinute = JSON.parse(JSON.stringify(films));
 
-  filmMinute = filmMinute.map(filmHour => {
-    switch (filmHour.duration.length) {
-      case 2:
-        return {
-          duration: filmHour.duration[0] * 60
-        };
-        break;
-      case 5:
-        return {
-          duration: parseInt(filmHour.duration[0] + filmHour.duration[1])
-        };
-        break;
-      case 7:
-        return {
-          duration: filmHour.duration[0] * 60 + parseInt(filmHour.duration[3])
-        };
-        break;
-      case 8:
-        return {
-          duration:
-            filmHour.duration[0] * 60 +
-            parseInt(filmHour.duration[3] + filmHour.duration[4])
-        };
-        break;
-      default:
-        return {
-          duration: parseInt(filmHour.duration[0])
-        };
-        break;
-    }
-  });
+  return (JSON.parse(JSON.stringify(films))).forEach(film => film.duration = convertToMinutes(film.duration));
+}
 
-  return filmMinute;
+function convertToMinutes(stringTime) {
+
+  let minutes = 0;
+  let times = stringTime.split(' ');
+
+  for (let i = 0; i < times.length; i++) {
+
+    (times[i].indexOf('h') >= 0) ? minutes += parseInt(times[i].substring(0, times[i].indexOf('h'))) * 60:
+      (times[i].indexOf('m') >= 0) ? minutes += parseInt(times[i].substring(0, times[i].indexOf('m')))
+
+  }
+  return minutes;
 }
 
 // BONUS Iteration: Best yearly rate average - Best yearly rate average
