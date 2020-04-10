09251e94e583ab5cfa2081eacef713043af5d0e1
Robert Ledezma <Robert@MacBook-Pro-de-Robert-2.local>
2020-04-10 15:12:25 +0200

    Falta el Bonus

--- src/movies.js
+++ src/movies.js
@@ -47,12 +47,90 @@ function orderAlphabetically(arrDePeliculas){
 
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
 
-function ratesAverage(arrAvergae){
-    let average = arrAvergae.map(arrAvergae => arrAvergae.rate);
-    return average.reduce()
-};
+function ratesAverage (arr){
+    if (arr.length === 0) {
+          return 0;
+      }
+    let moviesRatesuma = arr.reduce(function(a,b){
+        if(b.rate) {
+            return a + b.rate
+           }
+           return a  
+      let sum = a + b.rate
+      return sum
+    },0)
+    let promedio = moviesRatesuma / arr.length
+    return parseFloat(promedio.toFixed(2));
+  };
+
+
 // Iteration 5: Drama movies - Get the average of Drama Movies
+
+function dramaMoviesRate(peliculasDrama){
+       let dramaFiltro = peliculasDrama.filter(function(element){
+           return element.genre.includes("Drama")
+       })
+
+       return ratesAverage(dramaFiltro)
+};
 
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
 
+function turnHoursToMinutes(movieArr) {
+    // create a copy using JSON
+    let copyMovies = JSON.parse(JSON.stringify(movieArr));
+    // create a minutes array with conversion h-min format to min
+    /*copyMovies.forEach(function(movie){
+      let newVar = movie.duration*60+movie.duration;
+      movie.duration = newVar;
+    })*/
+    copyMovies.forEach(function(movie){
+      let hourIndex = movie.duration.indexOf("h");
+      let minIndex = movie.duration.indexOf("min");
+      let minutesHour = 0;
+      if (hourIndex === -1) {
+        minutesHour = 0;
+      } else {
+        minutesHour = parseInt(movie.duration[hourIndex-1])*60;
+      }
+      let minutes = 0;
+      if (minIndex === -1) {
+        minutes = 0;
+      } else {
+        minutes = parseInt(movie.duration[minIndex-2] + movie.duration[minIndex-1]);
+      }
+      movie.duration = minutesHour + minutes;
+    })
+
+    /*let minutesArray = movieArr.map(function(movie) {
+        let hourIndex = movie.duration.indexOf("h");
+        let minIndex = movie.duration.indexOf("min");
+        let minutesHour = 0;
+        if (hourIndex === -1) {
+          minutesHour = 0;
+        } else {
+          minutesHour = parseInt(movie.duration[hourIndex-1])*60;
+        }
+        let minutes = 0;
+        if (minIndex === -1) {
+          minutes = 0;
+        } else {
+          minutes = parseInt(movie.duration[minIndex-2] + movie.duration[minIndex-1]);
+        }
+        return minutesHour + minutes;
+    });
+    // change the duration format to a new min format
+    for (let i=0; i<copyMovies.length; i++) {
+        copyMovies[i].duration = minutesArray[i];
+    }*/
+    return copyMovies;
+}
+
 // BONUS Iteration: Best yearly rate average - Best yearly rate average
+
+function bestYearAvg(movieYear){
+    if(movieYear.length === 0 ){
+        return null;
+    } 
+
+};
