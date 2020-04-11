1da87945616989d7cbeb592a41099b736a09d67a
alex gera <alex.ger93@gmail.com>
2020-04-11 14:47:00 +0200

    iteration 4,5

--- src/movies.js
+++ src/movies.js
@@ -22,9 +22,23 @@ function orderAlphabetically (arr) {
 // return titles only and limit to first 20 values
     return arrCopy.sort(compareTitles).map(a => a.title).slice(0, 20)
 }
+// Iteration 4: All rates average - Get the average of all rates with 2 decimals
+function ratesAverage (arr){
+    let arrCopy = Array.from(arr)
+    if (!arrCopy.length) {
+        return 0;
+    }
+    let newArr = arrCopy.map(a => a.rate)
+    let avgRate = Math.round(newArr.reduce((a, b) => a + b, 0) / newArr.length * 100) / 100;
 
+   return !avgRate ? 0 : avgRate
+}
 // Iteration 5: Drama movies - Get the average of Drama Movies
-
+function dramaMoviesRate (arr) {
+    let newArr = arr.filter(a => a.genre.includes("Drama")).map(a => a.rate)
+    let dramaAvg = Math.round(newArr.reduce((acc, el) => acc + el, 0) / newArr.length * 100) / 100;
+    return !dramaAvg ? 0 : dramaAvg
+}
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
 
 // BONUS Iteration: Best yearly rate average - Best yearly rate average
