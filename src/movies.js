73de88be5851c16fcd700ca0bcf89e447613b08f
James Dawson <63356869+polymurph13@users.noreply.github.com>
2020-05-07 14:22:03 +0100

    complete up to iteration 7

--- src/movies.js
+++ src/movies.js
@@ -20,12 +20,52 @@ function ratesAverage(array) {
 }
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-
+function dramaMoviesRate(array) {
+    let dramaMovies = array.filter((obj) => obj['genre'].includes('Drama'));
+    return ratesAverage(dramaMovies);
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
+function orderByYear(array) {
+    let sortedArray = JSON.parse(JSON.stringify(array));
+    
+    return sortedArray.sort((a, b) => {
+        if(a.year != b.year) {
+            return a.year - b.year;
+        } else {
+            return (a.title >= b.title) ? 1 : -1;
+        }
+    });
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
+function orderAlphabetically(array) {
+    let sortedArray = JSON.parse(JSON.stringify(array));
+    sortedArray.sort((a, b) => {
+        return (a.title >= b.title) ? 1 : -1;
+    });
+    sortedArray = sortedArray.map((obj) => {return obj['title'];});
+    return sortedArray.slice(0, 20);
+}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
+function turnHoursToMinutes(array) {
+    let sortedArray = JSON.parse(JSON.stringify(array));
+    sortedArray = sortedArray.map((obj) => {
+        let sum = 0;
+        let time = obj['duration'];
+        if(time.includes('h')) {
+            sum += (parseInt(time[0]) * 60);
+            if(time.includes('min')) {
+                sum += (parseInt(time.slice(time.indexOf(' '), time.indexOf('min'))));
+            }
+        } else {
+            sum += parseInt(time.slice(0, time.indexOf('min')));
+        }
+        obj['duration'] = sum;
+        return obj;
+    });
+    return sortedArray;
+}
 
 // BONUS - Iteration 8: Best yearly rate average - Best yearly rate average
