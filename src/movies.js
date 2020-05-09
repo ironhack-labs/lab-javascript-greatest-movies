31eed2dd740a7dd93cf12a55f9bc245ea1afc70e
Pumpel <romanbaos@gmail.com>
2020-05-09 15:23:34 +0200

    Iteration 4: Drama movies

--- src/movies.js
+++ src/movies.js
@@ -29,6 +29,12 @@ function ratesAverage(myArray) {
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 
+function dramaMoviesRate(myArray) {
+    const totalDramaArray = myArray.filter(dramaMovies => dramaMovies.genre.includes("Drama"))
+    return ratesAverage(totalDramaArray)
+    
+}
+
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
