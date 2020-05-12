e5b869a5dd655e85c762974bf40844ecb4ed6832
Jaime Baro <jaimebaroval@gmail.com>
2020-05-12 19:59:05 +0200

    interaction 1,2,3

--- src/movies.js
+++ src/movies.js
@@ -1,9 +1,52 @@
+let allDirectors = []
+
 // Iteration 1: All directors? - Get the array of all directors.
+function getAllDirectors(moviesArray) {
+
+    allDirectors = moviesArray.map(dir => dir.director)
+
+    return allDirectors
+
+
+}
+
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
+// let unique = []
+
+// unique = allDirectors.filter((dir1, dir2) => {
+//     if (dir1.indexOf())
+
+// console.log(unique);
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 
+function howManyMovies(moviesArray) {
+
+    moviesArray.length === 0 ? 0 : null
+
+    const steveMovies = moviesArray.filter(num => num.director === 'Steven Spielberg')
+    const steveDramaMovies = []
+
+    steveMovies.filter(function (drama) {
+        drama.genre.map(function (d) {
+            d === 'Drama' ? steveDramaMovies.push(drama.title) : null
+        })
+    })
+
+    return steveDramaMovies.length
+}
+
+
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
+function ratesAverage(moviesArray) {
+    const movieRate = moviesArray.map(e => e.rate)
+    let totalRate = 0
+
+    movieRate.forEach(e => totalRate += e)
+
+    return (totalRate / movieRate.length) ? Math.round((totalRate / movieRate.length) * 100) / 100 :
+        totalRate === 0 ? 0 : 2
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 
