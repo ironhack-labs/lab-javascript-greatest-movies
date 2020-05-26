ec088b4b81625b5c3b331fb02ba48297727b0bc0
fermarinsanchez <fermarinsanchez@yahoo.es>
2020-05-26 12:34:02 +0200

    refactor some functions

--- src/movies.js
+++ src/movies.js
@@ -8,7 +8,7 @@ function getAllDirectors(array) {
 // Bonus - Iteration 1.1: Clean the array of directors
 
 function getAllDirectorsRemoveDupicates(array) {
-  const listDirectors = array.map(directors => directors.director)
+  const listDirectors = getAllDirectors(array)
   return listDirectors.filter((elem, index) => listDirectors.indexOf(elem) === index)
 }
 
@@ -21,7 +21,7 @@ function howManyMovies(array){
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
 
 function ratesAverage(array) {
-  if ( array.length === 0) {
+  if (!array.length) {
     return 0
   } else {
   let avg = array.reduce((acc, currentV) =>  {
@@ -87,20 +87,53 @@ function orderAlphabetically(array) {
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 
 function turnHoursToMinutes(array) {
-  const newObject = [...array]
-  const replaceLetters = newObject.map(movie => {
-  movie.duration = movie.duration.replace('h', '').replace('min', '').split(' ')
-  console.log(movie)
-  return movie
+
+  const newTimeInMinutes = array.map( movie => {
+      const mappedMovie = {
+          title: movie.title,
+          year: movie.year,
+          director: movie.director,
+          duration: movie.duration,
+          genre: movie.genre,
+          rate: movie.rate
+      }
+     
+      if (mappedMovie.duration.includes("h") && mappedMovie.duration.includes("min")) {
+          mappedMovie.duration = mappedMovie.duration.slice(0, -3).split("h");
+          mappedMovie.duration = 
+          (mappedMovie.duration[0])*60 + Number(mappedMovie.duration[1])
+      } else if (mappedMovie.duration.includes("h") && (mappedMovie.duration.includes("min") === false)) {
+          mappedMovie.duration = Number(mappedMovie.duration.slice(0, -1))*60
+      } else if (mappedMovie.duration.includes("h")=== false && (mappedMovie.duration.includes("min") === true)){
+          mappedMovie.duration = Number(mappedMovie.duration.slice(0, -3))
+      }
+      return mappedMovie
   })
-  const correctTime = replaceLetters.map((element) =>{
-    if (!element.duration[0]) {
-      return Number(element.duration[1])
-      } else {
-    element.duration = (element.duration[0] *60) + Number(element.duration[1])}
-    return element
-  })
-  return correctTime
+  return newTimeInMinutes;
 }
 
 // BONUS - Iteration 8: Best yearly rate average - Best yearly rate average
+
+function bestYearAvg(array) {
+  if (!array.length) {
+    return null
+  } else {
+    let flimsByYear = array.reduce((acc, el) => {
+      acc[el.year] = (acc[el.year] || [])
+      acc[el.year].push(el)
+      return acc
+    }, {})
+    
+    let yearRate = Object.keys(flimsByYear).reduce((acc, el) => {
+      acc[el] = flimsByYear[el].reduce((acc, el) => acc += el.rate, 0) / flimsByYear[el].length
+        return acc
+    }, {})
+    
+  
+    let sortedByRate = Object.keys(flimsByYear).sort((a, b) => yearRate[b] - yearRate[a])
+    console.log(sortedByRate)
+    
+    return `The best year was ${sortedByRate[0]} with an average rate of ${yearRate[sortedByRate[0]]}`
+  }
+  
+}
