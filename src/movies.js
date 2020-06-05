8a0cc7246ee5a7c3f8895f5436ffccc2a32c68d2
luisschmitz <luis.schmitz@web.de>
2020-06-05 19:03:40 +0200

    done

--- src/movies.js
+++ src/movies.js
@@ -41,9 +41,53 @@ function ratesAverage(movies) {
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 
+function dramaMoviesRate(movies){
+    let dramaMovies = movies.filter(function (movie) {
+        if (movie.genre.includes("Drama")) {
+          return movie;
+        }
+      });
+      if (dramaMovies.length === 0) {
+          return 0;
+      };
+    let sumRates = dramaMovies.reduce(function(acc, val){
+        return acc + (val.rate || 0)
+    }, 0);
+    return parseFloat((sumRates/dramaMovies.length).toFixed(2))
+}
+
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 
+function orderByYear(movies) {
+    let clone = movies.slice();
+
+    let sortedMovies = clone.sort(function(a,b){
+        
+           if (a.year > b.year)
+           return 1; 
+           else 
+       return -1; 
+         //return a.year-b.year; 
+    })
+    return sortedMovies; 
+}
+
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
+
+function orderAlphabetically(movies) {
+    let clone = movies.slice();
+
+    let sortedAlpha = clone.sort(function(a,b) {
+        if (a.title > b.title)
+        return 1;
+        else 
+        return -1;
+    })
+    .slice(0,20)
+    return sortedAlpha.map(function(movie){
+        return movie.title;
+    })
+}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 
