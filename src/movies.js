c65106bbe169379866018bf72bb45aa445a90f36
SiEr0212 <simonertel@gmx.de>
2020-06-07 21:01:04 +0200

    Iteration 1-6, with bonusIteration 1

--- src/movies.js
+++ src/movies.js
@@ -36,7 +36,7 @@ function howManyMovies(movies) {
   return spielbergDramaMovies.length;
   }
   
-// Iteration 3: All rates average - Get the average of all rates with 2 decimals;
+// Iteration 3: All rates average - Get the average of all rates with 2 decimals;//
 
 
 function ratesAverage(movies) {
