8912bbade89cb0bc4c5d726f7e211c17b0714c07
Roberto Gonzalez <rob.gonzalez3@gmail.com>
2020-06-08 19:00:07 +0200

    done without bonus

--- src/movies.js
+++ src/movies.js
@@ -1,15 +1,71 @@
 // Iteration 1: All directors? - Get the array of all directors.
+
+function getAllDirectors(movies) {
+    const directors = movies.map(movies => movies.director)
+    return directors
+
+    console.log(directors)
+}
+
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
+
+
+
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 
+function howManyMovies(movies) {
+    const moviesSpielberg = movies.filter(elm => elm.director === "Steven Spielberg" && elm.genre.includes("Drama")).length
+    return moviesSpielberg
+}
+
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
 
+function ratesAverage(movies) {
+    if (movies.length === 0) return 0;
+    const averageRates = movies.reduce((acc, elm) => {
+        if (elm.rate){
+            return elm.rate + acc
+        } else {
+            return acc
+        } 
+    },0 )
+    return Math.round((averageRates / movies.length) * 100) / 100
+}
 // Iteration 4: Drama movies - Get the average of Drama Movies
+
+function dramaMoviesRate(movies) {
+    const dramaMovies = movies.filter(elm => elm.genre.includes("Drama"))
+    if (dramaMovies.length === 0) return 0
+    const dramaMoviesRate = dramaMovies.reduce((acc, elm) => acc + elm.rate, 0)
+
+    return Math.round((dramaMoviesRate / dramaMovies.length) * 100) / 100
+}
+
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 
+function orderByYear(movies) {
+
+    const yearSort =[...movies].sort((a, b) => {
+        if (a.year === b.year) {
+            return a.title.localeCompare(b.title)
+        } 
+        return a.year - b.year
+    })
+
+    return yearSort
+}
+
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
+
+function orderAlphabetically(movies) {
+    
+    const alphaSort = [...movies].sort((a,b) => a.title.localeCompare(b.title))
+    const first20 = alphaSort.slice(0, 20).map(movies => movies.title)
+    return first20
+
+}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 
