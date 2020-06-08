a16886453c23fe7ca4960697dc9e1e5f7e22ea50
Rachel de la Torre <Rachel.delatorre1@gmail.com>
2020-06-08 10:56:57 +0200

    Rachel de la Torre didn't finish but is learning more every day

--- src/movies.js
+++ src/movies.js
@@ -1,12 +1,65 @@
 // Iteration 1: All directors? - Get the array of all directors.
+
+const getAllDirectors = movies.map(function(movie){
+    return movie.director
+})
+
+console.log(getAllDirectors)
+
+
+
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
+
+// const getAllDirectors = movies.map(function(movie){
+//     return !movies.includes(movie);
+// })
+
+// console.log(getAllDirectors)
+
+
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 
+
+
+// function howManyMovies(arr)
+
+
+const howManyMovies = movies.filter(function(movie) {
+    if movie.director === "Steven Spielberg" && if movie.genre.includes "Drama"
+})
+     
+
+
+
+
+    // const placesWithPool = places.filter(function (place) {
+    //     return place.pool === 'yes';
+    // }).map(function (place) {
+    //     return {
+    //         title: place.title
+    //     }
+    // });
+
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
+
+
+function ratesAverage(arr) {
+    const average = movies.reduce(function(sum,val){
+     return sum + val.rate || 0;
+     },0);{
+return Number((average / arr.length).toFixed(2));
+}
+
+// .filter 
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 
+function dramaMoviesRate(movies){
+    const dramaMovie = movies.filter (function(movie)}
+    return movie.genre.indexOf("Drama")
+})
+}
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
