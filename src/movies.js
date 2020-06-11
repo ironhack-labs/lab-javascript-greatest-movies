0e3db9e68010715f5f2522b574be0a7b09152806
Kevin Hernandez <cobra40@gmail.com>
2020-06-10 23:09:54 -0400

    iterations 1 - 6 are functional, but need work to pass all specs

--- src/movies.js
+++ src/movies.js
@@ -2,11 +2,10 @@ var movies = require('./data');
 
 // Iteration 1: All directors? - Get the array of all directors.
 function getAllDirectors(movies){
-    // let uniqueArray = [];
-    const directors = movies.map(movie => movie.director); //creates new array of director names
-    // const uniqueDirectors = directors.filter(director => uniqueDirectors.includes(director) ); //filters out repeated director names
+    const directors = movies.map(movie => movie.director); //creates new array of director name
     return directors;
 }
+
 console.log(getAllDirectors(movies));
 
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
@@ -26,25 +25,41 @@ function ratesAverage(movies){
     let averageRating = ratingSum / ratingsArray.length;
     return parseFloat(averageRating.toFixed(2));
 }
+
 console.log(ratesAverage(movies));
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesRate(movies){
     const dramaMovies = movies.filter(movie => movie.genre.includes("Drama"));
-    const dramaRatings = dramaMovies.map(movie => movie.genre.includes('Drama') ? movie.rate : 0);
+    const dramaRatings = dramaMovies.map(movie => movie.rate);
     const dramaRatingSum = dramaRatings.reduce((accumulator, current) => accumulator + current);
     let dramaRatingAve = dramaRatingSum / dramaMovies.length;
     return parseFloat(dramaRatingAve.toFixed(2));
 }
+
 console.log(dramaMoviesRate(movies));
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear(movies){
-
+    const ascending = [...movies].sort((a, b) => a.year - b.year);
+    return ascending;
 }
 
+console.log(orderByYear(movies));
+
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
+function orderAlphabetically(movies){
+    const alphabetOrder = movies.map(movie => movie.title).sort();
+    return alphabetOrder.slice(0, 20);
+}
+
+console.log(orderByYear(movies));
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
+function turnHoursToMinutes(movies){
+    const minuteArray = movies.map(movie => {
+        
+    })
+}
 
 // BONUS - Iteration 8: Best yearly rate average - Best yearly rate average
