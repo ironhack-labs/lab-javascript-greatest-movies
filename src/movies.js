9db215f1ba76630806cc971034ebfdd34539b346
Kevin Hernandez <cobra40@gmail.com>
2020-06-12 07:58:49 -0400

    working on iteration 3 & 4 last few specs

--- src/movies.js
+++ src/movies.js
@@ -3,13 +3,19 @@ var movies = require('./data');
 // Iteration 1: All directors? - Get the array of all directors.
 function getAllDirectors(movies){
     const directors = movies.map(movie => movie.director); //creates new array of director name
-    return directors;
+    return uniqueArray(directors);
 }
 
 console.log(getAllDirectors(movies));
 
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
-
+function uniqueArray(array){
+    let newArray = [];
+    array.forEach(element => {
+        if(!newArray.includes(element)) newArray.push(element);
+    })
+    return newArray;
+}
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 function howManyMovies(movies){
     const spielbergDramas = movies.filter(movie => movie.director === "Steven Spielberg" && movie.genre.includes("Drama"));
@@ -21,7 +27,7 @@ console.log(howManyMovies(movies));
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
 function ratesAverage(movies){
     const ratingsArray = movies.map(movie => movie.rate);
-    const ratingSum = ratingsArray.reduce((accumulator, current) => accumulator + current);
+    const ratingSum = ratingsArray.reduce((accumulator, current) => accumulator + current, 0);
     let averageRating = ratingSum / ratingsArray.length;
     return parseFloat(averageRating.toFixed(2));
 }
@@ -32,7 +38,7 @@ console.log(ratesAverage(movies));
 function dramaMoviesRate(movies){
     const dramaMovies = movies.filter(movie => movie.genre.includes("Drama"));
     const dramaRatings = dramaMovies.map(movie => movie.rate);
-    const dramaRatingSum = dramaRatings.reduce((accumulator, current) => accumulator + current);
+    const dramaRatingSum = dramaRatings.reduce((accumulator, current) => accumulator + current, 0);
     let dramaRatingAve = dramaRatingSum / dramaMovies.length;
     return parseFloat(dramaRatingAve.toFixed(2));
 }
@@ -45,7 +51,7 @@ function orderByYear(movies){
     return ascending;
 }
 
-console.log(orderByYear(movies));
+// console.log(orderByYear(movies));
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically(movies){
@@ -53,13 +59,44 @@ function orderAlphabetically(movies){
     return alphabetOrder.slice(0, 20);
 }
 
-console.log(orderByYear(movies));
+// console.log(orderAlphabetically(movies));
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes(movies){
     const minuteArray = movies.map(movie => {
-        
+        let sum = 0;
+        let split = movie.duration.split(' ');
+
+        for(let i = 0; i < split.length; i++){
+            if(split[i].slice(-1) === 'h') sum += +split[i][0] * 60;
+            else sum += +split[i].slice(0, -3);            
+        }
+        movie.duration = sum;
+        return movie;
     })
+    return minuteArray;
 }
 
+// console.log(turnHoursToMinutes(movies));
+
 // BONUS - Iteration 8: Best yearly rate average - Best yearly rate average
+function bestYearAvg(movies){
+    const yearOrderedArray = movies.map(movie => movie.year).sort();
+    const yearRating = [];
+
+    for(let i = 0; i < yearOrderedArray.length; i++){
+        let obj = {};
+        const yearArray = movies.map(movie => {
+            if(movie.rate === yearOrderedArray[i]) return movie.rate
+        });
+        return yearArray;
+        const summedYear = yearArray.reduce((accumulator, current) => accumulator + current);
+        let ave = summedYear / yearArray.length;
+        obj.yearOrderedArray[i] = ave;
+        yearRating.push(obj);
+    }
+    return yearRating;
+    // return 'The best year was ${} with an average rate of ${}';
+}
+
+console.log(bestYearAvg(movies));
