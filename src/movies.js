619b4030381ce7ced7a184193e57b7368f08a11f
Arianna Valle <ariannamarbella@gmail.com>
2020-06-12 22:33:02 -0400

    Complete iteration 7

--- src/movies.js
+++ src/movies.js
@@ -52,7 +52,6 @@ let orderByYear = (movies) => {
 	});
 	return orderedMoviesByYear;
 };
-console.log(orderByYear(movies));
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 let orderAlphabetically = (movies) => {
@@ -63,5 +62,27 @@ let orderAlphabetically = (movies) => {
 }; 
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
+let turnHoursToMinutes = (movies) => {
+	let durations = movies.map((movie) => movie.duration); //return value example: "2h 22min"
+	let split = durations.map((duration) => {duration = duration + ""; return duration.split(" ");}); //return value example: ["2h", "22min"]
+	let time = split.map((duration) => {
+		if (!duration[1] && duration[0].includes("min")) {		 //if there are only minutes
+			minutes = duration[0].replace(/[^0-9]/g, ""); 		 //evaluates to the minutes without the text and returns it, ie. 22
+			minutes = Number(minutes);
+			return minutes;
+		} else if (!duration[1] && duration[0].includes("h")) {  //if there are only hours
+			hoursToMinutes = duration[0].replace(/[^0-9]/g, ""); //evaluates to the hours without the text and returns it, ie. 2
+			hoursToMinutes = Number(hoursToMinutes) * 60; 		 //multiply 2 by 60 to get the total minutes in the hours
+			return hoursToMinutes;
+		} else {												 //else there are both hours and minutes
+			hoursToMinutes = duration[0].replace(/[^0-9]/g, ""); 
+			hoursToMinutes = Number(hoursToMinutes) * 60; 		 
+			minutes = duration[1].replace(/[^0-9]/g, ""); 		 
+			minutes = Number(minutes);
+			return hoursToMinutes + minutes;				
+		}});
+	let replaceWithMinutes = movies.map((movie, index) => {movie.duration = time[index]; return movie;});
+	return replaceWithMinutes;
+};
 
 // BONUS - Iteration 8: Best yearly rate average - Best yearly rate average
