11e50bed32c897dca4a36b62bdd0e3c62f7fe066
CassandreV <cassandre.vanzetta@live.fr>
2020-06-13 17:01:32 +0200

    iteration 5 ongoing + iteration 3 correction

--- src/movies.js
+++ src/movies.js
@@ -20,20 +20,38 @@ function howManyMovies(moviesArr) {
 
 howManyMovies(movies);
 
+
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
 function ratesAverage(array) {
-  return (
-    array.reduce(function (acc, el) {
-      return acc + el.rate.toFixed(2); // car 0 qui remplace acc.rate
-    }, 0) / array.length
-  );
+    const sum = array.reduce(function (acc, el) {
+        return acc + el.rate // car 0 qui remplace acc.rate
+    }, 0);
+  return Number((sum/array.length).toFixed(2));
 }
 let result = ratesAverage(movies);
 console.log(result);
 
+
 // Iteration 4: Drama movies - Get the average of Drama Movies
 
+function dramaMoviesRate (array) {
+    return array.reduce(function (acc, el) {
+        return acc + el.genre.includes("Drama").rate;
+    }, 0) / array.length;
+}
+
+dramaMoviesRate(movies);
+
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
+
+function orderByYear (moviesArr) {
+    copyMoviesArr = moviesArr.slice();
+    return copyMoviesArr.sort(function (a, b) {
+        return a.year - b.year;
+    });
+}
+
+orderByYear(movies);
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 
