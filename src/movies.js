0fac56407f1989db1903a3ad7efb0cd1581d12a2
Matthew Howard <mattalanhoward@gmail.com>
2020-06-14 11:27:12 +0200

    First commit (iteration 1,3,4 complete)

--- src/movies.js
+++ src/movies.js
@@ -1,11 +1,43 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
 
+const getAllDirectors = (arr) => {
+    const directorsNames = arr.map(function(dir){
+        return dir.director
+    })
+    return directorsNames
+    };
+
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
+const dramaSS = movies.filter(function(spielbergDramas){
+    return (spielbergDramas.director = "Steven Spielberg") && (spielbergDramas.genre ='Drama')})
 
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
-
+const ratesAverage = (arr) => {
+    if (arr.length > 0){
+        const average = arr.reduce(function(acc,value){
+      return acc + value.rate/arr.length
+    },0)
+        return Math.round(average * 100) / 100
+    } else {
+        return 0
+    }
+}
 // Iteration 4: Drama movies - Get the average of Drama Movies
+//create function to get newArr of movies based on genre.
+
+const dramaMoviesRate = (arr) => {
+    const newArr = arr.filter(movies=>movies.genre.includes('Drama'));
+        if (newArr.length > 0){
+            const average = newArr.reduce(function(acc,value){
+                return acc + value.rate/newArr.length
+    },0)
+                return Math.round(average * 100) / 100
+        } else {
+                return 0
+        }
+};
+
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 
