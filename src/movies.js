1acd715f243d4ca23e378225600c9ab2b62ce9c4
unknown <you@example.com>
2020-06-15 23:30:56 +0200

    final review

--- src/movies.js
+++ src/movies.js
@@ -1,11 +1,14 @@
 // Iteration 1: All directors? - Get the array of all directors.
+
 function getAllDirectors(movies) {
 const arrayDirectors = movies.map(allmovies => allmovies.director);
 return arrayDirectors;
 }
 
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
+
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
+
 function howManyMovies(movies) {
 const dramaMovies = movies.filter( (movies) => movies.director === "Steven Spielberg" && movies.genre.includes("Drama"));
 const dramaMoviesNumber = dramaMovies.length;
@@ -13,6 +16,7 @@ return dramaMoviesNumber;
 }
     
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
+
 function ratesAverage(movies) {
 let average = movies.reduce((acc, elm) => {
 if (elm.rate) {
@@ -28,12 +32,14 @@ return Math.round((average/movies.length)*100)/100
 }
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
+
 function dramaMoviesRate(movies) {
 let dramamovies = movies.filter(elm => elm.genre.includes("Drama"));
 return ratesAverage(dramamovies);
 }
 
-// Iteration 5: Ordering by year - Order by year, ascending (in growing order)GI
+// Iteration 5: Ordering by year - Order by year, ascending (in growing order)
+
 function orderByYear(movies){
 const order = [...movies].sort((a, b) => {
 if (a.year === b.year) {
@@ -48,6 +54,7 @@ return a.year - b.year;
 }
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
+
 function orderAlphabetically(movies) {
 const orderAlpha = [...movies].sort((a, b) => a.title.localeCompare(b.title));
 const twenty = orderAlpha.slice(0, 20).map((movie) => movie.title);
