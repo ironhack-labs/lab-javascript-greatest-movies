84274266c6e96bb4529d3878e38aebc18a075820
Aurore CR <aurorenaturerouge@hotmail.fr>
2020-07-01 01:43:20 +0200

    last

--- src/movies.js
+++ src/movies.js
@@ -1,30 +1,39 @@
 // Iteration 1: All directors? - Get the array of all directors.
 //We need to get the array of all directors. Since this is a warm up, we will give you a hint: you have to map through the array of movies and get all the directors into one array as a final result. Go ahead and create a function named getAllDirectors() that receives an array of movies as an argument and returns a new (mapped array)
 
-
-function getAllDirectors(arrParameters) {
-  let arrDirector = arrParameters.map(function(Movies))
-  return movies.director
-});
- return arrDirector
-
+function getAllDirectors(movies) {
+  return movies.map(function(movie){
+  return movie.director;
+})
+}
 
 
-// Iteration 2: Steven Spielberg. The best?
+// -----------Iteration 2: Steven Spielberg. The best?
 // One of the most famous directors in cinema is Steven Spielberg, and he has some really awesome drama movies that are on our list, but we want to know how many of them are there.
 
 // Go ahead and create a howManyMovies() function that receives an array as a parameter and filter  the array so we can have only the drama movies where Steven Spielberg is the director.
 
-function howManyMovies(selectionMovie[0]){
-  let bestSpil = movies.director.StevenSpielberg.filter(function(Spil))
-return {}
-});
 
-return howManyMovies
+const howManyMovies = arr =>{
+	let result = arr.filter(function(movie){return movie.director == 'Steven Spielberg' && movie.genre.includes('Drama')})
+	return result.length
+}
+
+// ---------- Iteration 3: All rates average
+// These are the best movies based on their rates, so supposedly all of them have a remarkable rate. In this iteration, we want to know the average rate of all of them and display it on the console. Create a ratesAverage() function that receives an array as a parameter and solves the challenge.
+// The rate must be returned rounded to 2 decimals!
+//  aybe you want to "reduce" the data to a single value. 
 
 
+const ratesAverage = arr => {
+	const reducer = (accumulator, item) => { 
+		if (item.rate){
+			return accumulator + item.rate}
+		else{
+			return accumulator + 0}
+	}
 
-// Iteration 3: All rates average - Get the average of all rates with 2 decimals
+
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 
