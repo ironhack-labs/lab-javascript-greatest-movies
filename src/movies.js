a0e682dffb1786e61949fbf315f361b9f4290465
carlos-82 <cdelafuenteh@gmail.com>
2020-07-02 23:42:53 +0200

    here the movie practice

--- src/movies.js
+++ src/movies.js
@@ -1,15 +1,80 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
 
+function getAllDirectors(movie) {
+    const moviesDirectors = movie.map(function (movieobj) {
+        return movieobj.director
+    })
+    return moviesDirectors
+}
+
+
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
+
+function howManyMovies(movies) {
+    let spilberMovies = movies.filter(function (movie) {
+        return movie.director === "Steven Spielberg" && movie.genre.includes("Drama")
+    })
+    return spilberMovies.length
+}
+
 
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
 
+function ratesAverage(movieArr) {
+    if (movieArr.length === 0) {
+        return 0
+    }
+    let rateAverage = movieArr.reduce((accumul, current) => {
+        if (current.rate) {
+            return accumul + current.rate
+        }
+        return accumul
+    }, 0);
+
+    return Number((rateAverage / movieArr.length).toFixed(2))
+}
+
+
 // Iteration 4: Drama movies - Get the average of Drama Movies
+
+function dramaMoviesRate(array) {
+    let ratesDrama = array.filter(function (movie) {
+        return movie.genre.includes("Drama");
+    })
+    return ratesAverage(ratesDrama)
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 
+function orderByYear(movieArr) {
+    let orderYear = movieArr.sort(function (movie1, movie2) {
+        if (movie1.year === movie2.year) {
+            if (movie1.title > movie2.title) {
+                return -1;
+            }
+            if (movie1.title < movie2.title) {
+                return 1;
+            }
+            return 0
+        }
+        return movie1.year - movie2.year
+    })
+
+    return [...orderYear]
+}
+
+
+
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
+
+function orderAlphabetically(movieArr) {
+    let twentyFirst = movieArr.filter((element, index) => index < 21);
+    let orderTitle = twentyFirst.sort(function (movie1, movie2) {
+        return movie1.title - movie2.title
+    })
+    return [...orderTitle]
+}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 
--- tests/movies.spec.js
+++ tests/movies.spec.js
@@ -17,8 +17,7 @@ describe(' All directors - getAllDirectors', () => {
   });
 
   it('Should return a new array with the same length as the original one', () => {
-    const testArr = [
-      {
+    const testArr = [{
         title: 'Paths of Glory',
         year: 1957,
         director: 'Stanley Kubrick',
@@ -61,19 +60,16 @@ describe('Get how many movies - howManyMovies', () => {
 
   it('Return 0 if he did not direct none of the movies in the array', () => {
     expect(
-      howManyMovies([
-        {
-          director: 'James McTeigue',
-          genre: ['Action', 'Drama', 'Thriller']
-        }
-      ])
+      howManyMovies([{
+        director: 'James McTeigue',
+        genre: ['Action', 'Drama', 'Thriller']
+      }])
     ).toBe(0);
   });
 
   it('Only drama movies! Should return 1', () => {
     expect(
-      howManyMovies([
-        {
+      howManyMovies([{
           director: 'Steven Spielberg',
           genre: ['Action', 'Drama', 'Thriller']
         },
@@ -87,8 +83,7 @@ describe('Get how many movies - howManyMovies', () => {
 
   it('Only Steven Spielberg movies! Should return 2', () => {
     expect(
-      howManyMovies([
-        {
+      howManyMovies([{
           director: 'Steven Spielberg',
           genre: ['Action', 'Drama', 'Thriller']
         },
@@ -126,11 +121,21 @@ describe('Get the average rate - ratesAverage', () => {
   });
 
   it('You should return the average rate of 2 movies with rate 8 each!', () => {
-    expect(ratesAverage([{ rate: 8 }, { rate: 8 }])).toBe(8);
+    expect(ratesAverage([{
+      rate: 8
+    }, {
+      rate: 8
+    }])).toBe(8);
   });
 
   it('It should be rounded to 2 decimals digits', () => {
-    expect(ratesAverage([{ rate: 8 }, { rate: 9 }, { rate: 9 }])).toBe(8.67);
+    expect(ratesAverage([{
+      rate: 8
+    }, {
+      rate: 9
+    }, {
+      rate: 9
+    }])).toBe(8.67);
   });
 
   it('It should return 0 if there is no movie', () => {
@@ -138,7 +143,11 @@ describe('Get the average rate - ratesAverage', () => {
   });
 
   it('Return average even if one of the movies does not have rate!', () => {
-    expect(ratesAverage([{ rate: 6 }, { rate: '' }, {}])).toBe(2);
+    expect(ratesAverage([{
+      rate: 6
+    }, {
+      rate: ''
+    }, {}])).toBe(2);
   });
 });
 
@@ -156,45 +165,80 @@ describe('Average rate of Drama Movies - dramaMoviesRate', () => {
   });
 
   it('Return the rate of a single element array!', () => {
-    expect(dramaMoviesRate([{ genre: ['Drama'], rate: 8 }])).toBe(8);
+    expect(dramaMoviesRate([{
+      genre: ['Drama'],
+      rate: 8
+    }])).toBe(8);
   });
 
   it('It should return the average of the array!', () => {
     expect(
-      dramaMoviesRate([
-        { genre: ['Drama'], rate: 8 },
-        { genre: ['Drama'], rate: 9 },
-        { genre: ['Drama'], rate: 7 }
+      dramaMoviesRate([{
+          genre: ['Drama'],
+          rate: 8
+        },
+        {
+          genre: ['Drama'],
+          rate: 9
+        },
+        {
+          genre: ['Drama'],
+          rate: 7
+        }
       ])
     ).toBe(8);
   });
 
   it('It should return the average of the array, float!', () => {
     expect(
-      dramaMoviesRate([
-        { genre: ['Drama'], rate: 9 },
-        { genre: ['Drama'], rate: 9 },
-        { genre: ['Drama'], rate: 7 }
+      dramaMoviesRate([{
+          genre: ['Drama'],
+          rate: 9
+        },
+        {
+          genre: ['Drama'],
+          rate: 9
+        },
+        {
+          genre: ['Drama'],
+          rate: 7
+        }
       ])
     ).toBe(8.33);
   });
 
   it('Only Drama Movies! You should return the average of Drama movies only!', () => {
     expect(
-      dramaMoviesRate([
-        { genre: ['Drama'], rate: 8 },
-        { genre: ['Romance'], rate: 9 },
-        { genre: ['Drama'], rate: 7 }
+      dramaMoviesRate([{
+          genre: ['Drama'],
+          rate: 8
+        },
+        {
+          genre: ['Romance'],
+          rate: 9
+        },
+        {
+          genre: ['Drama'],
+          rate: 7
+        }
       ])
     ).toBe(7.5);
   });
 
   it('Should return 0 if there is no Drama movie!', () => {
     expect(
-      dramaMoviesRate([
-        { genre: ['Action'], rate: 8 },
-        { genre: ['Romance'], rate: 9 },
-        { genre: ['Sci-Fi'], rate: 7 }
+      dramaMoviesRate([{
+          genre: ['Action'],
+          rate: 8
+        },
+        {
+          genre: ['Romance'],
+          rate: 9
+        },
+        {
+          genre: ['Sci-Fi'],
+          rate: 7
+        }
       ])
     ).toBe(0);
   });
@@ -219,26 +263,58 @@ describe('Order the movies by year - orderByYear', () => {
   });
 
   it('Should return the element in a single element array', () => {
-    expect(orderByYear([{ year: 1982 }])).toEqual([{ year: 1982 }]);
+    expect(orderByYear([{
+      year: 1982
+    }])).toEqual([{
+      year: 1982
+    }]);
   });
 
   it('Return the new array in ascending order', () => {
     expect(
-      orderByYear([{ year: 2002 }, { year: 1982 }, { year: 1995 }])
-    ).toEqual([{ year: 1982 }, { year: 1995 }, { year: 2002 }]);
+      orderByYear([{
+        year: 2002
+      }, {
+        year: 1982
+      }, {
+        year: 1995
+      }])
+    ).toEqual([{
+      year: 1982
+    }, {
+      year: 1995
+    }, {
+      year: 2002
+    }]);
   });
 
   it('If two movies have the same year, order them alphabetically by their title', () => {
     expect(
-      orderByYear([
-        { title: 'abc', year: 2002 },
-        { title: 'bac', year: 1982 },
-        { title: 'aab', year: 1982 }
+      orderByYear([{
+          title: 'abc',
+          year: 2002
+        },
+        {
+          title: 'bac',
+          year: 1982
+        },
+        {
+          title: 'aab',
+          year: 1982
+        }
       ])
-    ).toEqual([
-      { title: 'aab', year: 1982 },
-      { title: 'bac', year: 1982 },
-      { title: 'abc', year: 2002 }
+    ).toEqual([{
+        title: 'aab',
+        year: 1982
+      },
+      {
+        title: 'bac',
+        year: 1982
+      },
+      {
+        title: 'abc',
+        year: 2002
+      }
     ]);
   });
 });
@@ -257,71 +333,163 @@ describe('Order alphabetically - orderAlphabetically', () => {
   });
 
   it('You should not mutate the original array', () => {
-    const arr = [{ title: 'xyz' }, { title: 'abc' }];
+    const arr = [{
+      title: 'xyz'
+    }, {
+      title: 'abc'
+    }];
     orderAlphabetically(arr);
     expect(arr[0].title).toEqual('xyz');
   });
 
   it('Only return the title of the movies! Each element should be a string', () => {
-    expect(typeof orderAlphabetically([{ title: 'aab' }])[0]).toBe('string');
+    expect(typeof orderAlphabetically([{
+      title: 'aab'
+    }])[0]).toBe('string');
   });
 
   it('If there are less than 20 elements, return all of them.', () => {
-    const moviesArr = [{ title: 'aab' }, { title: 'bab' }, { title: 'acb' }];
+    const moviesArr = [{
+      title: 'aab'
+    }, {
+      title: 'bab'
+    }, {
+      title: 'acb'
+    }];
 
     expect(orderAlphabetically(moviesArr).length).toEqual(3);
   });
 
   it('If there are more than 20 elements, return only 20 of them.', () => {
-    const moviesArr = [
-      { title: 'aab' },
-      { title: 'bab' },
-      { title: 'acb' },
-      { title: 'aab' },
-      { title: 'bab' },
-      { title: 'acb' },
-      { title: 'aab' },
-      { title: 'bab' },
-      { title: 'acb' },
-      { title: 'aab' },
-      { title: 'bab' },
-      { title: 'acb' },
-      { title: 'aab' },
-      { title: 'bab' },
-      { title: 'acb' },
-      { title: 'aab' },
-      { title: 'bab' },
-      { title: 'acb' },
-      { title: 'aab' },
-      { title: 'bab' },
-      { title: 'acb' },
-      { title: 'aab' },
-      { title: 'bab' },
-      { title: 'acb' },
-      { title: 'aab' },
-      { title: 'bab' },
-      { title: 'acb' },
-      { title: 'aab' },
-      { title: 'bab' },
-      { title: 'acb' },
-      { title: 'aab' },
-      { title: 'bab' },
-      { title: 'acb' },
-      { title: 'aab' },
-      { title: 'bab' },
-      { title: 'acb' }
+    const moviesArr = [{
+        title: 'aab'
+      },
+      {
+        title: 'bab'
+      },
+      {
+        title: 'acb'
+      },
+      {
+        title: 'aab'
+      },
+      {
+        title: 'bab'
+      },
+      {
+        title: 'acb'
+      },
+      {
+        title: 'aab'
+      },
+      {
+        title: 'bab'
+      },
+      {
+        title: 'acb'
+      },
+      {
+        title: 'aab'
+      },
+      {
+        title: 'bab'
+      },
+      {
+        title: 'acb'
+      },
+      {
+        title: 'aab'
+      },
+      {
+        title: 'bab'
+      },
+      {
+        title: 'acb'
+      },
+      {
+        title: 'aab'
+      },
+      {
+        title: 'bab'
+      },
+      {
+        title: 'acb'
+      },
+      {
+        title: 'aab'
+      },
+      {
+        title: 'bab'
+      },
+      {
+        title: 'acb'
+      },
+      {
+        title: 'aab'
+      },
+      {
+        title: 'bab'
+      },
+      {
+        title: 'acb'
+      },
+      {
+        title: 'aab'
+      },
+      {
+        title: 'bab'
+      },
+      {
+        title: 'acb'
+      },
+      {
+        title: 'aab'
+      },
+      {
+        title: 'bab'
+      },
+      {
+        title: 'acb'
+      },
+      {
+        title: 'aab'
+      },
+      {
+        title: 'bab'
+      },
+      {
+        title: 'acb'
+      },
+      {
+        title: 'aab'
+      },
+      {
+        title: 'bab'
+      },
+      {
+        title: 'acb'
+      }
     ];
 
     expect(orderAlphabetically(moviesArr).length).toEqual(20);
   });
 
   it('You should order them alphabetically.', () => {
-    const moviesArr = [
-      { title: 'aab' },
-      { title: 'aaa' },
-      { title: 'abc' },
-      { title: 'acb' },
-      { title: 'abb' }
+    const moviesArr = [{
+        title: 'aab'
+      },
+      {
+        title: 'aaa'
+      },
+      {
+        title: 'abc'
+      },
+      {
+        title: 'acb'
+      },
+      {
+        title: 'abb'
+      }
     ];
 
     expect(orderAlphabetically(moviesArr)).toEqual([
@@ -334,43 +502,114 @@ describe('Order alphabetically - orderAlphabetically', () => {
   });
 
   it('You should return the top20 after ordering them alphabetically.', () => {
-    const moviesArr = [
-      { title: 'aab' },
-      { title: 'bab' },
-      { title: 'acb' },
-      { title: 'aaa' },
-      { title: 'bbb' },
-      { title: 'anc' },
-      { title: 'kns' },
-      { title: 'zds' },
-      { title: 'pow' },
-      { title: 'gda' },
-      { title: 'res' },
-      { title: 'ter' },
-      { title: 'bca' },
-      { title: 'ccc' },
-      { title: 'bbt' },
-      { title: 'qas' },
-      { title: 'kmn' },
-      { title: 'frt' },
-      { title: 'afb' },
-      { title: 'agb' },
-      { title: 'apo' },
-      { title: 'poa' },
-      { title: 'cdf' },
-      { title: 'sea' },
-      { title: 'lom' },
-      { title: 'acs' },
-      { title: 'qas' },
-      { title: 'mns' },
-      { title: 'bvc' },
-      { title: 'gha' },
-      { title: 'lkj' },
-      { title: 'era' },
-      { title: 'ert' },
-      { title: 'tex' },
-      { title: 'zas' },
-      { title: 'pol' }
+    const moviesArr = [{
+        title: 'aab'
+      },
+      {
+        title: 'bab'
+      },
+      {
+        title: 'acb'
+      },
+      {
+        title: 'aaa'
+      },
+      {
+        title: 'bbb'
+      },
+      {
+        title: 'anc'
+      },
+      {
+        title: 'kns'
+      },
+      {
+        title: 'zds'
+      },
+      {
+        title: 'pow'
+      },
+      {
+        title: 'gda'
+      },
+      {
+        title: 'res'
+      },
+      {
+        title: 'ter'
+      },
+      {
+        title: 'bca'
+      },
+      {
+        title: 'ccc'
+      },
+      {
+        title: 'bbt'
+      },
+      {
+        title: 'qas'
+      },
+      {
+        title: 'kmn'
+      },
+      {
+        title: 'frt'
+      },
+      {
+        title: 'afb'
+      },
+      {
+        title: 'agb'
+      },
+      {
+        title: 'apo'
+      },
+      {
+        title: 'poa'
+      },
+      {
+        title: 'cdf'
+      },
+      {
+        title: 'sea'
+      },
+      {
+        title: 'lom'
+      },
+      {
+        title: 'acs'
+      },
+      {
+        title: 'qas'
+      },
+      {
+        title: 'mns'
+      },
+      {
+        title: 'bvc'
+      },
+      {
+        title: 'gha'
+      },
+      {
+        title: 'lkj'
+      },
+      {
+        title: 'era'
+      },
+      {
+        title: 'ert'
+      },
+      {
+        title: 'tex'
+      },
+      {
+        title: 'zas'
+      },
+      {
+        title: 'pol'
+      }
     ];
 
     expect(orderAlphabetically(moviesArr)).toEqual([
@@ -423,25 +662,33 @@ describe('Turn duration of the movies - turnHoursToMinutes', () => {
   });
 
   it('The duration of the movie should return the correct number to a short movie - 31 minutes', () => {
-    const movieTry = [{ duration: '0h 31min' }];
+    const movieTry = [{
+      duration: '0h 31min'
+    }];
 
     expect(turnHoursToMinutes(movieTry)[0].duration).toBe(31);
   });
 
   it('The duration of the movie should return the correct number for really long movie - 341 minutes', () => {
-    const movieTry = [{ duration: '5h 41min' }];
+    const movieTry = [{
+      duration: '5h 41min'
+    }];
 
     expect(turnHoursToMinutes(movieTry)[0].duration).toBe(341);
   });
 
   it('It should return the correct number when the duration is only in hours', () => {
-    const movieTry = [{ duration: '2h' }];
+    const movieTry = [{
+      duration: '2h'
+    }];
 
     expect(turnHoursToMinutes(movieTry)[0].duration).toBe(120);
   });
 
   it('It should return the correct number when the duration is only in minutes', () => {
-    const movieTry = [{ duration: '54min' }];
+    const movieTry = [{
+      duration: '54min'
+    }];
 
     expect(turnHoursToMinutes(movieTry)[0].duration).toBe(54);
   });
@@ -461,7 +708,10 @@ describe('Best year average - bestYearAvg', () => {
   });
 
   it('Should return the correct answer to a single element array', () => {
-    expect(bestYearAvg([{ year: 2007, rate: 8 }])).toEqual(
+    expect(bestYearAvg([{
+      year: 2007,
+      rate: 8
+    }])).toEqual(
       'The best year was 2007 with an average rate of 8'
     );
   });
@@ -473,11 +723,22 @@ describe('Best year average - bestYearAvg', () => {
   });
 
   it('Should return the oldest year when there is a tie', () => {
-    const newMoviesArr = [
-      { year: 2000, rate: 9 },
-      { year: 2000, rate: 8 },
-      { year: 1978, rate: 10 },
-      { year: 1978, rate: 7 }
+    const newMoviesArr = [{
+        year: 2000,
+        rate: 9
+      },
+      {
+        year: 2000,
+        rate: 8
+      },
+      {
+        year: 1978,
+        rate: 10
+      },
+      {
+        year: 1978,
+        rate: 7
+      }
     ];
 
     expect(bestYearAvg(newMoviesArr)).toEqual(
