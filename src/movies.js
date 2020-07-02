3f0de03d1cd9c37ee90e078e611d648b3be42ce4
Noiret <sergio.ko@icloud.com>
2020-07-02 20:53:37 +0200

    Iteration 7 done. Missing last Iteration 8

--- src/movies.js
+++ src/movies.js
@@ -1,6 +1,7 @@
 // Iteration 1: All directors? - Get the array of all directors.
 function getAllDirectors(array) {
-    let allDirectors = array.map(film => film.director)
+    let allDirectors = array
+        .map(film => film.director)
     return allDirectors;
 };
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
@@ -49,17 +50,46 @@ function orderByYear(array) {
 };
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically(array) {
-    let firstTwenty = array.filter((film, i) => {
+    let newArray = JSON.parse(JSON.stringify(array))
+    let firstTwenty = newArray
+        .sort((a, b) => a.title.localeCompare(b.title))
+        .filter((filmTitle, i) => {
             if (i < 20) {
-                return film.title
+                return filmTitle;
             }
         })
-        .map(film => film.title)
-        .sort((a, b) => {
-            return a.localeCompare(b);
-        })
+        .map((item) => item.title)
     return firstTwenty;
-};
+}
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
+function turnHoursToMinutes(array) {
+    let newNovies = JSON.parse(JSON.stringify(array));
+    newNovies.map((film) => {
+        film.duration = stringToMinutes(film.duration);
+    });
+    return newNovies;
+}
+
+function stringToMinutes(timeString) {
+    let minutes = 0;
+    `${timeString}`.split(" ").forEach((item) => {
+        if (item.indexOf("h") != -1) {
+            minutes += parseInt(item) * 60;
+        } else {
+            minutes += parseInt(item);
+        }
+    });
+    return minutes;
+}
+
 
 // BONUS - Iteration 8: Best yearly rate average - Best yearly rate average
+function bestYearAvg(array) {
+    if (array.length === 0) {
+        return null;
+    }
+    let years = array
+        .map(film => film.year)
+        .sort((a, b) => a - b)
+    return years;
+};
