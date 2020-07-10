f973b3c815fc55358b2584d79931d28cfc791f74
Alejandro Hernández <alejandro_hj.11@outlook.com>
2020-07-10 10:33:04 +0100

    Done

--- src/movies.js
+++ src/movies.js
@@ -26,11 +26,8 @@ function ratesAverage(movies){
     if(movies==0){return 0}
     else{
         let avrg;
-        let count = 0;
         let answer = movies.reduce((acc, elem)=>{
-            if(elem!=0){
-                return acc + elem.rate;
-            }
+            return acc + (elem.rate || 0);
         },0)
         avrg = answer/movies.length;
         return parseFloat(avrg.toFixed(2));
@@ -52,7 +49,8 @@ function dramaMoviesRate(movies){
 }
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear(movies){
-    let byYear = movies.sort((a,b)=>{
+    let cloneMovie = JSON.parse(JSON.stringify(movies));
+    let byYear = cloneMovie.sort((a,b)=>{
         if(a.year>b.year){return 1;}
         else if(a.year<b.year){return -1;}
         else{
@@ -65,8 +63,9 @@ function orderByYear(movies){
 }
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically(movies){
+    let cloneMovie = JSON.parse(JSON.stringify(movies));
     let the20first=[];
-    let titlesInAlphabeticalOrder = movies.sort((a,b)=>{
+    let titlesInAlphabeticalOrder = cloneMovie.sort((a,b)=>{
         if(a.title>b.title){return 1;}
         else if(a.title<b.title){return -1;}
         else{return 0;}
@@ -74,11 +73,60 @@ function orderAlphabetically(movies){
     let arrOftitlesInAlphabeticalOrder = titlesInAlphabeticalOrder.map((elem,index)=>{
       return elem.title;
     });
-    for(let i=0; i<20; i++){
-        the20first.push(arrOftitlesInAlphabeticalOrder[i]);
-    }
-  return the20first;
+    the20first = arrOftitlesInAlphabeticalOrder.splice(0,20);
+    if(the20first<20){return arrOftitlesInAlphabeticalOrder;}
+    else{  return the20first;}
+
 }
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
+function turnHoursToMinutes(movies){
+    let cloneMovie = JSON.parse(JSON.stringify(movies));
+    let arrOfTheTime;
+    let hoursDuration;
+    let minutesDuration;
+    let filmDuration = cloneMovie.map((movie)=>{
+        if(movie.duration.includes(' ')){
+            arrOfTheTime = movie.duration.split(' '); //Now we have ['2h', '22min']
+            hoursDuration = arrOfTheTime[0].split('h')[0]; //
+            minutesDuration = arrOfTheTime[1].split('min')[0];
+            movie.duration = Number(hoursDuration) * 60 + Number(minutesDuration);
+        }
+        else if(movie.duration.includes('h')){
+            hoursDuration = movie.duration.split('h')[0];
+             movie.duration = Number(hoursDuration) * 60 
+        }
+        else{
+            minutesDuration = movie.duration.split('min')[0];
+            movie.duration = Number(minutesDuration);
+        }
+        return movie
+    });
+    return filmDuration;
+}
 
 // BONUS - Iteration 8: Best yearly rate average - Best yearly rate average
+function bestYearAvg(movies){
+    let moviesByYear = {}
+    if(movies ==0){return null;}
+    else{
+    movies.forEach((movie)=>{
+        if(movie.year in moviesByYear){
+            moviesByYear[movie.year].push(movie);
+        }
+        else{
+            moviesByYear[movie.year]= [];
+            moviesByYear[movie.year].push(movie);
+        }
+    });
+    let maxAvrg = 0;
+    let maxYear = '';
+    for(year in moviesByYear){
+        let avrgrating = ratesAverage(moviesByYear[year]);
+        if(avrgrating > maxAvrg){
+            maxAvrg = avrgrating;
+            maxYear = year;
+        }
+    }
+    return `The best year was ${maxYear} with an average rate of ${maxAvrg}`
+    }
+}
