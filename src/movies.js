248518d614646746bffb7883647a213853058ba4
gonzalo-development <gperalcorrales@gmail.com>
2020-07-10 12:55:00 +0200

    git commit

--- src/movies.js
+++ src/movies.js
@@ -26,34 +26,88 @@ function howManyMovies(movies){
 return spiel.length;
 };
 
+function howManyMovies(movies){
+    let Drama = movies.filter(function(ele){
+      return ele.genre.includes("drama")
+    })
+    return Drama;
+  }
+
+
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
 
 function ratesAverage(movies){
-    let averageRate = 0;
-    movies.reduce(function(acc,value){
-        for(var i=0; i<movie.rate.length; i++){
-          averagerRate =  acc + value(movie.rate[i])  
-        }
-        return averageRate 
+  
+    let sum = movies.reduce(function(acc,value){
+      return acc + value.rate
     },0)
-    return averageRate.length;
-}
+    return sum/sum.length;
+  }
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesRate(movies){
-    let averageDramaRate = 0;
-    movies.map(function(ele){
-        if(ele.genre == "drama"){
-
-        }
-
-}
+    let s = movies.filter(function(elw){
+      return elw.genre.includes("drama")
+    })
+    return rateAverage(s);
+    }
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
+function orderByYear(movies){
+
+   
+    let clone = JSON.parse(JSON.stringify(movies))
+    
+    let order =clone.sort(function(a,b){
+      if( a.year > b.year){
+        return 1;
+      }else if( a.year < b.year){
+        return -1;
+      }else{
+        return 0;
+      }
+    })
+    return order;
+  }
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
+function orderAlphabetically(movies){
+    let cloneTitle = JSON.parse(JSON.stringify(movies));
+   
+   let orderTitle = cloneTitle.sort(function(){
+     
+     if( a.title > b.title){
+       return 1;
+     }else if( a.title < b.title){
+       return -1;
+     }else{
+       return 0;
+     }
+     })
+   return orderTitle.splice(0,20)
+   }
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
+function turnHoursToMinutes(movies){
+  
+    let updateTime=  movies.map(function(movie){
+      
+      if(movies.duration.includes(" ")){// make sure thes separation
+        let time = movie.duration.split(" "); // array ["1h","20min"]
+        let hours= time[0].split('h');//string ["1", ""]
+        let min= time[1].split("min");//string ["20", ""]
+        let sum = Number(hours[0])*60 + Number(min[0]);
+      }else if(movie.duration.includes('h')){ 
+        let hours= movie.duration.split('h');// string ["1", ""]
+        movie.duration = Number(hours[0])*60;
+      }else if(movie.duration.includes('min') ){
+         let min= movie.duration.split('min');
+        movie.duration = Number(min[0]);
+      }
+      return movie,
+      })
+    return updateTime;
+  }
 
 // BONUS - Iteration 8: Best yearly rate average - Best yearly rate average
 
