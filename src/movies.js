90fa46f7b52ce4b8a25b34bcc66f021a5bc202a5
“Marta <martalouri@gmail.com>
2020-07-12 21:25:23 +0200

    updated lab 12/07

--- src/movies.js
+++ src/movies.js
@@ -23,46 +23,42 @@ function howManyMovies(movies) {
 
 function ratesAverage(movies) {
 
-    if (movies.length === 0) {
-      return 0
-    }
-    else { 
-        let total=movies.map(function(a,b){
-            return n*100;
-        }).reduce(function(a, b){
-            return a+(b || 0);
-        });
-        
-        Math.round((total/movies.length))/100;
-    }
-   
+  if (movies.length === 0) {
+    return 0;
+  }
+  let sumRates = movies.reduce(function(accumulator, film){
+    if (!film.rate){
+      return accumulator + 0 
+    } 
+      return accumulator + film.rate  
+  }, 0) / movies.length;
+
+  return Number((sumRates).toFixed(2));
 }
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 
 function dramaMoviesRate(movies) {
-    let dramaMovies = movies.filter(movie => {
-      if (movies.length === 0) {
-            return 0
-      }
-          
-      if (movie.genre.indexOf('Drama') > -1) {
-        return true;
-      }
-       else if (movie.genre.includes('Drama')) {
-        return true;
-      }
-    });
-       if (dramaMovies.length) return 0;
-        return dramaMoviesRate;
-  }
+  let moviesd = movies.filter((movies) => movies.genre.includes("Drama"));
 
+  if (moviesd.length === 0) {
+    return 0;
+  }
+  let sumRates = moviesd.reduce(function(accumulator, film){
+    if (!film.rate){
+      return accumulator + 0 
+    } 
+      return accumulator + film.rate  
+  }, 0) / moviesd.length;
+
+  return Number((sumRates).toFixed(2)); //we need 2 digits for get the float condition, so we need put 2 into the to.Fixed 
+}
 
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 
 function orderByYear(movies) {
-    let gettheyears = [...movies];
+    let gettheyears = [...movies] //it is the same that using JSON
     return gettheyears.sort((a, b) => {
       if (a.year > b.year) {
         return 1;
@@ -89,19 +85,26 @@ function orderByYear(movies) {
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 
-function orderAlphabetically (movies) {
+function orderAlphabetically(movies) {
+  let orderIt = [...movies] //it is the same that using JSON
+  
+   //orderIt.sort((a, b) => movies.includes("title"));{
+  orderIt.sort((a, b) =>  {
+    if (a.title > b.title) {
+      return 1;
+    } 
+    else if (a.title < b.title) {
+      return -1;
+    } 
+    else {
+      return 0
+    }
+  })
+    let moviestitles = orderIt.map(function(themovie){
+      return themovie.title
+  })
 
-    movies.sort(function(a, b){
-        if (a.tittle > b.tittle){
-            return 1;
-        }
-        else if (a.title < b.tittle){
-            return -1
-        }
-        else{
-            return 0;
-        }
-    })
+  return moviestitles.splice(0,20)
 
 }
 
