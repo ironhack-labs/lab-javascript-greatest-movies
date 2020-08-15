cb8ae3735414a534b60040c262cba098198cbda3
Diana <d.thueroff@gmail.com>
2020-08-15 13:33:43 +0200

    iteration 3 completed

--- src/movies.js
+++ src/movies.js
@@ -1,17 +1,8 @@
 // Iteration 1: All directors? - Get the array of all directors.
 
-// function myMap(arr, callback) {
-//     let resultArr = [];
-//     for(let i=0; i<arr.length; i++) {
-//         resultArr.push(callback(arr[i]));
-//     }
-//     return resultArr;
-// }
-
 function getAllDirectors(movies) {
     return movies.map(movie => movie.director);
 }
-
 
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
 
@@ -20,16 +11,33 @@ function getAllDirectors(movies) {
 // Filter property value of director and property of genre!
 
 function howManyMovies(movies) {
-    if (movies.length === 0) {
-        return 0;
-    }
     const spielbergDramas = movies.filter(movie => {
         return (movie.director === 'Steven Spielberg' && movie.genre.includes('Drama'));
     });
     return spielbergDramas.length;
 }
 
+
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
+// Step 1: Define a function
+// Step 2: Use the reduce method to iterate over the array and create an average rate
+// Step 3: Return the average rate with only two decimals using the toFixed method
+
+function ratesAverage(movies) {
+    let sumUnrated = 0;
+    if (movies.length === 0) {
+        return 0;
+    }
+    const sumRate = movies.reduce(function (allRates, movie) {
+        if (!movie.rate) {
+            //sumUnrated++; --> does not work with test, but I think unrated movies shouldn't count as 0
+            return allRates;
+        }
+        return allRates + movie.rate;
+    }, 0);
+
+    return Math.round((sumRate / (movies.length - sumUnrated)) * 100) / 100;
+}
 
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
