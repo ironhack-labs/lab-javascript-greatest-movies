c94bda5b7e6d6f5375707225ce4cfaaab00e8efa
Olesya Steinmetz <newsabome@icloud.com>
2020-08-17 11:29:26 +0200

    Refactored

--- src/movies.js
+++ src/movies.js
@@ -5,31 +5,25 @@ function getAllDirectors(array){
     })
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
     const unique = new Set(newArr);
-    const filArr = [...unique];
-    return filArr
+    return [...unique]
   }
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 function howManyMovies(array) {
-    if (array === "") return 0
-    const filtered = array.filter((movie) => {
+    return array.filter((movie) => {
       return movie.director === "Steven Spielberg" && movie.genre.includes('Drama')
-      })
-    return filtered.length
+      }).length
   }
+
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
 function ratesAverage(array) {
-    const rates = []
-    array.forEach((el) => {
-      if (el.rate){
-        rates.push(el.rate)  
-      }
-    })
-    const avg = rates.reduce((acc, val)=>{
-      return acc + val / array.length
-    }, 0)
+  if (array === "") return 0
+    const avg = array.reduce((acc, val)=>{
+      return acc + (val.rate  || 0) / array.length
+    }, 0);
     return +avg.toFixed(2)
   }
+
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesRate(array){
     if (array === "") return 0
@@ -40,8 +34,8 @@ function dramaMoviesRate(array){
   }
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear(array){
-    const sorted = array.sort((a, b) => (a.year > b.year) ? 1 : -1)
-    return sorted.concat()
+    const sorted = array.concat().sort((a, b) => (a.year > b.year) ? 1 : -1)
+    return sorted
 }
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically(array){
@@ -53,7 +47,7 @@ function orderAlphabetically(array){
 function turnHoursToMinutes(array) {
     const minutes = array.map((el) => {
     let mins = (+el.duration[0] * 60) + (+el.duration.slice(3, -3));
-      console.log(el.duration.slice(3, -3))
+    parseInt(el.duration.slice(3, -3))
     if (!el.duration.includes("h")){
       return {
      title: el.title,
