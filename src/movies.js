a6d500d99f402511aaaafe5830ec97ae1ef2d140
Rubem Diogo <rubemdiogo@gmail.com>
2020-08-20 22:37:06 -0400

    done

--- src/movies.js
+++ src/movies.js
@@ -1,16 +1,17 @@
 // Iteration 1: All directors? - Get the array of all directors.
-// _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
+
+const getAllDirectors = function (arrayMuvie) {
+    let directors = []
+    let x = arrayMuvie.map((y) => y.directors)
+    return x
+}
+
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 
-// Iteration 3: All rates average - Get the average of all rates with 2 decimals
-
-// Iteration 4: Drama movies - Get the average of Drama Movies
-
-// Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-
-// Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-
-// BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-
-// BONUS - Iteration 8: Best yearly rate average - Best yearly rate average
+const howManyMovies = function (arrayMuvie) {
+    let SpielbergFilm = []
+    SpielbergFilm = arrayMuvie.filter(y => y.genre.includes("Drama") && y.director === "Steven Spielberg");
+    return SpielbergFilm.length;
+}
+howManyMovies(movies);
