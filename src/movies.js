01880a82018ccb0d0c4918e9bbd579f2928be061
tinaCanCode <mc.scherzberg@googlemail.com>
2020-09-08 19:32:30 +0200

    complete iteration 6 and bonus iterations

--- src/movies.js
+++ src/movies.js
@@ -13,15 +13,15 @@ function getAllDirectors(moviesArr) {
 function howManyMovies(moviesArr) {
 
     let filteredArr = moviesArr.filter(movieObj => {
-        
+
         if (movieObj.director === "Steven Spielberg" && movieObj.genre.includes("Drama")) {
-          return true
+            return true
         }
 
     })
     return filteredArr.length
-    }
-                                  
+}
+
 
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
 
@@ -31,11 +31,11 @@ function ratesAverage(moviesArr) {
         return 0
     }
 
-    let ratesSum = moviesArr.reduce(function(accumulator, currentValue){
+    let ratesSum = moviesArr.reduce(function (accumulator, currentValue) {
         if (typeof currentValue.rate !== "number") {
             return accumulator
         } else {
-        return accumulator + currentValue.rate
+            return accumulator + currentValue.rate
         }
     }, 0);
 
@@ -52,7 +52,7 @@ function dramaMoviesRate(moviesArr) {
     let dramaMoviesCount = 0
     let dramaAverage = 0
 
-    let dramaRatesSum = moviesArr.reduce(function(accumulator, currentValue){
+    let dramaRatesSum = moviesArr.reduce(function (accumulator, currentValue) {
         if (currentValue.genre.indexOf("Drama") !== -1) {
             dramaMoviesCount++
             return accumulator + currentValue.rate
@@ -67,7 +67,7 @@ function dramaMoviesRate(moviesArr) {
     } else {
         dramaAverage = dramaRatesSum / dramaMoviesCount
     }
-    
+
     return Math.round(dramaAverage * 100) / 100
 }
 
@@ -80,15 +80,15 @@ let dramaMoviesRate2 = (movies) => ratesAverage(movies.filter(movie => movie.gen
 function orderByYear(moviesArr) {
 
     sortArr = moviesArr.slice()
-    
+
     sortArr.sort(
-      function compare(a,b) {
-  if (a.year > b.year) {
-    return 1
-  } else {
-    return -1
-  }
-}
+        function compare(a, b) {
+            if (a.year > b.year) {
+                return 1
+            } else {
+                return -1
+            }
+        }
     )
     return sortArr
 }
@@ -96,6 +96,98 @@ function orderByYear(moviesArr) {
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 
+function orderAlphabetically(moviesArr) {
+    let titleArr = []
+
+    moviesArr.map(movie => titleArr.push(movie.title))
+
+    titleArr.sort()
+
+    return titleArr.slice(0, 20)
+}
+
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 
+
+function turnHoursToMinutes(moviesArr) {
+
+
+    return moviesArr.map(function (movie) {
+
+        // if (typeof movie.duration === "number") {
+        //     return;
+        // }
+        movie = Object.assign({}, movie)
+
+        let duration = movie.duration;
+        let hours = 0;
+        let minutes = 0;
+        //console.log(duration)
+        //console.log(duration.indexOf("h")===-1)
+        if (duration.indexOf("h") === -1) {
+            hours = 0
+        } else {
+            hours = duration.slice((duration.indexOf("h") - 1), duration.indexOf("h"))
+        }
+
+        if (duration.indexOf("min") === -1) {
+            minutes = 0
+        } else {
+            minutes = duration.slice((duration.indexOf("min") - 2), duration.indexOf("min"))
+        }
+    
+        let durationInMin = parseInt(hours) * 60 + parseInt(minutes)
+       
+        movie.duration = durationInMin
+
+        return movie;
+    });
+}
+
 // BONUS - Iteration 8: Best yearly rate average - Best yearly rate average
+
+function bestYearAvg(moviesArr) {
+
+    let years = {};
+
+    if (moviesArr.length === 0) {
+        return null;
+    } 
+
+    moviesArr.forEach( function (movie) {
+       years[movie.year] = [];
+    });
+
+    moviesArr.forEach( function (movie) {
+        years[movie.year].push(movie.rate);
+    });
+
+    let best = {
+        year: 0,
+        rating: 0
+    };
+
+    for (const year in years) {
+        const average = years[year].reduce(function (previousValue, currentValue) {
+            return previousValue + currentValue;
+        }, 0) / years[year].length;
+        if (average > best.rating ) {
+            best.rating = average;
+            best.year = year;
+        }
+     }
+
+    return `The best year was ${best.year} with an average rate of ${best.rating}`;
+
+  
+}
+
+
+
+
+    
+
+
+
+
+
