e5d94ca29bb87f7f2a674eaf2b891afeda25df4d
Sant <santiagoandres@outlook.de>
2020-09-17 20:07:13 +0200

    001: Iterations 1 through 6.

--- src/movies.js
+++ src/movies.js
@@ -1,16 +1,82 @@
-// Iteration 1: All directors? - Get the array of all directors.
-// _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
+/*  Iteration 1: All directors? - Get the array of all directors.  */
+/*  Bonus: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
+    How could you "clean" a bit this array and make it unified (without duplicates)? */
+const getAllDirectors = (movies) => {
+  const directors = movies.map((movie) => {
+    return movie.director;
+  });
+  return directors;
+};
 
-// Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
+/*  Iteration 2: How many drama movies did Steven Spielberg direct?  */
+const howManyMovies = (movies) => {
+  const totalMovies = movies.filter((movie) => {
+    return (
+      movie.director === "Steven Spielberg" && movie.genre.includes("Drama")
+    );
+  });
+  return totalMovies.length;
+};
 
-// Iteration 3: All rates average - Get the average of all rates with 2 decimals
+/*  Iteration 3: All rates average - Get the average of all rates with 2 decimals.  */
+const ratesAverage = (movies) => {
+  const avgRating = movies.reduce((acc, movie, idx, src) => {
+    return acc + movie.rate / src.length;
+  }, 0);
+  return Math.round(100 * avgRating) / 100;
+};
 
-// Iteration 4: Drama movies - Get the average of Drama Movies
+/*  Iteration 4: Drama movies - Get the average of Drama Movies.  */
+const dramaMoviesRate = (movies) => {
+  const avgDramaRating = movies
+    .filter((movie) => {
+      return movie.genre.includes("Drama");
+    })
+    .reduce((acc, movie, idx, src) => {
+      return acc + movie.rate / src.length;
+    }, 0);
+  return Math.round(100 * avgDramaRating) / 100;
+};
 
-// Iteration 5: Ordering by year - Order by year, ascending (in growing order)
+/*  Iteration 5: Order by year, ascending  */
+const orderByYear = (movies) => {
+  const orderedMovies = JSON.parse(JSON.stringify(movies));
+  orderedMovies.sort((a, b) => {
+    if (a.year > b.year) {
+      return 1;
+    } else if (a.year < b.year) {
+      return -1;
+    } else {
+      if (a.title > b.title) {
+        return 1;
+      } else if (a.title < b.title) {
+        return -1;
+      } else {
+        return 0;
+      }
+    }
+  });
+  return orderedMovies;
+};
 
-// Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
+/*  Iteration 6: Alphabetic Order - Order by title and print the first 20 titles.  */
+const orderAlphabetically = (movies) => {
+  const orderedMovies = JSON.parse(JSON.stringify(movies));
+  const orderedTitles = orderedMovies
+    .map((movies) => {
+      return movies.title;
+    })
+    .sort();
 
-// BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
+  if (orderedTitles.length > 21) {
+    return orderedTitles.splice(0, 20)
+  } else {
+    return orderedTitles;
+  }
+};
 
-// BONUS - Iteration 8: Best yearly rate average - Best yearly rate average
+/*  BONUS - Iteration 7: Turn duration of the movies from hours to minutes.  */
+
+
+
+/*  BONUS - Iteration 8: Best yearly rate average.  */
