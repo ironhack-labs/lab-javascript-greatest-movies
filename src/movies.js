1575aa4162318234a647be12096c7517478494a2
Gonzalo Loza <cg.lozarojas@gmail.com>
2020-09-26 17:09:45 -0500

    Iteration 5 done

--- src/movies.js
+++ src/movies.js
@@ -1,13 +1,28 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
 
+let getAllDirectors = arrayMovies => arrayMovies.map (element => element.director).filter((item,index,array) => array.indexOf(item) === index);
+
+
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
+
+let howManyMovies = arrayMovies => arrayMovies.filter(item => item.genre.filter(element => element === "Drama").length === 1 && item.director === "Steven Spielberg").length;
 
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
 
+let ratesAverage = arrayMovies => (Math.round(arrayMovies.reduce((acc,element) => acc + (element.rate || 0),0)/arrayMovies.length*100)/100 || 0);
 // Iteration 4: Drama movies - Get the average of Drama Movies
 
+let dramaMoviesRate = arrayMovies => ratesAverage(arrayMovies.filter(item => item.genre.includes("Drama")));
+
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
+
+let orderByYear = arrayMovies => arrayMovies.map(e => e).sort((a,b) => {
+    if (a.year === b.year){
+        return a.title > b.title ? 1 : -1; 
+    };
+    return a.year - b.year;
+});
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 
