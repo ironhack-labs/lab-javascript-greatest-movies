1c1ff2388e9fd175bdddc23cad596c9118c1361c
Mathilde Congiu <mathilde.congiu@hotmail.com>
2020-10-02 09:04:29 +0200

    Bonus 1 Done

--- src/movies.js
+++ src/movies.js
@@ -12,8 +12,17 @@ console.log(movies.length)
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
 
 
+let cleanDirectorsFunction = arr => {
+    let cleanDirectorsArray = []
+    arr.forEach(element => {
+        if ( cleanDirectorsArray.indexOf(element) === -1) {
+            cleanDirectorsArray.push(element)
+        }
+    })
+    return cleanDirectorsArray
+}
 
-
+console.log(cleanDirectorsFunction(getAllDirectors(movies)))
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 
 let howManyMovies = function(arr) {
