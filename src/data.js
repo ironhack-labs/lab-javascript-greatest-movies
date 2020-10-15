6fad7140527c51e37860b889d86bfc98922b6d6c
Abbie <abbie@summers.gg>
2020-10-16 01:05:01 +0200

    Started Iterations #4 , #5

--- src/data.js
+++ src/data.js
@@ -2975,7 +2975,7 @@ let movies = [
   console.log(getAllDirectors(movies));
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
 
-//// Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
+// Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 function howManyMovies(movies) {
   const stevenSpDrama = movies.filter((movies) => {
       return movies.director === "Steven Spielberg" && movies.genre.includes("Drama")
@@ -2998,12 +2998,34 @@ function ratesAverage(movies) {
 }
 console.log(ratesAverage(movies));
 
-/*// Iteration 4: Drama movies - Get the average of Drama Movies
+// Iteration 4: Drama movies - Get the average of Drama Movies
+function dramaMoviesRate(movies) {
+  if (movies.length === 0) {
+      return 0
+  }
+  const filterMovie = movies.filter(movies => movies.rate && movies.genre.includes("Drama") != undefined)
+  const rate = filterMovie.reduce((accum, value) => {
+      return accum + value.rate
+  }, 0)
+  const average = rate / movies.length
+  return Math.round(average * 100) / 100
+}
+console.log(dramaMoviesRate(movies));
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
+function orderByYear(movies) {
+  if (movies.length === 0) {
+    return 0
+  }
+  const ascOrder = movies.filter(movies => movies.year != undefined)
+  return movies.year( a - b)};
+console.log(orderByYear(movies));
+
+
+
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 
-// BONUS - Iteration 8: Best yearly rate average - Best yearly rate average. */
+// BONUS - Iteration 8: Best yearly rate average - Best yearly rate average
--- src/movies.js
+++ src/movies.js
@@ -1,15 +1,15 @@
-  // Iteration 1: All directors? - Get the array of all directors.
-  function getAllDirectors(movies) {
-    let arr = movies.map((movies) => movies.director);
-    return arr;
-  }
-  console.log(getAllDirectors(movies));
+ // Iteration 1: All directors? - Get the array of all directors.
+ function getAllDirectors(movies) {
+  let arr = movies.map((movies) => movies.director);
+  return arr;
+}
+console.log(getAllDirectors(movies));
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
 
-// Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
+//// Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 function howManyMovies(movies) {
   const stevenSpDrama = movies.filter((movies) => {
-      return movies.director === "Steven Spielberg" 
+      return movies.director === "Steven Spielberg" && movies.genre.includes("Drama")
   })
   return stevenSpDrama.length;
 }
@@ -17,21 +17,40 @@ console.log(howManyMovies(movies));
 
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
 function ratesAverage(movies) {
+if (movies.length === 0) {
+    return 0
+}
+const filterMovie = movies.filter(movie => movie.rate != undefined)
+const rate = filterMovie.reduce((accum, value) => {
+    return accum + value.rate
+}, 0)
+const average = rate / movies.length
+return Math.round(average * 100) / 100
+}
+console.log(ratesAverage(movies));
+
+// Iteration 4: Drama movies - Get the average of Drama Movies
+function dramaMoviesRate(movies) {
   if (movies.length === 0) {
       return 0
   }
-  const filterMovie = movies.filter(movie => movie.rate != undefined)
+  const filterMovie = movies.filter(movies => movies.rate && movies.genre.includes("Drama") != undefined)
   const rate = filterMovie.reduce((accum, value) => {
       return accum + value.rate
   }, 0)
   const average = rate / movies.length
   return Math.round(average * 100) / 100
 }
-console.log(ratesAverage(movies));
-
-// Iteration 4: Drama movies - Get the average of Drama Movies
+console.log(dramaMoviesRate(movies));
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
+function orderByYear(movies) {
+  if (movies.length === 0) {
+    return 0
+  }
+  const ascOrder = movies.filter(movies => movies.year != undefined)
+  return movies.year( a - b)};
+console.log(orderByYear(movies));
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 
