49a364e85ca711593890584e38db1bd790e64daf
Ceyda Ulubas <ceyda.ulubas@gmail.com>
2020-10-19 09:42:59 +0300

    edited Iterations

--- src/movies.js
+++ src/movies.js
@@ -1,11 +1,3 @@
-// Iteration 1: All directors? - Get the array of all directors.
-
-const getAllDirectors = (moviesList) => {
-  const movieDirectorsList = moviesList.map((x) => x.director);
-  
-  return movieDirectorsList;
-}
-
 //just for debugging purposes
 let movies = [
   {
@@ -40,9 +32,40 @@ let movies = [
       "Drama"
     ],
     "rate": 9
-  }]
+  },
+  {
+    "title": "Schindler\"s List",
+    "year": 1993,
+    "director": "Steven Spielberg",
+    "duration": "3h 15min",
+    "genre": [
+      "Biography",
+      "Drama",
+      "History"
+    ],
+    "rate": 8.9
+  },
+  {
+    "title": "Saving Private Ryan",
+    "year": 1998,
+    "director": "Steven Spielberg",
+    "duration": "2h 49min",
+    "genre": [
+      "Drama",
+      "War"
+    ],
+    "rate": 8.6
+  }
+]
 
+// Iteration 1: All directors? - Get the array of all directors.
+
+const getAllDirectors = arr =>{
+  let movieDirectorsList= arr.map(name => name.director);
+  return movieDirectorsList;
+}
 getAllDirectors(movies);
+
 
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
 
@@ -61,55 +84,56 @@ getAllDirectors(movies);
 //Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 
 //x is used even it does not make sense for now, it made me understand iteration better
-const howManyMovies = (moviesList) => {
+const howManyMovies = moviesList => {
   let stevenDramaMovie= moviesList.filter((x) => x.director === "Steven Spielberg" && x.genre.includes("Drama"));
-  
   return stevenDramaMovie;
 }
 howManyMovies(movies);
 
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
 
-const ratesAverage = moviestList => {
-  if(moviestList.length ===0) {return 0};
-  let total = moviestList.map(x => x["rate"]);
-  let average = total.reduce((a,b) => a+b);
-
-  return Number((average / moviestList.length).toFixed(2));
-};
+const ratesAverage = arr =>{
+  if(arr.length ===0) {return 0};
+  let collectRate= arr.map(num=> num["rate"]);
+  let sumRate= collectRate.reduce((acc, curr) => acc + curr);
+  let averageRate= ((sumRate/arr.length).toFixed(2));
+  console.log(averageRate);
+}
 ratesAverage(movies);
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 
-const dramaMoviesRate = moviesList => {
-  let dramaMovies = moviesList.filter(dramas => dramas["genre"].includes("Drama"));
+const dramaMoviesRate = arr =>{
+  let dramaMovies= arr.map(film =>film["genre"].includes("Drama"));
   if (dramaMovies.length === 0){return 0};
-  let dramaRates= dramaMovies.map(movies => movies["rate"]);
-  let average = rates.reduce((a,b) => a+b);
-  
-  return Number((average / dramaMovies.length).toFixed(2));
+  let collectDramaMovies= dramaMovies.map(film =>film["rate"]);
+  let average= collectDramaMovies.reduce((a,b)=> a+b);
+  let dramaAverageRate=((average/arr.length).toFixed(2));
+  console.log(dramaAverageRate);
 }
-dramaMoviesRate(movies);
+ratesAverage(movies);
   
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 
-const orderByYear = (moviesList) => {
-  return moviesList.sort(function (a, b) {
-    return a.year - b.year;
-  });
+const orderByYear = arr =>{
+  let onlyYear= arr.map(date =>date.year);
+  let sortYear= onlyYear.sort((a,b)=> b-a);
+  return sortYear;
 }
+orderByYear(movies);
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 
-function orderAlphabetically(moviesList) {
-  let moviesTitle = moviesList.map(array => array.title)
+const orderAlphabetically = arr =>{
+  let moviesTitle = arr.map(name => name.title)
   let titleArray = moviesTitle.sort()
   //from 0 to 20
   let slicedTitleArray = titleArray.slice(0, 20)
-  return slicedTitleArray
+  return slicedTitleArray;
 }
 orderAlphabetically(movies);
+
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 
