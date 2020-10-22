8080c368594c482efee549d359b572ecf0b6678a
Rosana Batista <rosana.psb@gmail.com>
2020-10-22 22:47:17 +0200

    done

--- src/movies.js
+++ src/movies.js
@@ -18,23 +18,59 @@ function howManyMovies(movies) {
   return getStevenSpilbergDramas.length;
 }
 
-console.log(howManyMovies(movies))
-
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
- function ratesAverage(array) {
+function ratesAverage(array) {
   let avarageRate = array.reduce(function (acc, val) {
-    return acc + val.rate / array.length;
+      let rate = val.rate
+      if(isNaN(rate)){
+          rate = 0;
+      }
+    return acc + rate / array.length;
   }, 0);
   return Number(avarageRate.toFixed(2));
+
 }
+
+
 // Iteration 4: Drama movies - Get the average of Drama Movies
 const dramaMoviesRate = movies => {
-    return ratesAverage["drama"]
+   let dramaMovies =  movies.filter(movie => movie.genre.includes('Drama'));
+   if(dramaMovies.length === 0){
+       return 0;
+   }
+   let sumRates = dramaMovies.reduce(function(acc, movie){
+    return acc + movie.rate
+   }, 0);
+   let averageDramaRate = sumRates/dramaMovies.length;
+   return Number(averageDramaRate.toFixed(2));
 }
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
+const orderByYear = movies => {
 
+   return movies.slice().sort(function(movie1, movie2){
+        if(movie1.year < movie2.year) {
+            return -1;
+        };
+        if(movie1.year > movie2.year) {
+            return 1;
+        };
+
+        if(movie1.title < movie2.title) {
+            return -1;
+        };
+
+        if(movie1.title > movie2.title) {
+            return 1;
+        }
+        return 0;
+    });
+}
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-
+const orderAlphabetically = movies => {
+    const titles = movies.map(movie => movie.title);
+    const sortedTitles = titles.sort();
+    return sortedTitles.slice(0, 20);
+}
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 
 // BONUS - Iteration 8: Best yearly rate average - Best yearly rate average
