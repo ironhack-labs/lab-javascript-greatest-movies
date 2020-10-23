80b74ff7ca14d5f685469b4a236b7f0537d348ef
Marc Gotzens <marcgotzens@hotmail.com>
2020-10-23 08:55:52 +0200

    Got to 32 failures

--- src/movies.js
+++ src/movies.js
@@ -1,30 +1,40 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(){
+function getAllDirectors() {
 
-    let getAllDirectors = movies.map(movie => movie.director) 
+    let getAllDirectors = movies.map(movie => movie.director)
     return getAllDirectors;
 }
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 function howManyMovies(array) {
     const dramaSteven = array.filter(movie =>
         movie.director === "Steven Spielberg" && movie.genre.includes("Drama"))
-        console.log(dramaSteven);
+    console.log(dramaSteven);
     return dramaSteven.length;
-   
+
 }
 
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
-function ratesAverage(array) {
-    const allRatesAverage = array.reduce((acc, movie) => {
-        acc + movie.rate;
-        console.log("que es veu", acc)
+function allRatesAverage(array) {
+    if (array.length === 0) {
+        return 0;
+    }
+    const allRatesAverage = array.reduce((acc, movie,index) => {
+        if (!isNaN(movie.rate)) {
+            console.log("que es veu acc: ", acc);
+            console.log("que es veu movie rate: ", movie.rate);
+            console.log("que es veu index: ", index);
+            console.log("suma de acc i movie.rate:0,", acc + movie.rate);
+            return acc + movie.rate;
+            
+        }
+       
+    
     }, 0);
-        
-    
-        return Number((allRatesAverage / array.length).toFixed(2));
 
-    
+    return Number((allRatesAverage / array.length).toFixed(2));
+
+
 }
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesRate(array) {
@@ -37,7 +47,7 @@ function orderByYear(array) {
     const orderByYear = array.sort((movie1, movie2) => {
         return movie1.year - movie2.year;
     })
-    console.log("Ordered movies array",orderByYear)
+    console.log("Ordered movies array", orderByYear)
 
 }
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
