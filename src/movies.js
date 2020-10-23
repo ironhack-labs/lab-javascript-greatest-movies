f219a7fba3b07994e48ddb18379678187a53daff
Jan <jan.leon.voelker@gmail.com>
2020-10-23 18:23:49 +0200

    Finished #1,#2 and the evil #3

--- src/movies.js
+++ src/movies.js
@@ -1,9 +1,70 @@
 // Iteration 1: All directors? - Get the array of all directors.
+function getAllDirectors (movies) {
+let directors = movies.map (function (movie) {
+    return movie.director
+})
+return directors;
+}
+
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 
+function howManyMovies (movies) {
+    let allSpielbergMovies = movies.filter (function (movie) {
+    return movie.director === "Steven Spielberg" && movie.genre.includes("Drama")
+    });
+    return allSpielbergMovies.length; 
+    }
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
+// function ratesAverage(movies) {
+//     // let rateArray = movies.map(function(movie){
+//     //     return movie.rate;
+//     // });
+
+//     // if (rateArray.length === 0) {
+//     // return 0;
+//     // }
+
+//     let ratingsCombined = movies.reduce(function(sum,value) {
+//         if (value.rate === "" || value.rate ==== null) {
+//             return sum;
+//         }
+//         console.log("this is sum", sum)
+//         console.log("this is valueee", value.rate)
+        
+//         return sum + value.rate;
+//     },0)
+//     console.log("this is ratingsCombined", ratingsCombined/movies.length);
+//     //console.log(Number(ratingsCombined/rateArray.length).toFixed(2));
+//     //return Number((ratingsCombined/rateArray.length).toFixed(2));
+// };
+
+function ratesAverage(movies) {
+    // let rateArray = movies.map(function(movie){
+    //     return movie.rate;
+    // });
+
+    // if (rateArray.length === 0) {
+    // return 0;
+    // }
+    if (movies.length === 0) {
+        return 0;
+        }
+        let ratingsCombined = movies.reduce(function(sum,value) {
+            if (value.rate === "" || value.rate == null) {
+                return sum;
+            }
+            console.log("this is sum", sum)
+            console.log("this is valueee", value.rate)
+            
+            return sum + value.rate;
+        },0)
+        console.log("this is ratingsCombined/movies.length",ratingsCombined/movies.length);
+        //return Number((ratingsCombined/rateArray.length).toFixed(2));
+        return Number((ratingsCombined/movies.length).toFixed(2));
+    }; 
+    ratesAverage(movies);
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 
