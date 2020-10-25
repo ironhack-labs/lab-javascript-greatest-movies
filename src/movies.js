ea6a82b8221fd213b40b1fc61a79312265aa8640
ararita <munjizavesna@gmail.com>
2020-10-25 12:22:38 +0100

    Partly finish iteration 4, fix iteration 3 error

--- src/movies.js
+++ src/movies.js
@@ -45,22 +45,101 @@ function ratesAverage(array) {
 
   const sum = array.reduce((acc, val) => {
     //console.log("val.rate", val.rate);
-    if (val.rate === "" || "rate" in val === false) return 0;
+    if (val.rate === "" || "rate" in val === false) return acc;
 
-    // || val.rate == undefined
     return acc + val.rate;
   }, 0);
   //console.log("-------", average);
   return Number((sum / array.length).toFixed(2));
 }
 
-// const totalRates = product.reviews.reduce(function (sum, review) {
-//   return sum + review.rate;
-// }, 0);
-
-// console.log(totalRates);
-
 // Iteration 4: Drama movies - Get the average of Drama Movies
+
+// Drama is the genre that repeats the most on our array. Apparently, people love drama! 👀
+// Create a dramaMoviesRate() function that receives an array as a
+//parameter to get the average rate of all drama movies! Let's see if it is better than the general average.
+
+// function dramaMoviesRate(array) {
+//   const sum = array.reduce((acc, val) => {
+//     if (val.genre.includes("Drama")) {
+//       return acc + val.rate;
+//     } else {
+//       return 0;
+//     }
+//   }, 0);
+//   console.log(Number((sum / array.length).toFixed(2)));
+//   return Number((sum / array.length).toFixed(2));
+// }
+
+// function dramaMoviesRate(array) {
+//   const filtered = array.filter((element) => {
+//     // console.log("-----", element.genre);
+//     if (element.genre.includes("Drama")) {
+//       return true;
+//     }
+//   });
+//   // console.log("filtered", filtered);
+
+//   const sum = filtered.reduce((acc, val) => {
+//     console.log("rate", val.rate);
+
+//     return acc + val.rate;
+//   }, 0);
+//   console.log(Number((sum / filtered.length).toFixed(2)));
+//   return Number((sum / filtered.length).toFixed(2));
+// }
+
+//working except last test:
+function dramaMoviesRate(array) {
+  // const filtered = array.map((element) => {
+  //   if (element.genre.includes("Drama")) {
+  //     console.log("element.genre", element.genre.length);
+  //     return element;
+  //   } else {
+  //     return 0;
+  //   }
+  // });
+  // const filtered = array.filter((element) => {
+  //   console.log("-----", element.genre);
+  //   // if (element.genre.includes("Drama")) return true;
+  //   return element.genre.includes("Drama");
+  // });
+
+  // console.log("filtered", filtered);
+
+  // const sum = filtered.reduce((acc, val) => {
+  //   console.log("val", val);
+  //   console.log("acc", acc);
+  //   return acc + val.rate;
+  // }, 0);
+
+  // console.log("sum", sum);
+  // console.log(filtered.length);
+
+  // console.log(Number((sum / filtered.length).toFixed(2)));
+  // return Number((sum / filtered.length).toFixed(2));
+  const sum = array.reduce((acc, val) => {
+    // console.log("val.genre", val.genre);
+    // console.log("acc", acc);
+
+    console.log(val.genre.includes("Drama"));
+
+    if (val.genre.includes("Drama")) {
+      console.log("acc", acc);
+      console.log("val.rate", val.rate);
+
+      return acc + val.rate;
+    } else {
+      return acc;
+    }
+  }, 0);
+
+  // console.log("sum", sum);
+  // console.log(array.length);
+
+  console.log(Number((sum / array.length).toFixed(2)));
+  return Number((sum / array.length).toFixed(2));
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 
