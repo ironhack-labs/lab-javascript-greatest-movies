0884b1a3efbdcb48f61b4978e66809e49211c817
William Campbell <me@williamcampbell.ie>
2020-10-25 20:27:53 +0100

    done

--- src/movies.js
+++ src/movies.js
@@ -45,7 +45,7 @@ function ratesAverage(movies) {
 function dramaMoviesRate(movies) {
     let totalRating = 0
     let moviesArray = movies.map(function(movie) {
-    if (movie.genre == ('Drama')) {        
+    if (movie.genre.includes ('Drama')) {        
         totalRating = totalRating + movie.rate
         return totalRating;}
     });
@@ -56,22 +56,103 @@ return  Math.round(((totalRating/moviesArray.length) + Number.EPSILON) * 100) / 
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 
+
+
+function compare( a, b ) {
+    if ( a.year < b.year ){
+      return -1;
+    }
+    if ( a.year > b.year ){
+      return 1;
+    }
+    return 0;
+  }
+
+  
+ function orderByYear(movies) {
+    // console.log(movies.compare) ;
+    return movies.compare;
+
+ }
+
+
+
+
+
+
+// function orderByYear(movies) {
+//     let moviesArray = movies.sort(function(movie.year) {
+//        return movie;
+//        });
+//    return moviesArray;
+//    }
+   
+   
+
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
+
+function compare2( a, b ) {
+    if ( a.title < b.title ){
+      return -1;
+    }
+    if ( a.title > b.title ){
+      return 1;
+    }
+    return 0;
+  }
+
+  
+ function orderAlphabetically(movies) {
+    // console.log(movies.compare) ;
+    return movies.compare2;
+
+ }
+
+
+
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 
-function turnHoursToMinutes (movies) {
-    let timeArray = movies.map(function(movie) {
-        let hours = 1;//  movie.duration.slice(0 , 1)*60 ;
-        //let minutez = movie.duration.slice(3 , 5);
-        //let minutes = minutez.txt.replace(/[^0-9]/g,'');
-        console.log(hours);
-        return (hours + minutes);
-    });
-    console.log(timeArray);
-    return timeArray;
-   }
+// function turnHoursToMinutes (movies) {
+//     let timeArray = movies.map(function(movie) {
+//         let hours = 1;//  movie.duration.slice(0 , 1)*60 ;
+//         //let minutez = movie.duration.slice(3 , 5);
+//         //let minutes = minutez.txt.replace(/[^0-9]/g,'');
+//         console.log(hours);
+//         return (hours + minutes);
+//     });
+//     console.log(timeArray);
+//     return timeArray;
+//    }
    
   
+
+
+   function turnHoursToMinutes(movies) {
+    let moviesArray = movies.map(function(movie) {
+        let hourz =  movie.duration.slice(0 , 1) ; 
+        let hours = (parseInt(hourz, 10)*60);
+        let minutez = movie.duration.slice(3 , 5);
+        let minutes = parseInt(minutez, 10);
+        if ((isNaN(minutes)) && (!isNaN(hours))) {
+            // console.log (movie.hours);
+            return movie.hours
+        }
+
+
+        // console.log(movie.title);
+        // console.log('minutes ' + minutes);
+        // console.log('hours ' + hours);
+        forceToMinutes = parseInt(minutes + hours, 10);
+        // console.log(forceToMinutes);
+        return forceToMinutes;
+       });
+       //console.log(moviesArray);
+   return moviesArray;
+   }
+   
+   
+   
+
 
 // BONUS - Iteration 8: Best yearly rate average - Best yearly rate average
