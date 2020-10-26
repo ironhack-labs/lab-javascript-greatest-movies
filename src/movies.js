ac04c01ee8304c882e435244bf7f817c92164ef2
Monch87 <montserrat.mosqueda@gmail.com>
2020-10-26 16:19:16 +0100

    commit

--- src/movies.js
+++ src/movies.js
@@ -4,11 +4,8 @@ const getAllDirectors = arrayOfMovies => arrayOfMovies.map(elm => elm.director)
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-const howManyMovies = [Steven Spielberg]
-
-const drama = howManyMovies.filter (elm => {elm.drama && elm.director
-    return elm % 0;
-})
+const howManyMovies = [arrayOfMovies]
+const StevenS = howManyMovies.filter (elm => {elm.drama && elm.director})
 
 
 
