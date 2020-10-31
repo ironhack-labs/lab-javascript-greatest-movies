69b072803942900059e1f12af72440d89ee67714
eduardoLemes <eduardo_lsf@outlook.com>
2020-10-31 16:36:28 -0300

    in progress

--- src/movies.js
+++ src/movies.js
@@ -1,11 +1,55 @@
 // Iteration 1: All directors? - Get the array of all directors.
+function getAllDirectors(arrMovies){
+    let allDirectors = []
+    allDirectors = arrMovies.map(movie => movie.director)
+    return allDirectors
+}
+
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
-
+    
+    
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-
+function howManyMovies(arrMovies){
+    let movieSpielberg = arrMovies.filter(movie => movie.director === "Steven Spielberg" && movie.genre.includes("Drama"));
+    
+    return movieSpielberg.length
+}
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
+    function ratesAverage(arrMovies){
+
+    let allRates = arrMovies.map(movie => movie.rate)
+
+    if(arrMovies.length === 0){
+            return 0
+
+   }else{
+       let sumRates = allRates.reduce((acumulador, movie) => acumulador + movie, 0)
+       let totalRate = (sumRates / arrMovies.length).toFixed(2)  
+       return parseFloat(totalRate)   
+    }
+    
+    }
+
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
+    function dramaMoviesRate(arrMovies){
+
+        let filtroDrama = arrMovies.filter(movie => movie.genre.includes("Drama"));
+               
+        if(filtroDrama.length === 0){
+           return 0
+        }else{
+            let sumDrama = filtroDrama.reduce((acumulador, movie) => acumulador + movie, 0)
+            let dramaMedia= ((sumDrama / filtroDrama.length).toFixed(2))  
+
+        return parseFloat(dramaMedia)
+        //     let  dramaMovieRate = ratesAverage(dramaMovies)
+        //     return parseFloat(dramaMoviesRate)
+        // }
+        
+}
+    }
+
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 
