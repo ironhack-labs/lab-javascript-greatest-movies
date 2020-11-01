b71d338bd36b9ed7ba85dfed971a22fd7b85daea
aguiluxo91 <apuertadelaguila@gmail.com>
2020-11-01 21:56:27 +0100

    feat: Iteration 1 to 5 + syntax improvement in each Iteration

--- src/movies.js
+++ src/movies.js
@@ -6,49 +6,20 @@ function getAllDirectors (arr) {
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 function howManyMovies (arr) {
-    if(arr.length === 0) {
-        return 0;
-    } else {
-        let result = 0;
-        let spielbergMovies = arr.filter(movie => movie.director === "Steven Spielberg" && movie.genre.includes("Drama"))
-        result += spielbergMovies.length
-        return result;
-    }    
+    return arr.filter(movie => movie.director === 'Steven Spielberg' && movie.genre.includes('Drama')).length 
 }
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
 function ratesAverage(arr) {
-    if(arr.length === 0) {
-        return 0;
-    } else {
-        const total = arr.reduce((acc, el) => {
-            if(typeof el.rate !== "number") {
-                return acc;
-            } else {
-                return acc + el.rate;
-            } 
-        }, 0);
-        return Math.round(total / arr.length * 100) / 100;
-    }
+    return arr.length === 0 ? 0 : Math.round((arr.reduce((acc, el) => (typeof el.rate !== "number") ? acc : acc + el.rate, 0)) / arr.length * 100) / 100;
 }
-
-
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesRate (arr) {
-    if(arr.length === 0) {
-        return 0;
-    } else {
-        const total = arr.reduce((acc, el) => {
-            if(el.genre.includes("Drama")) {
-                acc += el.rate
-            } 
-            return acc;
-        }, 0);
-        let dramaMovies = arr.filter(dramaMovies => dramaMovies.genre.includes("Drama"))
-        return Math.round(total / dramaMovies.length * 100) / 100;
-    }
+    return (arr.filter(dramaMovies => dramaMovies.genre.includes("Drama"))).length === 0 ? 0 : Math.round(arr.reduce((acc, el) => el.genre.includes("Drama") ? acc += el.rate : acc, 0) / (arr.filter(dramaMovies => dramaMovies.genre.includes("Drama"))).length * 100) / 100;
 }
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-
+function orderByYear (arr) {
+    return (arr.filter(movieYear => movieYear.year)).sort((a, b) => (a.year > b.year) ? 1 : (a.year === b.year) ? ((a.title > b.title) ? 1 : -1) : -1);
+}
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
