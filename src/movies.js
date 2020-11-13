456d219f13200402f1e03e69962e8d9b82d2badb
Elisa Chen <skechen@gmail.com>
2020-11-13 19:18:08 +0100

    lab completed

--- src/movies.js
+++ src/movies.js
@@ -1,15 +1,516 @@
+// let movies = [
+//     {
+//       "title": "The Shawshank Redemption",
+//       "year": 1994,
+//       "director": "Frank Darabont",
+//       "duration": "2h 22min",
+//       "genre": [
+//         "Crime",
+//         "Drama"
+//       ],
+//       "rate": 9.3
+//     },
+//     {
+//       "title": "The Godfather",
+//       "year": 1972,
+//       "director": "Francis Ford Coppola",
+//       "duration": "2h 55min",
+//       "genre": [
+//         "Crime",
+//         "Drama"
+//       ],
+//       "rate": 9.2
+//     },
+//     {
+//       "title": "The Godfather: Part II",
+//       "year": 1974,
+//       "director": "Francis Ford Coppola",
+//       "duration": "3h 22min",
+//       "genre": [
+//         "Crime",
+//         "Drama"
+//       ],
+//       "rate": 9
+//     },
+//     {
+//       "title": "The Dark Knight",
+//       "year": 2008,
+//       "director": "Christopher Nolan",
+//       "duration": "2h 32min",
+//       "genre": [
+//         "Action",
+//         "Crime",
+//         "Drama",
+//         "Thriller"
+//       ],
+//       "rate": 9
+//     },
+//     {
+//       "title": "12 Angry Men",
+//       "year": 1957,
+//       "director": "Sidney Lumet",
+//       "duration": "1h 36min",
+//       "genre": [
+//         "Crime",
+//         "Drama"
+//       ],
+//       "rate": 8.9
+//     },
+//     {
+//       "title": "Schindler\"s List",
+//       "year": 1993,
+//       "director": "Steven Spielberg",
+//       "duration": "3h 15min",
+//       "genre": [
+//         "Biography",
+//         "Drama",
+//         "History"
+//       ],
+//       "rate": 8.9
+//     },
+//     {
+//       "title": "Pulp Fiction",
+//       "year": 1994,
+//       "director": "Quentin Tarantino",
+//       "duration": "2h 34min",
+//       "genre": [
+//         "Crime",
+//         "Drama"
+//       ],
+//       "rate": 8.9
+//     },
+//     {
+//       "title": "The Lord of the Rings: The Return of the King",
+//       "year": 2003,
+//       "director": "Peter Jackson",
+//       "duration": "3h 21min",
+//       "genre": [
+//         "Adventure",
+//         "Drama",
+//         "Fantasy"
+//       ],
+//       "rate": 8.9
+//     },
+//     {
+//       "title": "Il buono, il brutto, il cattivo",
+//       "year": 1966,
+//       "director": "Sergio Leone",
+//       "duration": "3h 2min",
+//       "genre": [
+//         "Western"
+//       ],
+//       "rate": 8.9
+//     },
+//     {
+//       "title": "Fight Club",
+//       "year": 1999,
+//       "director": "David Fincher",
+//       "duration": "2h 19min",
+//       "genre": [
+//         "Drama"
+//       ],
+//       "rate": 8.8
+//     },
+//     {
+//       "title": "The Lord of the Rings: The Fellowship of the Ring",
+//       "year": 2001,
+//       "director": "Peter Jackson",
+//       "duration": "2h 58min",
+//       "genre": [
+//         "Adventure",
+//         "Drama",
+//         "Fantasy"
+//       ],
+//       "rate": 8.8
+//     },
+//     {
+//       "title": "Forrest Gump",
+//       "year": 1994,
+//       "director": "Robert Zemeckis",
+//       "duration": "2h 22min",
+//       "genre": [
+//         "Comedy",
+//         "Drama",
+//         "Romance"
+//       ],
+//       "rate": 8.8
+//     },
+//     {
+//       "title": "Star Wars: Episode V - The Empire Strikes Back",
+//       "year": 1980,
+//       "director": "Irvin Kershner",
+//       "duration": "2h 4min",
+//       "genre": [
+//         "Action",
+//         "Adventure",
+//         "Fantasy",
+//         "Sci-Fi"
+//       ],
+//       "rate": 8.8
+//     },
+//     {
+//       "title": "Inception",
+//       "year": 2010,
+//       "director": "Christopher Nolan",
+//       "duration": "2h 28min",
+//       "genre": [
+//         "Action",
+//         "Adventure",
+//         "Sci-Fi",
+//         "Thriller"
+//       ],
+//       "rate": 8.8
+//     },
+//     {
+//       "title": "The Lord of the Rings: The Two Towers",
+//       "year": 2002,
+//       "director": "Peter Jackson",
+//       "duration": "2h 59min",
+//       "genre": [
+//         "Adventure",
+//         "Drama",
+//         "Fantasy"
+//       ],
+//       "rate": 8.7
+//     },
+//     {
+//       "title": "One Flew Over the Cuckoo\"s Nest",
+//       "year": 1975,
+//       "director": "Milos Forman",
+//       "duration": "2h 13min",
+//       "genre": [
+//         "Drama"
+//       ],
+//       "rate": 8.7
+//     },
+//     {
+//       "title": "Goodfellas",
+//       "year": 1990,
+//       "director": "Martin Scorsese",
+//       "duration": "2h 26min",
+//       "genre": [
+//         "Crime",
+//         "Drama"
+//       ],
+//       "rate": 8.7
+//     },
+//     {
+//       "title": "The Matrix",
+//       "year": 1999,
+//       "director": "Lana Wachowski",
+//       "duration": "2h 16min",
+//       "genre": [
+//         "Action",
+//         "Sci-Fi"
+//       ],
+//       "rate": 8.7
+//     },
+//     {
+//       "title": "Shichinin no samurai",
+//       "year": 1954,
+//       "director": "Akira Kurosawa",
+//       "duration": "3h 27min",
+//       "genre": [
+//         "Adventure",
+//         "Drama"
+//       ],
+//       "rate": 8.7
+//     },
+//     {
+//       "title": "Star Wars",
+//       "year": 1977,
+//       "director": "George Lucas",
+//       "duration": "2h 1min",
+//       "genre": [
+//         "Action",
+//         "Adventure",
+//         "Fantasy",
+//         "Sci-Fi"
+//       ],
+//       "rate": 8.7
+//     },
+//     {
+//       "title": "Cidade de Deus",
+//       "year": 2002,
+//       "director": "Fernando Meirelles",
+//       "duration": "2h 10min",
+//       "genre": [
+//         "Crime",
+//         "Drama"
+//       ],
+//       "rate": 8.7
+//     },
+//     {
+//       "title": "Se7en",
+//       "year": 1995,
+//       "director": "David Fincher",
+//       "duration": "2h 7min",
+//       "genre": [
+//         "Crime",
+//         "Drama",
+//         "Mystery",
+//         "Thriller"
+//       ],
+//       "rate": 8.6
+//     },
+//     {
+//       "title": "The Silence of the Lambs",
+//       "year": 1991,
+//       "director": "Jonathan Demme",
+//       "duration": "1h 58min",
+//       "genre": [
+//         "Crime",
+//         "Drama",
+//         "Thriller"
+//       ],
+//       "rate": 8.6
+//     },
+//     {
+//       "title": "It\"s a Wonderful Life",
+//       "year": 1946,
+//       "director": "Frank Capra",
+//       "duration": "2h 10min",
+//       "genre": [
+//         "Drama",
+//         "Family",
+//         "Fantasy"
+//       ],
+//       "rate": 8.6
+//     },
+//     {
+//       "title": "La vita è bella",
+//       "year": 1997,
+//       "director": "Roberto Benigni",
+//       "duration": "1h 56min",
+//       "genre": [
+//         "Comedy",
+//         "Drama",
+//         "War"
+//       ],
+//       "rate": 8.6
+//     },
+//     {
+//       "title": "The Usual Suspects",
+//       "year": 1995,
+//       "director": "Bryan Singer",
+//       "duration": "1h 46min",
+//       "genre": [
+//         "Crime",
+//         "Drama",
+//         "Mystery",
+//         "Thriller"
+//       ],
+//       "rate": 8.6
+//     },
+//     {
+//       "title": "Léon",
+//       "year": 1988,
+//       "director": "Luc Besson",
+//       "duration": "1h 50min",
+//       "genre": [
+//         "Crime",
+//         "Drama",
+//         "Thriller"
+//       ],
+//       "rate": 8.6
+//     },
+//     {
+//       "title": "Saving Private Ryan",
+//       "year": 1998,
+//       "director": "Steven Spielberg",
+//       "duration": "2h 49min",
+//       "genre": [
+//         "Drama",
+//         "War"
+//       ],
+//       "rate": 8.6
+//     },
+//     {
+//       "title": "Sen to Chihiro no kamikakushi",
+//       "year": 2001,
+//       "director": "Hayao Miyazaki",
+//       "duration": "2h 5min",
+//       "genre": [
+//         "Animation",
+//         "Adventure",
+//         "Family",
+//         "Fantasy",
+//         "Mystery"
+//       ],
+//       "rate": 8.6
+//     },
+//     {
+//       "title": "American History X",
+//       "year": 1998,
+//       "director": "Tony Kaye",
+//       "duration": "1h 59min",
+//       "genre": [
+//         "Crime",
+//         "Drama"
+//       ],
+//       "rate": 8.5
+//     },
+//     {
+//       "title": "C\"era una volta il West",
+//       "year": 1968,
+//       "director": "Sergio Leone",
+//       "duration": "2h 44min",
+//       "genre": [
+//         "Western"
+//       ],
+//       "rate": 8.6
+//     },
+//     {
+//       "title": "Interstellar",
+//       "year": 2014,
+//       "director": "Christopher Nolan",
+//       "duration": "2h 49min",
+//       "genre": [
+//         "Adventure",
+//         "Drama",
+//         "Sci-Fi"
+//       ],
+//       "rate": 8.6
+//     },
+//     {
+//       "title": "Psycho",
+//       "year": 1960,
+//       "director": "Alfred Hitchcock",
+//       "duration": "1h 49min",
+//       "genre": [
+//         "Horror",
+//         "Mystery",
+//         "Thriller"
+//       ],
+//       "rate": 8.5
+//     },
+//     {
+//       "title": "The Green Mile",
+//       "year": 1999,
+//       "director": "Frank Darabont",
+//       "duration": "3h 9min",
+//       "genre": [
+//         "Crime",
+//         "Drama",
+//         "Fantasy",
+//         "Mystery"
+//       ],
+//       "rate": 8.5
+//     },
+//     {
+//       "title": "Casablanca",
+//       "year": 1942,
+//       "director": "Michael Curtiz",
+//       "duration": "1h 42min",
+//       "genre": [
+//         "Drama",
+//         "Romance",
+//         "War"
+//       ],
+//       "rate": 8.5
+//     },
+// ]
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
 
+//const getAllDirectors = movies.map(function (toto) {return toto.director;});
+///***** NEEDS TO BE GENERALIZED, SO THAT ITS NOT SPECIFIC FOR MOVIES ARRAY ONLY    */
+
+function getAllDirectors(array) {
+    let directors = array.map(function (object) {return object.director});
+     return directors;
+}
+//console.log("Iteration 1")
+//console.log(getAllDirectors(movies));
+
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 
-// Iteration 3: All rates average - Get the average of all rates with 2 decimals
+//longer version
+// function howManyMovies(array) {
+//     let ssMovies = array.filter(function (movie) {
+//         return movie.director === 'Steven Spielberg' && movie.genre.includes("Drama")
+//     });
+//     return ssMovies;
+// }
 
-// Iteration 4: Drama movies - Get the average of Drama Movies
+//shorter version
+function howManyMovies(array) {
+    let ssMovies = array.filter((movie) => movie.director === 'Steven Spielberg' && movie.genre.includes("Drama"));
+    return ssMovies.length;
+}
+
+//console.log("Iteration 2")
+//console.log(howManyMovies(movies))
+
+// Iteration 3: All rates average - Get the average of all rates with 2 decimals
+//console.log('Iteration 3')
+
+function ratesSum(array) {
+    let sum = array.reduce(function (toto, movie) {
+        toto += movie.rate;
+        return toto ;
+    }, 0);
+    return sum;
+}
+
+function ratesAverage(toto) {
+    return Math.round((ratesSum(toto) / toto.length)*100)/100
+}
+
+// console.log("Iteration 2")
+// console.log(ratesSum(movies))
+// console.log(ratesAverage(movies))
+
+// // Iteration 4: Drama movies - Get the average of Drama Movies
+
+function selectDramaMovies(array) {
+    let ssMovies = array.filter(function (movie) {
+        return movie.genre.includes("Drama")
+    });
+    return ssMovies;
+}
+
+function dramaMoviesRate(array) {
+    return ratesAverage(selectDramaMovies(array))
+}
+
+//console.log("Iteration 4")
+//console.log(dramaMoviesRates(movies))
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
+function orderByYear(array) {
+    const cloneArray = [...array];
+    newList = cloneArray.sort(function(numA, numB) {
+        return numA.year-numB.year
+    });
+    return newList;
+}
+
+// function orderByYear(array) {
+//     newList = array.sort(function(numA, numB) {
+//         return numA.year-numB.year
+//     });
+//     return newList;
+// }
+
+// console.log("Iteration 5")
+// console.log(orderByYear(movies))
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
+
+function getTitles(array) {
+    let titles = array.map(function (object) {return object.title});
+     return titles;
+}
+
+function orderAlphabetically(array) {
+    let sortedList = getTitles(array).sort(function(a,b) {
+        return a.localeCompare(b);
+    });
+    return sortedList.slice(0,20);
+};
+
+//console.log(orderAlphabetically(movies))
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 
