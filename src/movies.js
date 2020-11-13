355be6303e29d70415b5ce76f7857994f1fe0492
Charlotte Oury <charlotte.ouryb@gmail.com>
2020-11-13 18:09:44 +0100

    First iteration done

--- src/movies.js
+++ src/movies.js
@@ -1,7 +1,21 @@
+var { movies } = require("./data.js");
+
 // Iteration 1: All directors? - Get the array of all directors.
+
+//const getAllDirectors = (array) => array.map((director) => );
+
+function getAllDirectors(movies) {
+  let newMovies = movies.map((movie) => movie.director);
+
+  return newMovies;
+}
+
+let getDir = getAllDirectors(movies);
+console.log(getDir);
+
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
 
-// Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
+// Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
 
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
 
