29e77bbed5f70cea02ef83ba1a9bef4dc0a4fba8
Pedro <pedroscampoy@gmail.com>
2020-11-23 00:05:03 +0100

    returning 0 in empty arrays - Iteration 3

--- src/movies.js
+++ src/movies.js
@@ -2,17 +2,28 @@
 
 // Iteration 1: All directors? - Get the array of all directors.
 
-function getAllDirectors(obj) {
-  return obj.map(movie => movie.director)
-}
+const getAllDirectors = (movies) => movies.map(movie => movie.director);
 
-console.log(getAllDirectors(testArr))
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
+
+const getAllUniqueDirectors = (movies) => [...new Set(movies.map(movie => movie.director))];
+
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 
+const howManyMovies = (movies) => movies.filter(movie => movie.director === "Steven Spielberg" && movie.genre.includes("Drama")).length;
+
 
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
+
+const ratesAverage = (movies) => {
+  console.log(movies.length)
+  movies.length === 0 ? 0 : Number((movies.reduce((sum, movie) => {
+    return (sum + movie.rate)
+  }, 0) / movies.length).toFixed(2))
+}
+
+console.log(ratesAverage([]))
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 
