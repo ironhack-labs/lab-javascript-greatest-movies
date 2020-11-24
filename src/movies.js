53d5c9675d06fa8e34d7b45b3fcfa774c0684e9b
Preacherush <jlrascon.job@gmail.com>
2020-11-24 22:41:49 +0100

    It 1 to 6 done. Working on It 7.

--- src/movies.js
+++ src/movies.js
@@ -1,10 +1,11 @@
 // Iteration 1: All directors? - Get the array of all directors.
 function getAllDirectors(arr) {
-    var mapped = arr.map(function (movie) {
+    var directors = arr.map(function (movie) {
         return movie.director;
     });
-    return mapped;
+    return directors;
 }
+
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 function howManyMovies(array) {
@@ -16,6 +17,7 @@ function howManyMovies(array) {
     });
     return filteredDirectors.length;
 }
+
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
 function ratesAverage(array) {
     if (array.length === 0) 
@@ -30,6 +32,7 @@ function ratesAverage(array) {
     let totalRate = parseFloat((sumRate / totalMovies).toFixed(2));
     return totalRate;
 }
+
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesRate(array) {
     let dramaMovies = array.filter(function (item) {
@@ -47,24 +50,64 @@ function dramaMoviesRate(array) {
     let totalRate = parseFloat((sumRate / totalDramaMovies).toFixed(2));
     return totalRate;
 }
-//Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(array) {
-        let moviesByYear = array.sort((a, b) => a.year < b.year) 
-        return moviesByYear; 
-}
-//=================================================================
-function orderByYear(arr){
 
-    movies.sort(function(a, b) { //comprobado que ordena
+// Iteration 5: Ordering by year - Order by year, ascending (in growing order)
+
+function orderByYear (arr){
+    let copiaArray = arr.filter(casilla => true);
+    let copiaArrayOrdenada = copiaArray.sort(function(a, b){
+        if(a.year === b.year){
+        return a.title.localeCompare(b.title);
+        }
         return a.year - b.year;
-        
     });
-    return movies
-    
+    return copiaArrayOrdenada;
 }
-console.log(movies.year);
+
+/* ============ Iteration 5: Ordering by year ======== OPTION 2 =====
+function orderByYear(array) {
+    let newArray = [...array]
+    let moviesByYear = newArray.sort(function (movie1, movie2){
+        if(movie1.year === movie2.year){
+            return movie1.title.localeCompare(movie2.title)
+        } 
+        return  movie1.year - movie2.year; 
+});
+    return moviesByYear;
+};
+*/
+
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
+function orderAlphabetically(array){
+    let mappedMovies = array.map(function (movie) {
+        return movie.title;
+    });
+    mappedMovies.sort()
+        return mappedMovies.slice(0,20)
+};
+
+/* ============ Iteration 6: Alphabetic Order ======== OPTION 2 =====
+function orderAlphabetically(arr){
+    let copiaArray= arr.filter(casilla => true);
+    let copiaArrayOrdenada = copiaArray.sort(function(a, b){
+        return a.title.localeCompare(b.title);
+        } 
+    );
+    let arrayTitles = copiaArrayOrdenada.map(peli => peli.title)
+    if(arrayTitles.length > 20){
+        return arrayTitles.slice(0,20);
+    }
+    return arrayTitles;
+}
+*/
+
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
+function turnHoursToMinutes(arr){
+    let arrayDuration = arr.map(function (movie){
+        return movie.duration;
+    });
+    let onlyNumbers = arrayDuration.replaceAll("[^0-9]", "")
+    return onlyNumbers
+};
+
 // BONUS - Iteration 8: Best yearly rate average - Best yearly rate average
-
-
