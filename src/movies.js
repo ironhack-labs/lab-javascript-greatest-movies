8f54ef5ac49af989a4dc897e3a9e968d8ac0fe93
Pedro Frias <plfriasf@gmail.com>
2020-11-26 13:10:56 -0800

    Bonus 8.1 Done

--- src/movies.js
+++ src/movies.js
@@ -6,17 +6,16 @@ function getAllDirectors(movieArray) {
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
 
 function getAllDirectorsClean(movieArray) {
-  const directors = movieArray.map(function (elem){
-    return elem.director
-  })
-  for (i= directors.length -1; i>=0; i--){
-    if(directors.indexOf(directors[i]) !== i){
-    directors.splice ([i], 1)
+  const directors = movieArray.map(function (elem) {
+    return elem.director;
+  });
+  for (i = directors.length - 1; i >= 0; i--) {
+    if (directors.indexOf(directors[i]) !== i) {
+      directors.splice([i], 1);
     }
   }
-  return directors
+  return directors;
 }
-
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 function howManyMovies(arr) {
@@ -44,24 +43,23 @@ function ratesAverage(arr) {
   return totalRate;
 }
 
-    
 // Iteration 4: Drama movies - Get the average of Drama Movies
-  function dramaMoviesRate(arr) {
-    let dramaMovies = arr.filter(function (elem) {
-      return elem.genre.includes("Drama");
-    });
-    if (dramaMovies.length === 0) {
-      return 0;
+function dramaMoviesRate(arr) {
+  let dramaMovies = arr.filter(function (elem) {
+    return elem.genre.includes("Drama");
+  });
+  if (dramaMovies.length === 0) {
+    return 0;
+  }
+  const sumRate = dramaMovies.reduce(function (acc, elem) {
+    if (elem.rate === undefined) {
+      return acc;
     }
-    const sumRate = dramaMovies.reduce(function (acc, elem) {
-      if (elem.rate === undefined) {
-        return acc;
-      }
-      return acc + elem.rate;
-    }, 0);
-    const numberOfMovies = dramaMovies.length;
-    const totalRate = parseFloat((sumRate / numberOfMovies).toFixed(2));
-    return totalRate;
+    return acc + elem.rate;
+  }, 0);
+  const numberOfMovies = dramaMovies.length;
+  const totalRate = parseFloat((sumRate / numberOfMovies).toFixed(2));
+  return totalRate;
 }
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
@@ -85,59 +83,88 @@ function orderByYear(arr) {
 }
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-  function orderAlphabetically(arr) {
-  let newArr = arr.slice()
-  let titles = newArr.map(function (elem) {
-    return elem.title
+function orderAlphabetically(arr) {
+  let newArr = arr.slice();
+  let titles = newArr
+    .map(function (elem) {
+      return elem.title;
     })
-    .sort()
+    .sort();
   if (titles.length > 20) {
-    return titles.slice(0, 20)
+    return titles.slice(0, 20);
   }
-  return titles
+  return titles;
 }
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes(arr) {
   return arr.map(function (elem) {
     if (elem.duration.length === 8) {
-      const hours = parseFloat(elem.duration.slice(0, 1) * 60)
-      const minutes = parseFloat(elem.duration.slice(3, -3))
-        //console.log (hours)
-        //console.log (minutes)
-      elem.duration = hours + minutes
-      return elem
+      const hours = parseFloat(elem.duration.slice(0, 1) * 60);
+      const minutes = parseFloat(elem.duration.slice(3, -3));
+      //console.log (hours)
+      //console.log (minutes)
+      elem.duration = hours + minutes;
+      return elem;
+    } else if (elem.duration.length == 2) {
+      const hours = parseFloat(elem.duration.slice(0, 1) * 60);
+      elem.duration = hours;
+      return elem;
+    } else if (elem.duration.length == 4 || elem.duration.length == 5) {
+      const minutes = parseFloat(elem.duration.slice(0, 3));
+      elem.duration = minutes;
+      return elem;
     }
-    else if (elem.duration.length == 2) {
-      const hours = parseFloat(elem.duration.slice(0, 1) * 60)
-      elem.duration = hours 
-      return elem
-    }
-    else if (elem.duration.length == 4 || elem.duration.length == 5){
-      const minutes = parseFloat(elem.duration.slice(0, 3))
-      elem.duration = minutes
-      return elem
-    }
-    })
+  });
 }
 
 // BONUS - Iteration 8: Best yearly rate average - Best yearly rate average
 function bestYearAvg(arr) {
   if (arr.length === 0) {
-    return null
+    return null;
   }
-  let firstTitle = arr[0].year
-  let filmSameYear = []
-  //console.log (firstTitle)
-  arr.filter(function (elem){
-    if (elem.year === firstTitle){
-      filmSameYear.push(elem)
+  //array of years
+  const years = arr.map((element) => element.year);
+  //array of years without repetition
+  for (i = years.length - 1; i >= 0; i--) {
+    if (years.indexOf(years[i]) !== i) {
+      years.splice([i], 1);
     }
-    //console.log (filmSameYear)
-  })
-  const rate = ratesAverage(filmSameYear)
-  //console.log (rate)
+  }
+  // array of obj  year/rate
+  const yearsRatio = years.map((year) => {
+    return {
+      year: year,
+      rates: [],
+    };
+  });
 
-  return `The best year was ${filmSameYear[0].year} with an average rate of ${rate}`
+  //rates avg by year
+  yearsRatio.forEach((element, index) => {
+    arr.forEach((movie) => {
+      if (movie.year == element.year) {
+        element["rates"].push(movie.rate);
+      }
+    });
+  });
+
+  yearsRatio.forEach((element) => {
+    let rateLength = element.rates.length;
+    let sum = element.rates.reduce((acc, curr) => {
+      return acc + curr;
+    }, 0);
+
+    element.rates = Number((sum / rateLength).toFixed(1));
+  });
+  // sort obj by rate
+  yearsRatio.sort((a, b) => {
+    if (a.rates === b.rates) {
+      if (a.year < b.year) {
+        return -1;
+      }
+    }
+    return b.rates - a.rates;
+  });
+
+  return `The best year was ${yearsRatio[0].year} with an average rate of ${yearsRatio[0].rates}`;
 }
-
