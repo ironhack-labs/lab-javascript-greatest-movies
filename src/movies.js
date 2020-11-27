fcc3e54a6f00c54de795b4d531b9c258f0a6565b
Eduardo García <edugarciasb16@gmail.com>
2020-11-27 02:11:58 +0100

    Iteration 5,6 done

--- src/movies.js
+++ src/movies.js
@@ -72,19 +72,51 @@ function dramaMoviesRate(arr) {
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
    
+
 function orderByYear(arr) {
 
-    const allYears = arr.map((element) => {
-        return element.year
-    });
+    if(arr.length === 0){
+        return 0
+    }
+    const orderYear = arr.sort((a, b) => {
 
-    const orderYear = allYears.sort((a, b) => {
-        return a - b
+        if (a.year > b.year) {
+            return 1
+        } else if (a.year < b.year) {
+            return -1
+        } else {
+            if (a.title > b.title) {
+                return 1
+            } else {
+                return -1
+            }
+        }
     })
 
     return orderYear
 }
+
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
+
+function orderAlphabetically(arr) {
+
+    if (arr.length === undefined) {
+        return null
+    }
+
+    const titles = arr.map((element) => {
+        return element.title
+    })
+
+    const numTitle = titles
+
+    if (numTitle.length >= 20) {
+        return numTitle.sort().slice(0,20)
+    } else {
+        return numTitle.sort()
+    }
+
+}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 
