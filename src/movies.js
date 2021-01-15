6ce84eea7a56148a606a7bc9187788c0fc427704
Michael Raman <raman_michael@hotmail.com>
2021-01-15 21:49:51 +0100

    finished everything beside bonus 8

--- src/movies.js
+++ src/movies.js
@@ -1,15 +1,16 @@
 // Iteration 1: All directors? - Get the array of all directors.
 
+// _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
+
 function getAllDirectors(movies) {
 
     const directorsArr = movies.map((movie) => movie.director);
 
-    return directorsArr;
+    //note/ if index of "director" in the arr = index, filter those out into a new array with the name uniqueDirector
+    const uniqueDirector = directorsArr.filter((v,i) => directorsArr.indexOf(v) === i)
+ 
+    return uniqueDirector;
 }
-
-// _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
-
-
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 
@@ -125,4 +126,28 @@ function orderAlphabetically(movies) {
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 
+function turnHoursToMinutes(movies) {
+
+    const moviesCopy = JSON.parse(JSON.stringify(movies));
+
+    for (i = 0; i < moviesCopy.length; i++) {
+      
+      if (moviesCopy[i].duration.length === 8 || moviesCopy[i].duration.length === 7) {
+        let splitHours = (moviesCopy[i].duration.split('h')[0])*60;
+        let splitMinutes = moviesCopy[i].duration.split('min')[0].split(' ')[1];
+        let totalMin = Number(splitHours) + Number(splitMinutes);
+        moviesCopy[i].duration = totalMin;
+      } else if (moviesCopy[i].duration.length === 2) {
+        let totalMin = (moviesCopy[i].duration.split('h')[0])*60;
+        moviesCopy[i].duration = totalMin;
+      } else if (moviesCopy[i].duration.length === 5) {
+        let totalMin = moviesCopy[i].duration.split('min')[0];
+        moviesCopy[i].duration = Number(totalMin);
+      } 
+    }
+
+   return moviesCopy;
+}
+
 // BONUS - Iteration 8: Best yearly rate average - Best yearly rate average
+
