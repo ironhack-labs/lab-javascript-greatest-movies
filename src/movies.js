745b7675feb3be994a81d68773efe7f9a057a612
Hugo Copca <copcal79@gmail.com>
2021-01-16 01:07:12 -0600

    Corregido parte1

--- src/movies.js
+++ src/movies.js
@@ -9,11 +9,10 @@
   console.log(directores);  */
 
 
-  function getAllDirectors(mos){
-    return valor= mos.map(e=> e.director)
+function getAllDirectors(mos) {
+    return valor = mos.map(e => e.director)
 }
 
-console.log(getAllDirectors(movies))
 
 
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
@@ -21,7 +20,7 @@ console.log(getAllDirectors(movies))
 
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(mos){
+function howManyMovies(mos) {
     const steven = mos.filter(eDirector => eDirector.director === "Steven Spielberg" && eDirector.genre.includes("Drama"))
     return steven.length
 }
@@ -31,40 +30,42 @@ howManyMovies(movies)
 
 
 function ratesAverage(arrMovies) {
-    if(arrMovies.length===0){
+    if (arrMovies.length === 0) {
         return 0
     }
-    const numArray = arrMovies.map(e => e.rate)
-    const suma = numArray.reduce((a,b) => {
-        return a + b
-    },0)
+    let numArray = arrMovies.map(e => e.rate ? e.rate : 0)
+    let suma = numArray.reduce((a, b) => a + b, 0)
+    let prom = (suma / arrMovies.length).toFixed(2)
+    return parseFloat(prom)
 }
 
-console.log(ratesAverage(movies))
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-const dmr = movies.filter(function(dramaMovie) {
-    return (dramaMovie.director==="Steven Spielberg" || dramaMovie.genre.indexOf('Drama') !== -1)
-    }).length
+function dramaMoviesRate(movies) {
+    const drama = movies.filter(function(dramaMovie) {
+        return (dramaMovie.genre.indexOf('Drama') > -1)
+    })
+    if (!drama.length) return 0
+    let suma = (drama.reduce((acc, cv) => acc + cv.rate, 0) / drama.length).toFixed(2)
+    return parseFloat(suma)
+}
 
-    console.log(dmr)
 
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically(arreglo){
+function orderAlphabetically(arreglo) {
     const ultima = arreglo.map(e => e.title)
     ultima.sort()
-    const veinte = ultima.slice(0,20)
+    const veinte = ultima.slice(0, 20)
     return veinte
 }
 
-console.log(orderAlphabetically(movies))
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-function turnHoursToMinutes(arrmovies){
+function turnHoursToMinutes(arrmovies) {
     const prueba = arrmovies.map(e => e)
     return prueba
 }
-console.log(turnHoursToMinutes(movies))
+
 // BONUS - Iteration 8: Best yearly rate average - Best yearly rate average
