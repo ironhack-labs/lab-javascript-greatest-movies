e2e451f51a6fd2f1964bd7da82de8e34ba0648c7
Simone Capuano <scapuano19@gmail.com>
2021-01-18 10:03:19 +0100

    I don’t know whyin the second step it keeps printing 0

--- src/movies.js
+++ src/movies.js
@@ -1,7 +1,30 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
+function getAllDirectors(arr) {
+    const nameOfDirectors = movies.map(function(name) {
+        return name.director
+    })
+    console.log(nameOfDirectors);
+}
+getAllDirectors();
+
+
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
+
+function howManyMovies(arr) {
+    let count = 0
+    const times = movies.filter(function(movie) {
+
+        if (movie.director === "Steven Spielberg" && movie.genre === "Drama") {
+            return true;
+        }
+    }).map(function(movie) {
+        return count++
+    })
+    console.log(count)
+}
+howManyMovies()
 
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
 
