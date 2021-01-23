d5a387ebae511c5ce1602d995cdb29b6f8eb8e57
Martin Stoyanov <martinestoyanov@gmail.com>
2021-01-23 17:58:44 -0500

    iteration 5 in progress

--- src/movies.js
+++ src/movies.js
@@ -58,6 +58,19 @@ function dramaMoviesRate(arr) {
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 
+function orderByYear(arr){
+    const sortByYear = arr.sort(function(, b)
+    {
+
+    }
+    
+    )
+    
+    
+
+
+}
+
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
