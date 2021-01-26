4141ca9fc224ecdea8f952467aa7e14b969270d5
Aleksandra Porada <aleksandra.porada92@gmail.com>
2021-01-26 18:20:24 +0100

    Re-write the remaining three (4-6) iterations using filter and reduce and sort

--- src/movies.js
+++ src/movies.js
@@ -38,12 +38,54 @@ function ratesAverage(arrayOfMovies) {
     return Number(averagetRate.toFixed(2))
 } 
 
-
 // Iteration 4: Drama movies - Get the average of Drama Movies
+function dramaMoviesRate(arrayOfMovies) {
+  let arrayOfDramas = arrayOfMovies.filter(function (movie) {
+    if (movie.genre.includes("Drama")) {
+      return true;
+    }
+  });
+  numberOfDramas = arrayOfDramas.length
+  averageRateDramas = arrayOfDramas.reduce(function (accumulator, currentValue) {
+    if (currentValue.hasOwnProperty("rate")) {
+      return accumulator + currentValue.rate;
+    }
+    else {
+      return accumulator;
+    }
+  }, 0);
+  if (numberOfDramas === 0) {
+    return 0
+  }
+  averageRateDramas = averageRateDramas / numberOfDramas
+  return Number(averageRateDramas.toFixed(2));
+};
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
+  function orderByYear(arrayOfMovies) {
+    let moviesOrderedByYear = [...arrayOfMovies];
+    moviesOrderedByYear.sort(function(a,b) {
+        if (a.year !== b.year) {
+            return a.year - b.year;
+        } else {
+            return a.title.localeCompare(b.title);
+        }
+    });
+    return moviesOrderedByYear;
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
+function orderAlphabetically(arrayOfMovies) {
+  let moviesOrderdAlphabetically = [...arrayOfMovies]
+  moviesOrderdAlphabetically.sort(function (a,b){
+    return a.title.localeCompare(b.title)
+  })
+  moviesOrderdAlphabetically = moviesOrderdAlphabetically.map(function (a) {
+    return a.title
+  })
+  let top20 = moviesOrderdAlphabetically.splice(0,20)
+  return top20
+}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 
