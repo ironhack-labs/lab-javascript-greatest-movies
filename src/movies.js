f2462fa7d19361d78c6c2a5a0c9821adea2392dc
Martin Stoyanov <martinestoyanov@gmail.com>
2021-01-26 17:13:40 -0500

    iteration 5 completed

--- src/movies.js
+++ src/movies.js
@@ -58,20 +58,20 @@ function dramaMoviesRate(arr) {
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 
-function orderByYear(arr){
-    const sortByYear = arr.sort(function(, b)
-    {
+function orderByYear(arr) {
+    let sortedByYear = arr.slice();
+    sortedByYear.sort((movie1, movie2) => {
+      if (movie1.year > movie2.year) { return 1 }
+      else if (movie1.year == movie2.year) {
+        if (movie1.title < movie2.title) { return -1; }
+      }
+      else { return -1 };
+      // return movie1.year - movie2.year;
+    })
+    return sortedByYear;
+  }
 
-    }
-    
-    )
-    
-    
-
-
-}
-
-// Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
+  // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 
