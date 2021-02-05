692ba503eef512e261248c3cb60aa403bc2d7053
adri <ad.mallie@gmail.com>
2021-02-05 18:38:13 +0100

    iteration one done

--- src/movies.js
+++ src/movies.js
@@ -1,7 +1,91 @@
+
+// import movies from "./data.js"
+let movieLab = [
+    {
+      "title": "The Shawshank Redemption",
+      "year": 1994,
+      "director": "Frank Darabont",
+      "duration": "2h 22min",
+      "genre": [
+        "Crime",
+        "Drama"
+      ],
+      "rate": 9.3
+    },
+    {
+      "title": "The Godfather",
+      "year": 1972,
+      "director": "Francis Ford Coppola",
+      "duration": "2h 55min",
+      "genre": [
+        "Crime",
+        "Drama"
+      ],
+      "rate": 9.2
+    },
+    {
+      "title": "The Godfather: Part II",
+      "year": 1974,
+      "director": "Francis Ford Coppola",
+      "duration": "3h 22min",
+      "genre": [
+        "Crime",
+        "Drama"
+      ],
+      "rate": 9
+    },
+    {
+      "title": "The Dark Knight",
+      "year": 2008,
+      "director": "Christopher Nolan",
+      "duration": "2h 32min",
+      "genre": [
+        "Action",
+        "Crime",
+        "Drama",
+        "Thriller"
+      ],
+      "rate": 9
+    },
+    {
+      "title": "12 Angry Men",
+      "year": 1957,
+      "director": "Sidney Lumet",
+      "duration": "1h 36min",
+      "genre": [
+        "Crime",
+        "Drama"
+      ],
+      "rate": 8.9
+    },
+    {
+      "title": "Schindler\"s List",
+      "year": 1993,
+      "director": "Steven Spielberg",
+      "duration": "3h 15min",
+      "genre": [
+        "Biography",
+        "Drama",
+        "History"
+      ],
+      "rate": 8.9
+    }]
+
 // Iteration 1: All directors? - Get the array of all directors.
+
+function getAllDirectors(array) {
+let directors = array.map(function(movie) {
+    console.log(movie.director);
+    return movie.director;
+    })
+    return directors;
+}
+getAllDirectors(movieLab);
+// console.log(getAllDirectors)
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
+
 
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
 
