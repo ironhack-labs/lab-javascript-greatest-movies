afc3aaa7a21753d08f18c23d52c051321e1f7e16
Anna Lefour <anna.lefour@laposte.net>
2021-02-06 19:19:51 +0100

    Iteration 5 done

--- src/movies.js
+++ src/movies.js
@@ -24,22 +24,22 @@ function howManyMovies(moviesList) {
 
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
 function ratesAverage(moviesRate) {
-  if (moviesRate.length === 0){
+  if (moviesRate.length === 0) {
     return 0;
   };
-  
+
   const rates = moviesRate.map(function (movie) {
     return movie.rate;
   });
   const sum = rates.reduce(function (acc, currentRate) {
     return (acc += currentRate);
   });
-  if (moviesRate.rate == undefined || moviesRate.rate == Number){
+  if (moviesRate.rate == undefined || moviesRate.rate == Number) {
     const average = sum / rates.length;
-  let res = average.toFixed(2);
-  let res2 = parseFloat(res);
-  console.log(res2);
-  return res2; // On n'est pas sûr de comprendre ce que Jasmine demande: "Return average even if one of the movies does not have rate!"
+    let res = average.toFixed(2);
+    let res2 = parseFloat(res);
+    console.log(res2);
+    return res2; // On n'est pas sûr de comprendre ce que Jasmine demande: "Return average even if one of the movies does not have rate!"
   }
 }
 
@@ -48,16 +48,16 @@ function ratesAverage(moviesRate) {
 function dramaMoviesRate(moviesDrama) {
   const dramaMovies = moviesDrama.filter(function (movies) {
     if (movies.genre.includes("Drama") === true) {
-    return true;
+      return true;
     }
   });
-  if (dramaMovies.length === 0){
+  if (dramaMovies.length === 0) {
     return 0;
   };
-  const rates = dramaMovies.map(function(movies){
+  const rates = dramaMovies.map(function (movies) {
     return movies.rate;
-});
-  const sum = rates.reduce(function(acc, currentRate){
+  });
+  const sum = rates.reduce(function (acc, currentRate) {
     return (acc += currentRate);
   });
   const average = sum / rates.length;
@@ -65,7 +65,28 @@ function dramaMoviesRate(moviesDrama) {
   let res2 = parseFloat(res);
   return res2;
 }
+
+
+
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
+
+function orderByYear(moviesYear) {
+  const cloneMoviesYear = [...moviesYear];
+  const cloneMap = cloneMoviesYear.map(function (movie) {
+    return movie;
+  });
+  const sortedCloneMap = cloneMap.sort(function (a, b) {
+    if (a.year !== b.year) {
+      return a.year - b.year;
+    } else {
+      if (a.title < b.title) {
+        return -1;
+      }
+    }
+  });
+  return sortedCloneMap;
+};
+
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 
