2ae710224389642eeafd63a9848f77003ee854ba
fmmyriam <benhmyriam@gmail.com>
2021-02-06 19:32:00 +0100

    do iteration 6 but not finished it

--- src/movies.js
+++ src/movies.js
@@ -22,6 +22,16 @@ function howManyMovies(arrDeMovies) {
     return result.length;
 }
 
+// autre écriture : 
+// function howManyMovies(arrDeMovies) {
+//     const result = arrDeMovies.filter(function (movie) {
+//         let doesMovieMatchConditions;
+//         doesMovieMatchConditions = movie.director === "Steven Spielberg" && movie.genre.includes("Drama");
+//         return doesMovieMatchConditions;
+//     })
+//     return result.length;
+// }
+
 /* console.log(howManyMovies(movies)) */
 //retourne un nombre de films DRAMA et dir SPIELBERG
 
@@ -44,25 +54,34 @@ function ratesAverage(movieRates) {
 }
 
 
-// function reduceMovie(MovieList) {
-//     var accumulator;
-//     for (var i = 0; i < MovieList.length; i++) {
-//         currentMovie = MovieList[i];
-//         if (accumulator) {
-//             reduceFonction(accumulator, MovieList[i]);
-//         } else {
-//             reduceFonction(MovieList[i], MovieList[i + 1]);
-//             i++;
-//         }
-//     }
-// }
-
-// function reduceFonction(accumulator, currentValue) {
-//     // Faire des choses
-//     return '';
-// }
+// Iteration 3: All rates average - Get the average of all rates with 2 decimals - but with map and reduce and filter
 
 
+function ratesAverage4(arrayOfMovies) {
+    if (arrayOfMovies.length === 0) {
+        return 0;
+    } else {
+        const filter1 = arrayOfMovies.filter(function(movie){
+            return movie.rate !== undefined;
+        });
+        const map1 = filter1.map(function (currentMovie) {
+            return currentMovie.rate;
+        });
+        let sumOfRatings = map1.reduce(function (accumulator, currentRate) {
+            return accumulator += currentRate;
+        });
+        return Math.round((sumOfRatings / arrayOfMovies.length) * 100) / 100;
+    }
+}
+
+function ratesAverage2(movieList){
+    if (movieList.length==0) {
+        return 0;
+    } 
+    var total = movieList.filter(m => m.rate).map(m => m.rate).reduce((acc, m) => acc+m);
+    return Math.round((total / movieList.length) * 100) / 100;
+}
+ 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesRate(movieRates) {
     if (movieRates.length === 0) {
@@ -86,7 +105,43 @@ function dramaMoviesRate(movieRates) {
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 
+function orderByYear(movieList) {
+    let orderedByYear = [];
+    movieList.sort(function(movieA, movieB){
+        if (movieA.year < movieB.year) {
+            return -1;
+        } else if (movieA.year > movieB.year) {
+            return 1;
+        } else {
+            if (movieA.title < movieB.title) {
+                return -1;
+            } else if (movieA.title > movieB.title){
+                return 1;
+            } else {
+                return 0;
+            }
+            
+        }
+    });
+    return orderedByYear.concat(movieList);
+}
+
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
+ function orderAlphabetically(arr) {
+    let map2 = arr.map(arr => {
+            return arr.title
+    });
+    map2.sort(function(movieA, movieB){
+        if (movieA.title < movieB.title) {
+            return -1;
+        } else if (movieA.title > movieB.title){
+            return 1;
+        } else {
+            return 0;
+        }
+    });
+    return map2.slice(0, 20);
+ }
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 
