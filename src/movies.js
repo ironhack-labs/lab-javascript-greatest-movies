725a12db96a7c6ca089ddf53f5681bc9fbc25c2c
Mathieu Lambertin <mathieu.lambertin03@gmail.com>
2021-02-06 17:50:34 +0100

    done

--- src/movies.js
+++ src/movies.js
@@ -60,7 +60,58 @@ const orderByYear = arr => {
 }
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
+const orderAlphabetically = (arr) => {
+    return arr.map(({title}) => title).sort().splice(0, 20)
+}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
+const turnHoursToMinutes = (arr) => {
+    const movies = JSON.parse(JSON.stringify(arr))
+    for(let movie of movies) {
+        let minutes = 0;
+        let hours = 0;
+        let duration = movie.duration;
+        for(let i = 0; i < duration.length - 1; i ++) {
+            if(duration[i + 1] === "h") {
+                hours += Number(duration[i]) * 60;
+            }
+            if(duration[i + 1] === "m") {
+                minutes += Number(duration[i - 1] + duration[i]);
+            }
+        }
+        movie.duration = hours + minutes;
+    }
+    return movies;
+}
 
 // BONUS - Iteration 8: Best yearly rate average - Best yearly rate average
+const bestYearAvg = (arr) => {
+    //Create a new array with one year and their rates
+    let newArr = [];
+    arr.forEach(({year, rate}) => {
+        let found = newArr.find(elem => elem.year === year);
+
+        if (!found){
+            found = {year:year, rates:[]};
+            newArr.push(found);
+        }
+
+        found.rates.push(rate);
+    })
+    
+    //Add average rate on each year
+    newArr.forEach((moviesByYear) => {
+        let sum = 0;
+        moviesByYear.rates.forEach((rate) => sum += rate)
+        moviesByYear.avg = sum / moviesByYear.rates.length;
+    })
+
+    //Sort by average rate + year
+    newArr.sort((movie1, movie2) => {
+        if (movie1.avg<movie2.avg) return 1;
+        else if (movie1.avg>movie2.avg) return -1;
+        else return movie1.year - movie2.year;
+    })
+    return newArr.length ? `The best year was ${newArr[0].year} with an average rate of ${newArr[0].avg}`:null
+}
+
