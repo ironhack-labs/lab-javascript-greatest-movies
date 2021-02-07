0f2e2e401710231ec6f34a0892be3313c72dcc61
Magali <fort_magali@hotmail.fr>
2021-02-07 23:16:23 +0100

    bonus 7 corrected

--- src/movies.js
+++ src/movies.js
@@ -3,9 +3,11 @@ const getAllDirectors = function (movies) {
   return movies.map((movie) => movie.director);
 };
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
-const getUniqueDirectors = function(movies,getAllDirectors) {
-  return getAllDirectors(movies).filter((director, index) => getAllDirectors(movies).indexOf(director)===index);
-}
+const getUniqueDirectors = function (movies, getAllDirectors) {
+  return getAllDirectors(movies).filter(
+    (director, index) => getAllDirectors(movies).indexOf(director) === index
+  );
+};
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 const howManyMovies = function (movies) {
   if (movies.length === 0) {
@@ -20,14 +22,14 @@ const howManyMovies = function (movies) {
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
 const ratesAverage = function (movies) {
   if (movies.length === 0) return 0;
-  
+
   let count = movies.length;
   const sum = movies.reduce((accumulator, movie) => {
-     if (typeof movie.rate == "number") {
-    return accumulator + movie.rate; 
-     } else {
-    return accumulator + 0
-     }
+    if (typeof movie.rate == "number") {
+      return accumulator + movie.rate;
+    } else {
+      return accumulator + 0;
+    }
   }, 0);
 
   const average = Number((sum / count).toFixed(2));
@@ -60,75 +62,52 @@ const orderAlphabetically = function (movies) {
 };
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 
- /*const timeToNum = function(time) {
- 
-  let check = time.split(" ")
-  
-  if (check.length>1) {
-    check[0]=check[0].replace("h","")*60;
-    check[1]=check[1].replace("min","")*1;
-    return check[0]+check[1]
-   } else if (check[0].includes("h")) {
-   check[0]=check[0].replace("h","")*60;
-     return check[0]
-   } else if (check[0].includes("min")) {
-   check[0]=check[0].replace("min","")*1;
-     return check[0]
-  }
-   
- }*/
-
- const durNum = function(time) {
-  
-  let result = 0;
-   
-  for (let i=0; i<time.length; i++) {
-    
-    if (time[i+1]==="h") result += time[i]*60
-    if (time[i+1]==="m") result += time[i]*1+time[i-1]*10
-  }
- 
-  return result
-}
-
-  
-  const turnHoursToMinutes = function(movies) {  
-   
-  let array = [...movies];
-  
-  array.forEach(movie => movie.duration = durNum(movie.duration));
-  
-  return array
- 
-}
+const turnHoursToMinutes = function (movies) {
+  let moviesInMinutes = JSON.parse(JSON.stringify(movies));
+  moviesInMinutes.forEach((movie) => {
+    movie.duration = movie.duration.toString();
+    movie.duration = movie.duration.split(" ");
+  });
+  moviesInMinutes.forEach((movie) => {
+    if (movie.duration.length > 1) {
+      movie.duration[0] = movie.duration[0].replace("h", "") * 60;
+      movie.duration[1] = movie.duration[1].replace("min", "") * 1;
+      movie.duration = movie.duration[0] + movie.duration[1];
+    } else if (movie.duration[0].includes("h")) {
+      movie.duration = movie.duration[0].replace("h", "") * 60;
+    } else if (movie.duration[0].includes("min")) {
+      movie.duration = movie.duration[0].replace("min", "") * 1;
+    }
+    return movie.duration;
+  });
+  return moviesInMinutes;
+};
 
 // BONUS - Iteration 8: Best yearly rate average - Best yearly rate average
 
-const bestYearAvg = function(movies) {
-
+const bestYearAvg = function (movies) {
   if (movies.length === 0) return null;
-  
-  let filmYears = {}
-  
-  movies.forEach(movie => {
-    if (filmYears[movie.year]) {
-      filmYears[movie.year].push(movie)
-    } else {
-      filmYears[movie.year] = [movie]
-    }
-  })
 
-  let rates = []
-  let years = []
-  
+  let filmYears = {};
+
+  movies.forEach((movie) => {
+    if (filmYears[movie.year]) {
+      filmYears[movie.year].push(movie);
+    } else {
+      filmYears[movie.year] = [movie];
+    }
+  });
+
+  let rates = [];
+  let years = [];
+
   for (let prop in filmYears) {
-    rates.push(ratesAverage(filmYears[prop]))
-    years.push(prop)
+    rates.push(ratesAverage(filmYears[prop]));
+    years.push(prop);
   }
-  
-  let max = Math.max(...rates)
-  let winner = rates.indexOf(max)
-  
-  return `The best year was ${years[winner]} with an average rate of ${rates[winner]}`
-  
-}
+
+  let max = Math.max(...rates);
+  let winner = rates.indexOf(max);
+
+  return `The best year was ${years[winner]} with an average rate of ${rates[winner]}`;
+};
