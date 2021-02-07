0961845ab63e54231a50316efd5bab55e16913b4
Juan Falconi <juanfalconi86@gmail.com>
2021-02-07 01:12:49 +0100

    Fatou, j'ai réussi à faire l'iteration 2,3 et 5! La 4 est à moitié faite, et la 6e rien

--- src/movies.js
+++ src/movies.js
@@ -13,7 +13,7 @@ function getAllDirectors(moviesArr) {
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-console.log("hello");
+
 
 function howManyMovies(moviesArr) {
   let spielbergDramaMovies = moviesArr.filter(function (movie) {
@@ -26,13 +26,43 @@ function howManyMovies(moviesArr) {
 
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
 
-// thinking / coding / testing / redo
-// what is an average
+function ratesAverage (moviesArr){
+const averageRatesMovies = moviesArr.reduce(function (acc, movie){
+    return acc += movie.rate / moviesArr.length;
+}, 0) 
+const averageToTwoDecimals = averageRatesMovies.toFixed(2);
+const result = parseFloat(averageToTwoDecimals)
+return result;
+}
 
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 
+function dramaMoviesRate (moviesArr){
+const howManyDramaMovies = moviesArr.filter(function(movie){
+    return movie.genre.includes("Drama");
+} 
+) 
+ let numberOfDramaMovies = howManyDramaMovies.length;
+ return numberOfDramaMovies;
+
+} 
+
+
+
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
+
+function orderByYear (moviesArr) {
+const newMoviesArr = [...moviesArr];
+const movieYearsOrder = newMoviesArr.sort(function (a, b) {
+ if (a.year === b.year) return a.title.localeCompare(b.title)
+else
+  return a.year - b.year;
+});
+
+return movieYearsOrder;
+}
+
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 
