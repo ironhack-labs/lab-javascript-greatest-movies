47175340e200db6a081e6791d1da33a28177ef6e
Bea Avis <bea.avis.p@gmail.com>
2021-03-26 00:39:59 +0100

    Add Iteration 3 and correct indentation

--- src/movies.js
+++ src/movies.js
@@ -1,11 +1,24 @@
 // Iteration 1: All directors? - Get the array of all directors.
 let getAllDirectors = (arr) => {
-  let directors = arr.map((el) => {
-    return el.director;
-  });
-  return directors;
+    let directors = arr.map((el) => {
+        return el.director;
+    });
+    return directors;
 };
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 let howManyMovies = (arr) => {
     return arr.filter((el) => el.director === "Steven Spielberg" && el.genre.includes("Drama")).length;};
+
+// Iteration 3: All rates average - Get the average of all rates with 2 decimals
+let ratesAverage = (arr) => {
+    let average = arr.reduce((acc, movie) => {
+      if (movie.rate === "" || !movie.rate) {
+        acc += 0;
+      } else {
+        acc += movie.rate / arr.length;
+      }
+      return acc;
+    }, 0);
+    return parseFloat(average.toFixed(2));
+};
