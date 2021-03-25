1a236f7d12753633587eabf4d5b9dd083b9e9a40
l-masip <laia.masip.a@gmail.com>
2021-03-25 17:08:51 +0100

    iteration 2 complete

--- src/movies.js
+++ src/movies.js
@@ -1,9 +1,38 @@
 // Iteration 1: All directors? - Get the array of all directors.
+
+function getAllDirectors(moviesArr) {
+    let directorList = moviesArr.map(oneMovie => oneMovie.director);
+    return directorList;
+}
+
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 
+function howManyMovies(movieArr) {
+    let movieDirector = movieArr.filter(function(oneMovie) {
+        return oneMovie.director === `Steven Spielberg`;
+    });
+    let movieGenre = movieDirector.filter(function(oneMovie) {
+        return oneMovie.genre.includes(`Drama`); 
+    })
+    let movieNumber = movieGenre.length;
+    if (movieArr.length > 0) {
+        return movieNumber;
+    } else {
+        return 0;
+        }
+    }
+
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
+
+function ratesAverage() {
+
+};
+
+//In this iteration, we want to know the average rate of all of them and display it on the console. 
+//Create a ratesAverage() function that receives an array as a parameter and solves the challenge.
+//The rate must be returned rounded to 2 decimals!
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 
