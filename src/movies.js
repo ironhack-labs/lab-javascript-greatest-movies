734274c063c6325a12b9b72f03e0987c897f9ac5
Silinde87 <paurodriguezmolina@gmail.com>
2021-03-25 17:20:40 +0100

    Done iteration 6

--- src/movies.js
+++ src/movies.js
@@ -1,6 +1,8 @@
 // Iteration 1: All directors? - Get the array of all directors.
-// _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
 let getAllDirectors = (arr) => arr.map((e) => e.director);
+
+// _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
+//How could you "clean" a bit this array and make it unified (without duplicates)?
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 let howManyMovies = (arr) =>
@@ -15,31 +17,35 @@ let ratesAverage = (arr) => {
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 let dramaMoviesRate = (arr) => {
-    let filteredDramaArray = arr.filter(movie => movie.genre.includes("Drama"));
+	let filteredDramaArray = arr.filter((movie) => movie.genre.includes("Drama"));
 	let average = filteredDramaArray.reduce((acc, movie) => acc + movie.rate / filteredDramaArray.length, 0);
-	return Number(average.toFixed(2));   
-}
+	return Number(average.toFixed(2));
+};
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 let orderByYear = (arr) => {
-    let sortedByYear = arr.sort((a, b) => a.year - b.year);
+	let sortedByYear = arr.sort((a, b) => a.year - b.year);
 
-    let sortedByName = sortedByYear.sort((a,b) => {
-        if(a.year === b.year){
-            if(a.title === b.title){
-                return 0;
-            } else if(a.title < b.title){
-                return -1;
-            } else {
-                return 1;
-            }
-        }
-    });
-    return [...sortedByName];
-}
+	let sortedByName = sortedByYear.sort((a, b) => {
+		if (a.year === b.year) {
+			if (a.title === b.title) {
+				return 0;
+			} else if (a.title < b.title) {
+				return -1;
+			} else {
+				return 1;
+			}
+		}
+	});
+	return [...sortedByName];
+};
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-
+let orderAlphabetically = (arr) => {
+	let mySort = [...arr].sort((a, b) => a.title.localeCompare(b.title));
+	let myTitles = mySort.map((el) => el.title);
+	return myTitles.slice(0, 20);
+};
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 
