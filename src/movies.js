797dff34d92d7a90d63b01c374e944e2c90a33fe
Pablo del Barrio <pablodbb@gmail.com>
2021-03-25 17:51:00 +0100

    Commit of iteration 3 & 4

--- src/movies.js
+++ src/movies.js
@@ -17,10 +17,29 @@ function howManyMovies(movies){
 
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
 
+function  ratesAverage (movies) {
+  let rates = movies.reduce((acc, singleMovie) => {
+      acc += singleMovie.rate / movies.length;
+      return acc;
+  }, 0)
+  return rates.toFixed(2);
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 
+function dramaMoviesRate(movies){
+  let dramaMovies = movies.filter(movieDrama=>movieDrama.genre.includes("Drama"));
+  let ratesDrama = dramaMovies.reduce((acc, singleDramaMovie) =>{
+    acc += singleDramaMovie.rate / dramaMovies.length;
+    return acc;
+  },0)
+  return ratesDrama.toFixed(2);
+}
+
+
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
+
+
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 
