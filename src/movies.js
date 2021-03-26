b4b6de4f23a7c576ca8690511825dfe7b96c9c9e
QuimRobres <quim.robres@gmail.com>
2021-03-26 08:27:31 +0100

    some jasmine problems solved

--- src/movies.js
+++ src/movies.js
@@ -6,7 +6,6 @@ function getAllDirectors(arr) {
   return directorsArray;
 }
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
-
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 let howManyMovies = (movies) => {
   let dramaMovies = movies.filter((el) => {
@@ -14,72 +13,76 @@ let howManyMovies = (movies) => {
   });
   return dramaMovies.length;
 };
-// Iteration 3: All rates average - Get the average of all rates with 2 decimals
+howManyMovies(movies);
+// Iteration 3: All rates average - Get the average of all rates with 2 decimal
 function ratesAverage(arr) {
   if (arr.length === 0) {
     return 0;
-  } else {
-    let ratesValue = movies.map((el) => {
-      return el.rate;
-    });
-    let rateSum = ratesValue.reduce((acc, el) => {
-      acc += el / ratesValue.length;
-      return acc;
-    }, 0);
-    return Math.round(rateSum * 100) / 100;
   }
+  let ratesValue = arr.map((el) => {
+    return el.rate;
+  });
+  let ratesSum = ratesValue.reduce((acc, el) => {
+    return acc + el;
+  }, 0);
+  let totalAv = ratesSum / ratesValue.length;
+  return Math.round(totalAv * 100) / 100;
 }
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesRate(arr) {
-  let dramaMovies = movies.filter((el) => {
+  let dramaMovies = arr.filter((el) => {
     return el.genre.includes("Drama");
   });
-  if (dramaMovies === 0) {
+  if (dramaMovies === 0 || arr.length < 0) {
     return 0;
-  } else {
-    let dramaRate = dramaMovies.map((el) => {
-      return el.rate;
-    });
-    let dramaSum = dramaRate.reduce((acc, el) => {
-      acc += el / dramaRate.length;
-      return acc;
-    }, 0);
-    return Math.round(dramaSum * 100) / 100;
   }
+  let dramaRate = dramaMovies.map((el) => {
+    return el.rate;
+  });
+  let dramaSum = dramaRate.reduce((acc, el) => {
+    acc += el / dramaRate.length;
+    return acc;
+  }, 0);
+  return Math.round(dramaSum * 100) / 100;
 }
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear(arr) {
-    let movYear = arr.map((el) => {
-      return el;
-    });
-    let sortedYears = movYear.sort((el1, el2) => {
-      if (el1.year === el2.year) {
-        movies.sort();
-        return 0;
-      } else if (el1.year > el2.year) {
+  let movYear = arr.map((el) => {
+    return el;
+  });
+  let sortedYears = movYear.sort((el1, el2) => {
+    if (el1.year === el2.year) {
+      if (el1.title < el2.title) {
+        if (el1.title < el2.title) {
+          return -1;
+        }
+      } else if (el1.title > el2.title) {
         return 1;
-      } else {
-        return -1;
       }
-    });
-    return sortedYears;
-  }
+      return 0;
+    } else if (el1.year > el2.year) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+  return sortedYears;
+}
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically (arr) {
-    let movTitle = arr.map((el) => {
-      return el.title;
-    });
-    let sortedTitle = movTitle.sort((el1, el2) => {
-      if (el1 === el2) {
-        return 0;
-      } else if (el1 > el2) {
-        return 1;
-      } else {
-        return -1;
-      };
-    });
-    return sortedTitle.slice(0, 20);
-  };
+function orderAlphabetically(arr) {
+  let movTitle = arr.map((el) => {
+    return el.title;
+  });
+  let sortedTitle = movTitle.sort((el1, el2) => {
+    if (el1 === el2) {
+      return 0;
+    } else if (el1 > el2) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+  return sortedTitle.slice(0, 20);
+}
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-
 // BONUS - Iteration 8: Best yearly rate average - Best yearly rate average
