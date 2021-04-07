70c2aa67aba26fa67b6289ce4c4cd784c5a49813
elenamarsili <e.marsili89@gmail.com>
2021-04-07 16:00:33 +0200

    third version of js movie lab

--- src/movies.js
+++ src/movies.js
@@ -40,25 +40,23 @@ function ratesAverage(movies) {
 
     function dramaMoviesRate(movies) {
         const dramaMovies = movies.filter((movie) => { 
-            let drama = [];
-            if (movie.genre.indexOf("Drama") !==-1){
-                drama.push(movie); 
-                return drama 
-            } 
+            return movie.genre.indexOf("Drama") !==-1
         }); 
         const dramaRates = dramaMovies.map((drama)=>{
             let rates = drama.rate
             return rates
         }); const total= dramaRates.reduce(function(acc, rate) {
             return (acc + Number(rate))
-        },0); return Number((total/dramaMovies.length).toFixed(2))
+        },0); 
+        return dramaMovies.length ? Number((total/dramaMovies.length).toFixed(2)) : 0
     }
 
     console.log(dramaMoviesRate(movies))
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear(movies){
-        const orderedMovies = movies.sort(function compare (a, b) {
+        const newMovies = movies.map((movie)=> movie)
+        const orderedMovies = newMovies.sort(function compare (a, b) {
             if (a.year === b.year){
                 if (a.title< b.title){return -1;}
                 else if (a.title>b.title){return 1}
@@ -70,35 +68,39 @@ function orderByYear(movies){
 
 console.log(orderByYear(movies))
 
-
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically(movies){
-    const orderedMovies = movies.sort(function compare (a, b) {
+    const newMovies = movies.map((movie)=> movie)
+    const orderedMovies = newMovies.sort(function compare (a, b) {
         if (a.title< b.title){return -1;}
         else if (a.title>b.title){return 1}
         else {return 0}
-        }); if (orderedMovies.length<20){
-            return orderedMovies.title
-            } else if (orderedMovies.length>=20) {
-                const orderedMovieTitles = orderedMovies.map((movie)=>{
-                    return movie.title
-                }); return orderedMovieTitles.slice(0, 20)
-                }        
-            }
+        }); 
+        return orderedMovies.map((movie)=>{
+            return movie.title
+        }).slice(0,20)  
+    }
 
 console.log(orderAlphabetically(movies))
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes(movies){
-    const moviesWithMinutes = movies.map((movie)=>
-    {return {
-        title: movie.title,
-        year: movie.year,
-        director: movie.director,
-        duration: Number(movie.duration.replace("h", "").replace("min", "").trim().split(" ")[0]*60) + Number(movie.duration.replace("h", "").replace("min", "").trim().split(" ")[1]) ,
-        genre: movie.genre,
-        rate: movie.rate
-    }
-}); return moviesWithMinutes
+    const newMovies = movies.map((movie)=>{
+
+        let newMovie = Object.assign({}, movie);
+        let durationArray = movie.duration.split(" ");
+        let total = 0;
+
+        if (durationArray.length ===2){
+            total = (parseInt(durationArray[0])*60) + parseInt(durationArray[1]);
+        } else if (durationArray.length === 1 && durationArray[0].indexOf("h")>-1){
+            total = (parseInt(durationArray[0])*60);
+        } else if (durationArray.length === 1 && durationArray[0].indexOf("min")>-1){
+            total = parseInt(durationArray[0]);
+        }
+        newMovie.duration = total;
+        return newMovie;
+    }); return newMovies
 }
+
 console.log(turnHoursToMinutes(movies))
 // BONUS - Iteration 8: Best yearly rate average - Best yearly rate average
