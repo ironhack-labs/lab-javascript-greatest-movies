382f459f666285e988a5fa191cba02a9c8628b91
BlancaML <blancamorenodeluna@gmail.com>
2021-04-07 20:18:09 +0200

    iteration 1 y la segunda no pasa todos los tests de jasmine ;/

--- src/movies.js
+++ src/movies.js
@@ -1,7 +1,39 @@
 // Iteration 1: All directors? - Get the array of all directors.
+
+function getAllDirectors(movies) { 
+    const onlyDirectors = movies.map((movie) => {
+      return movie.director
+    })
+    return onlyDirectors
+ }
+
+console.log(getAllDirectors(movies))
+
+
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
 
+
+function getUniqueDirectors() {
+    const unique =  getAllDirectors(movies).filter((current, index ) => {
+      return getAllDirectors(movies).indexOf(current) === index;
+    })
+    return unique
+  }
+ 
+  console.log(getUniqueDirectors());
+
+
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
+
+function howManyMovies() {
+    const value = movies.filter((element ) => {
+        return element.director === 'Steven Spielberg' && element.genre.includes('Drama');
+    })
+    return value.length;
+ }
+ 
+ console.log(howManyMovies());
+ 
 
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
 
