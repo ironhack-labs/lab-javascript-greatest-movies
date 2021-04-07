a0aa9b28a829ac8e5aa9bd09e376c53046ba6f8f
BlancaML <blancamorenodeluna@gmail.com>
2021-04-08 01:51:39 +0200

    iteration 1 to 6 testing jasmine :/

--- src/movies.js
+++ src/movies.js
@@ -25,23 +25,80 @@ function getUniqueDirectors() {
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 
-function howManyMovies() {
+function howManyMovies(movies) {
     const value = movies.filter((element ) => {
         return element.director === 'Steven Spielberg' && element.genre.includes('Drama');
     })
     return value.length;
  }
  
- console.log(howManyMovies());
+ console.log(howManyMovies(movies));
  
 
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
 
+function ratesAverage(movies) {
+    const onlyRates = movies.map((elem) => {
+            return elem.rate
+        })
+    const result = onlyRates.reduce((a,b) => {
+          return a + b / movies.length;
+        })
+     
+        return result.toFixed(2);
+      }
+      
+console.log(ratesAverage(movies));
+
 // Iteration 4: Drama movies - Get the average of Drama Movies
+
+function dramaMoviesRate(rating) {
+    const drama = movies.filter((type) => {
+        return type.genre.includes('Drama');
+    })
+    const avg = drama.reduce((a,b) => {
+        return a + b / drama.length;
+    })
+
+    return avg.toFixed(2);
+}
+
+console.log(dramaMoviesRate(rating));
+
+
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 
+function orderByYear(movies) {
+    const dates = movies.map((movie) => {
+      return movie.year
+    })
+    const order = dates.sort((a, b) => {
+  return b - a;
+    })
+    return order.reverse()
+  }
+  
+  console.log(orderByYear(movies));
+
+
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
+
+function orderAlphabetically() {
+    const titles = movies.map((movie) => {
+        return movie.title
+      })
+    const place = titles.sort((a, b) => {
+      return a - b;
+    })
+    const ascend = place.reverse()
+    const print = ascend.slice(0,20);
+      return print
+    }
+    
+    console.log(orderAlphabetically());
+
+
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 
