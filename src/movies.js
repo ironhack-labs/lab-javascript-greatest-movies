7dedb52cf967fbb032291778bd78dbf1693b95fa
Sabela <sabela.ons2@gmail.com>
2021-04-11 11:21:36 +0200

    done el 65% +/-   pero iteracion 4 seguro que no esta bien...

--- src/movies.js
+++ src/movies.js
@@ -1,41 +1,60 @@
 // Iteration 1: All directors? - Get the array of all directors.
-const getAllDirectors =(movies) => {
-    const directorlist= movies.map((getAllDirectors)=>{
+const getAllDirectors = (movies) => {
+  const directorlist = movies.map((getAllDirectors) => {
     return getAllDirectors.director;
-    })
-    return directorlist
-    }
+  });
+  return directorlist;
+};
 
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 
 const howManyMovies = (movies) => {
-    const listOfSteveDramaMovies = movies.filter((directorNameList) => {
-    return directorNameList.director === "Steven Spielberg" && directorNameList.genre.includes("Drama")
-    });
-    
-    return listOfSteveDramaMovies.length;
-    }
+  const listOfSteveDramaMovies = movies.filter((directorNameList) => {
+    return (
+      directorNameList.director === "Steven Spielberg" &&
+      directorNameList.genre.includes("Drama")
+    );
+  });
 
+  return listOfSteveDramaMovies.length;
+};
 
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
 
-function ratesAverage (movies) {
-    if (movies === null || movies.length == 0) {
-        return 0;
-    }
-    const ratesSum = movies.reduce(function (acc, movie) {
-        if (movie.rate)
-        return acc + movie.rate; 
-        else return acc
-    }, 0);
-    const averageRate = ratesSum/movies.length;
-    const roundedAverage = Number(averageRate).toFixed(2);
-    return roundedAverage;
-} 
+function ratesAverage(movies) {
+  if (movies === null || movies.length == 0) {
+    return 0;
+  }
+  const ratesSum = movies.reduce(function (acc, movie) {
+    if (movie.rate) return acc + movie.rate;
+    else return acc;
+  }, 0);
+  const averageRate = ratesSum / movies.length;
+  const roundedAverage = Number(averageRate).toFixed(2);
+  return roundedAverage;
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
+
+const howManyDramaMovies = (movies) => {
+  const listDramaMovies = movies.filter((dramaList) => {
+    return dramaList.genre === "Drama" && dramaList.genre.includes("Drama");
+  });
+
+  return listDramaMovies;
+};
+
+function ratesAverageDrama(listDramaMovies) {
+  if (listDramaMovies === null || listDramaMovies.length == 0) {
+    return 0;
+  }
+  const ratesSum = listDramaMovies.reduce(function (acc, movie) {
+    if (listDramaMovies.rate) return acc + listDramaMovies.rate;
+    else return acc;
+  }, 0);
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 
