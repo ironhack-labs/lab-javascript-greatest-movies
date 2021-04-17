473abf71567f1c38e84a2c7c3951685d4d894ed4
Jordan Caley <jordancaley10@gmail.com>
2021-04-17 13:19:18 +0200

    Iterations 1-5 done with Stephane

--- src/movies.js
+++ src/movies.js
@@ -1,13 +1,106 @@
 // Iteration 1: All directors? - Get the array of all directors.
+
+// function getAllDirectors(movies) {
+
+//     return movies.map(function(movie){
+
+//         return movie.director
+
+//     })
+
+// }
+
+
+const getAllDirectors = (movies) => movies.map((movie) => movie.director);
+
+
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 
+function howManyMovies(movies) {
+    
+    let shortlist = movies.filter( function (movie) {
+
+         return movie.director === "Steven Spielberg"  && movie.genre.includes("Drama")
+        
+    })
+    
+    return shortlist.length;
+}
+
+
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
+
+function ratesAverage(movies) {
+
+    if (!movies.length){
+        return 0;
+    } 
+
+    let totalRates = movies.reduce( function (a,b) {
+        if (!b.rate){
+            b.rate=0
+        }
+
+        return a+=b.rate; 
+    },0);
+
+    return Math.round((totalRates/movies.length)*100)/100;
+};
+
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 
+function dramaMoviesRate(movies) {
+  let shortlist = movies.filter( function (movie) {
+
+    return movie.genre.includes("Drama")
+   
+})
+
+return ratesAverage(shortlist);
+}
+
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
+
+function orderByYear(movies) {
+  if (movies.length === 1) {
+    return movies;
+  }
+    
+  const clone = [...movies];
+  let yearOrdering = clone.sort(function (a, b) {
+      return a.year - b.year;
+    });
+
+
+    for (let i = 0; i < yearOrdering.length; i++) {
+      if (yearOrdering[i].year === yearOrdering[i+1].year) {
+
+        for(index = 0; index < yearOrdering[i].title.length ; index++ ){
+          if (yearOrdering[i].title[index] > yearOrdering[i+1].title[index]){
+              
+            [yearOrdering[i], yearOrdering[i+1]] = [yearOrdering[i+1], yearOrdering[i]];
+              console.log(yearOrdering);
+              break
+          } else if (yearOrdering[i].title < yearOrdering[i+1].title){
+              
+        
+              break
+          }else {
+              continue;
+          }
+
+      }
+    }
+
+    
+    
+    return clone;
+  }
+}
+
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 
