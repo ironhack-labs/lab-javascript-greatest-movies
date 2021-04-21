dd10d2ab5a0ddabfc8d3977585120fc6e6c634d8
Mauricio Munoz <ostergard@Mauricios-MacBook-Pro.local>
2021-04-20 19:07:00 -0500

    done

--- src/movies.js
+++ src/movies.js
@@ -1,11 +1,64 @@
 // Iteration 1: All directors? - Get the array of all directors.
+function getAllDirectors (movies){
+
+    let allDirectors = movies.map((theDirector) => {
+      return theDirector.director
+      
+      
+    });
+    return allDirectors
+    }
+    
+
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 
+const howManyMovies = (movies) => {
+
+let dramaM = movies.filter(theDrama =>
+  theDrama.genre.includes ("Drama") && theDrama.director.includes ("Steven Spielberg"))
+    
+
+return dramaM.length
+}
+
+
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
+// Hice este ejercicio con mis companeros, pero no lo entiendo en absoluto.
+
+function ratesAverage (arr){
+    let rates = (theRate) => {
+        if (!theRate.rate){
+            return 0;
+        }
+      return theRate.rate;
+    }
+    let average = arr.reduce((accu, current) => {
+        accu += rates(current) / arr.length;
+        return accu;
+    }, 0);
+    return Number(average.toFixed(2));
+}
+
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
+function dramaMoviesRate (arr){
+  let dramaRate = (theDrama) => {
+    if (!theDrama.rate){
+        return 0;
+    }
+  return theDrama.rate;
+}
+let dramaAverage = arr.reduce((accu, current) => {
+    accu += dramaRate(current) / arr.length;
+    return accu;
+}, 0);
+return Number(dramaAverage.toFixed(2));
+
+
+}
+
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 
