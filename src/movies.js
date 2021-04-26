1300002c51294eac907a8d2ebb0a7f9d088c8e07
Israel Zambrana <zambrana90@gmail.com>
2021-04-26 15:58:46 +0200

    ejercicio completo sin Bonus

--- src/movies.js
+++ src/movies.js
@@ -35,36 +35,81 @@ const howManyMovies = (nombres) => {
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
 
 const ratesAverage = (rates) => {
-  const calif = rates.map(function (rate) {
-    return rate.rate;
-  });
-  console.log(calif);
-  let suma = 0;
-  let media = 0;
-
-  for (i = 0; i < calif.length; i++) {
-    if (calif.length <= 0) {
-      return 0;
-    } else {
-      if (calif[i].length <= 0) {
-        continue;
-      } else {
-        suma += calif[i];
-      }
-    }
+  if (!Array.isArray(rates) || rates.length <= 0) {
+    return 0;
   }
-  media = suma / calif.length;
-  media = media.toFixed(2);
-  media = parseFloat(media);
-  return media;
+  const calif = rates.reduce(function (acc, year) {
+    if (year.rate) {
+      return acc + year.rate;
+    } else {
+      return acc;
+    }
+  }, 0);
+
+  return parseFloat((calif / rates.length).toFixed(2));
 };
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 
+const dramaMoviesRate = (dramasMov) => {
+  const dramaPelis = dramasMov.filter(function (dramaMov) {
+    return dramaMov.genre.includes("Drama");
+  });
+  return ratesAverage(dramaPelis);
+};
+
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
+
+const orderByYear = (array) => {
+  const newSortArr = [...array];
+  newSortArr.sort(function (a, b) {
+    if (a.year < b.year) {
+      return -1;
+    } else if (a.year > b.year) {
+      return 1;
+    } else {
+      if (a.title < b.title) {
+        return -1;
+      } else if (a.title > b.title) {
+        return 1;
+      } else {
+        return 0;
+      }
+    }
+  });
+  return newSortArr;
+};
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 
+const orderAlphabetically = (nombres) => {
+  const arrOrdAlp = [...nombres];
+  arrOrdAlp.sort(function (a, b) {
+    if (a.title < b.title) {
+      return -1;
+    } else if (a.title > b.title) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+
+  const titulos = arrOrdAlp.map(function (nombre) {
+    return nombre.title;
+  });
+  if (titulos < 20) {
+    return titulos;
+  } else {
+    return titulos.slice(0, 20);
+  }
+};
+
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 
+const turnHoursToMinutes = (pelis) => {};
+
 // BONUS - Iteration 8: Best yearly rate average - Best yearly rate average
+
+const bestYearAvg = (mejor) => {
+  return "The best year was <YEAR> with an average rate of <RATE>";
+};
