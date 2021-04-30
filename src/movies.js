b772f10cfa866ba86ee239731f4129ef0137af62
José Carneiro <jose.carneiro@mercedes-benz.io>
2021-04-30 18:24:38 +0100

    Add missing function declarations, workflow file, fixes test messages

--- src/movies.js
+++ src/movies.js
@@ -1,27 +1,38 @@
 // Iteration 1: All directors? - Get the array of all directors.
-// _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
+// _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
+// How could you "clean" a bit this array and make it unified (without duplicates)?
+function getAllDirectors() {}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
+function howManyMovies() {}
 
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
+function ratesAverage() {}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
+function dramaMoviesRate() {}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
+function orderByYear() {}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
+function orderAlphabetically() {}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
+function turnHoursToMinutes() {}
 
 // BONUS - Iteration 8: Best yearly rate average - Best yearly rate average
+function bestYearAvg() {}
 
-module.exports = {
-  getAllDirectors,
-  howManyMovies,
-  ratesAverage,
-  dramaMoviesRate,
-  orderByYear,
-  orderAlphabetically,
-  turnHoursToMinutes,
-  bestYearAvg,
-};
+if (typeof module !== 'undefined') {
+  module.exports = {
+    getAllDirectors,
+    howManyMovies,
+    ratesAverage,
+    dramaMoviesRate,
+    orderByYear,
+    orderAlphabetically,
+    turnHoursToMinutes,
+    bestYearAvg
+  };
+}
