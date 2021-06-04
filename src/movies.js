b5a8da2d344c44ab9b2f8fb7f877a4b90d33d840
Margarida Pereira <margarida.arq@gmail.com>
2021-06-04 17:12:36 +0100

    Solution till 4

--- src/movies.js
+++ src/movies.js
@@ -1,16 +1,105 @@
+const movies = [
+  {
+    title: 'The Shawshank Redemption',
+    year: 1994,
+    director: 'Frank Darabont',
+    duration: '2h 22min',
+    genre: ['Crime', 'Drama'],
+    score: 9.3
+  },
+  {
+    title: 'The Godfather',
+    year: 1972,
+    director: 'Francis Ford Coppola',
+    duration: '2h 55min',
+    genre: ['Crime', 'Drama'],
+    score: 9.2
+  },
+  {
+    title: 'Schindler"s List',
+    year: 1993,
+    director: 'Steven Spielberg',
+    duration: '3h 15min',
+    genre: ['Biography', 'Drama', 'History'],
+    score: 8.9
+  }
+]
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors() {}
+const getAllDirectors = (movies) => {
+
+  return movies.map((movie) => movie.director);
+};
+
+
+console.log(getAllDirectors(movies));
+
+
+
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies() {}
+// const howManyMovies = movies.filter((movie) => {
+// return movie.director === "Steven Spielberg";
+// });
+
+
+
+const howManyMovies = (movies) => {
+  return movies.filter((movie) => movie.director === "Steven Spielberg" && movie.genre.includes("Drama")).length;
+};
+
+console.log(howManyMovies(movies));
+
+
+
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage() {}
+function scoresAverage(movies) {
+  if(movies.length === 0){
+    return 0;
+  }
+const sumScore = movies.reduce((accumulator, currentValue) => {
+  if(currentValue.score){
+    return accumulator + currentValue.score;
+  } else {
+    return accumulator;
+  }
+  }, 0); 
+  
+const avgScore = sumScore/ movies.length;
+  const finalScoreAvg = parseFloat(avgScore.toFixed(2));//when you do fixed 2 
+  return finalScoreAvg;
+};
+
+console.log(scoresAverage(movies));
+
+// const scoresAverage = (movies) => {
+//  movies.reduce((accumulator, currentValue) => {
+//   const totalScore = accumulator + currentValue.score;
+//   return totalScore/ movies.length;
+//   }, 0);
+
+// }; 
+
+
+
+
+
+console.log(scoresAverage(movies));
+  
+
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore() {}
+function dramaMoviesScore(movies) {
+  const dramaMovies = movies.filter((movie) => {
+    return movie.genre.includes("Drama");
+  });
+  return scoresAverage(dramaMovies);
+}
+
+console.log(dramaMoviesScore(movies));
+
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear() {}
