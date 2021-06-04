8be2ebddc8e49b9c6df6082881f785a438e1022c
Benjamin Bertelsen <benjaminbertelsen@gmail.com>
2021-06-04 21:16:25 +0100

    update

--- src/movies.js
+++ src/movies.js
@@ -1,3 +1,33 @@
+//Copy of a few movies
+
+const testmovies = [
+  {
+    title: 'The Shawshank Redemption',
+    year: 1994,
+    director: 'Frank Darabont',
+    duration: '2h 22min',
+    genre: ['Crime', 'Drama'],
+    score: 9.3
+  },
+  {
+    title: 'The Godfather',
+    year: 1972,
+    director: 'Francis Ford Coppola',
+    duration: '2h 55min',
+    genre: ['Crime', 'Drama'],
+    score: 9.2
+  },
+  {
+    title: 'The Godfather: Part II',
+    year: 1974,
+    director: 'Francis Ford Coppola',
+    duration: '3h 22min',
+    genre: ['Crime', 'Drama'],
+    score: 9
+  }
+];
+
+
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 
@@ -7,21 +37,46 @@ const movies = require("./data");
 const getAllDirectors = (movies) => {
   const directors = movies.map((movie) => movie.director);
   console.log(directors);
-  return directors;
+    return directors;
 };
 getAllDirectors(movies);
 
-
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies() {}
-//hint check if a string is inside an array
-//genre.includes("drama")
+  const howManyMovies = (movies) => { 
+    return movies.filter((movie) => {
+      return movie.director == "Steven Spielberg" && movie.genre.includes("Drama");
+    });
+  }
+   console.log(howManyMovies(movies).length);
+
+//Iteration 2.B - get only Steven Spielberg movies
+function howManyMovies_SS(movies) {
+  const result = movies.filter((movie) => {
+    movie.director === "Steven Spielberg"
+  });
+}
+console.log(howManyMovies_SS(movies));
+
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage() {}
+function scoresAverage(movies) {
+const sum = movies.reduce((accumulator, currentValue) => { //remember, we must have two params for .reduce function
+  return accumulator + currentValue.score; //get the specific part of the obj .score as the .reduce function loops through
+}, 0); // 0 because we are summing an array of object, and we start the index at 0 for our accumulator (it's always 0)
+
+const average = sum / movies.length; //this is to get the average
+return Number(average.toFixed(2)); //and ensuring the average is represented as a whole integer
+}
+// console.log(sum);
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore() {}
+function dramaMoviesScore(movies) {
+    const dramaMovies = movies.filter((movie) => {
+      return movie.genre.includes("Drama");
+    });
+    return scoresAverage(movies);
+}
+console.log(scoresAverage(movies));
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear() {}
