036675254a287d8c2e1185a259e631ffe9466632
kintrp <philipp.kintrup@googlemail.com>
2021-06-06 20:18:26 +0200

    lab solved

--- src/movies.js
+++ src/movies.js
@@ -1,13 +1,105 @@
+/////5/////
+const movies = [
+  {
+    title: 'The Shawshank Redemption',
+    year: 1994,
+    director: 'Frank Darabont',
+    duration: '2h 22min',
+    genre: ['Crime', 'Drama'],
+    score: 9.3
+  },
+  {
+    title: 'The Godfather',
+    year: 1972,
+    director: 'Francis Ford Coppola',
+    duration: '2h 55min',
+    genre: ['Crime', 'Drama'],
+    score: 9.2
+  },
+  {
+    title: 'The Godfather: Part II',
+    year: 1974,
+    director: 'Francis Ford Coppola',
+    duration: '3h 22min',
+    genre: ['Crime', 'Drama'],
+    score: 9
+  },
+  {
+    title: 'The Dark Knight',
+    year: 2008,
+    director: 'Christopher Nolan',
+    duration: '2h 32min',
+    genre: ['Action', 'Crime', 'Drama', 'Thriller'],
+    score: 9
+  },
+  {
+    title: '12 Angry Men',
+    year: 1957,
+    director: 'Sidney Lumet',
+    duration: '1h 36min',
+    genre: ['Crime', 'Drama'],
+    score: 8.9
+  },
+  {
+    title: 'Schindler"s List',
+    year: 1993,
+    director: 'Steven Spielberg',
+    duration: '3h 15min',
+    genre: ['Biography', 'Drama', 'History'],
+    score: 8.9
+  },
+  {
+    title: 'Pulp Fiction',
+    year: 1994,
+    director: 'Quentin Tarantino',
+    duration: '2h 34min',
+    genre: ['Crime', 'Drama'],
+    score: 8.9
+  }
+]
+
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors() {}
+
+function getAllDirectors (movies) { 
+  return movies.map(function(movie) {
+    return movie.director
+  } 
+  
+  
+  )}
+ 
+console.log(getAllDirectors(movies));
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies() {}
+
+function howManyMovies(arr) {
+ 
+  const movies = arr.filter(function(movie) {
+    return movie.director === 'Steven Spielberg' && movie.genre.includes('Drama')
+
+  })
+
+  return movies.length
+}
+
+// Filter function
+// How many drama movies?
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
+
 function scoresAverage() {}
+
+//movies.reduce(function (accumulator , currentValue))
+
+
+
+
+// reduce function
+// Score returned rounded to two decimals 
+
+
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesScore() {}
@@ -40,3 +132,6 @@ if (typeof module !== 'undefined') {
     bestYearAvg,
   };
 }
+
+
+
