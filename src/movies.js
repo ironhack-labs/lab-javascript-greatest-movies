0624379c4bfd85aa3beeb4c298d16b5da8b5b487
Pablo Molleda <pmolledadelrio@gmail.com>
2021-06-07 17:05:29 +0200

    semi-done

--- src/movies.js
+++ src/movies.js
@@ -1,28 +1,51 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors() {}
+function getAllDirectors(movies) {
+
+  const allDirectors = movies.map(eachDirector => {
+    const directorsArray = eachDirector.director
+    return directorsArray
+  })
+
+  return allDirectors
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies() {}
+function howManyMovies(movies) {
+
+  const onlySpil = movies.filter (eachFilm => {
+    if (eachFilm.director === 'Steven Spielberg' && eachFilm.genre.includes('Drama')) {
+      return true
+  }
+  })
+    return onlySpil.length
+  } 
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage() {}
+function scoresAverage(movies) { 
+
+  const argValue = movies.reduce((acc, avg) => {
+    return (acc + avg.score)
+  }, 0)
+  const finalAvr = ((argValue/movies.length)).toFixed(2)
+  return parseFloat(finalAvr)
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore() {}
+function dramaMoviesScore() { }
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear() {}
+function orderByYear() { }
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically() {}
+function orderAlphabetically() { }
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-function turnHoursToMinutes() {}
+function turnHoursToMinutes() { }
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
-function bestYearAvg() {}
+function bestYearAvg() { }
 
 
 
