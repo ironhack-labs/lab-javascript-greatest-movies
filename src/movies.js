ba0b447af09a74891ab05bfa6eee959cb4154bc8
Mario P. Cardenas <15961362+Mario-paul@users.noreply.github.com>
2021-06-14 19:09:32 -0400

    finish iteration 5, add one line versions to reinforce knowledge

--- src/movies.js
+++ src/movies.js
@@ -2,7 +2,7 @@
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
 
-// Arrow function version
+// ONE LINE Arrow function version
 const getAllDirectors = (arr) => arr.map(arrayElement => arrayElement.director)
 
 // Traditional function version
@@ -14,37 +14,52 @@ const getAllDirectors = (arr) => arr.map(arrayElement => arrayElement.director)
 //} //The getAllDirectors() function will receive (array), the .map() function will be applied to array, and arrayElement function will 
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(arr) {
-  let tempArray = arr.filter(drama => drama.director === 'Steven Spielberg' && drama.genre.includes('Drama'))
-  return tempArray.length
-}
+// LONG VERSION
+// function howManyMovies(arr) {
+//   let tempArray = arr.filter(drama => drama.director === 'Steven Spielberg' && drama.genre.includes('Drama'))
+//   return tempArray.length
+// }
+// ONE LINE VERSION
+const howManyMovies = (arr) => arr.filter(drama => drama.director === 'Steven Spielberg' && drama.genre.includes('Drama')).length
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(arr) {
-  let totalScore = arr.reduce((acc, val) => {
-    //console.log(number.score)
-    return acc + val.score;
+// Long VERSION
+// function scoresAverage(arr) {
+//   let totalScore = arr.reduce((acc, val) => {
+//     //console.log(number.score)
+//     return acc + val.score;
+//   }, 0);
+//   let average = totalScore / arr.length;
+//   // console.log(arr) // debug line
+//   return Number(average.toFixed(2))
+//   //return Math.floor(average * 100) / 100 //alternate method of 2 decimal rounding
+// }
+// Cleaner code VERSION
+const scoresAverage = (arr) => {
+  let sum = arr.reduce((acc, val) => {
+    return acc + val.score
   }, 0);
-
-  let average = totalScore / arr.length;
+  let average = sum / arr.length
   return Number(average.toFixed(2))
-  //return Math.floor(average * 100) / 100
 }
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(arr) {
-  let dramaMovies = array.filter((eachMovie) => eachMovie.genre.includes('Drama'))
-  return scoresAverage(dramaMovies)
+const dramaMoviesScore = (arr) => {
+  let dramaArray = arr.filter((eachMovie) => eachMovie.genre.includes('Drama'))
+  // return dramaArray // debug line
+  // console.log(dramaArray) debug line
+  return scoresAverage(dramaArray)
 }
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 // function orderByYear(arr) {
-//   return sortArr.sort((a,b)=>{
-
+//   return arr.sort((a,b)=>{
+// return a.year- b.year
 //   })
 // }
+// One line VERSION
+const orderByYear = (array) => array.sort((a, b) => a.year - b.year)
 
-// Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 // function orderAlphabetically(array) {
 //   let variable = array.sort((a, b) => {
@@ -59,7 +74,7 @@ function dramaMoviesScore(arr) {
 // return variable.slice(0,20)
 // }
 
-const orderAlphabetically = (array) => array.sort((a,b)=> a.title.localeCompare(b.title)).slice(0,20)
+const orderAlphabetically = (array) => array.sort((a, b) => a.title.localeCompare(b.title)).slice(0, 20)
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes() {}
