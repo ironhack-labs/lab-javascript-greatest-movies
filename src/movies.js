9b0b032ba219fb04503fc00d7cc2630e6237d2b6
Chenjia Li <cligrenet@gmail.com>
2021-07-02 23:17:59 +0200

    Basic iteration done without bonus

--- src/movies.js
+++ src/movies.js
@@ -1,30 +1,420 @@
 // Iteration 1: All directors? - Get the array of all directors.
+
+const movies = [
+  {
+    title: 'The Shawshank Redemption',
+    year: 1994,
+    director: 'Frank Darabont',
+    duration: '2h 22min',
+    genre: ['Crime', 'Drama'],
+    score: 9.3
+  },
+  {
+    title: 'The Godfather',
+    year: 1972,
+    director: 'Francis Ford Coppola',
+    duration: '2h 55min',
+    genre: ['Crime', 'Drama'],
+    score: 9.2
+  }
+];
+
+function getAllDirectors(movies) {
+  return movies.map((ele) => ele.director);
+}
+//console.log(getAllDirectors(movies));
+getAllDirectors(movies);
+
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors() {}
+
+//function getAllDirectors() {}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies() {}
+
+const movies2 = [
+  {
+    title: 'The Shawshank Redemption',
+    year: 1994,
+    director: 'Frank Darabont',
+    duration: '2h 22min',
+    genre: ['Crime', 'Drama'],
+    score: 9.3
+  },
+  {
+    title: 'The Godfather',
+    year: 1972,
+    director: 'Francis Ford Coppola',
+    duration: '2h 55min',
+    genre: ['Crime', 'Drama'],
+    score: 9.2
+  },
+  {
+    title: 'The Godfather: Part II',
+    year: 1974,
+    director: 'Francis Ford Coppola',
+    duration: '3h 22min',
+    genre: ['Crime', 'Drama'],
+    score: 9
+  },
+  {
+    title: 'The Dark Knight',
+    year: 2008,
+    director: 'Christopher Nolan',
+    duration: '2h 32min',
+    genre: ['Action', 'Crime', 'Drama', 'Thriller'],
+    score: 9
+  },
+  {
+    title: '12 Angry Men',
+    year: 1957,
+    director: 'Sidney Lumet',
+    duration: '1h 36min',
+    genre: ['Crime', 'Drama'],
+    score: 8.9
+  },
+  {
+    title: 'Schindler"s List',
+    year: 1993,
+    director: 'Steven Spielberg',
+    duration: '3h 15min',
+    genre: ['Biography', 'Drama', 'History'],
+    score: 8.9
+  },
+  {
+    title: 'Pulp Fiction',
+    year: 1994,
+    director: 'Quentin Tarantino',
+    duration: '2h 34min',
+    genre: ['Crime', 'Drama'],
+    score: 8.9
+  }
+];
+
+function howManyMovies(movies) {
+  if (!movies.length) return 0;
+
+  let moviesOfSpielberg = movies.filter((movie) => {
+    if (movie.director === 'Steven Spielberg' && movie.genre.includes('Drama'))
+      return movie;
+  });
+  return moviesOfSpielberg.length;
+}
+//console.log(howManyMovies(movies2));
+howManyMovies(movies2);
+
+//howManyMovies(movies);
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage() {}
+const movies3 = [
+  {
+    title: 'The Shawshank Redemption',
+    year: 1994,
+    director: 'Frank Darabont',
+    duration: '2h 22min',
+    genre: ['Crime', 'Drama'],
+    score: 9.3
+  },
+  {
+    title: 'The Godfather',
+    year: 1972,
+    director: 'Francis Ford Coppola',
+    duration: '2h 55min',
+    genre: ['Crime', 'Drama'],
+    score: 9.2
+  },
+  {
+    title: 'The Godfather: Part II',
+    year: 1974,
+    director: 'Francis Ford Coppola',
+    duration: '3h 22min',
+    genre: ['Crime', 'Drama'],
+    score: 9
+  }
+];
+
+function scoresAverage(movies) {
+  if (!movies.length) return 0;
+
+  const totalScore = movies.reduce((acc, movie) => {
+    if (movie.score === '' || movie.score === undefined) {
+      return acc;
+    } else {
+      return (acc += movie.score);
+    }
+  }, 0);
+  console.log(totalScore);
+
+  const aveScore = totalScore / movies.length;
+  return Number(aveScore.toFixed(2));
+}
+//scoresAverage(movies3);
+scoresAverage(movies3);
+//console.log(scoresAverage(movies3));
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore() {}
+const movies4 = [
+  {
+    title: 'The Shawshank Redemption',
+    year: 1994,
+    director: 'Frank Darabont',
+    duration: '2h 22min',
+    genre: ['Crime', 'Drama'],
+    score: 9.3
+  },
+  {
+    title: 'The Godfather',
+    year: 1972,
+    director: 'Francis Ford Coppola',
+    duration: '2h 55min',
+    genre: ['Crime', 'Drama'],
+    score: 9.2
+  },
+  {
+    title: 'The Godfather: Part II',
+    year: 1974,
+    director: 'Francis Ford Coppola',
+    duration: '3h 22min',
+    genre: ['Crime', 'Drama'],
+    score: 9
+  }
+];
+
+function dramaMoviesScore(array) {
+  if (!array.length) return 0;
+  const dramaMovies = array.filter((ele) => ele.genre.includes('Drama'));
+  let aveDramaScore =
+    dramaMovies.length === 0
+      ? 0
+      : dramaMovies.reduce(
+          (acc, mov) => (!dramaMovies.length ? 0 : (acc += mov.score)),
+          0
+        ) / dramaMovies.length;
+  //const aveDramaScore = totalDramaScore / dramaMovies.length;
+  return Number(aveDramaScore.toFixed(2));
+}
+dramaMoviesScore(movies4);
+//console.log(dramaMoviesScore(movies4));
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear() {}
+const movies5 = [
+  {
+    title: 'The Shawshank Redemption',
+    year: 1972,
+    director: 'Frank Darabont',
+    duration: '2h 22min',
+    genre: ['Crime', 'Drama'],
+    score: 9.3
+  },
+  {
+    title: 'The Godfather',
+    year: 1972,
+    director: 'Francis Ford Coppola',
+    duration: '2h 55min',
+    genre: ['Crime', 'Drama'],
+    score: 9.2
+  },
+  {
+    title: 'The Godfather: Part II',
+    year: 1972,
+    director: 'Francis Ford Coppola',
+    duration: '3h 22min',
+    genre: ['Crime', 'Drama'],
+    score: 9
+  }
+];
+function orderByYear(movies) {
+  return [...movies].sort((a, b) => {
+    if (a.year !== b.year) {
+      return a.year - b.year;
+    } else {
+      return a.title.localeCompare(b.title);
+    }
+  });
+}
+orderByYear(movies5);
+//console.log(orderByYear(movies5));
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically() {}
+const movies6 = [
+  {
+    title: 'The Shawshank Redemption',
+    year: 1994,
+    director: 'Frank Darabont',
+    duration: '2h 22min',
+    genre: ['Crime', 'Drama'],
+    score: 9.3
+  },
+  {
+    title: 'The Godfather',
+    year: 1972,
+    director: 'Francis Ford Coppola',
+    duration: '2h 55min',
+    genre: ['Crime', 'Drama'],
+    score: 9.2
+  },
+  {
+    title: 'The Godfather: Part II',
+    year: 1974,
+    director: 'Francis Ford Coppola',
+    duration: '3h 22min',
+    genre: ['Crime', 'Drama'],
+    score: 9
+  },
+  {
+    title: 'The Dark Knight',
+    year: 2008,
+    director: 'Christopher Nolan',
+    duration: '2h 32min',
+    genre: ['Action', 'Crime', 'Drama', 'Thriller'],
+    score: 9
+  },
+  {
+    title: '12 Angry Men',
+    year: 1957,
+    director: 'Sidney Lumet',
+    duration: '1h 36min',
+    genre: ['Crime', 'Drama'],
+    score: 8.9
+  },
+  {
+    title: 'Schindler"s List',
+    year: 1993,
+    director: 'Steven Spielberg',
+    duration: '3h 15min',
+    genre: ['Biography', 'Drama', 'History'],
+    score: 8.9
+  },
+  {
+    title: 'Pulp Fiction',
+    year: 1994,
+    director: 'Quentin Tarantino',
+    duration: '2h 34min',
+    genre: ['Crime', 'Drama'],
+    score: 8.9
+  },
+  {
+    title: 'The Lord of the Rings: The Return of the King',
+    year: 2003,
+    director: 'Peter Jackson',
+    duration: '3h 21min',
+    genre: ['Adventure', 'Drama', 'Fantasy'],
+    score: 8.9
+  },
+  {
+    title: 'Il buono, il brutto, il cattivo',
+    year: 1966,
+    director: 'Sergio Leone',
+    duration: '3h 2min',
+    genre: ['Western'],
+    score: 8.9
+  },
+  {
+    title: 'Fight Club',
+    year: 1999,
+    director: 'David Fincher',
+    duration: '2h 19min',
+    genre: ['Drama'],
+    score: 8.8
+  },
+  {
+    title: 'The Lord of the Rings: The Fellowship of the Ring',
+    year: 2001,
+    director: 'Peter Jackson',
+    duration: '2h 58min',
+    genre: ['Adventure', 'Drama', 'Fantasy'],
+    score: 8.8
+  },
+  {
+    title: 'Forrest Gump',
+    year: 1994,
+    director: 'Robert Zemeckis',
+    duration: '2h 22min',
+    genre: ['Comedy', 'Drama', 'Romance'],
+    score: 8.8
+  },
+  {
+    title: 'Star Wars: Episode V - The Empire Strikes Back',
+    year: 1980,
+    director: 'Irvin Kershner',
+    duration: '2h 4min',
+    genre: ['Action', 'Adventure', 'Fantasy', 'Sci-Fi'],
+    score: 8.8
+  },
+  {
+    title: 'Inception',
+    year: 2010,
+    director: 'Christopher Nolan',
+    duration: '2h 28min',
+    genre: ['Action', 'Adventure', 'Sci-Fi', 'Thriller'],
+    score: 8.8
+  },
+  {
+    title: 'The Lord of the Rings: The Two Towers',
+    year: 2002,
+    director: 'Peter Jackson',
+    duration: '2h 59min',
+    genre: ['Adventure', 'Drama', 'Fantasy'],
+    score: 8.7
+  },
+  {
+    title: 'One Flew Over the Cuckoo"s Nest',
+    year: 1975,
+    director: 'Milos Forman',
+    duration: '2h 13min',
+    genre: ['Drama'],
+    score: 8.7
+  },
+  {
+    title: 'Goodfellas',
+    year: 1990,
+    director: 'Martin Scorsese',
+    duration: '2h 26min',
+    genre: ['Crime', 'Drama'],
+    score: 8.7
+  },
+  {
+    title: 'The Matrix',
+    year: 1999,
+    director: 'Lana Wachowski',
+    duration: '2h 16min',
+    genre: ['Action', 'Sci-Fi'],
+    score: 8.7
+  },
+  {
+    title: 'Shichinin no samurai',
+    year: 1954,
+    director: 'Akira Kurosawa',
+    duration: '3h 27min',
+    genre: ['Adventure', 'Drama'],
+    score: 8.7
+  },
+  {
+    title: 'Star Wars',
+    year: 1977,
+    director: 'George Lucas',
+    duration: '2h 1min',
+    genre: ['Action', 'Adventure', 'Fantasy', 'Sci-Fi'],
+    score: 8.7
+  }
+];
+
+// 1.functions order is crutial, should sort first, then slice
+// 2.tricks of slice(start, end)!! no need to set condition if arr.length > 20 else
+
+function orderAlphabetically(array) {
+  let movieTitles = array.map((mov) => mov.title);
+  movieTitles.sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+  return movieTitles.slice(0, 20);
+}
+orderAlphabetically(movies6);
+//console.log(orderAlphabetically(movies6));
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes() {}
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
 function bestYearAvg() {}
-
-
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
@@ -37,6 +427,6 @@ if (typeof module !== 'undefined') {
     orderByYear,
     orderAlphabetically,
     turnHoursToMinutes,
-    bestYearAvg,
+    bestYearAvg
   };
 }
