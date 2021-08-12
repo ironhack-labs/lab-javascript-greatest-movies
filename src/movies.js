90e4ae6937d1283a75356dc63e60eac97caa7e5e
alejandro <ftvalex@hotmail.com>
2021-08-12 18:23:42 +0100

    hasta Iteracion 4
    hecho hasta la iteracion nº4 , teniendo un error en la iteracion nº3 no sabiendo usar el metodo fixed para que me devuelva dos decimales .

--- src/movies.js
+++ src/movies.js
@@ -1,16 +1,68 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
+
+const movies = require("./data");
+
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors() {}
+function getAllDirectors(movies){
+  const directors = movies.map(element => element.director);
+  return directors;
+}
+  /*
+  for(let i = 0 ; i < array.length; i++){
+    if(array.directors === true){
+      directorsArray.push(array.length)
+    }
+  }
+  return directorsArray
+}; 
+*/
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies() {}
+function howManyMovies(movies) {
+  return movies.filter(
+    element =>
+      element.director === 'Steven Spielberg' &&
+      element.genre.includes('Drama')
+  ).length;
+}
+
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage() {}
+function scoresAverage(movies) {
+  if(movies.length === 0){
+    return 0;
+  }
+  const avgScore = movies.reduce((acc,element)=>{
+    if(element.score){
+      return acc + element.score;
+    }else{
+      return acc;
+    }
+  },
+    0);
 
+    let avgScoreDecimals = (avgScore/movies.length);
+    return avgScoreDecimals;
+  }
+   function avgScoreDecimals(number){
+     return Math.round(number/100)*100;   // Metodo fixed
+   }
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore() {}
+function dramaMoviesScore(movies) {
+  if(movies.length===0){
+    return 0;
+  }
+  const avgDrama = movies.reduce((acc,element)=>{
+      if(element.genre === 'drama'){
+        return acc + element.length;
+      }else{
+        return acc;
+      }
+  },0);
+    let avgDramaReal = (avgDrama/movies.length);
+    return avgDramaReal;                           //devolver como un solo elemento de array.
+} 
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear() {}
