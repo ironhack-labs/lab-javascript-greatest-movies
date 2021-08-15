e929a8ffb5bbc9f7b8fecb2e038a38b1bccd54d1
dariohirsch <darohirsch@gmail.com>
2021-08-15 15:47:52 +0200

    lab terminado

--- src/movies.js
+++ src/movies.js
@@ -32,13 +32,71 @@ function scoresAverage(movies) {
 }
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore() {}
+
+function dramaMoviesScore(movies) {
+  const dramaMovies = movies.filter((movie) => {
+    return movie.genre.includes('Drama');
+  });
+
+  if (!dramaMovies.length) {
+    return 0;
+  }
+
+  const sumDramaMoviesScore = dramaMovies.reduce((sum, movie) => {
+    return sum + movie.score;
+  }, 0);
+
+  const dramaMoviesScoreAverage = sumDramaMoviesScore / dramaMovies.length;
+
+  return Math.round(dramaMoviesScoreAverage * 100) / 100;
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear() {}
+function orderByYear(movies) {
+  const sortedMoviesByYear = movies.sort((movie1, movie2) => {
+    const movie1Year = movie1.year;
+    const movie2Year = movie2.year;
+
+    if (movie1Year > movie2Year) {
+      return 1;
+    } else if (movie1Year < movie2Year) {
+      return -1;
+    } else {
+      if (movie1.tittle > movie2.tittle) {
+        return 1;
+      } else {
+        return -1;
+      }
+    }
+  });
+  const newSortedMoviesByYear = JSON.parse(JSON.stringify(sortedMoviesByYear));
+  return newSortedMoviesByYear;
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically() {}
+function orderAlphabetically(movies) {
+  let newArray = JSON.parse(JSON.stringify(movies));
+
+  newArray.sort((movie1, movie2) => {
+    if (movie1.title > movie2.title) {
+      return 1;
+    } else if (movie1.title < movie2.title) {
+      return -1;
+    } else {
+      return 0;
+    }
+  });
+
+  let onlyNames = newArray.map((movie) => {
+    return movie.title;
+  });
+
+  if (onlyNames.length > 20) {
+    return onlyNames.slice(0, 20);
+  }
+
+  return onlyNames;
+}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes() {}
