2cd0ae172f4784063cb4f443770ed6faa937f7dc
thomaskno <thomas.kno@yahoo.com>
2021-08-15 12:03:54 -0400

    Added Iterations 1, 2, 3, and 4

--- src/movies.js
+++ src/movies.js
@@ -1,16 +1,46 @@
+let movies = require("./data");
+
+
+
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors() {}
+const getAllDirectors = movies.map(function (movie) {
+  return movie.director;
+});
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies() {}
+const howManyMovies = movies.filter(function (movie) {
+  return movie.director === 'Steven Spielberg' && movie.genre.includes("Drama");
+});
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage() {}
+const scoresAverage = (movies.reduce(function (sum, movie) {
+  return sum + movie.score;
+},0) / movies.length).toFixed(2);
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore() {}
+const dramaMoviesScore = (movies.reduce(function (sum, movie) {
+  if(movie.genre.includes("Drama")){
+    console.log(sum + movie.score);
+    return sum + movie.score;
+  }else{
+    return sum;
+  }
+
+},0)) / (movies.reduce(function (sum, movie) {
+  if(movie.genre.includes("Drama")){
+    console.log(sum);
+
+    return sum + 1;
+  }else{
+    return sum;
+  }
+
+},0))
+
+console.log(dramaMoviesScore);
+
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear() {}
@@ -26,17 +56,18 @@ function bestYearAvg() {}
 
 
 
-// The following is required to make unit tests work.
-/* Environment setup. Do not modify the below code. */
-if (typeof module !== 'undefined') {
-  module.exports = {
-    getAllDirectors,
-    howManyMovies,
-    scoresAverage,
-    dramaMoviesScore,
-    orderByYear,
-    orderAlphabetically,
-    turnHoursToMinutes,
-    bestYearAvg,
-  };
-}
+
+// // The following is required to make unit tests work.
+// // Environment setup. Do not modify the below code. //
+// if (typeof module !== 'undefined') {
+//   module.exports = {
+//     getAllDirectors,
+//     howManyMovies,
+//     scoresAverage,
+//     dramaMoviesScore,
+//     orderByYear,
+//     orderAlphabetically,
+//     turnHoursToMinutes,
+//     bestYearAvg,
+//   };
+// }
