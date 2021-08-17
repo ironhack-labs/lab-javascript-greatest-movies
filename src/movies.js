a1b422ffc30e6640ff0aad59ea71f91b8070e410
Sara Mansori <sara.mansori.amar@gmail.com>
2021-08-17 09:33:13 +0200

    workflow update

--- src/movies.js
+++ src/movies.js
@@ -166,6 +166,7 @@ function bestYearAvg(movies) {
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
+
 if (typeof module !== 'undefined') {
   module.exports = {
     getAllDirectors,
