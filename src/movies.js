2f310fbdc4ad3673c554a493456fa38c5fa500d0
Geoffrey Laville <laville.geoffrey@gmail.com>
2021-09-24 00:34:52 +0200

    it 1 & 2

--- src/movies.js
+++ src/movies.js
@@ -1,16 +1,40 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors() {}
+function getAllDirectors(movies) {
+    const directorsNames = movies.map((movie) => {
+      return movie.director
+    }); 
+      return directorsNames; 
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies() {}
-
+function howManyMovies(movies) {
+  const countMovies = movies.filter((movie) => {
+    if (movie.director === "Steven Spielberg" && movie.genre.indexOf('Drama') !== -1) {
+      return movie;
+    }    
+  });
+  return countMovies.length;
+}
+    
+ 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage() {}
+function scoresAverage(movies) {
+  const average = movies.reduce((acc, curr) => {
+    console.log(`${acc} .... ${curr}`);
+  return acc + curr;
+  });
+  const averageRounded = average/movies.length;
+  return averageRounded.toFixed(2);
+}
+// Really try to understand something but I don't know how to do it
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore() {}
+function dramaMoviesScore(movies) {
+  const dramaScores = movies.reduce()
+}
+// Same here
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear() {}
