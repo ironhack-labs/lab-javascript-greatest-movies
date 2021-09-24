3ce7a7c12eb9c8377cd66e570d4480414d7fedd2
CodingEinhornDeu <88826323+CodingEinhornDeu@users.noreply.github.com>
2021-09-24 08:34:24 +0200

    3 Iterations

--- src/movies.js
+++ src/movies.js
@@ -1,28 +1,59 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
+
+const movies = require("./data");
+
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors() {}
+function getAllDirectors(mArray) {
+  const allDirectors = mArray.map(function (direct) {
+    return direct.director;
+  });
+  return allDirectors;
+}
+getAllDirectors(movies);
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies() {}
+function howManyMovies(arrMov) {
+  let numbOfMov = arrMov.filter(function (movie) {
+    if (movie.director === 'Steven Spielberg' && movie.genre.includes('Drama'))
+      return movie;
+
+  });
+  return (numbOfMov.length);
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage() {}
+function scoresAverage() {
+
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore() {}
+function dramaMoviesScore() { }
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear() {}
+function orderByYear(movieYear) {
+  const result = movieYear.sort((firstScore, secondScore) => {
+    if (firstScore.year > secondScore.year) {
+      return -1;
+    } else {
+      return 1;
+    }
+  });
+  return result;
+
+}
+orderByYear(movies);
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically() {}
+function orderAlphabetically() { }
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-function turnHoursToMinutes() {}
+function turnHoursToMinutes() { }
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
-function bestYearAvg() {}
+function bestYearAvg() {
+
+}
 
 
 
