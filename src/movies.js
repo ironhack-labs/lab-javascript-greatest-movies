8c78322f27288f9d0a6e33259e8f04a3210aa62d
Michael Seitz <michael.s.seitz@gmail.com>
2021-09-25 15:09:19 +0200

    done with iteration #2

--- src/movies.js
+++ src/movies.js
@@ -1,13 +1,44 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors() {}
+function getAllDirectors(arrOfMovies) {
+  let allDirectorsArray = []
+  let allDirectorsArrayUnique = []
+
+  // pushes all directors of arrOfMovies into allDirectorsArray
+  arrOfMovies.map(movieEntry => { allDirectorsArray.push(movieEntry.director) })
+
+  // pushes all directors of allDirectorsArray into allDirectorsArrayUnique if not already included
+  allDirectorsArray.map(director => {
+    if (!allDirectorsArrayUnique.includes(director))
+      allDirectorsArrayUnique.push(director)
+  })
+
+  return allDirectorsArrayUnique
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies() {}
+function howManyMovies(arrOfMovies) {
+  let countOfStevensDramas = []
+  
+  countOfStevensDramas = arrOfMovies.filter(movie => movie.director === "Steven Spielberg" && movie.genre.includes("Drama"))
+
+  return countOfStevensDramas.length
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage() {}
+function scoresAverage(arrOfMovies) {
+  let sumOfScores = 0
+  let avgScore = 0
+
+  // sums up all movie.score´s to sumOfScores
+  arrOfMovies.map(movieEntry => sumOfScores+=movieEntry.score)
+
+  // calculate
+  avgScore = sumOfScores/arrOfMovies.length
+
+  return avgScore
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesScore() {}
