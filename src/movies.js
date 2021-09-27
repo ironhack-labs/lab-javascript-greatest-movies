0073b9aef99f4cd4a0a9b560eb103da2d4e64ac1
Lilli Reilly <lilli.reilly@gmail.com>
2021-09-27 20:01:05 +0200

    Returning 27. Still making adjustments

--- src/movies.js
+++ src/movies.js
@@ -1,22 +1,59 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
+
+const movies = require("./data");
+
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors() {}
+function getAllDirectors(arrayOfMovies) {
+  let listOfDirectors =[];
+    arrayOfMovies.map(movie => listOfDirectors.push(movie.director))
+  return listOfDirectors;
+
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies() {}
+function howManyMovies(movies) {
+ function isSpielberg (movie) {
+   const isSpielberMovie = movie.director === "Steven Spielberg";
+   const isDramaMovie = movie.genre.includes("Drama");
+   if (isSpielberMovie && isDramaMovie) {
+     return true
+   }
+   return false
+ }
+ const spielbergMovies = movies.filter(isSpielberg);
+ return spielbergMovies.length
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage() {}
+function scoresAverage(movies) {
+  let scores = [];
+  movies.map(movie => scores.push(movie.score))
+  let sum = scores.reduce((total, score) => {
+    return total + score
+  }, 0)
+  let average = sum / scores.length;
+  return Math.round(average);
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesScore() {}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear() {}
+function orderByYear(moviesArray) {
+  let orderMovies = moviesArray.sort(function(a , b) {return a.year - b.year});
+  return orderMovies
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically() {}
+function orderAlphabetically(moviesArray) {
+  let onlyTitle = []
+  onlyTitle = moviesArray.map(movie => onlyTitle.push(movie.title))
+  let twentyMovies = onlyTitle.slice(0 ,20)
+  let alphabetOrder = twentyMovies.sort(function (a , b) {return a.title - b.title})
+  return alphabetOrder
+
+}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes() {}
