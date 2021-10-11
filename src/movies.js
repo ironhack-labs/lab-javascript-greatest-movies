f6186a0cc2cd0ee8bb9c5d8b74528b1deecac859
Oswaldo Jose Aviles Ibarra <oswaldoavilesib>
2021-10-10 23:23:55 -0500

    First 5 done

--- src/movies.js
+++ src/movies.js
@@ -13,20 +13,53 @@ function getAllDirectors(arr) {
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 function howManyMovies(arr) {
-  let dramaMovies = arr.filter(() => {
-    
-  })
+  let dramaMovies = arr.filter((curr) => {
+    return curr.director === "Steven Spielberg" && curr.genre.includes("Drama");
+  }).length
   return dramaMovies;
 }
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage() {}
+function scoresAverage(arr) {
+  //Average score of all movies and display it to the console
+  if(!arr.length) {
+    return 0
+  }
+  const totalObj = arr.length;
+  let sumOfScores = arr.reduce((acc, curr) => {
+      return curr.score += acc
+    },0)
+  let average = sumOfScores / totalObj;
+  return Number(average.toFixed(2));
+  }
+
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore() {}
+function dramaMoviesScore(arr) {
+  let dramaMovies = arr.filter((movie) => {
+    return movie.genre.includes("Drama")
+  });
+  let numberOfMovies = dramaMovies.length;
+  let totalScore = dramaMovies.reduce((acc,curr) => {
+    return curr.score += acc
+  },0)
+  let averageScore = totalScore / numberOfMovies
+  return Number(averageScore.toFixed(2));
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear() {}
+function compare(a,b) {
+  if (a.year < b.year) return -1; // a is less than b
+  if (a.year > b.year) return 1; // a is greater than b
+  if (a === b) {
+     if (a.title > b.title) return 1;
+     if (a.title < b.title) return -1;
+     if(a.title=== 0) return 0;}
+}
+
+function orderByYear(arr) {
+  return arr.sort(compare)
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically() {}
