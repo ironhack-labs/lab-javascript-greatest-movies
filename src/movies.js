83b77d8bb040f6fb917688de2012e0390f09713b
Guillem Ferrer <guillemfd@gmail.com>
2021-10-22 23:51:42 +0200

    Iterations 1 -> 4

--- src/movies.js
+++ src/movies.js
@@ -1,28 +1,56 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(data.js) {
-  const 
+function getAllDirectors(movies) {
+  return movies.map((item) => item.director) 
 }
 
-// const movies = [
-//   {
-//     title: 'The Shawshank Redemption',
-//     year: 1994,
-//     director: 'Frank Darabont',
-//     duration: '2h 22min',
-//     genre: ['Crime', 'Drama'],
-//     score: 9.3
-//   },
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies() {}
+function howManyMovies(movies) {
+
+  return movies.filter((item) => item.director === 'Steven Spielberg' && item.genre.includes('Drama')
+  ).length
+}
+
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage() {}
+function scoresAverage(movies) {
+
+  if (movies.length === 0)
+  return 0
+
+ const scoresSum = movies.reduce(function(accu, movie) {
+   if (movie.score) {
+       return accu + movie.score
+  } else {
+       return accu
+  }
+ }, 0)
+
+  return parseFloat((scoresSum / movies.length).toFixed(2))
+}
+
+
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore() {}
+function dramaMoviesScore(movies) {
+
+if (movies.length === 0)
+return 0
+
+const scoresSum = movies.reduce(function(accu, movie) {
+ if (movie.score && movie.genre.includes('Drama')) {
+     return accu + movie.score
+  } else {
+     return accu
+  }
+}, 0)
+
+return parseFloat((scoresSum / movies.length).toFixed(2))
+}
+
+
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear() {}
