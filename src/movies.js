73ce6e10c3ded3e6fffa4dcdf6e3a49aa835d3c6
Noam Shani <Noams156@gmail.com>
2021-10-24 22:06:39 +0200

    updated lab noam shani

--- src/movies.js
+++ src/movies.js
@@ -42,7 +42,7 @@ function scoresAverage(array) {
   }
  const {total, count} = array.reduce((a, b) => {
    
-  if (b.score === 8) {
+  if (b.score === 0) {
         a.total += b.score;
         a.count++;
     } 
@@ -69,11 +69,30 @@ function dramaMoviesScore(movies) {
   return scoresAverage(dramaArray)
 }
 
+
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear() {}
+function orderByYear(array) {
+
+  const sorted = movies.sort(function(a,b) { 
+      if(a.year > b.year) return 1;
+      if(a.year < b.year) return -1;
+      return 0;
+  })
+   
+  return sorted
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically() {}
+function orderAlphabetically(array) {
+
+  const alphaOrdered = movies.sort(function(a, b){
+    if(a.title < b.title) { return -1; }
+    if(a.title > b.title) { return 1; }
+    return 0;
+})
+return alphaOrdered.slice(0,20)
+
+}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes() {}
