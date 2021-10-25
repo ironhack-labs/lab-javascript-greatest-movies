f6efc299c9e37c792382f265d1c393d7cd6310ff
Fernando Cardona <fernando.cardona@the-lion.com>
2021-10-26 00:33:17 +0200

    almost Done

--- src/movies.js
+++ src/movies.js
@@ -4,11 +4,13 @@ const movies = require('./data');
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors() {
+function getAllDirectors(movies) {
 
   const allDirectors = movies.map(eachDirectors =>  eachDirectors.director);
-     
-    return allDirectors;
+  //Creaamos un array nuevo para limpiar el arrray All Directors con 'Set()'
+  const listUnique = [... new Set(allDirectors)]
+      
+      return listUnique
 
 }
 
@@ -50,30 +52,52 @@ function dramaMoviesScore(movies) {
   if(movies.length === 0) {
     return 0
   } 
-    const dramaMovies = movies.filter(eachMovie => eachMovie.genre.includes('Drama'))
-    if(!dramaMovies === 0) {
-      return 0
-    } console.log('hola')
-    const allScoreDramas = dramaMovies.map( eachScoreDrama => {
-           return eachScoreDrama +=
-     })
-    //const doubledNumbers = numbers1.map(eachNumber => {
-      //     return eachNumber * 2
-      // })
-    console.log(allScoreDramas)
-    const moviesDramaPromedio = allScoreDramas.reduce((acc, eachscore) => {
-      if(eachscore.score !== undefined) return acc += eachscore.score 
-        return acc
+    const dramaMovies = movies.filter(eachMovie => {
+      return eachMovie.genre.includes('Drama');
+    })
+    //console.log(dramaMovies.length)
+    if(dramaMovies.length === 0) return 0
+  
+    const moviesDramaPromedio = dramaMovies.reduce((acc, eachscore) => {
+      return acc += eachscore.score 
+        
     },0) 
-     
-      return Number((moviesDramaPromedio / allScoreDramas.length).toFixed(2))
+    //console.log(moviesDramaPromedio)
+    return Number((moviesDramaPromedio / dramaMovies.length).toFixed(2))
 }
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear() {}
+function orderByYear(movies) {
+  //const cloneMovies = [... movies]
+  const sortCloneMovies = movies.map((movie)=>movie);
+  sortCloneMovies.sort(function compare(movie_a, movie_b) {
+    //Utilizamos las propiedades ordenamiento -1,0,1
+    if (movie_a.year < movie_b.year) return -1; 
+    if (movie_a.year > movie_b.year) return 1;
+    if (movie_a.year === movie_b.year){ return 0;
+    
+    }
+
+    });
+    //console.log(sortCloneMovies)
+    return sortCloneMovies
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically() {}
+function orderAlphabetically() {
+  const cloneMovies2 = [... movies ];
+  const moviestitle =  cloneMovies2.map(eachDtitle =>  eachDtitle.title);
+  //console.log(moviestitle)
+  const alphaMovies = moviestitle.sort((movie_a, movie_b) => {
+    //Utilizamos las propiedades ordenamiento -1,0,1
+    if (movie_a == movie_b) { return 0; }
+    if (movie_a < movie_b) { return -1; }
+    if (movie_a > movie_b) { return -1; }  
+  });
+  
+  console.log(alphaMovies)
+  return alphaMovies.slice(0,20) 
+}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes() {}
