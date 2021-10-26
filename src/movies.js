34804bbacc7a32b90ca9e6823da2ec6ae27a10e8
Iñigo <inigo@MacBook-Air-de-Inigo.local>
2021-10-26 08:55:37 +0200

    trbajo DOne

--- src/movies.js
+++ src/movies.js
@@ -1,20 +1,74 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors() {}
+
+const movies = require("./data")
+
+function getAllDirectors() {
+  const allDirectors = movies.map (movie => movie.director);
+  return allDirectors;
+  
+  
+  
+
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies() {}
+function howManyMovies(movies) {
+  
+  const dramSteven = movies.filter (eachSteven => {
+    if (eachMovie.director === "Steven Spielberg" && eachMovie.genre.includes ("Drama")) {
+      return true
+  }
+      return false
+})
+return dramSteven.length;
+}
+
+
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage() {}
+function scoresAverage() {
+  if(movies.length == 0) {
+    return 0
+  }
+  
+  else{
+  const scoresTotal = movies.reduce((acc, eachScore) => {
+    if(eachScore.score !== undefined) return acc + eachScore.score;
+    return acc;
+  
+  }, 0)
+    return Number((scoresTotal / movies.length).toFixed(2));
+}
+}
+
+
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore() {}
+function dramaMoviesScore() {
+  if(movies.length == 0) {
+    return 0;
+  }
+  const dramaAll = movies.filter(oneMovie => oneMovie.genre.includes('Drama'))
+    if (!dramaAll.length) {
+    return 0;
+  }
+  const allScores = dramaAll.map (movieScore => movieScore.score)
+  const scoreDrama = allScores.reduce ((result, score) => {
+    if (typeof score !== "number") {
+    score = 0;
+    }
+      return result + score
+    }, 0)
+    return Number((scoreDrama / allScores.length).toFixed(2))
+  }
+  
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear() {}
-
+  
+  
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically() {}
 
