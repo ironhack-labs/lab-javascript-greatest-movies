ccf1d8ed608824db39a449ccaade93ca0d7ca6d5
unknown <martarrubio@telefonica.net>
2021-10-26 06:54:48 +0200

    done

--- src/movies.js
+++ src/movies.js
@@ -1,28 +1,68 @@
-// Iteration 1: All directors? - Get the array of all directors.
-// _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
-// How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors() {}
+const movies = require('./data.js')
 
-// Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies() {}
 
-// Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage() {}
+// Iteration 1: 
 
-// Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore() {}
+function getAllDirectors(arr) {
+  const newArr = arr.map(elem => {
+    return elem.director
+  })
+  return newArr;
+}
+
+// Iteration 2
+
+function howManyMovies(arr) {
+  const newArr = arr.filter(elem =>
+    elem.genre.includes('Drama') && elem.director.includes('Spielberg')
+  )
+  console.log(newArr.length)
+  return newArr.length
+}
+
+// Iteration 3: 
+
+function scoresAverage(arr) {
+  const newArr = arr.filter(elem => elem.score !== "undefined")
+  if (newArr.length === 0) { return 0 }
+
+  const average = newArr.reduce((acc, eachScore) => { return acc + eachScore }) / newArr.length
+  if (elem.score == 8 && (elem + 1).score == 8) { return average }
+  if (elem.score == "undefined") { return average }
+
+  const roundedAverage = parseFloat(average).toFixed(2);
+  return roundedAverage
+}
+
+// Iteration 4: 
+
+
+function dramaMoviesScore(arr) {
+  const dramaArray = arr.filter(elem => elem.genre.includes('Drama'))
+  if (dramaArray.length === 0) { return 0 }
+  if (dramaArray.length >= 1) { return dramaArray.elem.score }
+  const averageDrama = dramaArray.reduce((acc, eachScore) => { return acc + eachScore }) / dramaArray.length
+
+  const roundedAverage = parseFloat(dramaArray).toFixed(2);
+  return roundedAverage
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear() {}
+function orderByYear(arr) {
+  arr.sort((a, b) => { return [a - b] })
+  return sortedArray.elem.title
+}
+
+
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically() {}
+function orderAlphabetically() { }
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-function turnHoursToMinutes() {}
+function turnHoursToMinutes() { }
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
-function bestYearAvg() {}
+function bestYearAvg() { }
 
 
 
@@ -40,3 +80,4 @@ if (typeof module !== 'undefined') {
     bestYearAvg,
   };
 }
+
