067668469d3688a03ceb11eb33e4438605f9a5d9
Rachouan Rejeb <info@rachouan.com>
2021-11-13 16:24:23 +0000

    Solved lab

--- src/movies.js
+++ src/movies.js
@@ -1,25 +1,61 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors() {}
+function getAllDirectors(movies) {
+  return movies.map(movie => movie.director)
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies() {}
+function howManyMovies(movies) {
+  return movies.filter(movie => movie.director === 'Steven Spielberg' && movie.genre.includes('Drama')).length;
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage() {}
+function scoresAverage(movies) {
+  if(!movies.length) return 0;
+  const totalScore = movies.reduce((score, movie) => score += !movie.score ? 0 : movie.score,0)
+  return parseFloat((totalScore/movies.length).toFixed(2));
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore() {}
+function dramaMoviesScore(movies) {
+  return scoresAverage(movies.filter(movie => movie.genre.includes('Drama')));
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear() {}
+function orderByYear(movies) {
+  if(!movies.length) return [];
+  return movies.sort((a,b) => {
+    return a.year !== b.year ? a.year - b.year : a.title.localeCompare(b.title)
+  });
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically() {}
+function orderAlphabetically(movies) {
+  let orderedArray = movies;
+  return orderedArray.sort((a,b) => a.title.localeCompare(b.title)).slice(0,20).map(movie => movie.title);
+}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-function turnHoursToMinutes() {}
+function turnHoursToMinutes(movies) {
+  let duration;
+  let newArr = movies.map(movie => {
+    duration = movie.duration.split('h');
+    let hours;
+    let mins;
+    if(duration.length < 2){
+      hours = 0;
+      min = parseFloat(duration)
+    }else{
+      hours = parseFloat(duration[0]) * 60;
+      mins = duration[1] ? parseFloat(duration[1]): 0;
+    }
+    duration = hours + mins;
+    movie.duration = duration;
+    return movie;
+  })
+  return newArr;
+}
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
 function bestYearAvg() {}
