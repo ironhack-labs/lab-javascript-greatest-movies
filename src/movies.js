042e20e15fb07ed3423725f203d88e8a2151f346
Robert Cabrera <cabrera.rch@gmail.com>
2021-11-15 18:31:59 +0100

    lab done

--- src/movies.js
+++ src/movies.js
@@ -1,5 +1,3 @@
-
-
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
@@ -10,10 +8,14 @@ function getAllDirectors(movies) {
 }
  
 
+
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 function howManyMovies(movies) {
   return movies.filter((dramaMovies) => (dramaMovies.director === "Steven Spielberg" && dramaMovies.genre.includes("Drama"))).length
 }
+
+
+
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
 function scoresAverage(movies) {
@@ -34,10 +36,13 @@ function scoresAverage(movies) {
   return result * 1;
 }
 
+
+
+
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesScore(movies) {
   const dramaScore = movies.filter((drama) => drama.genre.includes("Drama"))
-                          .map((mov) => mov.score)
+                            .map((mov) => mov.score)
 
   if (dramaScore.length === 0) {
     return 0
@@ -50,6 +55,9 @@ function dramaMoviesScore(movies) {
   const dramaAverage = (dramaResult / dramaScore.length).toFixed(2);
   return dramaAverage * 1;
 }
+
+
+
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear(movies) {
@@ -66,28 +74,64 @@ function orderByYear(movies) {
   return orderMovies.map((theYear) => theYear)
 }
 
+
+
+
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically(movies) {
-  const sortedMovies = movies.sort((titleMovie1, titleMovie2) => {
-    if (titleMovie1.title > titleMovie2.title) {
+
+  const onlyTitle = movies.map((movie) => movie.title);
+  const sortedMovies = onlyTitle.sort((titleMovie1, titleMovie2) => {
+    if (titleMovie1 > titleMovie2) {
       return 1;
-    } else if (titleMovie1.title < titleMovie2.title) {
+    } else if (titleMovie1 < titleMovie2) {
       return -1;
     } else {
       return 0;
     }
   });
 
-  const twentyMovies = sortedMovies.slice(0, 20); 
-  
-  return twentyMovies.map(movie => movie.title);
+  const twentyMovies = sortedMovies.slice(0, 20);  
+  return twentyMovies
 }
 
+
+
+
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-function turnHoursToMinutes() {}
+function turnHoursToMinutes(movies) {
+  const moviesDuration = movies.map((timeMovie) => timeMovie.duration);
+  return moviesDuration;
+}
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
-function bestYearAvg() {}
+function bestYearAvg(movies) {
+  let puntuation = 0;
+
+  if (movies.length === 0) {
+    return null;
+  }
+
+  const orderMoviesByYear = movies.sort((yearFilm1, yearFilm2) => {
+    if (yearFilm1.year > yearFilm2.year) {
+      return 1
+    } else if (yearFilm1.year < yearFilm2.year) {
+      return -1
+    } else {
+      return 0
+    }
+  });
+
+  const yearOfMovie = orderMoviesByYear.map((movieYear) => movieYear.year);
+  const scoreOfMovie = orderMoviesByYear.map((movieScore) => movieScore.score);
+
+  for (let i = 0; i < orderMoviesByYear.length; i++) {
+    if (orderMoviesByYear[i].year === yearOfMovie[i]) {
+      moviePuntuation = scoreOfMovie[i] + orderMoviesByYear[i].score;
+      return `The best year was ${orderMoviesByYear[i].year} with an average score of ${moviePuntuation}`;
+    }
+  };
+}
 
 
 
