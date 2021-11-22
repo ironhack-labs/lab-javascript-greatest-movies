de165faf880dd1de673b0e1a1b80c03921a0ed2b
Vinicius Leite Batista <viniciusleite384@gmail.com>
2021-11-22 18:57:50 -0400

    done

--- src/movies.js
+++ src/movies.js
@@ -1,19 +1,50 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors() {}
+const getAllDirectors = moviesArray => moviesArray.map(movie => movie.director);
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies() {}
+const howManyMovies = moviesArray => {
+  const spielbergDramaMovies = moviesArray.filter(movie =>
+    movie.director === 'Steven Spielberg' && movie.genre.includes('Drama')
+  );
+  
+  const numberOfSpielbergDramaMovies = spielbergDramaMovies.length;
+  
+  return numberOfSpielbergDramaMovies;
+};
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage() {}
+const scoresAverage = moviesArray => {
+  const movieScores = moviesArray.map(movie => movie.score);
+  const sumMovieScores = movieScores.reduce((accumulator, currentValue) =>
+    accumulator + currentValue, 0);
+
+  const averageMovieScore = sumMovieScores / moviesArray.length;
+  const roundedMovieScore = Math.round((averageMovieScore + Number.EPSILON) * 100) / 100;
+  
+  return roundedMovieScore;
+};
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore() {}
+const dramaMoviesScore = moviesArray => {
+  const dramaMovies = moviesArray.filter(movie =>
+    movie.genre.includes('Drama')
+  );
+  
+  const dramaScores = dramaMovies.map(drama => drama.score);
+  const sumDramaScores = dramaScores.reduce((accumulator, currentValue) =>
+    accumulator + currentValue, 0);
+  
+  const averageDramaScore = sumDramaScores / dramaMovies.length;
+  const roundedDramaScore = Math.round((averageDramaScore + Number.EPSILON) * 100) / 100;
+  
+  return roundedDramaScore;
+};
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear() {}
+const orderByYear = moviesArray => {};
+//function orderByYear() {}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically() {}
