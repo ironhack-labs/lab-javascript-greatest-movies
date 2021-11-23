66215f677d5f2d34bd62dfb9892fc28a70f39b4e
Lucas Salomao <lucaas.afonso@hotmail.com>
2021-11-23 10:46:43 -0300

    Iteration 1 e 2  done - sem 1.1 (bonus)

--- src/movies.js
+++ src/movies.js
@@ -1,10 +1,58 @@
+/* Notas rápidas
+
+Function Expression - const welcomeMessage = function (message) {console.log(message)}
+
+
+
+*/
+
+
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors() {}
+
+movies = [
+  {
+    title: 'The Shawshank Redemption',
+    year: 1994,
+    director: 'Francis Ford Coppola',
+    duration: '2h 22min',
+    genre: ['Crime', 'Drama'],
+    score: 9.3
+  },
+  {
+    title: 'The Godfather',
+    year: 1972,
+    director: 'Francis Ford Coppola',
+    duration: '2h 55min',
+    genre: ['Crime'],
+    score: 9.2
+  }]
+
+function getAllDirectors(arrayOfMovies) {
+  let arrayOfDirectors = arrayOfMovies.map(movie => movie.director)
+  return arrayOfDirectors
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies() {}
+function howManyMovies(arrayGiven) {
+
+  let arraySpielberg = arrayGiven.filter(movie => movie.director === "Steven Spielberg")
+
+  let arraySpielbergDramaMovies = []
+
+  for (let i = 0; i < arraySpielberg.length; i++) {
+    let arrayHasDramaGenre = arraySpielberg[i].genre.filter(genreOfMovie => genreOfMovie === "Drama")
+
+    if (arrayHasDramaGenre.length > 0) {
+      arraySpielbergDramaMovies.push(arraySpielberg[i])
+    }
+  }
+  
+  return arraySpielbergDramaMovies.length
+}
+
+console.log(howManyMovies(movies))
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
 function scoresAverage() {}
