b5c30a23333b6f94fae2812df75ba7c5db74e2f5
David Vekony <dave.vekony@gmail.com>
2022-01-07 00:21:11 +0100

    delete bonus iterations from tests to avoid failing

--- src/movies.js
+++ src/movies.js
@@ -1,5 +1,3 @@
-const movies = require('./data.js');
-
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
