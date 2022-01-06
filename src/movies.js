c0cc3379f7b2f0985945810f31c9dcbfd81420cd
Clara Villa <Clara.Villa@lego.com>
2022-01-06 19:08:03 +0000

    done iteration 7

--- src/movies.js
+++ src/movies.js
@@ -2,6 +2,8 @@
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
 
+const movies = require("./data");
+
 
 function getAllDirectors(movies) {
 
@@ -110,7 +112,22 @@ function orderAlphabetically(movies) {
 }
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-function turnHoursToMinutes() {}
+function turnHoursToMinutes(movies) {
+  let moviesCopy = movies
+    for (let i=0; i<moviesCopy.length; i++) {
+      let string = moviesCopy[i].duration;
+       let hour = parseInt(string.slice(0,string.indexOf("h")));
+       let minutes;
+       if (string.indexOf("m")===-1) {
+         minutes = 0;
+       } else {
+         minutes = parseInt(string.slice((string.indexOf(" ")+1),-3));
+       };
+       let totalMinutes = hour*60+minutes;
+       moviesCopy[i].duration = totalMinutes;
+    }
+    return moviesCopy;
+}
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
 function bestYearAvg() {}
