3bcc5664390508af8facbf1191e371b5277f5c12
NielsMKristensen <Niels.Kristensen@Hotmail.com>
2022-01-07 09:27:02 +0100

    thos is how far i got

--- src/movies.js
+++ src/movies.js
@@ -1,13 +1,51 @@
+let movies = require('./data');
+
+//console.log(movies.lenght)
+
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors() {}
+function getAllDirectors(movies) {
+  const allDirectors = movies.map(function (mov) {
+    return mov.director
+  });
+  return allDirectors
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies() {}
+function howManyMovies(movies) {
+  const dramaSteven = movies.filter(function(array) {
+    return array.genre.includes('Drama') && array.director === "Steven Spielberg"
+  });
+  
+  return dramaSteven.length
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage() {}
+function scoresAverage(movies) {
+  if (movies.length < 1 || movies == undefined) {
+    return 0;
+  }
+  
+  const avgScore = movies.map(function (array) {
+    let allScores = array.score
+    //if (array.score.lenght === 0){
+    //  continue
+    //}
+    return allScores
+  });
+  
+  let sum = 0; 
+  let avg = 0 
+    for (let i = 0; i < avgScore.length; i++) {
+        sum += avgScore[i];
+    }
+    
+    avg = sum / avgScore.length
+  return Math.round(avg * 100) / 100
+
+}
+//console.log(scoresAverage(movies))
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesScore() {}
@@ -19,10 +57,22 @@ function orderByYear() {}
 function orderAlphabetically() {}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-function turnHoursToMinutes() {}
+function turnHoursToMinutes(movies) {
+  const allDirectors = movies.map(function (mov) {
+    let convertDuration = mov.duration
+    return convertDuration 
+  
+  });
+  return allDirectors
+}
+
+//console.log (turnHoursToMinutes(movies));
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
-function bestYearAvg() {}
+function bestYearAvg() {
+
+
+}
 
 
 
