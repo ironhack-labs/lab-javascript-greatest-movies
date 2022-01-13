0a10c9317500a5ac776bb76ddf27e1c1c99bd14b
Gertjan Schoutsen <gjs@Gertjans-MacBook-Pro.local>
2022-01-13 21:14:37 +0100

    Finished Day 4 labs, Iterations 1,2,3,4

--- src/movies.js
+++ src/movies.js
@@ -1,16 +1,58 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors() {}
-
+function getAllDirectors(arr) {
+  const pickDirector = arr.map((x) => x.director);
+  return pickDirector;
+}
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies() {}
+function howManyMovies(arr) {
+  const sortedMovie = arr.filter((movie) => {
+    if (
+      movie.director === 'Steven Spielberg' &&
+      movie.genre.indexOf('Drama') > -1
+    ) {
+      return true;
+    }
+  });
+  return sortedMovie.length;
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage() {}
+function scoresAverage(arr) {
+  const sumScore = arr.reduce((prevVal, curVal) => {
+    return prevVal + curVal.score;
+  }, 0);
+
+  if (arr.length === 0) {
+    return 0;
+  }
+
+  let averageScore = sumScore / arr.length;
+  let totalScore = Math.round(averageScore * 100) / 100;
+  return totalScore;
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore() {}
+function dramaMoviesScore(arr) {
+  const sortedMovie = arr.filter((movie) => {
+    if (movie.genre.indexOf('Drama') > -1) {
+      return true;
+    }
+  });
+
+  const sumScore = sortedMovie.reduce((prevVal, curVal) => {
+    return prevVal + curVal.score;
+  }, 0);
+  let averageScore = sumScore / sortedMovie.length;
+  let totalScore = Math.round(averageScore * 100) / 100;
+
+  if (sortedMovie.length === 0) {
+    return 0;
+  }
+
+  return totalScore;
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear() {}
