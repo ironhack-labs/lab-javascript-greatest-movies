44a7c503f1ff24c85d02383f2a433eaaf04712ad
Bjoern Hass <bjoernhass@gmx.net>
2022-01-14 09:47:43 +0100

    fix bracket

--- src/movies.js
+++ src/movies.js
@@ -2,6 +2,10 @@
 const movies = require("./data");
 //let Data = fs.readFileSync("./data.js", "utf-8");
 
+console.log(howManyMovies())
+
+
+
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
@@ -39,7 +43,7 @@ function dramaMoviesScore() {
   let Average = 0;
   let Counter = 0;
   movies.forEach(element => {
-      if (element.genre.find("Drama")
+      if (element.genre.find("Drama"))
       {
         Average += element.score;
         Counter++;
