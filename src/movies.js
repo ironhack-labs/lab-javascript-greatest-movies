672f27e8c85e3a9c6c8d8949e76a740fddcc1ecd
Saleh Ramadan <saleh.ramadan1989@gmail.com>
2022-01-17 00:32:35 +0200

    solved lab (somehow)

--- src/movies.js
+++ src/movies.js
@@ -1,30 +1,54 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors() {}
+function getAllDirectors(array) {
+  return array.map(function (array) 
+  {
+    return array.director;
+  }
+  )
+
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies() {}
+function howManyMovies(drama) {
+  return drama.filter(function (drama) {
+    return drama.movie;
+  })
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage() {}
+function scoresAverage(score) {
+  return score.reduce((accumulator, currentValue){
+    return accumulator + currentValue; 
+  })
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesScore() {}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear() {}
+function orderByYear(sorted) {
+  sorted.sort();
+  return sorted;
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically() {}
+function orderAlphabetically(alphabetic) {
+  
+    return alphabetic.sort((a, b) => {
+      if (a > b) return 1;
+      if (a < b) return -1;
+      return 0;
+    });
+}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes() {}
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
 function bestYearAvg() {}
-
-
+ 
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
