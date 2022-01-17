5bb32fbeee2bb76891583c1a964b075968df2a6e
yoricktf <yorick.tenfeld@gmail.com>
2022-01-17 09:02:53 +0100

    some exercises complete

--- src/movies.js
+++ src/movies.js
@@ -1,19 +1,57 @@
 // Iteration 1: All directors? - Get the array of all directors.
+function getAllDirectors(array) {
+  let directors = array.map(function (movie){
+    return movie.director
+  })
+  return directors
+}
+
+
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors() {}
+
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies() {}
+function howManyMovies(array) {
+  let stevenMovies = [];
+  let count = 0;
+  array.filter(function(movie){
+    if (movie.director === "Steven Spielberg" && movie.genre.includes('Drama')){
+     return stevenMovies.push(movie.genre)
+     }
+  })
+  return stevenMovies.length
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage() {}
+function scoresAverage(array) {
+  let score = array.reduce(function (x, y){
+    return x + y.score
+  }, 0)
+  return Number((score / array.length).toFixed(2))
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore() {}
+function dramaMoviesScore(array) {
+  let movies = array.filter(function (movie){
+    if (movie.genre.includes('Drama')){
+      return movie
+    }
+  });
+  let average = movies.reduce(function (x,y){
+      return x + y.score
+    }, 0)
+  return Number((average / movies.length).toFixed(2))
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear() {}
+function orderByYear(array) {
+  // console.log(array)
+  let ordered = array.sort(function (a, b){
+    return a.year - b.year
+  })
+  return(ordered)
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically() {}
