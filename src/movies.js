c22c7871232235df6939fbc1309b89b01516b368
Ernesto Espinoza M <emenocal07@gmail.com>
2022-01-17 17:01:40 +0100

    saved

--- src/movies.js
+++ src/movies.js
@@ -1,28 +1,54 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
+
+const movies = require("./data");
+
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors() {}
+function getAllDirectors(array) {
+  const getDirectors = array.map(eachMovie => {
+    return eachMovie.director
+  })
+  return getDirectors
+}
+
+
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies() {}
+function howManyMovies(array) {
+  let spielbergDrama = 0
+  const spielbergMovies = array.filter((eachMovie) => {
+    if (eachMovie.director === "Steven Spielberg" && eachMovie.genre.includes('Drama'))
+      spielbergDrama++
+  })
+  return spielbergDrama
+}
+
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage() {}
+function scoresAverage(array) {
+  const sumMovies = array.reduce((acc, eachMovie) => {
+    return acc + eachMovie.score
+    
+  }, 0)
+  return (sumMovies / array.length).toFixed(2)
+}
+
+console.log(scoresAverage(movies))
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore() {}
+function dramaMoviesScore() { }
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear() {}
+function orderByYear() { }
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically() {}
+function orderAlphabetically() { }
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-function turnHoursToMinutes() {}
+function turnHoursToMinutes() { }
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
-function bestYearAvg() {}
+function bestYearAvg() { }
 
 
 
