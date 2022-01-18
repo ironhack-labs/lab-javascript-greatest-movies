7c5926e2341515b86fa6d5cdf794fee0aad15e6e
Gianluca Francescato <gianluca@Gianlucas-MacBook-Pro.local>
2022-01-18 16:51:50 +0100

    <your message>

--- src/movies.js
+++ src/movies.js
@@ -1,22 +1,81 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors() {}
+function getAllDirectors(movies) {
+  const directors= movies
+  .map((movie) => {
+    return movie.director;
+  })
+  .filter((same, index, originalArray) => {
+    return originalArray.indexOf(same) === index;
+  });
+    return directors;
+} 
+
+// const finalGrades = students.map(theStudent => {
+//   return {
+//     name: theStudent.name,
+//   };
+// });
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies() {}
+
+function howManyMovies(movies) {
+  return movies.filter(movie => {
+    return movie.genre.includes('Drama') && movie.director === 'Steven Spielberg';
+  }).length;
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage() {}
+function scoresAverage(movies) {
+   return movies.reduce((sum, movie) => sum + movie.rate / movies.length, 0);
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore() {}
+function dramaMoviesScore(movies) {
+  const dramaMovies = movies.filter(movie => {
+    return movie.genre.includes('Drama');
+  });
+  return calculateAverageMovieRate(dramaMovies);
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear() {}
+function orderByYear(movies) {
+  const arrayCopy = [...movies];
+  return arrayCopy.sort((a, b) => {
+    if (a.year > b.year) {
+      return 1;
+    } else if (a.year < b.year) {
+      return -1;
+    } else {
+      const firstMovieTitle = a.title.toLowerCase();
+      const secondMovieTitle = b.title.toLowerCase();
+      if (firstMovieTitle > secondMovieTitle) {
+        return 1;
+      } else if (firstMovieTitle < secondMovieTitle) {
+        return -1;
+      } else {
+        return 0;
+      }
+    }
+  });}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically() {}
+function orderAlphabetically(movies) {
+  return movies
+    .map(movie => movie.title)
+    .sort((a, b) => {
+      const firstMovieTitle = a.toLowerCase();
+      const secondMovieTitle = b.toLowerCase();
+      if (firstMovieTitle > secondMovieTitle) {
+        return 1;
+      } else if (firstMovieTitle < secondMovieTitle) {
+        return -1;
+      } else {
+        return 0;
+      }
+    })
+    .slice(0, 20);}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes() {}
