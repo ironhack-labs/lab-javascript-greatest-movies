c02a10b4132cf5223399f9851f0f1266d8d9df5d
Nathalia Maia <nathaliamaiape@gmail.com>
2022-01-22 10:02:54 -0300

    finished iteration 6

--- src/movies.js
+++ src/movies.js
@@ -20,26 +20,79 @@ function howManyMovies(arr) {
   });
   return stevenDramaMovies.length;
 }
-
+ 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage() {}
-
-// Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore() {}
-
-// Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear() {}
-
-// Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically() {}
-
-// BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-function turnHoursToMinutes() {}
-
-// BONUS - Iteration 8: Best yearly score average - Best yearly score average
-function bestYearAvg() {}
-
-
+function scoresAverage(arr) {
+  if (arr.length === 0){
+    return 0;
+  }
+  
+  let finalScore = arr.reduce((acc, el) => {
+    if(el.hasOwnProperty("score")) {
+      return (acc + el.score);
+    } else {
+      return acc;
+    }
+  }, 0);
+  
+  return parseFloat((finalScore/arr.length).toFixed(2));
+ }
+  
+ // Iteration 4: Drama movies - Get the average of Drama Movies
+ function dramaMoviesScore(arr) {
+  if (arr.length === 0){
+    return 0;
+  }
+  
+  let numberOfDramas = 0;
+  
+  let finalScore = arr.reduce((acc, el) => {
+    if(el.hasOwnProperty("score") && el.genre.includes("Drama")) {
+      numberOfDramas++;
+      return (acc + el.score);
+    } else {
+      return acc;
+    }
+  }, 0);
+  
+  if (numberOfDramas === 0){
+    return 0;
+  }
+  return parseFloat((finalScore/numberOfDramas).toFixed(2));
+ }
+  
+ // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
+ function orderByYear(arr) {
+  let orderArray = Array.from(arr);
+  
+  orderArray.sort((a, b) => {
+    if(a.year < b.year) {
+      return -1;
+    } else if(a.year > b.year) {
+      return 1;
+    } else {
+      return a.title.localeCompare(b.title);
+    }
+  })
+  
+  return orderArray;
+ }
+  
+ // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
+ function orderAlphabetically(arr) {
+  let orderArray = Array.from(arr);
+  
+  orderArray.sort((a, b) => a.title.localeCompare(b.title));
+  const result = orderArray.map((el) => el.title);
+  
+  return result.slice(0, 20);
+ }
+ // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
+ function turnHoursToMinutes() {}
+  
+ // BONUS - Iteration 8: Best yearly score average - Best yearly score average
+ function bestYearAvg() {}
+ 
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
