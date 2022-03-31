b93878f94d4b828606c820a2223ab8a80321cbc6
Ruben Poelen <ruben.poelen@hotmail.com>
2022-03-31 21:57:35 +0200

    Man what was this a tiring exercise

--- src/movies.js
+++ src/movies.js
@@ -1,22 +1,77 @@
 // The `movies` array from the file `src/data.js`.
-console.log('movies: ', movies);
+// console.log('movies:', movies);
 
+// const movies = require('./data');
 
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors() {}
+// const arrayDirector = [];
+function getAllDirectors(arrOfMovies) {
+  return (arrayDirector = arrOfMovies.map(function (element, value) {
+    // console.log(element);
+    return element.director;
+    // console.log(element);
+  }));
+  // return arrayDirector;
+}
+// getAllDirectors(movies);
+// console.log(arrayDirector);
+
+// const directorArray = getAllDirectors(movies);
+// console.log(directorArray);
+// console.log(arrayDirector);
+
+// console.log(arrayDirector);
+// console.log(arrayDirector);
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies() {}
+function howManyMovies(array) {
+  if (array.length === 0) {
+    return 0;
+  }
+  return (spielbergMovies = array.filter(function (element, value) {
+    if (
+      element.genre.includes('Drama') &&
+      element.director.includes('Steven Spielberg')
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }).length);
+}
+
+// howManyMovies(movies);
+// console.log(spielbergMovies);
+// console.log(spielbergMovies);
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage() {}
+function scoresAverage(array) {
+  if (array.length === 0) {
+    return 0;
+  }
+  const totalScore = array.reduce(function (accumulator, currentValue) {
+    return accumulator + currentValue.score;
+  }, 0);
+  const averageScore = totalScore / array.length;
+  // console.log(scoreTotal / movies.length);
+  return Math.round(averageScore * 100) / 100;
+}
+
+// scoresAverage(movies);
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore() {}
+function dramaMoviesScore(array) {
+  const dramaMovies = array.filter(function (element, value) {
+    if (element.genre.includes('Drama')) {
+      return true;
+    }
+  });
+  return scoresAverage(dramaMovies);
+}
 
-// Iteration 5: Ordering by year - Order by year, ascending (in growing order)
+// Iteration 5: Ordering by year - Order by year, ascending (in growing order)534t
 function orderByYear() {}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
@@ -27,8 +82,6 @@ function turnHoursToMinutes() {}
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
 function bestYearAvg() {}
-
-
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
@@ -41,6 +94,6 @@ if (typeof module !== 'undefined') {
     orderByYear,
     orderAlphabetically,
     turnHoursToMinutes,
-    bestYearAvg,
+    bestYearAvg
   };
 }
