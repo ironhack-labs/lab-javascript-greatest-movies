89b012463da22bd0c7eee174533067d0125f579d
Alexander <alexander.nerz@web.de>
2022-04-01 08:13:53 +0200

    Solved lab

--- src/movies.js
+++ src/movies.js
@@ -1,26 +1,59 @@
 // The `movies` array from the file `src/data.js`.
-console.log('movies: ', movies);
+// console.log('movies: ', movies);
+
+const { NUMBER_UNARY_OPERATORS } = require("@babel/types");
 
 
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors() {}
-
+function getAllDirectors(movies) {
+  return movies.map(function (movie) {
+    return movie.director;
+  });
+}
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies() {}
+function howManyMovies(movies) {
+  if (movies.length === 0) {
+    return 0;
+  }
+
+
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage() {}
+function scoresAverage(movies) {
+  const totalScore = movies.reduce(function (accumulator, currentValue) {
+    return accumulator + currentValue.score;
+  }, 0)
+  const avgScore = (totalScore / movies.length);
+  return Number(avgScore.toFixed(2))
+}
+
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore() {}
+function dramaMoviesScore(movies) {
+  const dramaScore = movies.reduce(function (accumulator, currentValue) {
+    return accumulator + currentValue.genre['drama'];
+  }, 0)
+  const avgScore = (dramaScore / movies.length);
+  return Number(avgScore)
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear() {}
+function orderByYear(movies) {
+  movies.sort(function (a, b) {
+    return a.localCompare(b);
+  })
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically() {}
+function orderAlphabetically(movies) {
+
+
+
+
+}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes() {}
