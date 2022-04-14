91c817ea92c8a9abd6ed632168f7146a845d6814
Marina Palma <marina_palma@hotmail.com>
2022-04-14 21:01:52 +0100

    exercices 1 to 4

--- src/movies.js
+++ src/movies.js
@@ -1,20 +1,59 @@
 // The `movies` array from the file `src/data.js`.
-console.log('movies: ', movies);
 
+const movies = require('./data.js');
+
+//console.log('movies: ', movies);
+
+//
 
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors() {}
+
+function getAllDirectors(array) {
+  let directors = array.map((element) => element.director);
+  return directors;
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies() {}
+function howManyMovies(arr) {
+  let drama = arr.filter(
+    (element) =>
+      element.genre.includes('Drama') && element.director === 'Steven Spielberg'
+  );
+
+  return drama.length;
+}
+
+console.log(howManyMovies(movies));
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage() {}
+function scoresAverage(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+
+  let avg = arr.reduce(function (acc, value) {
+    if (!value.score) {
+      return acc;
+    }
+
+    return acc + value.score;
+  }, 0);
+
+  return Number((avg / arr.length).toFixed(2));
+}
+
+console.log(scoresAverage(movies));
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore() {}
+function dramaMoviesScore(moviesList) {
+  let dramaMovies = moviesList.filter((movie) => movie.genre.includes('Drama'));
+
+  return scoresAverage(dramaMovies);
+}
+
+console.log(dramaMoviesScore(movies));
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear() {}
@@ -28,8 +67,6 @@ function turnHoursToMinutes() {}
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
 function bestYearAvg() {}
 
-
-
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
 if (typeof module !== 'undefined') {
@@ -41,6 +78,6 @@ if (typeof module !== 'undefined') {
     orderByYear,
     orderAlphabetically,
     turnHoursToMinutes,
-    bestYearAvg,
+    bestYearAvg
   };
 }
