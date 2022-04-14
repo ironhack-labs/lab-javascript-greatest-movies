82d2622f7e449dcc2645c04b17a6a27377df9ee7
Juan Jose Ferran Carmona <jferran@gmail.com>
2022-04-14 17:39:37 +0200

    hechos 1 2 y 3

--- src/movies.js
+++ src/movies.js
@@ -5,16 +5,44 @@ console.log('movies: ', movies);
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors() {}
+function getAllDirectors(moviesParam) {
+  let directors = moviesParam.map((eachElemment)=>{
+    return eachElemment.director
+  })
+  return directors
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies() {}
+function howManyMovies(moviesParam) {
+  return moviesParam.filter((eachElem)=>{
+    return eachElem.director==='Steven Spielberg' && eachElem.genre.includes("Drama")
+  }).length
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage() {}
+function scoresAverage(moviesParam) {
+  //let counter=0;
+  let scores = moviesParam.reduce((acc, elem)=>{
+    return acc+elem.score;
+  },0)
+  return parseFloat((scores/moviesParam.length).toFixed(2))
+}
+//console.log(scoresAverage(movies))
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore() {}
+function dramaMoviesScore(moviesParam) {
+  let counter=0;
+  let scores = moviesParam.reduce((acc, elem)=>{
+    if(elem.genre.includes("Drama") && typeof(elem.score === "number")) {
+      counter++
+      console.log(counter, acc)
+      console.log(elem)
+      return acc+elem.score;
+    }
+  },0)
+  return parseFloat((scores/counter).toFixed(2))
+}
+console.log(dramaMoviesScore(movies))
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear() {}
