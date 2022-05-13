d0992abe884dfc9eb0360826dbee0ac85df81340
begoczb <begocz.31@gmail.com>
2022-05-13 13:03:31 +0200

    add second iteration

--- src/movies.js
+++ src/movies.js
@@ -1,14 +1,27 @@
 // The `movies` array from the file `src/data.js`.
-console.log('movies: ', movies);
-
+//console.log('movies: ', movies);
+// const movies = require('../src/data');
 
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors() {}
+function getAllDirectors(array) {
+  const directorsArray = array.map((element) => element.director);
+  return directorsArray;
+}
+
+//console log to check new array with live server
+// console.log(getAllDirectors(movies));
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies() {}
+function howManyMovies(array) {
+  return array.filter(
+    (element) =>
+      element.director === 'Steven Spielberg' && element.genre.includes('Drama')
+  ).length;
+}
+
+// console.log(howManyMovies(movies, 'Steven Spielberg'));
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
 function scoresAverage() {}
@@ -28,8 +41,6 @@ function turnHoursToMinutes() {}
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
 function bestYearAvg() {}
 
-
-
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
 if (typeof module !== 'undefined') {
@@ -41,6 +52,6 @@ if (typeof module !== 'undefined') {
     orderByYear,
     orderAlphabetically,
     turnHoursToMinutes,
-    bestYearAvg,
+    bestYearAvg
   };
 }
