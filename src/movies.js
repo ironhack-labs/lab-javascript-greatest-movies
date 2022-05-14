ff151808687eafd461ff8436ac53126e8d269bab
CrisArranz <c.arranz.arevalo@gmail.com>
2022-05-14 11:11:01 +0200

    Refacturing

--- src/movies.js
+++ src/movies.js
@@ -22,37 +22,66 @@ const getMessageYear = (movies) => `The best year was ${movies[0].year} with an 
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-const getAllDirectors = (movies) => movies.map((obj) => obj.director).filter((director, index, matrix) => matrix.indexOf(director) === index);
+
+//const getAllDirectors = (movies) => movies.map((obj) => obj.director).filter((director, index, matrix) => matrix.indexOf(director) === index);
+
+function getAllDirectors (movies) { 
+  return Object.keys(movies.reduce((directors, movie) => {
+    if(!directors[movie.director]){
+      directors[movie.director] = true;
+    }
+    return directors;
+  }, {}));
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-const howManyMovies = (movies) => movies.filter((object) => object.director === 'Steven Spielberg').filter((object) => object.genre.indexOf('Drama') !== -1).length
+function howManyMovies(movies) {
+  return movies.filter((object) => object.director === 'Steven Spielberg' && object.genre.indexOf('Drama') !== -1).length
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-const scoresAverage = (movies) => isEmpty(movies) ? 0 : decimalOperation(movies.map((obj) => obj.score).filter((score) => typeof score === 'number').reduce((acc, cur) => acc + (cur / movies.length), 0))
+function scoresAverage(movies){
+  return isEmpty(movies) ? 0 : decimalOperation(movies.map((obj) => obj.score)
+    .filter((score) => typeof score === 'number')
+    .reduce((acc, cur) => acc + (cur / movies.length), 0)
+  )
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-const dramaMoviesScore = (movies) => !movies.length || !movies.filter((object) => object.genre.indexOf('Drama') !== -1).length ? 0 : decimalOperation(movies.filter((object) => object.genre.indexOf('Drama') !== -1).reduce((acc, cur, index, array) => acc + (cur.score / array.length), 0))
+function dramaMoviesScore(movies){
+  return !movies.length || !movies.filter((object) => object.genre.indexOf('Drama') !== -1).length ? 0 : decimalOperation(movies.filter((object) => object.genre.indexOf('Drama') !== -1)
+    .reduce((acc, cur, index, array) => acc + (cur.score / array.length), 0)
+  )
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-const orderByYear = (movies) => isEmpty(movies) ? null : movies.slice().sort(() => -1).sort((a,b) => a.year - b.year);
+function orderByYear(movies) {
+  return isEmpty(movies) ? null : movies.slice().sort(() => -1).sort((a,b) => a.year - b.year);
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-const orderAlphabetically = (movies) => movies.map((obj) => obj.title).sort().filter((obj, index) => index < 20);
+function orderAlphabetically(movies) {
+  return movies.map((obj) => obj.title).sort().filter((obj, index) => index < 20);
+} 
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-const turnHoursToMinutes = (movies) => movies.map((obj) => ({duration: calculateDateToMinutes(obj)}))
+function turnHoursToMinutes(movies) {
+  return movies.map((obj) => ({duration: calculateDateToMinutes(obj)}))
+}
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
-const bestYearAvg = (movies) => isEmpty(movies) ? null : 
-  getMessageYear(
-    getAllYears(movies)
-      .map((year) => (
-        {
-          year: year, 
-          score: decimalOperation(getScoreYear(movies, year).reduce((acc, cur, index, array) => acc + (cur / array.length), 0))
-        }
-      ))
-      .sort((a, b) => b.score - a.score));
+function bestYearAvg(movies) {
+  return isEmpty(movies) ? null : 
+    getMessageYear(
+      getAllYears(movies)
+        .map((year) => (
+          {
+            year: year, 
+            score: decimalOperation(getScoreYear(movies, year).reduce((acc, cur, index, array) => acc + (cur / array.length), 0))
+          }
+        ))
+        .sort((a, b) => b.score - a.score));
+}
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
