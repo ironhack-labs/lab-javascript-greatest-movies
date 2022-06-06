205d1d8aa425711c45c4346c147c075847942cd5
monica <monicasastudy@gmail.com>
2022-06-06 17:06:19 +0200

    sin internet

--- src/movies.js
+++ src/movies.js
@@ -1,32 +1,49 @@
 // The `movies` array from the file `src/data.js`.
-console.log('movies: ', movies);
+//console.log('movies: ', movies);
 
 
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors() {}
+function getAllDirectors(movies) {
+  const allMovies = movies.map(elm => {
+    return elm.director
+  })
+  return allMovies
+}
+
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies() {}
+function howManyMovies(movies) {
+  const bestDirector = movies.filter(elm => {
+    return elm.director === 'Steven Spielberg' && elm.genre.includes('Drama')
+  })
+  return bestDirector.length
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage() {}
+function scoresAverage() { }
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore() {}
+function dramaMoviesScore(movie) {
+  const moviesDrama = movie.map(elm => {
+    const mediaDrama = elm.score / 2
+    return mediaDrama.toFixed(2)
+  })
+  return moviesDrama
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear() {}
+function orderByYear() { }
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically() {}
+function orderAlphabetically() { }
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-function turnHoursToMinutes() {}
+function turnHoursToMinutes() { }
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
-function bestYearAvg() {}
+function bestYearAvg() { }
 
 
 
