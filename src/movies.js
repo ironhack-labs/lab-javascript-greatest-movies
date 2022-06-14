fb1f6995c0ce0ad658073019c2613a066ead3b6f
Uros Cirkovic <cirkovic.uros.srb@gmail.com>
2022-06-14 10:49:01 +0200

    Update starter code in the movies.js
    - Make the parameter names more explicit: ~~films~~ -> `moviesArray`

--- src/movies.js
+++ src/movies.js
@@ -5,28 +5,28 @@ console.log('movies: ', movies);
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(films) {}
+function getAllDirectors(moviesArray) {}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(films) {}
+function howManyMovies(moviesArray) {}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(films) {}
+function scoresAverage(moviesArray) {}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(films) {}
+function dramaMoviesScore(moviesArray) {}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(films) {}
+function orderByYear(moviesArray) {}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically(films) {}
+function orderAlphabetically(moviesArray) {}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-function turnHoursToMinutes(films) {}
+function turnHoursToMinutes(moviesArray) {}
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
-function bestYearAvg(films) {}
+function bestYearAvg(moviesArray) {}
 
 
 
