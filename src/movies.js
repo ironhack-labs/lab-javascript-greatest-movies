0a74f1857b8db11e8a95998e39cfe0893185dfa0
“Alexandre <coma.alexandre@gmail.com>
2022-06-20 22:15:01 +0200

    Lab solved until iteration 4

--- src/movies.js
+++ src/movies.js
@@ -1,16 +1,46 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
+
+const movies = require("./data");
+
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {}
+function getAllDirectors(moviesArray) {
+  const allDirectors = moviesArray.map(function(movies){
+      return movies.director
+  });
+  return allDirectors
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
+function howManyMovies(moviesArray) {
+  const spielbergDramas = moviesArray.filter(function(movies) {
+    return (movies.director === 'Steven Spielberg' && movies.genre.includes('Drama'))
+  })
+  return spielbergDramas.length
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+function scoresAverage(moviesArray) {
+  const totalScore = moviesArray.reduce(function(accumulator, currentValue) {
+    return accumulator + currentValue.score
+  },0)
+  return Math.round((totalScore / moviesArray.length) * 100) / 100
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(moviesArray) {}
+function dramaMoviesScore(moviesArray) {
+  let counter = 0;
+  const totalDramaScore = moviesArray.reduce(function(accumulator,currentValue){
+    if (currentValue.genre.includes(`Drama`)){
+      counter++
+      return accumulator + currentValue.score
+    }
+    else {
+      return 0
+    }
+  },0)
+  return Math.round(Math.round((totalDramaScore / counter) * 100)/100)
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear(moviesArray) {}
