fbbf6cb05927e3ea6c980d42ceced9251bcfc823
Tobias Conrad <max.tobias.conrad@gmail.com>
2022-06-21 22:21:01 +0200

    5-7

--- src/movies.js
+++ src/movies.js
@@ -9,13 +9,14 @@ function getAllDirectors(moviesArray) {
 } 
 
 
+
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 function howManyMovies(moviesArray) {
     const arrOfSpielbergDrama =  moviesArray.filter((element) => element.director === "Steven Spielberg" && element.genre.indexOf("Drama") >= 0 )    
     return arrOfSpielbergDrama.length
   }
-
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
+
 function scoresAverage(moviesArray) {
   if (moviesArray.length === 0){
       return 0
@@ -28,8 +29,6 @@ function scoresAverage(moviesArray) {
   return parseFloat(average.toFixed(2))
 }
 
-
-
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesScore(moviesArray) {
   const dramaArr = moviesArray.filter((element) => element.genre.indexOf("Drama") >= 0 && typeof element.score === "number") 
@@ -40,17 +39,36 @@ function dramaMoviesScore(moviesArray) {
   const avg = sumDramaScore / dramaArr.length
   return parseFloat(avg.toFixed(2))
   }
-  
-
+  let name = 20
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
+function orderByYear(moviesArray) {
+  const copy = JSON.parse(JSON.stringify(moviesArray));
+  let result = copy.sort((a,b ) => a.year -b.year )
+  return result.sort()
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically(moviesArray) {}
+function orderAlphabetically(moviesArray) {
+  const copy = JSON.parse(JSON.stringify(moviesArray));
+  let titleArr = copy.map(element => element.title)
+  let orderedTitle = titleArr.sort((a, b) => a.localeCompare(b))
+  return orderedTitle.slice(0,20)
+}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-function turnHoursToMinutes(moviesArray) {}
+function turnHoursToMinutes(moviesArray) {
+  const copy = JSON.parse(JSON.stringify(moviesArray));
+  let toMinutes = copy.map(function (element){ 
+    let hours  = element.duration.slice(0,1) 
+    let minutes = element.duration.slice(3,5)
+    element.duration = Number (hours) *60  + Number(minutes)
+    return element
+    })
+return toMinutes
+}
+
+
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
 function bestYearAvg(moviesArray) {}
