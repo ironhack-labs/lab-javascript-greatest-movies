1dffad2b2a0da97f98735413ae47722f246f3ca4
Samuel Taboas <droidsam@gmail.com>
2022-07-01 20:51:49 +0200

    add test for function definition and return type

--- tests/iteration2.spec.js
+++ tests/iteration2.spec.js
@@ -2,3 +2,26 @@ const movies = require('../src/data');
 const { howManyMovies } = require('../src/movies');
 
 // Iteration 2
+describe('Iteration 2: Steven Spielberg. The best?', () => {
+  test('should be implemented by a function named howManyMovies()', () => {
+    expect(typeof howManyMovies).toBe('function');
+  });
+
+  test('should return an array', () => {
+    expect(Array.isArray(howManyMovies())).toBe(true);
+  });
+
+  test('should return only drama movies when all movies are drama movies', () => {
+    const input = [
+      {
+        title: 'The Shawshank Redemption',
+        year: 1994,
+        director: 'Frank Darabont',
+        duration: '2h 22min',
+        genre: ['Crime', 'Drama'],
+        score: 9.3
+      }
+    ];
+    expect(howManyMovies(input)).toBe(input);
+  });
+});
