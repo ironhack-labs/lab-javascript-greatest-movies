7c4aea5b2cd0d0a163e2ec90bffae0043a13f51e
Samuel Taboas <droidsam@gmail.com>
2022-07-01 21:04:29 +0200

    add test inmutability

--- tests/iteration2.spec.js
+++ tests/iteration2.spec.js
@@ -11,7 +11,7 @@ describe('Iteration 2: Steven Spielberg. The best?', () => {
     expect(Array.isArray(howManyMovies())).toBe(true);
   });
 
-  test('should return only drama movies when all movies are drama movies', () => {
+  test('should return only drama movies when there are only one movies and is a drama', () => {
     const input = [
       {
         title: 'The Shawshank Redemption',
@@ -45,5 +45,84 @@ describe('Iteration 2: Steven Spielberg. The best?', () => {
       }
     ];
     expect(howManyMovies(input).length).toBe(1);
+  });
+
+  test('should return the all drama movies when all movies are dramas', () => {
+    const input = [
+      {
+        title: 'The Shawshank Redemption',
+        year: 1994,
+        director: 'Frank Darabont',
+        duration: '2h 22min',
+        genre: ['Crime', 'Drama'],
+        score: 9.3
+      },
+      {
+        title: 'The Godfather',
+        year: 1972,
+        director: 'Francis Ford Coppola',
+        duration: '2h 55min',
+        genre: ['Crime', 'Drama'],
+        score: 9.2
+      },
+      {
+        title: 'The Godfather: Part II',
+        year: 1974,
+        director: 'Francis Ford Coppola',
+        duration: '3h 22min',
+        genre: ['Crime', 'Drama'],
+        score: 9
+      },
+      {
+        title: 'The Dark Knight',
+        year: 2008,
+        director: 'Christopher Nolan',
+        duration: '2h 32min',
+        genre: ['Action', 'Crime', 'Drama', 'Thriller'],
+        score: 9
+      }
+    ];
+    expect(howManyMovies(input).length).toBe(4);
+  });
+
+  test('should no modify the input movies array', () => {
+    const input = [
+      {
+        title: 'The Shawshank Redemption',
+        year: 1994,
+        director: 'Frank Darabont',
+        duration: '2h 22min',
+        genre: ['Crime', 'Drama'],
+        score: 9.3
+      },
+      {
+        title: 'The Godfather',
+        year: 1972,
+        director: 'Francis Ford Coppola',
+        duration: '2h 55min',
+        genre: ['Crime', 'Drama'],
+        score: 9.2
+      },
+      {
+        title: 'The Godfather: Part II',
+        year: 1974,
+        director: 'Francis Ford Coppola',
+        duration: '3h 22min',
+        genre: ['Crime', 'Drama'],
+        score: 9
+      },
+      {
+        title: 'The Dark Knight',
+        year: 2008,
+        director: 'Christopher Nolan',
+        duration: '2h 32min',
+        genre: ['Action', 'Crime', 'Drama', 'Thriller'],
+        score: 9
+      }
+    ];
+    const filteredMovies = howManyMovies(input);
+    input.pop();
+
+    expect(filteredMovies.length).not.toBe(input.length);
   });
 });
