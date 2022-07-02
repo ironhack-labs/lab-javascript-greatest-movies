c44ce3e1768f968888206537e294661765e5a951
Samuel Taboas <droidsam@gmail.com>
2022-07-02 15:37:10 +0200

    add test for average best year

--- tests/iteration8.spec.js
+++ tests/iteration8.spec.js
@@ -50,4 +50,83 @@ describe('BONUS - Iteration 8: Best yearly score average', () => {
       'The best year was 1998 with an average score of 8.6'
     );
   });
+
+  test('should return "The best year was 1998 with an average score of 8.6" when input two movie array one of 1998 and score 8.6 and another lower score from 1984', () => {
+    const input = [
+      {
+        title: 'Saving Private Ryan',
+        year: 1998,
+        director: 'Steven Spielberg',
+        duration: '2h 49min',
+        genre: ['Drama', 'War'],
+        score: 8.6
+      },
+      {
+        title: 'Once Upon a Time in America',
+        year: 1984,
+        director: 'Sergio Leone',
+        duration: '3h 49min',
+        genre: ['Crime', 'Drama'],
+        score: 8.4
+      }
+    ];
+    expect(bestYearAvg(input)).toBe(
+      'The best year was 1998 with an average score of 8.6'
+    );
+  });
+
+  test('should return "The best year was 1998 with an average score of 8.6" when input two movie array one of 1998 and score 8.6 and another movie without score from 1984', () => {
+    const input = [
+      {
+        title: 'Saving Private Ryan',
+        year: 1998,
+        director: 'Steven Spielberg',
+        duration: '2h 49min',
+        genre: ['Drama', 'War'],
+        score: 8.6
+      },
+      {
+        title: 'Once Upon a Time in America',
+        year: 1984,
+        director: 'Sergio Leone',
+        duration: '3h 49min',
+        genre: ['Crime', 'Drama']
+      }
+    ];
+    expect(bestYearAvg(input)).toBe(
+      'The best year was 1998 with an average score of 8.6'
+    );
+  });
+
+  test('should return "The best year was 1998 with an average score of 8.6" when input two movie array one of 1998 and score 8.6 and another two movies from 1984 with a high score but lower mean', () => {
+    const input = [
+      {
+        title: 'Saving Private Ryan',
+        year: 1998,
+        director: 'Steven Spielberg',
+        duration: '2h 49min',
+        genre: ['Drama', 'War'],
+        score: 8.6
+      },
+      {
+        title: 'Raiders of the Lost Ark',
+        year: 1981,
+        director: 'Steven Spielberg',
+        duration: '1h 55min',
+        genre: ['Action', 'Adventure'],
+        score: 9
+      },
+      {
+        title: 'Das Boot',
+        year: 1981,
+        director: 'Wolfgang Petersen',
+        duration: '2h 29min',
+        genre: ['Adventure', 'Drama', 'Thriller', 'War'],
+        score: 5
+      }
+    ];
+    expect(bestYearAvg(input)).toBe(
+      'The best year was 1998 with an average score of 8.6'
+    );
+  });
 });
