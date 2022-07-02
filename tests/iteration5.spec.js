3052475e96a9a89e5ad43745d8c527e295a19b00
Samuel Taboas <droidsam@gmail.com>
2022-07-02 09:46:26 +0200

    add test orderByYear returns an ordered array with several movies

--- tests/iteration5.spec.js
+++ tests/iteration5.spec.js
@@ -49,4 +49,43 @@ describe('Iteration 5: Order by year', () => {
     ];
     expect(orderByYear(input)[0].year).toBe(1981);
   });
+
+  test('should return the array with 1981 movie first, and 1989 movie in second place when the input array has several movies and the 1981 and 1989 are the oldest', () => {
+    const input = [
+      {
+        title: 'Indiana Jones and the Last Crusade',
+        year: 1989,
+        director: 'Steven Spielberg',
+        duration: '2h 7min',
+        genre: ['Action', 'Adventure', 'Drama'],
+        score: 8.3
+      },
+      {
+        title: 'Raiders of the Lost Ark',
+        year: 1981,
+        director: 'Steven Spielberg',
+        duration: '1h 55min',
+        genre: ['Action', 'Drama'],
+        score: 8.5
+      },
+      {
+        title: 'Saving Private Ryan',
+        year: 1998,
+        director: 'Steven Spielberg',
+        duration: '2h 49min',
+        genre: ['Drama', 'War'],
+        score: 8.6
+      },
+      {
+        title: 'Schindler"s List',
+        year: 1993,
+        director: 'Steven Spielberg',
+        duration: '3h 15min',
+        genre: ['Biography', 'Drama', 'History'],
+        score: 8.9
+      }
+    ];
+    expect(orderByYear(input)[0].year).toBe(1981);
+    expect(orderByYear(input)[1].year).toBe(1989);
+  });
 });
