d7f16bb385997308fec23b99e6d1fd746b3005bb
Walison Abram <abramwhas@gmail.com>
2022-07-06 03:18:42 -0300

    add iteration 2

--- src/movies.js
+++ src/movies.js
@@ -7,7 +7,11 @@ function getAllDirectors(moviesArray) {
 }
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
+function howManyMovies(moviesArray) {
+  let director = moviesArray.filter(movie => movie.director === 'Steven Spielberg')
+  let genAndDirector = director.filter(movie => movie.genre.filter(gen => gen === 'Drama').length !== 0)
+  return genAndDirector.length
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
 function scoresAverage(moviesArray) {}
