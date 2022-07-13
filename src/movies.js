287d5123bcf86aa3d47fae0dd05d50a343198bd0
Walison Abram <abramwhas@gmail.com>
2022-07-13 19:05:11 -0300

    add iteration 6

--- src/movies.js
+++ src/movies.js
@@ -43,7 +43,13 @@ function orderByYear(moviesArray) {
 }
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically(moviesArray) {}
+function orderAlphabetically(moviesArray) {
+  let newArr = [...moviesArray]
+  let orderByTitle = newArr.sort((a, b) => {
+    return a.title.localeCompare(b.title)
+  }).map(movie => movie.title)
+  return orderByTitle.slice(0, 20)
+}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes(moviesArray) {}
