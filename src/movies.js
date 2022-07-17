c2ba3a82c34a8e26e97339de7b4869fb0c1e95af
Renata Inojosa <inojosa.renata@gmail.com>
2022-07-17 19:21:34 -0300

    iteration4

--- src/movies.js
+++ src/movies.js
@@ -1,24 +1,53 @@
+console.log('js conectado!')
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 
 const movies = require("./data");
 
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {}
+
+function getAllDirectors(moviesArray) {
+  const movieDirectors = moviesArray.map((movie) => movie.director); 
+  return movieDirectors;
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 function howManyMovies(moviesArray) {
-  movies.filter(function moviesArray () {
-  if (movies.director === 'Steven Spielberg')console.log(movies.title);
-  } )
- // return moviesArray
+  const spielbergMovies = moviesArray.filter((movies) => movies.director === 'Steven Spielberg' && movies.genre.includes("Drama")); 
+  return spielbergMovies.length;
 }
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+function scoresAverage(moviesArray) {
+  if(!moviesArray.length) return 0; 
+
+   let sum = moviesArray.reduce((acc, movie) =>{
+    if (movie.score) {
+    return acc + movie.score
+    } else return acc;
+   }, 0);
+
+let average = sum / moviesArray.length
+return Number(average.toFixed(2));
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(moviesArray) {}
+function dramaMoviesScore(moviesArray) {
+  const dramaMovies = moviesArray.filter((movie) => {
+    return movie.genre.includes('Drama')}); 
+
+  const sumScore = dramaMovies.reduce((sum, movie) => {
+    return sum + movie.score;
+  }, 0);
+
+  const average = sumScore / dramaMovies.length; 
+  
+  return Number(average.toFixed(2));
+  }; 
+ 
+
+
+
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
  
