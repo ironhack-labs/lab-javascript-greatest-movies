39acc53d0109a44741acb52a71b7da15bb0a0046
Aberinkula36 <jmsaezteruel@hotmail.com>
2022-07-19 18:13:56 +0200

    Solved lab

--- src/movies.js
+++ src/movies.js
@@ -1,22 +1,135 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {}
+function getAllDirectors(moviesArray) {
+  const directors = moviesArray.map((director) => director.director);
+  console.log(directors);
+  return directors;
+}
+
+getAllDirectors(moviesArray);
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
+function howManyMovies(moviesArray) {
+  const spielbergMovies = moviesArray.filter((movie) => {
+    return (
+      movie.director === "Steven Spielberg" && movie.genre.includes("Drama")
+    );
+  });
+  console.log(spielbergMovies);
+  return spielbergMovies.length;
+}
+
+howManyMovies(movies);
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+function scoresAverage(moviesArray) {
+  if (moviesArray == "") {
+    return 0;
+  }
+  const sumaArray = moviesArray
+    .filter(function (movie) {
+      return movie.score;
+    })
+    //ES6 .filter((movie) => movie.score)
+    .map(function (movie) {
+      return movie.score;
+    })
+    .reduce(function (accumulator, movie) {
+      return accumulator + movie;
+    });
+  //ES6 .reduce((accumulator, movie) => accumulator + movie)
+  const media = sumaArray / moviesArray.length;
+  console.log(media);
+  let mediaDecimales = Math.round(media * 100) / 100;
+  return mediaDecimales;
+}
+
+scoresAverage(movies);
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(moviesArray) {}
+function dramaMoviesScore(moviesArray) {
+  const averageDrama = moviesArray
+    .filter((movie) => movie.genre.includes("Drama"));
+    const allDrama = averageDrama 
+    .map((movie) => movie.score)
+    .reduce((accumulator, value) => accumulator + value);
+
+    if (allDrama.length === 0) { return 0 };
+
+  const mediaDrama = allDrama / averageDrama.length;
+  let mediadramaDecimales = Math.round(mediaDrama * 100) / 100;
+  return mediadramaDecimales;
+}
+
+dramaMoviesScore(movies);
+
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
+function orderByYear(moviesArray) {
+  const ordenarArray = moviesArray
+  .sort((a, b) => {
+    if (a.year == b.year) {
+      return 0;
+    }
+    if (a.year < b.year) {
+      return -1;
+    }
+    return 1;
+  })
+  .map((movie) => movie)
+  console.log(ordenarArray);
+  return ordenarArray;
+}
+
+orderByYear(movies);
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically(moviesArray) {}
+//SOLUCIÓN 1
+function orderAlphabetically(moviesArray) {
+  const ordenarAlfa = moviesArray
+  .sort((a, b) => {
+    if (a.title == b.title) {
+      return 0;
+    }
+    if (a.title < b.title) {
+      return -1;
+    }
+    return 1;
+  })
+  .map((movie) => movie.title)
+  console.log(ordenarAlfa);
+  for (let i = 0; i < 19; i++){
+    console.log(ordenarAlfa[i]);
+    
+  }
+  return ordenarAlfa;
+}
+
+orderAlphabetically(movies);
+
+//SOLUCIÓN 2
+function orderAlphabetically(moviesArray) {
+  const ordenarAlfa = moviesArray
+  .sort((a, b) => {
+    if (a.title == b.title) {
+      return 0;
+    }
+    if (a.title < b.title) {
+      return -1;
+    }
+    return 1;
+  })
+  .map((movie) => movie.title)
+  let veintePelis = [];
+  for (let i = 0; i < 19; i++){
+    veintePelis.push(ordenarAlfa[i]);
+  }
+  console.log(veintePelis);
+  return veintePelis;
+}
+
+orderAlphabetically(movies);
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes(moviesArray) {}
