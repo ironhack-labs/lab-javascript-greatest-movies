f2a41307e0573ebe7dae5644bd3492bbcefe8d92
Elena Vida <elenavidaluc3a@gmail.com>
2022-07-27 11:04:51 +0200

    Greatest Movies of all time JS

--- src/movies.js
+++ src/movies.js
@@ -1,9 +1,13 @@
+const movies = require("./data");
+
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
 function getAllDirectors(moviesArray) {
   return moviesArray.map((movie) => movie.director);
 }
+console.log(getAllDirectors(movies));
+
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 function howManyMovies(moviesArray) {
   return moviesArray.filter(
@@ -11,6 +15,7 @@ function howManyMovies(moviesArray) {
       movie.director === "Steven Spielberg" && movie.genre.includes("Drama")
   ).length;
 }
+console.log(howManyMovies(movies));
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
 function scoresAverage(moviesArray) {
@@ -30,12 +35,14 @@ function scoresAverage(moviesArray) {
     ).toFixed(2)
   );
 }
+console.log(scoresAverage(movies));
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesScore(moviesArray) {
   const dramas = moviesArray.filter((movie) => movie.genre.includes("Drama"));
   return scoresAverage(dramas);
 }
+console.log(dramaMoviesScore(movies));
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear(moviesArray) {
@@ -49,6 +56,7 @@ function orderByYear(moviesArray) {
     })
     .sort((a, b) => a.year - b.year);
 }
+console.log(orderByYear(movies));
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically(moviesArray) {
@@ -63,6 +71,7 @@ function orderAlphabetically(moviesArray) {
     .slice(0, 20)
     .map((movie) => movie.title);
 }
+console.log(orderAlphabetically(movies));
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes(moviesArray) {}
