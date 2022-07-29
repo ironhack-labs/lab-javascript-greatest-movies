8f76cd93c62e1dbd28d64cc783966385c7fcba70
Jonathan Raposo <jonathan.raposo.luna@gmail.com>
2022-07-29 19:25:16 -0400

    lab-movies

--- src/movies.js
+++ src/movies.js
@@ -1,38 +1,13 @@
+
+
+
+
+
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
 
 
-
-const movies = [
-    {
-      title: 'The Shawshank Redemption',
-      year: 1994,
-      director: 'Frank Darabont',
-      duration: '2h 22min',
-      genre: ['Crime', 'Drama'],
-      score: 9.3
-    },
-    {
-      title: 'The Godfather',
-      year: 1972,
-      director: 'Francis Ford Coppola',
-      duration: '2h 55min',
-      genre: ['Crime', 'Drama'],
-      score: 9.2
-    },
-    {
-      title: 'The Godfather: Part II',
-      year: 1974,
-      director: 'Francis Ford Coppola',
-      duration: '3h 22min',
-      genre: ['Crime', 'Drama'],
-      score: 9
-    }
-];
-
-// Iteration 1 (MAP): The directors? - Get the array of all directors.
-// _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
 
 function getAllDirectors(moviesArray) {
 
@@ -40,31 +15,67 @@ function getAllDirectors(moviesArray) {
     return movie.director
   })
   return allDirectors
-
 }
 
-
-console.log(getAllDirectors(movies));
 
 
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 function howManyMovies(moviesArray) {
+      
+  if(moviesArray.length === 0){
+    return 0;
+  }
 
+   let spielbergMovies = moviesArray.filter( movie => {
+    if(movie.director ==='Steven Spielberg' && movie.genre.includes('Drama')){
+      return movie;
+    }
+   })
+  
+   return spielbergMovies.length;
    
-
 }
 
 
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+function scoresAverage(moviesArray) {
+
+    if(!moviesArray.length){
+      return 0;
+    }
+
+    let total = moviesArray.reduce(function(acc,curr){
+      return acc + curr.score;
+    },0)
+
+      const average = total/moviesArray.length;
+      const convertedNumber = Number(average.toFixed(2));
+      return convertedNumber;
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(moviesArray) {}
+function dramaMoviesScore(moviesArray) {
+
+  if(moviesArray.length === 1){
+    return moviesArray[0].score;
+   }
+
+    let dramaMovies = moviesArray.filter( movie => {
+      if(movie.genre.includes('Drama')){
+         return movie;
+      }
+    })
+         const DramaMoviesAverage = scoresAverage(dramaMovies)
+      return DramaMoviesAverage;
+
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
+function orderByYear(moviesArray) {
+  
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically(moviesArray) {}
