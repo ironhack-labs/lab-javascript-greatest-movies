eb6bccffffa49ad53c889d8ee43ef93d5486d1de
Jonathan Raposo <jonathan.raposo.luna@gmail.com>
2022-07-29 22:34:35 -0400

    modify

--- src/movies.js
+++ src/movies.js
@@ -87,7 +87,7 @@ function orderByYear(moviesArray) {
         return -1;
       } else if( a > b){
         return 1;
-      } else if(a.title < b.tittle){
+      } else if(a.title < b.title){
         return -1;
       } else if( a.title > b.title){
         return 1;
