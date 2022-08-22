11ebb8ed46982c8bb765ae8b01d0155d862bb0dd
Andrew <ayjtw7@gmail.com>
2022-08-22 17:49:24 -0400

    finished challenge. just need to add final validation for certain tests

--- src/movies.js
+++ src/movies.js
@@ -1,3 +1,35 @@
+// moviesArray = [{
+//         title: '12 Angry Men',
+//         year: 1957,
+//         director: 'Sidney Lumet',
+//         duration: '1h 36min',
+//         genre: ['Crime', 'Drama'],
+//         score: 8.9
+//     },
+//     {
+//         title: 'Schindler"s List',
+//         year: 1993,
+//         director: 'Steven Spielberg',
+//         duration: '3h 15min',
+//         genre: ['Biography', 'Drama', 'History'],
+//         score: 8.9
+//     },
+//     {
+//         title: 'Pulp Fiction',
+//         year: 1994,
+//         director: 'Quentin Tarantino',
+//         duration: '2h 34min',
+//         genre: ['Crime'],
+//         score: 8.9
+//     }
+// ]
+
+
+
+
+
+
+
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
@@ -20,53 +52,86 @@ function howManyMovies(moviesArray) {
     return x.length
 }
 
-
-moviesArray = [{
-        title: '12 Angry Men',
-        year: 1957,
-        director: 'Sidney Lumet',
-        duration: '1h 36min',
-        genre: ['Crime', 'Drama'],
-        score: 8.9
-    },
-    {
-        title: 'Schindler"s List',
-        year: 1993,
-        director: 'Steven Spielberg',
-        duration: '3h 15min',
-        genre: ['Biography', 'Drama', 'History'],
-        score: 8.9
-    },
-    {
-        title: 'Pulp Fiction',
-        year: 1994,
-        director: 'Quentin Tarantino',
-        duration: '2h 34min',
-        genre: ['Crime', 'Drama'],
-        score: 8.9
-    }
-]
-
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
 function scoresAverage(moviesArray) {
-    return moviesArray.reduce(function (acc, movie) {
+    let x = moviesArray.reduce(function (acc, movie) {
         console.log(movie.score)
-        movie = movie.score
-        return acc + movie
+        //movie = movie.score
+        return acc + movie.score
         // return acc / moviesArray
-    }, 0) / moviesArray.length
+    }, 0)
+    console.log(x)
+    x = x / moviesArray.length
+    console.log(x)
+
+    x = x.toFixed(2) // converts the DATA to a string!!!!
+    console.log(x)
+
+    return Number(x)
 }
 
-console.log(scoresAverage(moviesArray).toFixed(2))
+// console.log(scoresAverage(moviesArray))
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(moviesArray) {}
+function dramaMoviesScore(moviesArray) {
+
+    let a = moviesArray.filter(function (movie) {
+        if (movie.genre.includes('Drama')) {
+            return movie
+        }
+    })
+
+    console.log(a)
+
+
+    let x = a.reduce(function (acc, movie) {
+        console.log(a.length)
+        return acc + movie.score
+    }, 0)
+
+    x = x / a.length
+
+    console.log(x)
+    return x
+
+    // x = x / moviesArray.length
+
+
+}
+
+console.log(dramaMoviesScore(moviesArray))
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
+function orderByYear(moviesArray) {
+    let x = [...moviesArray].sort(function (a, b) {
+        if (a.year > b.year) {
+            return 1
+        } else if (b.year > a.year) {
+            return -1
+        } else {
+            return a.title.localeCompare(b.title) //When the years match, they will be ranked by year
+        }
+    })
+
+    console.log(x)
+    return x
+}
+
+console.log(orderByYear(moviesArray))
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically(moviesArray) {}
+function orderAlphabetically(moviesArray) {
+    let x = [...moviesArray].map(function (movie) {
+        return movie.title
+    }).sort(function (a, b) {
+        return a.localeCompare(b)
+    })
+
+    console.log(x)
+    return x.slice(0,20)
+}
+
+console.log(orderAlphabetically(moviesArray))
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes(moviesArray) {}
