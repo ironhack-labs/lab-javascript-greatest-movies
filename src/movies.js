09d804a1bd3d61e9b189ff313321d4711b99f87c
Andrew <ayjtw7@gmail.com>
2022-08-22 14:40:49 -0400

    initial commit: iterations 1,2,3

--- src/movies.js
+++ src/movies.js
@@ -1,13 +1,63 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {}
+function getAllDirectors(moviesArray) {
+    let x = moviesArray.map(function (movie) {
+        return movie.director
+    })
+    return x
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
+function howManyMovies(moviesArray) {
+    // let counter = 0
+    let x = moviesArray.filter(function (movie) {
+        if (movie.genre.includes('Drama') && movie.director.includes('Spielberg')) {
+            return movie
+        }
+        // return counter
+    })
+    return x.length
+}
+
+
+moviesArray = [{
+        title: '12 Angry Men',
+        year: 1957,
+        director: 'Sidney Lumet',
+        duration: '1h 36min',
+        genre: ['Crime', 'Drama'],
+        score: 8.9
+    },
+    {
+        title: 'Schindler"s List',
+        year: 1993,
+        director: 'Steven Spielberg',
+        duration: '3h 15min',
+        genre: ['Biography', 'Drama', 'History'],
+        score: 8.9
+    },
+    {
+        title: 'Pulp Fiction',
+        year: 1994,
+        director: 'Quentin Tarantino',
+        duration: '2h 34min',
+        genre: ['Crime', 'Drama'],
+        score: 8.9
+    }
+]
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+function scoresAverage(moviesArray) {
+    return moviesArray.reduce(function (acc, movie) {
+        console.log(movie.score)
+        movie = movie.score
+        return acc + movie
+        // return acc / moviesArray
+    }, 0) / moviesArray.length
+}
+
+console.log(scoresAverage(moviesArray).toFixed(2))
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesScore(moviesArray) {}
