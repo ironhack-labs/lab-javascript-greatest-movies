3d946e7331425a164dd3a4e476cd6be9d39fa42a
berkayyildirim <huseyinberkayyildirim@gmail.com>
2022-09-12 22:14:15 +0300

    do iteration 6

--- src/movies.js
+++ src/movies.js
@@ -51,16 +51,27 @@ function dramaMoviesScore(moviesArray) {
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear(moviesArray) {
-    let moviesCopy = moviesArray.slice();
-   let moviesSort =  moviesCopy.sort(function(a, b) {
-        return a.year - b.year  ||  a.title.localeCompare(b.title);
-      });
-    return moviesSort;
+  let moviesCopy = moviesArray.slice();
+  let moviesSort = moviesCopy.sort(function (a, b) {
+    return a.year - b.year || a.title.localeCompare(b.title);
+  });
+  return moviesSort;
 }
 
-
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically(moviesArray) {}
+function orderAlphabetically(moviesArray) {
+  if (moviesArray.length < 20) {
+    let moviesCopy = moviesArray.slice();
+  } else {
+    let moviesCopy = moviesArray.slice(0, 20);
+  }
+
+  const moviesAlphabetically = moviesCopy.map(function (obj) {
+    return obj.title;
+  });
+
+  return moviesAlphabetically;
+}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes(moviesArray) {}
