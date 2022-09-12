ec0bd5cee31d771784a72d8ac4cb11c5f7ec138d
Saskia Peach <saskiapeach@gmail.com>
2022-09-12 18:31:27 +0100

    complete iterations 1 and 2 so far

--- src/movies.js
+++ src/movies.js
@@ -1,13 +1,63 @@
 // Iteration 1: All directors? - Get the array of all directors.
+
+
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {}
+function getAllDirectors(moviesArray) {
+  const directors = moviesArray.map(function (movie) {
+    return movie.director;
+  });
+  return directors;
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
+function howManyMovies(moviesArray) {
+    const stephenMovies = moviesArray.filter(function(movie){
+        return  movie.director === 'Steven Spielberg' && movie.genre.includes('Drama')})
+    return stephenMovies.length
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+const ourMovies = [
+    {
+      title: 'The Shawshank Redemption',
+      year: 1994,
+      director: 'Frank Darabont',
+      duration: '2h 22min',
+      genre: ['Crime', 'Drama'],
+      score: 9.3
+    },
+    {
+      title: 'The Godfather',
+      year: 1972,
+      director: 'Francis Ford Coppola',
+      duration: '2h 55min',
+      genre: ['Crime', 'Drama'],
+      score: 9.2
+    },
+    {
+      title: 'The Godfather: Part II',
+      year: 1974,
+      director: 'Francis Ford Coppola',
+      duration: '3h 22min',
+      genre: ['Crime', 'Drama']
+    }]
+
+
+function scoresAverage(moviesArray) {
+    if(moviesArray.length === 0) {return 0};
+    const totalScore = moviesArray.reduce(function(accumulator,currentValue){
+        if(!moviesArray.score){return accumulator + 0}
+        return accumulator + currentValue.score;
+    }, 0) 
+    const total = totalScore/moviesArray.length;
+    return total.toFixed(2);
+}
+
+let result = scoresAverage(ourMovies)
+console.log(result)
+
+   
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesScore(moviesArray) {}
