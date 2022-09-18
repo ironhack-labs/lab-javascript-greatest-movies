55d05b303347bae55f1b58abfa30be32748e6c1c
Roxanie Fernandes <roxaniedmfernandes@gmail.com>
2022-09-18 15:19:20 -0300

    Iteration 6

--- src/movies.js
+++ src/movies.js
@@ -60,8 +60,12 @@ function orderByYear(moviesArray) {
     return newMoviesArray;
 }
 
-// Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically(moviesArray) {}
+// Iteration 6: Alphabetic Order - Order by title and print the first 20 titles 
+function orderAlphabetically(moviesArray) {
+    const titlesArray = moviesArray.map(movie => movie.title)
+    titlesArray.sort();
+    return titlesArray.slice(0, 20)
+}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes(moviesArray) {}
