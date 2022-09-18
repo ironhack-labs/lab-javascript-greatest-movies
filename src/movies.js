ae141e7ec2688eab53877740e22d81f6d0b627c9
Roxanie Fernandes <roxaniedmfernandes@gmail.com>
2022-09-18 13:51:16 -0300

    Iteration 3

--- src/movies.js
+++ src/movies.js
@@ -3,22 +3,36 @@
 // How could you "clean" a bit this array and make it unified (without duplicates)?
 
 function getAllDirectors(moviesArray) {
-    const allDirectors= moviesArray.map(movie =>
-        movie.director)
-        return allDirectors;
-    }
+  const allDirectors = moviesArray.map((movie) => movie.director);
+  return allDirectors;
+}
 
-    // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-    function howManyMovies(moviesArray) {
-        if(!moviesArray.length){
-            return 0
-        }
-        const dramaStevenSpielberg = moviesArray.filter(movie => movie.genre.includes('Drama') && movie.director === 'Steven Spielberg')
-        return dramaStevenSpielberg.length;
-    }
+// Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
+function howManyMovies(moviesArray) {
+  if (!moviesArray.length) {
+    return 0;
+  }
+  const dramaStevenSpielberg = moviesArray.filter(
+    (movie) =>
+      movie.genre.includes("Drama") && movie.director === "Steven Spielberg"
+  );
+  return dramaStevenSpielberg.length;
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+function scoresAverage(moviesArray) {
+    if(!moviesArray.length){
+        return 0
+    }
+    let sum = moviesArray.reduce((acc,movie) => {
+        if(!movie.score){
+            movie.score = 0
+        }
+        return acc + movie.score
+    }, 0)
+    let average = Number ((sum/moviesArray.length).toFixed(2))
+    return average
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesScore(moviesArray) {}
