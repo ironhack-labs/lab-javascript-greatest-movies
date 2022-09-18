dd53d2e4ec57b632cbc711d4c002cad0ef991283
Roxanie Fernandes <roxaniedmfernandes@gmail.com>
2022-09-18 13:42:51 -0300

    Iteration 2

--- src/movies.js
+++ src/movies.js
@@ -9,7 +9,13 @@ function getAllDirectors(moviesArray) {
     }
 
     // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-
+    function howManyMovies(moviesArray) {
+        if(!moviesArray.length){
+            return 0
+        }
+        const dramaStevenSpielberg = moviesArray.filter(movie => movie.genre.includes('Drama') && movie.director === 'Steven Spielberg')
+        return dramaStevenSpielberg.length;
+    }
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
 function scoresAverage(moviesArray) {}
