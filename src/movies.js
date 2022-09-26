fdcf68c51821caf25413e8fe3e852d08138e4766
Raquel Blázquez <raquel.blazquez.estrada@gmail.com>
2022-09-26 20:08:32 +0200

    iteracion 2 a medias

--- src/movies.js
+++ src/movies.js
@@ -1,10 +1,25 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {}
+function getAllDirectors(moviesArray) {
+    let nameEachDirector = moviesArray.map((eachDirector) => {
+        return eachDirector.director;
+    } );
+    return nameEachDirector;
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
+function howManyMovies(moviesArray) {
+    let dramaMovies = moviesArray.filter ((stevenSpielberg) =>{
+        if ( stevenSpielberg.director === "Steven Spielberg" ){
+            return true;
+        }
+        else{
+            return false;
+        }
+    })
+    console.log(dramaMovies)
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
 function scoresAverage(moviesArray) {}
