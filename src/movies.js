8b2fc6950dd7a3f64c8ffa2c6901a20a82ab8800
Alfie Collins <alfiecollins44@hotmail.co.uk>
2022-09-28 23:41:39 +0100

    iterations 1-4 done

--- src/movies.js
+++ src/movies.js
@@ -1,16 +1,41 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {}
+function getAllDirectors(moviesArray) {
+    return moviesArray.map(movie => movie.director)}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
+function howManyMovies(moviesArray) {
+    let spielbergArr = []
+    for (let i=0; i<moviesArray.length; i++)
+    if (moviesArray[i].director === `Steven Spielberg` && moviesArray[i].genre.includes(`Drama`)){
+        spielbergArr.push(moviesArray[i])
+    } return spielbergArr.length
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+function scoresAverage(moviesArray) {
+    if (!moviesArray.length) return 0;
+    let totalScore = 0
+    moviesArray.forEach(movie =>{
+     totalScore+=movie.score
+}) 
+return Number((totalScore/moviesArray.length).toFixed(2))
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(moviesArray) {}
+function dramaMoviesScore(moviesArray) {
+    let dramaTotalScore = 0
+    let dramaArray = []
+    for (let i=0; i<moviesArray.length; i++)
+    if (moviesArray[i].genre.includes(`Drama`)) {
+        dramaTotalScore += moviesArray[i].score
+        dramaArray.push(moviesArray[i])
+        if (!dramaArray.length) return 0;
+    }
+    if (!dramaArray.length) return 0;
+    return Number((dramaTotalScore/dramaArray.length).toFixed(2))
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear(moviesArray) {}
