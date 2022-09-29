561ff401a49a73ae5e90d14ffa90c201dd452dc5
Alfie Collins <alfiecollins44@hotmail.co.uk>
2022-09-29 13:15:05 +0100

    1-6 done

--- src/movies.js
+++ src/movies.js
@@ -1,8 +1,7 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {
-    return moviesArray.map(movie => movie.director)}
+const getAllDirectors = (moviesArray) => moviesArray.map(movie => movie.director)
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 function howManyMovies(moviesArray) {
@@ -38,10 +37,22 @@ function dramaMoviesScore(moviesArray) {
 }
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
+function orderByYear(moviesArray) {
+    let yearsArray = moviesArray.map(movie => movie.year)
+    let orderedYears = yearsArray.sort((a, b) => a - b);
+    return orderedYears
+}
+//the above code (iteration 5) functions fine, so not sure why error messages in specrunner
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically(moviesArray) {}
+function orderAlphabetically(moviesArray) {
+    let firstTwenty = []
+    for (let i=0; i<20; i++){
+    firstTwenty.push(moviesArray[i].title);
+    firstTwenty.sort()
+    } return firstTwenty
+}
+//Again, the above code works fine! So not sure why this is getting reds on Specrunner
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes(moviesArray) {}
