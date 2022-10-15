a9cec70e166e6c8bbb2d1174f467a5f93fdc78c9
weslleypmfortunato <111170704+weslleypmfortunato@users.noreply.github.com>
2022-10-14 23:13:20 -0700

    Solved Lab

--- src/movies.js
+++ src/movies.js
@@ -1,45 +1,46 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
+
 function getAllDirectors(moviesArray) {
-  return moviesArray.map(prod => prod.director)
+  return moviesArray.map(directors => directors.director)
 }
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 function howManyMovies(moviesArray) {
-  return moviesArray.filter(movie => movie.genre.includes("Drama") && movie.director.includes("Steven Spielberg")).length
+  return moviesArray.filter(movie => movie.director.includes('Steven Spielberg') && movie.genre.includes('Drama')).length
 }
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
 function scoresAverage(moviesArray) {
-  if (!moviesArray.length) return 0
+  if (moviesArray.length === 0) return 0
 
-  const average = ((moviesArray.filter((movie) => typeof movie.score === "number")).reduce((acumulador, valorAtual) => acumulador + valorAtual.score, 0)) / moviesArray.length
+  const avg = moviesArray.filter(validScore => typeof validScore.score === 'number').reduce((cum, value) => cum + value.score, 0) / moviesArray.length
 
-  return (Math.round(average*100))/100
+  return (Math.round(avg * 100)) / 100
 }
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesScore(moviesArray) {
+  
+  const dramaScore = moviesArray.filter(dramaScore => dramaScore.genre.includes('Drama'))
+  if (dramaScore.length === 0) return 0
+  const dramaAvgScore = dramaScore.reduce((cum, value) => cum + value.score, 0) / dramaScore.length
 
-  const dramaAverage = moviesArray.filter(drama => drama.genre.includes('Drama'))
-  if(!dramaAverage.length) return 0
-
-  const average = (dramaAverage.filter(number => typeof number.score === 'number')).reduce((acumulador, valorAtual) => acumulador + valorAtual.score,0) / dramaAverage.length
-  return (Math.round(average*100))/100
-}
+  return (Math.round(dramaAvgScore * 100)) / 100
+  }
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear(moviesArray) {
-  let orderedArr = [...moviesArray]
-  orderedArr.sort((a, b) => {
+  const yearRelease = [...moviesArray]
+  return yearRelease.sort((a, b) => {
     if (a.year > b.year) return 1
-    if (a.year < b.year) return -1
-    if (a.title > b.title) return 1
+    else if (a.year < b.year) return -1
+    else if (a.title > b.title) return 1
     else if (a.title < b.title) return -1
-    else return 0
-  }) 
-    return orderedArr
+    return 0
+  })
+  return yearRelease
 }
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
