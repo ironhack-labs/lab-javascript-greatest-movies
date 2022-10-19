050593cc7505effc61f4ba41a5927caf36f8c6dc
Cristian Gomez <gmz.mrs.cristian@gmail.com>
2022-10-18 19:14:19 -0500

    Se logro filtrar lo que se necesitaba

--- src/movies.js
+++ src/movies.js
@@ -1,16 +1,54 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {}
+function getAllDirectors(moviesArray) {
+    const newArray = moviesArray.map(( obj ) => {
+        let director = obj['director'];
+        return director;
+    });
+    return newArray;
+}
+
+
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
+function howManyMovies(moviesArray) {
+    let resultado = []
+    if(moviesArray.length == 0){
+        return 0
+    }else if(moviesArray.length > 0){
+        resultado = moviesArray.filter(obj => ( obj.director == 'Steven Spielberg' && obj.genre.indexOf('Drama') > -1));
+        return resultado.length;
+    }else {
+        return 0
+    }
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+function scoresAverage(moviesArray) {
+    let resultado = [];
+    if(moviesArray.length == 0){
+        return 0;
+    } else {
+        resultado = moviesArray.filter(obj => (obj.score))
+        let suma = resultado.map(obj => parseFloat(obj.score)).reduce(( ac, va ) => ac + va);
+        let avg = (suma/moviesArray.length).toFixed(2);
+        return parseFloat(avg);
+    }
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(moviesArray) {}
+function dramaMoviesScore(moviesArray) {
+    let resultado = [];
+    resultado = moviesArray.filter( obj => obj.genre == 'Drama')
+    if(resultado.length > 0 ){
+        let suma = resultado.map(obj => parseFloat(obj.score)).reduce((ac, va) => ac + va);
+        let avg = (suma/resultado.length)
+        return parseFloat(avg.toFixed(2));
+    }else{
+        return 0;
+    }
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear(moviesArray) {}
