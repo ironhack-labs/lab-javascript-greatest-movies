f13d0e291cdef08f149eafaa825d7666f73c4a19
Vika Shcherbak <vikashcherbaka@gmail.com>
2022-10-25 01:18:04 +0200

    Solved lab

--- src/movies.js
+++ src/movies.js
@@ -2,126 +2,6 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-const movi = [
-    {
-      title: 'The Shawshank Redemption',
-      year: 1994,
-      director: 'Frank Darabont',
-      duration: '2h 22min',
-      genre: ['Crime', 'Drama'],
-      score: 9.3
-    },
-    {
-      title: 'The Godfather',
-      year: 1972,
-      director: 'Francis Ford Coppola',
-      duration: '2h 55min',
-      genre: ['Crime', 'Drama'],
-      score: 9.2
-    },
-    {
-      title: 'The Godfather: Part II',
-      year: 1974,
-      director: 'Francis Ford Coppola',
-      duration: '3h 22min',
-      genre: ['Crime', 'Drama'],
-      score: 9
-    },
-    {
-        title: 'The Dark Knight',
-        year: 2008,
-        director: 'Christopher Nolan',
-        duration: '2h 32min',
-        genre: ['Action', 'Crime', 'Drama', 'Thriller'],
-        score: 9
-      },
-      {
-        title: '12 Angry Men',
-        year: 1957,
-        director: 'Sidney Lumet',
-        duration: '1h 39min',
-        genre: ['Crime', 'Drama'],
-        score: 8.9
-      },
-      {
-        title: 'Schindler"s List',
-        year: 1993,
-        director: 'Steven Spielberg',
-        duration: '3h 15min',
-        genre: ['Biography', 'Drama', 'History'],
-        score: 8.9
-      },
-      {
-        title: 'Pulp Fiction',
-        year: 1994,
-        director: 'Quentin Tarantino',
-        duration: '2h 34min',
-        genre: ['Crime', 'Drama'],
-        score: 8.9
-      },
-      {
-        title: 'The Lord of the Rings: The Return of the King',
-        year: 2003,
-        director: 'Steven Spielberg',
-        duration: '3h 21min',
-        genre: ['Adventure', 'Drama', 'Fantasy'],
-        score: 8.9
-      },
-      {
-        title: 'Fight Club',
-        year: 1999,
-        director: 'David Fincher',
-        duration: '2h 19min',
-        genre: ['Drama'],
-        score: 8.8
-      },
-      {
-        title: 'The Lord of the Rings: The Fellowship of the Ring',
-        year: 2001,
-        director: 'Peter Jackson',
-        duration: '2h 58min',
-        genre: ['Adventure', 'Drama', 'Fantasy'],
-        score: 8.8
-      },
-      {
-        title: 'Forrest Gump',
-        year: 1994,
-        director: 'Robert Zemeckis',
-        duration: '2h 22min',
-        genre: ['Comedy', 'Drama', 'Romance'],
-        score: 8.8
-      },
-      {
-        title: 'Star Wars: Episode V - The Empire Strikes Back',
-        year: 1980,
-        director: 'Irvin Kershner',
-        duration: '2h 4min',
-        genre: ['Action', 'Adventure', 'Fantasy', 'Sci-Fi'],
-        score: 8.8
-      },
-      {
-        title: 'Inception',
-        year: 2010,
-        director: 'Christopher Nolan',
-        duration: '2h 28min',
-        genre: ['Action', 'Adventure', 'Sci-Fi', 'Thriller'],
-        score: 8.8
-      },
-      {
-        title: 'The Lord of the Rings: The Two Towers',
-        year: 2002,
-        director: 'Peter Jackson',
-        duration: '2h',
-        genre: ['Adventure', 'Drama', 'Fantasy'],
-        score: 8.7
-      }
-    
-]
-
-//const movies = require ("./data.js");
-console.log(getAllDirectors(movi));
-console.log(howManyMovies(movi));
-
 function getAllDirectors(moviesArray) {
     const directors = moviesArray.map(movies => movies.director);
     return directors;
@@ -188,12 +68,16 @@ function turnHoursToMinutes(moviesArray) {
 console.log(turnHoursToMinutes(movi));
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average 
-//NOT DONE YET
 function bestYearAvg(moviesArray) {
-    const grouppedMovies = moviesArray.reduce((groups, el) => {
+    if (moviesArray.length === 0) {
+        return null;
+      }
+    else if (moviesArray.length === 1) {
+        return `The best year was ${moviesArray[0].year} with an average score of ${moviesArray[0].score}`;
+      }
+    const grouppedMovies = moviesArray.reduce((groups, el) => { //to group all movies by year
         if (el.year in groups) {
-            // groups: {2010: [{...}, {...}], 1999: [...], ...}
-            groups[el.year].push(el);
+            groups[el.year].push(el); // groups: {2010: [{...}, {...}], 1999: [...], ...}
         }
         else {
             groups[el.year] = [el];
@@ -201,6 +85,18 @@ function bestYearAvg(moviesArray) {
         return groups;
     },
     {});
-    return grouppedMovies;
+    let bestYear = undefined;
+    let bestScore = 0;
+    for (let year in grouppedMovies) { //loop and 
+        if (grouppedMovies.hasOwnProperty(year)) {
+            const sumScore = grouppedMovies[year].reduce(((val, el) => val + (el.score || 0)), 0);
+            const avg = sumScore / grouppedMovies[year].length;
+            if (avg > bestScore) {
+                bestScore = avg;
+                bestYear = year;
+            }
+        }
+    }
+    return `The best year was ${bestYear} with an average score of ${bestScore}`;
 }
 console.log(bestYearAvg(movi));
