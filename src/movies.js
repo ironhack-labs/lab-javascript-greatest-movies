b88100f6480d0b28935da48feca6fd045702143f
Jesus Gonzalez <jesusglezt27@gmail.com>
2022-10-24 14:25:18 -0500

    solved lab

--- src/movies.js
+++ src/movies.js
@@ -29,10 +29,21 @@ function dramaMoviesScore(moviesArray) {
 }
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
+function orderByYear(moviesArray) {
+    let moviesByYear = moviesArray.map((element)=> element);
+    let mixMovies = moviesByYear.sort((a,b)=> a.year - b.year);
+    return mixMovies;
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically(moviesArray) {}
+function orderAlphabetically(moviesArray) {
+    const orderedMovies = [...moviesArray];
+    orderedMovies.sort((firstMovie, secondMovie) => {
+    return firstMovie.title.localeCompare(secondMovie.title);
+    });
+    const movieTitles = orderedMovies.map((movie) => movie.title);
+    return movieTitles.slice(0, 20);
+};
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes(moviesArray) {}
