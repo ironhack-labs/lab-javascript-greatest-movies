d73df7abcdd84a78bc46b5fe7fa1347d476b76b5
patri <patricia.mareg@gmail.com>
2022-11-05 14:21:11 +0100

    iteration2

--- src/movies.js
+++ src/movies.js
@@ -13,7 +13,12 @@ function getAllDirectors(moviesArray) {
 }
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
+function howManyMovies(moviesArray) {
+    const newArray = moviesArray.filter((movie) => {
+        return (movie.director === "Steven Spielberg" && movie.genre.includes("Drama"))
+    })
+    return newArray.length
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
 function scoresAverage(moviesArray) {}
