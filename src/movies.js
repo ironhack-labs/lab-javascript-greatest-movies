e25dee9ef4e80efeef0f3d35cf01773a268f966f
silvi-hdez <silviahdez.b@gmail.com>
2022-11-07 15:11:17 +0100

    Ãúltimos ejercicios sin acabar

--- src/movies.js
+++ src/movies.js
@@ -3,9 +3,13 @@
 // How could you "clean" a bit this array and make it unified (without duplicates)?
 function getAllDirectors(moviesArray) {
 
-    const directorMovie = moviesArray.map( movie => ({
+   /* const directorMovie = moviesArray.map( movie => ({
         director: movie.director
-     }))
+     }))*/
+
+     const directorMovie = moviesArray.map(movie => (
+      movie.director
+    )) 
 
  return directorMovie
     
@@ -101,7 +105,31 @@ function orderAlphabetically(moviesArray) {
 }
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-function turnHoursToMinutes(moviesArray) {}
+function turnHoursToMinutes(moviesArray) {
+  arrDuration = moviesArray.map(movie => {
+    let hours = movie.duration.slice(0,1)
+    let minutes = movie.duration.slice(-5, -3)
+    console.log (hours)
+    return {duration: Number(hours)*60 + Number(minutes)}
+  })
+
+return arrDuration
+}
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
-function bestYearAvg(moviesArray) {}
+function bestYearAvg(moviesArray) {
+  if (moviesArray.length === 0) {
+    return null
+  }
+
+  if (moviesArray.length === 1) {
+    return `The best year was ${moviesArray[0].year} with an average score of ${moviesArray[0].score}`
+  }
+
+ const orderByScore = moviesArray.sort((a,b) => {
+   if (a.score > b.score) {return -1}
+   if (a.score < b.score) {return 1}
+   if (a.score = b.score) {return 0}
+ }) 
+  return `The best year was ${orderByScore[0].year} with an average score of ${orderByScore[0].score}`
+}
