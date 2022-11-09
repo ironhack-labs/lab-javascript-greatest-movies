c5657245f2b792430bf49c55adb52efaf51445c8
Nicole <92593875+ozzleme@users.noreply.github.com>
2022-11-09 23:07:47 +0530

    used .map and trying to use .reduce

--- src/movies.js
+++ src/movies.js
@@ -1,15 +1,46 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {}
+function getAllDirectors(moviesArray) {
+    // you have to map through the array of movies 
+   const directorsArray = moviesArray.map(function (movie) {
+    // get all the directors into one array as a final result. 
+    return movie.director
+    });
+    return directorsArray;
+}
+getAllDirectors(movies)
+
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
+// Go ahead and create a howManyMovies() function 
+// that receives an array as a parameter
+// filter 👀 the array 
+// so we can have only the drama movies where Steven Spielberg is the director.
 function howManyMovies(moviesArray) {}
 
+
+
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+// we want to know the average score of all of them 
+// and display it on the console. 
+// Create a scoresAverage() function that receives an array as a parameter 
+// and solves the challenge.
+// The score must be returned rounded to 2 decimals!
+// 💡 Maybe you want to "reduce" the data to a single value. 😉
+function scoresAverage(moviesArray) {
+    const movieAvg = moviesArray.reduce(function (a,b) {
+        return (a + b.score)/moviesArray.length;
+      }, 0);
+return +(movieAvg.toFixed(2))
+}
+  
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
+// Create a dramaMoviesScore() function that receives an array as a parameter 
+// to get the average score of all drama movies! 
+// Let's see if it is better than the general average.
+// Again, rounded to 2 decimals!
 function dramaMoviesScore(moviesArray) {}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
