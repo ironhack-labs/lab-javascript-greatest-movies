1cbe2b5866a3c051b02e705b5e3fe3598574a39a
asm-dev <salamanca.m@outlook.com>
2022-11-16 15:59:28 +0100

    iteration 2 --- howManyMovies()

--- src/movies.js
+++ src/movies.js
@@ -11,7 +11,16 @@ function getAllDirectors(moviesArray) {
 }
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
+function howManyMovies(moviesArray) {
+
+    if (moviesArray.length === 0) {
+        return 0;
+    } else {
+        let spielbergArr = moviesArray.filter(movie => movie.director === "Steven Spielberg" && movie.genre.includes("Drama"));
+        return spielbergArr.length;
+    };
+
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
 function scoresAverage(moviesArray) {}
