0a556ab335fd00966806fc591cc3a1c62769084e
Lai In Chang <lavender880331@gmail.com>
2022-11-28 17:40:52 +0100

    finish iteration 1

--- src/movies.js
+++ src/movies.js
@@ -1,10 +1,28 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {}
+/*    
+    title: 'The Shawshank Redemption',
+    year: 1994,
+    director: 'Frank Darabont',
+    duration: '2h 22min',
+    genre: ['Crime', 'Drama'],
+    score: 9.3
+*/
+
+function getAllDirectors(moviesArray) {
+    let directorArr = [];
+    moviesArray.map(element=> directorArr.push(element.director));
+    return directorArr;
+    
+}
+
+//console.log(getAllDirectors(movies));
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
+function howManyMovies(moviesArray) {
+    
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
 function scoresAverage(moviesArray) {}
