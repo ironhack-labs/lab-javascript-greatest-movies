719562517013ba61e621e12c4d2bbd0c3c4f9aa5
Thalita dos Reis <dosreistha@gmail.com>
2022-11-28 19:10:59 +0100

    lab-done (iteration 1 to 3)

--- src/movies.js
+++ src/movies.js
@@ -1,19 +1,53 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {}
+function getAllDirectors(moviesArray) {
+
+    //Iteration 1
+    return moviesArray.map((film) => film.director);
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
+function howManyMovies(moviesArray) {
+
+    if (moviesArray.length === 0) {
+        return 0;
+    } else {
+        let dramaArr = moviesArray.filter((movie) =>
+
+            movie.director === 'Steven Spielberg' && movie.genre.includes('Drama'));
+
+        return dramaArr.length;
+    }
+};
+
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+function scoresAverage(moviesArray) {
+
+    if (moviesArray.length === 0) {
+        return 0;
+    } else {
+        let scores = moviesArray.map(film => film.score);
+        let onlyNumbers = scores.filter(score => typeof(score) === 'number');
+        let total = scores.length;
+        let sumScore = onlyNumbers.reduce((scoreOne, scoreTwo) => scoreOne + scoreTwo, 0);
+        
+        return parseFloat((sumScore / total).toFixed(2));
+    }
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(moviesArray) {}
+function dramaMoviesScore(moviesArray) {
+
+
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
+function orderByYear(moviesArray) {
+    const copy = [];
+    copy.sort()
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically(moviesArray) {}
