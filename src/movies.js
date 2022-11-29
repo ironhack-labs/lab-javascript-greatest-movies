57e9830e479a2a7ddc40ef49de56f45033284caf
physiodevapp <ficapp2.0@gmail.com>
2022-11-29 16:28:00 +0100

    feat: Iteration 2

--- src/movies.js
+++ src/movies.js
@@ -6,7 +6,9 @@ function getAllDirectors(moviesArray) {
 }
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
+function howManyMovies(moviesArray) {
+    return moviesArray.filter((movie) => movie.director === 'Steven Spielberg' && movie.genre.includes('Drama')).length
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
 function scoresAverage(moviesArray) {}
