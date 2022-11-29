f5628acb9d004ae4d6813262dd0e0c6b8ef445a6
Ana Gutiérrez Ruiz <ana.gutierrez.ruiz@iMac-de-Ana.home>
2022-11-29 13:29:09 +0100

    tried to improve my code for some of the iterations already solved

--- src/movies.js
+++ src/movies.js
@@ -1,19 +1,30 @@
+//FOR SOME ITERATIONS, I HAVE 2 VERSIONS. 
+//THE FIRST VERSION IS LONGER, SPLITTING THE CODE IN MULTIPLE VARIABLES, EASIER THAT WAY FOR ME TO COMPREHEND
+//THE SECOND VERSION IS ME TRYING TO IMPROVE THE CODE. 
+// I HOPE IT'S NOT TOO BIG OF A MESS ;-)
+
+
+
+
+
 // Iteration 1: All directors? - Get the array of all directors.
+
 function getAllDirectors(moviesArray) {
-    const allDirectors = moviesArray.map((movie) => movie.director);
-    return allDirectors; 
+    return moviesArray.map((movie) => movie.director); 
 }
 
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {
-    const SpielbergDramaMovies = moviesArray.filter(movie => movie.director === "Steven Spielberg" && movie.genre.includes("Drama"));
-    return SpielbergDramaMovies.length;
+
+
+function howManyMovies(moviesArray) { 
+    return moviesArray.filter(movie => movie.director === "Steven Spielberg" && movie.genre.includes("Drama")).length;
 }
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
+//FIRST VERSION
 function scoresAverage(moviesArray) {
    if (!moviesArray.length) {
         return 0;
@@ -22,36 +33,33 @@ function scoresAverage(moviesArray) {
     const filteredScoresArray = scoresArray.filter(score => typeof score === "number");
     let scoresAverage = filteredScoresArray.reduce((a, b) => a + b, 0) / scoresArray.length;
     return +scoresAverage.toFixed(2);
-    
-
-   /*
-   HE INTENTADO MEJORAR EL CÓDIGO, PERO NO ME PASA LOS TESTS. 
-   LO DEJO AQUÍ PORQUE ME GUSTARÍA SABER CÓMO HABRÍA QUE HACERLO PARA QUE FUNCIONE CON ESTA SINTAXIS. 
-   
-   if (!moviesArray.length) {
-        return 0;
-    }
-        let scoresAverage = moviesArray
-        .map((movie) => movie.score)
-        .filter(score => typeof score === "number")
-        .reduce((a, b) => a + b, 0)
-    
-        return (+scoresAverage / moviesArray.length).toFixed(2);
-    */
 }
 
+//SECOND VERSION, I COMMENTED THIS VERSION BECAUSE IT DOES NOT PASS ALL THE TESTS, BUT I WOULD LIKE TO UNDERSTAND WHAT I AM DOING WRONG. 
+
+/* function scoresAverage(moviesArray) {
+    let moviesScores = moviesArray
+        .map((movie) => movie.score)
+        .filter(score => score !== undefined)
+        .reduce((acc, score) => acc + score, 0);
+    let scoresAverage = !moviesScores.length ? 0 : +(moviesScores / moviesArray.length).toFixed(2);
+    return scoresAverage;
+} */
+
 // Iteration 4: Drama movies - Get the average of Drama Movies
+
 function dramaMoviesScore(moviesArray) {
     const dramaMovies = moviesArray.filter(movie => movie.genre.includes("Drama"));
     if (!dramaMovies.length) {
         return 0;
     }
     const dramaMoviesScores = dramaMovies.map((movie) => movie.score);
-    let dramaScoresAverage = dramaMoviesScores.reduce((a, b) => a + b, 0) / dramaMovies.length;
+    let dramaScoresAverage = dramaMoviesScores.reduce((acc, movieScore) => acc + movieScore, 0) / dramaMovies.length;
     
     return +dramaScoresAverage.toFixed(2);
-
 }
+
+
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear(moviesArray) {}
