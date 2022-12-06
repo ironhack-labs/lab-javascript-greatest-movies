46b02d6f7b83cabd392279e0f842e4c0d618da64
Mevine Teelokee <mevine.teelokee@gmail.com>
2022-12-06 08:02:25 +0100

    done through iteration 5 so far

--- src/movies.js
+++ src/movies.js
@@ -1,19 +1,50 @@
+//const movies = require('./data')
+
 // Iteration 1: All directors? - Get the array of all directors.
+const getAllDirectors = (moviesArray) => { 
+    const newArr = moviesArray.map(e => e.director)
+    return newArr};
+
+
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {}
+
+
+
+
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
 
-// Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+const howManyMovies = (moviesArray) => {
+    let spilMovies = moviesArray.filter(e => e.genre.includes('Drama') && e.director === 'Steven Spielberg')
+    return !spilMovies ? 0 : spilMovies.length
+}
+
+
+// Iteration 3: All scores average - Get the average of all scores with 2 decimals 
+const scoresAverage = (moviesArray) => {
+    if(!moviesArray.length){return 0}
+     let avg = moviesArray.filter(e=>e.score).reduce((sum,rating) => sum +rating.score,0);
+    return Number((avg/moviesArray.length).toFixed(2))
+}
+
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(moviesArray) {}
+
+const dramaMoviesScore = (moviesArray) => {
+    let dramaMovies = moviesArray.filter(el=>el.genre.includes('Drama'))
+    return scoresAverage(dramaMovies)
+}
+
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
+
+
+const orderByYear = (moviesArray) => {
+   let sorted = moviesArray.sort((a, b) => b.year - a.year).map(e => e.year)
+   let sortedArr = sorted.map(i => {year: i})
+   return sortedArr
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically(moviesArray) {}
