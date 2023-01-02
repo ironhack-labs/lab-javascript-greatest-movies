b1cfc5d37ccc2fe7ee3e9f62cc2a2b34776ed4a2
Johannes Chorzempa <jchorzempa@posteo.org>
2023-01-02 13:27:17 +0100

    Create copy

--- src/movies.js
+++ src/movies.js
@@ -71,7 +71,9 @@ function orderAlphabetically(moviesArray) {
 }
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-function turnHoursToMinutes(moviesArray) {}
+function turnHoursToMinutes(moviesArray) {
+	const movieArrayCopy = [...moviesArray];
+}
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
 function bestYearAvg(moviesArray) {}
