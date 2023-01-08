cbfcf31378bc1d163c657df28a635e545e60fdf5
Wilkins Morales <wilkinscom@gmail.com>
2023-01-08 15:02:44 +0200

    iteration 5

--- src/movies.js
+++ src/movies.js
@@ -39,7 +39,19 @@ function dramaMoviesScore(moviesArray) {
 }
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
+function orderByYear(moviesArray) {
+    let orderYear = moviesArray.map((x) => x)
+    //let sortedByYear = orderYear.sort((a,b) =>a.year - b.year);
+    let sortedYearTitle = orderYear.sort(function (a,b){
+        if (a.year === b.year){
+            return a.title < b.title ? -1 : 1
+        } else {
+            return a.year < b.year ? -1 : 1
+        }
+    })
+    return sortedYearTitle
+
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically(moviesArray) {}
