19bd5e48ebcf20ca1be9e67aa8a32bf4a4e505a7
Paul Dombrowski <paul.dombr@googlemail.com>
2023-01-17 09:32:28 +0100

    done

--- src/movies.js
+++ src/movies.js
@@ -60,14 +60,25 @@ function dramaMoviesScore(moviesArray) {
 } 
    
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {
+    
+function orderByYear(movies) {
+        return [...movies].sort((a, b) => {
+            if (a.year === b.year) {
+                return a.title.localeCompare(b.title);
+            }
+            return a.year - b.year;
+        });
+    }
 
-newSortedArray 
-
-}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically(moviesArray) {}
+function orderAlphabetically(moviesArray) {
+    return moviesArray.map(function(movie) {
+        return movie.title
+    })
+    .sort()
+    .slice(0,20)
+}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes(moviesArray) {}
