2c15825c3b3f9c04edddfa1c5c08be2d78290d59
ElDav1d <dvivoes@yahoo.es>
2023-01-24 08:27:50 +0100

    Resolves Bonus Iteration 7

--- src/movies.js
+++ src/movies.js
@@ -97,7 +97,27 @@ function orderAlphabetically(moviesArray) {
 }
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-function turnHoursToMinutes(moviesArray) {}
+
+function turnHoursToMinutes(moviesArray) {
+  const formattedMovies = structuredClone(moviesArray).map(movie => {
+    const timeUnits = movie.duration.split(" ").map(unit => parseInt(unit));
+
+    const hours = timeUnits[0];
+    const minutes = timeUnits[1];
+
+    if (timeUnits.length === 2) {
+      movie.duration = hours * 60 + minutes;
+    } else {
+      movie.duration = hours * 60;
+    }
+
+    return movie;
+  });
+
+  return formattedMovies;
+}
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
-function bestYearAvg(moviesArray) {}
+function bestYearAvg(moviesArray) {
+
+}
