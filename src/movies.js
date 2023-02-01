d11eeee93754e6377678250bb5a2afe7db5e3c8f
Joao Victor Souto <jheibol1212@icloud.com>
2023-02-01 02:11:12 -0300

    Solved lab

--- src/movies.js
+++ src/movies.js
@@ -1,16 +1,47 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {}
+function getAllDirectors(moviesArray) {
+    const newArray = moviesArray.map(item => item.director);
+    return newArray
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
+function howManyMovies(moviesArray) {
+    if (!moviesArray) return 0
+   
+    const resultDirector = moviesArray.filter((film => film.director === 'Steven Spielberg'));
+    const resultFilm = resultDirector.filter((film => film.genre.includes('Drama')));
+
+    return resultFilm.length
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+function scoresAverage(moviesArray) {
+    const numbersCopy = moviesArray.map(item => item.score); 
+    const arrPreparado = numbersCopy.filter(item => typeof item === 'number')
+    const totalParcial = arrPreparado.reduce((acumulador, valorAtual) => acumulador + valorAtual, 0);
+    const total =+ (totalParcial / numbersCopy.length).toFixed(2)
+   
+    if(totalParcial === 0) return 0
+    console.log(total)
+    return total
+    
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(moviesArray) {}
+function dramaMoviesScore(moviesArray) {
+    const films = moviesArray.filter((film => film.genre.includes('Drama')));
+    const numbersCopy = films.map(item => item.score); 
+    const arrPreparado = numbersCopy.filter(item => typeof item === 'number')
+    const totalParcial = arrPreparado.reduce((acumulador, valorAtual) => acumulador + valorAtual, 0);
+    const total =+ (totalParcial / numbersCopy.length).toFixed(2)
+   
+    if(films.length === 0) return 0
+    console.log(films)
+    return total
+    
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear(moviesArray) {}
