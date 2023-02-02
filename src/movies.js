375dc7266b172ba86e16f3cc4984b1bceb96ecdc
MATHEUS CHAVES ARAUJO <002919@CESED.LOCAL>
2023-02-02 16:50:03 -0300

    Iteration 1 to 4

--- src/movies.js
+++ src/movies.js
@@ -1,16 +1,43 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {}
+function getAllDirectors(moviesArray) {
+   const allDirectors = moviesArray.map(movie => movie.director);
+   return allDirectors;
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
+function howManyMovies(moviesArray) {
+    const spilbergMovies = moviesArray.filter(movie => movie.director === 'Steven Spielberg' && movie.genre.includes('Drama'));
+    return spilbergMovies.length;
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+function scoresAverage(moviesArray) {
+    if(moviesArray.length === 0) return 0;
+
+    const scoresFromAllMovies = moviesArray.map(movie => movie.score); 
+    const movieHasScore = scoresFromAllMovies.filter(movie => typeof movie === 'number')
+    if(movieHasScore === 0) return 0;
+    const sumScores = movieHasScore.reduce((acc, cur) => acc + cur, 0);
+    const average = sumScores / movieHasScore.length
+    return average
+    
+
+    /* const movieHasScore = moviesArray.map(movie => typeof movie.score === 'number');
+    const scores = movieHasScore.reduce((acc, cur) => acc + cur.score, 0);  
+    const average = scores/movieHasScore.lentgh;
+    return average.toFixed(2); */
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(moviesArray) {}
+function dramaMoviesScore(moviesArray) {
+    const dramaMovies = moviesArray.map(movie => movie.genre.includes('Drama'));
+    const movieHasScore = dramaMovies.filter(movie => typeof movie.score === 'number');
+    const averageOfDramaMovies = movieHasScore.reduce((acc, cur) => acc + cur.score, 0);
+    const average = averageOfDramaMovies/movieHasScore.length;
+    return average.toFixed(2);
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear(moviesArray) {}
