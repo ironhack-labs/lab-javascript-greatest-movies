7ffaab66e18217af5f9cd8f224d62b61d85dc400
Joao Victor Souto <jheibol1212@icloud.com>
2023-02-02 22:21:07 -0300

    Solved lab

--- src/movies.js
+++ src/movies.js
@@ -3,7 +3,9 @@
 // How could you "clean" a bit this array and make it unified (without duplicates)?
 function getAllDirectors(moviesArray) {
     const newArray = moviesArray.map(item => item.director);
+
     return newArray
+    
 }
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
@@ -24,7 +26,7 @@ function scoresAverage(moviesArray) {
     const total =+ (totalParcial / numbersCopy.length).toFixed(2)
    
     if(totalParcial === 0) return 0
-    console.log(total)
+   
     return total
     
 }
@@ -36,18 +38,39 @@ function dramaMoviesScore(moviesArray) {
     const arrPreparado = numbersCopy.filter(item => typeof item === 'number')
     const totalParcial = arrPreparado.reduce((acumulador, valorAtual) => acumulador + valorAtual, 0);
     const total =+ (totalParcial / numbersCopy.length).toFixed(2)
-   
+
     if(films.length === 0) return 0
-    console.log(films)
+    
     return total
     
 }
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
+function orderByYear(moviesArray) {
+    const resultado = moviesArray
+    .map(item => item)
+    .sort((a, b) => {
+        if(!a.title) return 0
+        return a.title.localeCompare(b.title) 
+    })
+    .sort((a, b) => a.year - b.year)
+   
+    return resultado
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically(moviesArray) {}
+function orderAlphabetically(moviesArray) {
+    const resultado = moviesArray
+    .map(item => item.title).sort((a,b) => a.localeCompare(b))
+    if(resultado.length > 20){
+        
+        return resultado.splice(0, 20)
+
+    }
+    
+    return resultado
+
+}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes(moviesArray) {}
