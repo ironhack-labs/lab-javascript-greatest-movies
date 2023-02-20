e4e5a766aca6ca0ab0d03f8fa87fb7db0d34b649
Mauricio Benavente <mauriciobenavente97@icloud.com>
2023-02-21 00:22:12 +0100

    iteration 5 done

--- src/movies.js
+++ src/movies.js
@@ -46,7 +46,17 @@ function getAllDirectors(moviesArray) {
      }
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
+function orderByYear(moviesArray) {
+  
+    let sortedMovies = moviesArray.slice(0);
+  
+  sortedMovies.sort(function (a, b) {
+      return a.year - b.year;
+  });
+    return sortedMovies
+    
+  }
+
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically(moviesArray) {}
