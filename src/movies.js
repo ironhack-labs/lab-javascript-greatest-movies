8411c2cd5397e1f05344f3792db582926090a416
Sergi Vaqué Galdón <sergivaque@gmail.com>
2023-02-22 13:58:18 +0100

    Bonus done v1

--- src/movies.js
+++ src/movies.js
@@ -115,52 +115,5 @@ function bestYearAvg(moviesArray) {
         }
     }    
 
-    console.log(`The best year was ${bestYear} with an average score of ${bestAverageScore}`);
     return `The best year was ${bestYear} with an average score of ${bestAverageScore}`;
 }
-/*
-
-const movies = [
-    {
-      title: 'The Shawshank Redemption',
-      year: 1994,
-      director: 'Frank Darabont',
-      duration: '2h 22min',
-      genre: ['Crime', 'Drama'],
-      score: 9.3
-    },
-    {
-      title: 'The Godfather',
-      year: 1972,
-      director: 'Francis Ford Coppola',
-      duration: '2h 55min',
-      genre: ['Crime', 'Drama'],
-      score: 9.2
-    },
-    {
-      title: 'The Godfather: Part II',
-      year: 1994,
-      director: 'Francis Ford Coppola',
-      duration: '3h 22min',
-      genre: ['Crime', 'Drama'],
-      score: 9
-    },
-    {
-      title: 'The Dark Knight',
-      year: 2008,
-      director: 'Christopher Nolan',
-      duration: '2h 32min',
-      genre: ['Action', 'Crime', 'Drama', 'Thriller'],
-      score: 9
-    },
-    {
-      title: '12 Angry Men',
-      year: 2008,
-      director: 'Sidney Lumet',
-      duration: '1h 36min',
-      genre: ['Crime', 'Drama'],
-      score: 8.9
-    }   
-  ];
-  bestYearAvg(movies);
-*/
