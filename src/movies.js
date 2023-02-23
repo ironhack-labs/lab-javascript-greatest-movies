e29c16d8af23db7dff578bcf28da3dba8576e4d0
Elisa Johnson <elisajohnson@outlook.be>
2023-02-23 18:45:38 +0100

    Solved lab movies E.J

--- src/movies.js
+++ src/movies.js
@@ -1,10 +1,44 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {}
+
+ function getAllDirectors(moviesArray) {
+    const newMoviesArray = moviesArray.map((item) => item.director);
+    const uniqueDirectors = [...new Set(newMoviesArray)];
+    console.log(uniqueDirectors);
+    return uniqueDirectors;
+}
+
+/*This is what I did initially but I don't really get what is wrong. 
+    function getAllDirectors(moviesArray) {
+    const directors = moviesArray.map(moviesArray) => {
+    return moviesArray.directors }
+  } */
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
+function howManyMovies(moviesArray) {
+        const stevenDramas = moviesArray.filter(currentMovie, index, array) => (  
+            if (currentMovie.director === "Steven Spielberg" && currentMovie.genre.includes"Drama")){
+                return stevenDramas.length
+            }
+             }
+       
+             function howManyMovies(moviesArray) {
+                if(moviesArray.length === 0){
+                    return 0;
+                }else{
+                  const result = moviesArray.filter(function(movie) {
+                    if(movie.director === 'Steven Spielberg'){
+                        for(let i = 0; i < movie.genre.length; i++){
+                            if(movie.genre[i] === 'Drama'){
+                                return true;
+                            }
+                        }
+                    }
+                  })
+                 return result.length;
+                }
+            }
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
 function scoresAverage(moviesArray) {}
