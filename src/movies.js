99dd2a8acfaf358c9344b3e5fbf64c0f4707594d
Lena Imdahl <mail@lenaimdahl.de>
2023-02-23 21:32:18 +0100

    added some bonus

--- src/movies.js
+++ src/movies.js
@@ -70,7 +70,7 @@ function orderByYear(moviesArray) {
   }
   const NewOrderYear = moviesArray.sort((a, b) => {
     if (a.year === b.year) {
-      return a.title > blur.title ? 1 : -1;
+      return a.title > b.title ? 1 : -1;
     }
     return a.year - b.year;
   });
@@ -79,7 +79,8 @@ function orderByYear(moviesArray) {
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically(moviesArray) {
-  const newOrderArr = [...moviesArray].sort((a, b) => {
+  const copyArray = JSON.parse(JSON.stringify(moviesArray));
+  const newOrderArr = copyArray.sort((a, b) => {
     return a.title.localeCompare(b.title);
   });
 
@@ -93,4 +94,29 @@ function orderAlphabetically(moviesArray) {
 function turnHoursToMinutes(moviesArray) {}
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
-function bestYearAvg(moviesArray) {}
+function YearScore(moviesArray, year) {
+  const year = moviesArray.filter((movie) => {
+    return movie.year.includes(year);
+  });
+  if (year.length === 0) {
+    return null;
+  }
+  const sumScore = year.reduce((accumulator, movie) => {
+    if (movie.score) {
+      return accumulator + movie.score;
+    }
+    return accumulator;
+  }, 0);
+
+  return Math.round((sumScore / year.length) * 100) / 100;
+  //compare score of each year...
+  // bestYearAvg(moviesArray)sort(a, b) => {return a.sumScore - b.sumScore})
+}
+/*
+funtion bestYearAvg(moviesArray) {
+for (let i = 0; i < 2023; i++ ) {
+  YearScore
+};
+return bestYearAvg;
+};
+*/
