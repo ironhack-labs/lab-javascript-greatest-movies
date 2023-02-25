200dc6461c3c76c0f3ae0b6858e8ba1448ff480b
Rohan Singh <rsingh@faruse.com>
2023-02-25 08:52:35 +0100

    done

--- src/movies.js
+++ src/movies.js
@@ -35,20 +35,54 @@ function scoresAverage(moviesArray) {
     return Number((bestScore / moviesArray.length).toFixed(2));
 }
 
-// Iteration 4: Drama movies - Get the average of Drama Movies
-
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(moviesArray) {}
+function dramaMoviesScore(moviesArray) {
+
+    const dramaFilms = moviesArray.filter(function (movie) {
+        if (movie.genre.includes("Drama")) {
+            return movie
+        }
+    })
+    const averageDrama = scoresAverage(dramaFilms)
+    return averageDrama
+
+
+
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
+function orderByYear(moviesArray) {
+    const newOrder = moviesArray.splice(0, moviesArray.length)
+    return newOrder.sort(function (a, b) {
+        if (a.year === b.year) {
+            if (a.title < b.title) {
+                return -1
+            } else if (a.title > b.title) {
+                return 1
+            } else {
+                return 0;
+            }
+
+        } return a.year - b.year
+    })
+    return newOrder
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically(moviesArray) {}
+function orderAlphabetically(moviesArray) {
+
+    const movieTitles = moviesArray.map(function (movie) {
+        return movie.title;
+    })
+    const alphabeticalArr = movieTitles.sort().slice(0, 20);
+    return alphabeticalArr
+}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-function turnHoursToMinutes(moviesArray) {}
+
+
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
 function bestYearAvg(moviesArray) {}
+
