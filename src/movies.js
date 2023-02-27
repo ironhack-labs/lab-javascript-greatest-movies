fe4b838609506bf8268b8ce0bf242a2ec04b2174
drose123456 <acg_danial@hotmail.com>
2023-02-27 15:41:14 +0000

    3rd, 4th and 5th Iteration Complete

--- src/movies.js
+++ src/movies.js
@@ -20,13 +20,39 @@ function getAllDirectors(movies) {
   }
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
-
-// Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(moviesArray) {}
-
-// Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
+function scoresAverage(arr) {
+    if (arr.length === 0) {
+      return 0;
+    }
+  
+    const scores = arr.map(movie => movie.score || 0);
+    const sum = scores.reduce((acc, val) => acc + val, 0);
+    const avg = sum / arr.length;
+  
+    return parseFloat(avg.toFixed(2));
+  }
+  
+  
+  // Iteration 4: Drama movies - Get the average of Drama Movies
+  function dramaMoviesScore(movies) {
+    let dramaMovies = movies.filter(movie => movie.genre.includes('Drama'));
+    let totalScore = dramaMovies.reduce((acc, movie) => acc + movie.score, 0);
+    let averageScore = totalScore / dramaMovies.length || 0;
+    return parseFloat(averageScore.toFixed(2));
+  }
+  
+  
+  // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
+  function orderByYear(movies) {
+    const sortedMovies = movies.slice().sort((a, b) => {
+      if (a.year !== b.year) {
+        return a.year - b.year;
+      } else {
+        return a.title.localeCompare(b.title);
+      }
+    });
+    return sortedMovies;
+  }
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically(moviesArray) {}
