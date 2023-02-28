5ceb8e315e0aa98e4fc16e12893892f548cacd25
SenemDEV <121942044+SenemDEV@users.noreply.github.com>
2023-02-28 11:11:21 +0100

    Lab Done

--- src/movies.js
+++ src/movies.js
@@ -1,25 +1,63 @@
-// Iteration 1: All directors? - Get the array of all directors.
-// _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
-// How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {}
 
-// Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
 
-// Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
-
-// Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(moviesArray) {}
-
-// Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
-
-// Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically(moviesArray) {}
-
-// BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-function turnHoursToMinutes(moviesArray) {}
-
-// BONUS - Iteration 8: Best yearly score average - Best yearly score average
-function bestYearAvg(moviesArray) {}
+    // Iteration 1: All directors? - Get the array of all directors.
+    // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
+    // How could you "clean" a bit this array and make it unified (without duplicates)?
+    function getAllDirectors(moviesArray) {
+       const directors = moviesArray.map((currentmovie) => 
+         currentmovie.director
+       )
+       return directors;
+    }
+    
+    
+    
+    // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
+    function howManyMovies(moviesArray) {
+    
+       const totalMovies = moviesArray.filter(function (currentMovie) {
+        if(currentMovie.director === "Steven Spielbeg" && currentMovie.genre.includes("Drama")) {
+             return true;
+        } else {
+            return false;
+        } 
+        
+       });
+    
+       return totalMovies.length;
+    }
+    
+    
+    // Iteration 3: All scores average - Get the average of all scores with 2 decimals
+    function scoresAverage(moviesArray) {
+        const totalScore = moviesArray.reduce(function(accumulator, currentMovie) {
+            const newAccumulator = accumulator + currentMovie.score;
+     
+            return newAccumulator;
+        }, 0);
+     
+        return averageScore = totalScore / moviesArray.length;
+    }
+       
+    
+    
+    
+    
+    // Iteration 4: Drama movies - Get the average of Drama Movies
+    function dramaMoviesScore(moviesArray) {
+        const averageScoreDrama = moviesArray.reduce(function(acc, currentDrama) {
+            const newAcc = acc + currentDrama.score;
+            if (movies.genre.includes("Drama")) {
+              
+                return true;
+            } else {
+                return false;
+            }
+            
+            
+        }, 0);
+    
+        return averageScoreDrama;
+    }
+    
+    
