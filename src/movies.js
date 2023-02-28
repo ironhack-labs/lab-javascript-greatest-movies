173b2b6668d15d40ceb5dbc16d9bfdf24fade68d
José Antonio Escudero García <jaeg90@gmail.com>
2023-02-28 19:19:35 +0100

    First round done

--- src/movies.js
+++ src/movies.js
@@ -2,27 +2,57 @@
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
 function getAllDirectors(moviesArray) {
-  const directors = moviesArray.map((movie) => {
+  const directorsRaw = moviesArray.map((movie, index) => {
     return movie.director;
   });
+  const directors = directorsRaw.filter((director, index) => {
+    return directorsRaw.indexOf(director) == index;
+  });
+  console.log(directors);
   return directors;
 }
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 function howManyMovies(moviesArray) {
-  const stevenMovies = moviesArray.filter((movie) => {
-    return movie.director === "Steven Spielberg";
+  const stevenDramas = moviesArray.filter((movie) => {
+    return (
+      movie.director === "Steven Spielberg" && movie.genre.includes("Drama")
+    );
   });
-  const stevenDramas = stevenMovies.filter((movie) => {});
-  console.log(stevenDramas);
   return stevenDramas.length;
 }
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+function scoresAverage(moviesArray) {
+  if (moviesArray.length <= 0) {
+    return 0;
+  }
+
+  const sumScores = moviesArray.reduce((acc, movie) => {
+    if (movie.score >= 0) {
+      return acc + movie.score;
+    }
+    return acc;
+  }, 0);
+
+  return Math.round((sumScores / moviesArray.length) * 100) / 100;
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(moviesArray) {}
+function dramaMoviesScore(moviesArray) {
+  let countDramaMovies = 0;
+  const sumDramaScores = moviesArray.reduce((acc, movie) => {
+    if (movie.genre.includes("Drama")) {
+      countDramaMovies++;
+      return acc + movie.score;
+    }
+    return acc;
+  }, 0);
+  if (countDramaMovies > 0) {
+    return Math.round((sumDramaScores / countDramaMovies) * 100) / 100;
+  }
+  return 0;
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear(moviesArray) {}
