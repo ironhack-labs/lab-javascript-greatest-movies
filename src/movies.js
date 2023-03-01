1fe11714df81b67bcec74276e8b780b718b8c3f5
kobetoto <106164290+kobetoto@users.noreply.github.com>
2023-03-01 19:44:39 +0100

    iteration6

--- src/movies.js
+++ src/movies.js
@@ -41,14 +41,25 @@ function dramaMoviesScore(moviesArray) {
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear(moviesArray) {
-  return moviesArray
-    .slice()
-    .sort((a, b) => a.year - b.year)
-    .sort((a, b) => a.title - b.title);
+  return moviesArray.slice().sort(function (a, b) {
+    if (a.year < b.year) {
+      return -1;
+    } else if (a.year > b.year) {
+      return 1;
+    } else {
+      return a.title.localeCompare(b.title);
+    }
+  });
+  // .sort((a, b) => a.year - b.year)
+  // .sort((a, b) => a.title - b.title);
 }
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically(moviesArray) {}
+function orderAlphabetically(moviesArray) {
+  return moviesArray.slice().sort(function (a, b) {
+    return a.title.localeCompare(b.title);
+  }).map(el => el.title).slice(0,20);
+}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes(moviesArray) {}
