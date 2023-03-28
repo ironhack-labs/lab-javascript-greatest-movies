3f24d497b3839be7e46847361510e81545facd76
Neethu Kunjappan <neethu.kunjappan@tu_clausthal.de>
2023-03-28 18:26:29 +0530

    add

--- src/movies.js
+++ src/movies.js
@@ -1,25 +1,134 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {}
+function getAllDirectors(moviesArray) 
+{
+      const movieDirectors = moviesArray.map(movie => movie.director);
+      const directors =[...new Set (movieDirectors)];
+      return directors;
+
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
+function howManyMovies(moviesArray) {
+    return moviesArray.filter(movie => {
+        return movie.genre.includes('Drama') && movie.director === 'Steven Spielberg';
+    }).length;
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+function scoresAverage(moviesArray) 
+    { 
+        if (moviesArray.length === 0) 
+            return 0
+        
+        const totalScore = moviesArray.reduce(function(acc,val)
+        {
+        
+        if (!val.score) return acc
+        return  acc + val.score
+        }, 0) 
+        return ((totalScore / moviesArray.length),toFixed(2))
+    }
+  
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(moviesArray) {}
+function dramaMoviesScore(moviesArray)
+ {
+     const dramaMovies = moviesArray.filter(function(movie)
+        {
+          return movie.genre.includes("Drama")
+        })
+          return scoresAverage(dramaMovies)
+  }
+
+
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
+function orderByYear(moviesArray) {
+   {
+        const arrayCopy = [...moviesArray];
+        return arrayCopy.sort((a, b) => {
+          if (a.year > b.year) {
+            return 1;
+          } else if (a.year < b.year) {
+            return -1;
+          } else {
+            const firstMovieTitle = a.title.toLowerCase();
+            const secondMovieTitle = b.title.toLowerCase();
+            if (firstMovieTitle > secondMovieTitle) {
+              return 1;
+            } else if (firstMovieTitle < secondMovieTitle) {
+              return -1;
+            } else {
+              return 0;
+            }
+          }
+        });
+      }
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically(moviesArray) {}
+function orderAlphabetically(moviesArray) {
+    {
+        return moviesArray
+          .map(movie => movie.title)
+          .sort((a, b) => {
+            const firstMovieTitle = a.toLowerCase();
+            const secondMovieTitle = b.toLowerCase();
+            if (firstMovieTitle > secondMovieTitle) {
+              return 1;
+            } else if (firstMovieTitle < secondMovieTitle) {
+              return -1;
+            } else {
+              return 0;
+            }
+          })
+          .slice(0, 20);
+      }
+}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-function turnHoursToMinutes(moviesArray) {}
+function turnHoursToMinutes(moviesArray) {
+
+    {
+        return moviesArray.map(function(movie)
+        {
+          let minutes = 0
+          let hoursInMinutes = 0
+          if (movie.duration.includes("m"))
+          {
+            minutes = Number(movie.duration.slice(movie.duration.indexOf(" ") + 1,movie.duration.indexOf("m")))}
+ 
+            if (movie.duration.includes("h"))
+          {
+            hoursInMinutes = Number(movie.duration.slice(0,movie.duration.indexOf("h"))) * 60
+          }
+          movie.duration = minutes + hoursInMinutes
+          return movie
+
+        })
+          
+      }  } 
+
+
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
-function bestYearAvg(moviesArray) {}
+function bestYearAvg(moviesArray) 
+      {
+        if (moviesArray.length === 0) return null
+        const  years = [...new Set(moviesArray.map(function(movie) {
+          return movie.year
+        }))]
+        const scoresByYear = []
+        years.forEach(function(year)
+        {
+          scoresByYear.push[{year,score:scoresAverage(moviesArray.filter(function(movie){
+            return movie.year === year
+          }))}]
+        })
+
+        const scoresByYearSorted = scoresByYear.sort(function(a,b){
+          return b.score - a.score || a.year - b.year
+        })
+      }
