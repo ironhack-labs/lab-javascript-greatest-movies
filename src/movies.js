08ad73bce71e59bf4e4d0f47fee76042ce440cac
Diogo Parreira <diogomiguel.nunes@hotmail.com>
2023-03-31 23:23:45 +0100

    Iterations 1 - 4

--- src/movies.js
+++ src/movies.js
@@ -1,16 +1,61 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {}
+function getAllDirectors(moviesArray) {
+
+    const allDirectors = moviesArray.map(movies => movies.director );
+   
+    return  allDirectors;
+}
+
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
+function howManyMovies(moviesArray) {
+
+    const directedBy = moviesArray.filter(movie => movie.director === 'Steven Spielberg');
+ 
+    const getDramas = directedBy.filter(dramas => dramas.genre.includes("Drama"));
+ 
+    return getDramas.length;
+ }
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+function scoresAverage(moviesArray) {
+    const totalAverage = moviesArray.reduce(function (acc, val) {
+      if (val.score === undefined) {
+        return acc;
+      }
+  
+      return acc + val.score;
+    }, 0);
+    let total = totalAverage / moviesArray.length;
+  
+    if (totalAverage === 0) {
+      return 0;
+    }
+    return Math.round(total * 100) / 100;
+  }
+
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(moviesArray) {}
+function dramaMoviesScore(moviesArray) {
+
+    const getDramas = moviesArray.filter(dramas => dramas.genre.includes("Drama"));
+    const totalAverage = getDramas.reduce(function (acc, val) {
+        if (val.score === undefined) {
+          return acc;
+        }
+    
+        return acc + val.score;
+      }, 0);
+      let total = totalAverage / getDramas.length;
+    
+      if (totalAverage === 0) {
+        return 0;
+      }
+      return Math.round(total * 100) / 100;
+    }
+  
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear(moviesArray) {}
