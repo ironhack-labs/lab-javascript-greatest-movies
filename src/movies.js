62ca6ff3047bd2acc4fd37d8ac8ca2ad74e4e376
Patricia <marie.lou.trips@gmail.com>
2023-04-04 11:15:58 +0200

    Iteration 4 fails 1Jasmine test

--- src/movies.js
+++ src/movies.js
@@ -20,29 +20,42 @@ function howManyMovies(movies) {
     return filteredMovies.length;
   }
  
-// Iteration 3: All scores average - Get the average of all scores with 2 decimals
 
+// Iteration 3: All scores average - Get the average of all scores with 2 decimals
 function scoresAverage(moviesArray){
   //1.Creating a new array with movie scores
-  moviesArray = movies.map(movie => movie.score) 
- console.log(moviesArray)
+moviesArray = movies.map(movie => movie.score) 
+console.log(moviesArray);
     if (moviesArray.length === 0) {
     return 0;
     }
-  //Make the sum of all movies scores
+  //2.Make the sum of all movies scores
   const sumMovies = moviesArray.reduce((accumulator, movie) => {return accumulator + movie.score}, 0)
    if (typeof moviesArray.score !== 'number') {
     return accumulator;
     }
-return accumulator + movie.score;
+  return accumulator + movie.score;
 }
- //To get the average score: divide all movies scores by the total array length 
- console.log(sumMovies/moviesArray.length)
+ //3.To get the average score: divide all movies scores by the total array length 
+ console.log(sumMovies/moviesArray.length) //Why isn't the code working?! 
 
 
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(moviesArray) {}
+
+function dramaMoviesScore(movies) {
+//1. Create a new array for Drama movies only
+const dramaMoviesList = movies.filter(movie => movie.genre.includes('Drama'))
+//2.Mapping dramaMovieList array to extract the drama scores of each movie to another array "dramaScores"
+const dramaScores = dramaMoviesList.map(movie => movie.score || " " || 0);
+//3. Using reduce to sum the scores from the previous array
+const dramaticSum = dramaScores.reduce((acc, score) => acc + score, 0);
+//3.1 Dividing dramaticSum by the length of the dramaScores array to calculate the average score
+const avg = dramaticSum/ dramaScores.length;
+//4.Returning the result rounded to 2 decimal places as a number using the Number() function and the toFixed() method
+return Number(avg.toFixed(2));
+}
+
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear(moviesArray) {}
