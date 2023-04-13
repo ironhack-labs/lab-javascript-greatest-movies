d05205d19ce225c042e47180260e30379adf8c5b
Joao Madeira <129863118+jodmadeira@users.noreply.github.com>
2023-04-13 18:06:26 +0100

    Lab Done no Bonus

--- src/movies.js
+++ src/movies.js
@@ -1,25 +1,326 @@
+/*
+const movies = [
+    {
+      title: 'The Shawshank Redemption',
+      year: 1994,
+      director: 'Frank Darabont',
+      duration: '2h 22min',
+      genre: ['Crime', 'Drama'],
+      score: 9.3
+    },
+    {
+      title: 'The Godfather',
+      year: 1972,
+      director: 'Francis Ford Coppola',
+      duration: '2h 55min',
+      genre: ['Crime', 'Drama'],
+      score: 9.2
+    },
+    {
+      title: 'The Godfather: Part II',
+      year: 1974,
+      director: 'Francis Ford Coppola',
+      duration: '3h 22min',
+      genre: ['Crime', 'Drama'],
+      score: 9
+    },
+    {
+      title: 'The Dark Knight',
+      year: 2008,
+      director: 'Christopher Nolan',
+      duration: '2h 32min',
+      genre: ['Action', 'Crime', 'Drama', 'Thriller'],
+      score: 9
+    },
+    {
+      title: '12 Angry Men',
+      year: 1957,
+      director: 'Sidney Lumet',
+      duration: '1h 36min',
+      genre: ['Crime', 'Drama'],
+      score: 8.9
+    },
+    {
+      title: 'Schindler"s List',
+      year: 1993,
+      director: 'Steven Spielberg',
+      duration: '3h 15min',
+      genre: ['Biography', 'Drama', 'History'],
+      score: 8.9
+    },
+    {
+      title: 'Pulp Fiction',
+      year: 1994,
+      director: 'Quentin Tarantino',
+      duration: '2h 34min',
+      genre: ['Crime', 'Drama'],
+      score: 8.9
+    },
+    {
+      title: 'The Lord of the Rings: The Return of the King',
+      year: 2003,
+      director: 'Peter Jackson',
+      duration: '3h 21min',
+      genre: ['Adventure', 'Drama', 'Fantasy'],
+      score: 8.9
+    },
+    {
+      title: 'Il buono, il brutto, il cattivo',
+      year: 1966,
+      director: 'Sergio Leone',
+      duration: '3h 2min',
+      genre: ['Western'],
+      score: 8.9
+    },
+    {
+      title: 'Fight Club',
+      year: 1999,
+      director: 'David Fincher',
+      duration: '2h 19min',
+      genre: ['Drama'],
+      score: 8.8
+    },
+    {
+      title: 'The Lord of the Rings: The Fellowship of the Ring',
+      year: 2001,
+      director: 'Peter Jackson',
+      duration: '2h 58min',
+      genre: ['Adventure', 'Drama', 'Fantasy'],
+      score: 8.8
+    },
+    {
+      title: 'Forrest Gump',
+      year: 1994,
+      director: 'Robert Zemeckis',
+      duration: '2h 22min',
+      genre: ['Comedy', 'Drama', 'Romance'],
+      score: 8.8
+    },
+    {
+      title: 'Star Wars: Episode V - The Empire Strikes Back',
+      year: 1980,
+      director: 'Irvin Kershner',
+      duration: '2h 4min',
+      genre: ['Action', 'Adventure', 'Fantasy', 'Sci-Fi'],
+      score: 8.8
+    },
+    {
+      title: 'Inception',
+      year: 2010,
+      director: 'Christopher Nolan',
+      duration: '2h 28min',
+      genre: ['Action', 'Adventure', 'Sci-Fi', 'Thriller'],
+      score: 8.8
+    },
+    {
+      title: 'The Lord of the Rings: The Two Towers',
+      year: 2002,
+      director: 'Peter Jackson',
+      duration: '2h 59min',
+      genre: ['Adventure', 'Drama', 'Fantasy'],
+      score: 8.7
+    },
+    {
+      title: 'One Flew Over the Cuckoo"s Nest',
+      year: 1975,
+      director: 'Milos Forman',
+      duration: '2h 13min',
+      genre: ['Drama'],
+      score: 8.7
+    },
+    {
+      title: 'Goodfellas',
+      year: 1990,
+      director: 'Martin Scorsese',
+      duration: '2h 26min',
+      genre: ['Crime', 'Drama'],
+      score: 8.7
+    },
+    {
+      title: 'The Matrix',
+      year: 1999,
+      director: 'Lana Wachowski',
+      duration: '2h 16min',
+      genre: ['Action', 'Sci-Fi'],
+      score: 8.7
+    },
+    {
+      title: 'Shichinin no samurai',
+      year: 1954,
+      director: 'Akira Kurosawa',
+      duration: '3h 27min',
+      genre: ['Adventure', 'Drama'],
+      score: 8.7
+    },
+    {
+      title: 'Star Wars',
+      year: 1977,
+      director: 'George Lucas',
+      duration: '2h 1min',
+      genre: ['Action', 'Adventure', 'Fantasy', 'Sci-Fi'],
+      score: 8.7
+    },
+    {
+      title: 'Cidade de Deus',
+      year: 2002,
+      director: 'Fernando Meirelles',
+      duration: '2h 10min',
+      genre: ['Crime', 'Drama'],
+      score: 8.7
+    },
+    {
+      title: 'Se7en',
+      year: 1995,
+      director: 'David Fincher',
+      duration: '2h 7min',
+      genre: ['Crime', 'Drama', 'Mystery', 'Thriller'],
+      score: 8.6
+    },
+    {
+      title: 'The Silence of the Lambs',
+      year: 1991,
+      director: 'Jonathan Demme',
+      duration: '1h 58min',
+      genre: ['Crime', 'Drama', 'Thriller'],
+      score: 8.6
+    },
+    {
+      title: 'It"s a Wonderful Life',
+      year: 1946,
+      director: 'Frank Capra',
+      duration: '2h 10min',
+      genre: ['Drama', 'Family', 'Fantasy'],
+      score: 8.6
+    },
+    {
+      title: 'La vita è bella',
+      year: 1997,
+      director: 'Roberto Benigni',
+      duration: '1h 56min',
+      genre: ['Comedy', 'Drama', 'War'],
+      score: 8.6
+    },
+    {
+      title: 'The Usual Suspects',
+      year: 1995,
+      director: 'Bryan Singer',
+      duration: '1h 46min',
+      genre: ['Crime', 'Drama', 'Mystery', 'Thriller'],
+      score: 8.6
+    },
+    {
+      title: 'Léon',
+      year: 1988,
+      director: 'Luc Besson',
+      duration: '1h 50min',
+      genre: ['Crime', 'Drama', 'Thriller'],
+      score: 8.6
+    },
+    {
+      title: 'Saving Private Ryan',
+      year: 1998,
+      director: 'Steven Spielberg',
+      duration: '2h 49min',
+      genre: ['Drama', 'War'],
+      score: 8.6
+    }]
+*/
+
+
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {}
+function getAllDirectors(moviesArray) {
+    const directorArray = moviesArray.map(element => element.director)
+    return directorArray
+}
+
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
+function howManyMovies(moviesArray) {
+    
+    
+    if(moviesArray === null || moviesArray === undefined || moviesArray.length===0){
+        return 0
+    }
+    else{
+        const spielbergDramaMovies = moviesArray.filter(element => element.director ==='Steven Spielberg' && element.genre.includes('Drama'));
+       // console.log(spielbergMovies)
+        return spielbergDramaMovies.length
+    }
+}
+//howManyMovies(movies)
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+function scoresAverage(moviesArray) {
+    if (moviesArray.length === 0) {
+        return 0
+    }
+    const avgScore = moviesArray.reduce(
+        (acc, movie) => { 
+            if (!movie.score){
+                return acc
+            }
+            return acc + movie.score},0)
+
+    return (Math.round(avgScore/moviesArray.length*100))/100
+
+}
+//scoresAverage(movies)
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(moviesArray) {}
+function dramaMoviesScore(moviesArray) {
+    const dramaMovies = moviesArray.filter(element => element.genre.includes('Drama'));
+    //console.log(dramaMovies)
+    if(dramaMovies === undefined || dramaMovies.length===0){
+        return 0
+    }
+    else{
+    const avgDramaMovies = dramaMovies.reduce(
+        (accumulator, element )=> { 
+        const sum =  accumulator+element.score; 
+        return sum},
+        0);
+            console.log(Math.round(avgDramaMovies/dramaMovies.length*10)/10)
+    return  Math.round(avgDramaMovies/dramaMovies.length*100)/100
+}
+}
+//dramaMoviesScore(movies)
+
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
+function orderByYear(moviesArray) {
+    const orderMoviesYear = moviesArray.toSorted((a,b)=> {
+        if(a.year===b.year){
+            return a.title.localeCompare(b.title)
+        }
+        return a.year-b.year})
+
+    return orderMoviesYear
+}
+//orderByYear(movies)
+
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically(moviesArray) {}
+function orderAlphabetically(moviesArray) {
+    const titleArray = moviesArray.map(element => element.title)
+    const sortedArray = titleArray.sort()
+    //console.log(titleArray.length)
+    return titleArray.slice(0,20)
+
+}
+orderAlphabetically (movies)
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-function turnHoursToMinutes(moviesArray) {}
+function turnHoursToMinutes(moviesArray) {
+
+    return
+}
+turnHoursToMinutes(movies)
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
-function bestYearAvg(moviesArray) {}
+function bestYearAvg(moviesArray) {
+
+    return
+}
+bestYearAvg(movies)
