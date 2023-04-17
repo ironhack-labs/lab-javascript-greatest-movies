6d9c7c4a2941244b9bc16756eb41312daee7262b
lortes <lortes95@gmail.com>
2023-04-17 23:46:55 +0200

    done

--- src/movies.js
+++ src/movies.js
@@ -1,22 +1,138 @@
+
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {}
+
+
+function getAllDirectors(moviesArray) {
+    const allDirectors = moviesArray.map((eachDirector) => {
+        return eachDirector.director
+    })
+    return allDirectors
+}
+
+//console.log(getAllDirectors(movies))
+
+
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
+function howManyMovies(moviesArray) {
+    const moviesDramaSpilberg = moviesArray.filter((eachMovie) =>{
+            return eachMovie.director === "Steven Spielberg" && eachMovie.genre.includes("Drama")
+    })
+
+        if(moviesDramaSpilberg.length === 0 && moviesDramaSpilberg.director !== "Steven Spielberg"){
+            return 0
+        }else{
+            return moviesDramaSpilberg.length;
+        }
+    
+}
+
+//console.log(howManyMovies(movies))
+
+
+
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+function scoresAverage(moviesArray) {
+
+        const scoreAvg = moviesArray.reduce((acc, eachMovie) => {
+
+            if(moviesArray.length === 0){
+                return 0;
+            }else if(!eachMovie.score){
+                eachMovie.score = 0;
+                return acc + eachMovie.score / moviesArray.length   
+            }else{
+                return acc + eachMovie.score / moviesArray.length   
+            }
+
+        },0)
+     
+        return Number(scoreAvg.toFixed(2))
+}
+
+//console.log(scoresAverage(movies))
+
+
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(moviesArray) {}
+function dramaMoviesScore(moviesArray) {
+    const moviesDrama = moviesArray.filter(eachMovie => {
+        return eachMovie.genre.includes("Drama")
+    })
+    
+    const scoreMoviesDrama = moviesDrama.reduce((acc, eachMovie) => {
+        
+        if(moviesDrama === 0){
+            return 0
+        }else{
+            return acc + eachMovie.score / moviesDrama.length
+        }
+        
+    },0)
+
+    return Number(scoreMoviesDrama.toFixed(2))
+
+}
+
+
+
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
+function orderByYear(moviesArray) {
+
+    const moviesCopy = JSON.parse(JSON.stringify(moviesArray)) 
+
+    const moviesOrderByYear = moviesCopy.sort((a,b) => {
+        if(a.year < b.year){
+            return -1
+        }else if(a.year > b.year){
+            return 1
+        }else if(a.year === b.year){
+            if(a.title < b.title){
+                return -1
+            }else{
+                return 1
+            }
+        }else{
+            return 0
+        }
+    })
+  //  console.log(moviesOrderByYear)
+    return moviesOrderByYear
+}
+
+
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically(moviesArray) {}
+function orderAlphabetically(moviesArray) {
+    const moviesTitle = moviesArray.map((eachTitle) => {
+       // console.log(eachTitle.title)
+        return eachTitle.title
+    })
+
+    moviesTitle.sort();
+
+/*
+    const moviesOrderByTitle = moviesTitle.sort((a,b) => {
+        if(a.title < b.title){
+            return -1
+        }else if(a.title > b.title){
+            return 1
+        }else{
+            return 0
+        }
+    })
+*/
+
+    if(moviesTitle.length > 20){
+        return moviesTitle.slice(0,20)
+    }
+    return moviesTitle
+}
+
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes(moviesArray) {}
