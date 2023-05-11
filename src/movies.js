92757c608d3317a912eb9a4d94f809ee8aaf1f7e
KRcamara <113925794+KRcamara@users.noreply.github.com>
2023-05-11 17:54:32 -0600

    Primera parte

--- src/movies.js
+++ src/movies.js
@@ -1,16 +1,45 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {}
+function getAllDirectors(moviesArray) {
+    return moviesArray.map(movie => movie.director);
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
+function howManyMovies(moviesArray) {
+    return moviesArray.filter(moviesArray => moviesArray.director === "Steven Spilberg" && moviesArray.genre.filter ("Drama")).length;
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+function scoresAverage(moviesArray) {
+    if (moviesArray.length === 0){
+        return 0;
+    }
+    const sumeOfScores = moviesArray.reduce((total, movie) => {
+        if (movie.hasOwnProperty("score")) {
+            return total + movie.score;
+        }
+        return total;
+    }, 0);
+    const averageScore = sumeOfScores/moviesArray.length;
+    return parseFloat(averageScore.toFixed(2));
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(moviesArray) {}
+function dramaMoviesScore(moviesArray) {
+    const dramaMovies = moviesArray.filter(movie => movie.genre.includes("Drama"));
+    if(dramaMovies.length === 0){
+        return 0;
+    }
+    const sumeOfScoresDram = dramaMovies.reduce((total, movie) => {
+       if(movie.hasOwnProperty("score")){
+        return total + movie.score;
+       } 
+       return total;
+    },0);
+    const averageScore = sumeOfScoresDram/dramaMovies.length;
+    return parseFloat(averageScore.toFixed(2));
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear(moviesArray) {}
