119f8e873bae55e52dc487acb0546ea4c7e47ed5
Dok <pierredok@outlook.fr>
2023-05-15 17:21:34 +0200

    Add exercise 2 and 3 Done

--- src/movies.js
+++ src/movies.js
@@ -22,25 +22,45 @@ function clearAllDirector(arr){
     return arrClearDirector ;
 }
 
-
-
-
-
-
-
-
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
+function howManyMovies(moviesArray) {
+    
+    let counter = 0;
+
+    const spielbergMovie = moviesArray.filter(function(movie){    
+        if(movie.director === 'Steven Spielberg' && movie.genre.includes('Drama')){
+            counter++
+        }
+    });
+    return counter
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+function scoresAverage(moviesArray) {
+    if(moviesArray.length === 0){
+        return 0
+    }
+   
+    const totalScore = moviesArray.reduce(function(sum, movie){
+        if(movie.score === undefined){
+            movie.score = 0
+        }
+        return sum + movie.score
+    }, 0);
+
+    let avgScore = totalScore / moviesArray.length;
+    return Math.round(avgScore * 100) / 100;
+}
+
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesScore(moviesArray) {}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
-
+function orderByYear(moviesArray) {
+    const copyMovies = [...moviesArray]
+}
+    
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically(moviesArray) {}
 
