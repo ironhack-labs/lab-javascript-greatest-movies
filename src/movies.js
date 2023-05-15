7a96fb470b2d2437ae6a95677ec949d0885ccc1b
Lucas Silva <gaviao.lucas@gmail.com>
2023-05-15 23:09:12 +0100

    4 out of 6

--- src/movies.js
+++ src/movies.js
@@ -1,16 +1,63 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {}
+function getAllDirectors(moviesArray) {
+    const onlyDirectors = moviesArray.map((element) => element.director);
+    return onlyDirectors;
+};
+
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
+function howManyMovies(moviesArray) {
+    if(moviesArray.length === 0){
+        return 0;
+    }
+    let count = 0;
+    moviesArray.filter((object) => {
+     if(object.director === 'Steven Spielberg' && object.genre.includes('Drama')){
+        count++; 
+        }
+    }); 
+    return count;
+};
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+function scoresAverage(moviesArray) {
+ if(moviesArray.length === 0){
+      return 0;
+}
+ let sumScores =  moviesArray.reduce((acc, currentV) => { 
+     if(!currentV.score){   
+            currentV.score = 0;
+    };  
+        return acc + currentV.score}, 0);
+
+        let result = sumScores / moviesArray.length
+    return Number(result.toFixed(2));
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(moviesArray) {}
+function dramaMoviesScore(moviesArray) {
+    
+    let filteredMovies = moviesArray.filter((element) => {
+        return element.genre.includes('Drama') && element.score;
+    });
+
+    if(filteredMovies.length === 0){
+        return 0;
+    } else {
+        let dramaScore = 
+            filteredMovies.reduce((acc, currentV) => {
+                if(typeof currentV.score !== 'number'){
+                    currentV.score = 0;
+                    return acc + currentV.score;
+                } else {
+                return acc + currentV.score;
+            }
+        }, 0) /filteredMovies.length;
+     return Math.round(dramaScore * 100) / 100;    
+    }
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear(moviesArray) {}
