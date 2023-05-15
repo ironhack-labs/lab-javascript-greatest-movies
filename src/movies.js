9df32382c25f648faee0ac536f3027355853b49b
Filipe Ferreira <filipeferreira94@gmail.com>
2023-05-15 18:51:43 +0100

    iteration 5 and 6 completed

--- src/movies.js
+++ src/movies.js
@@ -56,10 +56,35 @@ function dramaMoviesScore(moviesArray) {
 }
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
+function orderByYear(moviesArray) {
+	let moviesArrayCopy = [...moviesArray];
+	let sortedByYear = moviesArrayCopy.sort((a, b) => {
+		if (a.year !== b.year) {
+			return a.year - b.year;
+		} else {
+			if (a.title > b.title) return 1;
+			if (a.title < b.title) return -1;
+		}
+	});
+
+	return sortedByYear;
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically(moviesArray) {}
+
+function orderAlphabetically(moviesArray) {
+	let moviesArrayCopy = [...moviesArray];
+	let sortedAlphabetic = moviesArrayCopy.sort((a, b) => {
+		if (a.title > b.title) return 1;
+		if (a.title < b.title) return -1;
+	});
+
+	let sortedAlphabeticTitles = sortedAlphabetic.map(
+		(movieObj) => movieObj.title
+	);
+
+	return sortedAlphabeticTitles.slice(0, 20);
+}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes(moviesArray) {}
