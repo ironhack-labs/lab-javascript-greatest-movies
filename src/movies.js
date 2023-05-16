1f1f7adf3bb277e1a047fbdb7870191a01665fa7
Juan Diego Sanchez <diegosanchezperez93@gmail.com>
2023-05-16 02:35:45 +0200

    Fifth iteration

--- src/movies.js
+++ src/movies.js
@@ -49,7 +49,11 @@ function dramaMoviesScore(moviesArray) {
 }
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
+function orderByYear(moviesArray) {
+  const sortedMovies = moviesArray.sort((a, b) => a.year - b.year);
+
+  return sortedMovies;
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically(moviesArray) {}
