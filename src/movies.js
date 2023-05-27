145cf683bf6b63625e948c2dafb20c9228e64230
KRcamara <113925794+KRcamara@users.noreply.github.com>
2023-05-27 17:50:31 -0600

    Solved lab

--- src/movies.js
+++ src/movies.js
@@ -7,8 +7,13 @@ function getAllDirectors(moviesArray) {
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 function howManyMovies(moviesArray) {
-    return moviesArray.filter(moviesArray => moviesArray.director === "Steven Spilberg" && moviesArray.genre.filter ("Drama")).length;
+    const filteredMovies = moviesArray.filter(movie =>
+    movie.director === "Steven Spielberg" && movie.genre.includes("Drama")
+    );
+    return filteredMovies.length;
 }
+const numberOfMovies = howManyMovies(movies);
+console.log(numberOfMovies);
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
 function scoresAverage(moviesArray) {
@@ -42,13 +47,50 @@ function dramaMoviesScore(moviesArray) {
 }
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
+function orderByYear(moviesArray) {
+    const sortedMovies = [...moviesArray];
+
+  sortedMovies.sort((a, b) => {
+        if (a.year !== b.year) {
+      return a.year - b.year;
+    } else {
+        return a.title.localeCompare(b.title);
+    }
+  });
+
+  return sortedMovies;
+}
+
+const sortedMovies = orderByYear(moviesArray);
+console.log(sortedMovies);
+    
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically(moviesArray) {}
+function orderAlphabetically(moviesArray) {
+    const sortedMovies = [...moviesArray];
+    sortedMovies.sort((a, b) => a.title.localeCompare(b.title));
+    const titles = sortedMovies.map(movie => movie.title);
+    return titles.slice(0, 20);
+}
+const sortedTitles = orderAlphabetically(moviesArray);
+console.log(sortedTitles);
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-function turnHoursToMinutes(moviesArray) {}
+function turnHoursToMinutes(moviesArray) {
+        const modifiedMovies = moviesArray.map(movie => {
+        const modifiedMovie = { ...moviesArray };
+        const durationParts = movie.duration.split(' ');
+        const hours = parseInt(durationParts[0], 10) || 0;
+        const minutes = parseInt(durationParts[1], 10) || 0;
+        const durationInMinutes = hours * 60 + minutes;
+        modifiedMovie.duration = durationInMinutes;
+        return modifiedMovie;
+        });
+        return modifiedMovies;
+      }
+      const modifiedMovies = turnHoursToMinutes(moviesArray);
+      console.log(modifiedMovies); 
+
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
 function bestYearAvg(moviesArray) {}
