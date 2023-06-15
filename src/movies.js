fa3681e65df86621f00d20f89b57e01c741d8ad5
Luis Ramirez <rhluisenrique@gmail.com>
2023-06-14 20:58:51 -0400

    Iteration 5 - 6

--- src/movies.js
+++ src/movies.js
@@ -2,23 +2,30 @@
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
 function getAllDirectors(moviesArray) {
-
-     
+ 
     const newArray = moviesArray.map( (element) => element.director)
     return newArray;
+
 }
 
 //Bonus 1.1:
 
-function filteringDirectors(moviesArray){
+function filteringDirectors(moviesArray, getDirectors) {
+
     const uniqueDirectors = [];
-    const allDirectors = getAllDirectors(moviesArray)
+
+    const allDirectors = getDirectors(moviesArray)
+
     allDirectors.forEach((element) => {
+
         if(!uniqueDirectors.includes(element)){
+
             uniqueDirectors.push(element)
         }
     })
+
     return uniqueDirectors;
+
 }
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
@@ -83,10 +90,25 @@ function dramaMoviesScore(moviesArray) {
 }
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
+function orderByYear(moviesArray) {
+    const sortedArray = moviesArray.slice().sort((a, b) => {
+        if( a.year === b.year ){
+            return a.title.localeCompare(b.title)
+        } else {
+            return a.year - b.year
+        }
+    })
+    return sortedArray
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically(moviesArray) {}
+function orderAlphabetically(moviesArray) {
+
+    const sortedArray = orderByYear(moviesArray)
+    return sortedArray.slice(0, 20).map(el => el.title);
+    // alguien me puede decir porque funciona este codigo? La #5 tambien ordena por año no?
+}
+
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes(moviesArray) {}
