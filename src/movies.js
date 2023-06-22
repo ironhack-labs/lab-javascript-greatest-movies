201c49daf2ff2ca340af82546389cb5eac10a0a4
UlviAzizli <133907952+UlviAzizli@users.noreply.github.com>
2023-06-22 15:53:31 +0200

    Solved lab

--- src/movies.js
+++ src/movies.js
@@ -40,20 +40,46 @@ function dramaMoviesScore(moviesArray) {
 
 
 
-
-
-
-
-
-
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
+function orderByYear(moviesArray) {
+    const moviesArrayCopy = [...moviesArray];
+    return moviesArrayCopy.sort((a,b) => {
+        if (a.year > b.year) {
+            return 1;
+        } else if (b.year > a.year) {
+            return -1;
+        } else {
+            if (a.title > b.title) {
+                return 1;
+            }
+            else if (b.title > a.title) {
+                return -1;
+            }
+            return 0;
+        }
+    })
+};
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically(moviesArray) {}
+function orderAlphabetically(moviesArray) {
+    const moviesArrayCopy = [...moviesArray];
+    moviesArrayCopy.sort((a,b) => {
+        if (a.title > b.title) {
+            return 1;
+        }
+        else if (b.title > a.title) {
+            return -1;
+        }
+        else {
+            return 0;
+        }
+    });
 
-// BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-function turnHoursToMinutes(moviesArray) {}
+    const titleArray = moviesArrayCopy.slice(0, 20);
+    const newTitleArray20 = [];
+    titleArray.forEach(function (movie) {
+        newTitleArray20.push(movie.title);
+    });
+    return newTitleArray20;
+}
 
-// BONUS - Iteration 8: Best yearly score average - Best yearly score average
-function bestYearAvg(moviesArray) {}
