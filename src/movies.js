531786b09e0174e9b42af290c9129ec517b8e796
Joao Borrega <joaoborrega18@gmail.com>
2023-07-13 20:01:30 +0100

    LAB | Greatest Movies of All Time

--- src/movies.js
+++ src/movies.js
@@ -1,22 +1,70 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {}
+function getAllDirectors(moviesArray) {
+    let newArray = moviesArray.map((allDirectors) => {
+        return allDirectors.director;
+    });
+    return newArray;
+}
+
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
+function howManyMovies(moviesArray) {
+    let numberMovies = moviesArray.filter((spielberDirector) => {
+        if(spielberDirector.director === "Steven Spielberg" && spielberDirector.genre.includes("Drama")) {
+            return spielberDirector;
+        }
+    })
+    return numberMovies.length;
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+function scoresAverage(moviesArray) {
+    if (moviesArray.length === 0) {
+        return 0;
+    }
+    
+    let totalMovies = moviesArray.reduce((sum,review) => {
+        if(review.score === " " || !review.score) {
+            review.score = 0;
+        }
+        return sum + review.score;
+    }, 0);
+    return Math.round(totalMovies / moviesArray.length *100)/100;
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(moviesArray) {}
+function dramaMoviesScore(moviesArray) {
+    let dramaMovies = moviesArray.filter((movie) => {
+        if (movie.genre.includes("Drama")) {
+            return movie;
+        }
+    })
+    return scoresAverage(dramaMovies)
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
+function orderByYear(moviesArray) {
+    let newArray = [...moviesArray]
+return newArray.sort((a,b) => {
+    if (a.year > b.year) return 1
+    else if(a.year < b.year) return -1
+    else if(a.title > b.title) return 1
+    else if(a.title < b.title) return -1
+    return 0
+})
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically(moviesArray) {}
+function orderAlphabetically(moviesArray) {
+    let titlesArr = moviesArray.map((films) => {
+        return films.title;
+    })
+    titlesArr.sort();
+
+    return titlesArr.slice(0, 20);
+}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes(moviesArray) {}
