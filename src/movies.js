38438b4c43cfa0b4f4387a6892e01b80a547dd90
EO <eo.code.wd@gmail.com>
2023-09-08 21:38:37 +0200

    solved -bonus 2&3

--- src/movies.js
+++ src/movies.js
@@ -24,25 +24,65 @@ function scoresAverage(moviesArray) {
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesScore(moviesArray) {
-  return !moviesArray
-    ? 0
-    : +(
-        moviesArray.reduce(
-          (acc, curr) =>
-            curr.genre === "Drama" ? acc + (curr.score || 0) : acc,
-          0
-        ) / moviesArray.length
-      ).toFixed(2);
+  let dramaMovies = moviesArray.filter((movie) =>
+    movie.genre.includes("Drama")
+  );
+  if (dramaMovies.genre === 0) {
+    return 0;
+  }
+  let dramaScore = +(
+    dramaMovies.reduce((acc, curr) => acc + curr.score, 0) / dramaMovies.length
+  ).toFixed(2);
+  if (!dramaScore) {
+    return 0;
+  }
+  return dramaScore;
+}
+// Iteration 5: Ordering by year - Order by year, ascending (in growing order)
+function orderByYear(moviesArray) {
+  const newArr = [...moviesArray];
+  newArr.sort((a, b) => {
+    if (a.year !== b.year) {
+      return a.year < b.year ? -1 : a.year > b.year ? 1 : 0;
+    }
+    return a.title.localeCompare(b.title);
+  });
+  return newArr;
 }
 
-// Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
-
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically(moviesArray) {}
+function orderAlphabetically(moviesArray) {
+  const titleArr = [...moviesArray];
+  titleArr.sort((a, b) => {
+    if (a.title !== b.title) {
+      return a.title.localeCompare(b.title);
+    }
+  });
+  newarr = [];
+  for (let i = 0; i < 20 && i < titleArr.length; i++) {
+    newarr.push(titleArr[i].title);
+  }
+  return newarr;
+}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes(moviesArray) {}
 
+
+
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
-function bestYearAvg(moviesArray) {}
+function bestYearAvg(moviesArray) {
+
+const srtYear = {};
+movies.map((element) => {
+  const year = element.year;
+  const score = element.score || 0;
+  if (!srtYear[year]) {
+    srtYear[year] = { tScore: 0, yearcount: 0 };
+    srtYear[year].tScore = score;
+    srtYear[year].yearcount++;
+  }
+});
+
+console.log(srtYear);
+}
