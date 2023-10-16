6d1519dd7f37bbfa8ef7693b8a6f1be7e30faa18
Marcelo Melo <melooliveira31@gmail.com>
2023-10-16 17:12:22 +0100

    Solved Lab

--- src/movies.js
+++ src/movies.js
@@ -1,4 +1,3 @@
-
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
@@ -17,46 +16,6 @@ function howManyMovies(moviesArray) {
 
     return stevenSpielberg.length
 }
-// console.log(howManyMovies([
-//     {
-//       title: 'The Shawshank Redemption',
-//       year: 1994,
-//       director: 'Frank Darabont',
-//       duration: '2h 22min',
-//       genre: ['Crime', 'Drama'],
-//       score: 9.3
-//     },
-//     {
-//       title: 'The Godfather',
-//       year: 1972,
-//       director: 'Francis Ford Coppola',
-//       duration: '2h 55min',
-//       genre: ['Crime', 'Drama'],
-//       score: 9.2
-//     },
-//     {
-//       title: 'The Godfather: Part II',
-//       year: 1974,
-//       director: 'Francis Ford Coppola',
-//       duration: '3h 22min',
-//       genre: ['Crime', 'Drama'],
-//       score: 9
-//     },
-//     {
-//       title: 'The Dark Knight',
-//       year: 2008,
-//       director: 'Christopher Nolan',
-//       duration: '2h 32min',
-//       genre: ['Action', 'Crime', 'Drama', 'Thriller'],
-//       score: 9
-//     },{
-//         title: 'Raiders of the Lost Ark',
-//         year: 1981,
-//         director: 'Steven Spielberg',
-//         duration: '1h 55min',
-//         genre: ['Action', 'Adventure'],
-//         score: 8.5
-//       }]))
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
 function scoresAverage(moviesArray) {
@@ -81,47 +40,6 @@ function scoresAverage(moviesArray) {
     return Math.round((sum/numberOfMovies)*100)/100
 }
 
-// console.log(scoresAverage([
-//     {
-//       title: 'The Shawshank Redemption',
-//       year: 1994,
-//       director: 'Frank Darabont',
-//       duration: '2h 22min',
-//       genre: ['Crime', 'Drama'],
-//       score: 9.3
-//     },
-//     {
-//       title: 'The Godfather',
-//       year: 1972,
-//       director: 'Francis Ford Coppola',
-//       duration: '2h 55min',
-//       genre: ['Crime', 'Drama'],
-//       score: 9.2
-//     },
-//     {
-//       title: 'The Godfather: Part II',
-//       year: 1974,
-//       director: 'Francis Ford Coppola',
-//       duration: '3h 22min',
-//       genre: ['Crime', 'Drama'],
-//       score: 9
-//     },
-//     {
-//       title: 'The Dark Knight',
-//       year: 2008,
-//       director: 'Christopher Nolan',
-//       duration: '2h 32min',
-//       genre: ['Action', 'Crime', 'Drama', 'Thriller'],
-//       score: 9
-//     },{
-//         title: 'Raiders of the Lost Ark',
-//         year: 1981,
-//         director: 'Steven Spielberg',
-//         duration: '1h 55min',
-//         genre: ['Action', 'Adventure'],
-//         score: 8.5
-//       }]))
-
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesScore(moviesArray) {
 
@@ -140,33 +58,6 @@ function dramaMoviesScore(moviesArray) {
 
     return Math.round((avg/dramaMovies.length)*100)/100
 }
-
-// console.log(dramaMoviesScore([
-//     {
-//       title: 'The Shawshank Redemption',
-//       year: 1994,
-//       director: 'Frank Darabont',
-//       duration: '2h 22min',
-//       genre: ['Crime', 'Drama'],
-//       score: 9.3
-//     },
-//     {
-//       title: 'The Godfather',
-//       year: 1972,
-//       director: 'Francis Ford Coppola',
-//       duration: '2h 55min',
-//       genre: ['Crime', 'Drama'],
-//       score: 9.2
-//     },
-//     {
-//       title: 'The Godfather: Part II',
-//       year: 1974,
-//       director: 'Francis Ford Coppola',
-//       duration: '3h 22min',
-//       genre: ['Crime', 'Drama'],
-//       score: 9
-//     }]))
-
 
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
@@ -187,33 +78,6 @@ function orderByYear(moviesArray) {
 
     return ascendingOrder
 }
-
-// console.log(orderByYear([
-//     {
-//       title: 'The Shawshank Redemption',
-//       year: 1994,
-//       director: 'Frank Darabont',
-//       duration: '2h 22min',
-//       genre: ['Crime', 'Drama'],
-//       score: 9.3
-//     },
-//     {
-//       title: 'The Godfather',
-//       year: 1972,
-//       director: 'Francis Ford Coppola',
-//       duration: '2h 55min',
-//       genre: ['Crime', 'Drama'],
-//       score: 9.2
-//     },
-//     {
-//       title: 'The Godfather: Part II',
-//       year: 1974,
-//       director: 'Francis Ford Coppola',
-//       duration: '3h 22min',
-//       genre: ['Crime', 'Drama'],
-//       score: 9
-//     }]))
-
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically(moviesArray) {
@@ -251,32 +115,6 @@ function turnHoursToMinutes(moviesArray) {
 
     return convertedTime
 }
-
-console.log(turnHoursToMinutes([
-    {
-      title: 'The Shawshank Redemption',
-      year: 1994,
-      director: 'Frank Darabont',
-      duration: '2h 22min',
-      genre: ['Crime', 'Drama'],
-      score: 9.3
-    },
-    {
-      title: 'The Godfather',
-      year: 1972,
-      director: 'Francis Ford Coppola',
-      duration: '2h 55min',
-      genre: ['Crime', 'Drama'],
-      score: 9.2
-    },
-    {
-      title: 'The Godfather: Part II',
-      year: 1974,
-      director: 'Francis Ford Coppola',
-      duration: '3h 22min',
-      genre: ['Crime', 'Drama'],
-      score: 9
-    }]))
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
 function bestYearAvg(moviesArray) {
