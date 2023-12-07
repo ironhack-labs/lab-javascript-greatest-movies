28888b1b2d769a09c00916341ffb9735578c0dc5
Juan Morales Martinez <juanmoralesmartinez@Juans-MacBook-Pro.local>
2023-12-07 21:11:56 +0100

    Solved lab

--- src/movies.js
+++ src/movies.js
@@ -1,3 +1,5 @@
+
+// My pull link is: https://github.com/ironhack-labs/lab-javascript-greatest-movies/pull/3667
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
@@ -50,16 +52,34 @@ function dramaMoviesScore(moviesArray) {
     },0)
 
     const averageDramaScore = dramaSum/dramaMovies.length
-    return averageDramaScore
+    const roundedResult = parseFloat((averageDramaScore).toFixed(2))
+    return roundedResult
 
 }
 
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
+function orderByYear(moviesArray) {
+    const copyMoviesArray =[...moviesArray]
+    const moviesByYear = copyMoviesArray.sort((a,b)=>{
+        if (a.year===b.year){
+            return(a.title.localeCompare(b.title))}
+        else{ return a.year-b.year}
+     })
+   return moviesByYear
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically(moviesArray) {}
+function orderAlphabetically(moviesArray) {
+    const copyMoviesArray =[...moviesArray]
+    const moviesByName = copyMoviesArray.sort((a,b)=>
+    a.title.localeCompare(b.title))
+    const twentyMovies = moviesByName.slice(0,20)
+    const onlyTitles = twentyMovies.map(element=>element.title)
+    
+    return onlyTitles
+    
+}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes(moviesArray) {}
