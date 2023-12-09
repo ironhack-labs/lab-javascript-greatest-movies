b230edc4e1aa296f6a5e50b0193b0323f006af2a
Mohammed Ashour <mashour8@gmail.com>
2023-12-09 05:27:48 +0200

    Solved lab 50%

--- src/movies.js
+++ src/movies.js
@@ -61,10 +61,29 @@ function dramaMoviesScore(moviesArray) {
 }
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
+function orderByYear(moviesArray) {
+    newArr = [...moviesArray]
+    newArr.sort((a,b)=> {
+        if(a.year === b.year){
+            return a.title.localeCompare(b.title)
+        }
+        return a.year - b.year
+    })
+    return newArr
+}
+
+
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically(moviesArray) {}
+function orderAlphabetically(moviesArray) {
+    let newArr = [...moviesArray]
+    newArr.sort((a,b) => {
+        return a.title.localeCompare(b.title)
+    })
+    newArr = newArr.slice(0,20)
+    let sortedMovies = newArr.map(element => element.title)
+    return sortedMovies
+}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes(moviesArray) {}
