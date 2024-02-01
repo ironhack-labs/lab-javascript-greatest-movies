0059f60bf619009df1c738264ac590760167a5a1
Idir FOUQUET <fouquetidir@gmail.com>
2024-02-01 21:27:45 +0100

    La vida es un carnaval, no hay que llorar

--- src/movies.js
+++ src/movies.js
@@ -7,29 +7,55 @@ function getAllDirectors(moviesArray) {
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 function howManyMovies(moviesArray) {
-  const stevenMovies = moviesArray.filter(
-    (movie) => movie.director == "Steven Spielberg"
-  );
-
-  const filterarray = stevenMovies.filter((movie) =>
-    movie.genre.filter((element) => element === "Drama")
-  );
-
-  return filterarray;
+    const stevenMovies = moviesArray.filter(
+        (movie) => movie.director == "Steven Spielberg"
+      );
+    
+     return stevenMovies.filter(element => element.genre.includes("Drama")).length
 }
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
 function scoresAverage(moviesArray) {
-    let reviewArray = moviesArray.map((element) => element.score)
-    let sum = reviewArray.reduce((accumulator, currentvalue) => {accumulator + currentvalue}, 0)
-   return sum 
+    if (moviesArray.length == 0)  {
+        return 0
+    } 
+  
+  
+    else {
+  
+       const arrayFilter = moviesArray.filter(element => element.score)
+       const scoreList = arrayFilter.map(element => element.score)
+       let average = scoreList.reduce((accumulator, currentValue) => accumulator + currentValue, 0) / scoreList.length
+      return Number(average.toFixed(2))
+    }
+
+
 }
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(moviesArray) {}
+function dramaMoviesScore(moviesArray) {
+    const listfiltered = moviesArray.filter(element => element.genre.includes("Drama"))
+
+    if (listfiltered.length == 0) {
+        return 0
+    } else {
+        const scoreList = listfiltered.map(element => element.score)
+        const sum = scoreList.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
+        let average = sum / scoreList.length
+        average = average.toFixed(2)
+        return Number.parseFloat(average)
+    }
+
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
+function orderByYear(moviesArray) {
+    const sortedByYearArray = [...moviesArray] 
+    sortedByYearArray.sort((a, b) =>  
+       a.year - b.year) 
+       
+    return sortedByYearArray
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically(moviesArray) {}
