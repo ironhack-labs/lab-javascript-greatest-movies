04afed99bd9956806fa88d4c541a0f553e0a2318
Eber Brown <eaba_89@hotmail.com>
2024-02-02 19:03:19 +0100

    js code for iteration 3 - scores average

--- src/movies.js
+++ src/movies.js
@@ -1,6 +1,38 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
+function getAllDirectors(moviesArray) {
+  if (moviesArray.length === 0) {
+    return;
+  }
+
+  const directors = moviesArray.map((movie) => {
+    return movie.director;
+  });
+
+  let uniqueDirectos = [];
+  directors.forEach((director) => {
+    if (!uniqueDirectos.includes(director)) {
+      uniqueDirectos.push(director);
+    }
+  });
+
+  //console.log(uniqueDirectos);
+  return uniqueDirectos;
+}
+
+// Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
+function howManyMovies(moviesArray) {
+  const stevenMovies = moviesArray.filter(
+    (movie) => movie.director === "Steven Spielberg"
+  );
+  const stevenDramaMovies = stevenMovies.filter((movie) =>
+    movie.genre.includes("Drama")
+  );
+
+  //console.log(stevenDramaMovies.length);
+  return stevenDramaMovies.length;
+}
 
 /* const movies = [
     {
@@ -11,41 +43,26 @@
       genre: ['Crime', 'Drama'],
       score: 9.3
     },] */
-
-
-function getAllDirectors(moviesArray) {
-
-    if (moviesArray.length === 0) {
-        return;
-    }
-
-    const directors = moviesArray.map(movie => {
-        return movie.director;
-    });
-
-    let uniqueDirectos = [];
-    directors.forEach(director => {
-        if (!uniqueDirectos.includes(director)) {
-            uniqueDirectos.push(director);
-        }
-    });
-
-    //console.log(uniqueDirectos);
-    return uniqueDirectos;
-}
-
-// Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {
-
-    const stevenMovies = moviesArray.filter(movie => movie.director === "Steven Spielberg");
-    const stevenDramaMovies = stevenMovies.filter(movie => movie.genre.includes("Drama"))
-
-    //console.log(stevenDramaMovies.length);
-    return stevenDramaMovies.length
-}
-
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+function scoresAverage(moviesArray) {
+  if (moviesArray.length === 0) {
+    return 0;
+  }
+
+  let totalScore = 0;
+  for (let i = 0; i < moviesArray.length; i++) {
+    if (!moviesArray[i].score || moviesArray[i].score == 0) {
+      continue;
+    } else {
+      totalScore += moviesArray[i].score;
+    }
+  }
+
+  let averageScore = totalScore / moviesArray.length;
+  //console.log(Math.round(averageScore * 100) / 100);
+  return Math.round(averageScore * 100) / 100;
+
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesScore(moviesArray) {}
