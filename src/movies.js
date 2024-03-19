91349b6beb795153b29935aef0bf10dd39225418
Felipe Calderero <felipecalderero@gmail.com>
2024-03-19 17:55:54 +0100

    lab completed

--- src/movies.js
+++ src/movies.js
@@ -36,7 +36,6 @@ function dramaMoviesScore(moviesArray) {
     const dramaMovies = moviesArray.filter((movie) =>
       movie.genre.includes("Drama")
     );
-    console.log(dramaMovies);
     let dramaTotalScore = 0;
     if (dramaMovies.length > 0) {
       dramaTotalScore = dramaMovies.reduce((acumulator, movie) => {
@@ -66,7 +65,6 @@ function orderByYear(moviesArray) {
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically(moviesArray) {
   const onlyTitles = moviesArray.map((movie) => movie.title);
-  console.log(onlyTitles);
   const twentyTitles = onlyTitles
     .toSorted((a, b) => {
       return a.localeCompare(b);
@@ -77,18 +75,68 @@ function orderAlphabetically(moviesArray) {
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes(moviesArray) {
-
-return moviesArray.map(movie => {
-
-
-    return movie;
-}
-
+  let newArray = moviesArray.map((movie) => {
+    const timeArray = movie.duration.split(" ");
+    let hours = 0;
+    let minutes = 0;
+    if (timeArray.length === 2) {
+      // If there are both hours and minutes
+      hours = Number(timeArray[0].slice(0, timeArray[0].length - 1));
+      minutes = Number(timeArray[1].slice(0, timeArray[1].length - 3));
+    } else if (timeArray.length === 1) {
+      // If there is only hours or minutes
+      if (timeArray[0].includes("h")) {
+        hours = Number(timeArray[0].slice(0, timeArray[0].length - 1));
+      } else if (timeArray[0].includes("min")) {
+        minutes = Number(timeArray[0].slice(0, timeArray[0].length - 3));
+      }
+    }
+    return {
+      title: movie.title,
+      year: movie.year,
+      director: movie.director,
+      duration: hours * 60 + minutes,
+      genre: movie.genre,
+      score: movie.score,
+    };
+  });
+  return newArray;
 }
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
-function bestYearAvg(moviesArray) {}
+function bestYearAvg(moviesArray) {
+  if (moviesArray.length > 0) {
+    // Group movies by year
+    const moviesByYear = moviesArray.reduce((acc, movie) => {
+      if (movie.year in acc) {
+        acc[movie.year].push(movie);
+      } else {
+        acc[movie.year] = [movie];
+      }
+      return acc;
+    }, {});
 
+    console.log(moviesByYear);
+
+    // Calculate average score for each year
+    const averageScoresByYear = Object.keys(moviesByYear).map((year) => ({
+      year: year,
+      averageScore: scoresAverage(moviesByYear[year]), // Assuming scoresAverage function is available
+    }));
+
+    // Sort years by average score in descending order to find the highest first
+    averageScoresByYear.sort(
+      (a, b) => b.averageScore - a.averageScore || a.year - b.year
+    );
+
+    // Considering the first element has the highest average score
+    const bestYear = averageScoresByYear[0];
+
+    return `The best year was ${bestYear.year} with an average score of ${bestYear.averageScore}`;
+  } else {
+    return null;
+  }
+}
 /*
 {
     title: 'Paper Moon',
